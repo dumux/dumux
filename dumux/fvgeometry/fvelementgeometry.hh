bc32926cd2fd8f0e705db6ba8508abe0e16d6d1d
Klaus Mosthaf <klmos@env.dtu.dk>
2008-02-15 15:59:05 +0000

    fvgeometry refined
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@164 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/fvgeometry/fvelementgeometry.hh
+++ dumux/fvgeometry/fvelementgeometry.hh
@@ -7,12 +7,14 @@ namespace Dune
 template<class G>
 class FVElementGeometry  
 {
+public:
 	enum{dim = G::dimension};
 	enum{maxNC = (dim < 3 ? 4 : 8)};
+	enum{maxNF = (dim < 3 ? 4 : 12)};
 	typedef typename G::ctype DT;
-    typedef typename G::Traits::template Codim<0>::Entity Entity;
-    typedef typename Entity::Geometry Geometry;
-
+   typedef typename G::Traits::template Codim<0>::Entity Entity;
+   typedef typename Entity::Geometry Geometry;
+    
 	struct SDValues 
 	{
 		FieldVector<DT, maxNC> shape;                    /* values of shape functions at ip              */
@@ -25,7 +27,8 @@ class FVElementGeometry
 	struct SubControlVolume /* FV intersected with element                  */
 	{
 		int co;                                                 /* # of corner                                                  */
-		FieldVector<DT, dim> center;                   /* node position                                                */
+		FieldVector<DT, dim> local;                   /* node position                                                */
+		FieldVector<DT, dim> global;                   /* node position                                                */
 	    DT volume;                                  /* volume (area) of scv                                 */
 	};                                     
 
@@ -38,7 +41,7 @@ class FVElementGeometry
 		SDValues sdv;                                  /* shape fcts, deriv. etc. at scv-faces */
 	};
 
-	typedef struct {
+	struct BoundaryFace {
 		int co;                                                 /* corresponding corner                                 */
 		int side;                                               /* boundary side of element                             */
 		FieldVector<DT, dim> ip_local;                 /* integration point in local coords    */
@@ -46,7 +49,66 @@ class FVElementGeometry
 		FieldVector<DT, dim> normal;                   /* normal on face at ip pointing to CV j*/
 		DT area;                                    /* area of boundary face                                */
 		SDValues sdv;                                  /* shape fcts, deriv. etc. at b-faces   */
-	} BoundaryFace; 
+	}; 
+
+   FieldVector<DT, dim> cellLocal;
+   FieldVector<DT, dim> cellGlobal;
+   DT cellVolume;
+   SubControlVolume subContVol[maxNC];
+   SubControlVolumeFace subContVolFace[maxNF];
+    
+	FVElementGeometry<G>(const Entity& e)
+	{
+      const Geometry& geometry = e.geometry();
+      GeometryType gt = geometry.type();
+       
+       const typename LagrangeShapeFunctionSetContainer<DT,DT,dim>::value_type& 
+       	sfs=LagrangeShapeFunctions<DT,DT,dim>::general(gt, 1);
+       	
+       // get cell volume
+       cellVolume = geometry.volume();
+       
+       // cell center in reference element
+ 		 cellLocal = ReferenceElements<DT,dim>::general(gt).position(0,0);
+ 		
+ 	  
+ 		 // get global coordinate of cell center
+ 		 cellGlobal = geometry.global(cellLocal);
+   	 
+ 		 int nodes = sfs.size();
+ 		 for (int i = 0; i < nodes; i++) {
+ 			 // assuming rectangular grids:
+ 			 subContVol[i].volume = 0.25*cellVolume;
+ 			 
+ 			 subContVol[i].local = sfs[i].position();
+ 			 
+ 			 subContVol[i].global = geometry.global(subContVol[i].local);
+ 		 }
+
+ 		 int faces = e.template count<dim-1>();
+ 		 
+ 		 switch (faces) {
+ 		 case 3: 
+ 			 subContVolFace[0].i = 1;
+ 			 subContVolFace[0].j = 2;
+ 			 subContVolFace[1].i = 0;
+ 			 subContVolFace[1].j = 2;
+ 			 subContVolFace[2].i = 0;
+ 			 subContVolFace[2].j = 1;
+ 			 break;
+ 		 case 4: 
+ 			 subContVolFace[0].i = 0;
+ 			 subContVolFace[0].j = 2;
+ 			 subContVolFace[1].i = 1;
+ 			 subContVolFace[1].j = 3;
+ 			 subContVolFace[2].i = 0;
+ 			 subContVolFace[2].j = 1;
+ 			 subContVolFace[3].i = 2;
+ 			 subContVolFace[3].j = 3;
+ 			 break;
+ 		 }
+ 		 
+	}
 
 };
 
