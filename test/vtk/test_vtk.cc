1d770c568db3d2a7ef8a463269373de853b0e163
Annika Fuchs <annika-rv@gmx.de>
2008-02-26 15:01:25 +0000

    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@177 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/vtk/test_vtk.cc
+++ test/vtk/test_vtk.cc
@@ -1,14 +1,14 @@
 #include "config.h"
 #include <iostream>
-#ifdef HAVE_UG
 #include <iomanip>
 #include <dune/grid/utility/gridtype.hh>
 #include <dune/grid/common/gridinfo.hh>
 #include <dune/grid/io/file/dgfparser/dgfparser.hh>
 #include <dune/grid/io/file/dgfparser/dgfug.hh>
-#include "dumux/io/vtkwriterextended.hh"
+#include <dune/grid/io/file/dgfparser/dgfalu.hh>
 #include <dune/istl/io.hh>
 #include <dune/common/timer.hh>
+#include "dumux/io/vtkwriterextended.hh"
 #include "dumux/material/properties.hh"
 #include "dumux/material/linearlaw.hh"
 #include "dumux/material/brookscoreylaw.hh"
@@ -19,26 +19,29 @@
 #include "dumux/diffusion/mimetic/mimeticdiffusion.hh"
 #include "dumux/diffusion/problems/heterogeneousproblem.hh"
 #include "dumux/diffusion/problems/uniformproblem.hh"
+#include "annikaproblem.hh" 
  
 int main(int argc, char** argv) 
 {
+  if(argc<2)
+      std::cout << "missing argument: dgfFileName" << std::endl;
+
   try{
     // define the problem dimensions  
     const int dim=2;
 
     // create a grid object
     typedef double NumberType; 
-    //typedef Dune::SGrid<dim,dim> GridType; 
-    //typedef Dune::YaspGrid<dim,dim> GridType; 
+  
     typedef Dune::UGGrid<dim> GridType; 
-
+    //typedef Dune::ALUSimplexGrid<dim,dim> GridType;
+    
     // use unitcube from grids 
     std::stringstream dgfFileName;
-    dgfFileName << "grids/unitcube" << GridType :: dimension << ".dgf";
 
     // create grid pointer, GridType is defined by gridtype.hh
-    Dune::GridPtr<GridType> gridPtr( dgfFileName.str() );
-
+    Dune::GridPtr<GridType> gridPtr(argv[1]);
+      
     // grid reference 
     GridType& grid = *gridPtr;
 
@@ -49,16 +52,16 @@ int main(int argc, char** argv)
     //Dune::BrooksCoreyLaw materialLaw(mat, mat);
     //Dune::LinearLaw materialLaw(mat, mat);
     
-    //Dune::HeterogeneousProblem<GridType, NumberType> problem(grid, "permeab.dat", true);
+    // Dune::HeterogeneousProblem<GridType, NumberType> problem(grid, "permeab.dat", true);
     //printvector(std::cout, *(problem.permeability), "permeability", "row", 200, 1);
-    Dune::UniformProblem<GridType, NumberType> problem;
+    Dune::AnnikaProblem<GridType, NumberType> problem;
     //problem.permeability.vtkout("permeability", grid);
 
     Dune::Timer timer;
     timer.reset();
-    Dune::FVDiffusion<GridType, NumberType> diffusion(grid, problem);
-    //Dune::FEDiffusion<GridType, NumberType> diffusion(grid, problem);
-    //Dune::MimeticDiffusion<GridType, NumberType> diffusion(grid, problem);
+    //  Dune::FVDiffusion<GridType, NumberType> diffusion(grid, problem);
+    // Dune::FEDiffusion<GridType, NumberType> diffusion(grid, problem);
+     Dune::MimeticDiffusion<GridType, NumberType> diffusion(grid, problem);
     
     
     diffusion.pressure();
@@ -67,18 +70,26 @@ int main(int argc, char** argv)
     //diffusion.vtkout("pressure", 0);
     
     const int blocksize = 2*dim;
-    typedef Dune::FieldVector<double, dim> R1;
+    typedef Dune::FieldVector<NumberType, dim> R1;
     typedef Dune::BlockVector< Dune::FieldVector<R1, blocksize> > VType;
     VType velocity(grid.size(0));
     diffusion.totalVelocity(velocity);
-    printvector(std::cout, velocity, "velocity", "row", 4, 1, 3);
-    
-	//Dune::VTKWriterExtended<GridType, GridType::Codim<0>::LevelIndexSet> 
-	//	vtkwriter(grid, grid.levelIndexSet( grid.maxLevel() ));
-	Dune::VTKWriter<GridType> vtkwriter(grid);
-	//vtkwriter.addFaceData(velocity, "face velocities");
-	vtkwriter.addCellData(*diffusion, "pressure");
-	vtkwriter.write("test_vtk", Dune::VTKOptions::ascii);		
+    printvector(std::cout, velocity, "velocity", "row", 2*dim, 1, 3);
+
+    Dune::VTKWriter<GridType> vtkwriter(grid);
+   
+    typedef Dune::BlockVector<R1> WType;
+    WType cellvelocity(grid.size(0));
+
+    typedef Dune::BlockVector<R1> ZType;
+    ZType vertexvelocity(grid.size(dim));
+
+    vtkwriter.faceToCellToVertex(velocity,cellvelocity,vertexvelocity);
+    vtkwriter.addFaceData(cellvelocity,vertexvelocity,"velocity");
+
+    vtkwriter.addCellData(*diffusion,"pressure");
+  
+    vtkwriter.write("test_vtk", Dune::VTKOptions::ascii);		
 
     return 0;
   }
@@ -89,17 +100,3 @@ int main(int argc, char** argv)
     std::cerr << "Unknown exception thrown!" << std::endl;
   }
 }
-#else 
-
-int main (int argc , char **argv) try
-{
-  std::cout << "Please install the UG library." << std::endl;
-
-  return 1;
-}
-catch (...) 
-{
-    std::cerr << "Generic exception!" << std::endl;
-    return 2;
-}
-#endif 
