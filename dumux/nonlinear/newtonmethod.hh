3855e8e88fb06e9a6de7141691c974d24c5c4910
Melanie Darcis <mdarcis@gmx.de>
2008-03-20 10:29:29 +0000

    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@193 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/nonlinear/newtonmethod.hh
+++ dumux/nonlinear/newtonmethod.hh
@@ -11,83 +11,35 @@ namespace Dune {
 	public:
 		void execute(bool verbose = true)
 		{
-//			int components = Model::m;
-//			int size = (*u).size();
-//			double oneByMagnitude[components];
-//			double initialNorm[components];
-//			for (int comp = 0; comp < components; comp++)
-//				initialNorm[comp] = 0;
-//			for (int i = 0; i < size; i++)
-//				for (int comp = 0; comp < components; comp++) {
-//					initialNorm[comp] += (*u)[i][comp]*(*u)[i][comp];
-//			}
-//			for (int comp = 0; comp < components; comp++) {
-//				initialNorm[comp] = sqrt(initialNorm[comp]);
-//				oneByMagnitude[comp] = 1.0/std::max(initialNorm[comp], 1e-6);
-//			}
-			
-			double initialTotalNorm = std::max((*u).two_norm(), 1e-6);
+			double oneByMagnitude = 1.0/std::max((*u).two_norm(), 1e-5);
 			double error = 1e100;			
 			double dt = localJacobian.getDt();
 			bool divided = false;
-
-		     while (dt > minDt && error > tolerance) {
-				double lambdaOld;
+			
+			while (dt > minDt && error > tolerance) {
 				double error = 1e100;
 				int iter = 0;
-				model.globalDefect(defectGlobal);
-				double globalResiduumOld = (*defectGlobal).two_norm();
 				while (error > tolerance && iter < maxIter) {
 					iter ++;
-			    	double lambda = 1;
+					*uOldNewtonStep = *u;
 					//printvector(std::cout, *uOldNewtonStep, "uOldNewtonStep", "row", 200, 1, 3);
 					//printvector(std::cout, *(model.uOldTimeStep), "uOldTimeStep", "row", 200, 1, 3);
 					*f = 0;
 					localJacobian.clearVisited();
-					*update = *u;
-					A.assemble(localJacobian, update, f);
+					A.assemble(localJacobian, u, f);
 					//printmatrix(std::cout, *A, "global stiffness matrix", "row", 11, 3);
 					std::cout << "matrix norm: " << (*A).infinity_norm() << std::endl;
 					//printvector(std::cout, *f, "right hand side", "row", 200, 1, 3);
 					model.solve();
-//					double updateNorm[components];
-//					for (int comp = 0; comp < components; comp++)
-//						updateNorm[comp] = 0;
-//					for (int i = 0; i < size; i++)
-//						for (int comp = 0; comp < components; comp++) {
-//							updateNorm[comp] += (*u)[i][comp]*(*u)[i][comp];
-//					}
-//					error = 0;
-//					for (int comp = 0; comp < components; comp++) {
-//						updateNorm[comp] = sqrt(updateNorm[comp]);
-//						error += oneByMagnitude[comp]*updateNorm[comp];
-//					}
-					error = (*update).two_norm()/initialTotalNorm;
-					std::cout << "update norm = " << error << std::endl;
-					
+					error = oneByMagnitude*((*u).two_norm());
 					//printvector(std::cout, *u, "update", "row", 200, 1, 3);
-					*update *= -lambda;
-					*u += *update;
+					*u *= -1.0;
+					*u += *uOldNewtonStep; 
+					for (int idx = 0; idx < (*u).size(); idx++) 
+						if ((*u)[idx][1] < -1.0 || (*u)[idx][1] > 2.0) 
+							error = 1e100;
 
 					//printvector(std::cout, *u, "u", "row", 200, 1, 3);
-					model.globalDefect(defectGlobal);
-					double globalResiduum = (*defectGlobal).two_norm();
-					std::cout << "old res = " << globalResiduumOld << ", new res = " << globalResiduum << std::endl;
-					while (globalResiduum > globalResiduumOld){
-					    lambda *= 0.5;
-					    std::cout << "Linesearch: lambda divided to " << lambda << std::endl;
-					    if (lambda < 1e-2)
-					    	break;
-					    *update *= 0.5;
-					    *u -= *update;
-					    globalResiduumOld = globalResiduum; 
-						model.globalDefect(defectGlobal);
-						globalResiduum = (*defectGlobal).two_norm();					    
-						std::cout << "old res = " << globalResiduumOld << ", new res = " << globalResiduum << std::endl;
-					}
-					
-					globalResiduumOld = globalResiduum;
-					
 					if (verbose)
 						std::cout << "Newton step " << iter << ", defect = " << error << std::endl;
 				}
@@ -120,16 +72,14 @@ namespace Dune {
 			return;
 		}
 		
-		NewtonMethod(const G& g, Model& mod, double tol = 1e-5, int maxIt = 12, double mind = 1e-5, int goodIt = 5)
+		NewtonMethod(const G& g, Model& mod, double tol = 1e-6, int maxIt = 12, double mind = 1e-5, int goodIt = 5)
 		: grid(g), model(mod), u(mod.u), f(mod.f), A(mod.A), localJacobian(mod.localJacobian), 
-		  tolerance(tol), maxIter(maxIt), minDt(mind), 
-		  goodIter(goodIt),defectGlobal(g), update(g)
+		  uOldNewtonStep(g), tolerance(tol), maxIter(maxIt), minDt(mind), goodIter(goodIt)
 		{ }
 		
-		NewtonMethod(const G& g, Model& mod, int level, double tol = 1e-5, int maxIt = 12, double mind = 1e-5, int goodIt = 5)
+		NewtonMethod(const G& g, Model& mod, int level, double tol = 1e-6, int maxIt = 12, double mind = 1e-5, int goodIt = 5)
 		: grid(g), model(mod), u(mod.u), f(mod.f), A(mod.A), localJacobian(mod.localJacobian), 
-		  tolerance(tol), maxIter(maxIt), minDt(mind), 
-		  goodIter(goodIt),defectGlobal(g,level), update(g, level)
+		  uOldNewtonStep(g, level), tolerance(tol), maxIter(maxIt), minDt(mind), goodIter(goodIt)
 		{ }
 		
 	private:
@@ -139,12 +89,11 @@ namespace Dune {
 		FunctionType& f;
 		OperatorAssembler& A;
 		LocalJacobian& localJacobian;
+		FunctionType uOldNewtonStep;
 		double tolerance;
 		int maxIter;
 		double minDt;
 		int goodIter;
-	    FunctionType  defectGlobal;
-	    FunctionType  update;
 	};
 }
 #endif
