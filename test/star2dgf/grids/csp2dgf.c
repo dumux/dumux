c6ae3deac9cc9e3da84626d2f6f8780ca1a20dc0
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2008-04-28 16:43:29 +0000

    csp2dgf test files added
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@218 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/star2dgf/grids/csp2dgf.c
+++ test/star2dgf/grids/csp2dgf.c
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TRI_3 8
+#define TETRA_4 4
+#define QUAD_4 14
+#define HEXA_8 6
+#define BAR_2 2
+
+typedef struct points{
+  double x;
+  double y;
+  double z;
+  double pbVal;
+  int pbFlag;
+} *POINTS;
+
+typedef struct elements{
+  int objectType;
+  int finiteElementType;
+  int anz;
+  int *nodes;
+  int materialType;
+} *ELEMENTS;
+
+
+
+
 int main (int argc, char **argv)
 {
   int i;
@@ -9,107 +34,209 @@ int main (int argc, char **argv)
 
   if (argc != 2)
     {
-      printf("usage: star2dgf basefilename\n");
+      printf("usage: csp2dgf basefilename\n");
       return (1);
     }
 
   char baseFileName[120];
   strcpy(baseFileName, argv[1]);
 
+  char ascFileName[120];
+  strcpy(ascFileName, baseFileName);
+  strcat(ascFileName, ".asc");
+
+  // test dimension
+  FILE *inputFile; 
+  if((inputFile=fopen(ascFileName, "r"))==NULL) {
+    printf("ERROR: %s doesn't exist\n",ascFileName);
+    return 1;
+  }
+  
+  char testString[120]; 
+  int triDim=0;
+ 
   char dgfFileName[120];
   strcpy(dgfFileName, baseFileName);
   strcat(dgfFileName, ".dgf");
-  FILE *dgfFile = fopen(dgfFileName, "w");
-  fprintf(dgfFile, "DGF\nVERTEX\nfirstindex 1\n");
 
-  char inputFileName[120];
-  strcpy(inputFileName, baseFileName);
-  strcat(inputFileName, ".inp");
-  FILE *inputFile = fopen(inputFileName, "r");
-
-  char testString[120]; 
-  do {
-    fscanf(inputFile, "%s", testString);
+  // input coordinates and boundary values
+  char datFileName[120];
+  strcpy(datFileName, baseFileName);
+  strcat(datFileName, ".dat");
+ 
+  if((inputFile = fopen(datFileName, "r"))==NULL) {
+    printf("ERROR: %s doesn't exist\n",datFileName);
+    return 1;
   }
-  while(strcmp(testString, "nodes"));
-  int numberOfNodes;
-  fscanf(inputFile, "%s %d", testString, &numberOfNodes);
+
+  int numberOfNodes;  
+  fscanf(inputFile,"%d",&numberOfNodes);
+  fgets(testString,120,inputFile);
+
+  POINTS nodes;
+  nodes=malloc(numberOfNodes*sizeof(*nodes));
+
+  for(i=0;i<numberOfNodes;++i)
+    fscanf(inputFile,"%lf", &nodes[i].x);
+  for(i=0;i<numberOfNodes;++i)
+    fscanf(inputFile,"%lf",&nodes[i].y);
+  for(i=0;i<numberOfNodes;++i){
+    fscanf(inputFile,"%lf",&nodes[i].z);
+    if(nodes[i].z!=0)
+      triDim=1;
+  }
+ 
+  fscanf(inputFile,"%s %s",&testString,&testString);
+  for(i=0;i<numberOfNodes;++i)
+    fscanf(inputFile,"%d",&nodes[i].pbFlag);
+
+  fscanf(inputFile,"%s %s",&testString,&testString);
+  for(i=0;i<numberOfNodes;++i)
+    fscanf(inputFile,"%lf",&nodes[i].pbVal);
+
+  // output vertex
+  // coordinates, pbFlag, pbValue
+  FILE *dgfFile = fopen(dgfFileName, "w");
+  if(triDim)
+    fprintf(dgfFile, "DGF\nVERTEX\nparameters 2\nfirstindex 1\n");
+  else
+    fprintf(dgfFile,"DGF\nVERTEX\nparameters 2\n");
   printf("nodes: %d\n", numberOfNodes);
 
-  do {
-    fscanf(inputFile, "%s", testString);
-  }
-  while(strcmp(testString, "elements"));
+  if(triDim)
+    for(i=0;i<numberOfNodes;++i)
+      fprintf(dgfFile, "%14.7e %14.7e %14.7e %d %14.7e \% % %d\n", nodes[i].x, nodes[i].y, nodes[i].z, nodes[i].pbFlag, nodes[i].pbVal,i+1);
+  else 
+    for(i=0;i<numberOfNodes;++i)
+      fprintf(dgfFile, "%14.7e %14.7e %d %14.7e \% % %d\n", nodes[i].x, nodes[i].y, nodes[i].pbFlag, nodes[i].pbVal, i);
+   
+
+
+  // input element types and node ids
   int numberOfElements;
-  fscanf(inputFile, "%s %d", testString, &numberOfElements);
-  printf("elements: %d\n", numberOfElements);
+  fscanf(inputFile,"%d",&numberOfElements);
+  fgets(testString,120,inputFile);
+  printf("elements: %d\n",numberOfElements);
+
+  ELEMENTS elements;
+  elements = malloc(numberOfElements*sizeof(*elements));
+  for(i=0;i<numberOfElements;++i)
+    fscanf(inputFile,"%d", &elements[i].finiteElementType);
+  
+  int ifSimplex=0;
+  for(i=0;i<numberOfElements;++i){
+    switch(elements[i].finiteElementType){
+    case HEXA_8:
+      elements[i].anz=8;
+      break;
+    case QUAD_4:
+    case TETRA_4:
+      elements[i].anz=4;
+      break;
+    case TRI_3:
+      elements[i].anz=3;
+      ifSimplex=1;
+      break;
+    case BAR_2:
+      elements[i].anz=2;
+      break;
+    default:
+      printf("unknown finiteElementType\n");
+      free(nodes);
+      free(elements);
+      fclose(dgfFile);
+      fclose(inputFile);
+      return 1;
+    } 
+  }
+
+  if(triDim)
+    if (ifSimplex)
+      fprintf(dgfFile,"#\nSIMPLEX\nparameters 1\n");
+    else 
+      fprintf(dgfFile,"#\nCUBE\nparameters 1\nmap 0 1 3 2 4 5 7 6\n");
+  else 
+    if(ifSimplex)
+      fprintf(dgfFile,"#\nSIMPLEX\nparameters 1\n");
+    else
+      fprintf(dgfFile,"#\nCUBE\nparameters 1\n");
+    
+  for(i=0;i<numberOfElements;++i)
+    elements[i].nodes=calloc(elements[i].anz,sizeof(int));
+
+  fgets(testString,120,inputFile);
+  fgets(testString,120,inputFile);
+
+  int j;
+  for(i=0;i<numberOfElements;++i)
+    for(j=0;j<elements[i].anz;++j)
+      fscanf(inputFile,"%d",&elements[i].nodes[j]);
+      
+  do {
+    fscanf(inputFile,"%s",testString);
+  } while(strcmp(testString,"PMATERIAL"));
+
+  for(i=0;i<numberOfElements;++i)
+      fscanf(inputFile,"%d",&elements[i].materialType);
 
   fclose(inputFile);
 
-  char vertexFileName[120];
-  strcpy(vertexFileName, baseFileName);
-  strcat(vertexFileName, ".vrt");
-  FILE *vertexFile = fopen(vertexFileName, "r");
 
-  for (i = 1; i <= numberOfNodes; i++) {
-    int dummy;
-    char xCoord[30], yCoord[30], zCoord[30];
-    fscanf(vertexFile, " %d %16s %16s %16s", &dummy, xCoord, yCoord, zCoord);
-    fprintf(dgfFile, "%14.7e %14.7e %14.7e \% %", strtod(xCoord, NULL), strtod(yCoord, NULL), strtod(zCoord, NULL));
-    fprintf(dgfFile, " %d\n", i);
-  }
-  fprintf(dgfFile, "#\n");
+  // output elements
+  int numberOfFamilies;
+  inputFile=fopen(ascFileName,"r");  
+  fgets(testString,120,inputFile);
+  fgets(testString,120,inputFile);
+  fscanf(inputFile,"%d",&numberOfFamilies);
+  do {
+    fscanf(inputFile,"%s",testString);
+  } while(strcmp(testString,"sequence"));
+ 
+  fgets(testString,120,inputFile);
 
-  char cellFileName[120];
-  strcpy(cellFileName, baseFileName);
-  strcat(cellFileName, ".cel");
-  FILE *cellFile = fopen(cellFileName, "r");
+  int index;
+  int numberOfUsedElements;
+  int k;
+  
+  //im 3d wird bei 1 angefangen zu zaehlen
+  if(triDim) 
+    for(i=0;i<numberOfElements;++i)
+      for(j=0;j<elements[i].anz;++j)
+	elements[i].nodes[j]+=1; 
 
-  int isVolumeElement = 1;
-  int isSimplex = 0;
-  int surfaceStarted = 0;
-  for (i = 1; i <= numberOfElements; i++) {
-    int dummy;
-    int vertices[8];
-    int boundaryId;
-    int surfaceOrVolume; 
-    fscanf(cellFile, " %d %d %d %d %d %d %d %d %d %d %d %d", &dummy, 
-	   &vertices[0], &vertices[1], &vertices[2], &vertices[3], 
-	   &vertices[4], &vertices[5], &vertices[6], &vertices[7], 
-	   &boundaryId, &surfaceOrVolume, &dummy);
-    if (i == 1) {
-      if (vertices[2] == vertices[3]) {
-	isSimplex = 1;
-	fprintf(dgfFile, "SIMPLEX\n");
-      }
-      else {
-	fprintf(dgfFile, "CUBE\n");
-      }
-    }
-    if (surfaceOrVolume == isVolumeElement) {
-      if (isSimplex)
-	fprintf(dgfFile, "%7d %7d %7d %7d\n", vertices[0], vertices[1], vertices[2], vertices[4]);
-      else 
-	fprintf(dgfFile, "%7d %7d %7d %7d %7d %7d %7d %7d\n", vertices[0], vertices[1], vertices[3], vertices[2], 
-		vertices[4], vertices[5], vertices[7], vertices[6]);
-    }
-    else {
-      if (!surfaceStarted) {
-	surfaceStarted = 1;
-	int numberOfVolumeElements = i - 1;
-	printf(" volume: %d\n", numberOfVolumeElements);
-	printf(" surface: %d\n", numberOfElements - numberOfVolumeElements);
-	fprintf(dgfFile, "#\nBOUNDARYSEGMENTS\n");
-      }
-      if (isSimplex)
-	fprintf(dgfFile, "%d %7d %7d %7d\n", boundaryId, vertices[0], vertices[1], vertices[2]);
-      else 
-	fprintf(dgfFile, "%d %7d %7d %7d %7d\n", boundaryId, vertices[0], vertices[1], vertices[3], vertices[2]);
-    }
+  fscanf(inputFile,"%s %s %d",&testString,&testString,&numberOfUsedElements);
+  for(i=0;i<numberOfUsedElements;++i) {
+    fscanf(inputFile,"%d",&index);
+    for(j=0;j<elements[index].anz;++j)
+      fprintf(dgfFile,"%7d ",elements[index].nodes[j]);
+    fprintf(dgfFile,"%7d\n",elements[index].materialType);
   }
 
-  fprintf(dgfFile, "#\n# %s\n", dgfFileName);
+  //boundary id, node ids
+  //boundary id ?! deshalb testhalber family-nr 
+  fprintf(dgfFile,"#\nBOUNDARYSEGMENTS\n");  
+  for(i=1;i<numberOfFamilies;++i) {
+    fscanf(inputFile,"%s %s %d",&testString,&testString,&numberOfUsedElements);
+    for(j=0;j<numberOfUsedElements;++j) {
+      fscanf(inputFile,"%d",&index);
+       if(elements[index].anz>2){
+	 fprintf(dgfFile,"%7d ",i); 
+	for(k=0;k<elements[index].anz;++k)
+	  fprintf(dgfFile,"%7d ",elements[index].nodes[k]);
+	fprintf(dgfFile,"\n");
+	}
+    }
+  }
+  
+  fprintf(dgfFile,"#\n# %s\n",dgfFileName);
+
+  fclose(inputFile);
   fclose(dgfFile);
 
-  return (0);
+  for(i=0;i<numberOfElements;++i)
+    free(elements[i].nodes);
+  free(elements);
+  free(nodes);
+
+  return 0;
 }
