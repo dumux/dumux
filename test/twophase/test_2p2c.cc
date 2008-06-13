bdd7a47ebdac7751d0996d99c2563b26acbe2863
Klaus Mosthaf <klmos@env.dtu.dk>
2008-06-13 11:19:48 +0000

    testfile for 2p2c generated
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@254 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/twophase/test_2p2c.cc
+++ test/twophase/test_2p2c.cc
@@ -6,41 +6,49 @@
 #include <dune/grid/common/gridinfo.hh>
 #include <dune/grid/io/file/dgfparser/dgfparser.hh>
 #include <dune/grid/io/file/dgfparser/dgfug.hh>
-#include <dune/grid/io/file/dgfparser/dgfyasp.hh>
 #include <dune/grid/io/file/vtk/vtkwriter.hh>
 #include <dune/istl/io.hh>
 #include <dune/common/timer.hh>
-#include "dumux/twophase/problems/lensproblem.hh"
-#include "dumux/twophase/problems/uniformtwophaseproblem.hh"
-#include "dumux/twophase/fv/boxpwsn.hh"
+//#include "dumux/2p2c/problems/lensproblem2p2c.hh"
+#include "dumux/2p2c/problems/layerproblem.hh"
+#include "dumux/2p2c/problems/uniformtwophaseproblem.hh"
+#include "dumux/2p2c/fv/box2p2c.hh"
+//#include "dumux/twophase/fv/boxpwsn.hh"
 #include "dumux/timedisc/timeloop.hh"
 #include "dumux/material/vangenuchtenlaw.hh"
+#include "dumux/material/solubilities.hh"
+#include "dumux/material/properties.hh"
+#include "dumux/material/water_props.hh"
+
 
 int main(int argc, char** argv) 
 {
   try{
-    // define the problem dimensions  
+    // define the problem dimensions (geometry of problem)  
     const int dim=2;
-    typedef double NumberType; 
+    typedef double NumberType;
     Dune::FieldVector<NumberType, dim> outerLowerLeft(0);
     Dune::FieldVector<NumberType, dim> outerUpperRight(6);
     outerUpperRight[1] = 4;
-    Dune::FieldVector<NumberType, dim> innerLowerLeft(1);
-    innerLowerLeft[1] = 2;
-    Dune::FieldVector<NumberType, dim> innerUpperRight(4);
-    innerUpperRight[1] = 3;
+    Dune::FieldVector<NumberType, dim> innerLowerLeft(0);
+    innerLowerLeft[1] = 1;
+    Dune::FieldVector<NumberType, dim> innerUpperRight(6);
+    innerUpperRight[1] = 1.5;
+    double depthBOR = 5;
+
+    // count number of arguments
     if (argc != 3) {
-      std::cout << "usage: test_2p2c tEnd dt" << std::endl;
+      std::cout << "usage: test_twophase tEnd dt" << std::endl;
       return 0;
     }
-    	std::string arg1(argv[1]);
-	std::istringstream is1(arg1);
-	double tEnd;
-	is1 >> tEnd;
-	std::string arg2(argv[2]);
-	std::istringstream is2(arg2);
-	double dt;
-	is2 >> dt;
+    // define tEnd
+    std::string arg1(argv[1]);
+    std::istringstream is1(arg1);
+    double tEnd; is1 >> tEnd;
+    // define dt
+    std::string arg2(argv[2]);
+    std::istringstream is2(arg2);
+    double dt; is2 >> dt;
 
 
     // create a grid object
@@ -48,9 +56,10 @@ int main(int argc, char** argv)
     //typedef Dune::YaspGrid<dim,dim> GridType; 
     typedef Dune::UGGrid<dim> GridType; 
 
-    // use unitcube from grids 
+    // use unitcube from grids (UGGrid)
     std::stringstream dgfFileName;
-    dgfFileName << "grids/unitcube" << GridType :: dimension << ".dgf";
+    dgfFileName << "/temp/klaus/DUMUX/dune-mux/test/twophase/grids/unitcube" 
+    	<< GridType :: dimension << ".dgf";
 
     // create grid pointer, GridType is defined by gridtype.hh
     Dune::GridPtr<GridType> gridPtr( dgfFileName.str() );
@@ -59,36 +68,50 @@ int main(int argc, char** argv)
     GridType& grid = *gridPtr;
 
     Dune::gridinfo(grid);
-    
-    DNAPL dnapl;
-    Water water;
-    Dune::VanGenuchtenLaw law(water, dnapl);
-    Dune::LensProblem<GridType, NumberType> problem(law, outerLowerLeft, outerUpperRight, 
-    		innerLowerLeft, innerUpperRight);
 
-    typedef Dune::BoxPwSn<GridType, NumberType> TwoPhase;
-    TwoPhase twoPhase(grid, problem);
+    // choose fluids and properties
+    Air air; Water water;
+    Dune::VanGenuchtenLaw law(water, air);
+    //Dune::LinearLaw law(water, air);
     
-    Dune::TimeLoop<GridType, TwoPhase> timeloop(0, tEnd, dt, "lens", 1);
+    // create problem properties and geometry
+    Dune::LayerProblem<GridType, NumberType> problem(law, outerLowerLeft, outerUpperRight, 
+    		innerLowerLeft, innerUpperRight, depthBOR);
+
+    // create two-phase two-component problem
+    typedef Dune::Box2P2C<GridType, NumberType> TwoPhaseTwoComp;
+    TwoPhaseTwoComp twoPhasetwoComp(grid, problem);
+    
+    Dune::TimeLoop<GridType, TwoPhaseTwoComp> timeloop(0, tEnd, dt, "lens", 5);
     
     Dune::Timer timer;
     timer.reset();
-    timeloop.execute(twoPhase);
+    timeloop.execute(twoPhasetwoComp);
     std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
-     
-//    typedef Dune::BoxPwSnOld<GridType, NumberType> TwoPhaseOld;
-//    TwoPhaseOld twoPhaseOld(grid, problem);
+
+// COMPARSION TO TWOPHASE MODEL    
+//    Dune::LensProblemOld<GridType, NumberType> problemOld(law, outerLowerLeft, outerUpperRight, 
+//    		innerLowerLeft, innerUpperRight, depthBOR);
+//
+//    typedef Dune::BoxPwSn<GridType, NumberType> TwoPhaseOld;
+//    TwoPhaseOld twoPhaseOld(grid, problemOld);
 //    
 //    Dune::TimeLoop<GridType, TwoPhaseOld> timeloopOld(0, tEnd, dt, "lensOld", 1);
 //    
+//    //Dune::Timer timer;
 //    timer.reset();
 //    timeloopOld.execute(twoPhaseOld);
-//    std::cout << "timeloopOld.execute took " << timer.elapsed() << " seconds" << std::endl;
-//     
-//    *twoPhase.u -= *twoPhaseOld.u; 
-//    std::cout << "difference = " << (*twoPhase.u).two_norm()/(*twoPhaseOld.u).two_norm() << std::endl;
+//    std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
+//
+//    *twoPhasetwoComp.u -= *twoPhaseOld.u; 
+//    std::cout << "difference = " << (*twoPhasetwoComp.u).two_norm()/(*twoPhaseOld.u).two_norm() 
+//    	<< std::endl;
+
+////////////////////////////////
     
-    //printvector(std::cout, *twoPhase.u, "u", "row", 2, 1, 3);
+    
+//    
+//    	printvector(std::cout, *twoPhase.u, "u", "row", 2, 1, 3);
 
     return 0;
   }
