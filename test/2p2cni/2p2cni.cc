43284941bb2ee9e499cd5843cc752a0402c564f0
Melanie Darcis <mdarcis@gmx.de>
2008-07-04 12:44:46 +0000

    test application for 2p2cni
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@338 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ test/2p2cni/2p2cni.cc
@@ -0,0 +1,106 @@
+#include "config.h"
+#include <iostream>
+#ifdef HAVE_UG
+#include <iomanip>
+//#include <dune/grid/utility/gridtype.hh>
+#include <dune/grid/common/gridinfo.hh>
+#include <dune/grid/io/file/dgfparser/dgfs.hh>
+#include <dune/grid/io/file/dgfparser/dgfug.hh>
+//#include <dune/grid/io/file/dgfparser/dgfalu.hh>
+//#include <dune/grid/io/file/dgfparser/dgfyasp.hh>
+//#include <dune/grid/io/file/dgfparser/dgfs.hh>
+#include <dune/grid/io/file/vtk/vtkwriter.hh>
+#include <dune/istl/io.hh>
+#include <dune/common/timer.hh>
+//#include "dumux/twophase/problems/uniformtwophaseproblem.hh"
+#include "waterco2problem.hh"
+//#include "dumux/material/properties.hh"
+#include "dumux/2p2cni/fv/box2p2cni.hh"
+#include "dumux/timedisc/timeloop.hh"
+#include "dumux/io/readstarformat.cc"
+int main(int argc, char** argv) 
+{
+  try{
+    // define the problem dimensions  
+    const int dim=2;
+    typedef double NumberType; 
+    Dune::FieldVector<NumberType, dim> outerLowerLeft(0);
+    Dune::FieldVector<NumberType, dim> outerUpperRight(4);
+    Dune::FieldVector<NumberType, dim> innerLowerLeft(1);
+    innerLowerLeft[1] = 2;
+    Dune::FieldVector<NumberType, dim> innerUpperRight(4);
+    innerUpperRight[1] = 3;
+    if (argc != 4) {
+      std::cout << "usage: 2p2cni basefilename tEnd dt" << std::endl;
+      return 0;
+    }
+    	std::string arg1(argv[2]);
+	std::istringstream is1(arg1);
+	double tEnd;
+	is1 >> tEnd;
+	std::string arg2(argv[3]);
+	std::istringstream is2(arg2);
+	double dt;
+	is2 >> dt;
+
+    // create a grid object
+    typedef Dune::SGrid<dim,dim> GridType; 
+    //typedef Dune::YaspGrid<dim,dim> GridType; 
+    //typedef Dune::UGGrid<dim> GridType; 
+	//typedef Dune::ALUSimplexGrid<dim,dim> GridType; 
+	
+    Dune::GridPtr<GridType> gridPointer(argv[1]);
+    GridType& grid = *gridPointer;
+    //readStarFormat(grid, argv[1]);
+    //grid.createLGMGrid(argv[1]);
+
+     Dune::gridinfo(grid);
+    
+    Water wPhase;
+    Air nPhase;
+
+//     Dune::LinearLaw law(brine, co2);
+//     Dune::CO2Problem2D<GridType, NumberType> problem(law, 1.e7); 
+      Dune::BrooksCoreyLaw law(wPhase, nPhase);
+      Dune::CWaterAir multicomp(wPhase, nPhase);
+
+      Dune::WaterCO2Problem<GridType, NumberType> problem(law, multicomp, 1.e7, 0.2, 0.05, 10000, 2); 
+
+      typedef Stupid::VtkMultiWriter<GridType> MultiWriter;
+    typedef Dune::Box2P2CNI<GridType, NumberType, MultiWriter> TwoPhase;
+    TwoPhase twoPhase(grid, problem);
+
+
+    
+    Dune::TimeLoop<GridType, TwoPhase> timeloop(0, tEnd, dt, "co2", 1);
+    
+    Dune::Timer timer;
+    timer.reset();
+    timeloop.execute(twoPhase);
+    std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
+     
+	//std::cout << twoPhase.injected() << " kg CO2 injected." << std::endl;
+
+    return 0;
+  }
+  catch (Dune::Exception &e){
+    std::cerr << "Dune reported error: " << e << std::endl;
+  }
+  catch (...){
+    std::cerr << "Unknown exception thrown!" << std::endl;
+  }
+}
+#else 
+
+int main (int argc , char **argv) try
+{
+  std::cout << "Please install the UG library." << std::endl;
+
+  return 1;
+}
+catch (...) 
+{
+    std::cerr << "Generic exception!" << std::endl;
+    return 2;
+}
+#endif 
---
+++ test/2p2cni/waterco2problem.hh
@@ -0,0 +1,227 @@
+#ifndef DUNE_WATERCO2PROBLEM_HH
+#define DUNE_WATERCO2PROBLEM_HH
+
+#include<iostream>
+#include<iomanip>
+
+#include<dune/common/fvector.hh>
+#include<dune/common/fmatrix.hh>
+#include<dune/common/exceptions.hh>
+#include<dune/grid/common/grid.hh>
+#include<dune/grid/common/referenceelements.hh>
+#include<dune/disc/operators/boundaryconditions.hh>
+#include<dumux/material/twophaserelations.hh>
+#include<dumux/2p2cni/2p2cniproblem.hh>
+#include<dumux/material/linearlaw.hh>
+#include<dumux/material/brookscoreylaw.hh>
+
+
+/**
+ * @file
+ * @brief  Base class for defining an instance of the TwoPhase problem
+ * @author Bernd Flemisch
+ */
+
+namespace Dune
+{
+  //! base class that defines the parameters of a diffusion equation
+  /*! An interface for defining parameters for the stationary diffusion equation 
+   * \f$ - \text{div}\, (\lambda K \text{grad}\, p ) = q, \f$, 
+   * \f$p = g\f$ on \f$\Gamma_1\f$, and \f$\lambda K \text{grad}\, p = J\f$ 
+   * on \f$\Gamma_2\f$. Here, 
+   * \f$p\f$ denotes the pressure, \f$K\f$ the absolute permeability, 
+   * and \f$\lambda\f$ the total mobility, possibly depending on the 
+   * saturation. 
+   *
+   *	Template parameters are:
+   *	
+   *	- Grid  a DUNE grid type
+   *	- RT    type used for return values 
+   */
+  template<class G, class RT>
+  class WaterCO2Problem : public TwoPTwoCNIProblem<G, RT> {
+	typedef typename G::ctype DT;
+	enum {dim=G::dimension, m=3};
+	enum {swrIdx=0, snrIdx=1, lamIdx=2, pbIdx=3};
+	typedef typename G::Traits::template Codim<0>::Entity Entity;
+	typedef typename IntersectionIteratorGetter<G,LeafTag>::IntersectionIterator IntersectionIterator;
+
+  public:
+	virtual const FieldMatrix<DT,dim,dim>& K (const FieldVector<DT,dim>& x, const Entity& e, 
+					const FieldVector<DT,dim>& xi)
+	{
+		return permloc_;
+	}
+
+	// permeabilities
+	virtual const FieldMatrix<DT,dim,dim>& K (const FieldVector<DT,dim>& x)
+	//, const Entity& e, const FieldVector<DT,dim>& xi)
+	{
+		return permloc_;
+	}
+		
+	virtual FieldVector<RT,m> q (const FieldVector<DT,dim>& x, const Entity& e, 
+					const FieldVector<DT,dim>& xi) const
+	{
+		FieldVector<RT,m> values(0);
+
+		return values;
+	}
+
+	virtual FieldVector<BoundaryConditions::Flags, m> bctype (const FieldVector<DT,dim>& x, const Entity& e, 
+					const IntersectionIterator& intersectionIt, 
+					   const FieldVector<DT,dim>& xi) const 
+	{
+		FieldVector<BoundaryConditions::Flags, m> values(Dune::BoundaryConditions::dirichlet); 
+		if(x[0]<1.e-2 && x[1] > 1. && x[1] < 3.)
+		{
+			values = Dune::BoundaryConditions::neumann;
+		}
+		return values;
+	}
+	
+	virtual void dirichletIndex(const FieldVector<DT,dim>& x, const Entity& e,
+			const IntersectionIterator& intersectionIt,
+			const FieldVector<DT,dim>& xi, FieldVector<int,m>& dirichletIndex) const 
+	{
+		for (int i = 0; i < m; i++)
+			dirichletIndex[i]=i;
+		return;
+	}
+
+	virtual FieldVector<RT,m> g (const FieldVector<DT,dim>& x, const Entity& e, 
+				const IntersectionIterator& intersectionIt, 
+				  const FieldVector<DT,dim>& xi) const 
+	{
+		FieldVector<RT,m> values(0);
+		
+		values[0] = p0_;//p0_ - x[0] * 1070 * 9.81; 
+		values[1] = 0.3;
+		values[2] = 311.; //311. - x[0]*0.03;
+		
+//		if(x[0]>4-1.e-2 && x[1] > 1. && x[1] < 3.)
+//		{
+//		    values[2] = 320;
+//		}
+		
+		return values;
+	}
+	  
+	virtual FieldVector<RT,m> J (const FieldVector<DT,dim>& x, const Entity& e, 
+				const IntersectionIterator& intersectionIt, const FieldVector<DT,dim>& xi) const 
+	{
+		FieldVector<RT,m> values(0);
+
+		values[0] = 0.;//1.e-3;
+		values[1] = 0.;//2.692798e-4;
+		values[2] = 10000.77;
+		
+
+//		if(x<1.e-2)
+//			values[2] = 312.;
+//				values[0] = -1.29188e-4;//-1.29188e-4; 
+//				values[1] = 0.0;
+//				values[2] = -19.51;//43.24;
+		
+		return values;
+	}
+	
+	// Initial Conditions for global vector x, element e and local vector xi
+	virtual FieldVector<RT,m> initial (const FieldVector<DT,dim>& x, const Entity& e, 
+				  const FieldVector<DT,dim>& xi) const 
+	{
+		FieldVector<RT,m> values(0);
+
+		values[0] = p0_;// - x[1] * 1070 * 9.81; 
+		values[1] = 0.3;
+		values[2] = 311.;// - x[1]*0.03;
+	
+		return values;
+	}
+
+	int initialPhaseState (const FieldVector<DT,dim>& x, const Entity& e, 
+				  const FieldVector<DT,dim>& xi) const 
+	{
+
+		enum {gasPhase = 0, waterPhase = 1, bothPhases = 2}; // Phase states
+		int state;
+
+//		if (x[1] >= innerLowerLeft_[1] && x[1] <= innerUpperRight_[1]
+//		      && x[0] >= innerLowerLeft_[0])
+			state = bothPhases;
+			
+		return state;
+	}
+
+	double porosity (const FieldVector<DT,dim>& x, const Entity& e, 
+			  const FieldVector<DT,dim>& xi) const 
+	{
+		return 0.2;
+	}
+	
+	virtual FieldVector<RT,4> soilParameters (const FieldVector<DT,dim>& x, const Entity& e, 
+			  const FieldVector<DT,dim>& xi) const
+	{
+		FieldVector<RT,4> values(0);
+		enum{soilDensity = 0,soilHeatCap = 1,soilLambdaDry = 2, soilLambdaSw = 3};
+		
+		values[soilDensity] = 2650.0;
+		values[soilHeatCap] = 800.0;
+		values[soilLambdaDry] = 0.32;
+		values[soilLambdaSw] = 2.7;
+		
+		return values;
+	}
+	
+	virtual FieldVector<RT,dim> gravity () const 
+	{
+		FieldVector<RT,dim> values(0);
+		
+		values[1] = -9.81;
+		
+		return values;
+	}
+	  
+	virtual FieldVector<RT,4> materialLawParameters (const FieldVector<DT,dim>& x, const Entity& e, 
+			  const FieldVector<DT,dim>& xi) const 
+	{
+		FieldVector<RT,4> values(0);
+		values[swrIdx] = swr_;
+		values[snrIdx] = snr_;
+		values[lamIdx] = lambda_;
+		values[pbIdx] = pb_;
+		
+		return values;
+	}
+
+	WaterCO2Problem(TwoPhaseRelations& law = *(new BrooksCoreyLaw), 
+			MultiComp& multicomp = *(new CWaterAir), RT pdown = 3.086e5, 
+			RT swr = 0.2, RT snr = 0.05, RT pb = 10000.0, RT lambda = 2.0 ) 
+	: TwoPTwoCNIProblem<G, RT>(law, multicomp) 
+	{	
+		p0_ = pdown;
+		swr_ = swr;
+		snr_ = snr;
+		pb_ = pb;
+		lambda_ = lambda;
+		permloc_ = 0; 
+		
+		for (int i = 0; i < dim; i++)
+			permloc_[i][i] = 1.0e-14;
+
+
+	}
+	
+	private:
+		Dune::FieldMatrix<DT,dim,dim> permloc_;
+		Dune::FieldMatrix<DT,dim,dim> permlocWell_;
+		Dune::FieldMatrix<DT,dim,dim> permlocAquitard_;
+		RT p0_;
+		RT swr_, snr_;
+		RT pb_, lambda_;
+		RT soilDens_, soilHeatCp_, soilLDry_, soilLSw_;
+		
+  };
+
+}
+#endif
