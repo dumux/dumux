dc6bcc03b321c0e9334ebab081bb7aa4c0ced439
Klaus Mosthaf <klmos@env.dtu.dk>
2008-07-25 08:34:38 +0000

    changed testfile for 2p2c
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@421 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/twophase/test_2p2c.cc
+++ test/twophase/test_2p2c.cc
@@ -14,6 +14,7 @@
 #include "dumux/2p2c/fv/box2p2c.hh"
 #include "dumux/timedisc/timeloop.hh"
 #include "dumux/material/vangenuchtenlaw.hh"
+#include "dumux/material/brookscoreylaw.hh"
 #include "dumux/material/multicomponentrelations.hh"
 #include "dumux/material/properties.hh"
 
@@ -24,20 +25,23 @@ int main(int argc, char** argv)
     const int dim=2;
     typedef double NumberType;
     Dune::FieldVector<NumberType, dim> outerLowerLeft(0);
-    Dune::FieldVector<NumberType, dim> outerUpperRight(6);
-    outerUpperRight[1] = 4;
+    Dune::FieldVector<NumberType, dim> outerUpperRight(60);
+    outerUpperRight[1] = 40;
+    double depthBOR = 1000.0;
+
+    // for defining e.g. a lense
     Dune::FieldVector<NumberType, dim> innerLowerLeft(4);
     innerLowerLeft[1] = 0.0;
     Dune::FieldVector<NumberType, dim> innerUpperRight(6);
     innerUpperRight[1] = 0.5;
-    double depthBOR = 5.0;
 
     // count number of arguments
     if (argc != 3) {
       std::cout << "usage: test_twophase tEnd dt" << std::endl;
       return 0;
     }
-    // define tEnd
+    
+    // define tEnd 
     std::string arg1(argv[1]);
     std::istringstream is1(arg1);
     double tEnd; is1 >> tEnd;
@@ -55,6 +59,7 @@ int main(int argc, char** argv)
     // use unitcube from grids (UGGrid)
     std::stringstream dgfFileName;
     dgfFileName << "dune-mux/test/twophase/grids/unitcube" 
+//    dgfFileName << "grids/unitcube" 
     	<< GridType :: dimension << ".dgf";
 
     // create grid pointer, GridType is defined by gridtype.hh
@@ -66,18 +71,19 @@ int main(int argc, char** argv)
     Dune::gridinfo(grid);
 
     // choose fluids and properties
-    Water wPhase; Air nPhase;
-    Dune::VanGenuchtenLaw law(wPhase, nPhase);
+    Water wPhase; CO2 nPhase;
+    Dune::BrooksCoreyLaw law(wPhase, nPhase);
 //    Dune::LinearLaw law(wPhase, nPhase);    
     Dune::CWaterAir multicomp(wPhase, nPhase);
     //Dune::LinearLaw law(water, air);
     
     // create problem properties and geometry
-    Dune::InjectionProblem<GridType, NumberType> problem(law, multicomp, outerLowerLeft, outerUpperRight, 
+    Dune::InjectionProblem<GridType, NumberType> problem(law, multicomp, 
+    		outerLowerLeft, outerUpperRight, 
     		innerLowerLeft, innerUpperRight, depthBOR);
 
     // create two-phase two-component problem
-    typedef Stupid::VtkMultiWriter<GridType> MultiWriter;
+    typedef Dune::VtkMultiWriter<GridType> MultiWriter;
     typedef Dune::Box2P2C<GridType, NumberType, MultiWriter> TwoPhaseTwoComp;
     TwoPhaseTwoComp twoPhasetwoComp(grid, problem);
     
@@ -85,33 +91,10 @@ int main(int argc, char** argv)
     
     Dune::Timer timer;
     timer.reset();
-    Stupid::VtkMultiWriter<GridType> writer("2p2c");
+    Dune::VtkMultiWriter<GridType> writer("2p2c");
     timeloop.executeMultiWriter(twoPhasetwoComp, writer);
     std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
 
-// COMPARSION TO TWOPHASE MODEL    
-//    Dune::LensProblemOld<GridType, NumberType> problemOld(law, outerLowerLeft, outerUpperRight, 
-//    		innerLowerLeft, innerUpperRight, depthBOR);
-//
-//    typedef Dune::BoxPwSn<GridType, NumberType> TwoPhaseOld;
-//    TwoPhaseOld twoPhaseOld(grid, problemOld);
-//    
-//    Dune::TimeLoop<GridType, TwoPhaseOld> timeloopOld(0, tEnd, dt, "lensOld", 1);
-//    
-//    //Dune::Timer timer;
-//    timer.reset();
-//    timeloopOld.execute(twoPhaseOld);
-//    std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
-//
-//    *twoPhasetwoComp.u -= *twoPhaseOld.u; 
-//    std::cout << "difference = " << (*twoPhasetwoComp.u).two_norm()/(*twoPhaseOld.u).two_norm() 
-//    	<< std::endl;
-
-////////////////////////////////
-    
-    
-//    
-//    	printvector(std::cout, *twoPhase.u, "u", "row", 2, 1, 3);
 
     return 0;
   }
