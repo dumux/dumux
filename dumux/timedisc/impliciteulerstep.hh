cfa3a58f5f3bbb022fb1dd3717c6016710e29a53
Melanie Darcis <mdarcis@gmx.de>
2008-08-01 16:54:07 +0000

    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@440 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/timedisc/impliciteulerstep.hh
+++ dumux/timedisc/impliciteulerstep.hh
@@ -10,13 +10,13 @@ namespace Dune {
 						double maxDt, double tEnd, double cFLFactor)
 		{
 			double eps = 1e-8;
+                        dt = std::min( dt, maxDt );
 
 			if (tEnd - t <= (1+eps)*dt)
 				dt = (1+eps)*(tEnd - t);
 			
 			model.update(dt);
-			dt = std::min( dt, maxDt );
-		    dt = std::min(dt, tEnd - t);
+		    	dt = std::min(dt, tEnd - t);
 
 		    return;			
 		}
