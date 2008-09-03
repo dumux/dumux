a9623d06d430dbd110fd287c6e10e0564f409582
Markus Wolff <markus.wolff@twt-gmbh.de>
2008-09-03 13:02:34 +0000

    corrections - change to new version/old version changed to _deprecated
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@516 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/capillary/test_capillary.cc
+++ test/capillary/test_capillary.cc
@@ -9,12 +9,12 @@
 #include "dumux/material/linearlaw_deprecated.hh"
 #include "dumux/material/brookscoreylaw_deprecated.hh"
 #include "dumux/material/vangenuchtenlaw_deprecated.hh"
-#include "dumux/transport/fv/fvtransport.hh"
+#include "dumux/transport/fv/fvtransport_deprecated.hh"
 #include "dumux/transport/fv/capillarydiffusion.hh"
-#include "dumux/diffusion/fv/fvdiffusion.hh"
-#include "dumux/diffusion/fv/fvdiffusionvelocity.hh"
+#include "dumux/diffusion/fv/fvdiffusion_deprecated.hh"
+#include "dumux/diffusion/fv/fvdiffusionvelocity_deprecated.hh"
 #include "dumux/diffusion/mimetic/mimeticdiffusion.hh"
-#include "dumux/fractionalflow/impes/impes.hh"
+#include "dumux/fractionalflow/impes/impes_deprecated.hh"
 #include "dumux/transport/problems/buckleyleverettproblem.hh"
 #include "dumux/transport/problems/simpleproblem.hh"
 #include "dumux/diffusion/problems/uniformproblem.hh"
@@ -22,51 +22,51 @@
 #include "dumux/timedisc/timeloop.hh"
 #include "dumux/timedisc/rungekuttastep.hh"
 #include "dumux/fractionalflow/variableclass.hh"
- 
-int main(int argc, char** argv) 
+
+int main(int argc, char** argv)
 {
   try{
-    // define the problem dimensions  
+    // define the problem dimensions
     const int dim=2;
 
-    // plotting parameters 
+    // plotting parameters
     char* fileName("with_pc");
-    int modulo = 3; 
+    int modulo = 3;
 
     // time loop parameters
     const double tStart = 0;
     const double tEnd = 5e9;
     const double cFLFactor = 0.2;
-    
+
     // slope limiter parameters
     bool reconstruct = true;
     double alphaMax = 0.8;
-    
+
     // IMPES parameters
-    int iterFlag = 0; 
-    int nIter = 1; 
+    int iterFlag = 0;
+    int nIter = 1;
     double maxDefect = 1e-5;
 
     // create a grid object
-    typedef double NumberType; 
-    typedef Dune::SGrid<dim,dim> GridType; 
-    typedef Dune::FieldVector<GridType::ctype,dim> FieldVector; 
-    Dune::FieldVector<int,dim> N(1); N[0] = 50;                   
-    FieldVector L(0); 
-    FieldVector H(300); H[0] = 600; 
-    GridType grid(N,L,H);  
- 
+    typedef double NumberType;
+    typedef Dune::SGrid<dim,dim> GridType;
+    typedef Dune::FieldVector<GridType::ctype,dim> FieldVector;
+    Dune::FieldVector<int,dim> N(1); N[0] = 50;
+    FieldVector L(0);
+    FieldVector H(300); H[0] = 600;
+    GridType grid(N,L,H);
+
     grid.globalRefine(0);
 
     Uniform mat;
     //Dune::LinearLaw materialLaw(mat, mat);
     Dune::BrooksCoreyLaw materialLaw(mat, mat,2,10000);
     //Dune::VanGenuchtenLaw materialLaw(mat, mat);
-    
+
     typedef Dune::VariableClass<GridType, NumberType> VC;
-    
+
     VC variables(grid);
-    
+
     Dune::BuckleyLeverettProblem<GridType, NumberType, VC> transportProblem(variables, materialLaw);
     Dune::UniformProblem<GridType, NumberType,VC> diffusionProblem(variables, materialLaw);
     //Dune::HeterogeneousProblem<GridType, NumberType> diffusionProblem(grid, "permeab.dat", false, materialLaw);
@@ -76,21 +76,21 @@ int main(int argc, char** argv)
     //Dune::DiffusivePart<GridType, NumberType> diffPart;
     Dune::CapillaryDiffusion<GridType, NumberType,VC> diffPart(diffusionProblem);
     Transport transport(grid, transportProblem, grid.maxLevel(), diffPart, reconstruct, alphaMax);
-        
+
     typedef Dune::FVDiffusionVelocity<GridType, NumberType,VC> Diffusion;
     Diffusion diffusion(grid, diffusionProblem, grid.maxLevel());
 
     typedef Dune::IMPES<GridType, Diffusion, Transport,VC> IMPES;
     IMPES fractionalflow(diffusion, transport, iterFlag, nIter, maxDefect);
-    
+
     Dune::TimeLoop<GridType, IMPES > timeloop(tStart, tEnd, fileName, modulo, cFLFactor);
-    
+
     Dune::Timer timer;
     timer.reset();
     timeloop.execute(fractionalflow);
     std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
     //printvector(std::cout, *fractionalflow, "saturation", "row", 200, 1);
-    
+
     return 0;
   }
   catch (Dune::Exception &e){
