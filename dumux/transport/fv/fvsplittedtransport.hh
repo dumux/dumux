9ee171b941780fbb834b59a10a0555fda0eafa0e
Markus Wolff <markus.wolff@twt-gmbh.de>
2008-09-08 09:50:44 +0000

    changed include fvtransport.hh to include fvtransport_deprecated.hh
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@535 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/transport/fv/fvsplittedtransport.hh
+++ dumux/transport/fv/fvsplittedtransport.hh
@@ -2,7 +2,7 @@
 #define DUNE_FVSPLITTEDTRANSPORT_HH
 
 #include "dumux/transport/splittedtransport.hh"
-#include "dumux/transport/fv/fvtransport.hh"
+#include "dumux/transport/fv/fvtransport_deprecated.hh"
 
 /**
  * @file
@@ -15,23 +15,23 @@ namespace Dune
 {
   //! \ingroup transport
   //! Base class for defining an instance of a numerical transport model.
-  /*! An interface for defining a numerical transport model for the 
-   *  solution of equations of the form 
-   *  \f$S_t - \text{div}\, (f_\text{w}(S) \boldsymbol{v}_\text{total}) = 0\f$, 
-   * \f$S = g\f$ on \f$\Gamma_1\f$, and \f$S(t = 0) = S_0\f$. Here, 
-   * \f$S\f$ denotes the wetting phase saturation, 
-   * \f$\boldsymbol{v}_\text{total}\f$ the total velocity, 
+  /*! An interface for defining a numerical transport model for the
+   *  solution of equations of the form
+   *  \f$S_t - \text{div}\, (f_\text{w}(S) \boldsymbol{v}_\text{total}) = 0\f$,
+   * \f$S = g\f$ on \f$\Gamma_1\f$, and \f$S(t = 0) = S_0\f$. Here,
+   * \f$S\f$ denotes the wetting phase saturation,
+   * \f$\boldsymbol{v}_\text{total}\f$ the total velocity,
    * and \f$f_\text{w}\f$ the wetting phase fractional flow function.
 
 	- Grid      a DUNE grid type
-	- RT        type used for return values 
-	- RepresentationType   type of the vector holding the saturation values 
-	- VelType   type of the vector holding the velocity values 
+	- RT        type used for return values
+	- RepresentationType   type of the vector holding the saturation values
+	- VelType   type of the vector holding the velocity values
 
    */
   template<class G, class RT, class VC>
-  class FVSplittedTransport : public SplittedTransport< G, RT, VC, 
-							FVTransport<G, RT, VC>, FVTransport<G, RT, VC> > 
+  class FVSplittedTransport : public SplittedTransport< G, RT, VC,
+							FVTransport<G, RT, VC>, FVTransport<G, RT, VC> >
   {
   public:
     typedef typename FVTransport<G, RT, VC>::RepresentationType HyperbolicRepresentationType;
@@ -39,53 +39,53 @@ namespace Dune
     typedef typename FVTransport<G, RT, VC>::RepresentationType RepresentationType;
     typedef FVTransport<G, RT, VC>  HyperbolicType;
     typedef FVTransport<G, RT, VC>  ParabolicType;
-   
 
-    virtual void transferHyperbolicToParabolic(const HyperbolicRepresentationType& hyperSat, 
+
+    virtual void transferHyperbolicToParabolic(const HyperbolicRepresentationType& hyperSat,
 					       ParabolicRepresentationType& paraSat)
     {
       paraSat = hyperSat;
       return;
     }
 
-    virtual void transferParabolicToHyperbolic(const ParabolicRepresentationType& paraSat, 
+    virtual void transferParabolicToHyperbolic(const ParabolicRepresentationType& paraSat,
 					       HyperbolicRepresentationType& hyperSat)
     {
       hyperSat = paraSat;
       return;
     }
 
-    virtual void transferParabolicToRepresentationType(const ParabolicRepresentationType& paraSat, 
-					       RepresentationType& rSat) 
+    virtual void transferParabolicToRepresentationType(const ParabolicRepresentationType& paraSat,
+					       RepresentationType& rSat)
     {
       rSat = paraSat;
       return;
     }
 
-    virtual void transferRepresentationTypeToParabolic(const RepresentationType& rSat, 
-					       ParabolicRepresentationType& paraSat) 
+    virtual void transferRepresentationTypeToParabolic(const RepresentationType& rSat,
+					       ParabolicRepresentationType& paraSat)
     {
       paraSat = rSat;
       return;
     }
 
     //! generate vtk output
-    virtual void vtkout (const char* name, int k) const 
+    virtual void vtkout (const char* name, int k) const
     {
       Dune::VTKWriter<G, typename G::LevelGridView> vtkwriter(this->grid.levelView(this->parabolicLevel()));
-      char fname[128];	
+      char fname[128];
       sprintf(fname,"%s-%05d",name,k);
       vtkwriter.addCellData(this->sat,"saturation");
-      vtkwriter.write(fname,Dune::VTKOptions::ascii);		
+      vtkwriter.write(fname,Dune::VTKOptions::ascii);
     }
 
     /*! @brief constructor
      *  @param g a DUNE grid object
      *  @param prob an object of class TransportProblem or derived
      */
-    FVSplittedTransport(const G& g, HyperbolicType& hyper, ParabolicType& para) 
+    FVSplittedTransport(const G& g, HyperbolicType& hyper, ParabolicType& para)
       : SplittedTransport< G, RT, VC, FVTransport<G, RT, VC>, FVTransport<G, RT, VC> >(g, hyper, para)
-    { 
+    {
       this->sat.resize(this->parabolicPart.transproblem.variables.saturation.size());
     }
   };
