55fa495b582454a86f88f241769c125cd8d17bb3
Markus Wolff <markus.wolff@twt-gmbh.de>
2008-09-08 07:26:39 +0000

    changed include diffusion.hh to include diffusion_deprecated.hh
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@530 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/diffusion/fe/fediffusion.hh
+++ dumux/diffusion/fe/fediffusion.hh
@@ -9,7 +9,7 @@
 #include <dune/istl/operators.hh>
 #include <dune/istl/solvers.hh>
 #include <dune/istl/preconditioners.hh>
-#include "dumux/diffusion/diffusion.hh"
+#include "dumux/diffusion/diffusion_deprecated.hh"
 #include "dune/disc/operators/p1operator.hh"
 #include "dumux/diffusion/fe/p1groundwater.hh"
 #include "dumux/transport/problems/simpleproblem.hh"
@@ -26,23 +26,23 @@ namespace Dune
 {
   //! \ingroup diffusion
   //! Base class for defining an instance of a numerical diffusion model.
-  /*! An interface for defining a numerical diffusion model for the 
-   *  solution of equations of the form 
-   * \f$ - \text{div}\, (\lambda K \text{grad}\, p ) = q, \f$, 
-   * \f$p = g\f$ on \f$\Gamma_1\f$, and 
-   * \f$\lambda K \text{grad}\, p \cdot \mathbf{n} = J\f$ 
-   * on \f$\Gamma_2\f$. Here, 
-   * \f$p\f$ denotes the pressure, \f$K\f$ the absolute permeability, 
-   * and \f$\lambda\f$ the total mobility, possibly depending on the 
-   * saturation, \f$q\f$ the source term. 
+  /*! An interface for defining a numerical diffusion model for the
+   *  solution of equations of the form
+   * \f$ - \text{div}\, (\lambda K \text{grad}\, p ) = q, \f$,
+   * \f$p = g\f$ on \f$\Gamma_1\f$, and
+   * \f$\lambda K \text{grad}\, p \cdot \mathbf{n} = J\f$
+   * on \f$\Gamma_2\f$. Here,
+   * \f$p\f$ denotes the pressure, \f$K\f$ the absolute permeability,
+   * and \f$\lambda\f$ the total mobility, possibly depending on the
+   * saturation, \f$q\f$ the source term.
 	Template parameters are:
 
 	- Grid      a DUNE grid type
-	- RT        type used for return values 
+	- RT        type used for return values
    */
   template<class G, class RT, class VC, class LocalStiffnessType = GroundwaterEquationLocalStiffness<G,RT> >
-  class FEDiffusion 
-  : public Diffusion< G, RT, VC > 
+  class FEDiffusion
+  : public Diffusion< G, RT, VC >
   {
 	  template<int dim>
 	  struct ElementLayout
@@ -51,74 +51,74 @@ namespace Dune
 	      {
 			  return gt.dim() == dim;
 	      }
-	  }; 
-	  
+	  };
+
 	  typedef LevelP1Function<G,RT,1> PressP1Type;
-	  typedef LevelP1OperatorAssembler<G,RT,1> LevelOperatorAssembler; 
+	  typedef LevelP1OperatorAssembler<G,RT,1> LevelOperatorAssembler;
 
   public:
         typedef BlockVector< FieldVector<FieldVector<RT, G::dimension>, 2*G::dimension> > VelType;
 	typedef BlockVector< FieldVector<RT,1> > RepresentationType;
 
-	void assemble(const RT t=0) 
+	void assemble(const RT t=0)
 	{
 		LocalStiffnessType lstiff(this->problem, false, this->grid, this->level());
-		A.assemble(lstiff, pressP1, f);		
+		A.assemble(lstiff, pressP1, f);
 		return;
 	}
-	
-	void solve() 
+
+	void solve()
 	{
 		typedef typename LevelP1Function<G,RT>::RepresentationType VectorType;
 		typedef typename LevelP1OperatorAssembler<G,RT,1>::RepresentationType MatrixType;
-		typedef MatrixAdapter<MatrixType,VectorType,VectorType> Operator; 
-		
+		typedef MatrixAdapter<MatrixType,VectorType,VectorType> Operator;
+
 		Operator op(*A);  // make operator out of matrix
 		double red=1E-10;
 		SeqILU0<MatrixType,VectorType,VectorType> ilu0(*A,1.0);// a precondtioner
-		CGSolver<VectorType> solver(op,ilu0,red,10000,1);         // an inverse operator 
+		CGSolver<VectorType> solver(op,ilu0,red,10000,1);         // an inverse operator
 		InverseOperatorResult r;
 		solver.apply(*pressP1, *f, r);
 		this->press = *pressP1;
 
-		return;		
+		return;
 	}
-	
-	void pressure(const RT t=0) 
+
+	void pressure(const RT t=0)
 	{
 		assemble(t);
 		solve();
 		return;
-	}	
-	
+	}
+
 	void totalVelocity(VelType& velocity, const RT t=0) const
 	{
-		DUNE_THROW(NotImplemented,"FEDiffusion :: totalVelocity");	
+		DUNE_THROW(NotImplemented,"FEDiffusion :: totalVelocity");
 		return;
 	}
 
-	void vtkout (const char* name, int k) const 
+	void vtkout (const char* name, int k) const
 	{
-		VTKWriter<G, typename G::LevelGridView> 
+		VTKWriter<G, typename G::LevelGridView>
 			vtkwriter(this->grid.levelView(this->level()));
-		char fname[128];	
+		char fname[128];
 		sprintf(fname,"%s-%05d",name,k);
 		vtkwriter.addVertexData(this->diffproblem.variables.pressure,"total pressure p~");
-		vtkwriter.write(fname, VTKOptions::ascii);		
+		vtkwriter.write(fname, VTKOptions::ascii);
 	}
-	
-	FEDiffusion(G& g, DiffusionProblem<G, RT, VC>& prob, 
-		    TransportProblem<G, RT, VC>& satprob = *(new typename Dune::SimpleProblem<G, RT, VC>), 
+
+	FEDiffusion(G& g, DiffusionProblem<G, RT, VC>& prob,
+		    TransportProblem<G, RT, VC>& satprob = *(new typename Dune::SimpleProblem<G, RT, VC>),
 		    int lev = -1)
-	  : Diffusion<G, RT, VC>(g, prob, lev == -1 ? g.maxLevel() : lev), 
+	  : Diffusion<G, RT, VC>(g, prob, lev == -1 ? g.maxLevel() : lev),
 	  pressP1(g, this->level()), f(g, this->level()), A(g, this->level())
-	{ 
+	{
 		this->press.resize(g.size(this->level(), G::dimension));
 		*pressP1 = 0;
 	}
 
-	PressP1Type pressP1;         
-	PressP1Type f;         
+	PressP1Type pressP1;
+	PressP1Type f;
 	LevelOperatorAssembler A;
   };
 
