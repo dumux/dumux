b2b91a0adea793edb05f7e00174203a811e809ca
Klaus Mosthaf <klmos@env.dtu.dk>
2008-09-09 13:07:03 +0000

    Adapted Injectionproblem and test_2p2c.cc to the new structure of the material properties. Generated class 'Injectionsoil' in injectionproblem.hh for problemspecific soil definition
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@541 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/twophase/injectionproblem.hh
+++ test/twophase/injectionproblem.hh
@@ -10,8 +10,11 @@
 #include<dune/grid/common/grid.hh>
 #include<dune/grid/common/referenceelements.hh>
 #include<dune/disc/operators/boundaryconditions.hh>
-#include<dumux/material/twophaserelations_deprecated.hh>
-#include<dumux/material/linearlaw_deprecated.hh>
+
+#include<dumux/material/property_baseclasses.hh>
+#include<dumux/material/twophaserelations.hh>
+#include<dumux/material/multicomponentrelations.hh>
+#include<dumux/material/relperm_pc_law.hh>
 #include<dumux/2p2c/2p2cproblem.hh>
 
 /**
@@ -23,18 +26,18 @@
 namespace Dune
 {
   //! base class that defines the parameters of a diffusion equation
-  /*! An interface for defining parameters for the stationary diffusion equation 
-   * \f$ - \text{div}\, (\lambda K \text{grad}\, p ) = q, \f$, 
-   * \f$p = g\f$ on \f$\Gamma_1\f$, and \f$\lambda K \text{grad}\, p = J\f$ 
-   * on \f$\Gamma_2\f$. Here, 
-   * \f$p\f$ denotes the pressure, \f$K\f$ the absolute permeability, 
-   * and \f$\lambda\f$ the total mobility, possibly depending on the 
-   * saturation. 
+  /*! An interface for defining parameters for the stationary diffusion equation
+   * \f$ - \text{div}\, (\lambda K \text{grad}\, p ) = q, \f$,
+   * \f$p = g\f$ on \f$\Gamma_1\f$, and \f$\lambda K \text{grad}\, p = J\f$
+   * on \f$\Gamma_2\f$. Here,
+   * \f$p\f$ denotes the pressure, \f$K\f$ the absolute permeability,
+   * and \f$\lambda\f$ the total mobility, possibly depending on the
+   * saturation.
    *
    *	Template parameters are:
-   *	
+   *
    *	- Grid  a DUNE grid type
-   *	- RT    type used for return values 
+   *	- RT    type used for return values
    */
   template<class G, class RT>
   class InjectionProblem : public TwoPTwoCProblem<G, RT> {
@@ -49,19 +52,15 @@ namespace Dune
 	enum {swrIdx = 0, snrIdx = 1, lamIdx = 2, pbIdx = 3};
 	enum {gasPhase = 0, waterPhase = 1, bothPhases = 2};
 
-	// permeabilities
-	virtual const FieldMatrix<DT,dim,dim>& K (const FieldVector<DT,dim>& x)
-	//, const Entity& e, const FieldVector<DT,dim>& xi)
-	{
-		if (x[0] >= innerLowerLeft_[0] && x[0] <= innerUpperRight_[0] 
-		    && x[1] >= innerLowerLeft_[1] && x[1] <= innerUpperRight_[1])
-			return innerK_;
-		else
-			return outerK_;
-	}
+//	// permeabilities
+//	virtual const FieldMatrix<DT,dim,dim>& K (const FieldVector<DT,dim>& x)
+//	//, const Entity& e, const FieldVector<DT,dim>& xi)
+//	{
+//			return outerK_;
+//	}
 
 	// sources and sinks
-	virtual FieldVector<RT,m> q (const FieldVector<DT,dim>& x, const Entity& e, 
+	virtual FieldVector<RT,m> q (const FieldVector<DT,dim>& x, const Entity& e,
 					const FieldVector<DT,dim>& xi) const
 	{
 		FieldVector<RT,m> values(0);
@@ -69,16 +68,16 @@ namespace Dune
 		return values;
 	}
 
-	
-	
+
+
 /////////////////////////////
 // TYPE of the boundaries
 /////////////////////////////
-	virtual FieldVector<BoundaryConditions::Flags, m> bctype (const FieldVector<DT,dim>& x, const Entity& e, 
-					const IntersectionIterator& intersectionIt, 
-					   const FieldVector<DT,dim>& xi) const 
+	virtual FieldVector<BoundaryConditions::Flags, m> bctype (const FieldVector<DT,dim>& x, const Entity& e,
+					const IntersectionIterator& intersectionIt,
+					   const FieldVector<DT,dim>& xi) const
 	{
-		FieldVector<BoundaryConditions::Flags, m> values(BoundaryConditions::neumann); 
+		FieldVector<BoundaryConditions::Flags, m> values(BoundaryConditions::neumann);
 
 		if (x[0] < outerLowerLeft_[0] + eps_)
 			values = BoundaryConditions::dirichlet;
@@ -87,35 +86,35 @@ namespace Dune
 
 		return values;
 	}
-	
-	
+
+
 /////////////////////////////
 // DIRICHLET boundaries
 /////////////////////////////
-	virtual FieldVector<RT,m> g (const FieldVector<DT,dim>& x, const Entity& e, 
-				const IntersectionIterator& intersectionIt, 
-				  const FieldVector<DT,dim>& xi) const 
+	virtual FieldVector<RT,m> g (const FieldVector<DT,dim>& x, const Entity& e,
+				const IntersectionIterator& intersectionIt,
+				  const FieldVector<DT,dim>& xi) const
 	{
 		FieldVector<RT,m> values(0);
 
 		values[pWIdx] = 1e5 - densityW_*gravity_[1]*(depthBOR_ - x[1]);
 		values[switchIdx] = 0.01;
-		
-//		if (x[1] >= innerLowerLeft_[1] && x[1] <= innerUpperRight_[1] 
+
+//		if (x[1] >= innerLowerLeft_[1] && x[1] <= innerUpperRight_[1]
 //		 && x[0] >= innerLowerLeft_[0])
 //			values[switchIdx] = 0.2;
 //		else
 //			values[switchIdx] = 1e-6;
-		
+
 		return values;
 	}
 
 /////////////////////////////
 // NEUMANN boundaries
 /////////////////////////////
-	virtual FieldVector<RT,m> J (const FieldVector<DT,dim>& x, const Entity& e, 
-				const IntersectionIterator& intersectionIt, 
-				  const FieldVector<DT,dim>& xi) const 
+	virtual FieldVector<RT,m> J (const FieldVector<DT,dim>& x, const Entity& e,
+				const IntersectionIterator& intersectionIt,
+				  const FieldVector<DT,dim>& xi) const
 	{
 		FieldVector<RT,m> values(0);
 
@@ -123,34 +122,34 @@ namespace Dune
 
 		if (x[1] < 15.0 && x[1] > 5.0)
 			values[switchIdx] = -1e-2;
-		
+
 		return values;
 	}
 
 /////////////////////////////
 // INITIAL values
 /////////////////////////////
-		virtual FieldVector<RT,m> initial (const FieldVector<DT,dim>& x, const Entity& e, 
-					  const FieldVector<DT,dim>& xi) const 
+		virtual FieldVector<RT,m> initial (const FieldVector<DT,dim>& x, const Entity& e,
+					  const FieldVector<DT,dim>& xi) const
 		{
 
 			FieldVector<RT,m> values;
-			
+
 			values[pWIdx] = 1e5 - densityW_*gravity_[1]*(depthBOR_ - x[1]);
 			values[switchIdx] = 0.01;
 
-//			if (x[1] >= innerLowerLeft_[1] && x[1] <= innerUpperRight_[1] 
+//			if (x[1] >= innerLowerLeft_[1] && x[1] <= innerUpperRight_[1]
 //			 && x[0] >= innerLowerLeft_[0])
 //				values[switchIdx] = 0.2;
 //			else
 //				values[switchIdx] = 1e-6;
-		
+
 			return values;
 		}
 
-		
-		int initialPhaseState (const FieldVector<DT,dim>& x, const Entity& e, 
-					  const FieldVector<DT,dim>& xi) const 
+
+		int initialPhaseState (const FieldVector<DT,dim>& x, const Entity& e,
+					  const FieldVector<DT,dim>& xi) const
 		{
 
 			enum {gasPhase = 0, waterPhase = 1, bothPhases = 2}; // Phase states
@@ -162,105 +161,153 @@ namespace Dune
 //			else
 
 			state = bothPhases;
-				
+
 			return state;
 		}
-	
-//////////////////////////////	  
-	
 
-	
-	double porosity (const FieldVector<DT,dim>& x, const Entity& e, 
-			  const FieldVector<DT,dim>& xi) const 
-	{
-		if (x[0] > innerLowerLeft_[0] && x[0] < innerUpperRight_[0] 
-		    && x[1] > innerLowerLeft_[1] && x[1] < innerUpperRight_[1])
-			return innerPorosity_;
-		else 
-			return outerPorosity_;
-	}
-	
-	virtual FieldVector<RT,dim> gravity () const 
+//////////////////////////////
+
+
+
+//	double porosity (const FieldVector<DT,dim>& x, const Entity& e,
+//			  const FieldVector<DT,dim>& xi) const
+//	{
+////		if (x[0] > innerLowerLeft_[0] && x[0] < innerUpperRight_[0]
+////		    && x[1] > innerLowerLeft_[1] && x[1] < innerUpperRight_[1])
+////			return innerPorosity_;
+////		else
+//			return outerPorosity_;
+//	}
+
+	virtual FieldVector<RT,dim> gravity () const
 	{
 		return gravity_;
 	}
-	
+
 	double depthBOR () const
 	{
 		return depthBOR_;
 	}
-	  
-	virtual FieldVector<RT,4> materialLawParameters (const FieldVector<DT,dim>& x, const Entity& e, 
-			  const FieldVector<DT,dim>& xi) const 
+
+	InjectionProblem(Liquid_GL& liq, Gas_GL& gas, Matrix2p<G, RT>& soil,
+			const FieldVector<DT,dim> outerLowerLeft = 0., const FieldVector<DT,dim> outerUpperRight = 0.,
+			const FieldVector<DT,dim> innerLowerLeft = 0., const FieldVector<DT,dim> innerUpperRight = 0.,
+			const RT depthBOR = 0., TwoPhaseRelations<G, RT>& law = *(new TwoPhaseRelations<G, RT>),
+			MultiComp& multicomp = *(new CWaterAir))
+//			RT outerK = 7.2e-13, RT innerK = 7.2e-13, RT outerSn = 0.01, RT innerSw = 0.01,
+//			RT outerPorosity = 0.4, RT innerPorosity = 0.4)
+	: TwoPTwoCProblem<G,RT>(soil, multicomp, law),
+	  outerLowerLeft_(outerLowerLeft), outerUpperRight_(outerUpperRight),
+	  depthBOR_(depthBOR), eps_(1e-8*outerUpperRight[0]),
+	  wetPhase_(liq), nwetPhase_(gas), soil_(soil)//, materialLaw(law)
 	{
-		FieldVector<RT,4> values;
-		
-// for BrooksCoreyLaw
-		values[swrIdx] = outerSwr_;
-		values[snrIdx] = outerSnr_;
-		values[lamIdx] = lambda_;
-		values[pbIdx] = pb_;
-
-// for VanGenuchtenLaw
-//		values[swrIdx] = innerSwr_;
-//		values[snrIdx] = innerSnr_;
-//		values[alphaIdx] = innerAlpha_;
-//		values[nIdx] = innerN_;
-		
-		return values;
-	}
-
-	InjectionProblem(TwoPhaseRelations& law = *(new LinearLaw), MultiComp& multicomp = *(new CWaterAir), 
-			const FieldVector<DT,dim> outerLowerLeft = 0., const FieldVector<DT,dim> outerUpperRight = 0., 
-			const FieldVector<DT,dim> innerLowerLeft = 0., const FieldVector<DT,dim> innerUpperRight = 0., 
-			const RT depthBOR = 0., RT outerK = 7.2e-13, RT innerK = 7.2e-13,
-			RT outerSwr = 0.01, RT outerSnr = 0.01, RT innerSwr = 0.01, RT innerSnr = 0.01, 
-			RT outerPorosity = 0.4, RT innerPorosity = 0.4, 
-			RT lambda = 2, RT pb = 10000)
-//			RT outerAlpha = 0.00047, RT innerAlpha = 0.00047,  //0.00045
-//			RT outerN = 4.7, RT innerN = 4.7)	//7.3
-	: TwoPTwoCProblem<G, RT>(law, multicomp), 
-	  outerLowerLeft_(outerLowerLeft), outerUpperRight_(outerUpperRight), 
-	  innerLowerLeft_(innerLowerLeft), innerUpperRight_(innerUpperRight), 
-	  depthBOR_(depthBOR), eps_(1e-8*outerUpperRight[0]), 
-	  densityW_(law.wettingPhase.density()), densityN_(law.nonwettingPhase.density()), 
-	  outerSwr_(outerSwr), outerSnr_(outerSnr), innerSwr_(innerSwr), innerSnr_(innerSnr), 
-	  outerPorosity_(outerPorosity), innerPorosity_(innerPorosity), 
-	  lambda_(lambda), pb_(pb)
-	  
-//	  outerAlpha_(outerAlpha), innerAlpha_(innerAlpha), 
-//	  outerN_(outerN), innerN_(innerN)
-	{	
-		outerK_[0][0] = outerK_[1][1] = outerK;
-		outerK_[0][1] = outerK_[1][0] = 0;
-		
-		innerK_[0][0] = innerK_[1][1] = innerK;
-		innerK_[0][1] = innerK_[1][0] = 0;
-		
+//		outerK_[0][0] = outerK_[1][1] = outerK;
+//		outerK_[0][1] = outerK_[1][0] = 0;
+//
+//		innerK_[0][0] = innerK_[1][1] = innerK;
+//		innerK_[0][1] = innerK_[1][0] = 0;
+//
 		height_ = outerUpperRight[1] - outerLowerLeft[1];
 		width_ = outerUpperRight[0] - outerLowerLeft[0];
-		 
+
 		gravity_[0] = 0;
 		gravity_[1] = -9.81;
 	}
-	
-	private:
-		FieldMatrix<DT,dim,dim> outerK_;
-		FieldMatrix<DT,dim,dim> innerK_;
-		FieldVector<DT,dim> outerLowerLeft_;
-		FieldVector<DT,dim> outerUpperRight_;
-		FieldVector<DT,dim> innerLowerLeft_;
-		FieldVector<DT,dim> innerUpperRight_;
-		DT width_, height_;
-		DT depthBOR_, eps_;
-		RT densityW_, densityN_;
-		FieldVector<DT,dim> gravity_;
-		RT outerSwr_, outerSnr_, innerSwr_, innerSnr_;
-		RT outerPorosity_, innerPorosity_;
-		RT lambda_, pb_;
-//		RT outerAlpha_, innerAlpha_;
-//		RT outerN_, innerN_;
+
+  private:
+//	FieldMatrix<DT,dim,dim> outerK_, innerK_;
+	FieldVector<DT,dim> outerLowerLeft_, outerUpperRight_;
+	FieldVector<DT,dim> innerLowerLeft_, innerUpperRight_;
+	DT width_, height_;
+	DT depthBOR_, eps_;
+	RT densityW_, densityN_;
+	FieldVector<DT,dim> gravity_;
+//	RT outerSnr_, innerSwr_;
+//	RT outerPorosity_, innerPorosity_;
+	Liquid_GL& wetPhase_;
+	Gas_GL& nwetPhase_;
+	Matrix2p<G,RT>& soil_;
   };
 
-}
+
+  template<class G, class RT>
+  class Injectionsoil: public Matrix2p<G,RT>
+  {
+  public:
+  	typedef typename G::Traits::template Codim<0>::Entity Entity;
+  	typedef typename G::ctype DT;
+  	enum {n=G::dimension, m=1};
+
+  	virtual FieldMatrix<DT,n,n> K (const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi)
+  	{
+  		return K_;
+  	}
+  	virtual double porosity(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
+  	{
+  		return 0.2;
+  	}
+
+  	virtual double Sr_w(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T) const
+  	{
+  		return 0.01;
+  	}
+
+  	virtual double Sr_n(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T) const
+  	{
+  		return 0.01;
+  	}
+
+  	/* ATTENTION: define heat capacity per cubic meter! Be sure, that it corresponds to porosity!
+  			 * Best thing will be to define heatCap = (specific heatCapacity of material) * density * porosity*/
+  	virtual double heatCap(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
+  	{
+  		return 	790 /* spec. heat cap. of granite */
+  						* 2700 /* density of granite */
+  						* porosity(x, e, xi);
+  	}
+
+  	virtual double heatCond(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double sat) const
+  	{
+  		static const double lWater = 0.6;
+  		static const double lGranite = 2.8;
+  		double poro = porosity(x, e, xi);
+  		double lsat = pow(lGranite, (1-poro)) * pow(lWater, poro);
+  		double ldry = pow(lGranite, (1-poro));
+  		return ldry + sqrt(sat) * (ldry - lsat);
+  	}
+
+  	virtual std::vector<double> paramRelPerm(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T) const
+  	{
+  		// example for Brooks-Corey parameters
+  		std::vector<double> param(2);
+  		param[0] = 2.; // lambda
+  		param[1] = 0.; // entry-pressures
+
+  		if (x[0] > 150)
+  			param[0] = 0.5;
+
+  		return param;
+  	}
+
+  	virtual typename Matrix2p<G,RT>::modelFlag relPermFlag(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
+  	{
+  		return Matrix2p<G,RT>::brooks_corey;
+  	}
+
+  	Injectionsoil():Matrix2p<G,RT>()
+  	{
+  		for(int i = 0; i < n; i++)
+  			K_[i][i] = 1e-10;
+  	}
+
+  	~Injectionsoil()
+  	{}
+
+  private:
+  	FieldMatrix<DT,n,n> K_;
+
+  };
+
+
+} //end namespace
 #endif
--- test/twophase/test_2p2c.cc
+++ test/twophase/test_2p2c.cc
@@ -6,28 +6,29 @@
 #include <dune/grid/uggrid.hh>
 #include <dune/grid/io/file/dgfparser/dgfparser.hh>
 #include <dune/grid/io/file/dgfparser/dgfug.hh>
-#include <dune/grid/io/file/vtk/vtkwriter.hh>
+//#include <dune/grid/io/file/vtk/vtkwriter.hh>
 #include <dune/istl/io.hh>
 #include <dune/common/timer.hh>
 #include "injectionproblem.hh"
-#include "dumux/2p2c/problems/uniformtwophaseproblem.hh"
 #include "dumux/2p2c/fv/box2p2c.hh"
 #include "dumux/timedisc/timeloop.hh"
-#include "dumux/material/vangenuchtenlaw_deprecated.hh"
-#include "dumux/material/brookscoreylaw_deprecated.hh"
+
+#include "dumux/material/phaseproperties/phaseproperties_waterair.hh"
+#include "dumux/material/matrixproperties.hh"
+#include "dumux/material/twophaserelations.hh"
+
 #include "dumux/material/multicomponentrelations.hh"
-#include "dumux/material/properties.hh"
 #include "dumux/io/vtkmultiwriter.hh"
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
   try{
-    // define the problem dimensions (geometry of problem)  
+    // define the problem dimensions (geometry of problem)
     const int dim=2;
     typedef double NumberType;
-    Dune::FieldVector<NumberType, dim> outerLowerLeft(0);
-    Dune::FieldVector<NumberType, dim> outerUpperRight(60);
-    outerUpperRight[1] = 40;
+    Dune::FieldVector<NumberType, dim> outerLowerLeft(0.0);
+    Dune::FieldVector<NumberType, dim> outerUpperRight(60.0);
+    outerUpperRight[1] = 40.0;
     double depthBOR = 1000.0;
 
     // for defining e.g. a lense
@@ -36,61 +37,55 @@ int main(int argc, char** argv)
     Dune::FieldVector<NumberType, dim> innerUpperRight(6);
     innerUpperRight[1] = 0.5;
 
-    // count number of arguments
-    if (argc != 3) {
-      std::cout << "usage: test_twophase tEnd dt" << std::endl;
+    if (argc != 4) {
+      std::cout << "usage: 2p2cni grid tEnd dt" << std::endl;
       return 0;
     }
-    
-    // define tEnd 
-    std::string arg1(argv[1]);
-    std::istringstream is1(arg1);
-    double tEnd; is1 >> tEnd;
+    // define tEnd
+    std::string arg1(argv[2]);
+	std::istringstream is1(arg1);
+	double tEnd;
+	is1 >> tEnd;
     // define dt
-    std::string arg2(argv[2]);
-    std::istringstream is2(arg2);
-    double dt; is2 >> dt;
-
+	std::string arg2(argv[3]);
+	std::istringstream is2(arg2);
+	double dt;
+	is2 >> dt;
 
     // create a grid object
-    //typedef Dune::SGrid<dim,dim> GridType; 
-    //typedef Dune::YaspGrid<dim,dim> GridType; 
-    typedef Dune::UGGrid<dim> GridType; 
+    //typedef Dune::SGrid<dim,dim> GridType;
+    //typedef Dune::YaspGrid<dim,dim> GridType;
+    typedef Dune::UGGrid<dim> GridType;
 
-    // use unitcube from grids (UGGrid)
-    std::stringstream dgfFileName;
-    dgfFileName << "grids/unitcube" << GridType :: dimension << ".dgf";
-
-    // create grid pointer, GridType is defined by gridtype.hh
-    Dune::GridPtr<GridType> gridPtr( dgfFileName.str() );
-
-    // grid reference 
-    GridType& grid = *gridPtr;
+    // use grid defined in the arguments
+    Dune::GridPtr<GridType> gridPointer(argv[1]);
+    // grid reference
+    GridType& grid = *gridPointer;
 
     Dune::gridinfo(grid);
 
     // choose fluids and properties
-    Water wPhase; CO2 nPhase;
-    Dune::BrooksCoreyLaw law(wPhase, nPhase);
-//    Dune::LinearLaw law(wPhase, nPhase);    
+    Dune::Liq_WaterAir wPhase;
+    Dune::Gas_WaterAir nPhase;
+    Dune::Injectionsoil<GridType, NumberType> soil;
+
+    Dune::TwoPhaseRelations<GridType, NumberType> materialLaw(soil, wPhase, nPhase);
     Dune::CWaterAir multicomp(wPhase, nPhase);
-    //Dune::LinearLaw law(water, air);
-    
+
     // create problem properties and geometry
-    Dune::InjectionProblem<GridType, NumberType> problem(law, multicomp, 
-    		outerLowerLeft, outerUpperRight, 
-    		innerLowerLeft, innerUpperRight, depthBOR);
+    Dune::InjectionProblem<GridType, NumberType> problem(wPhase, nPhase, soil, outerLowerLeft,
+    		outerUpperRight, innerLowerLeft, innerUpperRight, depthBOR, materialLaw, multicomp);
 
     // create two-phase two-component problem
     typedef Dune::VtkMultiWriter<GridType> MultiWriter;
     typedef Dune::Box2P2C<GridType, NumberType, MultiWriter> TwoPhaseTwoComp;
     TwoPhaseTwoComp twoPhasetwoComp(grid, problem);
-    
+
     Dune::TimeLoop<GridType, TwoPhaseTwoComp> timeloop(0, tEnd, dt, "lens", 5);
-    
+
     Dune::Timer timer;
     timer.reset();
-    Dune::VtkMultiWriter<GridType> writer("2p2c");
+    Dune::VtkMultiWriter<GridType> writer("out2p2c");
     timeloop.executeMultiWriter(twoPhasetwoComp, writer);
     std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
 
@@ -104,7 +99,7 @@ int main(int argc, char** argv)
     std::cerr << "Unknown exception thrown!" << std::endl;
   }
 }
-#else 
+#else
 
 int main (int argc , char **argv) try
 {
@@ -112,9 +107,9 @@ int main (int argc , char **argv) try
 
   return 1;
 }
-catch (...) 
+catch (...)
 {
     std::cerr << "Generic exception!" << std::endl;
     return 2;
 }
-#endif 
+#endif
