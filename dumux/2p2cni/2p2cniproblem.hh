78d929dc0ce9da310409598b74b1d016057b3628
Klaus Mosthaf <klmos@env.dtu.dk>
2008-09-11 08:15:01 +0000

    Added some virtual functions and media
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@561 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/2p2cni/2p2cniproblem.hh
+++ dumux/2p2cni/2p2cniproblem.hh
@@ -130,20 +130,18 @@ public:
 
 	virtual FieldVector<RT,dim> gravity() const = 0;
 
-	MultiComp& multicomp ()
+	virtual MultiComp& multicomp ()
 	{
 		return multicomp_;
 	}
 
-	TwoPhaseRelations<G, RT>& materialLaw ()
-	{
-		return materialLaw_;
-	}
+	virtual Liquid_GL& wettingPhase () const = 0;
 
-    Matrix2p<G, RT>& soil ()
-    {
-    	return soil_;
-    }
+	virtual Gas_GL& nonwettingPhase () const = 0;
+
+	virtual TwoPhaseRelations<G, RT>& materialLaw () const = 0;
+
+    virtual Matrix2p<G, RT>& soil () const = 0;
 
 	//element-wise return of the values of an Exact solution
 	virtual RT uExOutVertex(int &ElementIndex, int VariableIndex) const {
