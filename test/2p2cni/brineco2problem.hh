e1a463a8d10d516e257bfea7062dd44af3defbad
Melanie Darcis <mdarcis@gmx.de>
2008-09-18 17:01:25 +0000

    problem file for 2D brine CO2 system
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@609 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/2p2cni/brineco2problem.hh
+++ test/2p2cni/brineco2problem.hh
@@ -1,3 +1,4 @@
+// $Id$
 #ifndef DUNE_BRINECO2PROBLEM_HH
 #define DUNE_BRINECO2PROBLEM_HH
 
@@ -10,10 +11,12 @@
 #include<dune/grid/common/grid.hh>
 #include<dune/grid/common/referenceelements.hh>
 #include<dune/disc/operators/boundaryconditions.hh>
-#include<dumux/material/twophaserelations_deprecated.hh>
+
+#include<dumux/material/property_baseclasses.hh>
+#include<dumux/material/twophaserelations.hh>
+#include<dumux/material/multicomponentrelations.hh>
+#include<dumux/material/relperm_pc_law.hh>
 #include<dumux/2p2cni/2p2cniproblem.hh>
-#include<dumux/material/linearlaw_deprecated.hh>
-#include<dumux/material/brookscoreylaw_deprecated.hh>
 
 
 /**
@@ -25,48 +28,32 @@
 namespace Dune
 {
   //! base class that defines the parameters of a diffusion equation
-  /*! An interface for defining parameters for the stationary diffusion equation 
-   * \f$ - \text{div}\, (\lambda K \text{grad}\, p ) = q, \f$, 
-   * \f$p = g\f$ on \f$\Gamma_1\f$, and \f$\lambda K \text{grad}\, p = J\f$ 
-   * on \f$\Gamma_2\f$. Here, 
-   * \f$p\f$ denotes the pressure, \f$K\f$ the absolute permeability, 
-   * and \f$\lambda\f$ the total mobility, possibly depending on the 
-   * saturation. 
+  /*! An interface for defining parameters for the stationary diffusion equation
+   * \f$ - \text{div}\, (\lambda K \text{grad}\, p ) = q, \f$,
+   * \f$p = g\f$ on \f$\Gamma_1\f$, and \f$\lambda K \text{grad}\, p = J\f$
+   * on \f$\Gamma_2\f$. Here,
+   * \f$p\f$ denotes the pressure, \f$K\f$ the absolute permeability,
+   * and \f$\lambda\f$ the total mobility, possibly depending on the
+   * saturation.
    *
    *	Template parameters are:
-   *	
+   *
    *	- Grid  a DUNE grid type
-   *	- RT    type used for return values 
+   *	- RT    type used for return values
    */
   template<class G, class RT>
   class BrineCO2Problem : public TwoPTwoCNIProblem<G, RT> {
 	typedef typename G::ctype DT;
-	enum {dim=G::dimension, m=3};
-	enum {swrIdx=0, snrIdx=1, lamIdx=2, pbIdx=3};
-	enum {h2o = 0, co2 = 1};
 	typedef typename G::Traits::template Codim<0>::Entity Entity;
 	typedef typename IntersectionIteratorGetter<G,LeafTag>::IntersectionIterator IntersectionIterator;
+	enum {dim=G::dimension, m=3};
+	enum {wPhase = 0, nPhase = 1};
+	enum {pWIdx = 0, switchIdx = 1, teIdx = 2};
+
 
   public:
-	virtual const FieldMatrix<DT,dim,dim>& K (const FieldVector<DT,dim>& x, const Entity& e, 
-					const FieldVector<DT,dim>& xi)
-	{
-		return permloc_;
-	}
 
-	// permeabilities
-	virtual const FieldMatrix<DT,dim,dim>& K (const FieldVector<DT,dim>& x)
-	//, const Entity& e, const FieldVector<DT,dim>& xi)
-	{
-		return permloc_;
-	}
-		
-	virtual const FieldVector<RT,2>& D (const FieldVector<DT,dim>& x) const 
-	{
-		return diffusion_;
-	}
-	
-	virtual FieldVector<RT,m> q (const FieldVector<DT,dim>& x, const Entity& e, 
+	virtual FieldVector<RT,m> q (const FieldVector<DT,dim>& x, const Entity& e,
 					const FieldVector<DT,dim>& xi) const
 	{
 		FieldVector<RT,m> values(0);
@@ -74,154 +61,99 @@ namespace Dune
 		return values;
 	}
 
-	virtual FieldVector<BoundaryConditions::Flags, m> bctype (const FieldVector<DT,dim>& x, const Entity& e, 
-					const IntersectionIterator& intersectionIt, 
-					   const FieldVector<DT,dim>& xi) const 
+	virtual FieldVector<BoundaryConditions::Flags, m> bctype (const FieldVector<DT,dim>& x, const Entity& e,
+					const IntersectionIterator& intersectionIt,
+					   const FieldVector<DT,dim>& xi) const
 	{
-		FieldVector<BoundaryConditions::Flags, m> values(Dune::BoundaryConditions::dirichlet); 
-		// West and South Boundary
-		if((x[0] < 1.e-2 && (x[1] < 4 || x[1] > 6)) || (x[1] < 1.e-2))
-			values = Dune::BoundaryConditions::neumann;
-		
+		FieldVector<BoundaryConditions::Flags, m> values(Dune::BoundaryConditions::neumann);
+
+		if(x[0] >= 10-1e-2 )
+		values = Dune::BoundaryConditions::dirichlet;
+		if(x[0] < 1e-2 && x[1] < 5)
+		values[2] = Dune::BoundaryConditions::dirichlet;
+
 		return values;
 	}
-	
+
 	virtual void dirichletIndex(const FieldVector<DT,dim>& x, const Entity& e,
 			const IntersectionIterator& intersectionIt,
-			const FieldVector<DT,dim>& xi, FieldVector<int,m>& dirichletIndex) const 
+			const FieldVector<DT,dim>& xi, FieldVector<int,m>& dirichletIndex) const
 	{
 		for (int i = 0; i < m; i++)
 			dirichletIndex[i]=i;
 		return;
 	}
 
-	virtual FieldVector<RT,m> g (const FieldVector<DT,dim>& x, const Entity& e, 
-				const IntersectionIterator& intersectionIt, 
-				  const FieldVector<DT,dim>& xi) const 
+	virtual FieldVector<RT,m> g (const FieldVector<DT,dim>& x, const Entity& e,
+				const IntersectionIterator& intersectionIt,
+				  const FieldVector<DT,dim>& xi) const
 	{
 		FieldVector<RT,m> values(0);
-		
-		values[0] = p0_ - x[1] * 1070 * 9.81; 
-		values[1] = 1.e-9;
-		values[2] = t0_ - x[1]*0.03;
-		
-		if(x[0] > 10-1.e-2 && x[1] >= 4. && x[1] <= 6.)
+
+		values[0] = 1.013e5 + (depthBOR_ - x[1]) * 1045 * 9.81;
+		values[1] = 0.0;
+		values[2] = 283.15 + (depthBOR_ - x[1])*0.03;
+
+		return values;
+	}
+
+	virtual FieldVector<RT,m> J (const FieldVector<DT,dim>& x, const Entity& e,
+				const IntersectionIterator& intersectionIt, const FieldVector<DT,dim>& xi) const
+	{
+		FieldVector<RT,m> values(0);
+		if(x[0] <= 1.e-2 && x[1] <= 5.)
 		{
-			values[0] = 1.E7;
-			values[1] = 0.5;
-			values[2] = 310.0;
+			values[0] = 0.0;//-4.046e-5;
+			values[1] = -0.0002;
 		}
-		
 		return values;
 	}
-	  
-	virtual FieldVector<RT,m> J (const FieldVector<DT,dim>& x, const Entity& e, 
-				const IntersectionIterator& intersectionIt, const FieldVector<DT,dim>& xi) const 
-	{
-		FieldVector<RT,m> values(0);
 
-		return values;
-	}
-	
 	// Initial Conditions for global vector x, element e and local vector xi
-	virtual FieldVector<RT,m> initial (const FieldVector<DT,dim>& x, const Entity& e, 
-				  const FieldVector<DT,dim>& xi) const 
+	virtual FieldVector<RT,m> initial (const FieldVector<DT,dim>& x, const Entity& e,
+				  const FieldVector<DT,dim>& xi) const
 	{
 		FieldVector<RT,m> values(0);
-		values[0] = p0_ - x[1] * 1070 * 9.81; 
-		values[1] = 1.e-9;
-		values[2] = t0_ - x[1]*0.03;
-		
+		values[0] = 1.013e5 + (depthBOR_ - x[1]) * 1045 * 9.81;
+		values[1] = 0.00;
+		values[2] = 283.15 + (depthBOR_ - x[1])*0.03;
+
+
 		return values;
 	}
 
-	int initialPhaseState (const FieldVector<DT,dim>& x, const Entity& e, 
-				  const FieldVector<DT,dim>& xi) const 
+	int initialPhaseState (const FieldVector<DT,dim>& x, const Entity& e,
+				  const FieldVector<DT,dim>& xi) const
 	{
-
 		enum {gasPhase = 0, waterPhase = 1, bothPhases = 2}; // Phase states
 		int state;
 
-		if (x[0] > 10 - 1.e-2 && x[1] >= 4. && x[1] <= 6.)
-				state = bothPhases;
-		else	state = waterPhase;
-			
-			
+		state = waterPhase;
+
 		return state;
 	}
 
-	double porosity (const FieldVector<DT,dim>& x, const Entity& e, 
-			  const FieldVector<DT,dim>& xi) const 
-	{
-		return 0.2;
-	}
-	
-	virtual FieldVector<RT,4> soilParameters (const FieldVector<DT,dim>& x, const Entity& e, 
-			  const FieldVector<DT,dim>& xi) const
-	{
-		FieldVector<RT,4> values(0);
-		enum{soilDensity = 0,soilHeatCap = 1,soilLambdaDry = 2, soilLambdaSw = 3};
-		
-		values[soilDensity] = 2650.0;
-		values[soilHeatCap] = 800.0;
-		values[soilLambdaDry] = 0.32;
-		values[soilLambdaSw] = 2.7;
-		
-		return values;
-	}
-	
-	virtual FieldVector<RT,dim> gravity () const 
+	FieldVector<RT,dim> gravity () const
 	{
 		FieldVector<RT,dim> values(0);
-		
+
 		values[1] = -9.81;
-		
+
 		return values;
 	}
-	  
-	virtual FieldVector<RT,4> materialLawParameters (const FieldVector<DT,dim>& x, const Entity& e, 
-			  const FieldVector<DT,dim>& xi) const 
+
+
+	BrineCO2Problem(Liquid_GL& liq, Gas_GL& gas, Matrix2p<G, RT>& soil,
+			TwoPhaseRelations<G, RT>& law = *(new TwoPhaseRelations<G, RT>),
+			MultiComp& multicomp = *(new CWaterAir), RT depthBOR = 0.0)
+	: TwoPTwoCNIProblem<G, RT>(liq, gas, soil, multicomp, law)
 	{
-		FieldVector<RT,4> values(0);
-		values[swrIdx] = swr_;
-		values[snrIdx] = snr_;
-		values[lamIdx] = lambda_;
-		values[pbIdx] = pb_;
-		
-		return values;
+		depthBOR_ = depthBOR;
 	}
 
-	BrineCO2Problem(TwoPhaseRelations& law = *(new BrooksCoreyLaw), 
-			MultiComp& multicomp = *(new CWaterAir), RT pdown = 7.449015e6, RT tdown = 304.,
-			RT swr = 0.2, RT snr = 0.05, RT pb = 10000.0, RT lambda = 2.0 ) 
-	: TwoPTwoCNIProblem<G, RT>(law, multicomp) 
-	{	
-		p0_ = pdown;
-		t0_ = tdown;
-		swr_ = swr;
-		snr_ = snr;
-		pb_ = pb;
-		lambda_ = lambda;
-		permloc_ = 0; 
-		
-
-		
-		for (int i = 0; i < dim; i++)
-			permloc_[i][i] = 1.0e-14;
-		
-		diffusion_[h2o] = 2.0E-5; // diffusion coefficient for water in gas phase
-		diffusion_[co2] = 2.6E-9; // diffusion coefficient for co2 in water phase
-
-	}
-	
 	private:
-		Dune::FieldMatrix<DT,dim,dim> permloc_;
-		Dune::FieldVector<RT,2> diffusion_;
-		RT p0_, t0_;
-		RT swr_, snr_;
-		RT pb_, lambda_;
+		RT depthBOR_;
 		RT soilDens_, soilHeatCp_, soilLDry_, soilLSw_;
-		
   };
 
 }
