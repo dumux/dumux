ccaaa3e7eb9ee8d87ea83b99f67625140c6e4ea0
Melanie Darcis <mdarcis@gmx.de>
2008-09-18 16:53:00 +0000

    adjusted to new material properties
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@607 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/2p2cni/test_co2.cc
+++ test/2p2cni/test_co2.cc
@@ -1,45 +1,50 @@
 #include "config.h"
 #include <iostream>
-//#define DUMMY 
-#undef DUMMY 
-#ifdef DUMMY 
+#define DUMMY
+//#undef DUMMY
+#ifdef DUMMY
 //#ifdef HAVE_UG
 #include <iomanip>
 //#include <dune/grid/utility/gridtype.hh>
 #include <dune/grid/common/gridinfo.hh>
 #include <dune/grid/io/file/dgfparser/dgfs.hh>
 #include <dune/grid/io/file/dgfparser/dgfug.hh>
-//#include <dune/grid/io/file/dgfparser/dgfalu.hh>
+#include <dune/grid/io/file/dgfparser/dgfalu.hh>
 //#include <dune/grid/io/file/dgfparser/dgfyasp.hh>
 //#include <dune/grid/io/file/dgfparser/dgfs.hh>
 #include <dune/grid/io/file/vtk/vtkwriter.hh>
 #include <dune/istl/io.hh>
 #include <dune/common/timer.hh>
-//#include "dumux/twophase/problems/uniformtwophaseproblem.hh"
 #include "brineco2problem.hh"
-//#include "dumux/material/properties.hh"
+#include "benchmarkproblem.hh"
+#include "co2_soilproperties.hh"
+
+#include "dumux/material/phaseproperties/phaseproperties_brineco2.hh"
+#include "dumux/material/matrixproperties.hh"
+#include "dumux/material/twophaserelations.hh"
+
 #include "dumux/2p2cni/fv/boxco2.hh"
 #include "dumux/timedisc/timeloop.hh"
 #include "dumux/io/readstarformat.cc"
+#include "dumux/io/vtkmultiwriter.hh"
 
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
   try{
-    // define the problem dimensions  
+    // define the problem dimensions
     const int dim=2;
-    typedef double NumberType; 
+    typedef double NumberType;
+    double depthBOR = 800.0;  // bottom of reservoir
     Dune::FieldVector<NumberType, dim> outerLowerLeft(0);
-    Dune::FieldVector<NumberType, dim> outerUpperRight(4);
-    Dune::FieldVector<NumberType, dim> innerLowerLeft(1);
-    innerLowerLeft[1] = 2;
-    Dune::FieldVector<NumberType, dim> innerUpperRight(4);
-    innerUpperRight[1] = 3;
+    Dune::FieldVector<NumberType, dim> outerUpperRight(60);
+    outerUpperRight[1] = 40;
+
     if (argc != 4) {
       std::cout << "usage: 2p2cni basefilename tEnd dt" << std::endl;
       return 0;
     }
-    	std::string arg1(argv[2]);
+    std::string arg1(argv[2]);
 	std::istringstream is1(arg1);
 	double tEnd;
 	is1 >> tEnd;
@@ -49,42 +54,49 @@ int main(int argc, char** argv)
 	is2 >> dt;
 
     // create a grid object
-    typedef Dune::SGrid<dim,dim> GridType; 
-    //typedef Dune::YaspGrid<dim,dim> GridType; 
-    //typedef Dune::UGGrid<dim> GridType; 
-	//typedef Dune::ALUSimplexGrid<dim,dim> GridType; 
-	
+    //typedef Dune::SGrid<dim,dim> GridType;
+    //typedef Dune::YaspGrid<dim,dim> GridType;
+    //typedef Dune::UGGrid<dim> GridType;
+	typedef Dune::ALUSimplexGrid<dim,dim> GridType;
+
     Dune::GridPtr<GridType> gridPointer(argv[1]);
     GridType& grid = *gridPointer;
     //readStarFormat(grid, argv[1]);
     //grid.createLGMGrid(argv[1]);
 
      Dune::gridinfo(grid);
-    
-    Brine wPhase(0.2);
-    CO2 nPhase(0.05);
 
-//     Dune::LinearLaw law(brine, co2);
-//     Dune::CO2Problem2D<GridType, NumberType> problem(law, 1.e7); 
-      Dune::BrooksCoreyLaw law(wPhase, nPhase);
-      Dune::CBrineCO2 multicomp(wPhase, nPhase);
+     // choose fluids and properties
+     Dune::Liq_BrineCO2 wPhase;
+     Dune::Gas_BrineCO2 nPhase;
+     Dune::CO2Soil<GridType, NumberType> soil;
 
-      Dune::BrineCO2Problem<GridType, NumberType> problem(law, multicomp, 9.548355e6, 310, 0.2, 0.05, 10000, 2); 
+     Dune::TwoPhaseRelations<GridType, NumberType>
+     materialLaw(soil, wPhase, nPhase);
 
-      typedef Dune::VtkMultiWriter<GridType> MultiWriter;
-    typedef Dune::BoxCO2<GridType, NumberType, MultiWriter> TwoPhase;
-    TwoPhase twoPhase(grid, problem);
+     Dune::CBrineCO2 multicomp(wPhase, nPhase);
 
+Dune::BrineCO2Problem<GridType, NumberType> problem(wPhase, nPhase, soil,
+                 materialLaw, multicomp, depthBOR);
 
-    Dune::TimeLoop<GridType, TwoPhase> timeloop(0, tEnd, dt, "co2", 5);
-    Dune::Timer timer;
-    timer.reset();
-    Dune::VtkMultiWriter<GridType> writer("outco2");
+ //    Dune::BrineCO2Problem<GridType, NumberType> problem(wPhase, nPhase, soil,
+//    		 materialLaw, multicomp, depthBOR);
 
-    timeloop.executeMultiWriter(twoPhase, writer);
-    std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
-     
-	//std::cout << twoPhase.injected() << " kg CO2 injected." << std::endl;
+     typedef Dune::VtkMultiWriter<GridType> MultiWriter;
+     typedef Dune::BoxCO2<GridType, NumberType, MultiWriter> TwoPhase;
+     TwoPhase twoPhase(grid, problem);
+
+     Dune::TimeLoop<GridType, TwoPhase> timeloop(0, tEnd, dt, "co2", 1);
+
+     Dune::Timer timer;
+     timer.reset();
+     Dune::VtkMultiWriter<GridType> writer("co2");
+
+	 //timeloop.execute(twoPhase);
+     timeloop.executeMultiWriter(twoPhase, writer);
+     std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
+
+      //std::cout << twoPhase.injected() << " kg CO2 injected." << std::endl;
 
     return 0;
   }
@@ -95,7 +107,7 @@ int main(int argc, char** argv)
     std::cerr << "Unknown exception thrown!" << std::endl;
   }
 }
-#else 
+#else
 
 int main (int argc , char **argv) try
 {
@@ -104,9 +116,9 @@ int main (int argc , char **argv) try
 
   return 1;
 }
-catch (...) 
+catch (...)
 {
     std::cerr << "Generic exception!" << std::endl;
     return 2;
 }
-#endif 
+#endif
