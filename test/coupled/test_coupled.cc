f8ddfda67f256d348e1709b392cdfe74ba386452
Iryna Rybak <iryna.rybak@mathematik.uni-stuttgart.de>
2008-09-22 08:02:53 +0000

    coupledmodel updated
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@615 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/coupled/test_coupled.cc
+++ test/coupled/test_coupled.cc
@@ -1,10 +1,7 @@
 #include "config.h"
 #include <iostream>
+#include <dune/grid/yaspgrid.hh>
 #include <dune/grid/common/gridinfo.hh>
-#include <dune/grid/io/file/dgfparser/dgfparser.hh>
-#include <dune/grid/io/file/dgfparser/dgfug.hh>
-#include <dune/grid/io/file/dgfparser/dgfalu.hh>
-#include <dune/grid/io/file/dgfparser/dgfs.hh>
 #include <dune/grid/io/file/vtk/vtkwriter.hh>
 #include <dune/istl/preconditioners.hh>
 #include <dune/istl/solvers.hh>
@@ -28,8 +25,8 @@ struct P1Layout
 	{
 		return gt.dim() == 0;
 	}
-}; 
-template<class Grid, class Solution, class Problem> 
+};
+template<class Grid, class Solution, class Problem>
 double discreteError(const Grid& grid, const Solution& solution, const Problem& problem)
 {
 	  enum{dim=Grid::dimension};
@@ -37,11 +34,11 @@ double discreteError(const Grid& grid, const Solution& solution, const Problem& 
 	    typedef typename GV::IndexSet IS;
 	  typedef MultipleCodimMultipleGeomTypeMapper<Grid,IS,P1Layout> VM;
 		typedef typename GV::template Codim<dim>::Iterator VertexIterator;
-	  
+
 	  VM vertexMapper(grid, grid.leafIndexSet());
 	  double error = 0.0;
 	  const GV& gridview(grid.leafView());
-	  
+
 	  VertexIterator endIt = gridview.template end<dim>();
 	  VertexIterator it = gridview.template begin<dim>();
 	  for (; it != endIt; ++it)
@@ -53,66 +50,55 @@ double discreteError(const Grid& grid, const Solution& solution, const Problem& 
 		  // get approximate solution at vertex
 		  int globalId = vertexMapper.map(*it);
 		  double approximate = (*solution)[globalId];
-		  
+
 		  error += (exact - approximate)*(exact - approximate);
 	  }
-		  
+
 	  return sqrt(error);
 }
 }
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
   try{
-    // define the problem dimensions  
+    // define the problem dimensions
     const int dim=2;
-    typedef double NumberType; 
-    if (argc != 2 && argc != 3) {
-      std::cout << "usage: test_coupled dgffilename/basefilename [refinementsteps]" << std::endl;
-      return 1;
-    }
+    typedef double NumberType;
     int refinementSteps = 0;
-    if (argc == 3) {
-    	std::string arg2(argv[2]);
-    	std::istringstream is2(arg2);
-    	is2 >> refinementSteps;
-    }
-    
-    typedef Dune::SGrid<dim,dim> GridType; 
-    Dune::GridPtr<GridType> gridPtr( argv[1] );
-    GridType& grid = *gridPtr;
-    
-    if (refinementSteps)
-    	grid.globalRefine(refinementSteps);
 
-    Dune::gridinfo(grid);
+        Dune::FieldVector<double,dim> length(1.0);
+        Dune::FieldVector<int,dim> size(1);
+        Dune::FieldVector<bool,dim> periodic(false);
+        int overlap = 0;
+        typedef Dune::YaspGrid<dim,dim> GridType;
+        GridType grid(length, size,periodic,overlap);
 
-//    Dune::SubGrid subGrid(grid);
-    
+    //    Dune::SubGrid subGrid(grid);
+
     DiffusionParameters<GridType,NumberType> problem;
-    
+
     typedef Dune::LeafP1BoxDiffusion<GridType, NumberType> Diffusion;
     Diffusion diffusion(grid, problem);
-    
-    const int vOrder = 2; 
-    const int pOrder = 1; 
-    DGStokesParameters parameters; 
+
+    const int vOrder = 2;
+    const int pOrder = 1;
+    DGStokesParameters parameters;
     Example<dim, NumberType> exactSolution;
     DirichletBoundary<GridType> dirichletBoundary(exactSolution);
     RightHandSide<GridType> rightHandSide(exactSolution);
     typedef Dune::DGStokes<GridType, vOrder, pOrder> DGStokes;
-	DGStokes dGStokes(grid, exactSolution, parameters, dirichletBoundary, rightHandSide, refinementSteps); 
+	DGStokes dGStokes(grid, exactSolution, parameters, dirichletBoundary, rightHandSide, refinementSteps);
 
     typedef Dune::CoupledModel<Diffusion,DGStokes> CoupledModel;
     CoupledModel coupledModel(grid, diffusion, grid, dGStokes, true);
-    
+
     coupledModel.initial();
     coupledModel.assemble();
     printmatrix(std::cout, *(diffusion.A), "local stiffness matrix", "row", 11, 4);
     printmatrix(std::cout, coupledModel.matrix(), "global stiffness matrix", "row", 11, 4);
-    
+
     Dune::TimeLoop<GridType, Diffusion> timeloop(0, 1, 1, "box3d", 1);
-    
+
     timeloop.execute(diffusion);
 
     printmatrix(std::cout, *(diffusion.A), "local stiffness matrix", "row", 11, 4);
