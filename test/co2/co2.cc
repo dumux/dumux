34f5ec967acc535d00eb9dca71207e40295a4961
Melanie Darcis <mdarcis@gmx.de>
2008-10-10 14:42:35 +0000

    adjusted to new material properties
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@685 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/co2/co2.cc
+++ test/co2/co2.cc
@@ -1,84 +1,80 @@
 #include "config.h"
 #include <iostream>
-#ifdef HAVE_UG
+//#ifdef HAVE_UG
 #include <iomanip>
-//#include <dune/grid/utility/gridtype.hh>
+#include <dune/grid/utility/gridtype.hh>
 #include <dune/grid/common/gridinfo.hh>
 #include <dune/grid/io/file/dgfparser/dgfparser.hh>
 #include <dune/grid/io/file/dgfparser/dgfug.hh>
+#include <dune/grid/io/file/dgfparser/dgfs.hh>
 #include <dune/grid/io/file/dgfparser/dgfalu.hh>
 #include <dune/grid/io/file/dgfparser/dgfalberta.hh>
 #include <dune/grid/io/file/dgfparser/dgfyasp.hh>
 #include <dune/grid/io/file/vtk/vtkwriter.hh>
 #include <dune/istl/io.hh>
 #include <dune/common/timer.hh>
-#include "dumux/twophase/problems/lensproblem.hh"
-//#include "dumux/twophase/problems/uniformtwophaseproblem.hh"
-#include "dumux/twophase/fv/boxpwsn_deprecated.hh"
+#include "co2problem.hh"
+#include "dumux/twophase/fv/boxpwsn.hh"
 #include "dumux/timedisc/timeloop.hh"
-#include "dumux/io/readstarformat.cc"
-#include "co2problem11.hh"
+#include "dumux/material/phaseproperties/phaseproperties2p.hh"
+#include "dumux/material/twophaserelations.hh"
+#include "co2_soilproperties.hh"
 
 int main(int argc, char** argv)
 {
-    Dune::MPIHelper::instance(argc, argv);
+  try{
+	    // define the problem dimensions
+	    const int dim=2;
+	    typedef double NumberType;
+	    double depthBOR = 800.0;  // bottom of reservoir
+	    Dune::FieldVector<NumberType, dim> outerLowerLeft(0);
+	    Dune::FieldVector<NumberType, dim> outerUpperRight(60);
+	    outerUpperRight[1] = 40;
 
-    try{
-    // define the problem dimensions
-    const int dim=3;
-    typedef double NumberType;
-    double sRBrine = 0.0;
-    double densityBrine = 1045.0;
-    double viscosityBrine = 2.535e-4;
-    double sRCO2 = 0.0;
-    double densityCO2 = 479.0;
-    double viscosityCO2 = 3.95e-5;
+	    if (argc != 4) {
+	      std::cout << "usage: 2p2cni basefilename tEnd dt" << std::endl;
+	      return 0;
+	    }
+	    std::string arg1(argv[2]);
+		std::istringstream is1(arg1);
+		double tEnd;
+		is1 >> tEnd;
+		std::string arg2(argv[3]);
+		std::istringstream is2(arg2);
+		double dt;
+		is2 >> dt;
 
+	    // create a grid object
+	    typedef Dune::SGrid<dim,dim> GridType;
+	    //typedef Dune::YaspGrid<dim,dim> GridType;
+	    //typedef Dune::UGGrid<dim> GridType;
+		//typedef Dune::ALUSimplexGrid<dim,dim> GridType;
 
-    if (argc != 4) {
-      std::cout << "usage: co2 basefilename tEnd dt" << std::endl;
-      return 0;
-    }
-    	std::string arg1(argv[2]);
-	std::istringstream is1(arg1);
-	double tEnd;
-	is1 >> tEnd;
-	std::string arg2(argv[3]);
-	std::istringstream is2(arg2);
-	double dt;
-	is2 >> dt;
+	    Dune::GridPtr<GridType> gridPointer(argv[1]);
+	    GridType& grid = *gridPointer;
+	    //readStarFormat(grid, argv[1]);
+	    //grid.createLGMGrid(argv[1]);
 
+	     Dune::gridinfo(grid);
 
-    // create a grid object
-    //typedef Dune::UGGrid<dim> GridType;
-	typedef Dune::ALUSimplexGrid<dim,dim> GridType;
+    Dune::Brine wPhase(1045., 2.535e-4);
+    Dune::CO2 nPhase(479., 3.95e-5);
+    Dune::Soil<GridType, NumberType> soil;
+    Dune::TwoPhaseRelations<GridType, NumberType> law(soil, wPhase, nPhase);
 
-    Dune::GridPtr<GridType> gridPointer(argv[1]);
-    GridType& grid = *gridPointer;
-    //readStarFormat(grid, argv[1]);
-    //grid.createLGMGrid(argv[1]);
-
-    Dune::gridinfo(grid);
-
-    Brine brine(sRBrine, densityBrine, viscosityBrine);
-    CO2 co2(sRCO2, densityCO2, viscosityCO2);
-    Dune::LinearLaw law(brine, co2);
-    Dune::CO2Problem11<GridType, NumberType> problem(law, 3.086e7);
-
+    Dune::CO2Problem<GridType, NumberType> problem(wPhase, nPhase, soil, law, depthBOR);
 
     typedef Dune::BoxPwSn<GridType, NumberType> TwoPhase;
     TwoPhase twoPhase(grid, problem);
 
-
-
-    Dune::TimeLoop<GridType, TwoPhase> timeloop(0, tEnd, dt, "co2", 10);
+    Dune::TimeLoop<GridType, TwoPhase> timeloop(0, tEnd, dt, "co2-out", 1);
 
     Dune::Timer timer;
     timer.reset();
     timeloop.execute(twoPhase);
     std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
 
-	//std::cout << twoPhase.injected() << " kg CO2 injected." << std::endl;
+    //printvector(std::cout, *twoPhase.u, "u", "row", 2, 1, 3);
 
     return 0;
   }
@@ -89,17 +85,13 @@ int main(int argc, char** argv)
     std::cerr << "Unknown exception thrown!" << std::endl;
   }
 }
-#else
+//#else
+//
+//int main (int argc , char **argv) try
+//{
+//  std::cout << "Please install the UG library." << std::endl;
+//
+//  return 1;
+//}
 
-int main (int argc , char **argv) try
-{
-  std::cout << "Please install the UG library." << std::endl;
-
-  return 1;
-}
-catch (...)
-{
-    std::cerr << "Generic exception!" << std::endl;
-    return 2;
-}
-#endif
+//#endif
