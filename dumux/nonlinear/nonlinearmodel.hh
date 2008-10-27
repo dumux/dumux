f90ef128bdfdeddf3661e5b49d9979f9efbb9d3f
Melanie Darcis <mdarcis@gmx.de>
2008-10-27 14:49:28 +0000

    virtual functions "restart" and "writerestartfile" added
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@738 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/nonlinear/nonlinearmodel.hh
+++ dumux/nonlinear/nonlinearmodel.hh
@@ -1,4 +1,4 @@
-// $Id$ 
+// $Id$
 
 #ifndef DUNE_NONLINEARMODEL_HH
 #define DUNE_NONLINEARMODEL_HH
@@ -9,7 +9,7 @@ namespace Dune
 {
 template<class G, class RT, class ProblemType, class LocalJacobian, class FunctionType, class OperatorAssembler>
 class NonlinearModel {
-public:	
+public:
 	typedef typename FunctionType::RepresentationType VectorType;
 	typedef typename OperatorAssembler::RepresentationType MatrixType;
 	ProblemType& problem;
@@ -30,23 +30,23 @@ public:
 		return u;
 	}
 
-	virtual double getDt() 
+	virtual double getDt()
 	{
 		return localJacobian.getDt();
 	}
-	
-	virtual void setDt(double& dt) 
+
+	virtual void setDt(double& dt)
 	{
 		localJacobian.setDt(dt);
 	}
-	
-	virtual void assemble() 
+
+	virtual void assemble()
 	{
 		*f = 0;
 		localJacobian.clearVisited();
 		A.assemble(localJacobian, u, f);
 	}
-	
+
 	  virtual MatrixType& matrix()
 	  {
 		  return *A;
@@ -61,25 +61,33 @@ public:
 	  {
 		  return *u;
 	  }
+	  virtual void writerestartfile()
+	  {
+		  return;
+	  }
+	  virtual void restart()
+	  {
+		  return;
+	  }
 
 	//! always define virtual destructor in abstract base class
 	virtual ~NonlinearModel () {}
 
 	NonlinearModel(const G& g, ProblemType& prob)
-: problem(prob), u(g, g.overlapSize(0)==0), f(g, g.overlapSize(0)==0), A(g, g.overlapSize(0)==0), 
+: problem(prob), u(g, g.overlapSize(0)==0), f(g, g.overlapSize(0)==0), A(g, g.overlapSize(0)==0),
 localJacobian(prob, false, g, u, g.overlapSize(0)>0)
-//: problem(prob), u(g), f(g), A(g), 
+//: problem(prob), u(g), f(g), A(g),
 //localJacobian(prob, false, g, u)
 	{ }
 
 	NonlinearModel(const G& g, ProblemType& prob, int level)
-	: problem(prob), u(g, level, g.overlapSize(0)==0), f(g, level, g.overlapSize(0)==0), A(g, level, g.overlapSize(0)==0), 
+	: problem(prob), u(g, level, g.overlapSize(0)==0), f(g, level, g.overlapSize(0)==0), A(g, level, g.overlapSize(0)==0),
 	localJacobian(prob, false, g, u, g.overlapSize(0)>0)
 	{ }
 };
 
 template<class G, class RT, class ProblemType, class LocalJacobian, int m=1>
-class LeafP1NonlinearModel 
+class LeafP1NonlinearModel
 : public NonlinearModel<G, RT, ProblemType, LocalJacobian, LeafP1FunctionExtended<G, RT, m>, LeafP1OperatorAssembler<G, RT, m> >
 {
 public:
@@ -89,10 +97,10 @@ public:
 	// define the operator assembler type:
 	typedef LeafP1OperatorAssembler<G, RT, m> OperatorAssembler;
 
-    typedef NonlinearModel<G, RT, ProblemType, LocalJacobian, 
+    typedef NonlinearModel<G, RT, ProblemType, LocalJacobian,
     FunctionType, OperatorAssembler> ThisNonlinearModel;
 
-	LeafP1NonlinearModel (const G& g, ProblemType& prob) 
+	LeafP1NonlinearModel (const G& g, ProblemType& prob)
 	: ThisNonlinearModel(g, prob)
 	{ }
 };
