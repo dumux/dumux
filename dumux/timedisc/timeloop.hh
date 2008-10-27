152c06fa414a8d0b8a3bbdc99f7e87d2e88ba5eb
Melanie Darcis <mdarcis@gmx.de>
2008-10-27 14:45:48 +0000

    changes in timeloop.hh allow to write and read restart files
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@735 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/timedisc/timeloop.hh
+++ dumux/timedisc/timeloop.hh
@@ -11,7 +11,7 @@
 #include "dumux/timedisc/new_impliciteulerstep.hh"
 
 namespace Dune {
-    template<class G, class Model, bool useMultiWriter = false>
+   template<class G, class Model, bool useMultiWriter = false>
     class TimeLoop
     {
     public:
@@ -27,96 +27,93 @@ namespace Dune {
 		  << "compressor=\"vtkZLibDataCompressor\">"  << std::endl
 		  << " <Collection>" << std::endl;
 
-		  // initialize solution with initial values
-		  model.initial();
+		// initialize solution with initial values
+		model.initial();
 
-		  int k = 0;
-		  model.vtkout(fileName, k);
-		  switch (G::dimension) {
-		  case 1:
-			  sprintf(fileNameVTK,"%s-%05d.vtp", fileName, k);
-			  break;
-		  default:
-			  sprintf(fileNameVTK,"%s-%05d.vtu", fileName, k);
-			  break;
-		  }
-		  multiFile << "   <DataSet timestep=\"" << k << "\" file=\""
-		  << fileNameVTK << "\"/>" << std::endl;
+		int k = 0;
+		model.vtkout(fileName, k);
+		switch (G::dimension) {
+		case 1:
+			sprintf(fileNameVTK, "%s-%05d.vtp", fileName, k);
+			break;
+		default:
+			sprintf(fileNameVTK, "%s-%05d.vtu", fileName, k);
+			break;
+		}
+		multiFile << "   <DataSet timestep=\"" << k << "\" file=\""
+				<< fileNameVTK << "\"/>" << std::endl;
 
-		  // now do the time steps
-		  double t = tStart;
-		  //		  double dtOriginal;
-		  //		  if (fixed)
-		  //			  dtOriginal = dt;
-		  while (t < tEnd) {
-			  k++;
-			  double dtOld = dt;
+		// now do the time steps
+		double t = tStart;
+		//		  double dtOriginal;
+		//		  if (fixed)
+		//			  dtOriginal = dt;
+		while (t < tEnd) {
+			k++;
+			double dtOld = dt;
 
-			  if (t == tStart)
-				  timeStep.execute(model, t, dt, firstDt, tEnd, cFLFactor);
-			  else
-				  timeStep.execute(model, t, dt, maxDt, tEnd, cFLFactor);
+			if (t == tStart)
+				timeStep.execute(model, t, dt, firstDt, tEnd, cFLFactor);
+			else
+				timeStep.execute(model, t, dt, maxDt, tEnd, cFLFactor);
 
-			  if(fixed)
-			  {
-				  if (dt > dtOld) {
-					  t += dtOld;
-					  t = std::min(t, tEnd);
-					  if(dt > tEnd-t)
-					  {
-						  std::cout << "\t" << k << "\t" << t << "\t" << dtOld
-						  << "\t # timestep number k, time t, timestep size dt" << std::endl;
-						  //std::cout << ", timestep: " << k << "\t t=" << t << "\t dt=" << (tEnd-t) << std::endl;
-					  }
-					  else
-					  {
-						  std::cout << "\t" << k << "\t" << t << "\t" << dtOld
-						  << "\t # timestep number k, time t, timestep size dt" << std::endl;
-						  //std::cout << ", timestep: " << k << "\t t=" << t << "\t dt=" << dt << std::endl;
-					  }
-				  }
+			if (fixed) {
+				if (dt > dtOld) {
+					t += dtOld;
+					t = std::min(t, tEnd);
+					if (dt > tEnd - t) {
+						std::cout << "\t" << k << "\t" << t << "\t" << dtOld
+								<< "\t # timestep number k, time t, timestep size dt"
+								<< std::endl;
+						//std::cout << ", timestep: " << k << "\t t=" << t << "\t dt=" << (tEnd-t) << std::endl;
+					} else {
+						std::cout << "\t" << k << "\t" << t << "\t" << dtOld
+								<< "\t # timestep number k, time t, timestep size dt"
+								<< std::endl;
+						//std::cout << ", timestep: " << k << "\t t=" << t << "\t dt=" << dt << std::endl;
+					}
+				}
 
-				  else {
-					  t += dt;
-					  t = std::min(t, tEnd);
-					  std::cout << "\t" << k << "\t" << t << "\t" << dt
-					  << "\t # timestep number k, time t, timestep size dt" << std::endl;
-					  //std::cout << ", timestep: " << k << "\t t=" << t << "\t dt=" << dt << std::endl;
-				  }
-			  }
-			  else
-			  {
-				  t += dt;
-				  t = std::min(t, tEnd);
-				  std::cout << ", timestep: " << k << "\t t=" << t << "\t dt=" << dt << std::endl;
+				else {
+					t += dt;
+					t = std::min(t, tEnd);
+					std::cout << "\t" << k << "\t" << t << "\t" << dt
+							<< "\t # timestep number k, time t, timestep size dt"
+							<< std::endl;
+					//std::cout << ", timestep: " << k << "\t t=" << t << "\t dt=" << dt << std::endl;
+				}
+			} else {
+				t += dt;
+				t = std::min(t, tEnd);
+				std::cout << ", timestep: " << k << "\t t=" << t << "\t dt="
+						<< dt << std::endl;
 
-			  }
+			}
 
-			  // generate output
-			  if (k%modulo == 0)
-			  {
-				  model.vtkout(fileName, k/modulo);
-				  switch (G::dimension) {
-				  case 1:
-					  sprintf(fileNameVTK,"%s-%05d.vtp", fileName, k/modulo);
-					  break;
-				  default:
-					  sprintf(fileNameVTK,"%s-%05d.vtu", fileName, k/modulo);
-					  break;
-				  }
-				  multiFile << "   <DataSet timestep=\"" << t << "\" file=\""
-				  << fileNameVTK << "\"/>" << std::endl;
-			  }
-			  //		    if (fixed)
-			  //		    	dt = dtOriginal;
-		  }
-		  // finalize output
-		  multiFile << " </Collection>" << std::endl << "</VTKFile>" << std::endl;
-		  multiFile.close();
+			// generate output
+			if (k % modulo == 0) {
+				model.vtkout(fileName, k / modulo);
+				switch (G::dimension) {
+				case 1:
+					sprintf(fileNameVTK, "%s-%05d.vtp", fileName, k / modulo);
+					break;
+				default:
+					sprintf(fileNameVTK, "%s-%05d.vtu", fileName, k / modulo);
+					break;
+				}
+				multiFile << "   <DataSet timestep=\"" << t << "\" file=\""
+						<< fileNameVTK << "\"/>" << std::endl;
+			}
+			//		    if (fixed)
+			//		    	dt = dtOriginal;
+		}
+		// finalize output
+		multiFile << " </Collection>" << std::endl << "</VTKFile>" << std::endl;
+		multiFile.close();
 
-		  return;
-	  }
-    
+		return;
+	}
+
 	  TimeLoop(const double ts, const double te, const char* name = "timeloop", const int mod = 1,
 			  const double cfl = 1, const double mdt = 1e100, const double fdt = 1e100,
 			  TimeStep<G, Model>& tist = *(new RungeKuttaStep<G, Model>(1)))
@@ -124,13 +121,13 @@ namespace Dune {
 			  modulo(mod), timeStep(tist), fileName(name), fixed(false)
 			  { }
 
-	  TimeLoop(const double ts, const double te, const double dtime = 1e100,
+  TimeLoop(const double ts, const double te, const double dtime = 1e100,
 			  const char* name = "timeloop", const int mod = 1, const double fdt = 1e100,
 			  TimeStep<G, Model>& tist = *(new ImplicitEulerStep<G, Model>))
 			  : tStart(ts), tEnd(te), dt(dtime), maxDt(1e100), firstDt(fdt), cFLFactor(1),
 			  modulo(mod), timeStep(tist), fileName(name), fixed(true)
             { }
-        
+
     private:
         const double tStart;
         const double tEnd;
@@ -154,7 +151,7 @@ namespace Dune {
           // vtkMultiWriter. it is a temorary measure to ease the
           // migration...
 	  template<class MultiWriter>
-	  void executeMultiWriter (Model& model, MultiWriter& writer)
+	  void executeMultiWriter (Model& model, MultiWriter& writer, bool restart=false)
 	  {
                   typedef NewImplicitEulerStep<Model> NewTimeStep;
 
@@ -162,12 +159,19 @@ namespace Dune {
 
 		  writer.beginTimestep(0, model.grid().leafView());
 
-		  // initialize solution with initial values
-		  model.setVtkMultiWriter(&writer);
-		  model.initial();
-		  model.addvtkfields(writer);
-		  std::cout << ">>> writing initial output file" << std::endl;
-		  writer.endTimestep();  // writes output file
+		// initialize solution with initial values
+		model.setVtkMultiWriter(&writer);
+		if (!restart)
+		{
+			model.initial();
+		}
+		else
+		{
+			model.restart();
+		}
+		model.addvtkfields(writer);
+		std::cout << ">>> writing initial output file" << std::endl;
+		writer.endTimestep(); // writes output file
 
 		  // now do the time steps
 		  double t = tStart;
@@ -178,7 +182,7 @@ namespace Dune {
 			  k++;
 //			  double dtOld = dt;
 
-                          double nextDt;
+                         double nextDt;
 			  if (t == tStart)
                               NewTimeStep::execute(model, t, dt, nextDt, firstDt, tEnd, cFLFactor);
 			  else
@@ -189,7 +193,7 @@ namespace Dune {
                           dt = nextDt;
                           std::cout << ", timestep: " << k << "\t t=" << t << "\t dt=" << dt << std::endl;
 
-			  // generate output
+		  // generate output
 			  if (k%modulo == 0)
 			  {
 				  writer.beginTimestep(t, model.grid().leafView());
@@ -197,38 +201,42 @@ namespace Dune {
 				  std::cout << ">>> writing output-file at time : " << t << std::endl;
 				  writer.endTimestep();
 			  }
+			  if (k%(modulo*2) == 0)
+			  {
+				  model.writerestartfile();
+			  }
 		  }
 
-		  return;
-	  }
+		return;
+	}
 
+	TimeLoop(const double ts, const double te, const char* name = "timeloop",
+			const int mod = 1, const double cfl = 1, const double mdt = 1e100,
+			const double fdt = 1e100, TimeStep<G, Model>& tist =
+					*(new RungeKuttaStep<G, Model> (1))) :
+		tStart(ts), tEnd(te), maxDt(mdt), firstDt(fdt), cFLFactor(cfl), modulo(
+				mod), timeStep(tist), fileName(name), fixed(false) {
+	}
 
+	TimeLoop(const double ts, const double te, const double dtime = 1e100,
+			const char* name = "timeloop", const int mod = 1, const double fdt =
+					1e100, TimeStep<G, Model>& tist = *(new ImplicitEulerStep<
+					G, Model> )) :
+		tStart(ts), tEnd(te), dt(dtime), maxDt(1e100), firstDt(fdt), cFLFactor(
+				1), modulo(mod), timeStep(tist), fileName(name), fixed(true) {
+	}
 
-	  TimeLoop(const double ts, const double te, const char* name = "timeloop", const int mod = 1,
-			  const double cfl = 1, const double mdt = 1e100, const double fdt = 1e100,
-			  TimeStep<G, Model>& tist = *(new RungeKuttaStep<G, Model>(1)))
-			  : tStart(ts), tEnd(te), maxDt(mdt), firstDt(fdt), cFLFactor(cfl),
-			  modulo(mod), timeStep(tist), fileName(name), fixed(false)
-			  { }
-
-	  TimeLoop(const double ts, const double te, const double dtime = 1e100,
-			  const char* name = "timeloop", const int mod = 1, const double fdt = 1e100,
-			  TimeStep<G, Model>& tist = *(new ImplicitEulerStep<G, Model>))
-			  : tStart(ts), tEnd(te), dt(dtime), maxDt(1e100), firstDt(fdt), cFLFactor(1),
-			  modulo(mod), timeStep(tist), fileName(name), fixed(true)
-			  { }
-
-  private:
-	  const double tStart;
-	  const double tEnd;
-	  double dt;
-	  const double maxDt;
-	  const double firstDt;
-	  const double cFLFactor;
-	  const int modulo;
-	  TimeStep<G, Model>& timeStep;
-	  const char* fileName;
-	  const bool fixed;
-  };
+private:
+	const double tStart;
+	const double tEnd;
+	double dt;
+	const double maxDt;
+	const double firstDt;
+	const double cFLFactor;
+	const int modulo;
+	TimeStep<G, Model>& timeStep;
+	const char* fileName;
+	const bool fixed;
+};
 }
 #endif
