97e38c7014a9fcfc08830d49bec969459c6701a5
Melanie Darcis <mdarcis@gmx.de>
2008-12-04 10:20:35 +0000

    2pni model adjusted to new newton method, material law and multiwriter
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@885 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ test/2pni/energysoil.hh
@@ -0,0 +1,102 @@
+//$Id: energysoil.hh 882 2008-12-04 09:05:55Z melanie $
+#ifndef DUNE_TWOPHEATSOIL_HH
+#define DUNE_TWOPHEATSOIL_HH
+
+/**
+ * @file
+ * @brief  Class for defining an instance of a Matrix2p soil
+ * @author Bernd Flemisch, Klaus Mosthaf
+ */
+
+namespace Dune
+{
+
+  template<class G, class RT>
+  class TwoPHeatSoil: public Matrix2p<G,RT>
+  {
+  public:
+  	typedef typename G::Traits::template Codim<0>::Entity Entity;
+  	typedef typename G::ctype DT;
+  	enum {dim=G::dimension, m=1};
+
+  	// define PERMEABILITY tensor
+  	virtual FieldMatrix<DT,dim,dim> K (const FieldVector<DT,dim>& x, const Entity& e, const FieldVector<DT,dim>& xi)
+  	{
+			return K_;
+  	}
+  	virtual double porosity(const FieldVector<DT,dim>& x, const Entity& e, const FieldVector<DT,dim>& xi) const
+  	{
+  		return 0.15;
+  	}
+
+  	virtual double Sr_w(const FieldVector<DT,dim>& x, const Entity& e, const FieldVector<DT,dim>& xi, const double T) const
+  	{
+  			return 0.2;
+  	}
+
+  	virtual double Sr_n(const FieldVector<DT,dim>& x, const Entity& e, const FieldVector<DT,dim>& xi, const double T) const
+  	{
+  		return 0.05;
+  	}
+
+  	virtual typename Matrix2p<G,RT>::modelFlag relPermFlag(const FieldVector<DT,dim>& x, const Entity& e, const FieldVector<DT,dim>& xi) const
+  	{
+  		return Matrix2p<G,RT>::brooks_corey;
+  	}
+
+	virtual double heatCap(const FieldVector<DT,dim>& x, const Entity& e, const FieldVector<DT,dim>& xi) const
+	{
+		return 	(800 /* spec. heat cap. of sediment */
+						* 2650 /* density of sediment */
+						* (1-porosity(x, e, xi)));
+	}
+
+	virtual double heatCond(const FieldVector<DT,dim>& x, const Entity& e, const FieldVector<DT,dim>& xi, const double sat) const
+	{
+		static const double ldry = 0.32;
+		static const double lsat = 2.7;
+		double l_wurz, Sw;
+		Sw = sat;
+		if(Sw<0.0) Sw = 0.0; /* ACHTUNG Regularisierung */
+		if(Sw>1.) Sw = 1.; /* ACHTUNG Regularisierung */
+
+		l_wurz = ldry + sqrt(Sw)*(lsat-ldry);
+
+		if(isnan(l_wurz)) {
+			std::cout <<"isnan heatcondwurzel \n"<<std::endl;
+			l_wurz = 0.0;
+		}
+		return(l_wurz);
+	}
+
+  	virtual std::vector<double> paramRelPerm(const FieldVector<DT,dim>& x, const Entity& e, const FieldVector<DT,dim>& xi, const double T) const
+  	{
+  		// example for Brooks-Corey parameters
+		std::vector<double> param(5);
+
+			param[0] = 2.;
+			param[1] = 10000;
+
+  		return param;
+  	}
+
+  	TwoPHeatSoil()
+  	: Matrix2p<G,RT>()
+  	{
+  		K_ = 0;
+  		for(int i = 0; i < dim; i++)
+  		{
+  			K_[i][i] = 2e-14;
+  		}
+  	}
+
+  	~TwoPHeatSoil()
+  	{}
+
+  private:
+  	FieldMatrix<DT,dim,dim> K_;
+   };
+
+} // end namespace
+#endif
+
---
+++ test/2pni/test_2pni.cc
@@ -0,0 +1,101 @@
+//$Id: test_2pni.cc 882 2008-12-04 09:05:55Z melanie $
+#include "config.h"
+#include <iostream>
+//#ifdef HAVE_UG
+#include <iomanip>
+#include <dune/grid/utility/gridtype.hh>
+#include <dune/grid/common/gridinfo.hh>
+#include <dune/grid/io/file/dgfparser/dgfparser.hh>
+#include <dune/grid/io/file/dgfparser/dgfug.hh>
+#include <dune/grid/io/file/dgfparser/dgfs.hh>
+#include <dune/grid/io/file/dgfparser/dgfalu.hh>
+#include <dune/grid/io/file/dgfparser/dgfalberta.hh>
+#include <dune/grid/io/file/dgfparser/dgfyasp.hh>
+#include <dune/grid/io/file/vtk/vtkwriter.hh>
+#include <dune/istl/io.hh>
+#include <dune/common/timer.hh>
+#include "energyproblem.hh"
+#include "energysoil.hh"
+#include "dumux/2pni/fv/boxpwsnte.hh"
+#include "dumux/timedisc/timeloop.hh"
+#include "dumux/material/phaseproperties/phaseproperties2p.hh"
+#include "dumux/material/twophaserelations.hh"
+#include "dumux/io/vtkmultiwriter.hh"
+
+int main(int argc, char** argv)
+{
+  try{
+    // define the problem dimensions
+    const int dim=2;
+    typedef double NumberType;
+
+    // read tEnd and initial time step from console
+    if (argc != 3) {
+      std::cout << "usage: test_twophase tEnd dt" << std::endl;
+      return 0;
+    }
+    std::string arg1(argv[1]);
+	std::istringstream is1(arg1);
+	double tEnd;
+	is1 >> tEnd;
+	std::string arg2(argv[2]);
+	std::istringstream is2(arg2);
+	double dt;
+	is2 >> dt;
+
+    // create a grid object
+    typedef double NumberType;
+    typedef Dune::SGrid<dim,dim> GridType;
+    typedef Dune::FieldVector<GridType::ctype,dim> FieldVector;
+    Dune::FieldVector<int,dim> N(10); N[1] = 10; // number of cells
+    FieldVector L(0); //
+    FieldVector H(10);
+    GridType grid(N,L,H);
+
+    // print some information about the grid
+    Dune::gridinfo(grid);
+
+    // choose fluids
+    Dune::Brine wPhase;
+    Dune::CO2 nPhase;
+    // create soil object
+    Dune::TwoPHeatSoil<GridType, NumberType> soil;
+    // create material law object
+    Dune::TwoPhaseRelations<GridType, NumberType> law(soil, wPhase, nPhase);
+
+    // create Prolem object
+    Dune::TwoPHeatProblem<GridType, NumberType> problem(wPhase, nPhase, soil, law);
+
+    typedef Dune::VtkMultiWriter<GridType::LeafGridView> MultiWriter;
+    typedef Dune::BoxPwSnTe<GridType, NumberType, MultiWriter> TwoPhase;
+    TwoPhase twoPhase(grid, problem);
+
+    Dune::TimeLoop<GridType, TwoPhase, true> timeloop(0, tEnd, dt, "dummy", 1);
+
+    Dune::Timer timer;
+    timer.reset();
+    MultiWriter writer("out-twophase");
+    timeloop.executeMultiWriter(twoPhase, writer);
+    std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
+
+    //printvector(std::cout, *twoPhase.u, "u", "row", 2, 1, 3);
+
+    return 0;
+  }
+  catch (Dune::Exception &e){
+    std::cerr << "Dune reported error: " << e << std::endl;
+  }
+  catch (...){
+    std::cerr << "Unknown exception thrown!" << std::endl;
+  }
+}
+//#else
+//
+//int main (int argc , char **argv) try
+//{
+//  std::cout << "Please install the UG library." << std::endl;
+//
+//  return 1;
+//}
+
+//#endif
