1372d26799828fc7dd74aca63ad150d9d160e5bf
Klaus Mosthaf <klmos@env.dtu.dk>
2008-12-17 10:45:46 +0000

    constrelwater: replace T and Temp by temperature
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@930 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/material/constrel/constrelwater.hh
+++ dumux/material/constrel/constrelwater.hh
@@ -1,4 +1,4 @@
-// $Id$ 
+// $Id$
 
 #ifndef DUNE_CONSTRELWATER_HH
 #define DUNE_CONSTRELWATER_HH
@@ -6,19 +6,19 @@
 #include "constrelco2.hh"
 
 /************************************************************************/
-/*									*/
-/*	Calculation of the mass density of pure water                   */
-/*									*/
-/*	after IAPWS 1997 (only region 1)				*/
-/*									*/
-/*	validity: temperature: 273.15 K <= T <= 1073.15 K		*/
-/*		  pressure : p <= 100 MPa (1000 bar, 1.0E8Pa)		*/
-/*									*/
-/*	(IAPWS: The international Association for the properties	*/
-/*		of water and steam)					*/ 
-/*									*/
-/*	(http://www.iapws.org)						*/
-/*									*/
+/*																		*/
+/*	Calculation of the mass density of pure water                   	*/
+/*																		*/
+/*	after IAPWS 1997 (only region 1)									*/
+/*																		*/
+/*	validity: temperature: 273.15 K <= temperature <= 1073.15 K			*/
+/*		  pressure : p <= 100 MPa (1000 bar, 1.0E8Pa)					*/
+/*																		*/
+/*	(IAPWS: The international Association for the properties			*/
+/*		of water and steam)												*/
+/*																		*/
+/*	(http://www.iapws.org)												*/
+/*																		*/
 /************************************************************************/
 
 static const double I[35] = {
@@ -33,7 +33,7 @@ static const double I[35] = {
     2.0, 2.0,
     2.0, 2.0,
     3.0, 3.0,
-    3.0, 4.0, 
+    3.0, 4.0,
     4.0, 4.0,
     5.0, 8.0,
     8.0, 21.0,
@@ -47,7 +47,7 @@ static const double J[35] = {
     1.0,  2.0,
     3.0,  4.0,
     5.0, -9.0,
-    -7.0, -1.0, 
+    -7.0, -1.0,
     0.0,  1.0,
     3.0, -3.0,
     0.0,  1.0,
@@ -55,7 +55,7 @@ static const double J[35] = {
     -4.0,  0.0,
     6.0, -5.0,
     -2.0, 10.0,
-    -8.0, -11.0, 
+    -8.0, -11.0,
     -6.0, -29.0,
     -31.0, -38.0,
     -39.0, -40.0,
@@ -63,7 +63,7 @@ static const double J[35] = {
 };
 static const double n[35] = {
     0.0,                  0.14632971213167,
-    -0.84548187169114,    -0.37563603672040E1, 
+    -0.84548187169114,    -0.37563603672040E1,
     0.33855169168385E1,  -0.95791963387872,
     0.15772038513228,    -0.16616417199507E-1,
     0.81214629983568E-3,  0.28319080123804E-3,
@@ -86,9 +86,9 @@ static const double n[35] = {
 
 class ConstrelWater
 {
-public: 
-	
-	double mass_density_water_IAPWS (double Temp, double pw) const
+public:
+
+	double mass_density_water_IAPWS (double temperature, double pw) const
         {
             double gam_pi;
             double pi, p_star;
@@ -96,34 +96,34 @@ public:
             double v;	/* specific volume */
             double rho;	/* mass density */
 
-        
+
             /* Regularisierung */
-            if(Temp < 273.15 || Temp > 1073.15) {
+            if(temperature < 273.15 || temperature > 1073.15) {
                     DUNE_THROW(Dune::RangeError,
-                               "ConstrelWater: Temperature " << Temp << " out of range at " << __FILE__ << ":" << __LINE__);
+                               "ConstrelWater: Temperature " << temperature << " out of range at " << __FILE__ << ":" << __LINE__);
                 }
                 else if (pw < 1.0 || pw > 1.0E8) {
                     DUNE_THROW(Dune::RangeError,
                                "ConstrelWater: pressure " << pw << " out of range at " << __FILE__ << ":" << __LINE__);
                 }
                 /*
-                  if (Temp < 273.15) Temp = 273.15;
-                  if (Temp > 1073.15) Temp = 1073.15;
+                  if (temperature < 273.15) temperature = 273.15;
+                  if (temperature > 1073.15) temperature = 1073.15;
                   if (pw < 1.0) pw = 1.0;
                   if (pw > 1.0E8) pw = 1.0E8;
                 */
 
                 R = 461.526;	   /* [J/kg K] specific gas constant for ordinary water */
-                p_star = 16.53E6;	/* [Pa] */ 
+                p_star = 16.53E6;	/* [Pa] */
                 pi = pw / p_star;
-                gam_pi = gamma_pi(pi, Temp);
-                v = pi * gam_pi * R * Temp / pw;	
+                gam_pi = gamma_pi(pi, temperature);
+                v = pi * gam_pi * R * temperature / pw;
                 rho = 1 / v;	/* kg / m^3 */
-		
+
                 return(rho);  /* unit: [kg/m^3] */
         }
-    
-    double gamma_pi(double pi, double Temp) const
+
+    double gamma_pi(double pi, double temperature) const
         {
             int i;
             double T_star;
@@ -132,7 +132,7 @@ public:
 
             T_star = 1386.0;        /* [K] */
 
-            tau = T_star / Temp;    /* reduced temperature */
+            tau = T_star / temperature;    /* reduced temperature */
 
             gam_pi = 0.0;
 
@@ -151,42 +151,42 @@ public:
     /*									*/
     /*	after IAPWS 1997 (only region 1)				*/
     /*									*/
-    /*	validity: temperature: 273.15 K <= T <= 1073.15 K		*/
+    /*	validity: temperature: 273.15 K <= temperature <= 1073.15 K		*/
     /*		  pressure : p <= 100 MPa (1000 bar, 1.0E8Pa)		*/
     /*									*/
     /*	(IAPWS: The international Association for the properties	*/
-    /*		of water and steam)					*/ 
+    /*		of water and steam)					*/
     /*									*/
     /*	(http://www.iapws.org)						*/
     /*									*/
     /************************************************************************/
 
-    double enthalpy_water (double Temp, double pw) const
+    double enthalpy_water (double temperature, double pw) const
         {
             double tau;
-            const double T_star = 1386;	/* [K] */ 
+            const double T_star = 1386;	/* [K] */
             const double R = 461.526;/* [J/kg K] specific gas constant for ordinary water */
-		
+
             /* Regularisierung */
-            if(Temp < 273.15 || Temp > 623.15) {
+            if(temperature < 273.15 || temperature > 623.15) {
                     DUNE_THROW(Dune::RangeError,
-                               "ConstrelWater: Temperature " << Temp << " out of range at " << __FILE__ << ":" << __LINE__);
+                               "ConstrelWater: Temperature " << temperature << " out of range at " << __FILE__ << ":" << __LINE__);
                 }
                 else if (pw < 1.0 || pw > 1.0E8) {
                     DUNE_THROW(Dune::RangeError,
                                "ConstrelWater: pressure " << pw << " out of range at " << __FILE__ << ":" << __LINE__);
                 }
-/*        if (Temp < 273.15) Temp = 273.15;
-          if (Temp > 623.15) Temp = 623.15;
+/*        if (temperature < 273.15) temperature = 273.15;
+          if (temperature > 623.15) temperature = 623.15;
           if (pw < 1.0) pw = 1.0;
           if (pw > 1.0E8) pw = 1.0E8;
 */
-                
-                tau = T_star / Temp;	/* reduced temperature */
-                return tau * gamma_tau(tau, pw) * R * Temp;	/* J/kg */
+
+                tau = T_star / temperature;	/* reduced temperature */
+                return tau * gamma_tau(tau, pw) * R * temperature;	/* J/kg */
         }
-    
-    
+
+
     double gamma_tau (double tau, double pw) const
         {
             const double p_star = 1.653E7;  /* [Pa] */
@@ -196,12 +196,12 @@ public:
                 DUNE_THROW(Dune::RangeError,
                            "ConstrelWater: pressure " << pw << " out of range at " << __FILE__ << ":" << __LINE__);
             }
-/*	        if (pw < 1.0) 
+/*	        if (pw < 1.0)
             pw = 1.0;
-	        if (pw > 1.0E8) 
+	        if (pw > 1.0E8)
             pw = 1.0E8;
 */
-                
+
             double pi = pw / p_star;    /* reduced pressure */
 
             double gam_tau = 0.0;
@@ -227,33 +227,33 @@ public:
     /*                                                                        */
     /**************************************************************************/
 
-    double mass_density_water_Batzle (double Temp, double pw) const
+    double mass_density_water_Batzle (double temperature, double pw) const
         {
-            double TempC, pMPa, rhow;
+            double tempC, pMPa, rhow;
 
-            TempC = Temp - 273.15;
+            tempC = temperature - 273.15;
             pMPa  = pw/1.0E6;
 
-            rhow = (1 + 1.0E-6*(-80*TempC - 3.3*pow(TempC,2) + 0.00175*pow(TempC,3)
-                                + 489*pMPa - 2*TempC*pMPa + 0.016*pow(TempC,2)*pMPa
-                                - 1.3E-5*pow(TempC,3)*pMPa - 0.333*pow(pMPa,2)  
-                                - 0.002*TempC*pow(pMPa,2))) * 1000.0 ;
+            rhow = (1 + 1.0E-6*(-80*tempC - 3.3*pow(tempC,2) + 0.00175*pow(tempC,3)
+                                + 489*pMPa - 2*tempC*pMPa + 0.016*pow(tempC,2)*pMPa
+                                - 1.3E-5*pow(tempC,3)*pMPa - 0.333*pow(pMPa,2)
+                                - 0.002*tempC*pow(pMPa,2))) * 1000.0 ;
 
             return rhow; /* [kg/m^3] */
         }
 
     /***************************************************************************/
-    /*                                                                         */ 
-    /* Computation of the viscosity of pure water: IAPWS, 2003                 */ 
-    /* (revised release on the IAPS formulation 1985 for the viscosity of      */ 
-    /*  ordinary water substance, www.iapws.org)                               */ 
-    /*                                                                         */ 
-    /* validity: 273.15 K <= Temp <= 423.15 K                                  */ 
-    /*           p <= 5000 bar                                                 */ 
-    /*                                                                         */ 
+    /*                                                                         */
+    /* Computation of the viscosity of pure water: IAPWS, 2003                 */
+    /* (revised release on the IAPS formulation 1985 for the viscosity of      */
+    /*  ordinary water substance, www.iapws.org)                               */
+    /*                                                                         */
+    /* validity: 273.15 K <= temperature <= 423.15 K                                  */
+    /*           p <= 5000 bar                                                 */
+    /*                                                                         */
     /***************************************************************************/
 
-    double viscosity_water (double Temp, double pw) const
+    double viscosity_water (double temperature, double pw) const
         {
             double visco;
             double mu, mu0, mu1, mu_star;
@@ -264,13 +264,13 @@ public:
             int i,j;
 
             rho_star = 317.763;   /* rho at critical point [kg/m^3] */
-            T_star = 647.226;     /* Temp at critical point [K] */
+            T_star = 647.226;     /* temperature at critical point [K] */
             mu_star = 55.071E-6;   /* viscosity at critical point [Pa s] */
 
-            rho = mass_density_water_Batzle (Temp, pw);
+            rho = mass_density_water_Batzle (temperature, pw);
 
             rho_bar = rho/rho_star;
-            T_bar   = Temp/T_star;
+            T_bar   = temperature/T_star;
 
             /* computation of mu0 */
 
@@ -281,8 +281,8 @@ public:
             sum = 1.0 + H1/T_bar + H2/(T_bar*T_bar) + H3/(T_bar*T_bar*T_bar);
 
             mu0 = sqrt(T_bar)/sum;
-	 
-            /* computation of mu1 */  
+
+            /* computation of mu1 */
 
             for(i=0; i<=5; i++)
             {
@@ -337,26 +337,26 @@ public:
     /*                                                                  */
     /********************************************************************/
 
-    double mass_density_waterCO2 (double Temp, double pw, double x_CO2_w) const
+    double mass_density_waterCO2 (double temperature, double pw, double x_CO2_w) const
         {
             /* x_CO2_w : mole fraction of CO2 in water phase */
 
             double rho_pure, rho_inv, rho;
             double xww;
-            double T, M_CO2, M_H2O, M_T, V_phi;
+            double tempC, M_CO2, M_H2O, M_T, V_phi;
 
             M_CO2 = 0.044;
             M_H2O = 0.018;
 
-            T = Temp - 273.15;		/* T : temperature in °C */
+            tempC = temperature - 273.15;		/* tempC : temperature in °C */
 
-            rho_pure = mass_density_water_Batzle (Temp, pw);
+            rho_pure = mass_density_water_Batzle (temperature, pw);
 
             xww = 1.0 - x_CO2_w;
 
             M_T = M_H2O * xww + M_CO2 * x_CO2_w;
 
-            V_phi = (37.51 - 9.585E-2*T + 8.74E-4*pow(T,2) - 5.044E-7*pow(T,3)) / 1.0E6;
+            V_phi = (37.51 - 9.585E-2*tempC + 8.74E-4*pow(tempC,2) - 5.044E-7*pow(tempC,3)) / 1.0E6;
 
             rho_inv = x_CO2_w * V_phi/M_T + M_H2O * xww / (rho_pure * M_T);
 
@@ -367,13 +367,13 @@ public:
 
     /*********************************************************************/
     /* CITATION??????????????????????????????????*************************/
-    double lambda_w (double T) const
+    double lambda_w (double temperature) const
         {
             static double l[11], t[11];
             double downT, downl, upT, upl, lambda;
             int i;
 
-            l[1] = 0.564; 
+            l[1] = 0.564;
             l[2] = 0.574;
             l[3] = 0.584;
             l[4] = 0.597;
@@ -398,7 +398,7 @@ public:
 
             for (i=1; i<=10; i++)
             {
-                if (t[i] <= T && T <= t[i+1])
+                if (t[i] <= temperature && temperature <= t[i+1])
                 {
                     upT = t[i+1];
                     downT=t[i];
@@ -407,47 +407,47 @@ public:
                 }
             }
 
-            lambda = downl + ((T-downT)/(upT-downT))*(upl-downl);
+            lambda = downl + ((temperature-downT)/(upT-downT))*(upl-downl);
             return(lambda);
         }
     /*********************************************************************/
 
 
-    double lambda_pm (double T, double p, double Sw, double phi) const
+    double lambda_pm (double temperature, double p, double Sw, double phi) const
         {
-            double Sg, l_s, l_w, l_g, l_pm, l_dry, l_sat, llow, lhigh; 
+            double Sg, l_s, l_w, l_g, l_pm, l_dry, l_sat, llow, lhigh;
             ConstrelCO2 co2;
 
             Sg = 1 - Sw;
             l_s = 4.5; /*W/mK*/
-            l_w = lambda_w(T);
-            l_g = co2.lambda_CO2 (T, p);
+            l_w = lambda_w(temperature);
+            l_g = co2.lambda_CO2 (temperature, p);
             l_pm = l_s*(1- phi) + l_w*Sw*phi + l_g* Sg*phi;
-		
+
             /*wet*/
             /*Hashin-Shtrikman bounds for water and solid*/ /*Hartmann et al 2004*/
             /*HS-*/	llow = l_w + ( (1-phi) / (  ( 1/(l_s-l_w) )+(phi/(3*l_w))  ) );
             /*HS+*/	lhigh = l_s + ( phi / (  ( 1/(l_w-l_s) )+(phi/(3*l_s))  ) );
-		
+
             l_sat = 2 / ( 1/llow + 1/lhigh); /*harmonic averaging of the two
                                                bounds*/
-		
+
             /*dry*/
             /*Hashin-Shtrikman bounds for gas and solid*/
             /*HS-*/	llow = l_g + ( (1-phi) / (  ( 1/(l_s-l_g) )+(phi/(3*l_g))  ) );
             /*HS+*/	lhigh = l_s + ( phi / (  ( 1/(l_g-l_s) )+(phi/(3*l_s))  ) );
-		
+
             l_dry = 2 / ( 1/llow + 1/lhigh); /*harmonic averaging of the two
                                                bounds*/
-						
+
             l_pm = l_dry + sqrt(Sw)*(l_sat - l_dry); /*Somerton*/
 
             return(l_pm);
         }
 
-	
+
     //! enthalpy of steam according to IAPWS 97
-    double sp_enthalpy_IAPWS2(double Temp, double pg) const
+    double sp_enthalpy_IAPWS2(double temperature, double pg) const
         {
             static const double J0[10]={0.,0.,1.,-5.,-4.,-3.,-2.,-1.,2.,3.};
             static const double n0[10]={0.,-0.96927686500217E1, 0.10086655968018E2, -0.56087911283020E-2,
@@ -466,7 +466,7 @@ public:
                                         -0.21171472321355E-2, -0.23895741934104E2, -0.59059564324270E-17, -0.12621808899101E-5,
                                         -0.38946842435739E-1, 0.11256211360459E-10, -0.82311340897998E1, 0.19809712802088E-7,
                                         0.10406965210174E-18, -0.10234747095929E-12, -0.10018179379511E-8, -0.80882908646985E-10,
-                                        0.10693031879409, -0.33662250574171, 0.89185845355421E-24, 0.30629316876232E-12, 
+                                        0.10693031879409, -0.33662250574171, 0.89185845355421E-24, 0.30629316876232E-12,
                                         -0.42002467698208E-5, -0.59056029685639E-25, 0.37826947613457E-5, -0.12768608934681E-14,
                                         0.73087610595061E-28, 0.55414715350778E-16, -0.94369707241210E-6};
 
@@ -479,14 +479,14 @@ public:
 
             gammao = 0.0;
             for (i=1; i<=9; i++){
-                gammao = gammao + (n0[i] * J0[i] * pow((Tr/Temp),(J0[i]-1.)));
+                gammao = gammao + (n0[i] * J0[i] * pow((Tr/temperature),(J0[i]-1.)));
             }
 
             gammar=0.0;
             for (i=1; i<=43; i++){
-                gammar = gammar + (ns[i] * pow((pg/pr),Is[i]) * Js[i] * pow((Tr/Temp - 0.5),(Js[i]-1.)));
+                gammar = gammar + (ns[i] * pow((pg/pr),Is[i]) * Js[i] * pow((Tr/temperature - 0.5),(Js[i]-1.)));
             }
-            
+
             H = Rw * Tr * (gammao + gammar);
             return (H);
         }
