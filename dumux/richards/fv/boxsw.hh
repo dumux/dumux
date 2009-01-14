fe5033c879fa223093f707ce943d0cd292ee8963
Mehmet Onur Dogan <onur.dogan@enerjisa.com>
2009-01-14 17:19:05 +0000

    richards added
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@988 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ dumux/richards/fv/boxsw.hh
@@ -0,0 +1,187 @@
+// $Id: boxpwsn.hh 726 2008-10-22 16:42:22Z bernd $
+
+#ifndef DUNE_BOXPWSN_HH
+#define DUNE_BOXPWSN_HH
+
+#include<dune/istl/operators.hh>
+#include<dune/istl/solvers.hh>
+#include<dune/istl/preconditioners.hh>
+
+//#include <dune/common/array.hh>        // defines simple array class
+#include <dune/common/fixedarray.hh>   // defines simple array classes
+#include <dune/common/geometrytype.hh>
+#include <dune/grid/sgrid.hh>          // a complete structured grid
+#include <dune/grid/common/referenceelements.hh>
+#include <dune/grid/common/gridinfo.hh>
+#include <dune/grid/common/universalmapper.hh>
+#include <dune/grid/common/quadraturerules.hh>
+#include <dune/common/collectivecommunication.hh>
+#include <dune/istl/io.hh>
+#include <dune/common/timer.hh>
+#include <dune/istl/bvector.hh>
+#include <dune/istl/vbvector.hh>
+#include <dune/istl/bcrsmatrix.hh>
+#include <dune/istl/io.hh>
+#include <dune/istl/gsetc.hh>
+#include <dune/istl/ilu.hh>
+#include <dune/istl/operators.hh>
+#include <dune/istl/solvers.hh>
+#include <dune/istl/preconditioners.hh>
+#include <dune/istl/scalarproducts.hh>
+#include <dune/istl/paamg/amg.hh>
+#include <dune/grid/common/scsgmapper.hh>
+#include <dune/grid/common/mcmgmapper.hh>
+#include <dune/disc/functions/functions.hh>
+#include "dumux/operators/p1operatorextended.hh"
+#include "dumux/operators/owneroverlapcopyextended.hh"
+#include <dune/disc/operators/boundaryconditions.hh>
+#include <dune/grid/io/file/vtk/vtkwriter.hh>
+#include <dune/istl/paamg/amg.hh>
+#include "dumux/pardiso/pardiso.hh"
+#include "dumux/pardiso/identity.hh"
+#include "dumux/nonlinear/newtonmethod.hh"
+#include "dumux/richards/richardsmodel.hh"
+#include "dumux/richards/richardsproblem.hh"
+#include "dumux/richards/fv/boxswjacobian.hh"
+
+namespace Dune {
+
+/**
+ \brief Two phase model with Pw and Sn as primary unknowns
+
+ This implements a two phase model with Pw and Sn as primary unknowns.
+ */
+template<class G, class RT>
+class BoxSw
+: public LeafP1TwoPhaseModel<G, RT, TwoPhaseProblem<G, RT>, BoxSwJacobian<G, RT> >
+{
+
+public:
+	// define the problem type (also change the template argument above)
+	typedef TwoPhaseProblem<G, RT> ProblemType;
+
+	// define the local Jacobian (also change the template argument above)
+	typedef BoxSwJacobian<G, RT> LocalJacobian;
+
+	typedef LeafP1TwoPhaseModel<G, RT, ProblemType, LocalJacobian>
+			ThisLeafP1TwoPhaseModel;
+
+	typedef typename ThisLeafP1TwoPhaseModel::FunctionType FunctionType;
+
+	typedef typename G::Traits::LeafIndexSet IS;
+
+	enum {m = 1};
+
+	typedef BoxSw<G, RT> ThisType;
+	typedef typename ThisLeafP1TwoPhaseModel::FunctionType::RepresentationType VectorType;
+	typedef typename ThisLeafP1TwoPhaseModel::OperatorAssembler::RepresentationType MatrixType;
+	typedef MatrixAdapter<MatrixType,VectorType,VectorType> Operator;
+#if HAVE_MPI
+#else
+#ifdef HAVE_PARDISO
+	SeqPardiso<MatrixType,VectorType,VectorType> pardiso;
+#endif
+#endif
+
+	BoxSw(const G& g, ProblemType& prob)
+	: ThisLeafP1TwoPhaseModel(g, prob)
+	{}
+
+	virtual void solve()
+	{
+		Operator op(*(this->A));  // make operator out of matrix
+		double red=1E-14;
+
+#if HAVE_MPI
+			// set up parallel solvers
+		typedef typename G::Traits::GlobalIdSet::IdType GlobalIdType;
+		typedef OwnerOverlapCopyExtendedCommunication<GlobalIdType,int> CommunicationType;
+			Dune::IndexInfoFromGrid<GlobalIdType,int> indexinfo;
+			(this->u).fillIndexInfoFromGrid(indexinfo);
+			typedef Dune::OwnerOverlapCopyExtendedCommunication<GlobalIdType,int> CommunicationType;
+			CommunicationType oocc(indexinfo,(this->grid).comm());
+			int verbose=0;
+			if ((this->grid).comm().rank() == 0)
+				verbose = 1;
+			Dune::OverlappingSchwarzOperator<MatrixType,VectorType,VectorType,CommunicationType> oop(*(this->A),oocc);
+			Dune::OverlappingSchwarzScalarProduct<VectorType,CommunicationType> osp(oocc);
+			SeqILU0<MatrixType,VectorType,VectorType> ilu0(*(this->A),1.0);// a precondtioner
+			Dune::BlockPreconditioner<VectorType,VectorType,CommunicationType> parprec(ilu0,oocc);
+			Dune::BiCGSTABSolver<VectorType> parcg(oop,osp,parprec,red,1000,verbose);
+
+			// solve system
+			Dune::InverseOperatorResult r;
+			parcg.apply(*(this->u), *(this->f), r);
+#else
+#ifdef HAVE_PARDISO
+		pardiso.factorize(*(this->A));
+		BiCGSTABSolver<VectorType> solver(op,pardiso,red,100,2);         // an inverse operator
+#else
+		SeqILU0<MatrixType,VectorType,VectorType> ilu0(*(this->A),1.0);// a precondtioner
+		BiCGSTABSolver<VectorType> solver(op,ilu0,red,10000,1);         // an inverse operator
+#endif
+		InverseOperatorResult r;
+		solver.apply(*(this->u), *(this->f), r);
+#endif
+
+		return;
+	}
+
+	void update(double& dt) {
+		this->localJacobian.setDt(dt);
+		this->localJacobian.setOldSolution(this->uOldTimeStep);
+		NewtonMethod<G, ThisType> newtonMethod(this->grid(), *this);
+		newtonMethod.execute();
+		dt = this->localJacobian.getDt();
+		double upperMass, oldUpperMass;
+		double totalMass = this->injected(upperMass, oldUpperMass);
+		std::cout << totalMass << "\t"<< upperMass<< "\t"<< oldUpperMass
+				<< "\t"; //# totalMass, upperMass, oldUpperMass"<< std::endl;
+		*(this->uOldTimeStep) = *(this->u);
+
+		if (this->problem.exsolution)
+			this->problem.updateExSol(dt, *(this->u));
+
+	}
+
+	virtual void vtkout(const char* name, int k) {
+		VTKWriter<typename G::LeafGridView> vtkwriter(this->grid_.leafView());
+		int size=this->vertexmapper.size();
+		enum {dim = G::dimension};
+		char fname[128];
+		sprintf(fname, "%s-%05d", name, k);
+		double minSat = 1e100;
+		double maxSat = -1e100;
+		if (this->problem.exsolution) {
+			this->satEx.resize(size);
+			this->satError.resize(size);
+		}
+		for (int i = 0; i < size; i++) {
+			this->satW[i] = (*(this->u))[i][0];
+			double satWI = this->satW[i];
+			minSat = std::min(minSat, satWI);
+			maxSat = std::max(maxSat, satWI);
+			FieldVector<RT, dim> dummy (0);
+			this->pC[i]  = this->problem.materialLaw().pC(this->satW[i], dummy, *(this->grid_.template leafbegin<0>()), dummy); // functions as long as we have constant properties for pc
+			this->pW[i] = -this->pC[i]; // pw formulation
+			if (this->problem.exsolution) {
+				this->satEx[i]=this->problem.uExOutVertex(i, 1);
+				this->satError[i]=this->problem.uExOutVertex(i, 2);
+			}
+		}
+		vtkwriter.addVertexData(this->pW, "wetting phase pressure");
+		vtkwriter.addVertexData(this->pC, "capillary pressure");
+		vtkwriter.addVertexData(this->satW, "wetting phase saturation");
+		if (this->problem.exsolution) {
+			vtkwriter.addVertexData(this->satEx, "saturation, exact solution");
+			vtkwriter.addVertexData(this->satError, "saturation error");
+		}
+		vtkwriter.write(fname, VTKOptions::ascii);
+		std::cout << "wetting phase saturation: min = "<< minSat
+				<< ", max = "<< maxSat << std::endl;
+		if (minSat< -0.5 || maxSat > 1.5)DUNE_THROW(MathError, "Saturation exceeds range.");
+	}
+};
+
+}
+#endif
---
+++ dumux/richards/fv/boxswjacobian.hh
@@ -0,0 +1,267 @@
+// $Id: boxpwsnjacobian.hh 599 2008-09-18 13:57:47Z bernd $
+
+#ifndef DUNE_BOXPWSNJACOBIAN_HH
+#define DUNE_BOXPWSNJACOBIAN_HH
+
+#include<map>
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<vector>
+#include<sstream>
+
+#include<dune/common/exceptions.hh>
+#include<dune/grid/common/grid.hh>
+#include<dune/grid/common/referenceelements.hh>
+#include<dune/common/geometrytype.hh>
+#include<dune/grid/common/quadraturerules.hh>
+#include <dune/grid/utility/intersectiongetter.hh>
+
+#include<dune/disc/shapefunctions/lagrangeshapefunctions.hh>
+#include<dune/disc/operators/boundaryconditions.hh>
+#include"dumux/operators/boxjacobian.hh"
+#include"dumux/richards/richardsproblem.hh"
+
+/**
+ * @file
+ * @brief  compute local jacobian matrix for conforming finite elements for diffusion equation
+ * @author Peter Bastian
+ */
+
+
+
+namespace Dune
+{
+  /** @addtogroup DISC_Disc
+   *
+   * @{
+   */
+  /**
+   * @brief compute local jacobian matrix for conforming finite elements for diffusion equation
+   *
+   */
+
+
+  //! A class for computing local jacobian matrices
+  /*! A class for computing local jacobian matrix for the
+	diffusion equation
+
+	    div j = q; j = -K grad u; in Omega
+
+		u = g on Gamma1; j*n = J on Gamma2.
+
+	Uses conforming finite elements with the Lagrange shape functions.
+	It should work for all dimensions and element types.
+	All the numbering is with respect to the reference element and the
+	Lagrange shape functions
+
+	Template parameters are:
+
+	- Grid  a DUNE grid type
+	- RT    type used for return values
+  */
+  template<class G, class RT, class BoxFunction = LeafP1FunctionExtended<G, RT, 1> >
+  class BoxSwJacobian
+    : public BoxJacobian<BoxSwJacobian<G,RT,BoxFunction>,G,RT,1,BoxFunction>
+  {
+    typedef typename G::ctype DT;
+    typedef typename G::Traits::template Codim<0>::Entity Entity;
+    typedef typename Entity::Geometry Geometry;
+    typedef BoxSwJacobian<G,RT,BoxFunction> ThisType;
+    typedef typename LocalJacobian<ThisType,G,RT,1>::VBlockType VBlockType;
+    typedef typename LocalJacobian<ThisType,G,RT,1>::MBlockType MBlockType;
+	enum {satWIdx = 0};
+
+
+  public:
+	    // define the number of components of your system, this is used outside
+	    // to allocate the correct size of (dense) blocks with a FieldMatrix
+	    enum {dim=G::dimension};
+	    enum {m=1};
+	    enum {SIZE=LagrangeShapeFunctionSetContainer<DT,RT,dim>::maxsize};
+	    struct VariableNodeData;
+	    typedef FieldMatrix<RT,dim,dim> FMatrix;
+	    typedef FieldVector<RT,dim> FVector;
+
+	     //! Constructor
+    BoxSwJacobian (TwoPhaseProblem<G,RT>& params,
+			      bool levelBoundaryAsDirichlet_, const G& grid,
+			      BoxFunction& sol,
+			      bool procBoundaryAsDirichlet_=true)
+    : BoxJacobian<ThisType,G,RT,1,BoxFunction>(levelBoundaryAsDirichlet_, grid, sol, procBoundaryAsDirichlet_),
+      problem(params),
+      statNData(this->vertexMapper.size()), varNData(SIZE), oldVarNData(SIZE)
+    {
+      this->analytic = false;
+    }
+
+    void clearVisited ()
+    {
+    	return;
+    }
+
+    VBlockType computeM (const Entity& e, const VBlockType* sol,
+    		int node, const std::vector<VariableNodeData>& varData)
+    {
+   	 VBlockType result;
+   	 //std::cout << "rhoW = " << varData[node].density[pWIdx] << ", rhoN = " << varData[node].density[satNIdx] << std::endl;
+   	 result[0] = elData.porosity*varData[node].density[satWIdx]*varData[node].satW;//varData[node].density[pWIdx]*elData.porosity*varData[node].satW;//sol[node][satWIdx];
+
+   	 return result;
+    };
+
+    VBlockType computeM (const Entity& e, const VBlockType* sol, int node, bool old = false)
+    {
+    	if (old)
+    		return computeM(e, sol, node, oldVarNData);
+    	else
+    		return computeM(e, sol, node, varNData);
+    }
+
+    VBlockType computeA (const Entity& e, const VBlockType* sol, int face)
+    {
+		 int i = this->fvGeom.subContVolFace[face].i;
+     	 int j = this->fvGeom.subContVolFace[face].j;
+
+		  // permeability in edge direction
+		  FieldVector<DT,dim> Kij(0);
+		  elData.K.umv(this->fvGeom.subContVolFace[face].normal, Kij);
+
+		  VBlockType flux;
+		  for (int phase = 0; phase < m; phase++) {
+	          // calculate FE gradient
+	          FieldVector<RT, dim> pGrad(0);
+	          for (int k = 0; k < this->fvGeom.nNodes; k++) {
+	        	  FieldVector<DT,dim> grad(this->fvGeom.subContVolFace[face].grad[k]);
+	        	  grad *= varNData[k].pW ;  //(phase) ? varNData[k].pN : sol[k][pWIdx];
+	        	  pGrad += grad;
+	          }
+
+	          // adjust by gravity
+			  FieldVector<RT, dim> gravity = problem.gravity();
+			  gravity *= varNData[i].density[phase];
+			  pGrad -= gravity;
+
+			  // calculate the flux using upwind
+			  RT outward = pGrad*Kij;
+			  if (outward < 0)
+				  flux[phase] = varNData[i].density[phase]*varNData[i].mobility[phase]*outward;
+			  else
+				  flux[phase] = varNData[j].density[phase]*varNData[j].mobility[phase]*outward;
+		  }
+
+		  return flux;
+   };
+
+
+    VBlockType computeQ (const Entity& e, const VBlockType* sol, const int& node)
+    {
+   	 // ASSUME problem.q already contains \rho.q
+   	 return problem.q(this->fvGeom.subContVol[node].global, e, this->fvGeom.subContVol[node].local);
+    };
+
+
+	  // *********************************************************
+	  // *														 *
+	  // *	Calculation of Data at Elements			 			 *
+	  // *						 								 *
+	  // *														 *
+	  // *********************************************************
+
+    void computeElementData (const Entity& e)
+    {
+		 // ASSUMING element-wise constant permeability and porosity, evaluate at the cell center
+ 		 elData.K = this->problem.soil().K(this->fvGeom.cellGlobal, e, this->fvGeom.cellLocal);
+ 		 elData.porosity = this->problem.soil().porosity(this->fvGeom.cellGlobal, e, this->fvGeom.cellLocal);
+    };
+
+
+	  // *********************************************************
+	  // *														 *
+	  // *	Calculation of Data at Nodes that has to be			 *
+	  // *	determined only once	(statNData)					 *
+	  // *														 *
+	  // *********************************************************
+
+    // analog to EvalStaticData in MUFTE
+    void updateStaticData (const Entity& e, const VBlockType* sol)
+    {
+	  return;
+    }
+
+
+	  //*********************************************************
+	  //*														*
+	  //*	Calculation of variable Data at Nodes			 	*
+	  //*	(varNData)										 	*
+	  //*													 	*
+	  //*********************************************************
+
+
+    // the members of the struct are defined here
+    struct VariableNodeData
+    {
+    	RT satW;
+    	RT pC;
+    	RT pW;
+    	VBlockType mobility;  //Vector with the number of phases
+    	VBlockType density;
+    	FieldMatrix<DT,dim,dim> K;
+    };
+
+    // analog to EvalPrimaryData in MUFTE, uses members of varNData
+	void updateVariableData(const Entity& e, const VBlockType* sol,
+			int i, std::vector<VariableNodeData>& varData)
+    {
+//		this->fvGeom.update(e);
+		FVector& global = this->fvGeom.subContVol[i].global;
+  	   	FVector& local = this->fvGeom.subContVol[i].local;
+
+//		varData[i].K = problem.K(this->fvGeom.subContVol[i].global, e, this->fvGeom.subContVol[i].local);
+		varData[i].satW = sol[i][satWIdx];
+   		varData[i].pC = problem.materialLaw().pC(varData[i].satW, global, e, local);
+   		varData[i].pW = -varData[i].pC;
+   		varData[i].mobility[satWIdx] = problem.materialLaw().mobW(varData[i].satW, global, e, local);
+   		varData[i].density[satWIdx] = problem.wettingPhase().density(283.15, varData[i].pW);
+    }
+
+	void updateVariableData(const Entity& e, const VBlockType* sol, int i, bool old = false)
+	{
+		if (old) {
+			updateVariableData(e, sol, i, oldVarNData);
+		}
+		else
+			updateVariableData(e, sol, i, varNData);
+	}
+
+	void updateVariableData(const Entity& e, const VBlockType* sol, bool old = false)
+	{
+		int size = this->fvGeom.nNodes;
+
+		for (int i = 0; i < size; i++)
+				updateVariableData(e, sol, i, old);
+	}
+
+    struct StaticNodeData
+    {
+    	bool visited;
+    };
+
+    struct ElementData {
+   	 RT cellVolume;
+     RT porosity;
+   	 RT gravity;
+   	 FieldVector<RT, 4> parameters;
+   	 FieldMatrix<DT,dim,dim> K;
+   	 } elData;
+
+     // parameters given in constructor
+     TwoPhaseProblem<G,RT>& problem;
+     std::vector<StaticNodeData> statNData;
+     std::vector<VariableNodeData> varNData;
+     std::vector<VariableNodeData> oldVarNData;
+  };
+
+  /** @} */
+}
+#endif
