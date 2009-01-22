4ce714d25e634ae28e1265c6a7af72198d6436dd
Melanie Darcis <mdarcis@gmx.de>
2009-01-22 13:31:26 +0000

    corrected
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1054 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/material/matrixproperties.hh
+++ dumux/material/matrixproperties.hh
@@ -22,11 +22,11 @@ typedef	typename G::Traits::template Codim<0>::Entity Entity;
 	typedef typename G::ctype DT;
 	enum {n=G::dimension, m=1};
 
-	virtual const FieldMatrix<DT,n,n> &K(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi)
+	virtual const FieldMatrix<DT,n,n> &K(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const int idx=0)
 	{
 		return K_;
 	}
-	virtual double porosity(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
+	virtual double porosity(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const int idx=0) const
 	{
 		return 0.3;
 	}
@@ -43,14 +43,14 @@ typedef	typename G::Traits::template Codim<0>::Entity Entity;
 
 	/* ATTENTION: define heat capacity per cubic meter! Be sure, that it corresponds to porosity!
 	 * Best thing will be to define heatCap = (specific heatCapacity of material) * density * porosity*/
-	virtual double heatCap(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
+	virtual double heatCap(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const int idx=0) const
 	{
 		return 790 /* spec. heat cap. of granite */
 		* 2700 /* density of granite */
 		* (1 - porosity(x, e, xi));
 	}
 
-	virtual double heatCond(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double sat) const
+	virtual double heatCond(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double sat, const int idx=0) const
 	{
 		static const double lWater = 0.6;
 		static const double lGranite = 2.8;
