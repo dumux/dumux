697ad571c2353b5ec8bc015843dbf1bb4029c881
Melanie Darcis <mdarcis@gmx.de>
2009-01-22 12:39:29 +0000

    local index idx added as default argument in the functions: K, porosity, heatCap and heatCond of the HeterogeneousSoil class. Constructor of HeterogeneousSoil class overloaded for reading soil properties from file.
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1049 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/material/matrixproperties.hh
+++ dumux/material/matrixproperties.hh
@@ -18,78 +18,78 @@ template<class G, class RT>
 class HomogeneousSoil: public Matrix2p<G,RT>
 {
 public:
-    typedef typename G::Traits::template Codim<0>::Entity Entity;
-    typedef typename G::ctype DT;
-    enum {n=G::dimension, m=1};
+typedef	typename G::Traits::template Codim<0>::Entity Entity;
+	typedef typename G::ctype DT;
+	enum {n=G::dimension, m=1};
 
-    virtual const FieldMatrix<DT,n,n> &K(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi)
-    {
-        return K_;
-    }
-    virtual double porosity(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
-    {
-        return 0.3;
-    }
+	virtual const FieldMatrix<DT,n,n> &K(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi)
+	{
+		return K_;
+	}
+	virtual double porosity(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
+	{
+		return 0.3;
+	}
 
-    virtual double Sr_w(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T) const
-    {
-        return 0.;
-    }
+	virtual double Sr_w(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T) const
+	{
+		return 0.;
+	}
 
-    virtual double Sr_n(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T) const
-    {
-        return 0.;
-    }
+	virtual double Sr_n(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T) const
+	{
+		return 0.;
+	}
 
-    /* ATTENTION: define heat capacity per cubic meter! Be sure, that it corresponds to porosity!
-             * Best thing will be to define heatCap = (specific heatCapacity of material) * density * porosity*/
-    virtual double heatCap(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
-    {
-        return     790 /* spec. heat cap. of granite */
-                        * 2700 /* density of granite */
-                        * (1 - porosity(x, e, xi));
-    }
+	/* ATTENTION: define heat capacity per cubic meter! Be sure, that it corresponds to porosity!
+	 * Best thing will be to define heatCap = (specific heatCapacity of material) * density * porosity*/
+	virtual double heatCap(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
+	{
+		return 790 /* spec. heat cap. of granite */
+		* 2700 /* density of granite */
+		* (1 - porosity(x, e, xi));
+	}
 
-    virtual double heatCond(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double sat) const
-    {
-        static const double lWater = 0.6;
-        static const double lGranite = 2.8;
-        double poro = porosity(x, e, xi);
-        double lsat = pow(lGranite, (1-poro)) * pow(lWater, poro);
-        double ldry = pow(lGranite, (1-poro));
-        return ldry + sqrt(sat) * (ldry - lsat);
-    }
+	virtual double heatCond(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double sat) const
+	{
+		static const double lWater = 0.6;
+		static const double lGranite = 2.8;
+		double poro = porosity(x, e, xi);
+		double lsat = pow(lGranite, (1-poro)) * pow(lWater, poro);
+		double ldry = pow(lGranite, (1-poro));
+		return ldry + sqrt(sat) * (ldry - lsat);
+	}
 
-    virtual std::vector<double> paramRelPerm(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T) const
-    {
-        // example for Brooks-Corey parameters
-        std::vector<double> param(2);
-        param[0] = 2.; // lambda
-        param[1] = 0.; // entry-pressures
+	virtual std::vector<double> paramRelPerm(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T) const
+	{
+		// example for Brooks-Corey parameters
+		std::vector<double> param(2);
+		param[0] = 2.; // lambda
+		param[1] = 0.; // entry-pressures
 
-//        if (x[0] > 150)
-//            param[0] = 0.5;
+		//        if (x[0] > 150)
+		//            param[0] = 0.5;
 
-        return param;
-    }
+		return param;
+	}
 
-    virtual typename Matrix2p<G,RT>::modelFlag relPermFlag(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
-    {
-        return Matrix2p<G,RT>::brooks_corey;
-    }
+	virtual typename Matrix2p<G,RT>::modelFlag relPermFlag(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
+	{
+		return Matrix2p<G,RT>::brooks_corey;
+	}
 
-    HomogeneousSoil():Matrix2p<G,RT>()
-    {
-        K_ = 0;
-        for(int i = 0; i < n; i++)
-            K_[i][i] = 1e-12;
-    }
+	HomogeneousSoil():Matrix2p<G,RT>()
+	{
+		K_ = 0;
+		for(int i = 0; i < n; i++)
+		K_[i][i] = 1e-12;
+	}
 
-    ~HomogeneousSoil()
-    {}
+	~HomogeneousSoil()
+	{}
 
 private:
-    FieldMatrix<DT,n,n> K_;
+	FieldMatrix<DT,n,n> K_;
 
 };
 
@@ -98,72 +98,88 @@ class HeterogeneousSoil: public Matrix2p<G,RT>
 {
 public:
     typedef typename G::Traits::template Codim<0>::Entity Entity;
-    typedef typename G::ctype DT;
-    enum {n=G::dimension, m=1};
+	typedef typename G::ctype DT;
+	enum {n=G::dimension, m=1};
 
-    virtual FieldMatrix<DT,n,n> K (const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi)
-    {
-          return permeability.K(e);
-    }
-    virtual double porosity(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
-    {
-        return 0.3;
-    }
+	const virtual FieldMatrix<DT,n,n> &K (const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const int idx=0)
+	{
+		return permeability.K(e);
+	}
 
-    virtual double Sr_w(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T = 283.15) const
-    {
-        return 0;
-    }
+	virtual double porosity(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const int idx=0) const
+	{
+		return 0.3;
+	}
 
-    virtual double Sr_n(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T = 283.15) const
-    {
-        return 0;
-    }
+	virtual double Sr_w(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T = 283.15) const
+	{
+		return 0;
+	}
 
-    /* ATTENTION: define heat capacity per cubic meter! Be sure, that it corresponds to porosity!
-             * Best thing will be to define heatCap = (specific heatCapacity of material) * density * porosity*/
-    virtual double heatCap(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
-    {
-        return     790 /* spec. heat cap. of granite */
-                        * 2700 /* density of granite */
-                        * (1-porosity(x, e, xi));
-    }
+	virtual double Sr_n(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T = 283.15) const
+	{
+		return 0;
+	}
 
-    virtual double heatCond(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double sat) const
-    {
-        static const double lWater = 0.6;
-        static const double lGranite = 2.8;
-        double poro = porosity(x, e, xi);
-        double lsat = pow(lGranite, (1-poro)) * pow(lWater, poro);
-        double ldry = pow(lGranite, (1-poro));
-        return ldry + sqrt(sat) * (ldry - lsat);
-    }
+	/* ATTENTION: define heat capacity per cubic meter! Be sure, that it corresponds to porosity!
+	 * Best thing will be to define heatCap = (specific heatCapacity of material) * density * porosity*/
+	virtual double heatCap(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const int idx=0) const
+	{
+		return 790 /* spec. heat cap. of granite */
+		* 2700 /* density of granite */
+		* (1-porosity(x, e, xi, idx));
+	}
 
-    virtual std::vector<double> paramRelPerm(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T = 283.15) const
-    {
-        // example for Brooks-Corey parameters
-        std::vector<double> param(2);
-        param[0] = 2.; // lambda
-        param[1] = 0.; // entry-pressure
+	virtual double heatCond(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double sat, const int idx=0) const
+	{
+		static const double lWater = 0.6;
+		static const double lGranite = 2.8;
+		double poro = porosity(x, e, xi);
+		double lsat = pow(lGranite, (1-poro)) * pow(lWater, poro);
+		double ldry = pow(lGranite, (1-poro));
+		return ldry + sqrt(sat) * (ldry - lsat);
+	}
 
-        return param;
-    }
+	virtual std::vector<double> paramRelPerm(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi, const double T = 283.15) const
+	{
+		// example for Brooks-Corey parameters
+		std::vector<double> param(2);
+		param[0] = 2.; // lambda
+		param[1] = 0.; // entry-pressure
 
-    virtual typename Matrix2p<G,RT>::modelFlag relPermFlag(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
-    {
-        return Matrix2p<G,RT>::brooks_corey;
-    }
+		return param;
+	}
 
-    HeterogeneousSoil(const G& g,const char* name = "permeab.dat", const bool create = true)
-        :Matrix2p<G,RT>(),permeability(g, name, create)
-    {}
+	virtual typename Matrix2p<G,RT>::modelFlag relPermFlag(const FieldVector<DT,n>& x, const Entity& e, const FieldVector<DT,n>& xi) const
+	{
+		return Matrix2p<G,RT>::brooks_corey;
+	}
 
-    ~HeterogeneousSoil()
-    {}
+	virtual bool readPropertiesFlag()
+	{
+		return false;
+	}
+
+	HeterogeneousSoil(const G& g,const char* name = "permeab.dat", const bool create = true)
+	:Matrix2p<G,RT>(),permeability(g, name, create)
+	{}
+
+	HeterogeneousSoil(const G& g, const bool read, const char* name, const bool create = false)
+	:Matrix2p<G,RT>(),permeability(g, name, create)
+	{
+		K_ = 0;
+		for(int i = 0; i < n; i++)
+		K_[i][i] = 1e-12;
+	}
+
+	~HeterogeneousSoil()
+	{}
 
 private:
 public:
-    RandomPermeability<G> permeability;
+	RandomPermeability<G> permeability;
+	FieldMatrix<DT,n,n> K_;
+
 };
 
 } // end namespace
