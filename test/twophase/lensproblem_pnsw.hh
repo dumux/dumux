0e312c8bc5b1d3c73ebad9a3940f1b239b3cb040
Anneli Schöniger <anneli.schoeniger@bosscon.de>
2009-01-26 09:14:44 +0000

    edited initial and boundary conditions, sanitized white spaces
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1090 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/twophase/lensproblem_pnsw.hh
+++ test/twophase/lensproblem_pnsw.hh
@@ -1,4 +1,3 @@
-// $Id:
 
 #ifndef DUNE_LENSPROBLEM_PNSW_HH
 #define DUNE_LENSPROBLEM_PNSW_HH
@@ -32,143 +31,143 @@ namespace Dune
    * and \f$\lambda\f$ the total mobility, possibly depending on the
    * saturation.
    *
-   *	Template parameters are:
+   *    Template parameters are:
    *
-   *	- Grid  a DUNE grid type
-   *	- RT    type used for return values
+   *    - Grid  a DUNE grid type
+   *    - RT    type used for return values
    */
   template<class G, class RT>
   class LensProblem : public TwoPhaseProblem<G, RT> {
-	typedef typename G::ctype DT;
-	enum {dim=G::dimension, m=2};
-	typedef typename G::Traits::template Codim<0>::Entity Entity;
-	typedef typename IntersectionIteratorGetter<G,LeafTag>::IntersectionIterator IntersectionIterator;
+    typedef typename G::ctype DT;
+    enum {dim=G::dimension, m=2};
+    typedef typename G::Traits::template Codim<0>::Entity Entity;
+    typedef typename IntersectionIteratorGetter<G,LeafTag>::IntersectionIterator IntersectionIterator;
 
   public:
-	// Constructor
-	LensProblem(Fluid& liq1, Fluid& liq2, Matrix2p<G, RT>& soil,
-			const FieldVector<DT,dim>& outerLowerLeft = 0., const FieldVector<DT,dim>& outerUpperRight = 0,
-			const FieldVector<DT,dim>& innerLowerLeft = 0., const FieldVector<DT,dim>& innerUpperRight = 0,
-			TwoPhaseRelations<G, RT>& law = *(new TwoPhaseRelations<G, RT>))
-	: TwoPhaseProblem<G,RT>(liq1, liq2, soil, law),
-		outerLowerLeft_(outerLowerLeft), outerUpperRight_(outerUpperRight),
-		innerLowerLeft_(innerLowerLeft), innerUpperRight_(innerUpperRight),
-		eps_(1e-8*outerUpperRight[0]),
-		densityW_(liq1.density()), densityN_(liq2.density())
-	{
-		height_ = outerUpperRight[1] - outerLowerLeft[1];
-		width_ = outerUpperRight[0] - outerLowerLeft[0];
+    // Constructor
+    LensProblem(Fluid& liq1, Fluid& liq2, Matrix2p<G, RT>& soil,
+            const FieldVector<DT,dim>& outerLowerLeft = 0., const FieldVector<DT,dim>& outerUpperRight = 0,
+            const FieldVector<DT,dim>& innerLowerLeft = 0., const FieldVector<DT,dim>& innerUpperRight = 0,
+            TwoPhaseRelations<G, RT>& law = *(new TwoPhaseRelations<G, RT>))
+    : TwoPhaseProblem<G,RT>(liq1, liq2, soil, law),
+        outerLowerLeft_(outerLowerLeft), outerUpperRight_(outerUpperRight),
+        innerLowerLeft_(innerLowerLeft), innerUpperRight_(innerUpperRight),
+        eps_(1e-8*outerUpperRight[0]),
+        densityW_(liq1.density()), densityN_(liq2.density())
+    {
+        height_ = outerUpperRight[1] - outerLowerLeft[1];
+        width_ = outerUpperRight[0] - outerLowerLeft[0];
 
-		gravity_[0] = 0;
-		gravity_[1] = -9.81;
-	}
+        gravity_[0] = 0;
+        gravity_[1] = -9.81;
+    }
 
     // indices for the two phases
-	enum {pNIdx = 0, sWIdx = 1};
+    enum {pNIdx = 0, sWIdx = 1};
 
-	// function returning the BOUNDARY CONDITION TYPE depending on the position
-	virtual FieldVector<BoundaryConditions::Flags, m> bctype (const FieldVector<DT,dim>& x, const Entity& e,
-					const IntersectionIterator& intersectionIt,
-					   const FieldVector<DT,dim>& xi) const
-	{
-		// boundary condition type is set to Neumann
-		FieldVector<BoundaryConditions::Flags, m> values(BoundaryConditions::neumann);
+    // function returning the BOUNDARY CONDITION TYPE depending on the position
+    virtual FieldVector<BoundaryConditions::Flags, m> bctype (const FieldVector<DT,dim>& x, const Entity& e,
+                    const IntersectionIterator& intersectionIt,
+                       const FieldVector<DT,dim>& xi) const
+    {
+        // boundary condition type is set to Neumann
+        FieldVector<BoundaryConditions::Flags, m> values(BoundaryConditions::neumann);
 
-		if (x[0] < outerLowerLeft_[0] + eps_ || x[0] > outerUpperRight_[0] - eps_)
-		{
-			values = BoundaryConditions::dirichlet;
-		}
+        if (x[0] < outerLowerLeft_[0] + eps_ || x[0] > outerUpperRight_[0] - eps_)
+        {
+            values = BoundaryConditions::dirichlet;
+        }
 
-		return values;
-	}
+        return values;
+    }
 
-	// definition of DIRICHLET boundary conditions depending on the position
-	virtual FieldVector<RT,m> g (const FieldVector<DT,dim>& x, const Entity& e,
-				const IntersectionIterator& intersectionIt,
-				  const FieldVector<DT,dim>& xi) const
-	{
-		FieldVector<RT,m> values(0);
+    // definition of DIRICHLET boundary conditions depending on the position
+    virtual FieldVector<RT,m> g (const FieldVector<DT,dim>& x, const Entity& e,
+                const IntersectionIterator& intersectionIt,
+                  const FieldVector<DT,dim>& xi) const
+    {
+        FieldVector<RT,m> values(0);
 
-		if (x[0] < outerLowerLeft_[0] + eps_)
-		{
-			RT a = -(1 + 0.5/height_);
-			RT b = -a*outerUpperRight_[1];
-			values[sWIdx] = 1;
-			values[pNIdx] = -densityW_*gravity_[1]*(a*x[1] + b) - 42.5892;	// test!!
-		}
-		else {
-			RT a = -1;
-			RT b = outerUpperRight_[1];
-			values[sWIdx] = 1;
-			values[pNIdx] = -densityW_*gravity_[1]*(a*x[1] + b) - 42.5892;	// test!!
-		}
+        if (x[0] < outerLowerLeft_[0] + eps_)
+        {
+            RT a = -(1 + 0.5/height_);
+            RT b = -a*outerUpperRight_[1];
+            values[sWIdx] = 1;
+            values[pNIdx] = -densityW_*gravity_[1]*(a*x[1] + b);    // pN = pW for sW = 1 and pC = 0
+        }
+        else {
+            RT a = -1;
+            RT b = outerUpperRight_[1];
+            values[sWIdx] = 1;
+            values[pNIdx] = -densityW_*gravity_[1]*(a*x[1] + b);
+        }
 
-		return values;
-	}
+        return values;
+    }
 
-	// definition of NEUMANN boundary conditions depending on the position
-	virtual FieldVector<RT,m> J (const FieldVector<DT,dim>& x, const Entity& e,
-				const IntersectionIterator& intersectionIt,
-				  const FieldVector<DT,dim>& xi) const
-	{
-		FieldVector<RT,m> values(0);
+    // definition of NEUMANN boundary conditions depending on the position
+    virtual FieldVector<RT,m> J (const FieldVector<DT,dim>& x, const Entity& e,
+                const IntersectionIterator& intersectionIt,
+                  const FieldVector<DT,dim>& xi) const
+    {
+        FieldVector<RT,m> values(0);
 
-		RT lambda = (outerUpperRight_[0] - x[0])/width_;
-		if (lambda > 0.5 && lambda < 2.0/3.0 && x[1] > outerUpperRight_[1] - eps_) {
-			values[pNIdx] = -0.04;
-		}
+        RT lambda = (outerUpperRight_[0] - x[0])/width_;
+        if (lambda > 0.5 && lambda < 2.0/3.0 && x[1] > outerUpperRight_[1] - eps_) {
+            values[pNIdx] = -0.04;
+        }
 
-		return values;
-	}
+        return values;
+    }
 
-	// definition of INITIAL VALUES for pressure and saturation
-	virtual FieldVector<RT,m> initial (const FieldVector<DT,dim>& x, const Entity& e,
-				  const FieldVector<DT,dim>& xi) const
-	{
+    // definition of INITIAL VALUES for pressure and saturation
+    virtual FieldVector<RT,m> initial (const FieldVector<DT,dim>& x, const Entity& e,
+                  const FieldVector<DT,dim>& xi) const
+    {
 
-		FieldVector<RT,m> values;
+        FieldVector<RT,m> values;
 
-		values[pNIdx] = -densityW_*gravity_[1]*(height_ - x[1])- 42.5892;	// test!!
+        values[pNIdx] = -densityW_*gravity_[1]*(height_ - x[1]);
 
-		if (x[0] < outerLowerLeft_[0] + eps_) {
-			RT a = -(1 + 0.5/height_);
-			RT b = -a*outerUpperRight_[1];
-			values[pNIdx] = -densityW_*gravity_[1]*(a*x[1] + b) - 42.5892;	// test!!
-		}
+        if (x[0] < outerLowerLeft_[0] + eps_) {
+            RT a = -(1 + 0.5/height_);
+            RT b = -a*outerUpperRight_[1];
+            values[pNIdx] = -densityW_*gravity_[1]*(a*x[1] + b);
+        }
 
-		if (x[0] > innerLowerLeft_[0] && x[0] < innerUpperRight_[0]
-		    && x[1] > innerLowerLeft_[1] && x[1] < innerUpperRight_[1])
-			values[sWIdx] = 1;//innerSnr_;
-		else
-			values[sWIdx] = 1;//outerSnr_;
+        if (x[0] > innerLowerLeft_[0] && x[0] < innerUpperRight_[0]
+            && x[1] > innerLowerLeft_[1] && x[1] < innerUpperRight_[1])
+            values[sWIdx] = 1;//innerSnr_;
+        else
+            values[sWIdx] = 1;//outerSnr_;
 
-		return values;
-	}
+        return values;
+    }
 
 
-	// function returning SOURCE/SINK terms
-	virtual FieldVector<RT,m> q (const FieldVector<DT,dim>& x, const Entity& e,
-					const FieldVector<DT,dim>& xi) const
-	{
-		FieldVector<RT,m> values(0);
+    // function returning SOURCE/SINK terms
+    virtual FieldVector<RT,m> q (const FieldVector<DT,dim>& x, const Entity& e,
+                    const FieldVector<DT,dim>& xi) const
+    {
+        FieldVector<RT,m> values(0);
 
-		return values;
-	}
+        return values;
+    }
 
-	virtual FieldVector<RT,dim> gravity () const
-	{
-		return gravity_;
-	}
+    virtual FieldVector<RT,dim> gravity () const
+    {
+        return gravity_;
+    }
 
-	private:
-		FieldVector<DT,dim> outerLowerLeft_;
-		FieldVector<DT,dim> outerUpperRight_;
-		FieldVector<DT,dim> innerLowerLeft_;
-		FieldVector<DT,dim> innerUpperRight_;
-		DT width_, height_;
-		DT eps_;
-		RT densityW_, densityN_;
-		FieldVector<DT,dim> gravity_;
+    private:
+        FieldVector<DT,dim> outerLowerLeft_;
+        FieldVector<DT,dim> outerUpperRight_;
+        FieldVector<DT,dim> innerLowerLeft_;
+        FieldVector<DT,dim> innerUpperRight_;
+        DT width_, height_;
+        DT eps_;
+        RT densityW_, densityN_;
+        FieldVector<DT,dim> gravity_;
   };
 
 } // end namespace
