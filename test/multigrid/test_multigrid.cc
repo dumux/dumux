4112d4f4d0284ec18cbb25b6d3955541c5e6d43b
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2009-02-09 13:08:03 +0000

    multigrid adapted to dune-disc requirements
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1163 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/multigrid/test_multigrid.cc
+++ test/multigrid/test_multigrid.cc
@@ -100,7 +100,8 @@ void testadaptivity (G& grid, int maxsteps, int modulo, bool globalrefine=false,
 
     typedef Dune::LeafP1Function<G,NumberType> LeafFunction;
     typedef Dune::LeafP1OperatorAssembler<G,NumberType,1> LeafOperatorAssembler;
-    typedef Dune::GroundwaterEquationLocalStiffness<typename G::LevelGridView,NumberType> LocalStiffnessType;
+    typedef Dune::GroundwaterEquationLocalStiffness<typename G::LeafGridView,NumberType> LeafLocalStiffness;
+    typedef Dune::GroundwaterEquationLocalStiffness<typename G::LevelGridView,NumberType> LevelLocalStiffness;
     typedef typename Dune::LeafP1Function<G,NumberType>::RepresentationType VectorType;
     typedef typename Dune::LeafP1OperatorAssembler<G,NumberType,1>::RepresentationType MatrixType;
     int codim = dim;
@@ -142,7 +143,7 @@ void testadaptivity (G& grid, int maxsteps, int modulo, bool globalrefine=false,
 
         LeafOperatorAssembler A(grid);
 
-        LocalStiffnessType lstiff(grid.leafView(),mp,false);
+        LeafLocalStiffness lstiff(grid.leafView(),mp,false);
 
         A.assemble(lstiff,u,f);
 
@@ -161,22 +162,22 @@ void testadaptivity (G& grid, int maxsteps, int modulo, bool globalrefine=false,
 
         // geometric multigrid
 #if LOOPMGC
-        LocalStiffnessType mglstiff(mp,true); // with level bnd as Dirichlet
-        Dune::SeqP1GeomMG<MatrixType,G,LocalStiffnessType,VectorType,VectorType,1>
+        LevelLocalStiffness mglstiff(mp,true); // with level bnd as Dirichlet
+        Dune::SeqP1GeomMG<MatrixType,G,LevelLocalStiffness,VectorType,VectorType,1>
         mgc(*A,grid,mglstiff,1,0,2,false);
         Dune::LoopSolver<VectorType> solver(op,mgc,red,10000,1);
 #endif
 
 #if BICGMGC
-        LocalStiffnessType mglstiff(grid.leafView(),mp,true); // with level bnd as Dirichlet
-        Dune::SeqP1GeomMG<MatrixType,G,LocalStiffnessType,VectorType,VectorType,1>
+        LevelLocalStiffness mglstiff(grid.levelView(grid.maxLevel()),mp,true); // with level bnd as Dirichlet
+        Dune::SeqP1GeomMG<MatrixType,G,LevelLocalStiffness,VectorType,VectorType,1>
         mgc(*A,grid,mglstiff,0,1,1,false);
         Dune::CGSolver<VectorType> solver(op,mgc,red,10000,2);
 #endif
 
 #if CGBPX
-        LocalStiffnessType mglstiff(grid.leafView(),mp,true); // with level bnd as Dirichlet
-        Dune::SeqP1GeomMG<MatrixType,G,LocalStiffnessType,VectorType,VectorType,1>
+        LevelLocalStiffness mglstiff(grid.leafView(),mp,true); // with level bnd as Dirichlet
+        Dune::SeqP1GeomMG<MatrixType,G,LevelLocalStiffness,VectorType,VectorType,1>
         bpx(*A,grid,mglstiff,1,1,0,true);
         Dune::CGSolver<VectorType> solver(op,bpx,red,10000,1);         // an inverse operator
 #endif
