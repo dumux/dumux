bc947e86062d25429fd391576292e06a2de36d9a
Melanie Darcis <mdarcis@gmx.de>
2009-02-10 08:15:35 +0000

    naming conventions added
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1167 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/2pni/energyproblem.hh
+++ test/2pni/energyproblem.hh
@@ -16,7 +16,7 @@
 #include<dumux/material/twophaserelations.hh>
 #include<dumux/material/relperm_pc_law.hh>
 #include<dumux/2pni/2pniproblem.hh>
-
+#include<dumux/auxiliary/basicdomain.hh>
 
 /**
  * @file
@@ -26,108 +26,117 @@
 
 namespace Dune
 {
-  template<class G, class RT>
-  class TwoPHeatProblem : public TwoPhaseHeatProblem<G, RT> {
-    typedef typename G::ctype DT;
-    enum {dim=G::dimension, m=3};
-    typedef typename G::Traits::template Codim<0>::Entity Entity;
-    typedef typename IntersectionIteratorGetter<G,LeafTag>::IntersectionIterator IntersectionIterator;
+template<class Grid, class Scalar>
+class TwoPHeatProblem: public TwoPhaseHeatProblem<Grid, Scalar>
+{
+typedef    typename Grid::ctype CoordScalar;
+    enum
+    {   dim=Grid::dimension, numEq=3};
+    typedef typename Grid::Traits::template Codim<0>::Entity Element;
+    typedef typename IntersectionIteratorGetter<Grid,LeafTag>::IntersectionIterator IntersectionIterator;
 
-  public:
+    typedef BasicDomain<Grid, Scalar> ParentType;
+    // the domain traits of the domain
+    typedef typename ParentType::DomainTraits DomainTraits;
+    typedef typename DomainTraits::LocalPosition LocalPosition;
+    typedef typename DomainTraits::GlobalPosition GlobalPosition;
+
+public:
     // Constructor
-    TwoPHeatProblem(Fluid& liq1, Fluid& liq2, Matrix2p<G, RT>& soil,
-            TwoPhaseRelations<G, RT>& law = *(new TwoPhaseRelations<G, RT>))
-    : TwoPhaseHeatProblem<G,RT>(liq1, liq2, soil, law),
-        eps_(1e-4)
+    TwoPHeatProblem(Fluid& liq1, Fluid& liq2, Matrix2p<Grid, Scalar>& soil,
+            TwoPhaseRelations<Grid, Scalar>& law = *(new TwoPhaseRelations<Grid, Scalar>))
+    : TwoPhaseHeatProblem<Grid,Scalar>(liq1, liq2, soil, law),
+    eps_(1e-4)
     {
         gravity_[0] = 0;
         gravity_[1] = -9.81;
     }
 
     // indices for the two phases
-    enum {pWIdx = 0, sNIdx = 1, teIdx = 2};
+    enum
+    {   wPhase = 0, nPhase = 1, heat = 2};
 
     // function returning the BOUNDARY CONDITION TYPE depending on the position
-    virtual FieldVector<BoundaryConditions::Flags, m> bctype (const FieldVector<DT,dim>& x, const Entity& e,
-                    const IntersectionIterator& intersectionIt,
-                       const FieldVector<DT,dim>& xi) const
+    virtual FieldVector<BoundaryConditions::Flags, numEq> bctype (const GlobalPosition& globalPos, const Element& element,
+            const IntersectionIterator& intersectionIt,
+            const LocalPosition& localPos) const
     {
         // boundary condition type is set to neumann
-        FieldVector<BoundaryConditions::Flags, m> values(BoundaryConditions::neumann);
+        FieldVector<BoundaryConditions::Flags, numEq> values(BoundaryConditions::neumann);
 
-        if (x[0] < eps_)
+        if (globalPos[0] < eps_)
         {
             values = BoundaryConditions::dirichlet;
         }
-        if (x[1] < 5 && x[0] > 10 - eps_)
+        if (globalPos[1] < 5 && globalPos[0]> 10 - eps_)
         {
-            values[teIdx] = BoundaryConditions::dirichlet;
+            values[heat] = BoundaryConditions::dirichlet;
         }
         return values;
     }
 
     // definition of DIRICHLET boundary conditions depending on the position
-    virtual FieldVector<RT,m> g (const FieldVector<DT,dim>& x, const Entity& e,
-                const IntersectionIterator& intersectionIt,
-                  const FieldVector<DT,dim>& xi) const
+    virtual FieldVector<Scalar,numEq> g (const GlobalPosition& globalPos, const Element& element,
+            const IntersectionIterator& intersectionIt,
+            const LocalPosition& localPos) const
     {
-        FieldVector<RT,m> values(0);
+        FieldVector<Scalar,numEq> values(0);
 
-            values[pWIdx] = 101300 - gravity_[1] * 1066.7 * (800 - x[1]);
-            values[sNIdx] = 0;
-            values[teIdx] = 283.15 + (800 - x[1])*0.03;
+        values[wPhase] = 101300 - gravity_[1] * 1066.7 * (800 - globalPos[1]);
+        values[nPhase] = 0;
+        values[heat] = 283.15 + (800 - globalPos[1])*0.03;
 
         return values;
     }
 
     // definition of NEUMANN boundary conditions depending on the position
-    virtual FieldVector<RT,m> J (const FieldVector<DT,dim>& x, const Entity& e,
-                const IntersectionIterator& intersectionIt,
-                  const FieldVector<DT,dim>& xi) const
+    virtual FieldVector<Scalar,numEq> J (const GlobalPosition& globalPos, const Element& element,
+            const IntersectionIterator& intersectionIt,
+            const LocalPosition& localPos) const
     {
-        FieldVector<RT,m> values(0);
+        FieldVector<Scalar,numEq> values(0);
 
-        if (x[1] < 5 && x[0] > 10 - eps_) {
-            values[pWIdx] = 0.0;
-            values[sNIdx] = -0.04;
-            values[teIdx] = 0.0;
+        if (globalPos[1] < 5 && globalPos[0]> 10 - eps_)
+        {
+            values[wPhase] = 0.0;
+            values[nPhase] = -0.04;
+            values[heat] = 0.0;
         }
 
         return values;
     }
 
     // definition of INITIAL VALUES for pressure and saturation
-    virtual FieldVector<RT,m> initial (const FieldVector<DT,dim>& x, const Entity& e,
-                  const FieldVector<DT,dim>& xi) const
+    virtual FieldVector<Scalar,numEq> initial (const GlobalPosition& globalPos, const Element& element,
+            const LocalPosition& localPos) const
     {
-        FieldVector<RT,m> values;
+        FieldVector<Scalar,numEq> values;
 
-        values[pWIdx] = 101300 - gravity_[1] * 1066.7 * (800 - x[1]);
-        values[sNIdx] = 0;
-        values[teIdx] = 283.15 + (800 - x[1])*0.03;
+        values[wPhase] = 101300 - gravity_[1] * 1066.7 * (800 - globalPos[1]);
+        values[nPhase] = 0;
+        values[heat] = 283.15 + (800 - globalPos[1])*0.03;
 
         return values;
     }
-
 
     // function returning SOURCE/SINK terms
-    virtual FieldVector<RT,m> q (const FieldVector<DT,dim>& x, const Entity& e,
-                    const FieldVector<DT,dim>& xi) const
+    virtual FieldVector<Scalar,numEq> q (const GlobalPosition& globalPos, const Element& element,
+            const LocalPosition& localPos) const
     {
-        FieldVector<RT,m> values(0);
+        FieldVector<Scalar,numEq> values(0);
 
         return values;
     }
 
-    virtual FieldVector<RT,dim> gravity () const
+    virtual FieldVector<Scalar,dim> gravity () const
     {
         return gravity_;
     }
 
-    private:
-        DT eps_;
-        FieldVector<DT,dim> gravity_;
-  };
+private:
+    CoordScalar eps_;
+    FieldVector<CoordScalar,dim> gravity_;
+};
 
 } // end namespace
 #endif
