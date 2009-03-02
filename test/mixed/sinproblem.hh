5c2940b0dae66189262e0dda0b6df854280d65b5
Klaus Mosthaf <klmos@env.dtu.dk>
2009-03-02 14:19:19 +0000

    New naming convention implemented
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1326 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/mixed/sinproblem.hh
+++ test/mixed/sinproblem.hh
@@ -9,144 +9,142 @@ namespace Dune {
 
 /** \todo Please doc me! */
 
-template<class G, class RT>
-class SinProblem : public StokesProblem<G, RT>
+template<class Grid, class Scalar>
+class SinProblem : public StokesProblem<Grid, Scalar>
 {
-  typedef typename G::ctype DT;
-  enum {dim=G::dimension, m=G::dimension+1};
-  typedef typename G::Traits::template Codim<0>::Entity Entity;
-  typedef typename IntersectionIteratorGetter<G,LeafTag>::IntersectionIterator IntersectionIterator;
+    typedef typename Grid::ctype Scalar;
+    enum {dim=Grid::dimension, m=Grid::dimension+1};
+    typedef typename Grid::Traits::template Codim<0>::Entity Element;
+    typedef typename IntersectionIteratorGetter<Grid,LeafTag>::IntersectionIterator IntersectionIterator;
 
 public:
-  virtual FieldVector<RT,m> q(const FieldVector<DT,dim>& x, const Entity& e,
-                const FieldVector<DT,dim>& xi) const
-  {
-    FieldVector<RT,m> result(0);
-    result[0] = 8.0*pi*pi*cos(2.0*pi*x[0])*sin(2.0*pi*x[1]) + 4.0*pi*cos(4.0*pi*(x[0] + x[1]));
-    result[1] = -8.0*pi*pi*sin(2.0*pi*x[0])*cos(2.0*pi*x[1]) + 4.0*pi*cos(4.0*pi*(x[0] + x[1]));
+    virtual FieldVector<Scalar,m> q(const FieldVector<Scalar,dim>& globalPos, const Element& element,
+                                    const FieldVector<Scalar,dim>& localPos) const
+    {
+        FieldVector<Scalar,m> result(0);
+        result[0] = 8.0*pi*pi*cos(2.0*pi*globalPos[0])*sin(2.0*pi*globalPos[1]) + 4.0*pi*cos(4.0*pi*(globalPos[0] + globalPos[1]));
+        result[1] = -8.0*pi*pi*sin(2.0*pi*globalPos[0])*cos(2.0*pi*globalPos[1]) + 4.0*pi*cos(4.0*pi*(globalPos[0] + globalPos[1]));
 
-    return result;
-  }
+        return result;
+    }
 
-  virtual BoundaryConditions::Flags bctype (const FieldVector<DT,dim>& x, const Entity& e,
-                        const IntersectionIterator& intersectionIt,
-                        const FieldVector<DT,dim>& xi) const
-  {
-    return BoundaryConditions::dirichlet;
-  }
+    virtual BoundaryConditions::Flags bctype (const FieldVector<Scalar,dim>& globalPos, const Element& element,
+                                              const IntersectionIterator& intersectionIt,
+                                              const FieldVector<Scalar,dim>& localPos) const
+    {
+        return BoundaryConditions::dirichlet;
+    }
 
-  virtual FieldVector<RT,dim> g(const FieldVector<DT,dim>& x, const Entity& e,
-                const IntersectionIterator& intersectionIt,
-                const FieldVector<DT,dim>& xi) const
-  {
-    return velocity(x);
-  }
+    virtual FieldVector<Scalar,dim> g(const FieldVector<Scalar,dim>& globalPos, const Element& element,
+                                      const IntersectionIterator& intersectionIt,
+                                      const FieldVector<Scalar,dim>& localPos) const
+    {
+        return velocity(globalPos);
+    }
 
-  virtual RT mu(const FieldVector<DT,dim>& x, const Entity& e, const FieldVector<DT,dim>& xi) const
-  {
-    return 1.0;
-  }
+    virtual Scalar mu(const FieldVector<Scalar,dim>& globalPos, const Element& element, const FieldVector<Scalar,dim>& localPos) const
+    {
+        return 1.0;
+    }
 
-  virtual FieldVector<RT,dim> velocity(const FieldVector<DT,dim>& x) const
-  {
-    FieldVector<RT,dim> result(0);
-    result[0] = cos(2.0*pi*x[0])*sin(2.0*pi*x[1]);
-    result[1] = -sin(2.0*pi*x[0])*cos(2.0*pi*x[1]);
+    virtual FieldVector<Scalar,dim> velocity(const FieldVector<Scalar,dim>& globalPos) const
+    {
+        FieldVector<Scalar,dim> result(0);
+        result[0] = cos(2.0*pi*globalPos[0])*sin(2.0*pi*globalPos[1]);
+        result[1] = -sin(2.0*pi*globalPos[0])*cos(2.0*pi*globalPos[1]);
 
-    return result;
-  }
+        return result;
+    }
 
-  virtual RT pressure(const FieldVector<DT,dim>& x) const
-  {
-    return (sin(4.0*pi*(x[0] + x[1])));
-  }
+    virtual Scalar pressure(const FieldVector<Scalar,dim>& globalPos) const
+    {
+        return (sin(4.0*pi*(globalPos[0] + globalPos[1])));
+    }
 
-  virtual FieldMatrix<DT, dim, dim> velocityGradient(const FieldVector<DT,dim>& x) const
-  {
-    FieldMatrix<DT, dim, dim> result(0);
-    result[0][0] = -2.0*pi*sin(2.0*pi*x[0])*sin(2.0*pi*x[1]);
-    result[0][1] = 2.0*pi*cos(2.0*pi*x[0])*cos(2.0*pi*x[1]);
-    result[1][0] = -2.0*pi*cos(2.0*pi*x[0])*cos(2.0*pi*x[1]);
-    result[1][1] = 2.0*pi*sin(2.0*pi*x[0])*sin(2.0*pi*x[1]);
+    virtual FieldMatrix<Scalar, dim, dim> velocityGradient(const FieldVector<Scalar,dim>& globalPos) const
+    {
+        FieldMatrix<Scalar, dim, dim> result(0);
+        result[0][0] = -2.0*pi*sin(2.0*pi*globalPos[0])*sin(2.0*pi*globalPos[1]);
+        result[0][1] = 2.0*pi*cos(2.0*pi*globalPos[0])*cos(2.0*pi*globalPos[1]);
+        result[1][0] = -2.0*pi*cos(2.0*pi*globalPos[0])*cos(2.0*pi*globalPos[1]);
+        result[1][1] = 2.0*pi*sin(2.0*pi*globalPos[0])*sin(2.0*pi*globalPos[1]);
 
-    return result;
-  }
+        return result;
+    }
 
-  SinProblem()
-  {
-      pi = 4.0*atan(1.0);
-  }
+    SinProblem()
+    {
+        pi = 4.0*atan(1.0);
+    }
 
-  double pi;
+    double pi;
 };
 
-/** \todo Please doc me! */
-
-template<class G, class RT>
-class SinProblem2 : public StokesProblem<G, RT>
+template<class Grid, class Scalar>
+class SinProblem2 : public StokesProblem<Grid, Scalar>
 {
-  typedef typename G::ctype DT;
-  enum {dim=G::dimension, m=G::dimension+1};
-  typedef typename G::Traits::template Codim<0>::Entity Entity;
-  typedef typename IntersectionIteratorGetter<G,LeafTag>::IntersectionIterator IntersectionIterator;
+    typedef typename Grid::ctype Scalar;
+    enum {dim=Grid::dimension, m=Grid::dimension+1};
+    typedef typename Grid::Traits::template Codim<0>::Entity Element;
+    typedef typename IntersectionIteratorGetter<Grid,LeafTag>::IntersectionIterator IntersectionIterator;
 
 public:
-  virtual FieldVector<RT,m> q(const FieldVector<DT,dim>& x, const Entity& e,
-                const FieldVector<DT,dim>& xi) const
-  {
-    FieldVector<RT,m> result(0);
-    result[0] = 5.0*pi*pi*cos(2.0*pi*x[0])*sin(pi*x[1]) + 4.0*pi*cos(4.0*pi*(x[0])*x[1]);
-    result[1] = -10.0*pi*pi*sin(2.0*pi*x[0])*cos(pi*x[1]) + sin(4.0*pi*(x[0]));
+    virtual FieldVector<Scalar,m> q(const FieldVector<Scalar,dim>& globalPos, const Element& element,
+                                    const FieldVector<Scalar,dim>& localPos) const
+    {
+        FieldVector<Scalar,m> result(0);
+        result[0] = 5.0*pi*pi*cos(2.0*pi*globalPos[0])*sin(pi*globalPos[1]) + 4.0*pi*cos(4.0*pi*(globalPos[0])*globalPos[1]);
+        result[1] = -10.0*pi*pi*sin(2.0*pi*globalPos[0])*cos(pi*globalPos[1]) + sin(4.0*pi*(globalPos[0]));
 
-    return result;
-  }
+        return result;
+    }
 
-  virtual BoundaryConditions::Flags bctype (const FieldVector<DT,dim>& x, const Entity& e,
-                        const IntersectionIterator& intersectionIt,
-                        const FieldVector<DT,dim>& xi) const
-  {
-    return BoundaryConditions::dirichlet;
-  }
+    virtual BoundaryConditions::Flags bctype (const FieldVector<Scalar,dim>& globalPos, const Element& element,
+                                              const IntersectionIterator& intersectionIt,
+                                              const FieldVector<Scalar,dim>& localPos) const
+    {
+        return BoundaryConditions::dirichlet;
+    }
 
-  virtual FieldVector<RT,dim> g(const FieldVector<DT,dim>& x, const Entity& e,
-                const IntersectionIterator& intersectionIt,
-                const FieldVector<DT,dim>& xi) const
-  {
-    return velocity(x);
-  }
+    virtual FieldVector<Scalar,dim> g(const FieldVector<Scalar,dim>& globalPos, const Element& element,
+                                      const IntersectionIterator& intersectionIt,
+                                      const FieldVector<Scalar,dim>& localPos) const
+    {
+        return velocity(globalPos);
+    }
 
-  virtual RT mu(const FieldVector<DT,dim>& x, const Entity& e, const FieldVector<DT,dim>& xi) const
-  {
-    return 1.0;
-  }
+    virtual Scalar mu(const FieldVector<Scalar,dim>& globalPos, const Element& element, const FieldVector<Scalar,dim>& localPos) const
+    {
+        return 1.0;
+    }
 
-  virtual FieldVector<RT,dim> velocity(const FieldVector<DT,dim>& x) const
-  {
-    FieldVector<RT,dim> result(0);
-    result[0] = cos(2.0*pi*x[0])*sin(pi*x[1]);
-    result[1] = -2.0*sin(2.0*pi*x[0])*cos(pi*x[1]);
+    virtual FieldVector<Scalar,dim> velocity(const FieldVector<Scalar,dim>& globalPos) const
+    {
+        FieldVector<Scalar,dim> result(0);
+        result[0] = cos(2.0*pi*globalPos[0])*sin(pi*globalPos[1]);
+        result[1] = -2.0*sin(2.0*pi*globalPos[0])*cos(pi*globalPos[1]);
 
-    return result;
-  }
+        return result;
+    }
 
-  virtual RT pressure(const FieldVector<DT,dim>& x) const
-  {
-    return (sin(4.0*pi*(x[0]))*x[1]);
-  }
+    virtual Scalar pressure(const FieldVector<Scalar,dim>& globalPos) const
+    {
+        return (sin(4.0*pi*(globalPos[0]))*globalPos[1]);
+    }
 
-  virtual FieldMatrix<DT, dim, dim> velocityGradient(const FieldVector<DT,dim>& x) const
-  {
-    FieldMatrix<DT, dim, dim> result(0);
+    virtual FieldMatrix<Scalar, dim, dim> velocityGradient(const FieldVector<Scalar,dim>& globalPos) const
+    {
+        FieldMatrix<Scalar, dim, dim> result(0);
 
-    return result;
-  }
+        return result;
+    }
 
-  SinProblem2()
-  {
-      pi = 4.0*atan(1.0);
-  }
+    SinProblem2()
+    {
+        pi = 4.0*atan(1.0);
+    }
 
-  double pi;
+    double pi;
 };
 
 }
