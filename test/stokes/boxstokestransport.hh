b80af4db0e9d51de578f417cdb201d19614f946b
Iryna Rybak <iryna.rybak@mathematik.uni-stuttgart.de>
2009-03-04 16:40:21 +0000

    BoxStokesTransport implemented
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1374 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ test/stokes/boxstokestransport.hh
@@ -0,0 +1,359 @@
+#ifndef DUNE_BOXSTOKESTRANSPORT_HH
+#define DUNE_BOXSTOKESTRANSPORT_HH
+
+#include <dune/disc/shapefunctions/lagrangeshapefunctions.hh>
+#include "dumux/operators/p1operatorextended.hh"
+#include <dune/istl/io.hh>
+#include <dune/common/timer.hh>
+#include <dune/istl/bvector.hh>
+#include <dune/istl/vbvector.hh>
+#include <dune/istl/bcrsmatrix.hh>
+#include <dune/istl/io.hh>
+#include <dune/istl/gsetc.hh>
+#include <dune/istl/ilu.hh>
+#include <dune/istl/operators.hh>
+#include <dune/istl/solvers.hh>
+#include <dune/istl/preconditioners.hh>
+#include <dune/istl/scalarproducts.hh>
+#include <dune/istl/paamg/amg.hh>
+#include "dumux/nonlinear/nonlinearmodel.hh"
+#include "dumux/fvgeometry/fvelementgeometry.hh"
+#include "dumux/nonlinear/newtonmethod.hh"
+#include "boxstokesjacobian.hh"
+#include "dumux/stokes/stokesproblem.hh"
+#include "dumux/pardiso/pardiso.hh"
+
+#include "boxstokestransportjacobian.hh"
+#include "dumux/stokes/stokestransportproblem.hh"
+
+
+namespace Dune
+{
+  template<class Grid, class Scalar, class ProblemType, class LocalJacobian,
+            class FunctionType, class OperatorAssembler>
+  class BoxStokesTransport
+  : public NonlinearModel<Grid, Scalar, ProblemType, LocalJacobian, FunctionType, OperatorAssembler>
+  {
+  public:
+  typedef Dune::NonlinearModel<Grid,
+                               Scalar,
+                               ProblemType,
+                               LocalJacobian,
+                               FunctionType,
+                               OperatorAssembler> NonlinearModel;
+
+    BoxStokesTransport(const Grid& grid, ProblemType& prob)
+    : NonlinearModel(grid, prob), uOldTimeStep(grid)
+    { }
+
+    BoxStokesTransport(const Grid& grid, ProblemType& prob, int level)
+    : NonlinearModel(grid, prob, level), uOldTimeStep(grid, level)
+    {     }
+
+    virtual void initial() = 0;
+
+    virtual void update(double& dt) = 0;
+
+    virtual void solve() = 0;
+
+    virtual ~BoxStokesTransport () {}
+
+    FunctionType uOldTimeStep;
+  };
+
+  template<class Grid, class Scalar, int dim>
+  class LeafP1BoxStokesTransport : public BoxStokesTransport<Grid, Scalar, StokesTransportProblem<Grid, Scalar>, BoxStokesTransportJacobian<Grid, Scalar>,
+                                        LeafP1Function<Grid, Scalar, dim+2>, LeafP1OperatorAssembler<Grid, Scalar, dim+2> >
+  {
+  public:
+      enum{numEq = dim+2};
+
+      typedef Grid GridType;
+
+      // define the function type:
+      typedef LeafP1Function<Grid, Scalar, numEq> FunctionType;
+
+      // define the operator assembler type:
+      typedef LeafP1OperatorAssembler<Grid, Scalar, numEq> OperatorAssembler;
+
+      typedef Dune::BoxStokesTransport<Grid, Scalar, StokesTransportProblem<Grid, Scalar>, BoxStokesTransportJacobian<Grid, Scalar>,
+                              FunctionType, OperatorAssembler> BoxStokesTransport;
+
+      typedef LeafP1BoxStokesTransport<Grid, Scalar, dim> ThisType;
+
+      typedef BoxStokesTransportJacobian<Grid, Scalar> LocalJacobian;
+
+      // mapper: one data element per vertex
+      template<int dimension>
+      struct P1Layout
+      {
+          bool contains (Dune::GeometryType gt)
+          {
+              return gt.dim() == 0;
+          }
+      };
+
+      typedef typename Grid::LeafGridView GV;
+      typedef typename GV::IndexSet IS;
+      typedef MultipleCodimMultipleGeomTypeMapper<Grid,IS,P1Layout> VertexMapper;
+      typedef typename IntersectionIteratorGetter<Grid,LeafTag>::IntersectionIterator IntersectionIterator;
+      typedef typename ThisType::FunctionType::RepresentationType VectorType;
+      typedef typename ThisType::OperatorAssembler::RepresentationType MatrixType;
+      typedef MatrixAdapter<MatrixType,VectorType,VectorType> Operator;
+#ifdef HAVE_PARDISO
+//    SeqPardiso<MatrixType,VectorType,VectorType> pardiso;
+#endif
+
+      LeafP1BoxStokesTransport (const Grid& grid, StokesTransportProblem<Grid, Scalar>& prob)
+      : BoxStokesTransport(grid, prob), grid_(grid), vertexmapper(grid, grid.leafIndexSet()),
+        size((*(this->u)).size()), pressure(size), xVelocity(size), yVelocity(size),
+        partialDensity(size), uOldNewtonStep(size)
+      { }
+
+      VectorType& solOldNewtonStep()
+      {
+          return uOldNewtonStep;
+      }
+
+      virtual void initial()
+      {
+          typedef typename Grid::Traits::template Codim<0>::Entity Element;
+          typedef typename GV::template Codim<0>::Iterator Iterator;
+          enum{dimworld = Grid::dimensionworld};
+
+          const GV& gridview(this->grid_.leafView());
+          std::cout << "initializing solution." << std::endl;
+          // iterate through leaf grid an evaluate c0 at cell center
+          Iterator eendit = gridview.template end<0>();
+          for (Iterator it = gridview.template begin<0>(); it != eendit; ++it)
+          {
+              // get geometry type
+              Dune::GeometryType gt = it->geometry().type();
+
+              // get entity
+              const Element& entity = *it;
+
+              const typename Dune::LagrangeShapeFunctionSetContainer<Scalar,Scalar,dim>::value_type&
+                  sfs=Dune::LagrangeShapeFunctions<Scalar,Scalar,dim>::general(gt, 1);
+              int size = sfs.size();
+
+              IntersectionIterator is = IntersectionIteratorGetter<Grid,LeafTag>::end(entity);
+
+              for (int i = 0; i < size; i++)
+              {
+            	  // get cell center in reference element
+            	  const Dune::FieldVector<Scalar,dim>&local = sfs[i].position();
+
+            	  // get global coordinate of cell center
+            	  Dune::FieldVector<Scalar,dimworld> global = it->geometry().global(local);
+
+            	  int globalId = vertexmapper.template map<dim>(entity, sfs[i].entity());
+
+            	  // initialize cell concentration
+            	  (*(this->u))[globalId] = this->problem.initial(global, entity, local); //0;
+              }
+          }
+
+          // set Dirichlet boundary conditions
+          for (Iterator it = gridview.template begin<0>(); it != eendit; ++it)
+          {
+              // get geometry type
+              Dune::GeometryType gt = it->geometry().type();
+
+              // get entity
+              const Element& entity = *it;
+
+              const typename Dune::LagrangeShapeFunctionSetContainer<Scalar,Scalar,dim>::value_type&
+                  sfs=Dune::LagrangeShapeFunctions<Scalar,Scalar,dim>::general(gt, 1);
+              int size = sfs.size();
+
+              // set type of boundary conditions
+              this->localJacobian().fvGeom.update(entity);
+              this->localJacobian().template assembleBC<LeafTag>(entity);
+
+              IntersectionIterator endit = IntersectionIteratorGetter<Grid,LeafTag>::end(entity);
+              for (IntersectionIterator is = IntersectionIteratorGetter<Grid,LeafTag>::begin(entity);
+                   is!=endit; ++is)
+                  if (is->boundary())
+                  {
+                    for (int i = 0; i < size; i++)
+                      // handle subentities of this face
+                      for (int j = 0; j < ReferenceElements<Scalar,dim>::general(gt).size(is->numberInSelf(), 1, sfs[i].codim()); j++)
+                        if (sfs[i].entity() == ReferenceElements<Scalar,dim>::general(gt).subEntity(is->numberInSelf(), 1, j, sfs[i].codim()))
+                        {
+                            if (this->localJacobian().bc(i)[1] == BoundaryConditions::dirichlet)
+                            {
+                                // get cell center in reference element
+                                Dune::FieldVector<Scalar,dim> local = sfs[i].position();
+
+                                // get global coordinate of cell center
+                                Dune::FieldVector<Scalar,dimworld> global = it->geometry().global(local);
+
+                                int globalId = vertexmapper.template map<dim>(entity, sfs[i].entity());
+
+                                BoundaryConditions::Flags bctype = this->problem.bctype(global, entity, is, local);
+                                if (bctype == BoundaryConditions::dirichlet) {
+                                	FieldVector<Scalar,dim+1> dirichlet = this->problem.g(global, entity, is, local);
+                                	for (int eq = 0; eq < dim+1; eq++)
+                                		(*(this->u))[globalId][eq] = dirichlet[eq];
+                                }
+                                else {
+                                    std::cout << global << " is considered to be a Neumann node." << std::endl;
+                                }
+                            }
+                        }
+              }
+          }
+
+          *(this->uOldTimeStep) = *(this->u);
+          return;
+      }
+
+
+      virtual void assemble()
+      {
+          MatrixType& A = *(this->A);
+          *(this->f) = 0;
+          this->localJacobian().clearVisited();
+          this->A.assemble(this->localJacobian(), this->u, this->f);
+
+          //modify matrix for introducing pressure boundary condition
+          const GV& gridview(this->grid_.leafView());
+          typedef typename GV::template Codim<0>::Iterator Iterator;
+
+          Iterator it = gridview.template begin<0>();
+          unsigned int globalId = vertexmapper.template map<dim>(*it, 3);
+
+          for (typename MatrixType::RowIterator i=A.begin(); i!=A.end(); ++i)
+              if(i.index()==globalId)
+                  for (typename MatrixType::ColIterator j=(*i).begin(); j!=(*i).end(); ++j)
+                         A[i.index()][j.index()][dim+1] = 0.0;
+          A[globalId][globalId][dim+1][dim+1] = 1.0;
+          (*(this->f))[globalId][dim+1] = 0.0;
+      }
+
+    virtual void update(double& dt)
+    {
+        this->localJacobian().setDt(dt);
+        this->localJacobian().setOldSolution(this->uOldTimeStep);
+        double dtol = 1e-8;
+        double rtol = 1e7;
+        int maxIt = 10;
+        double mindt = 1e-5;
+        int goodIt = 4;
+        int maxInc = 2;
+        NewtonMethod<Grid, ThisType> newtonMethod(this->grid_, *this,
+        		dtol, rtol, maxIt, mindt, goodIt, maxInc);
+        newtonMethod.execute();
+        dt = this->localJacobian().getDt();
+        *(this->uOldTimeStep) = *(this->u);
+
+        return;
+    }
+
+    virtual void solve()
+    {
+    	MatrixType& A = *(this->A);
+        Operator op(A);  // make operator out of matrix
+        double red=1E-18;
+
+#ifdef HAVE_PARDISO
+        SeqPardiso<MatrixType,VectorType,VectorType> pardiso(A);
+        LoopSolver<VectorType> solver(op, pardiso, red, 10, 2);
+#else
+        SeqILU0<MatrixType,VectorType,VectorType> ilu0(A,1.0);// a precondtioner
+        BiCGSTABSolver<VectorType> solver(op,ilu0,red,10000,1);         // an inverse operator
+#endif
+        InverseOperatorResult r;
+        solver.apply(*(this->u), *(this->f), r);
+
+        return;
+    }
+
+    virtual void globalDefect(FunctionType& defectGlobal) {
+        typedef typename Grid::Traits::template Codim<0>::Entity Element;
+        typedef typename GV::template Codim<0>::Iterator Iterator;
+        typedef typename BoundaryConditions::Flags BCBlockType;
+
+        const GV& gridview(this->grid_.leafView());
+        (*defectGlobal)=0;
+
+        // allocate flag vector to hold flags for essential boundary conditions
+        std::vector<BCBlockType> essential(this->vertexmapper.size());
+        for (typename std::vector<BCBlockType>::size_type i=0; i
+                <essential.size(); i++)
+            essential[i] = BoundaryConditions::neumann;
+
+        // iterate through leaf grid
+        Iterator eendit = gridview.template end<0>();
+        for (Iterator it = gridview.template begin<0>(); it
+                != eendit; ++it) {
+            // get geometry type
+            Dune::GeometryType gt = it->geometry().type();
+
+            // get entity
+            const Element& entity = *it;
+            this->localJacobian().fvGeom.update(entity);
+            int size = this->localJacobian().fvGeom.numVertices;
+
+            this->localJacobian().setLocalSolution(entity);
+            this->localJacobian().computeElementData(entity);
+            this->localJacobian().updateVariableData(entity, this->localJacobian().u);
+            this->localJacobian().template localDefect<LeafTag>(entity, this->localJacobian().u);
+
+            // begin loop over vertices
+            for (int i=0; i < size; i++) {
+                int globalId = this->vertexmapper.template map<dim>(entity,i);
+                for (int equationnumber = 0; equationnumber < numEq; equationnumber++) {
+                    if (this->localJacobian().bc(i)[equationnumber] == BoundaryConditions::neumann)
+                        (*defectGlobal)[globalId][equationnumber]
+                                += this->localJacobian().def[i][equationnumber];
+                    else
+                        essential[globalId] = BoundaryConditions::dirichlet;
+                }
+            }
+        }
+
+        for (typename std::vector<BCBlockType>::size_type i=0; i<essential.size(); i++)
+            if (essential[i] == BoundaryConditions::dirichlet)
+            	for (int equationnumber = 0; equationnumber < dim+1; equationnumber++)
+            		(*defectGlobal)[i][equationnumber] = 0;
+    }
+
+    virtual void vtkout (const char* name, int k)
+    {
+        for (int i = 0; i < size; i++) {
+            pressure[i] = (*(this->u))[i][dim+1];
+            xVelocity[i] = (*(this->u))[i][0];
+            yVelocity[i] = (*(this->u))[i][1];
+            partialDensity[i]  = (*(this->u))[i][dim];
+        }
+
+    	VTKWriter<typename Grid::LeafGridView> vtkwriter(this->grid_.leafView());
+        vtkwriter.addVertexData(pressure,"pressure");
+        vtkwriter.addVertexData(xVelocity,"xVelocity");
+        vtkwriter.addVertexData(yVelocity,"yVelocity");
+        vtkwriter.addVertexData(partialDensity,"partialDensity");
+        char fname[128];
+        sprintf(fname, "%s-%05d", name, k);
+        vtkwriter.write(fname, VTKOptions::ascii);
+    }
+
+    const Grid& grid() const
+        { return grid_; }
+
+
+
+protected:
+  const Grid& grid_;
+  VertexMapper vertexmapper;
+  int size;
+  BlockVector<FieldVector<Scalar, 1> > pressure;
+  BlockVector<FieldVector<Scalar, 1> > xVelocity;
+  BlockVector<FieldVector<Scalar, 1> > yVelocity;
+  BlockVector<FieldVector<Scalar, 1> > partialDensity;
+  VectorType uOldNewtonStep;
+};
+
+}
+#endif
--- test/stokes/test_boxstokestransport.cc
+++ test/stokes/test_boxstokestransport.cc
@@ -17,7 +17,12 @@
 #include "yxproblem.hh"
 #include "sinproblem.hh"
 #include "curlproblem.hh"
-#include "analyticstokesproblem.hh"
+#include "dumux/material/phaseproperties/phaseproperties_waterair.hh"
+#include "dumux/material/multicomponentrelations.hh"
+
+#include "boxstokestransport.hh"
+#include "analyticproblem.hh"
+#include "transportproblem.hh"
 
 template<int dim>
 struct VertexLayout
@@ -53,9 +58,10 @@ void calculateError(const Grid& grid, const Problem& problem, Vector& solution)
     ElementMapper elementMapper(grid, grid.leafView().indexSet());
 
     Scalar errPressure = 0;
-    Scalar errVelocityX = 0;
-    Scalar errVelocityY = 0;
+    Scalar errVelocity = 0;
     Scalar constant = 0;
+    Scalar errPartialDensity = 0;
+
     ElementIterator endEIt = grid.template leafend<0>();
     for (ElementIterator eIt = grid.template leafbegin<0>(); eIt != endEIt; ++eIt)
     {
@@ -67,93 +73,106 @@ void calculateError(const Grid& grid, const Problem& problem, Vector& solution)
         Dune::FieldVector<Scalar,dim> global = element.geometry().global(local);
 
         Scalar volume = element.geometry().integrationElement(local)
-            *Dune::ReferenceElements<Scalar,dim>::general(geomType).volume();
+                *Dune::ReferenceElements<Scalar,dim>::general(geomType).volume();
 
         int eIdx = elementMapper.map(element);
 
-        Scalar approxPressure = solution.evallocal (2, element, local);
+        Scalar approxPressure = solution.evallocal (3, element, local);
         Scalar exactPressure = problem.pressure(global);
         if (eIdx == 0)
             constant = exactPressure - approxPressure;
         approxPressure += constant;
         errPressure += volume*(approxPressure - exactPressure)*(approxPressure - exactPressure);
 
+        //transport
+        Scalar approxPartialDensity = solution.evallocal (2, element, local);
+        Scalar exactPartialDensity = problem.partialdensity(global);
+        errPartialDensity += volume*(approxPartialDensity - exactPartialDensity)*(approxPartialDensity - exactPartialDensity);
+
         Scalar approxXV = solution.evallocal (0, element, local);
         Scalar exactXV = problem.velocity(global)[0];
-        errVelocityX += volume*(approxXV - exactXV)*(approxXV - exactXV);
+        errVelocity += volume*(approxXV - exactXV)*(approxXV - exactXV);
 
         Scalar approxYV = solution.evallocal (1, element, local);
         Scalar exactYV = problem.velocity(global)[1];
-        errVelocityY += volume*(approxYV - exactYV)*(approxYV - exactYV);
+        errVelocity += volume*(approxYV - exactYV)*(approxYV - exactYV);
     }
 
     errPressure = sqrt(errPressure);
-    errVelocityX = sqrt(errVelocityX);
-    errVelocityY = sqrt(errVelocityY);
+    errVelocity = sqrt(errVelocity);
+    errPartialDensity = sqrt(errPartialDensity);
 
-    std::cout << "error in discrete L2 norm:\npressure: " << errPressure
-              << "\nx-velocity: " << errVelocityX << "\ny-velocity: " << errVelocityY << std::endl;
+    std::cout << "Error in discrete L2 norm:\nPressure: " << errPressure << "\nVelocity: " << errVelocity << "\nPdensity: " << errPartialDensity << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-    try{
-        // define the problem dimensions
-        const int dim = 2;
+  try{
+    // define the problem dimensions
+    const int dim = 2;
+    double tStart = 0;
+    double tEnd = 50;
+    double dt = 10;
+    int modulo = 1; // print every modulo'th time step
 
-        // create a grid object
-        typedef double NumberType;
-        typedef Dune::SGrid<dim,dim> GridType;
-        //typedef Dune::AlbertaGrid<dim,dim> GridType;
+    // create a grid object
+    typedef double NumberType;
+    typedef Dune::SGrid<dim,dim> GridType;
+    //typedef Dune::AlbertaGrid<dim,dim> GridType;
 
-        if (argc != 2 && argc != 3) {
-            std::cout << "Usage: test_boxstokes dgffilename [refinementsteps]" << std::endl;
-            return (1);
-        }
-        int refinementSteps = 0;
-        if (argc == 3) {
-            std::string arg2(argv[2]);
-            std::istringstream is2(arg2);
-            is2 >> refinementSteps;
-        }
-
-        Dune::GridPtr<GridType> gridPtr( argv[1] );
-        GridType& grid = *gridPtr;
-
-        if (refinementSteps)
-            grid.globalRefine(refinementSteps);
-
-        Dune::YXProblem<GridType, double> problem;
-        typedef Dune::LeafP1BoxStokes<GridType, NumberType, dim> BoxStokes;
-        BoxStokes boxStokes(grid, problem);
-
-        Dune::TimeLoop<GridType, BoxStokes> timeloop(0, 1, 1, "test_boxstokes", 1);
-
-        timeloop.execute(boxStokes);
-
-        //printvector(std::cout, *(boxStokes.u), "solution", "row", 200, 1, 3);
-        calculateError(grid, problem, boxStokes.u);
-
-        return 0;
+    if (argc != 2 && argc != 3) {
+        std::cout << "Usage: test_boxstokestransport dgffilename [refinementsteps]" << std::endl;
+        return (1);
     }
-    catch (Dune::Exception &e){
-        std::cerr << "Dune reported error: " << e << std::endl;
+    int refinementSteps = 0;
+    if (argc == 3) {
+        std::string arg2(argv[2]);
+        std::istringstream is2(arg2);
+        is2 >> refinementSteps;
     }
-    catch (...){
-        std::cerr << "Unknown exception thrown!" << std::endl;
-    }
+
+    Dune::GridPtr<GridType> gridPtr( argv[1] );
+    GridType& grid = *gridPtr;
+
+    if (refinementSteps)
+        grid.globalRefine(refinementSteps);
+
+    Dune::Liq_WaterAir dummyPhase;
+    Dune::Gas_WaterAir gasPhase;
+
+    Dune::CWaterAir multicomp(dummyPhase, gasPhase);
+
+    Dune::TransportProblem<GridType, double> problem(gasPhase, multicomp);
+    typedef Dune::LeafP1BoxStokesTransport<GridType, NumberType, dim> BoxStokesTransport;
+    BoxStokesTransport boxStokesTransport(grid, problem);
+
+    Dune::TimeLoop<GridType, BoxStokesTransport> timeloop(tStart, tEnd, dt, "test_boxstokestransport", modulo);
+
+    timeloop.execute(boxStokesTransport);
+
+    //printvector(std::cout, *(boxStokes.u), "solution", "row", 200, 1, 3);
+    //calculateError(grid, problem, boxStokesTransport.u);
+
+    return 0;
+  }
+  catch (Dune::Exception &e){
+    std::cerr << "Dune reported error: " << e << std::endl;
+  }
+  catch (...){
+    std::cerr << "Unknown exception thrown!" << std::endl;
+  }
 }
 #else
 
 int main (int argc , char **argv) try
- {
-     std::cout << "This test is not finished yet." << std::endl;
+{
+  std::cout << "This test is not finished yet." << std::endl;
 
-     return 1;
- }
- catch (...)
- {
-     std::cerr << "Generic exception!" << std::endl;
-     return 2;
- }
+  return 1;
+}
+catch (...)
+{
+    std::cerr << "Generic exception!" << std::endl;
+    return 2;
+}
 #endif
