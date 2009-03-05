bf9065a5309d2c5fc5e8b4172f80ab98e7902cca
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2009-03-05 13:04:19 +0000

    bug fixed in csp2dgf.c
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1383 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/io/csp2dgf.c
+++ dumux/io/csp2dgf.c
@@ -47,15 +47,15 @@ int main (int argc, char **argv)
   strcat(ascFileName, ".asc");
 
   // test dimension
-  FILE *ascFile; 
+  FILE *ascFile;
   if((ascFile=fopen(ascFileName, "r"))==NULL) {
     printf("ERROR: %s doesn't exist\n",ascFileName);
     return 1;
   }
-  
-  char testString[120]; 
+
+  char testString[120];
   int triDim=0;
- 
+
   char dgfFileName[120];
   strcpy(dgfFileName, baseFileName);
   strcat(dgfFileName, ".dgf");
@@ -64,14 +64,14 @@ int main (int argc, char **argv)
   char datFileName[120];
   strcpy(datFileName, baseFileName);
   strcat(datFileName, ".dat");
- 
-  FILE *datFile; 
+
+  FILE *datFile;
   if((datFile = fopen(datFileName, "r"))==NULL) {
     printf("ERROR: %s doesn't exist\n",datFileName);
     return 1;
   }
 
-  int numberOfNodes;  
+  int numberOfNodes;
   fscanf(datFile,"%d",&numberOfNodes);
   fgets(testString,120,datFile);
 
@@ -87,7 +87,7 @@ int main (int argc, char **argv)
     if(nodes[i].z!=0)
       triDim=1;
   }
- 
+
   fscanf(datFile,"%s %s",&testString,&testString);
   for(i=0;i<numberOfNodes;++i)
     fscanf(datFile,"%d",&nodes[i].pbFlag);
@@ -105,10 +105,10 @@ int main (int argc, char **argv)
   if(triDim)
     for(i=0;i<numberOfNodes;++i)
       fprintf(dgfFile, "%14.7e %14.7e %14.7e %d %14.7e \% % %d\n", nodes[i].x, nodes[i].y, nodes[i].z, nodes[i].pbFlag, nodes[i].pbVal,i+1);
-  else 
+  else
     for(i=0;i<numberOfNodes;++i)
       fprintf(dgfFile, "%14.7e %14.7e %d %14.7e \% % %d\n", nodes[i].x, nodes[i].y, nodes[i].pbFlag, nodes[i].pbVal, i);
-   
+
 
 
   // input element types and node ids
@@ -121,7 +121,7 @@ int main (int argc, char **argv)
   elements = malloc(numberOfElements*sizeof(*elements));
   for(i=0;i<numberOfElements;++i)
     fscanf(datFile,"%d", &elements[i].finiteElementType);
-  
+
   int ifSimplex=0;
   for(i=0;i<numberOfElements;++i){
     switch(elements[i].finiteElementType){
@@ -177,20 +177,20 @@ int main (int argc, char **argv)
       fclose(ascFile);
       fclose(datFile);
       return 1;
-    } 
+    }
   }
 
   if(triDim)
     if (ifSimplex)
       fprintf(dgfFile,"#\nSIMPLEX\nparameters 1\n");
-    else 
+    else
       fprintf(dgfFile,"#\nCUBE\nparameters 1\nmap 0 1 3 2 4 5 7 6\n");
-  else 
+  else
     if(ifSimplex)
       fprintf(dgfFile,"#\nSIMPLEX\nparameters 1\n");
     else
-      fprintf(dgfFile,"#\nCUBE\nparameters 1\nmap 2 1 3 0\n");
-    
+      fprintf(dgfFile,"#\nCUBE\nparameters 1\nmap 3 2 0 1\n");
+
   for(i=0;i<numberOfElements;++i)
     elements[i].nodes=calloc(elements[i].anz,sizeof(int));
 
@@ -201,7 +201,7 @@ int main (int argc, char **argv)
   for(i=0;i<numberOfElements;++i)
     for(j=0;j<elements[i].anz;++j)
       fscanf(datFile,"%d",&elements[i].nodes[j]);
-      
+
   do {
     fscanf(datFile,"%s",testString);
   } while(strcmp(testString,"PMATERIAL"));
@@ -235,7 +235,7 @@ int main (int argc, char **argv)
 
       if (!boundaryStarted && !elements[index].isVolume) {
 	boundaryStarted = 1;
-	fprintf(dgfFile,"#\nBOUNDARYSEGMENTS\n"); 
+	fprintf(dgfFile,"#\nBOUNDARYSEGMENTS\n");
 	printf("  volume elements: %d\n", count);
 	count = 0;
       }
@@ -246,7 +246,7 @@ int main (int argc, char **argv)
       }
 
       if (boundaryStarted && elements[index].isFace) {
-	fprintf(dgfFile,"%7d ",elements[index].materialType); 
+	fprintf(dgfFile,"%7d ",elements[index].materialType);
 	for(k=0;k<elements[index].anz;++k)
 	  fprintf(dgfFile,"%7d ",elements[index].nodes[k]);
 	fprintf(dgfFile,"\n");
