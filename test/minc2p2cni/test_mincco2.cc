fb2575662eb59ef1e22eee1d785e9171a1434904
Alexandru Tatomir <atatomi@gwdg.de>
2009-03-05 08:26:10 +0000

    MINC method applied to the 2p2cni model
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1375 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/minc2p2cni/test_mincco2.cc
+++ test/minc2p2cni/test_mincco2.cc
@@ -28,98 +28,96 @@
 #include "dumux/io/readstarformat.cc"
 #include "dumux/io/vtkmultiwriter.hh"
 
-#define NUM_CONTINUA 3
+#define NUM_CONTINUA 4 
 #define NUM_EQUATIONS (NUM_CONTINUA*3)
 
 int main(int argc, char** argv)
 {
-    try{
-        // define the problem dimensions
-        const int dim=2;
-        typedef double NumberType;
-        double depthBOR = 800.0;  // bottom of reservoir
-        Dune::FieldVector<NumberType, dim> outerLowerLeft(0);
-        Dune::FieldVector<NumberType, dim> outerUpperRight(60);
-        outerUpperRight[1] = 40;
+  try{
+    // define the problem dimensions
+    const int dim=2;
+    typedef double NumberType;
+    double depthBOR = 800.0;  // bottom of reservoir
+    Dune::FieldVector<NumberType, dim> outerLowerLeft(0);
+    Dune::FieldVector<NumberType, dim> outerUpperRight(60);
+    outerUpperRight[1] = 40;
 
-        if (argc != 4) {
-            std::cout << "usage: minc2p2cni basefilename tEnd dt" << std::endl;
-            return 0;
-        }
-        std::string arg1(argv[2]);
-        std::istringstream is1(arg1);
-        double tEnd;
-        is1 >> tEnd;
-        std::string arg2(argv[3]);
-        std::istringstream is2(arg2);
-        double dt;
-        is2 >> dt;
-
-        // create a grid object
-        typedef Dune::SGrid<dim,dim> GridType;
-        //typedef Dune::YaspGrid<dim,dim> GridType;
-        //typedef Dune::UGGrid<dim> GridType;
-        //        typedef Dune::ALUSimplexGrid<dim,dim> GridType;
-
-        Dune::GridPtr<GridType> gridPointer(argv[1]);
-        GridType& grid = *gridPointer;
-        //readStarFormat(grid, argv[1]);
-        //grid.createLGMGrid(argv[1]);
-
-        Dune::gridinfo(grid);
-
-        // choose fluids and properties
-        Dune::Liq_BrineCO2 wPhase;
-        Dune::Gas_BrineCO2 nPhase;
-        Dune::MincCO2Soil<GridType, NumberType> soil;
-
-        Dune::TwoPhaseRelations<GridType, NumberType>
-            materialLaw(soil, wPhase, nPhase);
-
-        Dune::CBrineCO2 multicomp(wPhase, nPhase);
-
-        Dune::BrineCO2Problem<GridType, NumberType, NUM_EQUATIONS> problem(wPhase, nPhase, soil,
-                                                                           materialLaw, multicomp, depthBOR);
-
-        //    Dune::BrineCO2Problem<GridType, NumberType> problem(wPhase, nPhase, soil,
-        //             materialLaw, multicomp, depthBOR);
-
-        typedef Dune::VtkMultiWriter<GridType::LeafGridView> MultiWriter;
-        typedef Dune::BoxMincCO2<GridType, NumberType, NUM_EQUATIONS, MultiWriter> MincCO2;
-        MincCO2 mincco2(grid, problem);
-
-        Dune::TimeLoop<GridType, MincCO2, true> timeloop(0, tEnd, dt, "co2", 1);
-
-        Dune::Timer timer;
-        timer.reset();
-        MultiWriter writer("co2");
-
-        //timeloop.execute(twoPhase);
-        timeloop.executeMultiWriter(mincco2, writer);
-        std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
-
-
-        return 0;
+    if (argc != 4) {
+      std::cout << "usage: minc2p2cni basefilename tEnd dt" << std::endl;
+      return 0;
     }
-    catch (Dune::Exception &e){
-        std::cerr << "Dune reported error: " << e << std::endl;
-    }
-    catch (...){
-        std::cerr << "Unknown exception thrown!" << std::endl;
-    }
-}
-#else
+    std::string arg1(argv[2]);
+	std::istringstream is1(arg1);
+	double tEnd;
+		is1 >> tEnd;
+		std::string arg2(argv[3]);
+		std::istringstream is2(arg2);
+		double dt;
+		is2 >> dt;
 
-int main (int argc , char **argv) try
- {
-     //std::cout << "Please install the UG library." << std::endl;
-     std::cout << "Dummy implementation, this test would not compile at the moment." << std::endl;
+	    // create a grid object
+	    typedef Dune::SGrid<dim,dim> GridType;
+	    //typedef Dune::YaspGrid<dim,dim> GridType;
+	    //typedef Dune::UGGrid<dim> GridType;
+//		typedef Dune::ALUSimplexGrid<dim,dim> GridType;
 
-     return 1;
- }
- catch (...)
- {
-     std::cerr << "Generic exception!" << std::endl;
-     return 2;
- }
-#endif
+	    Dune::GridPtr<GridType> gridPointer(argv[1]);
+	    GridType& grid = *gridPointer;
+	    //readStarFormat(grid, argv[1]);
+	    //grid.createLGMGrid(argv[1]);
+
+	     Dune::gridinfo(grid);
+
+	     // choose fluids and properties
+	     Dune::Liq_BrineCO2 wPhase;
+	     Dune::Gas_BrineCO2 nPhase;
+	     Dune::MincCO2Soil<GridType, NumberType> soil;
+
+	     Dune::TwoPhaseRelations<GridType, NumberType>
+	     materialLaw(soil, wPhase, nPhase);
+
+	     Dune::CBrineCO2 multicomp(wPhase, nPhase);
+
+	     Dune::BrineCO2Problem<GridType, NumberType, NUM_EQUATIONS> problem(wPhase, nPhase, soil,
+			 materialLaw, multicomp, depthBOR);
+
+
+	     typedef Dune::VtkMultiWriter<GridType::LeafGridView> MultiWriter;
+	     typedef Dune::BoxMincCO2<GridType, NumberType, NUM_EQUATIONS, MultiWriter> MincCO2;
+	     MincCO2 mincco2(grid, problem);
+
+	      Dune::TimeLoop<GridType, MincCO2, true> timeloop(0, tEnd, dt, "co2", 1);
+
+	      Dune::Timer timer;
+	      timer.reset();
+	      MultiWriter writer("co2");
+
+		 //timeloop.execute(twoPhase);
+	      timeloop.executeMultiWriter(mincco2, writer);
+	      std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
+
+
+	    return 0;
+	  }
+	  catch (Dune::Exception &e){
+	    std::cerr << "Dune reported error: " << e << std::endl;
+	  }
+	  catch (...){
+	    std::cerr << "Unknown exception thrown!" << std::endl;
+	  }
+	}
+	#else
+
+	int main (int argc , char **argv) try
+	{
+	  //std::cout << "Please install the UG library." << std::endl;
+	  std::cout << "Dummy implementation, this test would not compile at the moment." << std::endl;
+
+	  return 1;
+	}
+	catch (...)
+	{
+	    std::cerr << "Generic exception!" << std::endl;
+	    return 2;
+	}
+	#endif
