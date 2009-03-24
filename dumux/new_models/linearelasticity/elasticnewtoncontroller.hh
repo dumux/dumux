51ea7ad403db43eccfc6cc3a26fb1e126df7ff10
Melanie Darcis <mdarcis@gmx.de>
2009-03-24 14:02:12 +0000

    first version of a very simple model that describes linear elastic deformations
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1491 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ dumux/new_models/linearelasticity/elasticnewtoncontroller.hh
@@ -0,0 +1,111 @@
+//$Id$
+
+/*****************************************************************************
+ *   Copyright (C) 2008 by Bernd Flemisch, Andreas Lauser                    *
+ *   Institute of Hydraulic Engineering                                      *
+ *   University of Stuttgart, Germany                                        *
+ *   email: and _at_ poware.org                                              *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version, as long as this copyright notice    *
+ *   is included in its original form.                                       *
+ *                                                                           *
+ *   This program is distributed WITHOUT ANY WARRANTY.                       *
+ *****************************************************************************/
+/*!
+ * \file
+ * \brief An elastic deformation specific controller for the newton solver.
+ *
+ * This controller 'knows' what a 'physically meaningful' solution is
+ * which allows the newton method to abort quicker if the solution is
+ * way out of bounds.
+ */
+#ifndef DUMUX_ELASTIC_NEWTON_CONTROLLER_HH
+#define DUMUX_ELASTIC_NEWTON_CONTROLLER_HH
+
+#include <dumux/nonlinear/new_newtoncontroller.hh>
+
+namespace Dune
+{
+/*!
+ * \brief An elastic deformation specific controller for the newton solver.
+ *
+ * This controller 'knows' what a 'physically meaningful' solution is
+ * which allows the newton method to abort quicker if the solution is
+ * way out of bounds.
+ */
+template<class NewtonMethod>
+class ElasticNewtonController: public NewtonControllerBase<NewtonMethod,
+        ElasticNewtonController<NewtonMethod> >
+{
+typedef        typename NewtonMethod::Model Model;
+        typedef typename Model::ElasticTraits ElasticTraits;
+
+        enum
+        {
+            uxIdx = ElasticTraits::uxIdx,
+            uyIdx = ElasticTraits::uyIdx,
+            uzIdx = ElasticTraits::uzIdx
+        };
+
+        public:
+        typedef ElasticNewtonController<NewtonMethod> ThisType;
+        typedef NewtonControllerBase<NewtonMethod, ThisType> ParentType;
+
+        typedef typename ParentType::Scalar Scalar;
+        typedef typename ParentType::Function Function;
+        typedef typename ParentType::JacobianAssembler JacobianAssembler;
+
+        ElasticNewtonController(Scalar tolerance = 1e-7,
+                int targetSteps = 9,
+                int maxSteps = 18)
+        : ParentType(tolerance, targetSteps, maxSteps)
+        {};
+
+        //! Suggest a new time stepsize based either on the number of newton
+        //! iterations required or on the variable switch
+        void newtonEndStep(Function &u, Function &uOld)
+        {
+            // call the method of the base class
+            ParentType::model().localJacobian().updateStaticData(u, uOld);
+            ParentType::newtonEndStep(u, uOld);
+        }
+
+        //! Suggest a new time stepsize based either on the number of newton
+        //! iterations required
+        Scalar suggestTimeStepSize(Scalar oldTimeStep) const
+        {
+            // use function of the newtoncontroller
+            return ParentType::suggestTimeStepSize(oldTimeStep);
+        }
+
+        //! Returns true iff the current solution can be considered to
+        //! be acurate enough
+        bool newtonConverged()
+        {
+            return ParentType::newtonConverged();
+        };
+
+        //! Returns true iff another iteration should be done.
+        bool newtonProceed(Function &u)
+        {
+            return ParentType::newtonProceed(u);
+        }
+
+        /** \todo Please doc me! */
+
+        protected:
+        friend class NewtonControllerBase<NewtonMethod, ThisType>;
+        //! called by the base class the get an indication of how physical
+        //! an iterative solution is 1 means "completely physical", 0 means
+        //! "completely unphysical"
+        Scalar physicalness_(Function &u)
+        {
+            return 1;
+        }
+    };
+}
+
+#endif
