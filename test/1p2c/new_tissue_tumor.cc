898d9e5a939294c91131963a3dc900f3b85e99cf
Karin Erbertseder <k.erbertseder@web.de>
2009-04-08 14:16:45 +0000

    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1566 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ test/1p2c/new_tissue_tumor.cc
@@ -0,0 +1,96 @@
+// $Id$
+
+#include "config.h"
+
+#include "new_tissue_tumor_problem.hh"
+
+
+//#include <dune/grid/yaspgrid.hh>
+#include<dune/grid/uggrid.hh>
+//#include<dune/grid/sgrid.hh>
+
+#include<dune/grid/common/grid.hh>
+#include <dune/grid/io/file/dgfparser/dgfug.hh>
+#include <dune/grid/common/gridinfo.hh>
+
+
+#include <dune/common/exceptions.hh>
+//#include <dune/common/mpihelper.hh>
+
+#include <iostream>
+#include <boost/format.hpp>
+
+int main(int argc, char** argv)
+{
+    try {
+        // Set the type for scalar values (should be one of float, double
+        // or long double)
+        const int dim = 2;
+        typedef double                                   Scalar;
+
+
+        //create a grid object
+
+        //        typedef Dune::ALUSimplexGrid<dim, dim>           Grid;
+        //        typedef Dune::YaspGrid<dim>                      Grid;
+        typedef Dune::UGGrid<dim>                        Grid;
+        //        typedef Dune::SGrid<dim>                        Grid;
+
+        typedef Dune::NewTissueTumorProblem<Grid, Scalar>  Problem;
+        typedef Problem::DomainTraits::GlobalPosition    GlobalPosition;
+        typedef Dune::GridPtr<Grid>                      GridPointer;
+
+        // initialize MPI, finalize is done automatically on exit
+        //Dune::MPIHelper::instance(argc, argv);
+
+        // parse the command line arguments for the program
+        if (argc != 4) {
+            std::cout << boost::format("usage: %s grid tEnd dt\n")%argv[0];
+            return 1;
+        }
+        double tEnd, dt;
+        const char *dgfFileName = argv[1];
+        std::istringstream(argv[2]) >> tEnd;
+        std::istringstream(argv[3]) >> dt;
+
+        // create grid
+
+        /*
+          GlobalPosition upperRight;
+          Dune::FieldVector<int,dim> res; // cell resolution
+          upperRight[0] = 60.0;
+          res[0]        = 24;
+
+          upperRight[1] = 40.0;
+          res[1]        = 16;
+
+          Grid grid(
+          #ifdef HAVE_MPI
+          Dune::MPIHelper::getCommunicator(),
+          #endif
+          upperRight, // upper right
+          res, // number of cells
+          Dune::FieldVector<bool,dim>(false), // periodic
+          2); // overlap
+        */
+
+        // load the grid from file
+        GridPointer gridPtr =  GridPointer(dgfFileName);
+        Dune::gridinfo(*gridPtr);
+
+
+        // instantiate and run the concrete problem
+        Problem problem(&(*gridPtr), dt, tEnd);
+        if (!problem.simulate())
+            return 2;
+
+        return 0;
+    }
+    catch (Dune::Exception &e) {
+        std::cerr << "Dune reported error: " << e << std::endl;
+    }
+    catch (...) {
+        std::cerr << "Unknown exception thrown!\n";
+    }
+    return 3;
+}
