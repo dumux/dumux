0aea40cd17e4875a2f0571e023b75b577791c0de
Anneli Schöniger <anneli.schoeniger@bosscon.de>
2009-05-12 13:29:45 +0000

    included analytic solution for buckley-leverett problem
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1769 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- benchmarkproblems/fractionalflow/2p/buckleyleverett/buckleyleverett_ff.cc
+++ benchmarkproblems/fractionalflow/2p/buckleyleverett/buckleyleverett_ff.cc
@@ -9,38 +9,36 @@
 #include <dune/grid/io/file/vtk/vtkwriter.hh>
 #include <dune/istl/io.hh>
 #include <dune/common/timer.hh>
-#include "dumux/material/properties.hh"
-#include "dumux/material/linearlaw_deprecated.hh"
-#include "dumux/material/brookscoreylaw_deprecated.hh"
-#include "dumux/material/vangenuchtenlaw_deprecated.hh"
-#include "dumux/transport/fv/fvtransport_deprecated.hh"
-#include "dumux/diffusion/fv/fvdiffusion_deprecated.hh"
-#include "dumux/diffusion/fv/fvdiffusionvelocity_deprecated.hh"
-//#include "dumux/diffusion/mimetic/mimeticdiffusion.hh"
-#include "dumux/fractionalflow/impes/impes_deprecated.hh"
-#include "../problemdefinitions/buckleyleveretttransportproblem.hh"
-#include "../problemdefinitions/buckleyleverettdiffproblem.hh"
-#include "dumux/diffusion/problems/homogeneousproblem.hh"
+#include "../problemdefinitions/buckleyleverett_soilproperties.hh"
+#include "dumux/material/phaseproperties/phaseproperties2p.hh"
+#include "dumux/material/twophaserelations.hh"
+#include "../problemdefinitions/buckleyleverettproblem.hh"
+#include "dumux/diffusion/fv/fvdiffusion.hh"
+#include "dumux/diffusion/fv/fvdiffusionvelocity.hh"
+#include "dumux/transport/fv/fvtransport.hh"
+#include "dumux/fractionalflow/impes/impes_buckleyleverett_analytic.hh"
 #include "dumux/timedisc/timeloop.hh"
-#include "dumux/timedisc/rungekuttastep.hh"
+#include "dumux/timedisc/expliciteulerstep.hh"
 #include "dumux/fractionalflow/variableclass.hh"
 
 int main(int argc, char** argv)
 {
-    try{
+    try
+    {
         // define the problem dimensions
-        const int dim=2;
+        const int dim = 2;
         typedef double NumberType;
         Dune::FieldVector<NumberType, dim> LowerLeft(0);
         Dune::FieldVector<NumberType, dim> UpperRight(300);
-        UpperRight[1]=70;
-        if (argc != 2) {
+        UpperRight[1] = 70;
+        if (argc != 2)
+        {
             std::cout << "usage: tEnd" << std::endl;
             return 0;
         }
         std::string arg1(argv[1]);
         std::istringstream is1(arg1);
-        int tEnd;
+        double tEnd;
         is1 >> tEnd;
         // std::string arg2(argv[2]);
         // std::istringstream is2(arg2);
@@ -48,65 +46,78 @@ int main(int argc, char** argv)
         // is2 >> dt;
 
         // create a grid object
-        typedef Dune::SGrid<dim,dim> GridType;
-
-        //      // use unitcube from grids
+        typedef Dune::SGrid<dim, dim> GridType;
+        // use unitcube from grids
         std::stringstream dgfFileName;
-        dgfFileName << "grids/unitcube" << GridType :: dimension << ".dgf";
-        //
-        //      // create grid pointer, GridType is defined by gridtype.hh
-        Dune::GridPtr<GridType> gridPtr( dgfFileName.str() );
-        //
-        //      // grid reference
+        dgfFileName << "grids/unitcube" << GridType::dimension << ".dgf";
+        // create grid pointer, GridType is defined by gridtype.hh
+        Dune::GridPtr<GridType> gridPtr(dgfFileName.str());
+        // grid reference
         GridType& grid = *gridPtr;
-
         Dune::gridinfo(grid);
 
-        Oil oil(0.2);
-        Water water(0.2);
-        //      Dune::BrooksCoreyLaw law(water, oil,2,0);
-        Dune::DeprecatedLinearLaw law(water, oil);
+        // timeloop parameters
+        double tStart = 0;
+        //double tEnd = 2.5e9;
+        double cFLFactor = 1;
+        double maxDt = 1e6;
+        double firstDt = 10;
 
-        typedef Dune::VariableClass<GridType, NumberType> VC;
-
-        VC variables(grid);
-
-        Dune::BuckleyLeverettTransportProblem<GridType, NumberType, VC> transportProblem(variables, law,LowerLeft,UpperRight);
-        Dune::BuckleyLeverettDiffProblem<GridType, NumberType, VC> diffusionProblem(variables, law,LowerLeft,UpperRight);
-
-        typedef Dune::DeprecatedFVTransport<GridType, NumberType, VC> DeprecatedTransport;
-        DeprecatedTransport transport(grid, transportProblem, grid.maxLevel());
-
-        typedef Dune::DeprecatedFVDiffusionVelocity<GridType, NumberType, VC> DeprecatedDiffusion;
-        DeprecatedDiffusion diffusion(grid, diffusionProblem, grid.maxLevel());
-        //      typedef Dune::MimeticDiffusion<GridType, NumberType> DeprecatedDiffusion;
-        //          DeprecatedDiffusion diffusion(grid, diffusionProblem, transportProblem);
-
+        // IMPES parameters
         int iterFlag = 2;
         int nIter = 30;
         double maxDefect = 1e-5;
-        typedef Dune::IMPES<GridType, DeprecatedDiffusion, DeprecatedTransport, VC> IMPES;
-        IMPES fractionalflow(diffusion, transport, iterFlag, nIter, maxDefect);
 
-        double tStart = 0;
-        //double tEnd = 2.5e9;
+        // plotting parameters
         const char* fileName = "buckleyleverett";
         int modulo = 1;
-        double cFLFactor = 1;
-        Dune::TimeLoop<GridType, IMPES > timeloop(tStart, tEnd, fileName, modulo, cFLFactor);
 
+        Dune::Water wPhase;
+        Dune::Oil nPhase;
+
+        typedef Dune::BuckleyLeverettSoil<GridType, NumberType> SoilType;
+        SoilType soil;
+
+        typedef Dune::TwoPhaseRelations<GridType, NumberType>
+                TwoPhaseRelationsType;
+        TwoPhaseRelationsType materialLaw(soil, wPhase, nPhase);
+
+        typedef Dune::VariableClass<GridType, NumberType> VariableType;
+        VariableType variables(grid);
+
+        typedef Dune::BuckleyLeverettProblem<GridType, NumberType, VariableType>
+                ProblemType;
+        ProblemType problem(variables, wPhase, nPhase, soil, materialLaw,
+                LowerLeft, UpperRight);
+
+        typedef Dune::FVDiffusionVelocity<GridType, NumberType, VariableType,
+                ProblemType> DiffusionType;
+        DiffusionType diffusion(grid, problem);
+
+        typedef Dune::FVTransport<GridType, NumberType, VariableType,
+                ProblemType> TransportType;
+        TransportType transport(grid, problem);
+
+        typedef Dune::IMPESBLAnalytic<GridType, DiffusionType, TransportType,
+                VariableType> IMPESType;
+        IMPESType impes(diffusion, transport, iterFlag, nIter, maxDefect);
+
+        Dune::ExplicitEulerStep<GridType, IMPESType> timestep;
+        Dune::TimeLoop<GridType, IMPESType> timeloop(tStart, tEnd, fileName,
+                modulo, cFLFactor, maxDt, firstDt, timestep);
         Dune::Timer timer;
         timer.reset();
-        timeloop.execute(fractionalflow);
-        std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
+        timeloop.execute(impes);
+        std::cout << "timeloop.execute took " << timer.elapsed() << " seconds"
+                << std::endl;
         //printvector(std::cout, *fractionalflow, "saturation", "row", 200, 1);
 
         return 0;
-    }
-    catch (Dune::Exception &e){
+    } catch (Dune::Exception &e)
+    {
         std::cerr << "Dune reported error: " << e << std::endl;
-    }
-    catch (...){
+    } catch (...)
+    {
         std::cerr << "Unknown exception thrown!" << std::endl;
     }
 }
