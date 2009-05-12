de003b12105af4d7ed74b2e53849f8413d2c82a5
Anneli Schöniger <anneli.schoeniger@bosscon.de>
2009-05-12 17:12:17 +0000

    included analytic solution for buckley-leverett problem
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dune-mux/trunk@1776 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- benchmarkproblems/fractionalflow/2p/buckleyleverett/buckleyleverett1D_ff.cc
+++ benchmarkproblems/fractionalflow/2p/buckleyleverett/buckleyleverett1D_ff.cc
@@ -8,34 +8,30 @@
 #include <dune/grid/io/file/vtk/vtkwriter.hh>
 #include <dune/istl/io.hh>
 #include <dune/common/timer.hh>
-#include "dumux/material/properties.hh"
-#include "dumux/material/linearlaw_deprecated.hh"
-#include "dumux/material/brookscoreylaw_deprecated.hh"
-#include "dumux/material/vangenuchtenlaw_deprecated.hh"
-#include "dumux/transport/fv/fvtransport_deprecated.hh"
-#include "dumux/diffusion/fv/fvdiffusion_deprecated.hh"
-//#include "dumux/diffusion/mimetic/mimeticdiffusion.hh"
-//#include "dumux/diffusion/fe/fediffusion.hh"
-#include "dumux/diffusion/fv/fvdiffusionvelocity_deprecated.hh"
-#include "dumux/fractionalflow/impes/impes_deprecated.hh"
-#include "../problemdefinitions/buckleyleveretttransportproblem.hh"
-#include "../problemdefinitions/buckleyleverettdiffproblem.hh"
+#include "../problemdefinitions/buckleyleverett_soilproperties.hh"
+#include "dumux/material/phaseproperties/phaseproperties2p.hh"
+#include "dumux/material/twophaserelations.hh"
+#include "../problemdefinitions/buckleyleverettproblem.hh"
+#include "dumux/diffusion/fv/fvdiffusion.hh"
+#include "dumux/diffusion/fv/fvdiffusionvelocity.hh"
+#include "dumux/transport/fv/fvtransport.hh"
+#include "dumux/fractionalflow/impes/impes_buckleyleverett_analytic.hh"
 #include "dumux/timedisc/timeloop.hh"
-#include "dumux/timedisc/rungekuttastep.hh"
+#include "dumux/timedisc/expliciteulerstep.hh"
 #include "dumux/fractionalflow/variableclass.hh"
-#include "../problemdefinitions/buckleyleverettanalytical.hh"
 
 int main(int argc, char** argv)
 {
-    try{
+    try
+    {
         // define the problem dimensions
-        const int dim=1;
+        const int dim = 1;
         typedef double NumberType;
         typedef GridType::ctype ctype;
-
         Dune::FieldVector<NumberType, dim> Left(0);
         Dune::FieldVector<NumberType, dim> Right(300);
-        if (argc != 2) {
+        if (argc != 2)
+        {
             std::cout << "usage: tEnd" << std::endl;
             return 0;
         }
@@ -46,94 +42,96 @@ int main(int argc, char** argv)
 
         // create a grid object
         typedef Dune::OneDGrid GridType;
-
-        //deffinition of a stretched grid
+        //definition of a stretched grid
         //    const int numberofelements = 15;
         const int numberofelements = 30;
         //    const int numberofelements = 60;
         //    const int numberofelements = 120;
-
         double strfactor = 0;
-
         //vector with coordinates
         std::vector<ctype> coord;
-        coord.resize(numberofelements+1);
-        coord[0]=0;
-        coord[1]=1;
+        coord.resize(numberofelements + 1);
+        coord[0] = 0;
+        coord[1] = 1;
         //generate coordinates for a stretched grid
-        for (int i=2;i<numberofelements+1;i++){
-            coord[i]=coord[i-1]+(coord[i-1]-coord[i-2])*(1+strfactor);
+        for (int i = 2; i < numberofelements + 1; i++)
+        {
+            coord[i] = coord[i - 1] + (coord[i - 1] - coord[i - 2]) *
+            (1 + strfactor);
         }
-
         //scale coordinates to geometry
-        for (int i=0;i<numberofelements+1;i++){
-            coord[i]*=Right[0]/coord[numberofelements];
+        for (int i = 0; i < numberofelements + 1; i++)
+        {
+            coord[i] *= Right[0] / coord[numberofelements];
             std::cout << "coordinates =  " << coord[i] << std::endl;
         }
-
         const std::vector<ctype>& coordinates(coord);
-
         // grid
         GridType grid(coordinates);
-
         Dune::gridinfo(grid);
 
+        // IMPES parameters
         int iterFlag = 2;
         int nIter = 30;
         double maxDefect = 1e-5;
 
+        // timeloop parameters
         double tStart = 0;
         //double tEnd = 2.5e9;
+        double cFLFactor = 1.0;
+        double maxDt = 1e6;
+        double firstDt = 1e6;
+
+        // plotting parameters
         const char* fileName = "buckleyleverett1D";
         int modulo = 1;
-        double cFLFactor = 1.0;
-        // slope limiter parameters
-        //    bool reconstruct = true;
-        //    double alphaMax = 0.8;
 
-        Oil oil(0.2);
-        Water water(0.2);
-        //    Dune::BrooksCoreyLaw materialLaw(water, oil,2,0);
-        Dune::DeprecatedLinearLaw materialLaw(water, oil);
+        Dune::Water wPhase;
+        Dune::Oil nPhase;
 
-        double initpress = 2e5;
-        double initsat = 0.2;
-        Dune::FieldVector<double,dim> initvel(3e-7);
+        typedef Dune::BuckleyLeverettSoil<GridType, NumberType> SoilType;
+        SoilType soil;
 
-        typedef Dune::VariableClass<GridType, NumberType> VC;
+        typedef Dune::TwoPhaseRelations<GridType, NumberType>
+                TwoPhaseRelationsType;
+        TwoPhaseRelationsType materialLaw(soil, wPhase, nPhase);
 
-        VC variables(grid,initsat,initpress,initvel);
+        typedef Dune::VariableClass<GridType, NumberType> VariableType;
+        VariableType variables(grid);
 
-        Dune::BLWithAnalytical<GridType, NumberType, VC> transportProblem(variables, materialLaw,Left,Right,cFLFactor);
-        Dune::BuckleyLeverettDiffProblem<GridType, NumberType, VC> diffusionProblem(variables, materialLaw,Left,Right);
+        typedef Dune::BuckleyLeverettProblem<GridType, NumberType, VariableType>
+                ProblemType;
+        ProblemType problem(variables, wPhase, nPhase, soil, materialLaw, Left,
+                Right);
 
-        typedef Dune::DeprecatedFVTransport<GridType, NumberType, VC> DeprecatedTransport;
-        DeprecatedTransport transport(grid, transportProblem, grid.maxLevel());
+        typedef Dune::FVDiffusionVelocity<GridType, NumberType, VariableType,
+                ProblemType> DiffusionType;
+        DiffusionType diffusion(grid, problem);
 
-        //    typedef Dune::DeprecatedFVDiffusion<GridType, NumberType, VC> DeprecatedDiffusion;
-        typedef Dune::DeprecatedFVDiffusionVelocity<GridType, NumberType, VC> DeprecatedDiffusion;
-        DeprecatedDiffusion diffusion(grid, diffusionProblem, grid.maxLevel());
+        typedef Dune::FVTransport<GridType, NumberType, VariableType,
+                ProblemType> TransportType;
+        TransportType transport(grid, problem);
 
+        typedef Dune::IMPESBLAnalytic<GridType, DiffusionType, TransportType,
+                VariableType> IMPESType;
+        IMPESType impes(diffusion, transport, iterFlag, nIter, maxDefect);
 
-        typedef Dune::IMPES<GridType, DeprecatedDiffusion, DeprecatedTransport, VC> IMPES;
-        IMPES fractionalflow(diffusion, transport, iterFlag, nIter, maxDefect);
-
-        //    Dune::TimeLoop<GridType, DeprecatedTransport > timeloop(tStart, tEnd, fileName, modulo, cFLFactor);
-        Dune::TimeLoop<GridType, IMPES > timeloop(tStart, tEnd, fileName, modulo, cFLFactor);
-
+        Dune::ExplicitEulerStep<GridType, IMPESType> timestep;
+        Dune::TimeLoop<GridType, IMPESType> timeloop(tStart, tEnd, fileName,
+                modulo, cFLFactor, maxDt, firstDt, timestep);
         Dune::Timer timer;
         timer.reset();
-        timeloop.execute(fractionalflow);
-        //    timeloop.execute(transport);
-        std::cout << "timeloop.execute took " << timer.elapsed() << " seconds" << std::endl;
+        timeloop.execute(impes);
+        std::cout << "timeloop.execute took " << timer.elapsed() << " seconds"
+                << std::endl;
         //printvector(std::cout, *fractionalflow, "saturation", "row", 200, 1);
 
         return 0;
-    }
-    catch (Dune::Exception &e){
+    } catch (Dune::Exception &e)
+    {
         std::cerr << "Dune reported error: " << e << std::endl;
-    }
-    catch (...){
+    } catch (...)
+    {
         std::cerr << "Unknown exception thrown!" << std::endl;
     }
 }
