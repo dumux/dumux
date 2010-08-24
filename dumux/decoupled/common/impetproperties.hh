363d31dc7191838d520d1de8c140614e88bcb6cc
Markus Wolff <markus.wolff@twt-gmbh.de>
2010-08-24 15:52:07 +0000

    changed impes base clases to impet
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@4155 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ dumux/decoupled/common/impetproperties.hh
@@ -0,0 +1,70 @@
+// $Id$
+/*****************************************************************************
+ *   Copyright (C) 2009 by Markus Wolff                                      *
+ *   Institute of Hydraulic Engineering                                      *
+ *   University of Stuttgart, Germany                                        *
+ *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version, as long as this copyright notice    *
+ *   is included in its original form.                                       *
+ *                                                                           *
+ *   This program is distributed WITHOUT ANY WARRANTY.                       *
+ *****************************************************************************/
+#ifndef DUMUX_IMPET_PROPERTIES_HH
+#define DUMUX_IMPET_PROPERTIES_HH
+
+#include <dumux/decoupled/common/decoupledproperties.hh>
+
+/*!
+ * \file
+ * \brief Specify the shape functions, operator assemblers, etc
+ *        used for the BoxScheme.
+ */
+namespace Dumux
+{
+
+template<class TypeTag>
+class IMPET;
+
+namespace Properties
+{
+/*!
+ * \addtogroup diffusion
+ */
+// \{
+
+//////////////////////////////////////////////////////////////////
+// Type tags tags
+//////////////////////////////////////////////////////////////////
+
+//! The type tag for models based on the diffusion-scheme
+NEW_TYPE_TAG(IMPET, INHERITS_FROM(DecoupledModel));
+
+//////////////////////////////////////////////////////////////////
+// Property tags
+//////////////////////////////////////////////////////////////////
+
+NEW_PROP_TAG(PressureModel);         //!< The type of the discretizations
+NEW_PROP_TAG(TransportModel);         //!< The type of the discretizations
+
+NEW_PROP_TAG(CFLFactor);
+NEW_PROP_TAG(IterationFlag);
+NEW_PROP_TAG(IterationNumber);
+NEW_PROP_TAG(MaximumDefect);
+NEW_PROP_TAG(RelaxationFactor);
+
+SET_TYPE_PROP(IMPET, Model, IMPET<TypeTag>);
+
+SET_SCALAR_PROP(IMPET, CFLFactor, 1);
+SET_INT_PROP(IMPET, IterationFlag, 0);
+SET_INT_PROP(IMPET, IterationNumber, 2);
+SET_SCALAR_PROP(IMPET, MaximumDefect, 1e-5);
+SET_SCALAR_PROP(IMPET, RelaxationFactor, 1);
+
+}
+}
+
+#endif
