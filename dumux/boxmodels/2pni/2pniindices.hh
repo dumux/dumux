00296949d069fdafb5c3bbcfad086735639b95ee
Andreas Lauser <and@poware.org>
2010-08-26 15:49:59 +0000

    2pni box model: add forgotten headers
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@4187 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ dumux/boxmodels/2pni/2pniindices.hh
@@ -0,0 +1,46 @@
+// $Id$
+/*****************************************************************************
+ *   Copyright (C) 2008 by Klaus Mosthaf                                     *
+ *   Copyright (C) 2008-2009 by Andreas Lauser                               *
+ *   Copyright (C) 2008 Bernd Flemisch                                       *
+ *   Institute of Hydraulic Engineering                                      *
+ *   University of Stuttgart, Germany                                        *
+ *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version, as long as this copyright notice    *
+ *   is included in its original form.                                       *
+ *                                                                           *
+ *   This program is distributed WITHOUT ANY WARRANTY.                       *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief Defines the indices used by the non-isotherm two-phase BOX model.
+ */
+#ifndef DUMUX_2PNI_INDICES_HH
+#define DUMUX_2PNI_INDICES_HH
+
+namespace Dumux
+{
+/*!
+ * \addtogroup TwoPNIBoxModel
+ */
+// \{
+
+/*!
+ * \brief Enumerations for the non-isothermal two-phase model
+ */
+template <int PVOffset = 0>
+class TwoPNIIndices : public TwoPIndices<PVOffset>
+{
+public:
+    static const int temperatureIdx = PVOffset + 2; //! The primary variable index for temperature
+    static const int energyEqIdx = PVOffset + 2; //! The equation index of the energy equation
+};
+// \}
+}
+
+#endif
