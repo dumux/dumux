f6ab0acb2b857d5d9fbdcc2a881ae73e00640541
Markus Wolff <markus.wolff@twt-gmbh.de>
2011-06-08 13:29:30 +0000

    added automatic choice of grid flag for mpfa method
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@5963 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/decoupled/2p/diffusion/fvmpfa/mpfaproperties.hh
+++ dumux/decoupled/2p/diffusion/fvmpfa/mpfaproperties.hh
@@ -30,6 +30,12 @@
 
 // dumux environment
 #include <dumux/decoupled/2p/2pproperties.hh>
+#include <dune/grid/yaspgrid.hh>
+#include <dune/grid/sgrid.hh>
+#include <dune/grid/alugrid.hh>
+#if HAVE_UG
+#include <dune/grid/uggrid.hh>
+#endif
 
 namespace Dumux
 {
@@ -39,15 +45,43 @@ namespace Dumux
 struct GridTypes
 {
 public:
+    //
+    static const int any = 0;
     //SGrid
-    static const int sGrid = 0;
+    static const int sGrid = 1;
     //YaspGrid
-    static const int yaspGrid = 1;
+    static const int yaspGrid = 2;
     //UGGrid
-    static const int ugGrid = 2;
+    static const int ugGrid = 3;
     //ALUGrid
-    static const int aluGrid = 3;
+    static const int aluGrid = 4;
 };
+
+template<class Grid, int dim>
+struct GridImp
+{
+    static const int imp = GridTypes::any;
+};
+
+template<int dim>
+struct GridImp<Dune::YaspGrid<dim>, dim>
+{
+    static const int imp = GridTypes::yaspGrid;
+};
+
+template<int dim>
+struct GridImp<Dune::SGrid<dim, dim>, dim>
+{
+    static const int imp = GridTypes::sGrid;
+};
+
+#if HAVE_UG
+template<int dim>
+struct GridImp<Dune::UGGrid<dim>, dim>
+{
+    static const int imp = GridTypes::ugGrid;
+};
+#endif
 
 namespace Properties
 {
@@ -56,7 +90,13 @@ NEW_TYPE_TAG(MPFAProperties);
 NEW_PROP_TAG( GridTypeIndices );
 NEW_PROP_TAG( GridImplementation ); //returns kind of grid implementation
 
-SET_INT_PROP(MPFAProperties, GridImplementation, GridTypes::sGrid);
+SET_PROP(MPFAProperties, GridImplementation)
+{
+private:
+    typedef typename GET_PROP_TYPE(TypeTag, PTAG(Grid)) Grid;
+public:
+    static const int value = GridImp<Grid, Grid::dimension>::imp;
+};
 
 SET_TYPE_PROP(MPFAProperties, GridTypeIndices, GridTypes);
 
