56499bfe950241d28ae068bf78ff4a1a06072737
Markus Wolff <markus.wolff@twt-gmbh.de>
2011-08-08 14:13:36 +0000

    added missing spatial parameters file to 1p decoupled test problem
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@6422 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ test/decoupled/1p/test_1p_spatialparams.hh
@@ -0,0 +1,85 @@
+/*****************************************************************************
+ *   Copyright (C) 2008-2009 by Markus Wolff                                 *
+ *   Institute of Hydraulic Engineering                                      *
+ *   University of Stuttgart, Germany                                        *
+ *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief spatial parameters for the test problem for diffusion models.
+ */
+#ifndef TEST_1P_SPATIALPARAMETERS_HH
+#define TEST_1P_SPATIALPARAMETERS_HH
+
+#include <dumux/material/spatialparameters/fvspatialparameters.hh>
+
+namespace Dumux
+{
+
+/*!
+ * \ingroup IMPETtests
+ * \brief spatial parameters for the test problem for 1-p diffusion models.
+ */
+template<class TypeTag>
+class TestOnePSpatialParams: public FVSpatialParameters<TypeTag>
+{
+    typedef FVSpatialParameters<TypeTag> ParentType;
+    typedef typename GET_PROP_TYPE(TypeTag, PTAG(Grid)) Grid;
+    typedef typename GET_PROP_TYPE(TypeTag, PTAG(GridView)) GridView;
+    typedef typename GET_PROP_TYPE(TypeTag, PTAG(Scalar)) Scalar;
+    typedef typename Grid::ctype CoordScalar;
+
+    enum
+        {dim=Grid::dimension, dimWorld=Grid::dimensionworld, numEq=1};
+    typedef typename Grid::Traits::template Codim<0>::Entity Element;
+
+    typedef Dune::FieldVector<CoordScalar, dimWorld> GlobalPosition;
+    typedef Dune::FieldMatrix<Scalar,dim,dim> FieldMatrix;
+
+public:
+
+    const FieldMatrix& intrinsicPermeabilityAtPos (const GlobalPosition& globalPos) const
+    {
+        double rt = globalPos[0]*globalPos[0]+globalPos[1]*globalPos[1];
+        permeability_[0][0] = (delta_*globalPos[0]*globalPos[0] + globalPos[1]*globalPos[1])/rt;
+        permeability_[0][1] = permeability_[1][0] = -(1.0 - delta_)*globalPos[0]*globalPos[1]/rt;
+        permeability_[1][1] = (globalPos[0]*globalPos[0] + delta_*globalPos[1]*globalPos[1])/rt;
+
+        return permeability_;
+    }
+
+    double porosity(const Element& element) const
+    {
+        return 0.2;
+    }
+
+    void setDelta(const double delta)
+    {
+        delta_ = delta;
+    }
+
+    TestOnePSpatialParams(const GridView& gridView)
+    : ParentType(gridView), permeability_(0)
+    { }
+
+private:
+    mutable FieldMatrix permeability_;
+    double delta_;
+};
+
+} // end namespace
+#endif
