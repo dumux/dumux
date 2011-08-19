735d7d1ee8943a3777427b4ea560138c994d6fc6
Michael Sinsbeck <michael@sinsbeck.com>
2011-08-19 09:53:45 +0000

    Improved ViP-Lab output
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@6524 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- appl/lecture/msm/buckleyleverett/buckleyleverettproblem.hh
+++ appl/lecture/msm/buckleyleverett/buckleyleverettproblem.hh
@@ -279,6 +279,10 @@ public:
     	if (time < 0)
     		return;
 
+        double discretizationLength = Params::tree().template get<double>("Geometry.discretizationLength");
+		int cellNumberX = static_cast<int>(300/discretizationLength);
+		discretizationLength = 300/cellNumberX; //Might be slightly different from the input parameter
+
     	std::ofstream dataFile;
 		dataFile.open("vipplot.vgf", std::fstream::app);
 
@@ -290,30 +294,63 @@ public:
 		dataFile << "# title Buckley-Leverett-Problem\n";
 		dataFile << "# x-label x\n";
 		dataFile << "# y-label Saturation\n";
-		dataFile << "# color 0 0 255\n";
 		dataFile << "# x-range -10 310\n";
 		dataFile << "# y-range 0 1\n";
-//		dataFile << "# symbol none";
-//		dataFile << "# linestyle solid";
-		ElementIterator eItEnd = this->gridView().template end<0>();
-	    for (ElementIterator eIt = this->gridView().template begin<0>(); eIt != eItEnd; ++eIt)
-	    {
-	    	int currentIdx = this->variables().index(*eIt);
-	    	Scalar sat = this->variables().saturation()[currentIdx];
-	    	Scalar leftPos = eIt->geometry().corner(0)[0];
-	    	Scalar rightPos = eIt->geometry().corner(1)[0];
-	    	dataFile << leftPos <<" "<<sat<<" "<<rightPos<<" "<<sat<<"\n";
-	    }
 
+		dataFile << "# color 0 0 255\n";
+
+		Scalar curSat = this->variables().saturation()[0];
+
+		// Draw first piece separately, as no vertical line is needed
+		dataFile << 0 << " " << curSat << " "
+		         << discretizationLength << " " << curSat <<std::endl;
+		for (int i=1; i < cellNumberX; i++)
+		{
+			// Vertical Line
+			dataFile << i*discretizationLength << " "
+					 << curSat << " ";
+			curSat = this->variables().saturation()[i];
+			dataFile << i*discretizationLength << " " << curSat << std::endl;
+			//Horizontal Line
+			dataFile << i*discretizationLength << " " << curSat << " "
+					 << (i+1)*discretizationLength << " " << curSat << std::endl;
+		}
 		dataFile << "# color 255 0 0\n";
-		for (ElementIterator eIt = this->gridView().template begin<0>(); eIt != eItEnd; ++eIt)
-	    {
-	    	int currentIdx = this->variables().index(*eIt);
-	    	Scalar sat = analyticSolution_.AnalyticSolution()[currentIdx];
-	    	Scalar leftPos = eIt->geometry().corner(0)[0];
-	    	Scalar rightPos = eIt->geometry().corner(1)[0];
-	    	dataFile << leftPos <<" "<<sat<<" "<<rightPos<<" "<<sat<<"\n";
-	    }
+
+		curSat = analyticSolution_.AnalyticSolution()[0];
+
+		dataFile << 0 << " " << curSat << " "
+				 << discretizationLength << " " << curSat <<std::endl;
+		for (int i=1; i < cellNumberX; i++)
+		{
+			// Vertical Line
+			dataFile << i*discretizationLength << " "
+					 << curSat << " ";
+			curSat = analyticSolution_.AnalyticSolution()[i];
+			dataFile << i*discretizationLength << " " << curSat << std::endl;
+			//Horizontal Line
+			dataFile << i*discretizationLength << " " << curSat << " "
+					 << (i+1)*discretizationLength << " " << curSat << std::endl;
+		}
+//		ElementIterator eItEnd = this->gridView().template end<0>();
+//	    for (ElementIterator eIt = this->gridView().template begin<0>(); eIt != eItEnd; ++eIt)
+//	    {
+//	    	int currentIdx = this->variables().index(*eIt);
+//	    	Scalar sat = this->variables().saturation()[currentIdx];
+//	    	Scalar leftPos = eIt->geometry().corner(0)[0];
+//	    	Scalar rightPos = eIt->geometry().corner(1)[0];
+//	    	dataFile << leftPos <<" "<<sat<<" "<<rightPos<<" "<<sat<<"\n";
+//	    }
+
+
+//		for (ElementIterator eIt = this->gridView().template begin<0>(); eIt != eItEnd; ++eIt)
+//	    {
+//	    	int currentIdx = this->variables().index(*eIt);
+//	    	Scalar sat = analyticSolution_.AnalyticSolution()[currentIdx];
+//	    	Scalar leftPos = eIt->geometry().corner(0)[0];
+//	    	Scalar rightPos = eIt->geometry().corner(1)[0];
+//	    	dataFile << leftPos <<" "<<sat<<" "<<rightPos<<" "<<sat<<"\n";
+//	    }
 
 		dataFile << "# legend Numerical Solution,Analytic Solution\n";
 		dataFile.close();
