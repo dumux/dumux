b11516c4a15e9e67b3e3e3e080b86ad4843af1f4
Markus Wolff <markus.wolff@twt-gmbh.de>
2012-01-23 09:40:05 +0000

    added missing property file
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@7483 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ dumux/decoupled/2p/diffusion/diffusionproperties2p.hh
@@ -0,0 +1,65 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   Copyright (C) 2009 by Markus Wolff                                      *
+ *   Institute of Hydraulic Engineering                                      *
+ *   University of Stuttgart, Germany                                        *
+ *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+#ifndef DUMUX_DIFFUSION_PROPERTIES_2P_HH
+#define DUMUX_DIFFUSION_PROPERTIES_2P_HH
+
+#include <dumux/decoupled/common/pressureproperties.hh>
+#include <dumux/decoupled/2p/2pproperties.hh>
+
+/*!
+ * \ingroup Saturation2p
+ * \ingroup Properties
+ */
+/*!
+ * \file
+ * \brief Specifies the properties for immiscible 2p transport
+ */
+namespace Dumux
+{
+namespace Properties
+{
+// \{
+
+//////////////////////////////////////////////////////////////////
+// Type tags tags
+//////////////////////////////////////////////////////////////////
+
+//! The type tag for models based on the diffusion-scheme
+NEW_TYPE_TAG(PressureTwoP, INHERITS_FROM(Pressure, DecoupledTwoP));
+
+//////////////////////////////////////////////////////////////////
+// Property tags
+//////////////////////////////////////////////////////////////////
+}
+}
+
+namespace Dumux
+{
+namespace Properties
+{
+//////////////////////////////////////////////////////////////////
+// Property tags
+//////////////////////////////////////////////////////////////////
+}
+}
+
+#endif
