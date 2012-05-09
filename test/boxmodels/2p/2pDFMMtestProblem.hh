7ed8e8b23e716c0c77f9e0123e76487ac0455310
Alexandru Tatomir <atatomi@gwdg.de>
2012-05-09 13:42:07 +0000

    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@8260 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ test/boxmodels/2p/2pDFMMtestProblem.hh
@@ -0,0 +1,510 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   Copyright (C) 2007-2008 by Klaus Mosthaf                                *
+ *   Copyright (C) 2007-2008 by Bernd Flemisch                               *
+ *   Copyright (C) 2008-2009 by Andreas Lauser                               *
+ *   Institute for Modelling Hydraulic and Environmental Systems             *
+ *   University of Stuttgart, Germany                                        *
+ *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief Soil contamination problem where DNAPL infiltrates a fully
+ *        water saturated medium.
+ */
+
+#ifndef DUMUX_TWPDFMM_TESTPROBLEM_HH
+#define DUMUX_TWPDFMM_TESTPROBLEM_HH
+
+#if HAVE_UG
+#include <dune/grid/uggrid.hh>
+#endif
+
+#include <dune/grid/yaspgrid.hh>
+#include <dune/grid/sgrid.hh>
+
+#include <dumux/material/components/simpleh2o.hh>
+#include <dumux/material/components/simplednapl.hh>
+#include <dumux/boxmodels/2pDFMM/2pdfmmodel.hh>
+//#include <dumux/boxmodels/common/porousmediaboxproblem.hh>
+
+#include <dumux/material/fluidsystems/h2on2fluidsystem.hh>
+
+#include "2pDFMMspatialparameters.hh"
+
+namespace Dumux
+{
+
+template <class TypeTag>
+class TwoPDFMMTestProblem;
+
+//////////
+// Specify the properties for the lens problem
+//////////
+namespace Properties
+{
+NEW_TYPE_TAG(TwoPDFMMTestProblem, INHERITS_FROM(BoxTwoPDFM, TwoPDFMMSpatialParameters));
+
+// Set the grid type
+#if HAVE_UG
+SET_TYPE_PROP(TwoPDFMMTestProblem, Grid, Dune::UGGrid<2>);
+#else
+SET_TYPE_PROP(TwoPDFMMTestProblem, Grid, Dune::YaspGrid<2>);
+#endif
+
+// Set the problem property
+SET_TYPE_PROP(TwoPDFMMTestProblem, Problem, Dumux::TwoPDFMMTestProblem<TypeTag>);
+
+// TODO: remove this macro switch
+#if 1
+// Set the wetting phase
+SET_PROP(TwoPDFMMTestProblem, WettingPhase)
+{
+private:
+    typedef typename GET_PROP_TYPE(TypeTag, Scalar) Scalar;
+public:
+    typedef Dumux::LiquidPhase<Scalar, Dumux::SimpleH2O<Scalar> > type;
+};
+
+// Set the non-wetting phase
+SET_PROP(TwoPDFMMTestProblem, NonwettingPhase)
+{
+private:
+    typedef typename GET_PROP_TYPE(TypeTag, Scalar) Scalar;
+public:
+    typedef Dumux::LiquidPhase<Scalar, Dumux::SimpleDNAPL<Scalar> > type;
+};
+#else
+// OR: set the fluid system
+SET_TYPE_PROP(TwoPDFMMTestProblem, FluidSystem, H2ON2FluidSystem<TypeTag>);
+#endif
+
+// Enable partial reassembly of the jacobian matrix?
+SET_BOOL_PROP(TwoPDFMMTestProblem, EnablePartialReassemble, true);
+
+// Enable reuse of jacobian matrices?
+SET_BOOL_PROP(TwoPDFMMTestProblem, EnableJacobianRecycling, true);
+
+// Write the solutions of individual newton iterations?
+SET_BOOL_PROP(TwoPDFMMTestProblem, NewtonWriteConvergence, false);
+
+// Use forward differences instead of central differences
+SET_INT_PROP(TwoPDFMMTestProblem, NumericDifferenceMethod, +1);
+
+// Linear solver settings
+SET_TYPE_PROP(TwoPDFMMTestProblem, LinearSolver, Dumux::BoxBiCGStabILU0Solver<TypeTag> );
+SET_INT_PROP(TwoPDFMMTestProblem, LinearSolverVerbosity, 0);
+SET_INT_PROP(TwoPDFMMTestProblem, PreconditionerIterations, 1);
+SET_SCALAR_PROP(TwoPDFMMTestProblem, PreconditionerRelaxation, 1.0);
+
+// Enable gravity
+SET_BOOL_PROP(TwoPDFMMTestProblem, EnableGravity, true);
+}
+
+/*!
+ * \ingroup TwoPBoxModel
+ * \ingroup BoxTestProblems
+ * \brief Soil contamination problem where DNAPL infiltrates a fully
+ *        water saturated medium.
+ *
+ * This problem uses the \ref TwoPBoxModel.
+ *
+ * This problem should typically be simulated until \f$t_{\text{end}}
+ * \approx 20\,000\;s\f$ is reached. A good choice for the initial time step
+ * size is \f$t_{\text{inital}} = 250\;s\f$.
+ *
+ * To run the simulation execute the following line in shell:
+ * <tt>./test_2p -parameterFile test_2p.input</tt>
+ */
+template <class TypeTag >
+class TwoPDFMMTestProblem : public PorousMediaBoxProblem<TypeTag>
+{
+    typedef PorousMediaBoxProblem<TypeTag> ParentType;
+    typedef typename GET_PROP_TYPE(TypeTag, GridView) GridView;
+
+    typedef typename GET_PROP_TYPE(TypeTag, Indices) Indices;
+
+    typedef typename GET_PROP_TYPE(TypeTag, FluidSystem) FluidSystem;
+    typedef typename GET_PROP_TYPE(TypeTag, WettingPhase) WettingPhase;
+    typedef typename GET_PROP_TYPE(TypeTag, NonwettingPhase) NonwettingPhase;
+
+    enum {
+
+        // primary variable indices
+        pwIdx = Indices::pwIdx,
+        SnIdx = Indices::SnIdx,
+
+        // equation indices
+        contiNEqIdx = Indices::contiNEqIdx,
+
+        // phase indices
+        wPhaseIdx = Indices::wPhaseIdx,
+        nPhaseIdx = Indices::nPhaseIdx,
+
+
+        // Grid and world dimension
+        dim = GridView::dimension,
+        dimWorld = GridView::dimensionworld
+    };
+
+    typedef typename GridView::template Codim<0>::Entity Element;
+    typedef typename GridView::template Codim<dim>::Entity Vertex;
+    typedef typename GridView::Intersection Intersection;
+    typedef typename GridView::template Codim<0>::Iterator ElementIterator;
+
+    typedef typename GET_PROP_TYPE(TypeTag, PrimaryVariables) PrimaryVariables;
+    typedef typename GET_PROP_TYPE(TypeTag, BoundaryTypes) BoundaryTypes;
+    typedef typename GET_PROP_TYPE(TypeTag, TimeManager) TimeManager;
+    typedef typename GridType::ctype DT;
+
+    typedef typename GET_PROP_TYPE(TypeTag, Scalar) Scalar;
+    typedef Dune::FieldVector<Scalar, dimWorld> GlobalPosition;
+
+public:
+    /*!
+     * \brief The constructor
+     *
+     * \param timeManager The time manager
+     * \param gridView The grid view
+     */
+    TwoPDFMMTestProblem(TimeManager &timeManager,
+                const GridView &gridView)
+        : ParentType(timeManager, gridView)
+    {
+        eps_ = 3e-6;
+        temperature_ = 273.15 + 20; // -> 20°C
+    }
+
+    TwoPDFMMTestProblem(TimeManager &timeManager,
+                        const GridView &gridView,
+                std::vector<bool>& isDuneFractureVertex,
+                std::vector<bool>& isDuneFractureEdge,
+                std::vector<int>& fractureEdgesIdx)
+            : ParentType(timeManager, gridView),
+              isDuneFractureVertex_(isDuneFractureVertex),
+              isDuneFractureEdge_(isDuneFractureEdge),
+              fractureEdgesIdx_(fractureEdgesIdx)
+        {
+    		eps_ = 3e-6;
+    	    temperature_ = 273.15 + 20; // -> 20°C
+
+
+        	inactivate_fractures_ 	= false;
+        	this->spatialParameters().setGridView(gridView);
+            this->spatialParameters().setFractureBoolVectors(isDuneFractureVertex_,
+            		isDuneFractureEdge_, fractureEdgesIdx_, inactivate_fractures_);
+            bboxMin_ = this->bboxMin();
+            bboxMax_ = this->bboxMax();
+           // this->injectionLength();
+    //        this->fractureVolumeFraction();
+            //DEFINE HERE the options for the simulation
+            one_phase_problem 		= false;
+            no_flow_problem 		= false;
+            coarse_transmissibility = false;
+            use_interface_condition = true;
+        }
+
+    /*
+     *
+     */
+
+    void injectionLength() //TODO generalize for all types of boundaries
+    {
+        ElementIterator elementIt = this->gridView().template begin<0>();
+        ElementIterator elementEndIt = this->gridView().template end<0>();
+        //unsigned numVertices = this->gridView().size(dim);
+        int numElem = 0;
+        for (; elementIt != elementEndIt; ++elementIt)
+        {
+        	if (elementIt->partitionType() != Dune::InteriorEntity)
+        		continue;
+            Dune::GeometryType gt = elementIt->geometry().type();
+            const typename Dune::GenericReferenceElementContainer<DT,dim>::value_type&
+                            refElem = Dune::GenericReferenceElements<DT,dim>::general(gt);
+            for (int i=0; i<refElem.size(1); i++)
+            {
+                int localV1Idx = refElem.subEntity(i, 1, 0, dim);
+                int localV2Idx = refElem.subEntity(i, 1, 1, dim);
+                Dune::FieldVector<DT, dim> node_from;
+                Dune::FieldVector<DT, dim> node_to;
+                Dune::FieldVector<DT, dim> diff_ij;
+
+                node_from = elementIt->geometry().corner(localV1Idx);
+                node_to = elementIt->geometry().corner(localV2Idx);
+                if (onLowerBoundary_(node_from)&& onLowerBoundary_(node_to))
+                {
+                    //checks if the nodes are on the same axes
+                    if (
+                            (node_from[0]==node_to[0])
+                            ||
+                            (node_from[1]==node_to[1])
+                            )
+                    {
+                        numElem++;
+                        distNodesOnBoundary.resize(numElem);
+                        diff_ij = node_from - node_to;
+                        distNodesOnBoundary[numElem-1] = diff_ij.two_norm();
+//                        std::cout<<"distNodes["<<numElem-1<<"] diff_ij= "<<diff_ij
+//                        		<<"distNodes "<<distNodesOnBoundary[numElem-1]
+//                        		<<" nodeF = "<<node_from
+//                        		<<" nodeTo = "<<node_to <<std::endl;
+                    }
+                }
+            }
+        }
+        inletArea = 0;
+        for (int i=0; i<numElem; i++)
+        {
+            inletArea += (distNodesOnBoundary[i]);
+        }
+        inletArea = this->gridView().comm().sum(inletArea);
+        std::cout<<"inletArea="<<inletArea<<std::endl;
+    }
+
+
+    /*!
+     * \name Problem parameters
+     */
+    // \{
+
+    /*!
+     * \brief The problem name.
+     *
+     * This is used as a prefix for files generated by the simulation.
+     */
+    const char *name() const
+    { return "out_2pDFMM"; }
+
+    /*!
+     * \brief Called directly after the time integration.
+     */
+    void postTimeStep()
+    {
+        // Calculate storage terms
+        PrimaryVariables storage;
+        this->model().globalStorage(storage);
+
+        // Write mass balance information for rank 0
+        if (this->gridView().comm().rank() == 0) {
+            std::cout<<"Storage: " << storage << std::endl;
+        }
+    }
+
+    /*!
+     * \brief Returns the temperature within the domain.
+     *
+     * This problem assumes a uniform temperature of 10 degrees Celsius.
+     */
+    Scalar temperature() const
+    { return temperature_; };
+
+
+    void sourceAtPos(PrimaryVariables &values,
+                const GlobalPosition &globalPos) const
+    {
+        values = 0;
+    }
+
+    // \}
+
+    /*!
+     * \name Boundary conditions
+     */
+    // \{
+
+    /*!
+     * \brief Specifies which kind of boundary condition should be
+     *        used for which equation on a given boundary control volume.
+     *
+     * \param values The boundary types for the conservation equations
+     * \param globalPos The position of the center of the finite volume
+     */
+    void boundaryTypesAtPos(BoundaryTypes &values,
+            const GlobalPosition &globalPos) const
+    {
+        if (onLeftBoundary_(globalPos) || onRightBoundary_(globalPos)) {
+            values.setAllDirichlet();
+        }
+        else {
+            values.setAllNeumann();
+        }
+    }
+
+    /*!
+     * \brief Evaluate the boundary conditions for a dirichlet
+     *        control volume.
+     *
+     * \param values The dirichlet values for the primary variables
+     * \param globalPos The center of the finite volume which ought to be set.
+     *
+     * For this method, the \a values parameter stores primary variables.
+     */
+    void dirichletAtPos(PrimaryVariables &values,
+                        const GlobalPosition &globalPos) const
+    {
+        typename GET_PROP_TYPE(TypeTag, FluidState) fluidState;
+        fluidState.setTemperature(temperature_);
+        fluidState.setPressure(FluidSystem::wPhaseIdx, /*pressure=*/1e5);
+        fluidState.setPressure(FluidSystem::nPhaseIdx, /*pressure=*/1e5);
+
+        Scalar densityW = FluidSystem::density(fluidState, FluidSystem::wPhaseIdx);
+
+        if (onLeftBoundary_(globalPos))
+        {
+            Scalar height = this->bboxMax()[1] - this->bboxMin()[1];
+            Scalar depth = this->bboxMax()[1] - globalPos[1];
+            Scalar alpha = (1 + 1.5/height);
+
+            // hydrostatic pressure scaled by alpha
+            values[pwIdx] = 1e5 - alpha*densityW*this->gravity()[1]*depth;
+            values[SnIdx] = 0.0;
+        }
+        else if (onRightBoundary_(globalPos))
+        {
+            Scalar depth = this->bboxMax()[1] - globalPos[1];
+
+            // hydrostatic pressure
+            values[pwIdx] = 1e5 - densityW*this->gravity()[1]*depth;
+            values[SnIdx] = 0.0;
+        }
+        else
+            values = 0.0;
+    }
+
+    /*!
+     * \brief Evaluate the boundary conditions for a neumann
+     *        boundary segment.
+     *
+     * \param values The neumann values for the conservation equations [kg / (m^2 *s )]
+     * \param globalPos The position of the integration point of the boundary segment.
+     *
+     * For this method, the \a values parameter stores the mass flux
+     * in normal direction of each phase. Negative values mean influx.
+     */
+    void neumannAtPos(PrimaryVariables &values,
+                      const GlobalPosition &globalPos) const
+    {
+        values = 0.0;
+        if (onInlet_(globalPos)) {
+            values[contiNEqIdx] = -0.04; // kg / (m * s)
+        }
+    }
+    // \}
+
+    /*!
+     * \name Volume terms
+     */
+    // \{
+
+
+    /*!
+     * \brief Evaluate the initial value for a control volume.
+     *
+     * \param values The initial values for the primary variables
+     * \param globalPos The center of the finite volume which ought to be set.
+     *
+     * For this method, the \a values parameter stores primary
+     * variables.
+     */
+    void initialAtPos(PrimaryVariables &values,
+                      const GlobalPosition &globalPos) const
+    {
+        Scalar depth = this->bboxMax()[1] - globalPos[1];
+
+        typename GET_PROP_TYPE(TypeTag, FluidState) fluidState;
+        fluidState.setTemperature(temperature_);
+        fluidState.setPressure(FluidSystem::wPhaseIdx, /*pressure=*/1e5);
+        fluidState.setPressure(FluidSystem::nPhaseIdx, /*pressure=*/1e5);
+
+        Scalar densityW = FluidSystem::density(fluidState, FluidSystem::wPhaseIdx);
+
+        // hydrostatic pressure
+        values[pwIdx] = 1e5 - densityW*this->gravity()[1]*depth;
+        values[SnIdx] = 0.0;
+    }
+    // \}
+
+private:
+
+    bool onLeftBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[0] < this->bboxMin()[0] + eps_;
+    }
+
+    bool onRightBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[0] > this->bboxMax()[0] - eps_;
+    }
+
+    bool onLowerBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[1] < this->bboxMin()[1] + eps_;
+    }
+
+    bool onUpperBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[1] > this->bboxMax()[1] - eps_;
+    }
+
+    bool onInlet_(const GlobalPosition &globalPos) const
+    {
+        Scalar width = this->bboxMax()[0] - this->bboxMin()[0];
+        Scalar lambda = (this->bboxMax()[0] - globalPos[0])/width;
+        return onUpperBoundary_(globalPos) && 0.5 < lambda && lambda < 2.0/3.0;
+    }
+public:
+    bool upperRightCorner(const GlobalPosition &globalPos) const
+    {
+        return ((globalPos[0]>bboxMax_[0] - eps_)
+                &&(globalPos[1]>bboxMax_[1] - eps_));
+    }
+    bool isFractureVertexOnBoundary_(const Element &element, int scvIdx) const
+    {
+        const GlobalPosition &globalPos = element.geometry().corner(scvIdx);
+        int globalIdx = this->soil().vertexmapper().map(element, scvIdx, dim);
+
+        int isVF = this->soil().isVertexFracture(globalIdx);
+        return (    (isVF && onUpperBoundary_(globalPos))
+                        ||(isVF && onLowerBoundary_(globalPos))
+                        ||(isVF && onLeftBoundary_(globalPos))
+                        ||(isVF && onRightBoundary_(globalPos))
+                        );
+
+    }
+
+    Scalar temperature_;
+    Scalar eps_;
+
+    GlobalPosition bboxMin_;
+	GlobalPosition bboxMax_;
+	std::vector<bool> isDuneFractureVertex_;
+	std::vector<bool> isDuneFractureEdge_;
+	std::vector<bool> fractureVolFractionVector;
+	std::vector<int> fractureEdgesIdx_;
+	std::vector<Scalar> distNodesOnBoundary;
+	Scalar inletArea;
+	bool no_flow_problem;
+	bool one_phase_problem;
+	bool use_interface_condition;
+	bool coarse_transmissibility;
+	bool inactivate_fractures_;
+
+};
+} //end namespace
+
+#endif
