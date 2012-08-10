ac68ab14252e5050a502937ab50c2d787c6d97d3
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2012-08-10 09:02:31 +0000

    box test 2p: Adapted usage message and parameter file to naming conventions.
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@8831 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/boxmodels/2p/test_2p.cc
+++ test/boxmodels/2p/test_2p.cc
@@ -45,15 +45,15 @@ void usage(const char *progName, const std::string &errorMsg)
                     errorMessageOut += progName;
                     errorMessageOut += " [options]\n";
                     errorMessageOut += errorMsg;
-                    errorMessageOut += "\n\nThe List of Mandatory arguments for this program is:\n"
-                                        "\t-TimeManager.TEnd                               The end of the simulation [s] \n"
-                                        "\t-TimeManager.DtInitial                          The initial timestep size [s] \n"
-                                        "\t-Grid.File                           The file name of the file containing the grid \n"
-                                        "\t                                        definition in DGF format\n"
-                                        "\t-SpatialParameters.LensLowerLeftX   Dimension of the lens [m] \n"
-                                        "\t-SpatialParameters.LensLowerLeftY   Dimension of the lens [m] \n"
-                                        "\t-SpatialParameters.LensUpperRightX  Dimension of the lens [m] \n"
-                                        "\t-SpatialParameters.LensUpperRighty  Dimension of the lens [m] \n"
+                    errorMessageOut += "\n\nThe list of mandatory options for this program is:\n"
+                                        "\t-TimeManager.TEnd               End of the simulation [s] \n"
+                                        "\t-TimeManager.DtInitial          Initial timestep size [s] \n"
+                                        "\t-Grid.File                      Name of the file containing the grid \n"
+                                        "\t                                definition in DGF format\n"
+                                        "\t-SpatialParams.LensLowerLeftX   Dimension of the lens [m] \n"
+                                        "\t-SpatialParams.LensLowerLeftY   Dimension of the lens [m] \n"
+                                        "\t-SpatialParams.LensUpperRightX  Dimension of the lens [m] \n"
+                                        "\t-SpatialParams.LensUpperRighty  Dimension of the lens [m] \n"
                                         "\n";
 
         std::cout << errorMessageOut
