67dad42421e87f2114e02562a8e504fde673b5a8
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2012-08-15 16:52:40 +0000

    MPFA O: keep inclusion of old header files working
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@8882 2fb0f335-1f38-0410-981e-8018bf24f1b0

---
+++ dumux/decoupled/2p/diffusion/fvmpfa/fvmpfaopressure2p.hh
@@ -0,0 +1,10 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+#ifndef DUMUX_FVMPFAOPRESSURE2P_OLD_HH
+#define DUMUX_FVMPFAOPRESSURE2P_OLD_HH
+
+#warning This file is deprecated. Use fvmpfa/omethod/fvmpfaopressure2p.hh instead
+
+#include <dumux/decoupled/2p/diffusion/fvmpfa/omethod/fvmpfaopressure2p.hh>
+
+#endif
---
+++ dumux/decoupled/2p/diffusion/fvmpfa/fvmpfaopressureproperties2p.hh
@@ -0,0 +1,11 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+#ifndef DUMUX_FVMPFAOPROPERTIES2P_OLD_HH
+#define DUMUX_FVMPFAOPROPERTIES2P_OLD_HH
+
+#warning This file is deprecated. Use fvmpfa/omethod/fvmpfaopressureproperties2p.hh or \
+         fvmpfa/omethod/fvmpfaopressurepropertiessimple2p.hh instead
+
+#include <dumux/decoupled/2p/diffusion/fvmpfa/omethod/fvmpfaopressurepropertiessimple2p.hh>
+
+#endif
