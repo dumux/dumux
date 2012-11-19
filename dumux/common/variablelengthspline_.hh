b3d9067f17f90b8ed6dd5b5af4e61dfead54f9db
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2012-11-19 15:16:29 +0000

    release branch: merge commit 9584
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/branches/release-2.2@9585 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/common/variablelengthspline_.hh
+++ dumux/common/variablelengthspline_.hh
@@ -186,8 +186,8 @@ public:
      * This method uses a single STL-compatible container of sampling
      * points, which are assumed to be tuple-like objects storing the
      * X and Y coordinates.  "tuple-like" means that the objects
-     * provide access to the x values via std::get<0>(obj) and to the
-     * y value via std::get<1>(obj) (e.g. std::tuple or
+     * provide access to the x values via Dune::get<0>(obj) and to the
+     * y value via Dune::get<1>(obj) (e.g. std::tuple or
      * std::pair). "STL-compatible" means that the container provides
      * access to iterators using the begin(), end() methods and also
      * provides a size() method. Also, the number of entries in the X
@@ -203,8 +203,8 @@ public:
         typename XYContainer::const_iterator it = points.begin();
         typename XYContainer::const_iterator endIt = points.end();
         for (int i = 0; it != endIt; ++i, ++it) {
-            xPos_[i] = std::get<0>(*it);
-            yPos_[i] = std::get<1>(*it);
+            xPos_[i] = Dune::get<0>(*it);
+            yPos_[i] = Dune::get<1>(*it);
         }
 
         // make a full spline
@@ -333,8 +333,8 @@ public:
      * This method uses a single STL-compatible container of sampling
      * points, which are assumed to be tuple-like objects storing the
      * X and Y coordinates.  "tuple-like" means that the objects
-     * provide access to the x values via std::get<0>(obj) and to the
-     * y value via std::get<1>(obj) (e.g. std::tuple or
+     * provide access to the x values via Dune::get<0>(obj) and to the
+     * y value via Dune::get<1>(obj) (e.g. std::tuple or
      * std::pair). "STL-compatible" means that the container provides
      * access to iterators using the begin(), end() methods and also
      * provides a size() method. Also, the number of entries in the X
@@ -348,8 +348,8 @@ public:
         typename XYContainer::const_iterator it = points.begin();
         typename XYContainer::const_iterator endIt = points.end();
         for (int i = 0; it != endIt; ++i, ++it) {
-            xPos_[i] = std::get<0>(*it);
-            yPos_[i] = std::get<1>(*it);
+            xPos_[i] = Dune::get<0>(*it);
+            yPos_[i] = Dune::get<1>(*it);
         }
 
         // make a natural spline
