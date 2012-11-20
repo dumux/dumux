896e8fefd7c14528d0d49682e1a56465c48d85c7
Christoph Grueninger <christoph.grueninger@iws.uni-stuttgart.de>
2012-11-20 08:58:56 +0000

    [decoupled] Remove files which are deprecated since 2.2. (reviewed by mwolff)
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@9593 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/decoupled/2p/impes/impesproblem2p.hh
+++ dumux/decoupled/2p/impes/impesproblem2p.hh
@@ -26,7 +26,7 @@
 #include <dumux/decoupled/common/impet.hh>
 #include <dumux/decoupled/common/impetproblem.hh>
 #include "impesproperties2p.hh"
-//DEPRECATED: include should be removed!!! -> Goal: Only include property files in the problem!
+//TODO: include should be removed!!! -> Goal: Only include property files in the problem!
 #include "impesproperties2padaptive.hh"
 
 
