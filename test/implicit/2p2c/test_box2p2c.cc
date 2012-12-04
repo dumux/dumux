6f7f334f930a8fcd742a4b2de2fb5572925eaa84
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2012-12-04 13:16:49 +0000

    implicit branch: unify 2p2c test problem
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/branches/implicit@9764 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- test/implicit/2p2c/test_box2p2c.cc
+++ test/implicit/2p2c/test_box2p2c.cc
@@ -62,6 +62,6 @@ void usage(const char *progName, const std::string &errorMsg)
 
 int main(int argc, char** argv)
 {
-    typedef TTAG(InjectionProblem) ProblemTypeTag;
+    typedef TTAG(InjectionBoxProblem) ProblemTypeTag;
     return Dumux::start<ProblemTypeTag>(argc, argv, usage);
 }
