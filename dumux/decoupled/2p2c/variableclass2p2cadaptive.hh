b421e504978e2ede6f3bb65130169f1ff98d0ad1
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2013-05-24 07:29:06 +0000

    fix copyright preambles
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@10747 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/decoupled/2p2c/variableclass2p2cadaptive.hh
+++ dumux/decoupled/2p2c/variableclass2p2cadaptive.hh
@@ -1,19 +1,16 @@
 // -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
 // vi: set et ts=4 sw=4 sts=4:
 /*****************************************************************************
- *   Copyright (C) 2012 by Benjamin Faigle                                   *
- *   Institute for Modelling Hydraulic and Environmental Systems             *
- *   University of Stuttgart, Germany                                        *
- *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *   See the file COPYING for full copying permissions.                      *
  *                                                                           *
- *   T1his program is free software: you can redistribute it and/or modify    *
+ *   This program is free software: you can redistribute it and/or modify    *
  *   it under the terms of the GNU General Public License as published by    *
  *   the Free Software Foundation, either version 2 of the License, or       *
  *   (at your option) any later version.                                     *
  *                                                                           *
- *   T1his program is distributed in the hope that it will be useful,         *
+ *   This program is distributed in the hope that it will be useful,         *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
  *   GNU General Public License for more details.                            *
  *                                                                           *
  *   You should have received a copy of the GNU General Public License       *
--- test/decoupled/2p2c/test_adaptive2p2c.cc
+++ test/decoupled/2p2c/test_adaptive2p2c.cc
@@ -1,18 +1,20 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
 /*****************************************************************************
- *   Copyright (C) 2010 by Benjamin Faigle                                   *
- *   Copyright (C) 20010 by Markus Wolff                                     *
- *   Copyright (C) 2007-2008 by Bernd Flemisch                               *
- *   Institute of Hydraulic Engineering                                      *
- *   University of Stuttgart, Germany                                        *
- *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *   See the file COPYING for full copying permissions.                      *
  *                                                                           *
- *   This program is free software; you can redistribute it and/or modify    *
+ *   This program is free software: you can redistribute it and/or modify    *
  *   it under the terms of the GNU General Public License as published by    *
- *   the Free Software Foundation; either version 2 of the License, or       *
- *   (at your option) any later version, as long as this copyright notice    *
- *   is included in its original form.                                       *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
  *                                                                           *
- *   This program is distributed WITHOUT ANY WARRANTY.                       *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  *****************************************************************************/
 /*!
  * \file
