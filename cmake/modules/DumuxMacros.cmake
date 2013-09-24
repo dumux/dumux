c27ad85428cca1cb13c671a0b71124ee57075637
Christoph Grueninger <christoph.grueninger@iws.uni-stuttgart.de>
2013-09-24 08:34:26 +0000

    [CMake] Overhaul CMake files to support modules depending on dumux.
    (reviewed by bernd)
    
    
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@11551 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- cmake/modules/DumuxMacros.cmake
+++ cmake/modules/DumuxMacros.cmake
@@ -1,6 +1,6 @@
 # checks
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules/CheckAlwaysInline.cmake)
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules/CheckConstexpr.cmake)
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules/CheckPatchedPDELab.cmake)
+include(CheckAlwaysInline)
+include(CheckConstexpr)
+include(CheckPatchedPDELab)
 # additional macros
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules/DumuxTestMacros.cmake)
+include(DumuxTestMacros)
