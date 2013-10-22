b7416f8eac73822fd0d133515d785035a1062791
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2013-10-22 09:14:26 +0000

    remove deprecated 1p cellData and fluxData files
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@11770 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/decoupled/1p/cellData1p.hh
+++
@@ -1,2 +0,0 @@
-#warning This file is deprecated. Use celldata1p.hh (non-capitalized) instead.
-#include "celldata1p.hh"
--- dumux/decoupled/1p/fluxData1p.hh
+++
@@ -1,2 +0,0 @@
-#warning This file is deprecated. Use fluxdata1p.hh (non-capitalized) instead.
-#include "fluxdata1p.hh"
