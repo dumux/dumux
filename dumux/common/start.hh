14c2dd96406569b785e588537149b602f54b497d
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2014-09-17 14:48:48 +0000

    correct usage of dune-alugrid
    CMake defines HAVE_DUNE_ALUGRID and sets it to 0 or 1. Therefore, #ifdef
    does not work here.
    
    
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@13353 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/common/start.hh
+++ dumux/common/start.hh
@@ -30,7 +30,7 @@
 #include <dune/common/parallel/mpihelper.hh>
 
 #include <dune/grid/io/file/dgfparser.hh>
-#ifdef HAVE_DUNE_ALUGRID
+#if HAVE_DUNE_ALUGRID
 #include <dune/alugrid/dgf.hh>
 #endif // HAVE_DUNE_ALUGRID
 
