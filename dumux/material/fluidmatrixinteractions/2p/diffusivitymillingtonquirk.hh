2b08f44b167478a61c83fcb1847e3556621d6d29
Thomas Fetzer <thomas.fetzer@iws.uni-stuttgart.de>
2014-09-22 11:58:31 +0000

    [material] removed file which was deprecated a year ago
    reviewed by martinb
    
    
    git-svn-id: svn://svn.iws.uni-stuttgart.de/DUMUX/dumux/trunk@13382 2fb0f335-1f38-0410-981e-8018bf24f1b0

--- dumux/material/fluidmatrixinteractions/2p/diffusivitymillingtonquirk.hh
+++
@@ -1,3 +0,0 @@
-#warning This file is deprecated. Include dumux/material/fluidmatrixinteractions/diffusivitymillingtonquirk.hh instead.
-
-#include <dumux/material/fluidmatrixinteractions/diffusivitymillingtonquirk.hh>
