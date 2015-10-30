68251a741de4ce19de20d5f86dcbcc8c9109264d
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2015-10-30 10:00:03 +0100

    [doc][pointsource] Correct unit to conserved quantity per sec

--- dumux/implicit/common/implicitproblem.hh
+++ dumux/implicit/common/implicitproblem.hh
@@ -448,8 +448,9 @@ public:
               source values for all phases and space positions.
      *
      * For this method, the \a values method of the point source
-     * has to return the absolute mass rate in kg/s. Positive values mean
-     * that mass is created, negative ones mean that it vanishes.
+     * has to return the absolute mass rate in untis
+     * \f$ [ \textnormal{unit of conserved quantity} / s ] \f$.
+     * Positive values mean that mass is created, negative ones mean that it vanishes.
      */
     void addPointSources(std::vector<PointSource>& pointSources) const {}
 
