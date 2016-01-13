1c577beb129162f4ce94b2bb38476e9ce1e38f27
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2016-01-13 10:00:01 +0100

    [doxygen] Add target only if doxygen was found
    The dune macro used to add the target only adds it if doxygen was found.
    Then the target was not know in add_custom_command which is an error
    in CMake > v3.2.

--- cmake/modules/DumuxDoxygen.cmake
+++ cmake/modules/DumuxDoxygen.cmake
@@ -2,8 +2,10 @@
 #
 # make sure, that the doxygen links to todo list, bibliography, etc. are correct
 MACRO (add_dumux_doxygen_target)
+if(DOXYGEN_FOUND)
   add_doxygen_target()
   add_custom_command(TARGET doxygen_${ProjectName}
                      POST_BUILD
                      COMMAND ${CMAKE_SOURCE_DIR}/doc/doxygen/sanitizelinks.sh)
+endif()
 ENDMACRO (add_dumux_doxygen_target)
