8eb2ad3c3216251ab906f08eb0d3f612626e3777
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2016-01-20 12:11:54 +0100

    [doxygen] Fix shell function syntax
    The function definition requires brackets (). The function keyword
    is unnecessary and results in less portable shell code, see:
    http://unix.stackexchange.com/questions/73750/difference-between-function-foo-and-foo

--- doc/doxygen/sanitizelinks.sh
+++ doc/doxygen/sanitizelinks.sh
@@ -1,6 +1,6 @@
 # sanitizes the links to the given lists, because doxygen somehow links
 # to a page with a wrong index
-function sanitizelinks {
+sanitizelinks () {
   NEW_FILE=`grep -l "\"title\">$1" html/*html | egrep -o [0-9]+`
   OLD_FILE=`awk -v a=$NEW_FILE 'BEGIN {printf("%05d", a-1)}'`
   sed -i "s/$OLD_FILE/$NEW_FILE/g" html/*html
