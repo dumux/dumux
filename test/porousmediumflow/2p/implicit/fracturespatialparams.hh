b9781185986664c43df880e32a079689ace8f87c
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2016-07-22 17:25:44 +0200

    [test] Add discrete fracture 2d in 3d box 2p test

---
+++ test/porousmediumflow/2p/implicit/fracturespatialparams.hh
@@ -0,0 +1,157 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief The spatial parameters for the LensProblem which uses the
+ *        two-phase fully implicit model
+ */
+#ifndef DUMUX_TWOP_FRACTURE_TEST_SPATIALPARAMS_HH
+#define DUMUX_TWOP_FRACTURE_TEST_SPATIALPARAMS_HH
+
+#include <dumux/material/spatialparams/implicit.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/regularizedvangenuchten.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/linearmaterial.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/efftoabslaw.hh>
+
+#include <dumux/porousmediumflow/2p/implicit/model.hh>
+
+namespace Dumux
+{
+
+//forward declaration
+template<class TypeTag>
+class FractureSpatialParams;
+
+namespace Properties
+{
+// The spatial parameters TypeTag
+NEW_TYPE_TAG(FractureSpatialParams);
+
+// Set the spatial parameters
+SET_TYPE_PROP(FractureSpatialParams, SpatialParams, Dumux::FractureSpatialParams<TypeTag>);
+
+// Set the material Law
+SET_PROP(FractureSpatialParams, MaterialLaw)
+{
+private:
+    // define the material law which is parameterized by effective
+    // saturations
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+    using EffectiveLaw = RegularizedVanGenuchten<Scalar>;
+public:
+    // define the material law parameterized by absolute saturations
+    using type = EffToAbsLaw<EffectiveLaw>;
+};
+}
+/*!
+ * \ingroup TwoPModel
+ * \ingroup ImplicitTestProblems
+ * \brief The spatial parameters for the LensProblem which uses the
+ *        two-phase fully implicit model
+ */
+template<class TypeTag>
+class FractureSpatialParams : public ImplicitSpatialParams<TypeTag>
+{
+    using ParentType = ImplicitSpatialParams<TypeTag>;
+    using Grid = typename GET_PROP_TYPE(TypeTag, Grid);
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+    using CoordScalar = typename Grid::ctype;
+
+    enum {
+        dim=GridView::dimension,
+        dimWorld=GridView::dimensionworld
+    };
+
+    using GlobalPosition = Dune::FieldVector<CoordScalar,dimWorld>;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using FVElementGeometry = typename GET_PROP_TYPE(TypeTag, FVElementGeometry);
+
+public:
+    //get the material law from the property system
+    using MaterialLaw = typename GET_PROP_TYPE(TypeTag, MaterialLaw);
+    using MaterialLawParams = typename MaterialLaw::Params;
+
+    /*!
+     * \brief The constructor
+     *
+     * \param gridView The grid view
+     */
+    FractureSpatialParams(const GridView& gridView)
+    : ParentType(gridView)
+    {
+        // residual saturations
+        materialParams_.setSwr(0.05);
+        materialParams_.setSnr(0.0);
+
+        // parameters for the Van Genuchten law
+        // alpha and n
+        materialParams_.setVgAlpha(0.0037);
+        materialParams_.setVgn(4.7);
+    }
+
+    /*!
+     * \brief Returns the scalar intrinsic permeability \f$[m^2]\f$
+     *
+     * \param element The finite element
+     * \param fvGeometry The finite volume geometry of the element
+     * \param scvIdx The local index of the sub-control volume
+     */
+    Scalar intrinsicPermeability(const Element &element,
+                                 const FVElementGeometry &fvGeometry,
+                                 int scvIdx) const
+    {
+        return 1e-10;
+    }
+
+    /*!
+     * \brief Returns the porosity \f$[-]\f$
+     *
+     * \param element The finite element
+     * \param fvGeometry The finite volume geometry of the element
+     * \param scvIdx The local index of the sub-control volume
+     */
+    Scalar porosity(const Element &element,
+                    const FVElementGeometry &fvGeometry,
+                    int scvIdx) const
+    { return 0.4; }
+
+    /*!
+     * \brief Returns the parameter object for the Brooks-Corey material law
+     *
+     * \param element The finite element
+     * \param fvGeometry The finite volume geometry of the element
+     * \param scvIdx The local index of the sub-control volume
+     */
+    const MaterialLawParams& materialLawParams(const Element &element,
+                                                const FVElementGeometry &fvGeometry,
+                                                int scvIdx) const
+    {
+        return materialParams_;
+    }
+
+
+private:
+    MaterialLawParams materialParams_;
+};
+
+} // end namespace
+
+#endif
