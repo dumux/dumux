7a615f4ea2b6a187af0a6dfc5933508724ce4799
Martin Beck <martin.beck@iws.uni-stuttgart.de>
2016-09-27 16:35:15 +0200

    [fix/doxygen2.10] Fixed doxygen description in steaminjectionproblem.hh

--- test/porousmediumflow/2p1c/implicit/steaminjectionproblem.hh
+++ test/porousmediumflow/2p1c/implicit/steaminjectionproblem.hh
@@ -203,12 +203,8 @@ public:
      * \brief Evaluate the boundary conditions for a neumann
      *        boundary segment.
      *
-     * \param values The neumann values for the conservation equations
-     * \param element The finite element
-     * \param fvGeomtry The finite-volume geometry in the box scheme
-     * \param is The intersection between element and boundary
-     * \param scvIdx The local vertex index
-     * \param boundaryFaceIdx The index of the boundary face
+     * \param values The dirichlet values for the primary variables
+     * \param globalPos The position for which the bc type should be evaluated
      *
      * For this method, the \a values parameter stores the mass flux
      * in normal direction of each phase. Negative values mean influx.
