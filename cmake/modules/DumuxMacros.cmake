b0ea36b82a905a654ff93db8f870df3e78c5d428
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2016-11-14 14:29:51 +0100

    [cmake] Add NLOPT and GLPK cmake find scripts
    The NLOPT and GLPK are libraries for nonlinear and linear
    optimization algorithms.

--- cmake/modules/DumuxMacros.cmake
+++ cmake/modules/DumuxMacros.cmake
@@ -4,9 +4,11 @@ include(AddInputFileLinks)
 include(DumuxDoxygen)
 include(DumuxTestMacros)
 
+find_package(GLPK)
 find_package(Gstat)
 find_package(Gnuplot)
 set(HAVE_GNUPLOT ${GNUPLOT_FOUND})
 
+find_package(NLOPT)
 find_package(Valgrind)
 find_package(PTScotch)
