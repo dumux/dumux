cc731c92eaf6b32d5bc4b407841a4058a9e9d544
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2016-12-05 14:37:06 +0100

    [test][2d3d] Add discrete fracture 2p (2d in 3d) cc tpfa test

---
+++ test/porousmediumflow/2p/implicit/test_fracture_cc2p.cc
@@ -0,0 +1,52 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief Fracture 2d in 3d test for the two-phase box model
+ */
+#include <config.h>
+#include "fractureproblem.hh"
+#include <dumux/common/start.hh>
+
+/*!
+ * \brief Provides an interface for customizing error messages associated with
+ *        reading in parameters.
+ *
+ * \param progName  The name of the program, that was tried to be started.
+ * \param errorMsg  The error message that was issued by the start function.
+ *                  Comprises the thing that went wrong and a general help message.
+ */
+void usage(const char *progName, const std::string &errorMsg)
+{}
+
+////////////////////////
+// the main function
+////////////////////////
+int main(int argc, char** argv)
+{
+#if HAVE_DUNE_FOAMGRID
+    typedef TTAG(FractureCCProblem) TypeTag;
+    return Dumux::start<TypeTag>(argc, argv, usage);
+#else
+#warning External grid module dune-foamgrid needed to run this example.
+    std::cerr << "Test skipped, it needs dune-foamgrid!" << std::endl;
+    return 77;
+#endif
+}
