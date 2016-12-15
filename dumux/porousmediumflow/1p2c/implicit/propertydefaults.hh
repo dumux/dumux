d7d7ad3d0c0237a329c25f08ff0b093308d43e7a
DennisGlaeser <dennis.glaeser@iws.uni-stuttgart.de>
2016-12-15 18:08:42 +0100

    [1p2c] remove unused Scaling property

--- dumux/porousmediumflow/1p2c/implicit/propertydefaults.hh
+++ dumux/porousmediumflow/1p2c/implicit/propertydefaults.hh
@@ -54,7 +54,6 @@ namespace Properties
 SET_INT_PROP(OnePTwoC, NumEq, 2); //!< set the number of equations to 2
 SET_INT_PROP(OnePTwoC, NumPhases, 1); //!< The number of phases in the 1p2c model is 1
 SET_INT_PROP(OnePTwoC, NumComponents, 2); //!< The number of components in the 1p2c model is 2
-SET_SCALAR_PROP(OnePTwoC, Scaling, 1); //!< Scaling of the model is set to 1 by default
 SET_BOOL_PROP(OnePTwoC, UseMoles, true); //!< Define that mole fractions are used in the balance equations
 
 //! Use the 1p2c local residual function for the 1p2c model
