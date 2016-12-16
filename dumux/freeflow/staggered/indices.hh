864019741a64faf3aa56fddde333e12632e65c2f
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2016-12-16 11:01:37 +0100

    [staggeredGrid][indices] Revise indices for Navier-Stokes
    *Remove unused ones
    *introduce velocityIdx

--- dumux/freeflow/staggered/indices.hh
+++ dumux/freeflow/staggered/indices.hh
@@ -36,29 +36,19 @@ namespace Dumux
 template <class TypeTag, int PVOffset = 0>
 struct NavierStokesCommonIndices
 {
-    // number of dimensions
-    typedef typename GET_PROP_TYPE(TypeTag, Grid) Grid;
-    static const int dim = Grid::dimension;
 
-    // Primary variable indices
-    static const int momentumXIdx = PVOffset + 0; //!< Index of the x-component of the momentum equation
-    static const int momentumYIdx = PVOffset + 1; //!< Index of the y-component of the momentum equation
-    static const int momentumZIdx = PVOffset + 2; //!< Index of the z-component of the momentum equation
-    static const int lastMomentumIdx = momentumXIdx+dim-1; //!< Index of the last component of the momentum equation
+    static constexpr int dimXIdx = 0; //!< Index of the x-component of a vector of size dim
+    static constexpr int dimYIdx = 1; //!< Index of the y-component of a vector of size dim
+    static constexpr int dimZIdx = 2; //!< Index of the z-component of a vector of size dim
 
-    static const int dimXIdx = 0; //!< Index of the x-component of a vector of size dim
-    static const int dimYIdx = 1; //!< Index of the y-component of a vector of size dim
-    static const int dimZIdx = 2; //!< Index of the z-component of a vector of size dim
+    static constexpr int massBalanceIdx = PVOffset + 0; //!< Index of the mass balance equation
+    static constexpr int conti0EqIdx = massBalanceIdx; //!< Index of first (for C-guys: 0th) mass conservation equation
+    static constexpr int pressureIdx = massBalanceIdx; //!< Index of the pressure in a solution vector
 
-    static const int massBalanceIdx = PVOffset + 0; //!< Index of the mass balance equation
-    static const int conti0EqIdx = massBalanceIdx; //!< Index of first (for C-guys: 0th) mass conservation equation
+    static constexpr int momentumBalanceIdx = PVOffset + GET_PROP_VALUE(TypeTag, NumEqCellCenter); //!< Index of the momentum balance equation
+    static constexpr int velocityIdx = PVOffset + 0; //!< Index of the velocity in a solution vector (NOTE: This PV lives in a different vector than the pressure)
 
-    static const int pressureIdx = massBalanceIdx; //!< Index of the pressure in a solution vector
-    static const int velocityXIdx = momentumXIdx; //!< Index of the x-component of the velocity
-    static const int velocityYIdx = momentumYIdx; //!< Index of the y-component of the velocity
-    static const int velocityZIdx = momentumZIdx; //!< Index of the z-component of the velocity
-
-    static const int phaseIdx = 0; //!< Index of the fluid phase (required to use the same fluid system in coupled models)
+    static constexpr int phaseIdx = 0; //!< Index of the fluid phase (required to use the same fluid system in coupled models)
 };
 
 // \}
