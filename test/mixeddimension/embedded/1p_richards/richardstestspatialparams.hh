f2cd89f9f22dcf1de29d69e2f5e9ffc8092d4d90
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2017-01-18 20:33:29 +0100

    [mixeddimension][embedded] Add 1p_richards test (root--soil)

---
+++ test/mixeddimension/embedded/1p_richards/richardstestspatialparams.hh
@@ -0,0 +1,139 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief spatial parameters for the RichardsTestProblem
+ */
+#ifndef DUMUX_RICHARDS_TEST_SPATIAL_PARAMETERS_HH
+#define DUMUX_RICHARDS_TEST_SPATIAL_PARAMETERS_HH
+
+#include <dumux/material/spatialparams/implicit.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/regularizedvangenuchten.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/linearmaterial.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/efftoabslaw.hh>
+
+namespace Dumux
+{
+template<class TypeTag>
+class RichardsTestSpatialParams;
+
+namespace Properties
+{
+// The spatial parameters TypeTag
+NEW_TYPE_TAG(RichardsTestSpatialParams);
+
+// Set the spatial parameters
+SET_TYPE_PROP(RichardsTestSpatialParams, SpatialParams, RichardsTestSpatialParams<TypeTag>);
+
+// Set the material law
+SET_PROP(RichardsTestSpatialParams, MaterialLaw)
+{
+private:
+    // define the material law which is parameterized by effective
+    // saturations
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+public:
+    // define the material law parameterized by absolute saturations
+    using type = EffToAbsLaw<RegularizedVanGenuchten<Scalar>>;
+};
+} // end namespace Properties
+
+/*!
+ * \ingroup RichardsModel
+ * \ingroup ImplicitTestProblems
+ * \brief The spatial parameters for the RichardsTestProblem
+ */
+template<class TypeTag>
+class RichardsTestSpatialParams : public ImplicitSpatialParams<TypeTag>
+{
+    using ParentType = ImplicitSpatialParams<TypeTag>;
+    using Problem = typename GET_PROP_TYPE(TypeTag, Problem);
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+
+    enum {
+        dim=GridView::dimension,
+        dimWorld=GridView::dimensionworld
+    };
+
+    using GlobalPosition = Dune::FieldVector<Scalar, dimWorld>;
+    using MaterialLaw = typename GET_PROP_TYPE(TypeTag, MaterialLaw);
+    using MaterialLawParams = typename MaterialLaw::Params;
+
+public:
+    // export permeability type
+    using PermeabilityType = Scalar;
+
+    /*!
+     * \brief Constructor
+     *
+     * \param gridView The DUNE GridView representing the spatial
+     *                 domain of the problem.
+     */
+    RichardsTestSpatialParams(const Problem& problem, const GridView& gridView)
+        : ParentType(problem, gridView)
+    {
+       // residual saturations
+        materialParams_.setSwr(0.05);
+        materialParams_.setSnr(0.0);
+
+        // parameters for the Van Genuchten law
+        // alpha and n
+        materialParams_.setVgAlpha(2.956e-4);
+        materialParams_.setVgn(1.5);
+
+        permeability_ = GET_RUNTIME_PARAM(TypeTag, Scalar, SpatialParams.Permeability);
+    }
+
+    /*!
+     * \brief Returns the intrinsic permeability tensor [m^2] at a given location
+     *
+     * \param globalPos The global position where we evaluate
+     */
+    PermeabilityType permeabilityAtPos(const GlobalPosition& globalPos) const
+    { return permeability_; }
+
+    /*!
+     * \brief Returns the porosity [] at a given location
+     *
+     * \param globalPos The global position where we evaluate
+     */
+    Scalar porosityAtPos(const GlobalPosition& globalPos) const
+    { return 0.4; }
+
+    /*!
+     * \brief Returns the parameters for the material law at a given location
+     *
+     * This method is not actually required by the Richards model, but provided
+     * for the convenience of the RichardsLensProblem
+     *
+     * \param globalPos A global coordinate vector
+     */
+    const MaterialLawParams& materialLawParamsAtPos(const GlobalPosition &globalPos) const
+    { return materialParams_; }
+
+private:
+    MaterialLawParams materialParams_;
+    Scalar permeability_;
+};
+
+} // end namespace Dumux
+
+#endif
