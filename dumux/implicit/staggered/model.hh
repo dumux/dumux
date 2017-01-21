15790fe3349f2a35c82d592b25cc39ed5b14d105
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2017-01-21 16:24:18 +0100

    [staggeredGrid][model] Remove stencils-related functions

--- dumux/implicit/staggered/model.hh
+++ dumux/implicit/staggered/model.hh
@@ -518,38 +518,6 @@ public:
         return this->gridView_().size(1);
     }
 
-    /*!
-    * \brief Returns the size of a complete face dof stencil
-    */
-    size_t fullFaceToCellCenterStencilSize(const int idx) const
-    {
-        return this->stencilsVector_.fullFaceToCellCenterStencilSize(idx);
-    }
-
-    /*!
-    * \brief Returns the size of a complete face dof stencil
-    */
-    size_t fullfaceToFaceStencilSize(const int idx) const
-    {
-        return this->stencilsVector_.fullfaceToFaceStencilSize(idx);
-    }
-
-    /*!
-    * \brief Returns a unique pointer to the complete face dof stencils which is used once for setting up the global matrix and deleted afterwards
-    */
-    auto getFullFaceToCellCenterStencilsPtr()
-    {
-        return this->stencilsVector_.getFullFaceToCellCenterStencilsPtr();
-    }
-
-    /*!
-    * \brief Returns a unique pointer to the complete face dof stencils which is used once for setting up the global matrix and deleted afterwards
-    */
-    auto getFullfaceToFaceStencilsPtr()
-    {
-        return this->stencilsVector_.getFullfaceToFaceStencilsPtr();
-    }
-
     const GlobalFaceVariables& curGlobalFaceVars() const
     { return curGlobalFaceVariables_; }
 
