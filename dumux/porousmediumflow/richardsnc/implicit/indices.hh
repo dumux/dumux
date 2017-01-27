65c6463920ae5aa50fb462e38bc47eb4ebd1a34e
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2017-01-27 18:28:34 +0100

    [richardsnc] Implement a Richards n-component model
    The components are only in the water phase. The number of components
    is statically extracted from the fluidsystem. Simply changing the
    fluid system and boundary and initial conditions is sufficient to setup
    a new model with n tracers.

---
+++ dumux/porousmediumflow/richardsnc/implicit/indices.hh
@@ -0,0 +1,63 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \brief Defines the primary variable and equation indices used by
+ *        the richardsnc model
+ */
+
+#ifndef DUMUX_RICHARDSNC_INDICES_HH
+#define DUMUX_RICHARDSNC_INDICES_HH
+
+#include "properties.hh"
+
+namespace Dumux
+{
+// \{
+
+/*!
+ * \ingroup RichardsNCModel
+ * \ingroup ImplicitIndices
+ * \brief The indices for the isothermal Richards, n-component model.
+ */
+template <class TypeTag, int PVOffset = 0>
+struct RichardsNCIndices
+{
+
+    //! Set the index of the phases for accessing the volvars
+    static const int wPhaseIdx = 0;
+    static const int nPhaseIdx = 1;
+
+    //! Component indices
+    static const int compMainIdx = PVOffset + 0; //!< main component index
+
+    //! primary variable indices
+    static const int pressureIdx = PVOffset + 0; //!< pressure
+
+    //! \note These indices make sense if the first balance is replaced by the
+    //!       total mass balance.
+
+    //! Equation indices
+    static const int conti0EqIdx = PVOffset + 0; //!< continuity equation index
+};
+
+// \}
+}
+
+#endif
---
+++ dumux/porousmediumflow/richardsnc/implicit/volumevariables.hh
@@ -0,0 +1,392 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \brief @copybrief Dumux::RichardsNCVolumeVariables
+ */
+#ifndef DUMUX_RICHARDSNC_VOLUME_VARIABLES_HH
+#define DUMUX_RICHARDSNC_VOLUME_VARIABLES_HH
+
+#include <dumux/discretization/volumevariables.hh>
+
+#include "properties.hh"
+
+namespace Dumux
+{
+
+/*!
+ * \ingroup RichardsNCModel
+ * \ingroup ImplicitVolumeVariables
+ * \brief Contains the quantities which are constant within a
+ *        finite volume in the Richards, n-component model.
+ */
+template <class TypeTag>
+class RichardsNCVolumeVariables : public ImplicitVolumeVariables<TypeTag>
+{
+    using ParentType = ImplicitVolumeVariables<TypeTag>;
+
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+    using Problem = typename GET_PROP_TYPE(TypeTag, Problem);
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using Implementation = typename GET_PROP_TYPE(TypeTag, VolumeVariables);
+    using SubControlVolume = typename GET_PROP_TYPE(TypeTag, SubControlVolume);
+    using PrimaryVariables = typename GET_PROP_TYPE(TypeTag, PrimaryVariables);
+    using ElementSolutionVector = typename GET_PROP_TYPE(TypeTag, ElementSolutionVector);
+    using SpatialParams = typename GET_PROP_TYPE(TypeTag, SpatialParams);
+    using PermeabilityType = typename SpatialParams::PermeabilityType;
+    using FluidSystem = typename GET_PROP_TYPE(TypeTag, FluidSystem);
+    using Indices = typename GET_PROP_TYPE(TypeTag, Indices);
+
+    enum
+    {
+        wPhaseIdx = Indices::wPhaseIdx,
+        nPhaseIdx = Indices::nPhaseIdx,
+        pressureIdx = Indices::pressureIdx
+    };
+
+    static const bool useMoles = GET_PROP_VALUE(TypeTag, UseMoles);
+    static const int dim = GridView::dimension;
+    static const int dimWorld = GridView::dimensionworld;
+    static const int numComponents = GET_PROP_VALUE(TypeTag, NumComponents);
+
+    using MaterialLaw = typename GET_PROP_TYPE(TypeTag, MaterialLaw);
+    using GlobalPosition = Dune::FieldVector<Scalar,dimWorld>;
+    using Element = typename GridView::template Codim<0>::Entity;
+
+public:
+
+    using FluidState = typename GET_PROP_TYPE(TypeTag, FluidState);
+
+    /*!
+     * \copydoc ImplicitVolumeVariables::update
+     */
+    void update(const ElementSolutionVector &elemSol,
+                const Problem &problem,
+                const Element &element,
+                const SubControlVolume &scv)
+    {
+        ParentType::update(elemSol, problem, element, scv);
+
+        //calculate all secondary variables from the primary variables and store results in fluidstate
+        completeFluidState(elemSol, problem, element, scv, fluidState_);
+
+        const auto& materialParams = problem.spatialParams().materialLawParams(element, scv, elemSol);
+        relativePermeabilityWetting_ = MaterialLaw::krw(materialParams, fluidState_.saturation(wPhaseIdx));
+
+        //! precompute the minimum capillary pressure (entry pressure)
+        minPc_ = MaterialLaw::pc(materialParams, /*Sw=*/ 1.0);
+        pn_ = problem.nonWettingReferencePressure();
+        porosity_ = problem.spatialParams().porosity(element, scv, elemSol);
+        permeability_ = problem.spatialParams().permeability(element, scv, elemSol);
+
+        // Second instance of a parameter cache.
+        // Could be avoided if diffusion coefficients also
+        // became part of the fluid state.
+        typename FluidSystem::ParameterCache paramCache;
+        paramCache.updatePhase(fluidState_, wPhaseIdx);
+
+        const int compIIdx = wPhaseIdx;
+        for (unsigned int compJIdx = 0; compJIdx < numComponents; ++compJIdx)
+            if(compIIdx != compJIdx)
+                setDiffusionCoefficient_(compJIdx,
+                                         FluidSystem::binaryDiffusionCoefficient(fluidState_,
+                                                                                 paramCache,
+                                                                                 wPhaseIdx,
+                                                                                 compIIdx,
+                                                                                 compJIdx));
+    }
+
+    /*!
+     * \copydoc ImplicitModel::completeFluidState
+     */
+    static void completeFluidState(const ElementSolutionVector &elemSol,
+                                   const Problem& problem,
+                                   const Element& element,
+                                   const SubControlVolume &scv,
+                                   FluidState& fluidState)
+    {
+        Scalar t = ParentType::temperature(elemSol, problem, element, scv);
+        fluidState.setTemperature(t);
+
+        const auto& materialParams = problem.spatialParams().materialLawParams(element, scv, elemSol);
+        const auto& priVars = ParentType::extractDofPriVars(elemSol, scv);
+
+        // set the wetting pressure
+        fluidState.setPressure(wPhaseIdx, priVars[pressureIdx]);
+
+        // compute the capillary pressure to compute the saturation
+        // the material law takes care of cases where pc gets negative
+        const Scalar pc = problem.nonWettingReferencePressure() - fluidState.pressure(wPhaseIdx);
+        const Scalar sw = MaterialLaw::sw(materialParams, pc);
+        fluidState.setSaturation(wPhaseIdx, sw);
+
+        // set the mole/mass fractions
+        if(useMoles)
+        {
+            Scalar sumSecondaryFractions = 0.0;
+            for (int compIdx = 1; compIdx < numComponents; ++compIdx)
+            {
+                fluidState.setMoleFraction(wPhaseIdx, compIdx, priVars[compIdx]);
+                sumSecondaryFractions += priVars[compIdx];
+            }
+            fluidState.setMoleFraction(wPhaseIdx, 0, 1.0 - sumSecondaryFractions);
+        }
+        else
+        {
+            for (int compIdx = 1; compIdx < numComponents; ++compIdx)
+                fluidState.setMassFraction(wPhaseIdx, compIdx, priVars[compIdx]);
+        }
+
+        // density and viscosity
+        typename FluidSystem::ParameterCache paramCache;
+        paramCache.updateAll(fluidState);
+        fluidState.setDensity(wPhaseIdx, FluidSystem::density(fluidState, paramCache, wPhaseIdx));
+        fluidState.setViscosity(wPhaseIdx, FluidSystem::viscosity(fluidState, paramCache, wPhaseIdx));
+
+        // compute and set the enthalpy
+        fluidState.setEnthalpy(wPhaseIdx, Implementation::enthalpy(fluidState, paramCache, wPhaseIdx));
+    }
+
+    /*!
+     * \brief Return the fluid configuration at the given primary
+     *        variables
+     */
+    const FluidState &fluidState() const
+    { return fluidState_; }
+
+    /*!
+     * \brief Return the temperature
+     */
+    Scalar temperature() const
+    { return fluidState_.temperature(); }
+
+    /*!
+     * \brief Returns the average porosity [] within the control volume.
+     *
+     * The porosity is defined as the ratio of the pore space to the
+     * total volume, i.e. \f[ \Phi := \frac{V_{pore}}{V_{pore} + V_{rock}} \f]
+     */
+    Scalar porosity() const
+    { return porosity_; }
+
+    /*!
+     * \brief Returns the permeability within the control volume in \f$[m^2]\f$.
+     */
+    const PermeabilityType& permeability() const
+    { return permeability_; }
+
+    /*!
+     * \brief Returns the average absolute saturation [] of a given
+     *        fluid phase within the finite volume.
+     *
+     * The saturation of a fluid phase is defined as the fraction of
+     * the pore volume filled by it, i.e.
+     * \f[ S_\alpha := \frac{V_\alpha}{V_{pore}} = \phi \frac{V_\alpha}{V} \f]
+     *
+     * \param phaseIdx The index of the fluid phase
+     */
+    Scalar saturation(const int phaseIdx) const
+    { return phaseIdx == wPhaseIdx ? fluidState_.saturation(wPhaseIdx) : 1.0-fluidState_.saturation(wPhaseIdx); }
+
+    /*!
+     * \brief Returns the average mass density \f$\mathrm{[kg/m^3]}\f$ of a given
+     *        fluid phase within the control volume.
+     *
+     * \param phaseIdx The index of the fluid phase
+     */
+    Scalar density(const int phaseIdx) const
+    { return phaseIdx == wPhaseIdx ? fluidState_.density(phaseIdx) : 0.0; }
+
+    /*!
+     * \brief Returns the effective pressure \f$\mathrm{[Pa]}\f$ of a given phase within
+     *        the control volume.
+     *
+     * For the non-wetting phase (i.e. the gas phase), we assume
+     * infinite mobility, which implies that the non-wetting phase
+     * pressure is equal to the finite volume's reference pressure
+     * defined by the problem.
+     *
+     * \param phaseIdx The index of the fluid phase
+     */
+    Scalar pressure(const int phaseIdx) const
+    { return phaseIdx == wPhaseIdx ? fluidState_.pressure(phaseIdx) : pn_; }
+
+    /*!
+     * \brief Returns the effective mobility \f$\mathrm{[1/(Pa*s)]}\f$ of a given phase within
+     *        the control volume.
+     *
+     * The mobility of a fluid phase is defined as the relative
+     * permeability of the phase (given by the chosen material law)
+     * divided by the dynamic viscosity of the fluid, i.e.
+     * \f[ \lambda_\alpha := \frac{k_{r\alpha}}{\mu_\alpha} \f]
+     *
+     * \param phaseIdx The index of the fluid phase
+     */
+    Scalar mobility(const int phaseIdx) const
+    { return relativePermeability(phaseIdx)/fluidState_.viscosity(phaseIdx); }
+
+    /*!
+     * \brief Returns the dynamic viscosity \f$\mathrm{[Pa*s]}\f$ of a given phase within
+     *        the control volume.
+     *
+     * \param phaseIdx The index of the fluid phase
+     * \note The non-wetting phase is infinitely mobile
+     */
+    Scalar viscosity(const int phaseIdx) const
+    { return phaseIdx == wPhaseIdx ? fluidState_.viscosity(wPhaseIdx) : 0.0; }
+
+    /*!
+     * \brief Returns relative permeability [-] of a given phase within
+     *        the control volume.
+     *
+     * \param phaseIdx The index of the fluid phase
+     */
+    Scalar relativePermeability(const int phaseIdx) const
+    { return phaseIdx == wPhaseIdx ? relativePermeabilityWetting_ : 1.0; }
+
+    /*!
+     * \brief Returns the effective capillary pressure \f$\mathrm{[Pa]}\f$ within the
+     *        control volume.
+     *
+     * The capillary pressure is defined as the difference in
+     * pressures of the non-wetting and the wetting phase, i.e.
+     * \f[ p_c = p_n - p_w \f]
+     *
+     * \note Capillary pressures are always larger than the entry pressure
+     *       This regularization doesn't affect the residual in which pc is not needed.
+     */
+    Scalar capillaryPressure() const
+    {
+        using std::max;
+        return max(minPc_, pn_ - fluidState_.pressure(wPhaseIdx));
+    }
+
+    /*!
+     * \brief Returns the pressureHead \f$\mathrm{[cm]}\f$ of a given phase within
+     *        the control volume.
+     *
+     * For the non-wetting phase (i.e. the gas phase), we assume
+     * infinite mobility, which implies that the non-wetting phase
+     * pressure is equal to the finite volume's reference pressure
+     * defined by the problem.
+     *
+     * \param phaseIdx The index of the fluid phase
+     * \note this function is here as a convenience to the user to not have to
+     *       manually do a conversion. It is not correct if the density is not constant
+     *       or the gravity different
+     */
+    Scalar pressureHead(const int phaseIdx) const
+    { return 100.0 *(pressure(phaseIdx) - pn_)/density(phaseIdx)/9.81; }
+
+    /*!
+     * \brief Returns the water content
+     *        fluid phase within the finite volume.
+     *
+     * The water content is defined as the fraction of
+     * the saturation devided by the porosity
+
+     * \param phaseIdx The index of the fluid phase
+     * \note this function is here as a convenience to the user to not have to
+     *       manually do a conversion.
+     */
+    Scalar waterContent(const int phaseIdx) const
+    { return saturation(phaseIdx) * porosity_; }
+
+    /*!
+     * \brief Return molar density \f$\mathrm{[mol/m^3]}\f$ the of the fluid phase.
+     *
+     * We always forward to the fluid state with the phaseIdx property (see class description).
+     */
+    Scalar molarDensity(const int phaseIdx) const
+    { return phaseIdx == wPhaseIdx ? fluidState_.molarDensity(phaseIdx) : 0.0; }
+
+    /*!
+     * \brief Return mole fraction \f$\mathrm{[mol/mol]}\f$ of a component in the phase.
+     *
+     * \param compIdx The index of the component
+     *
+     * We always forward to the fluid state with the phaseIdx property (see class description).
+     */
+    Scalar moleFraction(const int phaseIdx, const int compIdx) const
+    { return phaseIdx == wPhaseIdx ? fluidState_.moleFraction(phaseIdx, compIdx) : 0.0; }
+
+    /*!
+     * \brief Return mass fraction \f$\mathrm{[kg/kg]}\f$ of a component in the phase.
+     *
+     * \param compIdx The index of the component
+     *
+     * We always forward to the fluid state with the phaseIdx property (see class description).
+     */
+    Scalar massFraction(const int phaseIdx, const int compIdx) const
+    { return phaseIdx == wPhaseIdx ? fluidState_.massFraction(phaseIdx, compIdx) : 0.0; }
+
+    /*!
+     * \brief Return concentration \f$\mathrm{[mol/m^3]}\f$  of a component in the phase.
+     *
+     * \param compIdx The index of the component
+     *
+     * We always forward to the fluid state with the phaseIdx property (see class description).
+     */
+    Scalar molarity(const int phaseIdx, const int compIdx) const
+    { return phaseIdx == wPhaseIdx ? fluidState_.molarity(phaseIdx, compIdx) : 0.0; }
+
+    /*!
+     * \brief Return the binary diffusion coefficient \f$\mathrm{[m^2/s]}\f$ in the fluid.
+     */
+    Scalar diffusionCoefficient(const int phaseIdx, const int compIdx) const
+    {
+        assert(phaseIdx == wPhaseIdx);
+        assert(compIdx > wPhaseIdx);
+        return diffCoefficient_[compIdx-1];
+    }
+
+    /*!
+     * \brief Returns the dispersivity of the fluid's streamlines.
+     */
+    const GlobalPosition &dispersivity() const
+    { return dispersivity_; }
+
+protected:
+    FluidState fluidState_; //! the fluid state
+    Scalar relativePermeabilityWetting_; //! the relative permeability of the wetting phase
+    Scalar porosity_; //! the porosity
+    PermeabilityType permeability_; //! the instrinsic permeability
+    Scalar pn_; //! the reference non-wetting pressure
+    Scalar minPc_; //! the minimum capillary pressure (entry pressure)
+
+private:
+    void setDiffusionCoefficient_(int compIdx, Scalar d)
+    {
+        assert(compIdx > wPhaseIdx);
+        diffCoefficient_[compIdx-1] = d;
+    }
+
+    std::array<Scalar, numComponents-1> diffCoefficient_;
+    GlobalPosition dispersivity_;
+
+    Implementation &asImp_()
+    { return *static_cast<Implementation*>(this); }
+
+    const Implementation &asImp_() const
+    { return *static_cast<const Implementation*>(this); }
+};
+
+}// end namespace Dumux
+
+#endif
