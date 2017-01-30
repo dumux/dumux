dumux/dumux

commit 3b62b6afb52189eab3a25f39cbc049b582fad862

Replace const char * by std::string for more flexibility

dumux/material/fluidsystems/h2on2.hh
@@ -98,13 +98,8 @@ public:
      */
     static const std::string& phaseName(int phaseIdx)
     {
-        static const std::string name[] = {
-            "w",
-            "n"
-        };
-
         assert(0 <= phaseIdx && phaseIdx < numPhases);
-        return name[phaseIdx];
+        return phaseIdx == wPhaseIdx ? "w" : "n";
     }
 
     /*!

commit 680e59b1ae98040e756362b97a7150140eb9e5b5

[fix] Return reference to static. Fix bug introduced in 3b62b6afb52189eab3a25f39cbc049b582fad862

dumux/material/fluidsystems/h2on2.hh
@@ -98,8 +98,9 @@ public:
      */
     static const std::string& phaseName(int phaseIdx)
     {
+        static const std::string name[] = { std::string("w"), std::string("n") };
         assert(0 <= phaseIdx && phaseIdx < numPhases);
-        return phaseIdx == wPhaseIdx ? "w" : "n";
+        return name[phaseIdx];
     }
 
     /*!