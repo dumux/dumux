9d4bde5a2e497b7839c581d17319984f2dfbc043
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2017-02-02 15:16:06 +0100

    [richardsnc][test] Implement a pumping test for richardsnc

---
+++ test/porousmediumflow/richardsnc/implicit/richardswelltracerproblem.hh
@@ -0,0 +1,308 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief A water infiltration problem with a low-permeability lens
+ *        embedded into a high-permeability domain which uses the
+ *        Richards box model.
+ */
+#ifndef DUMUX_RICHARDS_NC_WELL_TRACER_PROBLEM_HH
+#define DUMUX_RICHARDS_NC_WELL_TRACER_PROBLEM_HH
+
+#include <dumux/implicit/cellcentered/tpfa/properties.hh>
+#include <dumux/porousmediumflow/implicit/problem.hh>
+#include <dumux/porousmediumflow/richardsnc/implicit/model.hh>
+
+#include "richardswelltracerspatialparams.hh"
+
+namespace Dumux
+{
+
+template <class TypeTag>
+class RichardsWellTracerProblem;
+
+
+// Specify the properties for the lens problem
+namespace Properties
+{
+NEW_TYPE_TAG(RichardsWellTracerProblem, INHERITS_FROM(RichardsNC, RichardsWellTracerSpatialParams));
+NEW_TYPE_TAG(RichardsWellTracerBoxProblem, INHERITS_FROM(BoxModel, RichardsWellTracerProblem));
+NEW_TYPE_TAG(RichardsWellTracerCCProblem, INHERITS_FROM(CCTpfaModel, RichardsWellTracerProblem));
+
+// Use 2d YaspGrid
+SET_TYPE_PROP(RichardsWellTracerProblem, Grid, Dune::YaspGrid<2>);
+
+// Set the physical problem to be solved
+SET_TYPE_PROP(RichardsWellTracerProblem, Problem, RichardsWellTracerProblem<TypeTag>);
+
+// Set the physical problem to be solved
+SET_TYPE_PROP(RichardsWellTracerProblem, PointSource, SolDependentPointSource<TypeTag>);
+
+// Enable gravity
+SET_BOOL_PROP(RichardsWellTracerProblem, ProblemEnableGravity, true);
+}
+
+/*!
+ * \ingroup RichardsNCModel
+ * \ingroup ImplicitTestProblems
+ *
+ * \brief A water infiltration problem with a low-permeability lens
+ *        embedded into a high-permeability domain which uses the
+ *        Richards model.
+ *
+ * The domain is box shaped. Left and right boundaries are Dirichlet
+ * boundaries with fixed water pressure (hydostatic, gradient from right to left),
+ * bottom boundary is closed (Neumann 0 boundary), the top boundary
+ * (Neumann 0 boundary) is also closed. Water is extracted at a point in
+ * the middle of the domain.
+ * which uses the TwoPBoxModel, with the main difference being that
+ * the domain is initally fully saturated by gas instead of water and
+ * water instead of a %DNAPL infiltrates from the top.
+ *
+ * This problem uses the \ref RichardsNCModel
+ *
+ * To run the simulation execute the following line in shell:
+ * <tt>./test_boxrichardsnc -ParameterFile test_boxrichardsnc.input -TimeManager.TEnd 10000000</tt>
+ * <tt>./test_ccrichardsnc -ParameterFile test_ccrichardsnc.input -TimeManager.TEnd 10000000</tt>
+ *
+ * where the initial time step is 100 seconds, and the end of the
+ * simulation time is 10,000,000 seconds (115.7 days)
+ */
+template <class TypeTag>
+class RichardsWellTracerProblem : public ImplicitPorousMediaProblem<TypeTag>
+{
+    using ParentType = ImplicitPorousMediaProblem<TypeTag>;
+    using Problem = typename GET_PROP_TYPE(TypeTag, Problem);
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using PrimaryVariables = typename GET_PROP_TYPE(TypeTag, PrimaryVariables);
+    using FVElementGeometry = typename GET_PROP_TYPE(TypeTag, FVElementGeometry);
+    using ElementVolumeVariables = typename GET_PROP_TYPE(TypeTag, ElementVolumeVariables);
+    using SubControlVolume = typename GET_PROP_TYPE(TypeTag, SubControlVolume);
+    using MaterialLaw = typename GET_PROP_TYPE(TypeTag, MaterialLaw);
+    using BoundaryTypes = typename GET_PROP_TYPE(TypeTag, BoundaryTypes);
+    using TimeManager = typename GET_PROP_TYPE(TypeTag, TimeManager);
+    using PointSource = typename GET_PROP_TYPE(TypeTag, PointSource);
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+    using Indices = typename GET_PROP_TYPE(TypeTag, Indices);
+    enum {
+        // copy some indices for convenience
+        pressureIdx = Indices::pressureIdx,
+        conti0EqIdx = Indices::conti0EqIdx,
+        compIdx = Indices::compMainIdx + 1,
+        wPhaseIdx = Indices::wPhaseIdx,
+
+        // Grid and world dimension
+        dimWorld = GridView::dimensionworld
+    };
+    using Element = typename GridView::template Codim<0>::Entity;
+    using GlobalPosition = Dune::FieldVector<Scalar, dimWorld>;
+
+public:
+    /*!
+     * \brief Constructor
+     *
+     * \param timeManager The Dumux TimeManager for simulation management.
+     * \param gridView The grid view on the spatial domain of the problem
+     */
+    RichardsWellTracerProblem(TimeManager &timeManager, const GridView &gridView)
+        : ParentType(timeManager, gridView)
+    {
+        name_ = GET_RUNTIME_PARAM_FROM_GROUP(TypeTag, std::string, Problem, Name);
+        contaminantMoleFraction_ = GET_RUNTIME_PARAM_FROM_GROUP(TypeTag, Scalar, Problem, ContaminantMoleFraction);
+
+        // for initial conditions
+        const Scalar sw = 0.4; // start with 80% saturation on top
+        pcTop_ = MaterialLaw::pc(this->spatialParams().materialLawParamsAtPos(this->bBoxMax()), sw);
+    }
+
+    /*!
+     * \name Problem parameters
+     */
+    // \{
+
+    /*!
+     * \brief The problem name
+     *
+     * This is used as a prefix for files generated by the simulation.
+     */
+    const std::string& name() const
+    { return name_; }
+
+    /*!
+     * \brief Returns the temperature [K] within a finite volume
+     *
+     * This problem assumes a temperature of 10 degrees Celsius.
+     */
+    Scalar temperature() const
+    { return 273.15 + 10; }; // -> 10°C
+
+    /*!
+     * \brief Returns the reference pressure [Pa] of the non-wetting
+     *        fluid phase within a finite volume
+     *
+     * This problem assumes a constant reference pressure of 1 bar.
+     */
+    Scalar nonWettingReferencePressure() const
+    { return 1.0e5; };
+
+    // \}
+
+    /*!
+     * \name Boundary conditions
+     */
+    // \{
+
+    /*!
+     * \brief Specifies which kind of boundary condition should be
+     *        used for which equation on a given boundary segment.
+     *
+     * \param globalPos The position for which the boundary type is set
+     */
+    BoundaryTypes boundaryTypesAtPos(const GlobalPosition &globalPos) const
+    {
+        BoundaryTypes bcTypes;
+        if (onLeftBoundary_(globalPos) || onRightBoundary_(globalPos))
+            bcTypes.setAllDirichlet();
+        else
+            bcTypes.setAllNeumann();
+        return bcTypes;
+    }
+
+    /*!
+     * \brief Evaluate the boundary conditions for a dirichlet
+     *        boundary segment.
+     *
+     * \param globalPos The position for which the Dirichlet value is set
+     *
+     * For this method, the \a values parameter stores primary variables.
+     */
+    PrimaryVariables dirichletAtPos(const GlobalPosition &globalPos) const
+    { return initial_(globalPos); }
+
+    /*!
+     * \brief Evaluate the boundary conditions for a neumann
+     *        boundary segment.
+     *
+     * For this method, the \a values parameter stores the mass flux
+     * in normal direction of each phase. Negative values mean influx.
+     *
+     * \param globalPos The position for which the Neumann value is set
+     */
+    PrimaryVariables neumannAtPos(const GlobalPosition &globalPos) const
+    { return PrimaryVariables(0.0); }
+
+    /*!
+     * \name Volume terms
+     */
+    // \{
+
+    /*!
+     * \brief Applies a vector of point sources. The point sources
+     *        are possibly solution dependent.
+     *
+     * \param pointSources A vector of PointSource s that contain
+              source values for all phases and space positions.
+     *
+     * For this method, the \a values method of the point source
+     * has to return the absolute rate values in units
+     * \f$ [ \textnormal{unit of conserved quantity} / s ] \f$.
+     * Positive values mean that the conserved quantity is created, negative ones mean that it vanishes.
+     * E.g. for the mass balance that would be a mass rate in \f$ [ kg / s ] \f$.
+     */
+    void addPointSources(std::vector<PointSource>& pointSources) const
+    {
+        auto globalPos = this->bBoxMax()-this->bBoxMin();
+        globalPos *= 0.5;
+        pointSources.emplace_back(globalPos,
+             [](const Problem &problem,
+                const Element &element,
+                const FVElementGeometry &fvGeometry,
+                const ElementVolumeVariables &elemVolVars,
+                const SubControlVolume &scv)
+                {
+                    const auto& volVars = elemVolVars[scv];
+                    //! Add point source with 0.7 kg/s and upwind tracer component
+                    return PrimaryVariables({-0.7, -0.7*volVars.moleFraction(wPhaseIdx, compIdx)
+                                                       *volVars.molarDensity(wPhaseIdx)});
+                });
+    }
+
+    /*!
+     * \brief Evaluate the initial values for a control volume.
+     *
+     * For this method, the \a values parameter stores primary
+     * variables.
+     *
+     * \param globalPos The position for which the boundary type is set
+     */
+    PrimaryVariables initialAtPos(const GlobalPosition &globalPos) const
+    { return initial_(globalPos); };
+
+    // \}
+
+private:
+    PrimaryVariables initial_(const GlobalPosition &globalPos) const
+    {
+        const auto xTracer = [&,this]()
+        {
+            const GlobalPosition contaminationPos({0.2*this->bBoxMax()[0], 0.5*this->bBoxMax()[1]});
+            if ((globalPos - contaminationPos).two_norm() < 0.1*(this->bBoxMax()-this->bBoxMin()).two_norm() + eps_)
+                return contaminantMoleFraction_;
+            else
+                return 0.0;
+        }();
+
+        PrimaryVariables values(0.0);
+        //! hydrostatic pressure profile
+        values[pressureIdx] = (nonWettingReferencePressure() - pcTop_)
+                               - 9.81*1000*(globalPos[dimWorld-1] - this->bBoxMax()[dimWorld-1]);
+        values[compIdx] = xTracer;
+        return values;
+    }
+
+    bool onLeftBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[0] < this->bBoxMin()[0] + eps_;
+    }
+
+    bool onRightBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[0] > this->bBoxMax()[0] - eps_;
+    }
+
+    bool onLowerBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[1] < this->bBoxMin()[1] + eps_;
+    }
+
+    bool onUpperBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[1] > this->bBoxMax()[1] - eps_;
+    }
+
+    static constexpr Scalar eps_ = 1.5e-7;
+    std::string name_;
+    Scalar contaminantMoleFraction_;
+    Scalar pcTop_;
+};
+
+} //end namespace Dumux
+
+#endif
---
+++ test/porousmediumflow/richardsnc/implicit/richardswelltracerspatialparams.hh
@@ -0,0 +1,178 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief spatial parameters for the RichardsWellTracerProblem
+ */
+#ifndef DUMUX_RICHARDS_LENS_SPATIAL_PARAMETERS_HH
+#define DUMUX_RICHARDS_LENS_SPATIAL_PARAMETERS_HH
+
+#include <dumux/material/spatialparams/implicit.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/vangenuchten.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/efftoabslaw.hh>
+
+#include <dumux/porousmediumflow/richards/implicit/model.hh>
+
+#include <dumux/io/gnuplotinterface.hh>
+#include <dumux/io/plotmateriallaw.hh>
+
+namespace Dumux
+{
+
+// forward declaration
+template<class TypeTag>
+class RichardsWellTracerSpatialParams;
+
+namespace Properties
+{
+// The spatial parameters TypeTag
+NEW_TYPE_TAG(RichardsWellTracerSpatialParams);
+
+// Set the spatial parameters
+SET_TYPE_PROP(RichardsWellTracerSpatialParams, SpatialParams, RichardsWellTracerSpatialParams<TypeTag>);
+
+// Set the material law
+SET_PROP(RichardsWellTracerSpatialParams, MaterialLaw)
+{
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+    // define the material law parameterized by effective saturations
+    using type = EffToAbsLaw<VanGenuchten<Scalar>>;
+};
+}
+
+/*!
+ * \ingroup RichardsModel
+ * \ingroup ImplicitTestProblems
+ * \brief The spatial parameters for the RichardsWellTracerProblem
+ */
+template<class TypeTag>
+class RichardsWellTracerSpatialParams : public ImplicitSpatialParams<TypeTag>
+{
+    using ParentType = ImplicitSpatialParams<TypeTag>;
+    using Problem = typename GET_PROP_TYPE(TypeTag, Problem);
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+
+    enum {
+        dim=GridView::dimension,
+        dimWorld=GridView::dimensionworld
+    };
+
+    using GlobalPosition = Dune::FieldVector<Scalar, dimWorld>;
+    using MaterialLaw = typename GET_PROP_TYPE(TypeTag, MaterialLaw);
+    using MaterialLawParams = typename MaterialLaw::Params;
+
+public:
+    // export permeability type
+    using PermeabilityType = Scalar;
+
+    /*!
+     * \brief Constructor
+     *
+     * \param gridView The DUNE GridView representing the spatial
+     *                 domain of the problem.
+     */
+    RichardsWellTracerSpatialParams(const Problem& problem, const GridView& gridView)
+        : ParentType(problem, gridView)
+    {
+
+        lensLowerLeft_ = GET_RUNTIME_PARAM_FROM_GROUP(TypeTag, GlobalPosition, Problem, LensLowerLeft);
+        lensUpperRight_ = GET_RUNTIME_PARAM_FROM_GROUP(TypeTag, GlobalPosition, Problem, LensUpperRight);
+
+        // residual saturations
+        lensMaterialParams_.setSwr(0.18);
+        lensMaterialParams_.setSnr(0.0);
+        outerMaterialParams_.setSwr(0.05);
+        outerMaterialParams_.setSnr(0.0);
+
+        // parameters for the Van Genuchten law
+        // alpha and n
+        lensMaterialParams_.setVgAlpha(0.00045);
+        lensMaterialParams_.setVgn(7.3);
+        outerMaterialParams_.setVgAlpha(0.0037);
+        outerMaterialParams_.setVgn(4.7);
+
+        lensMaterialParams_.setVgn(7.3);
+        outerMaterialParams_.setVgAlpha(0.0037);
+
+        lensK_ = 1e-14;
+        outerK_ = 5e-12;
+    }
+
+    /*!
+     * \brief Returns the intrinsic permeability tensor [m^2] at a given location
+     *
+     * \param globalPos The global position where we evaluate
+     */
+    PermeabilityType permeabilityAtPos(const GlobalPosition& globalPos) const
+    {
+        if (isInLens_(globalPos))
+            return lensK_;
+        return outerK_;
+    }
+
+    /*!
+     * \brief Returns the porosity [] at a given location
+     *
+     * \param globalPos The global position where we evaluate
+     */
+    Scalar porosityAtPos(const GlobalPosition& globalPos) const
+    {
+        if (isInLens_(globalPos))
+            return 0.2;
+        return 0.4;
+    }
+
+    /*!
+     * \brief Returns the parameters for the material law at a given location
+     *
+     * \param globalPos A global coordinate vector
+     */
+    const MaterialLawParams& materialLawParamsAtPos(const GlobalPosition &globalPos) const
+    {
+        if (isInLens_(globalPos))
+            return lensMaterialParams_;
+        return outerMaterialParams_;
+    }
+
+private:
+    bool isInLens_(const GlobalPosition &globalPos) const
+    {
+        for (int i = 0; i < dimWorld; ++i)
+            if (globalPos[i] < lensLowerLeft_[i] - eps_ || globalPos[i] > lensUpperRight_[i] + eps_)
+                return false;
+
+        return true;
+    }
+
+    static constexpr Scalar eps_ = 1.5e-7;
+
+    GlobalPosition lensLowerLeft_;
+    GlobalPosition lensUpperRight_;
+
+    Scalar lensK_;
+    Scalar outerK_;
+    MaterialLawParams lensMaterialParams_;
+    MaterialLawParams outerMaterialParams_;
+};
+
+} // end namespace Dumux
+
+#endif
