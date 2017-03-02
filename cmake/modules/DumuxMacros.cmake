115831b7bf5bdfae90c3949dcb89a1f55da5260e
Sina Ackermann <sina.ackermann@iws.uni-stuttgart.de>
2017-03-02 15:29:45 +0100

    Merge branch 'cmake/findPVPython' into 'master'
    [cmake] Add module to find pvpython, which can be used to run postprocessing scripts
    
    See merge request !393

--- cmake/modules/DumuxMacros.cmake
+++ cmake/modules/DumuxMacros.cmake
@@ -5,10 +5,10 @@ include(DumuxDoxygen)
 include(DumuxTestMacros)
 
 find_package(GLPK)
-find_package(Gstat)
 find_package(Gnuplot)
 set(HAVE_GNUPLOT ${GNUPLOT_FOUND})
-
+find_package(Gstat)
 find_package(NLOPT)
-find_package(Valgrind)
 find_package(PTScotch)
+find_package(PVPython)
+find_package(Valgrind)
