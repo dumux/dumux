2a8defaa130c60b440af356f674ae8dd3f37c203
Thomas Fetzer <thomas.fetzer@iws.uni-stuttgart.de>
2017-03-21 08:31:10 +0100

    [doxygen] Only link pages when they are available

--- doc/doxygen/sanitizelinks.sh
+++ doc/doxygen/sanitizelinks.sh
@@ -4,9 +4,11 @@ sanitizelinks () {
   NEW_FILE=`grep -l "\"title\">$1" html/*html | egrep -o [0-9]+`
   OLD_FILE=`awk -v a=$NEW_FILE 'BEGIN {printf("%05d", a-1)}'`
 #   echo $1: $OLD_FILE $NEW_FILE
-  sed -i "s#$OLD_FILE#$NEW_FILE#g" html/a*html
-  sed -i "s# $1 # <a href=\"a$NEW_FILE.html\">$1</a> #g" html/index.html
-  sed -i "s# $1,# <a href=\"a$NEW_FILE.html\">$1</a>,#g" html/index.html
+  if [ $OLD_FILE -gt 0 ]; then
+    sed -i "s#$OLD_FILE#$NEW_FILE#g" html/a*html
+    sed -i "s# $1 # <a href=\"a$NEW_FILE.html\">$1</a> #g" html/index.html
+    sed -i "s# $1,# <a href=\"a$NEW_FILE.html\">$1</a>,#g" html/index.html
+  fi
 }
 
 sanitizelinks "Deprecated List" # has to be called before Todo List
