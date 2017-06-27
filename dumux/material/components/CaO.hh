75af9679cc29dca26cd6e42a3535e950d788d15c
Gabriele Seitz <gabriele.seitz@iws.uni-stuttgart.de>
2017-06-27 09:40:27 +0200

    [material][1pncmin] add new fluidsystem steamaircao2h2
    including the new components CaO.hh and CaO2H2.hh for
    the thermochemical reaction

---
+++ dumux/material/components/CaO.hh
@@ -0,0 +1,83 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \ingroup Components
+ *
+ * \brief Material properties of pure Calcium-Oxide \f$CaO\f$.
+ */
+#ifndef DUMUX_CAO_HH
+#define DUMUX_CAO_HH
+
+#include <dumux/common/exceptions.hh>
+#include <dune/common/deprecated.hh>
+#include <dumux/material/components/component.hh>
+
+#include <cmath>
+#include <iostream>
+
+namespace Dumux
+{
+/*!
+ * \brief A class for the CaO properties
+ */
+template <class Scalar>
+class CaO : public Component<Scalar, CaO<Scalar> >
+{
+public:
+    /*!
+     * \brief A human readable name for the CaO.
+     */
+    static const char *name()
+    {
+        return "CaO";
+    }
+
+    /*!
+     * \brief The molar mass of CaOH2 in \f$\mathrm{[kg/mol]}\f$.
+     */
+    static Scalar molarMass()
+    {
+        return 56.0774e-3;
+    }
+
+    /*!
+     * \brief The mass density \f$\mathrm{[kg/m^3]}\f$ of CaO.
+     */
+    static Scalar density()
+    {
+        return 3370.0; //at 293 K ; Shao et al. (2013)
+    }
+
+    /*!
+     * \brief The specific heat capacity \f$\mathrm{[J/molK]}\f$ of CaO.
+     */
+    static Scalar heatCapacity()
+    {
+        return 0.0166;
+        //Literature values:
+        //Nagel et al. (2014) : 934 J/kgK
+        // actually 47.719 at T = 500 K cf. http://link.springer.com/chapter/10.1007/10681719_232
+    }
+};
+
+} // end namespace
+
+#endif
