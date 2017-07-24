d8b2f2832353831d267f48e02c384398aa7d6cc7
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2017-07-24 18:20:47 +0200

    [tut] Initial commit for capillary fringe tutorial

---
+++ tutorial/capillaryfringe/fringespatialparams.hh
@@ -0,0 +1,126 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief spatial parameters for the RichardsLensProblem
+ */
+#ifndef DUMUX_FRINGE_SPATIAL_PARAMETERS_HH
+#define DUMUX_FRINGE_SPATIAL_PARAMETERS_HH
+
+#include <dumux/material/spatialparams/implicit.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/regularizedvangenuchten.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/efftoabslaw.hh>
+
+#include <dumux/porousmediumflow/richards/implicit/model.hh>
+
+namespace Dumux
+{
+
+/*!
+ * \ingroup RichardsModel
+ * \ingroup ImplicitTestProblems
+ * \brief The spatial parameters for the RichardsLensProblem
+ */
+template<class TypeTag>
+class FringeSpatialParams : public ImplicitSpatialParams<TypeTag>
+{
+    using ParentType = ImplicitSpatialParams<TypeTag>;
+    using Problem = typename GET_PROP_TYPE(TypeTag, Problem);
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+
+    enum {
+        dim=GridView::dimension,
+        dimWorld=GridView::dimensionworld
+    };
+
+    using GlobalPosition = Dune::FieldVector<Scalar, dimWorld>;
+    using MaterialLaw = typename GET_PROP_TYPE(TypeTag, MaterialLaw);
+    using MaterialLawParams = typename MaterialLaw::Params;
+
+public:
+    // export permeability type
+    using PermeabilityType = Scalar;
+
+    /*!
+     * \brief Constructor
+     *
+     * \param gridView The DUNE GridView representing the spatial
+     *                 domain of the problem.
+     */
+    FringeSpatialParams(const Problem& problem, const GridView& gridView)
+        : ParentType(problem, gridView)
+    {
+        // residual saturations
+        outerMaterialParams_.setSwr(0.05);
+        outerMaterialParams_.setSnr(0.0);
+
+        // parameters for the Van Genuchten law
+        // alpha and n
+        outerMaterialParams_.setVgAlpha(0.0037);
+        outerMaterialParams_.setVgn(4.7);
+
+        outerMaterialParams_.setPcLowSw(0.001);
+
+        outerK_ = 5e-12;
+    }
+
+    /*!
+     * \brief Returns the intrinsic permeability tensor [m^2] at a given location
+     *
+     * \param globalPos The global position where we evaluate
+     */
+    PermeabilityType permeabilityAtPos(const GlobalPosition& globalPos) const
+    {
+        return outerK_;
+    }
+
+    /*!
+     * \brief Returns the porosity [] at a given location
+     *
+     * \param globalPos The global position where we evaluate
+     */
+    Scalar porosityAtPos(const GlobalPosition& globalPos) const
+    { return 0.4; }
+
+    /*!
+     * \brief Returns the parameters for the material law at a given location
+     *
+     * This method is not actually required by the Richards model, but provided
+     * for the convenience of the RichardsLensProblem
+     *
+     * \param globalPos A global coordinate vector
+     */
+    const MaterialLawParams& materialLawParamsAtPos(const GlobalPosition &globalPos) const
+    {
+        return outerMaterialParams_;
+    }
+
+private:
+
+    static constexpr Scalar eps_ = 1.5e-7;
+
+    Scalar outerK_;
+    MaterialLawParams outerMaterialParams_;
+};
+
+} // end namespace Dumux
+
+#endif
