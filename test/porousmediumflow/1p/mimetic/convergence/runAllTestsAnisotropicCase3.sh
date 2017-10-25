e7a16f5144a64ba07f3e90b5836bd4b1f5c6c5df
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2017-10-25 17:27:15 +0200

    [1pmimetic] Update DMP test cases

--- test/porousmediumflow/1p/mimetic/convergence/runAllTestsAnisotropicCase3.sh
+++ test/porousmediumflow/1p/mimetic/convergence/runAllTestsAnisotropicCase3.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-name=solutionFor_anisotropic_case3_mimetic
-rm $name"_rates.txt"
-rm $name".out"
-sh ./runTestAnisotropicCase3.sh $name
+outName=solutionFor_anisotropic_case3_mimetic
+rm $outName".out"
+
+make test_1panisotropic_mimetic
+
+./test_1panisotropic_mimetic  -ParameterFile ./test_1panisotropic_mimetic_case3.input -Problem.Name $outName -Problem.TestCase 3 >> $outName".out"
+
+#######################################################################################
+
+outName=solutionFor_anisotropic_case3_mpfao
+rm $outName".out"
+
+make test_1panisotropic_mpfao
+
+./test_1panisotropic_mpfao  -ParameterFile ./test_1panisotropic_mpfao_case3.input -Problem.Name $outName -Problem.TestCase 3 >> $outName".out"
+
+#######################################################################################
--- test/porousmediumflow/1p/mimetic/convergence/runTestAnisotropicCase3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-make test_1pmimeticanisotropic
-
-outName=$1
-inputArgs=""
-./test_1pmimeticanisotropic  -ParameterFile ./test_1pmimeticanisotropic_case3.input -Problem.Name $outName $inputArgs -Problem.TestCase 3 >> $1".out"
