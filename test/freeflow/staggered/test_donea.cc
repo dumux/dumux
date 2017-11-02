4ba327fc4b8a1aaef3f8504b217c0e249a4c8cc6
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2017-11-02 10:54:02 +0100

    [freeflow][test] Call makeGrid() without arguments

--- test/freeflow/staggered/test_donea.cc
+++ test/freeflow/staggered/test_donea.cc
@@ -104,7 +104,7 @@ int main(int argc, char** argv)
 
     // try to create a grid (from the given grid file or the input file)
     using GridCreator = typename GET_PROP_TYPE(TypeTag, GridCreator);
-    GridCreator::makeGrid(Parameters::getTree());
+    GridCreator::makeGrid();
     GridCreator::loadBalance();
 
     ////////////////////////////////////////////////////////////
