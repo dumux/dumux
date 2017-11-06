713cb102a1406855ab48e01509229b896ea8632e
DennisGlaeser <dennis.glaeser@iws.uni-stuttgart.de>
2017-11-06 14:22:37 +0100

    [2p] port fracture tests

--- test/porousmediumflow/2p/implicit/fracture/problem.hh
+++ test/porousmediumflow/2p/implicit/fracture/problem.hh
@@ -27,13 +27,15 @@
 
 #include <dumux/material/components/simpleh2o.hh>
 #include <dumux/material/components/dnapl.hh>
-#include <dumux/porousmediumflow/2p/implicit/model.hh>
-#include <dumux/porousmediumflow/implicit/problem.hh>
-#include <dumux/implicit/cellcentered/tpfa/properties.hh>
-#include <dumux/implicit/cellcentered/mpfa/properties.hh>
-#include <dumux/implicit/cellcentered/propertydefaults.hh>
 
-#include "fracturespatialparams.hh"
+#include <dumux/porousmediumflow/2p/implicit/model.hh>
+#include <dumux/porousmediumflow/problem.hh>
+
+#include <dumux/discretization/cellcentered/tpfa/properties.hh>
+#include <dumux/discretization/cellcentered/mpfa/properties.hh>
+#include <dumux/discretization/box/properties.hh>
+
+#include "spatialparams.hh"
 
 namespace Dumux
 {
@@ -45,17 +47,13 @@ namespace Properties
 {
 NEW_TYPE_TAG(FractureProblem, INHERITS_FROM(TwoP, FractureSpatialParams));
 NEW_TYPE_TAG(FractureBoxProblem, INHERITS_FROM(BoxModel, FractureProblem));
-NEW_TYPE_TAG(FractureCCProblem, INHERITS_FROM(CCTpfaModel, FractureProblem));
+NEW_TYPE_TAG(FractureCCTpfaProblem, INHERITS_FROM(CCTpfaModel, FractureProblem));
 NEW_TYPE_TAG(FractureCCMpfaProblem, INHERITS_FROM(CCMpfaModel, FractureProblem));
-
-SET_BOOL_PROP(FractureProblem, EnableFVGridGeometryCache, true);
-SET_BOOL_PROP(FractureProblem, EnableGlobalVolumeVariablesCache, true);
-SET_BOOL_PROP(FractureProblem, EnableGlobalFluxVariablesCache, true);
-SET_BOOL_PROP(FractureProblem, SolutionDependentAdvection, false);
 
 #if HAVE_DUNE_FOAMGRID
 SET_TYPE_PROP(FractureProblem, Grid, Dune::FoamGrid<2, 3>);
 #else
+#warning "FoamGrid is required for this test!"
 SET_TYPE_PROP(FractureProblem, Grid, Dune::YaspGrid<3>);
 #endif
 
@@ -80,58 +78,30 @@ public:
     using type = FluidSystems::LiquidPhase<Scalar, DNAPL<Scalar>>;
 };
 
-// Linear solver settings
-SET_TYPE_PROP(FractureProblem, LinearSolver, Dumux::ILU0BiCGSTABBackend<TypeTag>);
+// Use global caching
+SET_BOOL_PROP(FractureProblem, EnableFVGridGeometryCache, true);
+SET_BOOL_PROP(FractureProblem, EnableGlobalVolumeVariablesCache, true);
+SET_BOOL_PROP(FractureProblem, EnableGlobalFluxVariablesCache, true);
 
-SET_BOOL_PROP(FractureProblem, ProblemEnableGravity, false);
+// permeablility is solution-independent
+SET_BOOL_PROP(FractureProblem, SolutionDependentAdvection, false);
 }
 
 /*!
  * \ingroup TwoPModel
  * \ingroup ImplicitTestProblems
- * \brief Soil contamination problem where DNAPL infiltrates a fully
- *        water saturated medium.
- *
- * The domain is sized 6m times 4m and features a rectangular lens
- * with low permeablility which spans from (1 m , 2 m) to (4 m, 3 m)
- * and is surrounded by a medium with higher permability. Note that
- * this problem is discretized using only two dimensions, so from the
- * point of view of the two-phase model, the depth of the domain
- * implicitly is 1 m everywhere.
- *
- * On the top and the bottom of the domain neumann boundary conditions
- * are used, while dirichlet conditions apply on the left and right
- * boundaries.
- *
- * DNAPL is injected at the top boundary from 3m to 4m at a rate of
- * 0.04 kg/(s m^2), the remaining neumann boundaries are no-flow
- * boundaries.
- *
- * The dirichlet boundaries on the left boundary is the hydrostatic
- * pressure scaled by a factor of 1.125, while on the right side it is
- * just the hydrostatic pressure. The DNAPL saturation on both sides
- * is zero.
- *
- * This problem uses the \ref TwoPModel.
- *
- * This problem should typically be simulated until \f$t_{\text{end}}
- * \approx 20\,000\;s\f$ is reached. A good choice for the initial time step
- * size is \f$t_{\text{inital}} = 250\;s\f$.
- *
- * To run the simulation execute the following line in shell:
- * <tt>./test_box2p -parameterFile test_box2p.input</tt> or
- * <tt>./test_cc2p -parameterFile test_cc2p.input</tt>
+ * \brief DNAPL transport through a fracture network (2d in 3d).
  */
 template <class TypeTag>
-class FractureProblem : public ImplicitPorousMediaProblem<TypeTag>
+class FractureProblem : public PorousMediumFlowProblem<TypeTag>
 {
-    using ParentType = ImplicitPorousMediaProblem<TypeTag>;
+    using ParentType = PorousMediumFlowProblem<TypeTag>;
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
     using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
-    using GridCreator = typename GET_PROP_TYPE(TypeTag, GridCreator);
+    using FVGridGeometry = typename GET_PROP_TYPE(TypeTag, FVGridGeometry);
     using Indices = typename GET_PROP_TYPE(TypeTag, Indices);
-    using FluidSystem = typename GET_PROP_TYPE(TypeTag, FluidSystem);
-    using WettingPhase = typename GET_PROP_TYPE(TypeTag, WettingPhase);
-    using NonwettingPhase = typename GET_PROP_TYPE(TypeTag, NonwettingPhase);
+    using PrimaryVariables = typename GET_PROP_TYPE(TypeTag, PrimaryVariables);
+    using BoundaryTypes = typename GET_PROP_TYPE(TypeTag, BoundaryTypes);
 
     enum {
 
@@ -153,45 +123,16 @@ class FractureProblem : public ImplicitPorousMediaProblem<TypeTag>
         dimWorld = GridView::dimensionworld
     };
 
-
-    using PrimaryVariables = typename GET_PROP_TYPE(TypeTag, PrimaryVariables);
-    using FVElementGeometry = typename GET_PROP_TYPE(TypeTag, FVElementGeometry);
-    using BoundaryTypes = typename GET_PROP_TYPE(TypeTag, BoundaryTypes);
-    using TimeManager = typename GET_PROP_TYPE(TypeTag, TimeManager);
-    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
     using GlobalPosition = Dune::FieldVector<Scalar, dimWorld>;
-    using Vertex = typename GridView::template Codim<dim>::Entity;
-    using Element = typename GridView::template Codim<0>::Entity;
-    using Intersection = typename GridView::Intersection;
-    using ReferenceElements = typename Dune::ReferenceElements<Scalar, dim>;
 
 public:
-    /*!
-     * \brief The constructor
-     *
-     * \param timeManager The time manager
-     * \param gridView The grid view
-     */
-    FractureProblem(TimeManager &timeManager, const GridView &gridView)
-    : ParentType(timeManager, gridView)
-    {
-        name_ = GET_RUNTIME_PARAM_FROM_GROUP(TypeTag, std::string, Problem, Name);
-    }
+  FractureProblem(std::shared_ptr<const FVGridGeometry> fvGridGeometry)
+  : ParentType(fvGridGeometry) {}
 
     /*!
      * \name Problem parameters
      */
     // \{
-
-    /*!
-     * \brief Returns the problem name
-     *
-     * This is used as a prefix for files generated by the simulation.
-     */
-    const std::string& name() const
-    {
-        return name_;
-    }
 
     /*!
      * \brief User defined output after the time integration
@@ -200,14 +141,14 @@ public:
      */
     void postTimeStep()
     {
-        // Calculate storage terms
-        PrimaryVariables storage;
-        this->model().globalStorage(storage);
-
-        // Write mass balance information for rank 0
-        if (this->gridView().comm().rank() == 0) {
-            std::cout<<"Storage: " << storage << std::endl;
-        }
+        // // Calculate storage terms
+        // PrimaryVariables storage;
+        // this->model().globalStorage(storage);
+        //
+        // // Write mass balance information for rank 0
+        // if (this->gridView().comm().rank() == 0) {
+        //     std::cout<<"Storage: " << storage << std::endl;
+        // }
     }
 
     /*!
@@ -241,8 +182,7 @@ public:
      * \brief Specifies which kind of boundary condition should be
      *        used for which equation on a given boundary segment.
      *
-     * \param values The boundary types for the conservation equations
-     * \param vertex The vertex for which the boundary type is set
+     * \param globalPos The global position where to set the BC types
      */
     BoundaryTypes boundaryTypesAtPos(const GlobalPosition &globalPos) const
     {
@@ -309,16 +249,13 @@ public:
      * \param globalPos The global position
      */
     PrimaryVariables initialAtPos(const GlobalPosition &globalPos) const
-    {
-        return dirichletAtPos(globalPos);
-    }
+    { return dirichletAtPos(globalPos); }
     // \}
 
 private:
+
     bool onInlet_(const GlobalPosition &globalPos) const
-    {
-        return globalPos[0] < eps_ && globalPos[1] > -0.5 - eps_;
-    }
+    { return globalPos[0] < eps_ && globalPos[1] > -0.5 - eps_; }
 
     static constexpr Scalar eps_ = 1.5e-7;
     std::string name_;
