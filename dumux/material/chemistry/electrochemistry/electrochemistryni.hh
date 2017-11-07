6c710130dee4a6a18954d9ec93b9feb8235ab0cc
DennisGlaeser <dennis.glaeser@iws.uni-stuttgart.de>
2017-11-07 16:15:59 +0100

    [chemistryNI] remove obsolete property declarations

--- dumux/material/chemistry/electrochemistry/electrochemistryni.hh
+++ dumux/material/chemistry/electrochemistry/electrochemistryni.hh
@@ -32,14 +32,6 @@
 namespace Dumux
 {
 
-namespace Properties
-{
-NEW_PROP_TAG(FluidSystem);
-NEW_PROP_TAG(Indices);
-NEW_PROP_TAG(VolumeVariables);
-NEW_PROP_TAG(NumEqVector);
-}
-
 /*!
 * \brief
 * Class calculating source terms and current densities for fuel cells
