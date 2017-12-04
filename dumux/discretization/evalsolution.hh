dfa908b631b956fc9360ab6ceedf841f32b642e5
DennisGlaeser <dennis.glaeser@iws.uni-stuttgart.de>
2017-12-04 15:46:22 +0100

    [evalsolution] introduce evalSolution() function without fvGridGeometry as argument

--- dumux/discretization/evalsolution.hh
+++ dumux/discretization/evalsolution.hh
@@ -35,9 +35,11 @@ namespace Dumux
 
 /*!
  * \brief Interpolates a given box element solution to a given global position.
+ *        Uses the finite element cache of the grid geometry.
  *
  * \return the interpolated Primary Variables
  * \param element The element
+ * \param geometry The element geometry
  * \param fvGridGeometry The finite volume grid geometry
  * \param elemSol The primary variables at the dofs of the element
  * \param globalPos The global position
@@ -73,10 +75,54 @@ evalSolution(const Element& element,
 }
 
 /*!
+ * \brief Interpolates a given box element solution to a given global position.
+ *        Overload of the above evalSolution() function without a given fvGridGeometry.
+ *        The local basis is computed on the fly.
+ *
+ * \return the interpolated Primary Variables
+ * \param element The element
+ * \param geometry The element geometry
+ * \param elemSol The primary variables at the dofs of the element
+ * \param globalPos The global position
+ */
+template< class Element, class TypeTag >
+typename BoxElementSolution<TypeTag>::PrimaryVariables
+evalSolution(const Element& element,
+             const typename Element::Geometry& geometry,
+             const BoxElementSolution<TypeTag>& elemSol,
+             const typename Element::Geometry::GlobalCoordinate& globalPos)
+{
+    //! The box scheme always uses linear Ansatz functions
+    using FeCache = Dune::PQkLocalFiniteElementCache<CoordScalar, Scalar, dim, 1>;
+    using PrimaryVariables = typename BoxElementSolution<TypeTag>::PrimaryVariables;
+    using Scalar = typename PrimaryVariables::value_type;
+
+    // obtain local finite element basis
+    FeCache feCache;
+    const auto& localBasis = feCache.get(geometry.type()).localBasis;
+
+    // evaluate the shape functions at the scv center
+    const auto localPos = geometry.local(globalPos);
+    std::vector< Dune::FieldVector<Scalar, 1> > shapeValues;
+    localBasis.evaluateFunction(localPos, shapeValues);
+
+    PrimaryVariables result(0.0);
+    for (int i = 0; i < geometry.corners(); ++i)
+    {
+        auto value = elemSol[i];
+        value *= shapeValues[i][0];
+        result += value;
+    }
+
+    return result;
+}
+
+/*!
  * \brief Interpolates a given CCElementSolution to a given global position.
  *
  * \return the (constant over the element) Primary Variables
  * \param element The element
+ * \param geometry The element geometry
  * \param fvGridGeometry The finite volume grid geometry
  * \param elemSol The primary variables at the dofs of the element
  * \param globalPos The global position
@@ -88,6 +134,27 @@ evalSolution(const Element& element,
              const FVGridGeometry& fvGridGeometry,
              const CCElementSolution<TypeTag>& elemSol,
              const typename Element::Geometry::GlobalCoordinate& globalPos)
+{
+    return elemSol[0];
+}
+
+/*!
+ * \brief Interpolates a given CCElementSolution to a given global position.
+ *        Overload of the above evalSolution() function without a given fvGridGeometry.
+ *        For compatibility reasons with the box scheme.
+ *
+ * \return the (constant over the element) Primary Variables
+ * \param element The element
+ * \param geometry The element geometry
+ * \param elemSol The primary variables at the dofs of the element
+ * \param globalPos The global position
+ */
+template< class Element, class TypeTag >
+typename CCElementSolution<TypeTag>::PrimaryVariables
+evalSolution(const Element& element,
+            const typename Element::Geometry& geometry,
+            const CCElementSolution<TypeTag>& elemSol,
+            const typename Element::Geometry::GlobalCoordinate& globalPos)
 {
     return elemSol[0];
 }
