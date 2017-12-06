e3713fa82df2f9bde7d001051330fbe421411ed0
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2017-12-06 19:41:21 +0100

    [scvf] Implement boundary flag class to be used in scv faces

---
+++ dumux/common/boundaryflag.hh
@@ -0,0 +1,82 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief Boundary flag to store e.g. in sub control volume faces
+ */
+#ifndef DUMUX_BOUNDARY_FLAG_HH
+#define DUMUX_BOUNDARY_FLAG_HH
+
+// ALUGrid specific includes
+#if HAVE_DUNE_ALUGRID
+#include <dune/alugrid/grid.hh>
+#endif
+
+namespace Dumux
+{
+
+//! a boundary flag class specialized on the grid type
+template<class Grid>
+class BoundaryFlag
+{
+    BoundaryFlag() : flag_(-1) {}
+
+    template<class Intersection>
+    BoundaryFlag(const Intersection& i) : flag_(-1)
+    {
+        if (i.boundary())
+            flag_ = i.boundarySegmentIndex();
+    }
+
+    using value_type = int;
+
+    value_type get() { return flag_; } const
+
+private:
+    int flag_;
+};
+
+
+#if HAVE_DUNE_ALUGRID && DUNE_GRID_EXPERIMENTAL_GRID_EXTENSIONS
+//! alu uses boundary id
+template<int dim, int dimworld, Dune::ALUGridElementType elType, Dune::ALUGridRefinementType refinementType>
+class BoundaryFlag<Dune::ALUGrid<dim, dimworld, elType, refinementType>>
+{
+    BoundaryFlag() : flag_(-1) {}
+
+    template<class Intersection>
+    BoundaryFlag(const Intersection& i) : flag_(-1)
+    {
+        if (i.boundary())
+            flag_ = i.boundaryId();
+    }
+
+    using value_type = int;
+
+    value_type get() { return flag_; } const
+
+private:
+    int flag_;
+};
+#endif
+
+}  // end namespace Dumux
+
+#endif
