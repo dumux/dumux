71655b456e0b4a505954d9d1bc20de754b516bd9
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2017-12-08 23:16:30 +0100

    [1p] Move properties to model.hh

--- dumux/porousmediumflow/1p/implicit/model.hh
+++ dumux/porousmediumflow/1p/implicit/model.hh
@@ -43,6 +43,69 @@
 #ifndef DUMUX_1P_MODEL_HH
 #define DUMUX_1P_MODEL_HH
 
-#include "properties.hh"
+#include <dumux/common/properties.hh>
+
+#include <dumux/material/fluidmatrixinteractions/1p/thermalconductivityaverage.hh>
+#include <dumux/material/fluidstates/immiscible.hh>
+
+#include <dumux/porousmediumflow/properties.hh>
+#include <dumux/porousmediumflow/immiscible/localresidual.hh>
+#include <dumux/porousmediumflow/nonisothermal/implicit/properties.hh>
+
+#include "indices.hh"
+#include "volumevariables.hh"
+#include "vtkoutputfields.hh"
+
+namespace Dumux
+{
+namespace Properties {
+//! The type tags for the isothermal & non-isothermal single phase model
+NEW_TYPE_TAG(OneP, INHERITS_FROM(PorousMediumFlow));
+NEW_TYPE_TAG(OnePNI, INHERITS_FROM(OneP, NonIsothermal));
+
+///////////////////////////////////////////////////////////////////////////
+// properties for the isothermal single phase model
+///////////////////////////////////////////////////////////////////////////
+SET_INT_PROP(OneP, NumEq, 1);                                         //! set the number of equations to 1
+SET_INT_PROP(OneP, NumPhases, 1);                                     //! The number of phases in the 1p model is 1
+SET_INT_PROP(OneP, NumComponents, 1);                                 //! The number of components in the 1p model is 1
+SET_TYPE_PROP(OneP, LocalResidual, ImmiscibleLocalResidual<TypeTag>); //! The local residual function
+SET_TYPE_PROP(OneP, VolumeVariables, OnePVolumeVariables<TypeTag>);   //! the VolumeVariables property
+SET_BOOL_PROP(OneP, EnableAdvection, true);                           //! The one-phase model considers advection
+SET_BOOL_PROP(OneP, EnableMolecularDiffusion, false);                 //! The one-phase model has no molecular diffusion
+SET_BOOL_PROP(OneP, EnableEnergyBalance, false);                      //! Isothermal model by default
+SET_TYPE_PROP(OneP, Indices, OnePIndices);                            //! The indices required by the isothermal single-phase model
+SET_TYPE_PROP(OneP, VtkOutputFields, OnePVtkOutputFields<TypeTag>);   //! Set the vtk output fields specific to this model
+
+/*!
+ * \brief The fluid state which is used by the volume variables to
+ *        store the thermodynamic state. This should be chosen
+ *        appropriately for the model ((non-)isothermal, equilibrium, ...).
+ *        This can be done in the problem.
+ */
+SET_PROP(OneP, FluidState)
+{
+private:
+    typedef typename GET_PROP_TYPE(TypeTag, Scalar) Scalar;
+    typedef typename GET_PROP_TYPE(TypeTag, FluidSystem) FluidSystem;
+public:
+    typedef ImmiscibleFluidState<Scalar, FluidSystem> type;
+};
+
+///////////////////////////////////////////////////////////////////////////
+// properties for the non-isothermal single phase model
+///////////////////////////////////////////////////////////////////////////
+SET_INT_PROP(OnePNI, IsothermalNumEq, 1);                                           //! set number of equations of isothermal model
+SET_BOOL_PROP(OnePNI, EnableEnergyBalance, true);                                   //! we do solve for the energy balance here
+SET_TYPE_PROP(OnePNI, IsothermalVtkOutputFields, OnePVtkOutputFields<TypeTag>);     //! the isothermal vtk output fields
+SET_TYPE_PROP(OnePNI, IsothermalVolumeVariables, OnePVolumeVariables<TypeTag>);     //! Vol vars of the isothermal model
+SET_TYPE_PROP(OnePNI, IsothermalLocalResidual, ImmiscibleLocalResidual<TypeTag>);   //! Local residual of the isothermal model
+SET_TYPE_PROP(OnePNI, IsothermalIndices, OnePIndices);                              //! Indices of the isothermal model
+SET_TYPE_PROP(OnePNI,
+              ThermalConductivityModel,
+              ThermalConductivityAverage<typename GET_PROP_TYPE(TypeTag, Scalar)>); //! Use the average for effective conductivities
+
+} // end namespace Properties
+} // end namespace Dumux
 
 #endif
