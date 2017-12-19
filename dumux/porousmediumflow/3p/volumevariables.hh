e678688eef94481c735663733960f8997a89e7f9
Sina Ackermann <sina.ackermann@iws.uni-stuttgart.de>
2017-12-19 11:42:27 +0100

    [doc][3p][3p3c] Remove copydoc of former implicit classes

--- dumux/porousmediumflow/3p/volumevariables.hh
+++ dumux/porousmediumflow/3p/volumevariables.hh
@@ -79,10 +79,15 @@ public:
 
     using FluidState = typename GET_PROP_TYPE(TypeTag, FluidState);
 
-
     /*!
-     * \copydoc ImplicitVolumeVariables::update
-     */
+     * \brief Update all quantities for a given control volume
+     *
+     * \param elemSol A vector containing all primary variables connected to the element
+     * \param problem The object specifying the problem which ought to
+     *                be simulated
+     * \param element An element which contains part of the control volume
+     * \param scv The sub control volume
+    */
     void update(const ElementSolutionVector &elemSol,
                 const Problem &problem,
                 const Element &element,
@@ -115,7 +120,15 @@ public:
     }
 
     /*!
-     * \copydoc ImplicitModel::completeFluidState
+     * \brief Complete the fluid state
+     *
+     * \param elemSol A vector containing all primary variables connected to the element
+     * \param problem The problem
+     * \param element The element
+     * \param scv The sub control volume
+     * \param fluidState The fluid state
+     *
+     * Set temperature, saturations, capillary pressures, viscosities, densities and enthalpies.
      */
     static void completeFluidState(const ElementSolutionVector& elemSol,
                                    const Problem& problem,
@@ -262,7 +275,9 @@ private:
     { return *static_cast<const Implementation*>(this); }
 };
 
-// TODO doc me!
+/*!
+ * \brief  The ideal gas constant \f$\mathrm{[J/(mol K)]}\f$
+ */
 template <class TypeTag>
 const typename ThreePVolumeVariables<TypeTag>::Scalar ThreePVolumeVariables<TypeTag>::R = Constants<typename GET_PROP_TYPE(TypeTag, Scalar)>::R;
 
