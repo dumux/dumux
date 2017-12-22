84c98d44e2cd50a99778be470db00c84fa832426
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2017-12-22 11:45:10 +0100

    [staggered] include Dumux' IntRange if Dune doesn't provide it

--- dumux/common/staggeredfvproblem.hh
+++ dumux/common/staggeredfvproblem.hh
@@ -27,6 +27,8 @@
 #include <dune/common/version.hh>
 #if DUNE_VERSION_NEWER(DUNE_COMMON,2,6)
 #include <dune/common/rangeutilities.hh>
+#else
+#include <dumux/common/intrange.hh>
 #endif
 
 #include <dumux/common/properties.hh>
