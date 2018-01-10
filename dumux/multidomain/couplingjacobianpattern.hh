169152c53d3413f4aabec3cae4b0d5f638ab508c
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2018-01-10 22:59:13 +0100

    [multidim] First draft of assembly. TODO Fix this commit.

---
+++ dumux/multidomain/couplingjacobianpattern.hh
@@ -0,0 +1,112 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup Assembly
+ * \brief Helper function to generate Jacobian pattern for multidomain models
+ */
+#ifndef DUMUX_MUTLIDOMAIN_COUPLING_JACOBIAN_PATTERN_HH
+#define DUMUX_MUTLIDOMAIN_COUPLING_JACOBIAN_PATTERN_HH
+
+#include <type_traits>
+#include <dune/istl/matrixindexset.hh>
+#include <dumux/discretization/methods.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup Assembly
+ * \brief Helper function to generate coupling Jacobian pattern (off-diagonal blocks)
+ *        for cell-centered schemes
+ */
+template<bool isImplicit, class CouplingManager, class GridGeometry0, class GridGeometry1, std::size_t i,
+         typename std::enable_if_t<( (GridGeometry0::discretizationMethod == DiscretizationMethods::CCTpfa)
+                                     || (GridGeometry0::discretizationMethod == DiscretizationMethods::CCMpfa) ), int> = 0>
+Dune::MatrixIndexSet getCouplingJacobianPattern(const CouplingManager& couplingManager,
+                                                const GridGeometry0& gridGeometry0,
+                                                const GridGeometry1& gridGeometry1,
+                                                Dune::index_constant<i> domainId1)
+{
+    const auto numDofs0 = gridGeometry0.numDofs();
+    const auto numDofs1 = gridGeometry1.numDofs();
+    Dune::MatrixIndexSet pattern;
+    pattern.resize(numDofs0, numDofs1);
+
+    // matrix pattern for implicit Jacobians
+    if (isImplicit)
+    {
+        for (const auto& element0 : elements(gridGeometry0.gridView()))
+        {
+            const auto& stencil = couplingManager.couplingStencil(element0, domainId1);
+            const auto globalI = gridGeometry0.elementMapper().index(element0);
+            for (const auto globalJ : stencil)
+                pattern.add(globalI, globalJ);
+        }
+    }
+
+    // matrix pattern for explicit Jacobians
+    // -> diagonal matrix, so coupling block is empty
+    // just return the empty pattern
+
+    return pattern;
+}
+
+/*!
+ * \ingroup Assembly
+ * \brief Helper function to generate coupling Jacobian pattern (off-diagonal blocks)
+ *        for the box scheme
+ */
+template<bool isImplicit, class CouplingManager, class GridGeometry0, class GridGeometry1, std::size_t i,
+         typename std::enable_if_t<(GridGeometry0::discretizationMethod == DiscretizationMethods::Box), int> = 0>
+Dune::MatrixIndexSet getCouplingJacobianPattern(const CouplingManager& couplingManager,
+                                                const GridGeometry0& gridGeometry0,
+                                                const GridGeometry1& gridGeometry1,
+                                                Dune::index_constant<i> domainId1)
+{
+    const auto numDofs0 = gridGeometry0.numDofs();
+    const auto numDofs1 = gridGeometry1.numDofs();
+    Dune::MatrixIndexSet pattern;
+    pattern.resize(numDofs0, numDofs1);
+
+    // matrix pattern for implicit Jacobians
+    if (isImplicit)
+    {
+        static constexpr int dim = std::decay_t<decltype(gridGeometry0.gridView())>::dimension;
+        for (const auto& element0 : elements(gridGeometry0.gridView()))
+        {
+            const auto& stencil = couplingManager.couplingStencil(element0, domainId1);
+            for (std::size_t vIdxLocal = 0; vIdxLocal < element0.subEntities(dim); ++vIdxLocal)
+            {
+                const auto globalI = gridGeometry0.vertexMapper().subIndex(element0, vIdxLocal, dim);
+                for (const auto globalJ : stencil)
+                    pattern.add(globalI, globalJ);
+            }
+        }
+    }
+
+    // matrix pattern for explicit Jacobians
+    // -> diagonal matrix, so coupling block is empty
+    // just return the empty pattern
+
+    return pattern;
+}
+
+} // namespace Dumux
+
+#endif
---
+++ dumux/multidomain/newtoncontroller.hh
@@ -0,0 +1,285 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup Nonlinear
+ * \ingroup MultiDomain
+ * \brief A Newton controller for mixed dimension simulations
+ */
+#ifndef DUMUX_MULTIDOMAIN_NEWTON_CONTROLLER_HH
+#define DUMUX_MULTIDOMAIN_NEWTON_CONTROLLER_HH
+
+#include <dune/common/exceptions.hh>
+#include <dune/common/hybridutilities.hh>
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/istl/bvector.hh>
+
+#include <dumux/common/exceptions.hh>
+#include <dumux/common/math.hh>
+#include <dumux/common/timeloop.hh>
+#include <dumux/linear/seqsolverbackend.hh>
+#include <dumux/linear/matrixconverter.hh>
+#include <dumux/nonlinear/newtoncontroller.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup Nonlinear
+ * \brief A Newton controller for mixed dimension simulations
+ */
+template <class Scalar, class CouplingManager,
+          class Comm = Dune::CollectiveCommunication<Dune::MPIHelper::MPICommunicator>>
+class MultiDomainNewtonController
+: public NewtonController<Scalar, Comm>
+{
+    using ParentType = NewtonController<Scalar, Comm>;
+public:
+    /*!
+     * \brief Constructor for stationary problems
+     */
+    MultiDomainNewtonController(std::shared_ptr<CouplingManager> couplingManager,
+                                const Comm& comm = Dune::MPIHelper::getCollectiveCommunication(),
+                                const std::string& paramGroup = "")
+    : ParentType(comm, paramGroup)
+    , couplingManager_(couplingManager)
+    {}
+
+    /*!
+     * \brief Constructor for instationary problems
+     */
+    MultiDomainNewtonController(std::shared_ptr<TimeLoop<Scalar>> timeLoop,
+                                std::shared_ptr<CouplingManager> couplingManager,
+                                const Comm& comm = Dune::MPIHelper::getCollectiveCommunication(),
+                                const std::string& paramGroup = "")
+    : ParentType(timeLoop, comm, paramGroup)
+    , couplingManager_(couplingManager)
+    {}
+
+    /*!
+     * \brief Indicates the beginning of a Newton iteration.
+     */
+    template<class SolutionVector>
+    void newtonBeginStep(const SolutionVector& uCurrentIter)
+    {
+        ParentType::newtonBeginStep(uCurrentIter);
+        // update the coupling manager's solution vector
+        couplingManager_->updateSolution(uCurrentIter);
+    }
+
+    /*!
+     * \brief Update the maximum relative shift of the solution compared to
+     *        the previous iteration.
+     *
+     * \param uLastIter The current iterative solution
+     * \param deltaU The difference between the current and the next solution
+     */
+    template<class SolutionVector>
+    void newtonUpdateShift(const SolutionVector &uLastIter,
+                           const SolutionVector &deltaU)
+    {
+        this->shift_ = 0;
+
+        using namespace Dune::Hybrid;
+        forEach(integralRange(Dune::Hybrid::size(uLastIter)), [&](const auto domainId)
+        {
+            for (int i = 0; i < int(uLastIter[domainId].size()); ++i)
+            {
+                auto uNewI = uLastIter[domainId][i];
+                uNewI -= deltaU[domainId][i];
+
+                Scalar shiftAtDof = this->relativeShiftAtDof_(uLastIter[domainId][i], uNewI);
+                using std::max;
+                this->shift_ = max(this->shift_, shiftAtDof);
+            }
+        });
+
+        if (this->comm().size() > 1)
+            this->shift_ = this->comm().max(this->shift_);
+    }
+
+    /*!
+     * \brief Solve the linear system of equations \f$\mathbf{A}x - b = 0\f$.
+     *
+     * \throws Dumux::NumericalProblem if the linear solver didn't
+     * converge.
+     * TODO: parallel
+     *
+     * \param ls The linear solver to be used
+     * \param A The matrix of the linear system of equations
+     * \param x The vector which solves the linear system
+     * \param b The right hand side of the linear system
+     */
+    template<class LinearSolver, class JacobianMatrix, class SolutionVector>
+    void solveLinearSystem(LinearSolver& ls,
+                           JacobianMatrix& A,
+                           SolutionVector& x,
+                           SolutionVector& b)
+    {
+        const auto& jac = A;
+        const auto& res = b;
+        using namespace Dune::Indices;
+        if (jac[_0][_0].N() < 28)
+        {
+            Dune::printmatrix(std::cout, jac[_0][_0], "", "", 15, 15);
+            Dune::printmatrix(std::cout, jac[_0][_1], "", "", 15, 15);
+            Dune::printmatrix(std::cout, jac[_1][_0], "", "", 15, 15);
+            Dune::printmatrix(std::cout, jac[_1][_1], "", "", 15, 15);
+            Dune::printvector(std::cout, res[_0], "", "", 15, 15);
+            Dune::printvector(std::cout, res[_1], "", "", 15, 15);
+        }
+
+        try {
+            if (this->numSteps_ == 0)
+            {
+                Scalar norm2 = b.two_norm2();
+                if (this->comm().size() > 1)
+                    norm2 = this->comm().sum(norm2);
+
+                using std::sqrt;
+                this->initialResidual_ = sqrt(norm2);
+            }
+
+            // TODO: Only do this is the linear solver really can't handle multitype matrices
+            // create the bcrs matrix the IterativeSolver backend can handle
+            const auto M = MatrixConverter<JacobianMatrix>::multiTypeToBCRSMatrix(A);
+            const auto bTmp = VectorConverter<SolutionVector>::multiTypeToBlockVector(b);
+            auto y = bTmp; y = 0.0;
+
+            // solve
+            const bool converged = ls.template solve</*precondBlockLevel=*/2>(M, y, bTmp);
+
+            // copy back the result y into x
+            VectorConverter<SolutionVector>::retrieveValues(x, y);
+
+            if (!converged) {
+                DUNE_THROW(NumericalProblem,
+                           "Linear solver did not converge");
+            }
+        }
+        catch (const Dune::Exception &e) {
+            throw NumericalProblem(e.what());
+        }
+    }
+
+    /*!
+     * \brief Update the current solution with a delta vector.
+     *
+     * The error estimates required for the newtonConverged() and
+     * newtonProceed() methods should be updated inside this method.
+     *
+     * Different update strategies, such as line search and chopped
+     * updates can be implemented. The default behavior is just to
+     * subtract deltaU from uLastIter, i.e.
+     * \f[ u^{k+1} = u^k - \Delta u^k \f]
+     *
+     * \param assembler The assembler (needed for global residual evaluation)
+     * \param uCurrentIter The solution vector after the current iteration
+     * \param uLastIter The solution vector after the last iteration
+     * \param deltaU The delta as calculated from solving the linear
+     *               system of equations. This parameter also stores
+     *               the updated solution.
+     * TODO: introducte update GridVariables for normal newton controller
+     * then this overload can be removed
+     */
+    template<class JacobianAssembler, class SolutionVector>
+    void newtonUpdate(JacobianAssembler& assembler,
+                      SolutionVector &uCurrentIter,
+                      const SolutionVector &uLastIter,
+                      const SolutionVector &deltaU)
+    {
+        if (this->enableShiftCriterion_)
+            this->newtonUpdateShift(uLastIter, deltaU);
+
+        if (this->useLineSearch_)
+        {
+            this->lineSearchUpdate_(assembler, uCurrentIter, uLastIter, deltaU);
+        }
+        else {
+            uCurrentIter = uLastIter;
+            uCurrentIter -= deltaU;
+
+            if (this->enableResidualCriterion_)
+            {
+                this->residualNorm_ = assembler.residualNorm(uCurrentIter);
+                this->reduction_ = this->residualNorm_;
+                this->reduction_ /= this->initialResidual_;
+            }
+            else
+            {
+                // If we get here, the convergence criterion does not require
+                // additional residual evalutions. Thus, the grid variables have
+                // not yet been updated to the new uCurrentIter.
+                assembler.updateGridVariables(uCurrentIter);
+            }
+        }
+    }
+
+    /*!
+     * \brief Indicates that one Newton iteration was finished.
+     *
+     * \param assembler The jacobian assembler
+     * \param uCurrentIter The solution after the current Newton iteration
+     * \param uLastIter The solution at the beginning of the current Newton iteration
+     */
+    template<class JacobianAssembler, class SolutionVector>
+    void newtonEndStep(JacobianAssembler& assembler,
+                       SolutionVector &uCurrentIter,
+                       const SolutionVector &uLastIter)
+    {
+        couplingManager_->updateSolution(uCurrentIter);
+        ParentType::newtonEndStep(assembler, uCurrentIter, uLastIter);
+    }
+
+    /*!
+     * \brief Called if the Newton method broke down.
+     * This method is called _after_ newtonEnd()
+     * TODO: Fix resetTimeStep for other models
+     */
+    template<class Assembler, class SolutionVector>
+    void newtonFail(Assembler& assembler, SolutionVector& u)
+    {
+        if (!assembler.isStationaryProblem())
+        {
+            // set solution to previous solution
+            u = assembler.prevSol();
+
+            // reset the grid variables to the previous solution
+            assembler.resetTimeStep(u);
+
+            if (this->verbose())
+            {
+                std::cout << "Newton solver did not converge with dt = "
+                          << this->timeLoop_->timeStepSize() << " seconds. Retrying with time step of "
+                          << this->timeLoop_->timeStepSize()/2.0 << " seconds\n";
+            }
+
+            // try again with dt = dt/2
+            this->timeLoop_->setTimeStepSize(this->timeLoop_->timeStepSize()/2.0);
+        }
+        else
+            DUNE_THROW(Dune::MathError, "Newton solver did not converge");
+    }
+
+private:
+    std::shared_ptr<CouplingManager> couplingManager_;
+};
+
+} // end namespace Dumux
+
+#endif
