44cf2339cd338c73dc6ea731fdb218a1b6e72bf8
Thomas Fetzer <thomas.fetzer@iws.uni-stuttgart.de>
2018-01-12 14:44:04 +0100

    [rans] First basic model

---
+++ dumux/freeflow/rans/problem.hh
@@ -0,0 +1,133 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup RANSModel
+ * \copydoc Dumux::RANSProblem
+ */
+#ifndef DUMUX_RANS_PROBLEM_HH
+#define DUMUX_RANS_PROBLEM_HH
+
+#include <dumux/common/properties.hh>
+#include <dumux/common/staggeredfvproblem.hh>
+#include <dumux/discretization/methods.hh>
+#include <dumux/freeflow/navierstokes/problem.hh>
+
+#include "model.hh"
+
+namespace Dumux
+{
+
+/*!
+ * \ingroup RANSModel
+ * \brief Reynolds-Averaged Navier-Stokes problem base class.
+ *
+ * This implements gravity (if desired) and a function returning the temperature.
+ * Includes a specialized method used only by the staggered grid discretization.
+ *
+ * \todo inherit all functions (especially gravity and temperature from Navier-Stokes)
+ */
+template<class TypeTag>
+class RANSProblem : public NavierStokesParentProblem<TypeTag>
+{
+    using ParentType = NavierStokesParentProblem<TypeTag>;
+    using Implementation = typename GET_PROP_TYPE(TypeTag, Problem);
+
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using Grid = typename GridView::Grid;
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+
+    using FVGridGeometry = typename GET_PROP_TYPE(TypeTag, FVGridGeometry);
+    using SubControlVolumeFace = typename GET_PROP_TYPE(TypeTag, SubControlVolumeFace);
+    using SolutionVector = typename GET_PROP_TYPE(TypeTag, SolutionVector);
+    using PrimaryVariables = typename GET_PROP_TYPE(TypeTag, PrimaryVariables);
+    using Indices = typename GET_PROP_TYPE(TypeTag, Indices);
+
+    enum {
+        dim = Grid::dimension,
+        dimWorld = Grid::dimensionworld
+      };
+    // TODO: dim or dimWorld appropriate here?
+    using GlobalPosition = Dune::FieldVector<Scalar, dimWorld>;
+
+public:
+    //! The constructor sets the gravity, if desired by the user.
+    RANSProblem(std::shared_ptr<const FVGridGeometry> fvGridGeometry)
+    : ParentType(fvGridGeometry)
+    {
+        if (getParamFromGroup<bool>(GET_PROP_VALUE(TypeTag, ModelParameterGroup), "Problem.EnableGravity"))
+            gravity_[dim-1]  = -9.81;
+    }
+
+    /*!
+     * \brief Returns the temperature \f$\mathrm{[K]}\f$ at a given global position.
+     *
+     * This is not specific to the discretization. By default it just
+     * calls temperature().
+     *
+     * \param globalPos The position in global coordinates where the temperature should be specified.
+     */
+    Scalar temperatureAtPos(const GlobalPosition &globalPos) const
+    { return asImp_().temperature(); }
+
+    /*!
+     * \brief Returns the temperature within the domain.
+     *
+     * This method MUST be overwritten by the actual problem.
+     */
+    Scalar temperature() const
+    { DUNE_THROW(Dune::NotImplemented, "temperature() method not implemented by the actual problem"); }
+
+    /*!
+     * \brief Returns the acceleration due to gravity.
+     *
+     * If the <tt>Problem.EnableGravity</tt> parameter is true, this means
+     * \f$\boldsymbol{g} = ( 0,\dots,\ -9.81)^T \f$, else \f$\boldsymbol{g} = ( 0,\dots, 0)^T \f$
+     */
+    const GlobalPosition &gravity() const
+    { return gravity_; }
+
+    //! Applys the initial face solution (velocities on the faces). Specialization for staggered grid discretization.
+    template <class T = TypeTag>
+    typename std::enable_if<GET_PROP_VALUE(T, DiscretizationMethod) == DiscretizationMethods::Staggered, void>::type
+    applyInititalFaceSolution(SolutionVector& sol,
+                              const SubControlVolumeFace& scvf,
+                              const PrimaryVariables& initSol) const
+    {
+        typename GET_PROP(TypeTag, DofTypeIndices)::FaceIdx faceIdx;
+        const auto numEqCellCenter = GET_PROP_VALUE(TypeTag, NumEqCellCenter);
+        sol[faceIdx][scvf.dofIndex()][numEqCellCenter] = initSol[Indices::velocity(scvf.directionIndex())];
+    }
+
+private:
+
+    //! Returns the implementation of the problem (i.e. static polymorphism)
+    Implementation &asImp_()
+    { return *static_cast<Implementation *>(this); }
+
+    //! \copydoc asImp_()
+    const Implementation &asImp_() const
+    { return *static_cast<const Implementation *>(this); }
+
+    GlobalPosition gravity_;
+};
+
+}
+
+#endif
---
+++ dumux/freeflow/rans/volumevariables.hh
@@ -0,0 +1,197 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup RANSModel
+ *
+ * \copydoc Dumux::RANSVolumeVariables
+ */
+#ifndef DUMUX_RANS_VOLUME_VARIABLES_HH
+#define DUMUX_RANS_VOLUME_VARIABLES_HH
+
+#include <dumux/common/properties.hh>
+#include <dumux/material/fluidstates/immiscible.hh>
+
+namespace Dumux
+{
+
+// forward declaration
+template <class TypeTag, bool enableEnergyBalance>
+class RANSVolumeVariablesImplementation;
+
+/*!
+ * \ingroup Reynolds-Averaged NavierStokesModel
+ * \brief Volume variables for the single-phase Reynolds-Averaged Navier-Stokes model.
+ *        The class is specialized for isothermal and non-isothermal models.
+ */
+template <class TypeTag>
+using RANSVolumeVariables = RANSVolumeVariablesImplementation<TypeTag, GET_PROP_VALUE(TypeTag, EnableEnergyBalance)>;
+
+/*!
+ * \ingroup Reynolds-Averaged NavierStokesModel
+ * \brief Volume variables for the isothermal single-phase Reynolds-Averaged Navier-Stokes model.
+ */
+template <class TypeTag>
+class RANSVolumeVariablesImplementation<TypeTag, false>
+: public NavierStokesVolumeVariablesImplementation<TypeTag, false>
+{
+    using ParentType = NavierStokesVolumeVariablesImplementation<TypeTag, false>;
+    using Implementation = typename GET_PROP_TYPE(TypeTag, VolumeVariables);
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+    using Problem = typename GET_PROP_TYPE(TypeTag, Problem);
+    using SubControlVolume = typename GET_PROP_TYPE(TypeTag, SubControlVolume);
+    using ElementSolutionVector = typename GET_PROP_TYPE(TypeTag, ElementSolutionVector);
+    using Indices = typename GET_PROP_TYPE(TypeTag, Indices);
+    using FluidSystem = typename GET_PROP_TYPE(TypeTag, FluidSystem);
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using Element = typename GridView::template Codim<0>::Entity;
+
+    static const int defaultPhaseIdx = GET_PROP_VALUE(TypeTag, PhaseIdx);
+
+public:
+
+    using FluidState = typename GET_PROP_TYPE(TypeTag, FluidState);
+
+    /*!
+     * \brief Update all quantities for a given control volume
+     *
+     * \param elemSol A vector containing all primary variables connected to the element
+     * \param problem The object specifying the problem which ought to
+     *                be simulated
+     * \param element An element which contains part of the control volume
+     * \param scv The sub-control volume
+     */
+    void update(const ElementSolutionVector &elemSol,
+                const Problem &problem,
+                const Element &element,
+                const SubControlVolume& scv)
+    {
+        ParentType::completeFluidState(elemSol, problem, element, scv, fluidState_);
+        dynamicEddyViscosity_ = scv.dofPosition()[1] * scv.dofPosition()[1];
+        std::cout << dynamicEddyViscosity_ << " "
+                  << asImp_().viscosity(defaultPhaseIdx) << " "
+                  << effectiveViscosity(defaultPhaseIdx) << " "
+                  << std::endl;
+    };
+
+    /*!
+     * \brief Return the dynamic eddy viscosity \f$\mathrm{[Pa s]}\f$ of the flow within the
+     *        control volume.
+     */
+    Scalar dynamicEddyViscosity() const
+    { return dynamicEddyViscosity_; }
+
+    /*!
+     * \brief Return the effective dynamic viscosity \f$\mathrm{[Pa s]}\f$ of the fluid within the
+     *        control volume.
+     */
+    Scalar effectiveViscosity(int phaseIdx = 0) const
+    { return asImp_().viscosity(defaultPhaseIdx) + dynamicEddyViscosity(); }
+
+private:
+    //! Returns the implementation of the problem (i.e. static polymorphism)
+    Implementation &asImp_()
+    { return *static_cast<Implementation *>(this); }
+
+    //! \copydoc asImp_()
+    const Implementation &asImp_() const
+    { return *static_cast<const Implementation *>(this); }
+
+protected:
+    FluidState fluidState_;
+    Scalar dynamicEddyViscosity_;
+};
+
+/*!
+ * \ingroup RANSModel
+ * \brief Volume variables for the non-isothermal single-phase Reynolds-Averaged Navier-Stokes model.
+ */
+template <class TypeTag>
+class RANSVolumeVariablesImplementation<TypeTag, true>
+: public NavierStokesVolumeVariablesImplementation<TypeTag, true>,
+  public RANSVolumeVariablesImplementation<TypeTag, false>
+{
+    using ParentTypeNonIsothermal = NavierStokesVolumeVariablesImplementation<TypeTag, true>;
+    using ParentTypeIsothermal = RANSVolumeVariablesImplementation<TypeTag, false>;
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+    using Problem = typename GET_PROP_TYPE(TypeTag, Problem);
+    using SubControlVolume = typename GET_PROP_TYPE(TypeTag, SubControlVolume);
+    using ElementSolutionVector = typename GET_PROP_TYPE(TypeTag, ElementSolutionVector);
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using Element = typename GridView::template Codim<0>::Entity;
+    using FluidSystem = typename GET_PROP_TYPE(TypeTag, FluidSystem);
+    using Indices = typename GET_PROP_TYPE(TypeTag, Indices);
+
+    static const int defaultPhaseIdx = GET_PROP_VALUE(TypeTag, PhaseIdx);
+//     static const int temperatureIdx = Indices::temperatureIdx;
+
+public:
+
+    using FluidState = typename GET_PROP_TYPE(TypeTag, FluidState);
+
+    /*!
+     * \brief Update all quantities for a given control volume
+     *
+     * \param elemSol A vector containing all primary variables connected to the element
+     * \param problem The object specifying the problem which ought to
+     *                be simulated
+     * \param element An element which contains part of the control volume
+     * \param scv The sub-control volume
+     */
+    void update(const ElementSolutionVector &elemSol,
+                const Problem &problem,
+                const Element &element,
+                const SubControlVolume &scv)
+    {
+        completeFluidState(elemSol, problem, element, scv, this->fluidState_);
+    }
+
+    /*!
+     * \brief Update the fluid state
+     */
+    static void completeFluidState(const ElementSolutionVector& elemSol,
+                                   const Problem& problem,
+                                   const Element& element,
+                                   const SubControlVolume& scv,
+                                   FluidState& fluidState)
+    {
+        ParentTypeIsothermal::completeFluidState(elemSol, problem, element, scv, fluidState);
+        ParentTypeNonIsothermal::completeFluidState(elemSol, problem, element, scv, fluidState);
+    }
+
+    /*!
+     * \brief Returns the eddy thermal conductivity \f$\mathrm{[W/(m*K)]}\f$
+     *        of the flow phase in the sub-control volume.
+     */
+    Scalar eddyThermalConductivity() const
+    { return ParentTypeIsothermal::dynamicEddyViscosity_(); }
+
+    /*!
+     * \brief Returns the effective thermal conductivity \f$\mathrm{[W/(m*K)]}\f$
+     *        of the fluid-flow in the sub-control volume.
+     */
+    Scalar effectiveThermalConductivity(int phaseIdx = 0) const
+    {
+        return FluidSystem::thermalConductivity(this->fluidState_, defaultPhaseIdx)
+               + eddyThermalConductivity();
+    }
+};
+}
+
+#endif
