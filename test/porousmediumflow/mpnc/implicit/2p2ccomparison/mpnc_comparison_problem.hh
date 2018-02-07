61e409706b4d637d68f41b0e4363066c1a8bc511
Katharina Heck <katharina.heck@iws.uni-stuttgart.de>
2018-02-07 14:24:25 +0100

    [fix] path to vtkoutputfields after renaming of folder

--- test/porousmediumflow/mpnc/implicit/2p2ccomparison/mpnc_comparison_problem.hh
+++ test/porousmediumflow/mpnc/implicit/2p2ccomparison/mpnc_comparison_problem.hh
@@ -31,7 +31,7 @@
 
 #include <dumux/porousmediumflow/mpnc/model.hh>
 #include <dumux/porousmediumflow/problem.hh>
-#include <test/porousmediumflow/2p2c/implicit/comparison/vtkoutputfields.hh>
+#include <test/porousmediumflow/2p2c/implicit/mpnccomparison/vtkoutputfields.hh>
 
 #include <dumux/material/fluidsystems/h2on2.hh>
 #include <dumux/material/fluidstates/compositional.hh>
