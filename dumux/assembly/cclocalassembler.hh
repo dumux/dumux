b0a873f9520a066273f1939fd9dd05863d1393e4
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2018-02-22 12:13:03 +0100

    [assembly] Use diffMethod for all DiffMethod template args

--- dumux/assembly/cclocalassembler.hh
+++ dumux/assembly/cclocalassembler.hh
@@ -116,7 +116,7 @@ public:
  * \tparam DM The differentiation method to residual compute derivatives
  * \tparam implicit Specifies whether the time discretization is implicit or not not (i.e. explicit)
  */
-template<class TypeTag, class Assembler, DiffMethod DM = DiffMethod::numeric, bool implicit = true>
+template<class TypeTag, class Assembler, DiffMethod diffMethod = DiffMethod::numeric, bool implicit = true>
 class CCLocalAssembler;
 
 /*!
