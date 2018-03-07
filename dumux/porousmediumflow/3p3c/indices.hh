1d7e4c9db05fef947aaca9feac8eeb5550d369bf
Beatrix Becker <beatrix.becker@iws.uni-stuttgart.de>
2018-03-07 15:49:19 +0100

    [3p3c] remove TypeTag from indices

--- dumux/porousmediumflow/3p3c/indices.hh
+++ dumux/porousmediumflow/3p3c/indices.hh
@@ -35,11 +35,9 @@ namespace Dumux
  *
  * \tparam PVOffset The first index in a primary variable vector.
  */
-template <class TypeTag, int PVOffset = 0>
+template <class FluidSystem, int PVOffset = 0>
 class ThreePThreeCIndices
 {
-    using FluidSystem = typename GET_PROP_TYPE(TypeTag, FluidSystem);
-
 public:
     // Phase indices
     static const int wPhaseIdx = FluidSystem::wPhaseIdx; //!< index of the wetting liquid phase
