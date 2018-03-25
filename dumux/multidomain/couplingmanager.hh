80f546f0d1c080cc0bf732994da53713f2fb2440
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2018-03-25 17:54:02 +0200

    [fixup][FVAssembler] Make additionalDofDependicies method const

--- dumux/multidomain/couplingmanager.hh
+++ dumux/multidomain/couplingmanager.hh
@@ -53,7 +53,7 @@ public:
 
     // Return an empty list of additional dof dependencies per default
     template<std::size_t id>
-    std::vector<std::vector<std::size_t>> additionalDofDependicies(Dune::index_constant<id>)
+    std::vector<std::vector<std::size_t>> additionalDofDependicies(Dune::index_constant<id>) const
     {
         return std::vector<std::vector<std::size_t>>();
     }
