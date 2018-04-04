79f3950eab13660320d8c06984bdd3a3dd890aa7
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2018-04-04 16:46:53 +0200

    [common] Partly free point source of TypeTag

--- dumux/common/properties/grid.hh
+++ dumux/common/properties/grid.hh
@@ -25,6 +25,7 @@
 #define DUMUX_GRID_PROPERTIES_HH
 
 #include <dune/common/version.hh>
+#include <dune/common/fvector.hh>
 
 #include <dumux/common/properties.hh>
 #include <dumux/common/pointsource.hh>
@@ -44,10 +45,18 @@ SET_TYPE_PROP(GridProperties, GridView, typename GET_PROP_TYPE(TypeTag, Grid)::L
 SET_TYPE_PROP(GridProperties, GridCreator, GridCreator<TypeTag>);
 
 //! Use the minimal point source implementation as default
-SET_TYPE_PROP(GridProperties, PointSource, PointSource<TypeTag>);
+SET_PROP(GridProperties, PointSource)
+{
+private:
+    using SourceValues = typename GET_PROP_TYPE(TypeTag, NumEqVector);
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using GlobalPosition = typename Dune::FieldVector<typename GridView::ctype, GridView::dimensionworld>;
+public:
+    using type = PointSource<GlobalPosition, SourceValues>;
+};
 
 //! Use the point source helper using the bounding box tree as a default
-SET_TYPE_PROP(GridProperties, PointSourceHelper, BoundingBoxTreePointSourceHelper<TypeTag>);
+SET_TYPE_PROP(GridProperties, PointSourceHelper, BoundingBoxTreePointSourceHelper);
 
 } // namespace Properties
 } // namespace Dumux
