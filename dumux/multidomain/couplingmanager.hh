a2ecc914dd275ea2b460d6a60674bf9b6587aa48
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2018-05-14 16:11:46 +0200

    adjust coupling framework to work with differing discretization methods

--- dumux/multidomain/couplingmanager.hh
+++ dumux/multidomain/couplingmanager.hh
@@ -35,50 +35,81 @@ namespace Dumux {
 template<class Traits, class Imp = void>
 class CouplingManager
 {
-
 public:
-    using StencilType = std::vector<std::size_t>;
+    //! export the type of the implementation
     using Implementation = Imp;
 
+    //! default type used for coupling element stencils
+    template<std::size_t domainI, std::size_t domainJ>
+    using CouplingStencilType = std::vector< std::size_t >;
+
+    //! default type for encapsulating both the element-local and grid index of a dof
+    template<std::size_t domainI, std::size_t domainJ>
+    struct DofData
+    {
+        std::size_t index;
+        unsigned int localIndex;
+    };
+
+    //! default type for storing data of all dofs within a coupled element
+    template<std::size_t domainI, std::size_t domainJ>
+    using CoupledElementDofData = std::vector< DofData<domainI, domainJ> >;
+
     /*!
-     * \brief The coupling stencil, i.e. which dofs of domain i are in the stencil of the element
+     * \brief The coupling element stencil, i.e. which elements of domain J
+     *        are coupled to the given element of domain I
      */
     template<class Element, std::size_t i, std::size_t j>
-    const StencilType& couplingStencil(const Element& element,
-                                       Dune::index_constant<i> domainI,
-                                       Dune::index_constant<j> domainJ) const
-    {
-        return asImp_().couplingStencil(element, domainI, domainJ);
-    }
-
-    // Return an empty list of additional dof dependencies per default
-    template<std::size_t id>
-    std::vector<std::vector<std::size_t>> additionalDofDependencies(Dune::index_constant<id>)
-    {
-        return std::vector<std::vector<std::size_t>>();
-    }
+    const CouplingStencilType<i, j>& couplingElementStencil(const Element& element,
+                                                            Dune::index_constant<i> domainI,
+                                                            Dune::index_constant<j> domainJ) const
+    { DUNE_THROW(Dune::NotImplemented, "Coupling manager does not implement couplingElementStencil() function"); }
 
     /*!
-     * \brief The coupling stencil, i.e. which dofs of domain i are in the stencil of the element
+     * \brief returns data on all dofs inside an element of domain j
+     *        that is coupled to an element of domain i with the given index
+     */
+    template<class ElementI, std::size_t i, class IndexTypeJ, std::size_t j>
+    const CoupledElementDofData<i, j>& coupledElementDofData(Dune::index_constant<i> domainI,
+                                                             const ElementI& elementI,
+                                                             Dune::index_constant<j> domainJ,
+                                                             IndexTypeJ globalJ) const
+    { DUNE_THROW(Dune::NotImplemented, "Coupling manager does not implement coupledElementDofData() function"); }
+
+    //! Return an empty list of additional dof dependencies per default
+    template<std::size_t id>
+    std::vector<std::vector<std::size_t>> additionalDofDependencies(Dune::index_constant<id>)
+    { return std::vector<std::vector<std::size_t>>(); }
+
+    //! Return an empty list of additional dof dependencies per default
+    template<std::size_t id, class Element>
+    std::vector<std::size_t> getAdditionalDofDependencies(Dune::index_constant<id>, const Element& element) const
+    { return std::vector<std::size_t>(); }
+
+    //! Return an empty list of additional dof dependencies per default
+    template<std::size_t id, class Element>
+    std::vector<std::size_t> getAdditionalDofDependenciesInverse(Dune::index_constant<id>, const Element& element) const
+    { return std::vector<std::size_t>(); }
+
+    /*!
+     * \brief Prepares all data on the other domains necessary for the assembly of an element of domain i
      */
     template<class Element, std::size_t i, class Assembler>
     void bindCouplingContext(Dune::index_constant<i> domainI, const Element& element, const Assembler& assembler)
-    {
-        return asImp_().bindCouplingContext(element, domainI, assembler);
-    }
+    { DUNE_THROW(Dune::NotImplemented, "Coupling manager does not implement bindCouplingContext() function"); }
 
     /*!
      * \brief Update the context for a derivative i->j
      */
-    template<std::size_t i, class Element, class PrimaryVariables, class Assembler>
+    template<std::size_t i, class Element, class ElementSolution, class Assembler>
     void updateCouplingContext(Dune::index_constant<i> domainI, Dune::index_constant<i> domainJ,
-                               const Element& element, const PrimaryVariables& priVars, const Assembler& assembler)
-    {
-        return asImp_().updateCouplingContext(domainI, domainJ, element, priVars, assembler);
-    }
+                               const Element& element, const ElementSolution& elemSol, const Assembler& assembler)
+    { DUNE_THROW(Dune::NotImplemented, "Coupling manager does not implement updateCouplingContext() function"); }
 
     /*!
-     * \brief The coupling stencil, i.e. which dofs of domain i are in the stencil of the element
+     * \brief Update the local views of domain i after the coupling context has been updated.
+     *        This is only necessary if some of these containers depend on quantities of the other domain
+     *        stored in the coupling context here in the coupling manager.
      */
     template<class Element, class FVElementGeometry,
              class ElementVolumeVariables, class ElementFluxVariablesCache, class GridVariables>
