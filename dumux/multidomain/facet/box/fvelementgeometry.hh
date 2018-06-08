be22c662515b2631f39c5a34ac67001c23791c79
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2018-06-08 12:44:52 +0200

    [multidomain][facet] introduce box facet coupling geometry classes

---
+++ dumux/multidomain/facet/box/fvelementgeometry.hh
@@ -0,0 +1,352 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup MixedDimension
+ * \ingroup MixedDimensionFacet
+ * \ingroup BoxDiscretization
+ * \brief \copydoc Dumux::BoxFacetCouplingFVElementGeometry
+ */
+#ifndef DUMUX_FACETCOUPLING_BOX_FV_ELEMENT_GEOMETRY_HH
+#define DUMUX_FACETCOUPLING_BOX_FV_ELEMENT_GEOMETRY_HH
+
+#include <algorithm>
+
+#include <dune/geometry/referenceelements.hh>
+
+#include <dumux/discretization/scvandscvfiterators.hh>
+#include <dumux/discretization/box/boxgeometryhelper.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup MixedDimension
+ * \ingroup MixedDimensionFacet
+ * \ingroup BoxDiscretization
+ * \brief Base class for the element-local finite volume geometry for box models
+ *        in the context of models considering coupling of different domains across the
+ *        bulk grid facets. This builds up the sub control volumes and sub control volume
+ *        faces for an element.
+ * \tparam GG the finite volume grid geometry type
+ * \tparam enableFVGridGeometryCache if the grid geometry is cached or not
+ */
+template<class GG, bool enableFVGridGeometryCache>
+class BoxFacetCouplingFVElementGeometry;
+
+//! specialization in case the FVElementGeometries are stored
+template<class GG>
+class BoxFacetCouplingFVElementGeometry<GG, true>
+{
+    using GridView = typename GG::GridView;
+    static constexpr int dim = GridView::dimension;
+    static constexpr int dimWorld = GridView::dimensionworld;
+    using IndexType = typename GridView::IndexSet::IndexType;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using CoordScalar = typename GridView::ctype;
+    using FeLocalBasis = typename GG::FeCache::FiniteElementType::Traits::LocalBasisType;
+    using ReferenceElements = typename Dune::ReferenceElements<CoordScalar, dim>;
+public:
+    //! export type of subcontrol volume
+    using SubControlVolume = typename GG::SubControlVolume;
+    //! export type of subcontrol volume face
+    using SubControlVolumeFace = typename GG::SubControlVolumeFace;
+    //! export type of finite volume grid geometry
+    using FVGridGeometry = GG;
+    //! the maximum number of scvs per element (2^dim for cubes)
+    static constexpr std::size_t maxNumElementScvs = (1<<dim);
+
+    //! Constructor
+    BoxFacetCouplingFVElementGeometry(const FVGridGeometry& fvGridGeometry)
+    : fvGridGeometryPtr_(&fvGridGeometry) {}
+
+    //! Get a sub control volume with a local scv index
+    const SubControlVolume& scv(IndexType scvIdx) const
+    { return fvGridGeometry().scvs(eIdx_)[scvIdx]; }
+
+    //! Get a sub control volume face with a local scvf index
+    const SubControlVolumeFace& scvf(IndexType scvfIdx) const
+    { return fvGridGeometry().scvfs(eIdx_)[scvfIdx]; }
+
+    //! iterator range for sub control volumes. Iterates over
+    //! all scvs of the bound element.
+    //! This is a free function found by means of ADL
+    //! To iterate over all sub control volumes of this FVElementGeometry use
+    //! for (auto&& scv : scvs(fvGeometry))
+    friend inline Dune::IteratorRange<typename std::vector<SubControlVolume>::const_iterator>
+    scvs(const BoxFacetCouplingFVElementGeometry& fvGeometry)
+    {
+        const auto& g = fvGeometry.fvGridGeometry();
+        using Iter = typename std::vector<SubControlVolume>::const_iterator;
+        return Dune::IteratorRange<Iter>(g.scvs(fvGeometry.eIdx_).begin(), g.scvs(fvGeometry.eIdx_).end());
+    }
+
+    //! iterator range for sub control volumes faces. Iterates over
+    //! all scvfs of the bound element.
+    //! This is a free function found by means of ADL
+    //! To iterate over all sub control volume faces of this FVElementGeometry use
+    //! for (auto&& scvf : scvfs(fvGeometry))
+    friend inline Dune::IteratorRange<typename std::vector<SubControlVolumeFace>::const_iterator>
+    scvfs(const BoxFacetCouplingFVElementGeometry& fvGeometry)
+    {
+        const auto& g = fvGeometry.fvGridGeometry();
+        using Iter = typename std::vector<SubControlVolumeFace>::const_iterator;
+        return Dune::IteratorRange<Iter>(g.scvfs(fvGeometry.eIdx_).begin(), g.scvfs(fvGeometry.eIdx_).end());
+    }
+
+    //! Get a local finite element basis
+    const FeLocalBasis& feLocalBasis() const
+    { return fvGridGeometry().feCache().get(elementPtr_->geometry().type()).localBasis(); }
+
+    //! The total number of sub control volumes
+    std::size_t numScv() const
+    { return fvGridGeometry().scvs(eIdx_).size(); }
+
+    //! The total number of sub control volume faces
+    std::size_t numScvf() const
+    { return fvGridGeometry().scvfs(eIdx_).size(); }
+
+    //! this function is for compatibility reasons with cc methods
+    //! The box stencil is always element-local so bind and bindElement
+    //! are identical.
+    void bind(const Element& element)
+    {
+        this->bindElement(element);
+    }
+
+    //! Binding of an element, has to be called before using the fvgeometries
+    //! Prepares all the volume variables within the element
+    //! For compatibility reasons with the FVGeometry cache being disabled
+    void bindElement(const Element& element)
+    {
+        elementPtr_ = &element;
+        eIdx_ = fvGridGeometry().elementMapper().index(element);
+    }
+
+    //! The global finite volume geometry we are a restriction of
+    const FVGridGeometry& fvGridGeometry() const
+    { return *fvGridGeometryPtr_; }
+
+private:
+    const Element* elementPtr_;
+    const FVGridGeometry* fvGridGeometryPtr_;
+
+    IndexType eIdx_;
+};
+
+//! specialization in case the geometries are not stored grid-wide
+template<class GG>
+class BoxFacetCouplingFVElementGeometry<GG, false>
+{
+    using GridView = typename GG::GridView;
+    static constexpr int dim = GridView::dimension;
+    static constexpr int dimWorld = GridView::dimensionworld;
+
+    using IndexType = typename GridView::IndexSet::IndexType;
+    using Element = typename GridView::template Codim<0>::Entity;
+
+    using CoordScalar = typename GridView::ctype;
+    using FeLocalBasis = typename GG::FeCache::FiniteElementType::Traits::LocalBasisType;
+    using ReferenceElements = typename Dune::ReferenceElements<CoordScalar, dim>;
+
+    using GeometryHelper = BoxGeometryHelper<GridView, dim,
+                                             typename GG::SubControlVolume,
+                                             typename GG::SubControlVolumeFace>;
+public:
+    //! export type of subcontrol volume
+    using SubControlVolume = typename GG::SubControlVolume;
+    //! export type of subcontrol volume face
+    using SubControlVolumeFace = typename GG::SubControlVolumeFace;
+    //! export type of finite volume grid geometry
+    using FVGridGeometry = GG;
+    //! the maximum number of scvs per element (2^dim for cubes)
+    static constexpr std::size_t maxNumElementScvs = (1<<dim);
+
+    //! Constructor
+    BoxFacetCouplingFVElementGeometry(const FVGridGeometry& fvGridGeometry)
+    : fvGridGeometryPtr_(&fvGridGeometry) {}
+
+    //! Get a sub control volume with a local scv index
+    const SubControlVolume& scv(IndexType scvIdx) const
+    { return scvs_[scvIdx]; }
+
+    //! Get a sub control volume face with a local scvf index
+    const SubControlVolumeFace& scvf(IndexType scvfIdx) const
+    { return scvfs_[scvfIdx]; }
+
+    //! iterator range for sub control volumes. Iterates over
+    //! all scvs of the bound element.
+    //! This is a free function found by means of ADL
+    //! To iterate over all sub control volumes of this FVElementGeometry use
+    //! for (auto&& scv : scvs(fvGeometry))
+    friend inline Dune::IteratorRange<typename std::vector<SubControlVolume>::const_iterator>
+    scvs(const BoxFacetCouplingFVElementGeometry& fvGeometry)
+    {
+        using Iter = typename std::vector<SubControlVolume>::const_iterator;
+        return Dune::IteratorRange<Iter>(fvGeometry.scvs_.begin(), fvGeometry.scvs_.end());
+    }
+
+    //! iterator range for sub control volumes faces. Iterates over
+    //! all scvfs of the bound element.
+    //! This is a free function found by means of ADL
+    //! To iterate over all sub control volume faces of this FVElementGeometry use
+    //! for (auto&& scvf : scvfs(fvGeometry))
+    friend inline Dune::IteratorRange<typename std::vector<SubControlVolumeFace>::const_iterator>
+    scvfs(const BoxFacetCouplingFVElementGeometry& fvGeometry)
+    {
+        using Iter = typename std::vector<SubControlVolumeFace>::const_iterator;
+        return Dune::IteratorRange<Iter>(fvGeometry.scvfs_.begin(), fvGeometry.scvfs_.end());
+    }
+
+    //! Get a local finite element basis
+    const FeLocalBasis& feLocalBasis() const
+    { return fvGridGeometry().feCache().get(elementPtr_->geometry().type()).localBasis(); }
+
+    //! The total number of sub control volumes
+    std::size_t numScv() const
+    { return scvs_.size(); }
+
+    //! The total number of sub control volume faces
+    std::size_t numScvf() const
+    { return scvfs_.size(); }
+
+    //! this function is for compatibility reasons with cc methods
+    //! The box stencil is always element-local so bind and bindElement
+    //! are identical.
+    void bind(const Element& element)
+    {
+        this->bindElement(element);
+    }
+
+    //! Binding of an element, has to be called before using the fvgeometries
+    //! Prepares all the volume variables within the element
+    //! For compatibility reasons with the FVGeometry cache being disabled
+    void bindElement(const Element& element)
+    {
+        elementPtr_ = &element;
+        eIdx_ = fvGridGeometry().elementMapper().index(element);
+        makeElementGeometries(element);
+    }
+
+    //! The global finite volume geometry we are a restriction of
+    const FVGridGeometry& fvGridGeometry() const
+    { return *fvGridGeometryPtr_; }
+
+private:
+
+    void makeElementGeometries(const Element& element)
+    {
+        auto eIdx = fvGridGeometry().elementMapper().index(element);
+
+        // get the element geometry
+        auto elementGeometry = element.geometry();
+        const auto referenceElement = ReferenceElements::general(elementGeometry.type());
+
+        // get the sub control volume geometries of this element
+        GeometryHelper geometryHelper(elementGeometry);
+
+        // construct the sub control volumes
+        scvs_.clear();
+        scvs_.reserve(elementGeometry.corners());
+        using LocalIndexType = typename SubControlVolumeFace::Traits::LocalIndexType;
+        for (LocalIndexType scvLocalIdx = 0; scvLocalIdx < elementGeometry.corners(); ++scvLocalIdx)
+            scvs_.emplace_back(geometryHelper,
+                               scvLocalIdx,
+                               eIdx,
+                               fvGridGeometry().vertexMapper().subIndex(element, scvLocalIdx, dim));
+
+        // construct the sub control volume faces
+        const auto numInnerScvf = (dim==1) ? 1 : element.subEntities(dim-1);
+        scvfs_.clear();
+        scvfs_.reserve(numInnerScvf);
+
+        unsigned int scvfLocalIdx = 0;
+        for (; scvfLocalIdx < numInnerScvf; ++scvfLocalIdx)
+        {
+            // find the local scv indices this scvf is connected to
+            std::vector<LocalIndexType> localScvIndices({static_cast<LocalIndexType>(referenceElement.subEntity(scvfLocalIdx, dim-1, 0, dim)),
+                                                         static_cast<LocalIndexType>(referenceElement.subEntity(scvfLocalIdx, dim-1, 1, dim))});
+
+            // create the sub-control volume face
+            scvfs_.emplace_back(geometryHelper,
+                                element,
+                                elementGeometry,
+                                scvfLocalIdx,
+                                std::move(localScvIndices));
+        }
+
+        // construct the sub control volume faces on the domain/interior boundaries
+        for (const auto& intersection : intersections(fvGridGeometry().gridView(), element))
+        {
+            // determine if all corners live on the facet grid
+            const auto isGeometry = intersection.geometry();
+            const auto numFaceCorners = isGeometry.corners();
+            const auto idxInInside = intersection.indexInInside();
+            const auto boundary = intersection.boundary();
+
+            std::vector<LocalIndexType> vIndicesLocal(numFaceCorners);
+            for (int i = 0; i < numFaceCorners; ++i)
+                vIndicesLocal[i] = static_cast<LocalIndexType>(referenceElement.subEntity(idxInInside, 1, i, dim));
+
+            // if all vertices are living on the facet grid, this is an interiour boundary
+            const bool isOnFacet = std::all_of( vIndicesLocal.begin(),
+                                                vIndicesLocal.end(),
+                                                [&] (auto vIdxLocal)
+                                                {
+                                                    const auto dofIdx = this->fvGridGeometry().vertexMapper().subIndex(element, vIdxLocal, dim);
+                                                    return this->fvGridGeometry().dofOnInteriorBoundary(dofIdx);
+                                                } );
+            if (isOnFacet || boundary)
+            {
+                for (unsigned int isScvfLocalIdx = 0; isScvfLocalIdx < isGeometry.corners(); ++isScvfLocalIdx)
+                {
+                    // find the inside scv this scvf is belonging to (localIdx = element local vertex index)
+                    std::vector<LocalIndexType> localScvIndices = {vIndicesLocal[isScvfLocalIdx], vIndicesLocal[isScvfLocalIdx]};
+
+                    // create the sub-control volume face
+                    scvfs_.emplace_back(geometryHelper,
+                                        intersection,
+                                        isGeometry,
+                                        isScvfLocalIdx,
+                                        scvfLocalIdx,
+                                        std::move(localScvIndices),
+                                        boundary,
+                                        isOnFacet);
+
+                    // increment local counter
+                    scvfLocalIdx++;
+                }
+            }
+        }
+    }
+
+    //! The bound element
+    const Element* elementPtr_;
+    IndexType eIdx_;
+
+    //! The global geometry this is a restriction of
+    const FVGridGeometry* fvGridGeometryPtr_;
+
+    //! vectors to store the geometries locally after binding an element
+    std::vector<SubControlVolume> scvs_;
+    std::vector<SubControlVolumeFace> scvfs_;
+};
+
+} // end namespace Dumux
+
+#endif
---
+++ dumux/multidomain/facet/box/subcontrolvolumeface.hh
@@ -0,0 +1,221 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup MixedDimension
+ * \ingroup MixedDimensionFacet
+ * \ingroup BoxDiscretization
+ * \brief Base class for a sub control volume face of the box method
+ *        in the context of of models considering coupling of different
+ *        domains across the bulk grid facets.
+ */
+#ifndef DUMUX_FACETCOUPLING_BOX_SUBCONTROLVOLUMEFACE_HH
+#define DUMUX_FACETCOUPLING_BOX_SUBCONTROLVOLUMEFACE_HH
+
+#include <utility>
+
+#include <dune/geometry/type.hh>
+#include <dune/geometry/multilineargeometry.hh>
+
+#include <dumux/common/boundaryflag.hh>
+#include <dumux/discretization/subcontrolvolumefacebase.hh>
+#include <dumux/discretization/box/boxgeometryhelper.hh>
+#include <dumux/discretization/box/subcontrolvolumeface.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup MixedDimension
+ * \ingroup MixedDimensionFacet
+ * \ingroup BoxDiscretization
+ * \brief Class for a sub control volume face in the box method, i.e a part of the boundary
+ *        of a sub control volume we compute fluxes on. This is a specialization for models
+ *        considering coupling of different domains across the bulk grid facets.
+ * \tparam GV the type of the grid view
+ * \tparam T the scvf geometry traits
+ */
+template<class GV, class T = BoxDefaultScvfGeometryTraits<GV> >
+class BoxFacetCouplingSubControlVolumeFace
+: public SubControlVolumeFaceBase<BoxFacetCouplingSubControlVolumeFace<GV, T>, T>
+{
+    using ThisType = BoxSubControlVolumeFace<GV, T>;
+    using ParentType = SubControlVolumeFaceBase<ThisType, T>;
+    using GridIndexType = typename T::GridIndexType;
+    using LocalIndexType = typename T::LocalIndexType;
+    using Scalar = typename T::Scalar;
+    using CornerStorage = typename T::CornerStorage;
+    using Geometry = typename T::Geometry;
+    using BoundaryFlag = typename T::BoundaryFlag;
+
+public:
+    //! export the type used for global coordinates
+    using GlobalPosition = typename T::GlobalPosition;
+    //! state the traits public and thus export all types
+    using Traits = T;
+
+    //! The default constructor
+    BoxFacetCouplingSubControlVolumeFace() = default;
+
+    //! Constructor for inner scvfs
+    template<class GeometryHelper, class Element>
+    BoxFacetCouplingSubControlVolumeFace(const GeometryHelper& geometryHelper,
+                                         const Element& element,
+                                         const typename Element::Geometry& elemGeometry,
+                                         unsigned int scvfIndex,
+                                         std::vector<LocalIndexType>&& scvIndices)
+    : corners_(geometryHelper.getScvfCorners(scvfIndex))
+    , center_(0.0)
+    , unitOuterNormal_(geometryHelper.normal(corners_, scvIndices))
+    , area_(geometryHelper.scvfArea(corners_))
+    , scvfIndex_(scvfIndex)
+    , scvIndices_(std::move(scvIndices))
+    , facetIndex_(/*undefined*/)
+    , indexInFacet_(/*undefined*/)
+    , boundary_(false)
+    , interiorBoundary_(false)
+    , boundaryFlag_{}
+    {
+        for (const auto& corner : corners_)
+            center_ += corner;
+        center_ /= corners_.size();
+    }
+
+    //! Constructor for domain or interior boundary scvfs
+    template<class GeometryHelper, class Intersection>
+    BoxFacetCouplingSubControlVolumeFace(const GeometryHelper& geometryHelper,
+                                         const Intersection& intersection,
+                                         const typename Intersection::Geometry& isGeometry,
+                                         LocalIndexType indexInIntersection,
+                                         GridIndexType scvfIndex,
+                                         std::vector<LocalIndexType>&& scvIndices,
+                                         bool boundary,
+                                         bool interiorBoundary)
+    : corners_(geometryHelper.getBoundaryScvfCorners(intersection, isGeometry, indexInIntersection))
+    , center_(0.0)
+    , unitOuterNormal_(intersection.centerUnitOuterNormal())
+    , area_(geometryHelper.scvfArea(corners_))
+    , scvfIndex_(scvfIndex)
+    , scvIndices_(std::move(scvIndices))
+    , facetIndex_(intersection.indexInInside())
+    , indexInFacet_(indexInIntersection)
+    , boundary_(boundary)
+    , interiorBoundary_(interiorBoundary)
+    , boundaryFlag_{intersection}
+    {
+        for (const auto& corner : corners_)
+            center_ += corner;
+        center_ /= corners_.size();
+    }
+
+    //! The center of the sub control volume face
+    const GlobalPosition& center() const
+    { return center_; }
+
+    //! The integration point for flux evaluations in global coordinates
+    const GlobalPosition& ipGlobal() const
+    { return center_; }
+
+    //! The area of the sub control volume face
+    Scalar area() const
+    { return area_; }
+
+    //! returns bolean if the sub control volume face is on the boundary
+    bool boundary() const
+    { return boundary_; }
+
+    //! returns bolean if the sub control volume face is on an interior boundary
+    bool interiorBoundary() const
+    { return interiorBoundary_; }
+
+    //! returns the unit nurmal vector pointing outwards
+    //! of the sub-control volume that this scvf encloses
+    const GlobalPosition& unitOuterNormal() const
+    { return unitOuterNormal_; }
+
+    //! index of the inside sub control volume for spatial param evaluation
+    LocalIndexType insideScvIdx() const
+    { return scvIndices_[0]; }
+
+    //! The element-local index of this sub control volume face
+    GridIndexType index() const
+    { return scvfIndex_; }
+
+    //! index of the outside sub control volume for spatial param evaluation
+    //! This results in undefined behaviour if boundary is true
+    LocalIndexType outsideScvIdx() const
+    {
+        assert(!boundary());
+        return scvIndices_[1];
+    }
+
+    //! returns the element-local index of the facet this scvf is embedded in.
+    //! This is only valid to be called for scvfs on domain/interior boundaries.
+    LocalIndexType facetIndexInElement() const
+    {
+        assert(interiorBoundary_ || boundary_);
+        return facetIndex_;
+    }
+
+    //! returns the facet-local (intersection-local) index of this scvf.
+    //! This is only valid to be called for scvfs on domain/interior boundaries.
+    LocalIndexType indexInElementFacet() const
+    {
+        assert(interiorBoundary_ || boundary_);
+        return indexInFacet_;
+    }
+
+    //! The geometry of the sub control volume face
+    Geometry geometry() const
+    { return Geometry(Dune::GeometryTypes::cube(Geometry::mydimension), corners_); }
+
+    //! Return the boundary flag
+    typename BoundaryFlag::value_type boundaryFlag() const
+    { return boundaryFlag_.get(); }
+
+    //! returns the position of a corner of the face
+    const GlobalPosition& corner(unsigned int localIdx) const
+    {
+        assert(localIdx < corners_.size() && "provided index exceeds the number of corners");
+        return corners_[localIdx];
+    }
+
+private:
+    // geometrical information
+    CornerStorage corners_;
+    GlobalPosition center_;
+    GlobalPosition unitOuterNormal_;
+    Scalar area_;
+
+    // indices
+    GridIndexType scvfIndex_;
+    std::vector<LocalIndexType> scvIndices_;
+
+    // indices valid for domain/interior boundary scvfs
+    LocalIndexType facetIndex_;
+    LocalIndexType indexInFacet_;
+
+    // boundary information
+    bool boundary_;
+    bool interiorBoundary_;
+    BoundaryFlag boundaryFlag_;
+};
+
+} // end namespace Dumux
+
+#endif
