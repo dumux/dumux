544b447ca9a4b1578f650c8e9c6490906c1103dd
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2018-06-13 14:43:52 +0200

    [geometry] Add option for collision detection for structured cube grids

--- dumux/common/geometry/intersectingentities.hh
+++ dumux/common/geometry/intersectingentities.hh
@@ -40,11 +40,12 @@ namespace Dumux {
 template<class EntitySet, class ctype, int dimworld>
 inline std::vector<std::size_t>
 intersectingEntities(const Dune::FieldVector<ctype, dimworld>& point,
-                     const BoundingBoxTree<EntitySet>& tree)
+                     const BoundingBoxTree<EntitySet>& tree,
+                     bool isStructuredCubeGrid = false)
 {
     // Call the recursive find function to find candidates
     std::vector<std::size_t> entities;
-    intersectingEntities(point, tree, tree.numBoundingBoxes() - 1, entities);
+    intersectingEntities(point, tree, tree.numBoundingBoxes() - 1, entities, isStructuredCubeGrid);
     return entities;
 }
 
@@ -56,7 +57,8 @@ template<class EntitySet, class ctype, int dimworld>
 void intersectingEntities(const Dune::FieldVector<ctype, dimworld>& point,
                           const BoundingBoxTree<EntitySet>& tree,
                           std::size_t node,
-                          std::vector<std::size_t>& entities)
+                          std::vector<std::size_t>& entities,
+                          bool isStructuredCubeGrid = false)
 {
     // Get the bounding box for the current node
     const auto& bBox = tree.getBoundingBoxNode(node);
@@ -69,17 +71,24 @@ void intersectingEntities(const Dune::FieldVector<ctype, dimworld>& point,
     else if (tree.isLeaf(bBox, node))
     {
         const std::size_t entityIdx = bBox.child1;
-        const auto geometry = tree.entitySet().entity(entityIdx).geometry();
-        // if the primitive is positive it intersects the actual geometry, add the entity to the list
-        if (intersectsPointGeometry(point, geometry))
+        // for structured cube grids skip the primitive test
+        if (isStructuredCubeGrid)
             entities.push_back(entityIdx);
+        else
+        {
+
+            const auto geometry = tree.entitySet().entity(entityIdx).geometry();
+            // if the primitive is positive it intersects the actual geometry, add the entity to the list
+            if (intersectsPointGeometry(point, geometry))
+                entities.push_back(entityIdx);
+        }
     }
 
     // No leaf. Check both children nodes.
     else
     {
-        intersectingEntities(point, tree, bBox.child0, entities);
-        intersectingEntities(point, tree, bBox.child1, entities);
+        intersectingEntities(point, tree, bBox.child0, entities, isStructuredCubeGrid);
+        intersectingEntities(point, tree, bBox.child1, entities, isStructuredCubeGrid);
     }
 }
 
