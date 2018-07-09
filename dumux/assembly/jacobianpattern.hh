ff4be888af3d058085695a42fe99eef2c1b08e6c
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2018-07-09 18:11:35 +0200

    [assembly] Add jacobian pattern for multidomain staggered

--- dumux/assembly/jacobianpattern.hh
+++ dumux/assembly/jacobianpattern.hh
@@ -105,6 +105,52 @@ Dune::MatrixIndexSet getJacobianPattern(const GridGeometry& gridGeometry)
     return pattern;
 }
 
+/*!
+ * \ingroup Assembly
+ * \brief Helper function to generate Jacobian pattern for the staggered method
+ */
+template<bool isImplicit, class GridGeometry,
+         typename std::enable_if_t<( (GridGeometry::discMethod == DiscretizationMethod::staggered) ), int> = 0>
+auto getJacobianPattern(const GridGeometry& gridGeometry)
+{
+    // resize the jacobian and the residual
+    const auto numDofs = gridGeometry.numDofs();
+    Dune::MatrixIndexSet pattern(numDofs, numDofs);
+
+
+    const auto& connectivityMap = gridGeometry.connectivityMap();
+
+    // evaluate the acutal pattern
+    for (const auto& element : elements(gridGeometry.gridView()))
+    {
+        if(gridGeometry.isCellCenter())
+        {
+            // the global index of the element at hand
+            static constexpr auto cellCenterIdx = GridGeometry::cellCenterIdx();
+            const auto ccGlobalI = gridGeometry.elementMapper().index(element);
+
+            for (auto&& ccGlobalJ : connectivityMap(cellCenterIdx, cellCenterIdx, ccGlobalI))
+                pattern.add(ccGlobalI, ccGlobalJ);
+        }
+        else
+        {
+            static constexpr auto faceIdx = GridGeometry::faceIdx();
+            auto fvGeometry = localView(gridGeometry);
+            fvGeometry.bindElement(element);
+
+            // loop over sub control faces
+            for (auto&& scvf : scvfs(fvGeometry))
+            {
+                const auto faceGlobalI = scvf.dofIndex();
+                for (auto&& faceGlobalJ : connectivityMap(faceIdx, faceIdx, scvf.index()))
+                    pattern.add(faceGlobalI, faceGlobalJ);
+            }
+        }
+    }
+
+    return pattern;
+}
+
 } // namespace Dumux
 
 #endif
