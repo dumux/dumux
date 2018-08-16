eb011e5d035c7b3710e1014a92c3cf7f45f34f98
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2018-08-16 10:16:39 +0200

    [material][porodeformation] add comment with reference

--- dumux/material/fluidmatrixinteractions/porositydeformation.hh
+++ dumux/material/fluidmatrixinteractions/porositydeformation.hh
@@ -51,6 +51,15 @@ public:
      * \param globalPos The global position (in the element)
      * \param refPoro The solid matrix porosity without deformation
      * \param minPoro A minimum porosity value
+     *
+     * \note Han and Dusseault (2003, doi 10.1016/S0920-4105(03)00047-0)
+     *       provide a derivation for \f$\text{d} \phi = -(1 - \phi ) \text{d} \epsilon_v\f$.
+     *       Here, \f$\epsilon_v\f$ is equal to \f$text{div} \mathbf{u}\f$.
+     *       By using an initial porosity \f$\phi_0\f$ and assuming \epsilon_{v, 0} = 0,
+     *       one obtains \f$\phi = \frac{\phi_0 - \text{div} \mathbf{u}}{1 - \text{div} \mathbf{u}}\f$,
+     *       which is the formulation for the rock mechanics sign convention. Here we are
+     *       using the continuum mechanics sign convention, thus, the final formula reads:
+     *       \f$\phi = \frac{\phi_0 + \text{div} \mathbf{u}}{1 + \text{div} \mathbf{u}}\f$.
      */
     template< class FVGridGeom, class ElemSol >
     static Scalar evaluatePorosity(const FVGridGeom& fvGridGeometry,
