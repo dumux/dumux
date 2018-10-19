a206b6c035140cde7e7add792c2993412d70b05c
Hanchuan WU <whc.shmily@gmail.com>
2018-10-19 17:35:11 +0200

    BCproblemfromimpes

---
+++ test/porousmediumflow/2p/sequential/buckleyleverett/pressureproblem.hh
@@ -0,0 +1,270 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \ingroup TwoPTests
+ * \brief The properties for the incompressible 2p test
+ */
+#ifndef DUMUX_BUCKLEYLEVERETT_PRESSURE_TEST_PROBLEM_HH
+#define DUMUX_BUCKLEYLEVERETT_PRESSURE_TEST_PROBLEM_HH
+
+#include <dune/grid/yaspgrid.hh>
+#include <dune/grid/uggrid.hh>
+
+#include <dumux/discretization/box/properties.hh>
+#include <dumux/discretization/cellcentered/tpfa/properties.hh>
+#include <dumux/discretization/cellcentered/mpfa/properties.hh>
+
+#include <dumux/material/components/trichloroethene.hh>
+#include <dumux/material/components/simpleh2o.hh>
+#include <dumux/material/fluidsystems/1pliquid.hh>
+#include <dumux/material/fluidsystems/2pimmiscible.hh>
+
+#include <dumux/porousmediumflow/problem.hh>
+#include <dumux/porousmediumflow/2p/sequential/pressure/model.hh>
+
+#include "pressurespatialparams.hh"
+
+namespace Dumux {
+// forward declarations
+template<class TypeTag> class TwoPTestProblem;
+
+namespace Properties {
+NEW_TYPE_TAG(TwoPImpes, INHERITS_FROM(CCTpfaModel,Pressure));
+
+// Set the grid type
+SET_TYPE_PROP(TwoPImpes, Grid, Dune::YaspGrid<2>);
+
+// Set the problem type
+SET_TYPE_PROP(TwoPImpes, Problem, TwoPTestProblem<TypeTag>);
+
+// Set the fluid system
+SET_PROP(TwoPImpes, FluidSystem)
+{
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+    using WettingPhase = FluidSystems::OnePLiquid<Scalar, Components::SimpleH2O<Scalar> >;
+#if PROBLEM == 2
+    using NonwettingPhase = FluidSystems::OnePLiquid<Scalar, Components::Trichloroethene<Scalar> >;
+#else
+    using NonwettingPhase = FluidSystems::OnePLiquid<Scalar, Components::Constant<1, Scalar> >;
+#endif
+    using type = FluidSystems::TwoPImmiscible<Scalar, WettingPhase, NonwettingPhase>;
+};
+
+// Set the spatial parameters
+SET_PROP(TwoPImpes, SpatialParams)
+{
+private:
+    using FVGridGeometry = typename GET_PROP_TYPE(TypeTag, FVGridGeometry);
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+public:
+    using type = TwoPTestSpatialParams<FVGridGeometry, Scalar>;
+};
+
+// Enable caching
+SET_BOOL_PROP(TwoPImpes, EnableGridVolumeVariablesCache, true);
+SET_BOOL_PROP(TwoPImpes, EnableGridFluxVariablesCache, true);
+SET_BOOL_PROP(TwoPImpes, EnableFVGridGeometryCache, true);
+} // end namespace Properties
+
+/*!
+ * \ingroup TwoPTests
+ * \brief The incompressible 2p test problem.
+ */
+template<class TypeTag>
+class TwoPTestProblem : public PorousMediumFlowProblem<TypeTag>
+{
+    using ParentType = PorousMediumFlowProblem<TypeTag>;
+    using GridView = typename GET_PROP_TYPE(TypeTag, GridView);
+    using Element = typename GridView::template Codim<0>::Entity;
+    using Scalar = typename GET_PROP_TYPE(TypeTag, Scalar);
+    using FluidSystem = typename GET_PROP_TYPE(TypeTag, FluidSystem);
+    using PrimaryVariables = typename GET_PROP_TYPE(TypeTag, PrimaryVariables);
+    using FVGridGeometry = typename GET_PROP_TYPE(TypeTag, FVGridGeometry);
+    using BoundaryTypes = typename GET_PROP_TYPE(TypeTag, BoundaryTypes);
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+    using NumEqVector = typename GET_PROP_TYPE(TypeTag, NumEqVector);
+    using Indices = typename GET_PROP_TYPE(TypeTag, ModelTraits)::Indices;
+    enum {
+        pressureEqIdx = Indices::pressureEqIdx,
+        pressureIdx = Indices::pressureIdx
+    };
+
+    static constexpr int dimWorld = GridView::dimensionworld;
+public:
+    TwoPTestProblem(std::shared_ptr<const FVGridGeometry> fvGridGeometry)
+    : ParentType(fvGridGeometry)
+    {
+        Scalar inletWidth = getParam<Scalar>("Problem.InletWidth", 1.0);
+        GlobalPosition inletCenter = this->fvGridGeometry().bBoxMax();
+        inletCenter[0] *= 0.5;
+
+        inletLeftCoord_ = inletCenter;
+        inletLeftCoord_[0] -=0.5*inletWidth;
+        inletRightCoord_ = inletCenter;
+        inletRightCoord_[0] +=0.5*inletWidth;
+
+        inFlux_ = getParam<Scalar>("Problem.InjectionFlux", 1e-4);
+    }
+
+    /*!
+     * \brief Specifies which kind of boundary condition should be
+     *        used for which equation on a given boundary segment
+     *
+     * \param values Stores the value of the boundary type
+     * \param globalPos The global position
+     */
+    BoundaryTypes boundaryTypesAtPos(const GlobalPosition &globalPos) const
+    {
+#if PROBLEM == 2
+    BoundaryTypes values;
+    if (onLowerBoundary_(globalPos) || onUpperBoundary_(globalPos))
+    {
+        values.setAllNeumann();
+    }
+    else
+    {
+        values.setAllDirichlet();
+    }
+    return values;
+#else
+        BoundaryTypes values;
+        if (onLeftBoundary_(globalPos))
+            values.setAllDirichlet();
+        else
+            values.setAllNeumann();
+        return values;
+#endif
+    }
+
+    /*!
+     * \brief Evaluates the boundary conditions for a Dirichlet
+     *        boundary segment
+     *
+     * \param values Stores the Dirichlet values for the conservation equations in
+     *               \f$ [ \textnormal{unit of primary variable} ] \f$
+     * \param globalPos The global position
+     */
+    PrimaryVariables dirichletAtPos(const GlobalPosition &globalPos) const
+    {
+        PrimaryVariables values(1.0e5);
+//        if (onLeftBoundary_(globalPos))
+//                values[pressureIdx] = 1.0e5;
+
+        Scalar pRef = 1.0e5;
+
+        using WettingPhase = typename GET_PROP(TypeTag, FluidSystem)::WettingPhase;
+        values[pressureIdx] = (pRef - (this->fvGridGeometry().bBoxMax()- globalPos)
+                                       * this->gravity() * WettingPhase::density(temperature(), pRef));
+
+        return values;
+    }
+
+    /*!
+     * \brief Evaluate the boundary conditions for a neumann
+     *        boundary segment.
+     *
+     * \param values Stores the Neumann values for the conservation equations in
+     *               \f$ [ \textnormal{unit of conserved quantity} / (m^(dim-1) \cdot s )] \f$
+     * \param globalPos The position of the integration point of the boundary segment.
+     *
+     * For this method, the \a values parameter stores the mass flux
+     * in normal direction of each phase. Negative values mean influx.
+     */
+    NumEqVector neumannAtPos(const GlobalPosition &globalPos) const
+    {
+        using NonwettingPhase = typename GET_PROP(TypeTag, FluidSystem)::NonwettingPhase;
+        NumEqVector values(0.0);
+        if(isInlet(globalPos))
+            values[pressureEqIdx] = -inFlux_/NonwettingPhase::density(temperature(), 1.0e5);
+
+        return values;
+    }
+
+    /*!
+     * \brief Evaluates the initial values for a control volume
+     *
+     * \param values Stores the initial values for the conservation equations in
+     *               \f$ [ \textnormal{unit of primary variables} ] \f$
+     * \param globalPos The global position
+     */
+    PrimaryVariables initialAtPos(const GlobalPosition &globalPos) const
+    {
+        PrimaryVariables values;
+        values[pressureIdx] = 1e5;
+
+        return values;
+    }
+
+    /*!
+     * \brief Returns the temperature \f$\mathrm{[K]}\f$ for an isothermal problem.
+     *
+     * This is not specific to the discretization. By default it just
+     * throws an exception so it must be overloaded by the problem if
+     * no energy equation is used.
+     */
+    Scalar temperature() const
+    {
+        return 293.15; // 10°C
+    }
+
+private:
+    bool onLeftBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[0] < this->fvGridGeometry().bBoxMin()[0] + eps_;
+    }
+
+    bool onRightBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[0] > this->fvGridGeometry().bBoxMax()[0] - eps_;
+    }
+
+    bool onLowerBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[1] < this->fvGridGeometry().bBoxMin()[1] + eps_;
+    }
+
+    bool onUpperBoundary_(const GlobalPosition &globalPos) const
+    {
+        return globalPos[1] > this->fvGridGeometry().bBoxMax()[1] - eps_;
+    }
+
+    bool isInlet(const GlobalPosition& globalPos) const
+    {
+        if (!onUpperBoundary_(globalPos))
+            return false;
+
+        for (int i = 0; i < dimWorld; i++)
+        {
+            if (globalPos[i] < inletLeftCoord_[i] - eps_)
+                return false;
+            if (globalPos[i] > inletRightCoord_[i] + eps_)
+                return false;
+        }
+        return true;
+    }
+
+    static constexpr Scalar eps_ = 1e-6;
+    Scalar inFlux_;
+    GlobalPosition inletLeftCoord_;
+    GlobalPosition inletRightCoord_;
+};
+
+} // end namespace Dumux
+
+#endif
---
+++ test/porousmediumflow/2p/sequential/buckleyleverett/pressurespatialparams.hh
@@ -0,0 +1,233 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \ingroup TwoPTests
+ * \brief The spatial params for the incompressible 2p test
+ */
+#ifndef DUMUX_BUCKLEYLEVERETT_PRESSURE_TEST_SPATIAL_PARAMS_HH
+#define DUMUX_BUCKLEYLEVERETT_PRESSURE_TEST_SPATIAL_PARAMS_HH
+
+#include <dumux/material/spatialparams/fv.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/regularizedbrookscorey.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/efftoabslaw.hh>
+
+#include <dumux/porousmediumflow/2p/boxmaterialinterfaceparams.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup TwoPTests
+ * \brief The spatial params for the incompressible 2p test
+ */
+template<class FVGridGeometry, class Scalar>
+class TwoPTestSpatialParams
+: public FVSpatialParams<FVGridGeometry, Scalar, TwoPTestSpatialParams<FVGridGeometry, Scalar>>
+{
+    using GridView = typename FVGridGeometry::GridView;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using FVElementGeometry = typename FVGridGeometry::LocalView;
+    using SubControlVolume = typename FVElementGeometry::SubControlVolume;
+    using SubControlVolumeFace = typename FVElementGeometry::SubControlVolumeFace;
+    using ThisType = TwoPTestSpatialParams<FVGridGeometry, Scalar>;
+    using ParentType = FVSpatialParams<FVGridGeometry, Scalar, ThisType>;
+
+    static constexpr int dimWorld = GridView::dimensionworld;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+
+    using EffectiveLaw = RegularizedBrooksCorey<Scalar>;
+
+public:
+    using MaterialLaw = EffToAbsLaw<EffectiveLaw>;
+    using MaterialLawParams = typename MaterialLaw::Params;
+    using PermeabilityType = Scalar;
+
+    TwoPTestSpatialParams(std::shared_ptr<const FVGridGeometry> fvGridGeometry)
+    : ParentType(fvGridGeometry)
+    {
+        materialLawParamsBackground_.setSwr(0.);
+        materialLawParamsBackground_.setSnr(0.);
+
+        materialLawParamsLenses_.setSwr(0.);
+        materialLawParamsLenses_.setSnr(0.);
+
+        //parameters for Brooks-Corey law
+
+        // entry pressures
+        materialLawParamsBackground_.setPe(getParam<Scalar>("SpatialParams.BackgroundEntryPressure", 0.0));
+        materialLawParamsLenses_.setPe(getParam<Scalar>("SpatialParams.LenseEntryPressure", 0.0));
+
+        materialLawParamsBackground_.setLambda(getParam<Scalar>("SpatialParams.BackgroundLambda", 3.0));
+        materialLawParamsLenses_.setLambda(getParam<Scalar>("SpatialParams.LenseLambda", 2.0));
+
+        permBackground_ = getParam<Scalar>("SpatialParams.BackgroundPermeability", 1e-10);
+        permLenses_ = getParam<Scalar>("SpatialParams.LensPermeability", 1e-10);
+
+        lensOneLowerLeft_ = getParam<GlobalPosition>("SpatialParams.LensOneLowerLeft", GlobalPosition(0.0));
+        lensOneUpperRight_ = getParam<GlobalPosition>("SpatialParams.LensOneUpperRight", GlobalPosition(0.0));
+        lensTwoLowerLeft_ = getParam<GlobalPosition>("SpatialParams.LensTwoLowerLeft", GlobalPosition(0.0));
+        lensTwoUpperRight_ = getParam<GlobalPosition>("SpatialParams.LensTwoUpperRight", GlobalPosition(0.0));
+        lensThreeLowerLeft_ = getParam<GlobalPosition>("SpatialParams.LensThreeLowerLeft", GlobalPosition(0.0));
+        lensThreeUpperRight_ = getParam<GlobalPosition>("SpatialParams.LensThreeUpperRight", GlobalPosition(0.0));
+
+        wettingSaturation_.resize(fvGridGeometry->numDofs(),0.0);
+    }
+
+    /*!
+     * \brief Function for defining the (intrinsic) permeability \f$[m^2]\f$.
+     *        In this test, we use element-wise distributed permeabilities.
+     *
+     * \param element The current element
+     * \param scv The sub-control volume inside the element.
+     * \param elemSol The solution at the dofs connected to the element.
+     * \return permeability
+     */
+    template<class ElementSolution>
+    PermeabilityType permeability(const Element& element,
+                                  const SubControlVolume& scv,
+                                  const ElementSolution& elemSol) const
+    {
+
+        // do not use a less permeable lens in the test with inverted wettability
+        auto globalPos = element.geometry().center();
+        if (isLensOne(globalPos))
+            return permLenses_;
+        else if (isLensTwo(globalPos))
+            return permLenses_;
+        else if (isLensThree(globalPos))
+            return permLenses_;
+        else
+            return permBackground_;
+    }
+
+    /*!
+     * \brief Returns the porosity \f$[-]\f$
+     *
+     * \param globalPos The global position
+     */
+    Scalar porosityAtPos(const GlobalPosition& globalPos) const
+    { return 0.4; }
+
+    /*!
+     * \brief Returns the parameter object for the Brooks-Corey material law.
+     *        In this test, we use element-wise distributed material parameters.
+     *
+     * \param element The current element
+     * \param scv The sub-control volume inside the element.
+     * \param elemSol The solution at the dofs connected to the element.
+     * \return the material parameters object
+     */
+    template<class ElementSolution>
+    const MaterialLawParams& materialLawParams(const Element& element,
+                                               const SubControlVolume& scv,
+                                               const ElementSolution& elemSol) const
+    {
+        auto globalPos = element.geometry().center();
+        if (isLensOne(globalPos))
+            return materialLawParamsLenses_;
+        else if (isLensTwo(globalPos))
+            return materialLawParamsLenses_;
+        else if (isLensThree(globalPos))
+            return materialLawParamsLenses_;
+        else
+            return materialLawParamsBackground_;
+    }
+
+    /*!
+     * \brief Function for defining which phase is to be considered as the wetting phase.
+     *
+     * \return the wetting phase index
+     * \param globalPos The global position
+     */
+    template<class FluidSystem>
+    int wettingPhaseAtPos(const GlobalPosition& globalPos) const
+    {
+        return FluidSystem::phase0Idx;
+    }
+
+    Scalar wettingSaturation(const Element& element,
+                           const SubControlVolume& scv) const
+    {
+        return wettingSaturation_[scv.dofIndex()];
+    }
+
+    template<class Scvf>
+    Scalar wettingSaturation(const Element& element,
+                             const SubControlVolume& scv,
+                             const Scvf& scvf) const
+    {
+        return wettingSaturation_[scvf.outsideScvIdx()];
+    }
+
+    void setWettingSaturation(const std::vector<Scalar>& sat)
+    { wettingSaturation_ = sat;}
+
+private:
+    bool isLensOne(const GlobalPosition& globalPos) const
+    {
+        for (int i = 0; i < dimWorld; i++)
+        {
+            if (globalPos[i] < lensOneLowerLeft_[i] - eps_ || globalPos[i] > lensOneUpperRight_[i] + eps_)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isLensTwo(const GlobalPosition& globalPos) const
+    {
+        for (int i = 0; i < dimWorld; i++)
+        {
+            if (globalPos[i] < lensTwoLowerLeft_[i] - eps_ || globalPos[i] > lensTwoUpperRight_[i] + eps_)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isLensThree(const GlobalPosition& globalPos) const
+    {
+        for (int i = 0; i < dimWorld; i++)
+        {
+            if (globalPos[i] < lensThreeLowerLeft_[i] - eps_ || globalPos[i] > lensThreeUpperRight_[i] + eps_)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    MaterialLawParams materialLawParamsBackground_;
+    MaterialLawParams materialLawParamsLenses_;
+    PermeabilityType permBackground_;
+    PermeabilityType permLenses_;
+    GlobalPosition lensOneLowerLeft_;
+    GlobalPosition lensOneUpperRight_;
+    GlobalPosition lensTwoLowerLeft_;
+    GlobalPosition lensTwoUpperRight_;
+    GlobalPosition lensThreeLowerLeft_;
+    GlobalPosition lensThreeUpperRight_;
+
+    std::vector<Scalar> wettingSaturation_;
+
+    static constexpr Scalar eps_ = 1.5e-7;
+};
+
+} // end namespace Dumux
+
+#endif
