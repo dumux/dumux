e4efe57ab34e3735e477926256169899ddb4a1c5
Katharina Heck <katharina.heck@iws.uni-stuttgart.de>
2018-11-05 15:21:46 +0100

    [1pnc][test] add new thermal nonequilibrium test for 1pnc

---
+++ test/porousmediumflow/1pnc/implicit/nonequilibrium/spatialparams.hh
@@ -0,0 +1,104 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup OnePNCTests
+ * \brief Definition of the spatial parameters for the 1pnc
+ *        problems.
+ */
+#ifndef DUMUX_1PNC_TEST_SPATIAL_PARAMS_HH
+#define DUMUX_1PNC_TEST_SPATIAL_PARAMS_HH
+
+#include <dumux/porousmediumflow/properties.hh>
+#include <dumux/material/spatialparams/fv1p.hh>
+#include <dumux/material/spatialparams/fvnonequilibrium.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup OnePNCTests
+ * \brief Definition of the spatial parameters for the 1pnc
+ *        test problems.
+ */
+template<class FVGridGeometry, class Scalar>
+class OnePNCNonequilibriumTestSpatialParams
+: public FVNonEquilibriumSpatialParams<FVGridGeometry, Scalar,
+                                       OnePNCNonequilibriumTestSpatialParams<FVGridGeometry, Scalar>>
+{
+    using GridView = typename FVGridGeometry::GridView;
+    using FVElementGeometry = typename FVGridGeometry::LocalView;
+    using SubControlVolume = typename FVElementGeometry::SubControlVolume;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using ParentType = FVNonEquilibriumSpatialParams<FVGridGeometry, Scalar,
+                                           OnePNCNonequilibriumTestSpatialParams<FVGridGeometry, Scalar>>;
+
+    static const int dimWorld = GridView::dimensionworld;
+    using GlobalPosition = typename Dune::FieldVector<Scalar, dimWorld>;
+
+public:
+    // export permeability type
+    using PermeabilityType = Scalar;
+
+    OnePNCNonequilibriumTestSpatialParams(std::shared_ptr<const FVGridGeometry> fvGridGeometry)
+    : ParentType(fvGridGeometry)
+    {
+        permeability_ = 1e-11;
+        porosity_ = 0.4;
+
+        characteristicLength_ = 5e-4;
+        factorEnergyTransfer_ = 0.5;
+    }
+
+    /*!
+     * \brief Define the intrinsic permeability \f$\mathrm{[m^2]}\f$.
+     *
+     * \param globalPos The global position
+     */
+    PermeabilityType permeabilityAtPos(const GlobalPosition& globalPos) const
+    { return permeability_; }
+
+    /*!
+     * \brief Define the porosity \f$\mathrm{[-]}\f$.
+     *
+     * \param globalPos The global position
+     */
+    Scalar porosityAtPos(const GlobalPosition& globalPos) const
+    { return porosity_; }
+
+    /*!\brief Return the characteristic length for the mass transfer.
+     * \param globalPos The position in global coordinates.*/
+    const Scalar characteristicLengthAtPos(const  GlobalPosition & globalPos) const
+    { return characteristicLength_ ; }
+
+    /*!\brief Return the pre factor the the energy transfer
+     * \param globalPos The position in global coordinates.*/
+    const Scalar factorEnergyTransferAtPos(const  GlobalPosition & globalPos) const
+    { return factorEnergyTransfer_; }
+
+private:
+    Scalar permeability_;
+    Scalar porosity_;
+
+    Scalar factorEnergyTransfer_ ;
+    Scalar characteristicLength_ ;
+};
+
+} // end namespace Dumux
+
+#endif
