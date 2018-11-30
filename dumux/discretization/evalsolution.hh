12adf86f4cb36fdde2bd213f7fd1473bb5695b5e
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2018-11-30 16:00:24 +0100

    [evalsolution] Simplify implementation

--- dumux/discretization/evalsolution.hh
+++ dumux/discretization/evalsolution.hh
@@ -24,12 +24,58 @@
 #ifndef DUMUX_DISCRETIZATION_EVAL_SOLUTION_HH
 #define DUMUX_DISCRETIZATION_EVAL_SOLUTION_HH
 
+#include <iterator>
+#include <algorithm>
+#include <type_traits>
+
+#include <dune/common/hybridutilities.hh>
+#include <dune/localfunctions/lagrange/pqkfactory.hh>
+
+#include <dumux/common/typetraits/state.hh>
 #include <dumux/discretization/box/elementsolution.hh>
 #include <dumux/discretization/cellcentered/elementsolution.hh>
 
-#include "evalsolution_impl.hh"
-
 namespace Dumux {
+
+// some implementation details
+namespace Detail {
+
+//! returns true if all states in an element solution are the same
+template<class ElementSolution>
+bool allStatesEqual(const ElementSolution& elemSol, std::true_type hasState)
+{
+    // determine if all states are the same at all vertices
+    auto firstState = elemSol[0].state();
+    for (std::size_t i = 1; i < elemSol.size(); ++i)
+        if (elemSol[i].state() != firstState)
+            return false;
+
+    return true;
+}
+
+//! overload if the solution is stateless
+template<class ElementSolution>
+bool allStatesEqual(const ElementSolution& elemSol, std::false_type hasState)
+{
+    return true;
+}
+
+//! return the solution at the closest dof
+template<class Geometry, class ElementSolution>
+auto minDistVertexSol(const Geometry& geometry, const typename Geometry::GlobalCoordinate& globalPos,
+                      const ElementSolution& elemSol)
+{
+    // calculate the distances from the evaluation point to the vertices
+    std::vector<typename Geometry::ctype> distances(geometry.corners());
+    for (int i = 0; i < geometry.corners(); ++i)
+        distances[i] = (geometry.corner(i) - globalPos).two_norm2();
+
+    // determine the minimum distance and corresponding vertex
+    auto minDistanceIt = std::min_element(distances.begin(), distances.end());
+    return elemSol[std::distance(distances.begin(), minDistanceIt)];
+}
+
+} // end namespace Detail
 
 /*!
  * \brief Interpolates a given box element solution at a given global position.
@@ -52,7 +98,51 @@ PrimaryVariables evalSolution(const Element& element,
                               const typename Element::Geometry::GlobalCoordinate& globalPos,
                               bool ignoreState = false)
 {
-    return Detail::evalSolutionImpl(element, geometry, fvGridGeometry, elemSol, globalPos, ignoreState);
+    // determine if all states are the same at all vertices
+    using HasState = decltype(isValid(Detail::hasState())(elemSol[0]));
+    bool allStatesEqual = ignoreState || Detail::allStatesEqual(elemSol, HasState{});
+
+    if (allStatesEqual)
+    {
+        using Scalar = typename PrimaryVariables::value_type;
+
+        // interpolate the solution
+        const auto& localBasis = fvGridGeometry.feCache().get(geometry.type()).localBasis();
+
+        // evaluate the shape functions at the scv center
+        const auto localPos = geometry.local(globalPos);
+        std::vector< Dune::FieldVector<Scalar, 1> > shapeValues;
+        localBasis.evaluateFunction(localPos, shapeValues);
+
+        PrimaryVariables result(0.0);
+        for (int i = 0; i < geometry.corners(); ++i)
+        {
+            auto value = elemSol[i];
+            value *= shapeValues[i][0];
+            result += value;
+        }
+
+        // set an arbitrary state if the model requires a state (models constexpr if)
+        Dune::Hybrid::ifElse(HasState{}, [&](auto&& evalLazy){
+            if (!ignoreState)
+                evalLazy(result).setState(evalLazy(elemSol)[0].state());
+        });
+
+        return result;
+    }
+    else
+    {
+        static bool warnedAboutUsingMinDist = false;
+        if (!warnedAboutUsingMinDist)
+        {
+            std::cout << "Warning: Using nearest-neighbor interpolation in evalSolution"
+            << "\nbecause not all states are equal and ignoreState is false!"
+            << std::endl;
+            warnedAboutUsingMinDist = true;
+        }
+
+        return Detail::minDistVertexSol(geometry, globalPos, elemSol);
+    }
 }
 
 /*!
@@ -76,7 +166,58 @@ PrimaryVariables evalSolution(const Element& element,
                               const typename Element::Geometry::GlobalCoordinate& globalPos,
                               bool ignoreState = false)
 {
-    return Detail::evalSolutionImpl(element, geometry, elemSol, globalPos, ignoreState);
+    // determine if all states are the same at all vertices
+    using HasState = decltype(isValid(Detail::hasState())(elemSol[0]));
+    bool allStatesEqual = ignoreState || Detail::allStatesEqual(elemSol, HasState{});
+
+    if (allStatesEqual)
+    {
+        using Scalar = typename PrimaryVariables::value_type;
+        using CoordScalar = typename Element::Geometry::GlobalCoordinate::value_type;
+        static constexpr int dim = Element::Geometry::mydimension;
+
+        //! The box scheme always uses linear Ansatz functions
+        using FeCache = Dune::PQkLocalFiniteElementCache<CoordScalar, Scalar, dim, 1>;
+        using ShapeValue = typename FeCache::FiniteElementType::Traits::LocalBasisType::Traits::RangeType;
+
+        // obtain local finite element basis
+        FeCache feCache;
+        const auto& localBasis = feCache.get(geometry.type()).localBasis();
+
+        // evaluate the shape functions at the scv center
+        const auto localPos = geometry.local(globalPos);
+        std::vector< ShapeValue > shapeValues;
+        localBasis.evaluateFunction(localPos, shapeValues);
+
+        PrimaryVariables result(0.0);
+        for (int i = 0; i < geometry.corners(); ++i)
+        {
+            auto value = elemSol[i];
+            value *= shapeValues[i][0];
+            result += value;
+        }
+
+        // set an arbitrary state if the model requires a state (models constexpr if)
+        Dune::Hybrid::ifElse(HasState{}, [&](auto&& evalLazy){
+            if (!ignoreState)
+                evalLazy(result).setState(evalLazy(elemSol)[0].state());
+        });
+
+        return result;
+    }
+    else
+    {
+        static bool warnedAboutUsingMinDist = false;
+        if (!warnedAboutUsingMinDist)
+        {
+            std::cout << "Warning: Using nearest-neighbor interpolation in evalSolution"
+            << "\nbecause not all states are equal and ignoreState is false!"
+            << std::endl;
+            warnedAboutUsingMinDist = true;
+        }
+
+        return Detail::minDistVertexSol(geometry, globalPos, elemSol);
+    }
 }
 
 /*!
