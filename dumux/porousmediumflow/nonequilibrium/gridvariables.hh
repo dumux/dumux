dumux/dumux

commit ee45c7339aa2ea097362adb4846a51f7e4acda62

[io][veloutput] avoid binding flux cache for each phase

dumux/porousmediumflow/nonequilibrium/gridvariables.hh
@@ -98,13 +98,15 @@ public:
 
             auto fvGeometry = localView(*this->fvGridGeometry_);
             auto elemVolVars = localView(this->curGridVolVars());
+            auto elemFluxVarsCache = localView(this->gridFluxVarsCache());
 
             fvGeometry.bind(element);
             elemVolVars.bind(element, fvGeometry, curSol);
+            elemFluxVarsCache.bind(element, fvGeometry, curSol);
 
             for (int phaseIdx = 0; phaseIdx < numPhases; ++phaseIdx)
             {
-                velocityOutput.calculateVelocity(velocity[phaseIdx], elemVolVars, fvGeometry, element, phaseIdx);
+                velocityOutput.calculateVelocity(velocity[phaseIdx], element, fvGeometry, elemVolVars, elemFluxVarsCache, phaseIdx);
 
                 for (auto&& scv : scvs(fvGeometry))
                 {

dumux/discretization/staggered/freeflow/velocityoutput.hh
@@ -24,6 +24,7 @@
 #ifndef DUMUX_STAGGERED_FF_VELOCITYOUTPUT_HH
 #define DUMUX_STAGGERED_FF_VELOCITYOUTPUT_HH
 
+#include <dune/common/deprecated.hh>
 #include <dumux/io/velocityoutput.hh>
 #include <dumux/common/parameters.hh>
 
@@ -41,8 +42,9 @@ class StaggeredFreeFlowVelocityOutput : public VelocityOutput<GridVariables>
     using Scalar = typename GridVariables::Scalar;
     using FVElementGeometry = typename FVGridGeometry::LocalView;
     using SubControlVolumeFace = typename FVGridGeometry::SubControlVolumeFace;
-    using ElementVolumeVariables = typename GridVariables::GridVolumeVariables::LocalView;
     using GridVolumeVariables = typename GridVariables::GridVolumeVariables;
+    using ElementVolumeVariables = typename GridVolumeVariables::LocalView;
+    using ElementFluxVarsCache = typename GridVariables::GridFluxVariablesCache::LocalView;
     using VolumeVariables = typename GridVariables::VolumeVariables;
     using FluidSystem = typename VolumeVariables::FluidSystem;
     using GridView = typename FVGridGeometry::GridView;
@@ -80,10 +82,25 @@ public:
 
     //! Calculate the velocities for the scvs in the element
     //! We assume the local containers to be bound to the complete stencil
+    DUNE_DEPRECATED_MSG("Use the new interface signature with elemFluxVarsCache")
     void calculateVelocity(VelocityVector& velocity,
                            const ElementVolumeVariables& elemVolVars,
                            const FVElementGeometry& fvGeometry,
                            const Element& element,
+                           int phaseIdx) const override
+    {
+        auto elemFluxVarsCache = localView(gridVariables_.gridFluxVarsCache());
+        elemFluxVarsCache.bind(element, fvGeometry, elemVolVars);
+        calculateVelocity(velocity, element, fvGeometry, elemVolVars, elemFluxVarsCache, phaseIdx);
+    }
+
+    //! Calculate the velocities for the scvs in the element
+    //! We assume the local containers to be bound to the complete stencil
+    void calculateVelocity(VelocityVector& velocity,
+                           const Element& element,
+                           const FVElementGeometry& fvGeometry,
+                           const ElementVolumeVariables& elemVolVars,
+                           const ElementFluxVarsCache& elemFluxVarsCache,
                            int phaseIdx) const override
     {
         auto elemFaceVars = localView(gridVariables_.curGridFaceVars());

dumux/io/velocityoutput.hh
@@ -28,6 +28,7 @@
 
 #include <dune/common/fvector.hh>
 #include <dune/common/exceptions.hh>
+#include <dune/common/deprecated.hh>
 #include <dumux/common/parameters.hh>
 
 namespace Dumux {
@@ -42,6 +43,7 @@ class VelocityOutput
     using Scalar = typename GridVariables::Scalar;
     static constexpr int dimWorld = GridVariables::GridGeometry::GridView::dimensionworld;
     using ElementVolumeVariables = typename GridVariables::GridVolumeVariables::LocalView;
+    using ElementFluxVarsCache = typename GridVariables::GridFluxVariablesCache::LocalView;
     using FVElementGeometry = typename GridVariables::GridGeometry::LocalView;
     using Element = typename GridVariables::GridGeometry::GridView::template Codim<0>::Entity;
 
@@ -67,6 +69,17 @@ public:
 
     //! Calculate the velocities for the scvs in the element
     //! We assume the local containers to be bound to the complete stencil
+    virtual void calculateVelocity(VelocityVector& velocity,
+                                   const Element& element,
+                                   const FVElementGeometry& fvGeometry,
+                                   const ElementVolumeVariables& elemVolVars,
+                                   const ElementFluxVarsCache& elemFluxVarsCache,
+                                   int phaseIdx) const
+    {}
+
+    //! Calculate the velocities for the scvs in the element
+    //! We assume the local containers to be bound to the complete stencil
+    DUNE_DEPRECATED_MSG("Use the new interface signature with elemFluxVarsCache")
     virtual void calculateVelocity(VelocityVector& velocity,
                                    const ElementVolumeVariables& elemVolVars,
                                    const FVElementGeometry& fvGeometry,

dumux/io/vtkoutputmodule.hh
@@ -340,8 +340,13 @@ private:
 
                 // velocity output
                 if (velocityOutput_->enableOutput())
+                {
+                    auto elemFluxVarsCache = localView(gridVariables_.gridFluxVarsCache());
+                    elemFluxVarsCache.bind(element, fvGeometry, elemVolVars);
+
                     for (int phaseIdx = 0; phaseIdx < velocityOutput_->numFluidPhases(); ++phaseIdx)
-                        velocityOutput_->calculateVelocity(velocity[phaseIdx], elemVolVars, fvGeometry, element, phaseIdx);
+                        velocityOutput_->calculateVelocity(velocity[phaseIdx], element, fvGeometry, elemVolVars, elemFluxVarsCache, phaseIdx);
+                }
 
                 //! the rank
                 if (addProcessRank)
@@ -525,8 +530,13 @@ private:
 
                 // velocity output
                 if (velocityOutput_->enableOutput())
+                {
+                    auto elemFluxVarsCache = localView(gridVariables_.gridFluxVarsCache());
+                    elemFluxVarsCache.bind(element, fvGeometry, elemVolVars);
+
                     for (int phaseIdx = 0; phaseIdx < velocityOutput_->numFluidPhases(); ++phaseIdx)
-                        velocityOutput_->calculateVelocity(velocity[phaseIdx], elemVolVars, fvGeometry, element, phaseIdx);
+                        velocityOutput_->calculateVelocity(velocity[phaseIdx], element, fvGeometry, elemVolVars, elemFluxVarsCache, phaseIdx);
+                }
 
                 //! the rank
                 if (addProcessRank)

dumux/porousmediumflow/boxdfm/vtkoutputmodule.hh
@@ -231,8 +231,13 @@ private:
 
                 // velocity output
                 if (this->velocityOutput().enableOutput())
+                {
+                    auto elemFluxVarsCache = localView(this->gridVariables().gridFluxVarsCache());
+                    elemFluxVarsCache.bind(element, fvGeometry, elemVolVars);
+
                     for (int phaseIdx = 0; phaseIdx < this->velocityOutput().numFluidPhases(); ++phaseIdx)
-                        this->velocityOutput().calculateVelocity(velocity[phaseIdx], elemVolVars, fvGeometry, element, phaseIdx);
+                        this->velocityOutput().calculateVelocity(velocity[phaseIdx], element, fvGeometry, elemVolVars, elemFluxVarsCache, phaseIdx);
+                }
 
                 //! the rank
                 if (addProcessRank)
@@ -410,8 +415,13 @@ private:
 
                 // velocity output
                 if (this->velocityOutput().enableOutput())
+                {
+                    auto elemFluxVarsCache = localView(this->gridVariables().gridFluxVarsCache());
+                    elemFluxVarsCache.bind(element, fvGeometry, elemVolVars);
+
                     for (int phaseIdx = 0; phaseIdx < this->velocityOutput().numFluidPhases(); ++phaseIdx)
-                        this->velocityOutput().calculateVelocity(velocity[phaseIdx], elemVolVars, fvGeometry, element, phaseIdx);
+                        this->velocityOutput().calculateVelocity(velocity[phaseIdx], element, fvGeometry, elemVolVars, elemFluxVarsCache, phaseIdx);
+                }
 
                 //! the rank
                 if (addProcessRank)

dumux/porousmediumflow/velocityoutput.hh
@@ -25,6 +25,7 @@
 #ifndef DUMUX_POROUSMEDIUMFLOW_VELOCITYOUTPUT_HH
 #define DUMUX_POROUSMEDIUMFLOW_VELOCITYOUTPUT_HH
 
+#include <dune/common/deprecated.hh>
 #include <dune/common/float_cmp.hh>
 #include <dune/geometry/referenceelements.hh>
 
@@ -50,6 +51,7 @@ class PorousMediumFlowVelocityOutput : public VelocityOutput<GridVariables>
     using GridView = typename FVGridGeometry::GridView;
     using Element = typename GridView::template Codim<0>::Entity;
     using GridVolumeVariables = typename GridVariables::GridVolumeVariables;
+    using ElementFluxVarsCache = typename GridVariables::GridFluxVariablesCache::LocalView;
     using VolumeVariables = typename GridVariables::VolumeVariables;
     using ElementVolumeVariables = typename GridVolumeVariables::LocalView;
     using FluidSystem = typename VolumeVariables::FluidSystem;
@@ -107,12 +109,27 @@ public:
     //! Returns the number of phases.
     int numFluidPhases() const override { return VolumeVariables::numFluidPhases(); }
 
-    //! Calculates the velocities for the scvs in the element.
-    //! We assume the local containers to be bound to the complete stencil.
+    //! Calculate the velocities for the scvs in the element
+    //! We assume the local containers to be bound to the complete stencil
+    DUNE_DEPRECATED_MSG("Use the new interface signature with elemFluxVarsCache")
     void calculateVelocity(VelocityVector& velocity,
                            const ElementVolumeVariables& elemVolVars,
                            const FVElementGeometry& fvGeometry,
                            const Element& element,
+                           int phaseIdx) const override
+    {
+        auto elemFluxVarsCache = localView(gridVariables_.gridFluxVarsCache());
+        elemFluxVarsCache.bind(element, fvGeometry, elemVolVars);
+        calculateVelocity(velocity, element, fvGeometry, elemVolVars, elemFluxVarsCache, phaseIdx);
+    }
+
+    //! Calculates the velocities for the scvs in the element.
+    //! We assume the local containers to be bound to the complete stencil.
+    void calculateVelocity(VelocityVector& velocity,
+                           const Element& element,
+                           const FVElementGeometry& fvGeometry,
+                           const ElementVolumeVariables& elemVolVars,
+                           const ElementFluxVarsCache& elemFluxVarsCache,
                            int phaseIdx) const override
     {
         using Velocity = typename VelocityVector::value_type;
@@ -121,10 +138,6 @@ public:
 
         const auto geometry = element.geometry();
         const Dune::GeometryType geomType = geometry.type();
-
-        // bind the element flux variables cache
-        auto elemFluxVarsCache = localView(gridVariables_.gridFluxVarsCache());
-        elemFluxVarsCache.bind(element, fvGeometry, elemVolVars);
 
         // the upwind term to be used for the volume flux evaluation
         auto upwindTerm = [phaseIdx](const auto& volVars) { return volVars.mobility(phaseIdx); };

commit 997de4d5618050a404f7a341e093726016a963b4

[bugfix][nonequil] Use correct signature (fix bug introduced in ee45c733)

dumux/porousmediumflow/nonequilibrium/gridvariables.hh
@@ -102,7 +102,7 @@ public:
 
             fvGeometry.bind(element);
             elemVolVars.bind(element, fvGeometry, curSol);
-            elemFluxVarsCache.bind(element, fvGeometry, curSol);
+            elemFluxVarsCache.bind(element, fvGeometry, elemVolVars);
 
             for (int phaseIdx = 0; phaseIdx < numPhases; ++phaseIdx)
             {