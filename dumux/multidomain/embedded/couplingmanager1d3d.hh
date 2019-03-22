da2776d778106f23eca9849ff737d1c27ae98b65
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2019-03-22 13:34:36 +0100

    [1d3d] Add restriction for box method

--- dumux/multidomain/embedded/couplingmanager1d3d.hh
+++ dumux/multidomain/embedded/couplingmanager1d3d.hh
@@ -1058,6 +1058,8 @@ class EmbeddedCouplingManager1d3d<MDTraits, EmbeddedCouplingMode::kernel>
     static constexpr bool isBox()
     { return GridGeometry<id>::discMethod == DiscretizationMethod::box; }
 
+    static_assert(!isBox<bulkIdx>() && !isBox<lowDimIdx>(), "The kernel coupling method is only implemented for the tpfa method");
+
     enum {
         bulkDim = GridView<bulkIdx>::dimension,
         lowDimDim = GridView<lowDimIdx>::dimension,
