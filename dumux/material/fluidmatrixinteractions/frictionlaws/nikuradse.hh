3a6fefe8661991861f3ae8d5e6b5885de11f9506
Martin Utz <martin.utz@baw.de>
2019-05-02 10:02:51 +0200

    [swe][frictionlaws] Complete the comments.

--- dumux/material/fluidmatrixinteractions/frictionlaws/nikuradse.hh
+++ dumux/material/fluidmatrixinteractions/frictionlaws/nikuradse.hh
@@ -37,56 +37,69 @@ namespace Dumux {
  * The LET mobility model is used to limit the friction for small water depths.
  */
 
-template <typename RF, class VolumeVariables>
+template <typename Scalar>
 class FrictionLawNikuradse
 {
 public:
     /*!
      * \brief Compute the friction ustar_h.
      *
-     * \param volVars volume variables.
+     * \param h water depth.
      * \param ks the Strickler friction value.
      */
 
-    RF computeUstarH(const VolumeVariables& volVars, RF ks)
+    Scalar computeUstarH(const Scalar h,const Scalar ks)
     {
         using std::pow;
         using std::log;
 
-        RF ustar_h = 0.0;
-        RF rough_h = ks;
+        Scalar ustar_h = 0.0;
+        Scalar rough_h = ks;
 
-        rough_h = limitRoughH(rough_h, volVars);
-        ustar_h = pow(0.41,2.0)/pow(log((12*(volVars::getH + rough_h))/ks),2.0);
+        rough_h = limitRoughH(rough_h, h);
+        ustar_h = pow(0.41,2.0)/pow(log((12*(h + rough_h))/ks),2.0);
         return ustar_h;
     }
 
     /*!
      * \brief Limit the friction for small water depth.
      *
-     * The friction is represented by the roughness height rough_h.
-     * For the limiting the LET model is used.
+     * We define a water depth minUpperH. If the water depth is
+     * smaller, we start to limit the friciton.
+     * So the friciton term get's not extreme large for small water
+     * depths
+     *
+     * ------------------------- minUpperh -----------
+     *
+     *
+     *
+     * ------------------------rough_h ---------------
+     *    /\  /\   roughness                  /grain\
+     * -------------------------------bottom ------------------
+     * /////////////////////////////////////////////////
+     *
+     * For the limiting the LET model is used, which is usually applied in the
+     * porouse media flow to limit the permeability due to the saturation. It employs
+     * the three empirical paramaters L, E and T, which describe the limiting curve.
      *
      * \param rough_h roughness height of the representive structure (e.g. largest grain size).
-     * \param volVars volume variables.
+     * \params h water depth.
      */
-    RF limitRoughH(RF rough_h, const VolumeVariables volVars)
+    Scalar limitRoughH(Scalar rough_h, const Scalar h)
     {
         using std::pow;
         using std::min;
         using std::max;
 
-        const RF letL = 0.0;
-        const RF letT = 2.0;
-        const RF letE = 1.0;
-        RF mobility = 1.0;
-        RF krw = 1.0;
-        RF sw = 0.0;
+        const Scalar letL = 0.0; // empirical parameter of the LET model
+        const Scalar letT = 2.0; // empirical parameter of the LET model
+        const Scalar letE = 1.0; // empirical parameter of the LET model
+        Scalar mobility_max = 1.0; // maximal mobility
 
         auto minUpperH = rough_h * 2.0;
-        sw = min(volVars::getH * (1.0/minUpperH),1.0);
+        auto sw = min(h * (1.0/minUpperH),1.0);
         sw = max(0.0,sw);
-        mobility = (krw * pow(sw,letL))/(pow(sw,letL) + letE * pow(1.0-sw,letT));
+        auto mobility = (krw * pow(sw,letL))/(pow(sw,letL) + letE * pow(1.0-sw,letT));
         return rough_h * (1.0 - mobility);
     }
 
