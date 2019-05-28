c37ec7c656337afba4a8a48abf6620617b6bd661
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2019-05-28 10:51:29 +0200

    Revert "Merge branch 'fix/water-vapour-viscosity-calculation' into 'master'"
    This reverts merge request !1615

--- dumux/material/fluidsystems/h2oairmesitylene.hh
+++ dumux/material/fluidsystems/h2oairmesitylene.hh
@@ -349,9 +349,9 @@ public:
          */
         Scalar muResult;
         const Scalar mu[numComponents] = {
-            H2O::gasViscosity(fluidState.temperature(phaseIdx), fluidState.pressure(phaseIdx)),
+            H2O::gasViscosity(fluidState.temperature(phaseIdx), H2O::vaporPressure(fluidState.temperature(phaseIdx))),
             Air::gasViscosity(fluidState.temperature(phaseIdx), fluidState.pressure(phaseIdx)),
-            NAPL::gasViscosity(fluidState.temperature(phaseIdx), fluidState.pressure(phaseIdx))
+            NAPL::gasViscosity(fluidState.temperature(phaseIdx), NAPL::vaporPressure(fluidState.temperature(phaseIdx)))
         };
         // molar masses
         const Scalar M[numComponents] = {
