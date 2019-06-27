f07f6a7f24f8309b5b6a1071ad4d00fa2d44c3db
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2019-06-27 15:51:55 +0200

    [spatialparams] Implement a wrapper that support old and new material law interfaces

--- dumux/common/deprecated.hh
+++ dumux/common/deprecated.hh
@@ -26,6 +26,7 @@
 #define DUMUX_COMMON_DEPRECATED_HH
 
 #include <dune/common/deprecated.hh>
+#include <dumux/common/typetraits/isvalid.hh>
 
 namespace Dumux {
 
@@ -45,6 +46,252 @@ namespace Deprecated {
 ///////////////////////////////////////////////////////////////
 // Deprecation warnings for effective diffusion coefficients //
 ///////////////////////////////////////////////////////////////
+
+// support old interface of the effective thermal conductivity laws
+template<class E, class SCV, class Sol>
+struct HasNewFIAIF
+{
+    template<class S>
+    auto operator()(S&& sp)
+    -> decltype(sp.fluidMatrixInteraction(std::declval<const E&>(),
+                                          std::declval<const SCV&>(),
+                                          std::declval<const Sol&>())) {}
+};
+
+template<class Pos>
+struct HasNewFIAIFAtPos
+{
+    template<class S>
+    auto operator()(S&& sp)
+    -> decltype(sp.fluidMatrixInteractionAtPos(std::declval<const Pos&>())) {}
+};
+
+// support new and old twop material law interface
+template<class Scalar, class SpatialParams, class Element, class Scv, class ElemSol>
+class TwoPMaterialLawWrapper
+{
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+
+    template<class S>
+    static constexpr bool hasNew()
+    { return decltype(isValid(HasNewFIAIF<Element, Scv, ElemSol>()).template check<S>())::value; }
+
+    template<class S>
+    static constexpr bool hasNewAtPos()
+    { return decltype(isValid(HasNewFIAIFAtPos<GlobalPosition>()).template check<S>())::value; }
+
+public:
+    TwoPMaterialLawWrapper(const SpatialParams& sp,
+                           const Element& element,
+                           const Scv& scv,
+                           const ElemSol& elemSol)
+    : spatialParams_(sp), element_(element), scv_(scv), elemSol_(elemSol)
+    {
+        // maybe throw deprecation warning
+        checkUsingOldMaterialLaw();
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<!hasNew<S>() && !hasNewAtPos<S>(), int> = 0>
+    [[deprecated("The material laws have been overhauled! Your spatial params implement the old interface. Use the new style material laws. Old material laws will no longer be supported after release 3.2")]]
+    void checkUsingOldMaterialLaw() {}
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNew<S>() || hasNewAtPos<S>(), int> = 0>
+    void checkUsingOldMaterialLaw() {}
+
+    /*!
+     * \brief The capillary pressure-saturation curve
+     */
+    template<class S = SpatialParams, typename std::enable_if_t<!hasNew<S>() && !hasNewAtPos<S>(), int> = 0>
+    Scalar pc(const Scalar sw) const
+    {
+        const auto& params = spatialParams_.materialLawParams(element_, scv_, elemSol_);
+        return S::MaterialLaw::pc(params, sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNew<S>(), int> = 0>
+    Scalar pc(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteraction(element_, scv_, elemSol_).pc(sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNewAtPos<S>() && !hasNew<S>(), int> = 0>
+    Scalar pc(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteractionAtPos(scv_.center()).pc(sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<!hasNew<S>() && !hasNewAtPos<S>(), int> = 0>
+    Scalar dpc_dsw(const Scalar sw) const
+    {
+        const auto& params = spatialParams_.materialLawParams(element_, scv_, elemSol_);
+        return S::MaterialLaw::dpc_dsw(params, sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNew<S>(), int> = 0>
+    Scalar dpc_dsw(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteraction(element_, scv_, elemSol_).dpc_dsw(sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNewAtPos<S>() && !hasNew<S>(), int> = 0>
+    Scalar dpc_dsw(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteractionAtPos(scv_.center()).dpc_dsw(sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<!hasNew<S>() && !hasNewAtPos<S>(), int> = 0>
+    Scalar endPointPc() const
+    {
+        const auto& params = spatialParams_.materialLawParams(element_, scv_, elemSol_);
+        return S::MaterialLaw::endPointPc(params);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNew<S>(), int> = 0>
+    Scalar endPointPc() const
+    {
+        return spatialParams_.fluidMatrixInteraction(element_, scv_, elemSol_).endPointPc();
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNewAtPos<S>() && !hasNew<S>(), int> = 0>
+    Scalar endPointPc() const
+    {
+        return spatialParams_.fluidMatrixInteractionAtPos(scv_.center()).endPointPc();
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<!hasNew<S>() && !hasNewAtPos<S>(), int> = 0>
+    Scalar sw(const Scalar pc) const
+    {
+        const auto& params = spatialParams_.materialLawParams(element_, scv_, elemSol_);
+        return S::MaterialLaw::sw(params, pc);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNew<S>(), int> = 0>
+    Scalar sw(const Scalar pc) const
+    {
+        return spatialParams_.fluidMatrixInteraction(element_, scv_, elemSol_).sw(pc);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNewAtPos<S>() && !hasNew<S>(), int> = 0>
+    Scalar sw(const Scalar pc) const
+    {
+        return spatialParams_.fluidMatrixInteractionAtPos(scv_.center()).sw(pc);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<!hasNew<S>() && !hasNewAtPos<S>(), int> = 0>
+    Scalar dsw_dpc(const Scalar pc) const
+    {
+        const auto& params = spatialParams_.materialLawParams(element_, scv_, elemSol_);
+        return S::MaterialLaw::dsw_dpc(params, pc);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNew<S>(), int> = 0>
+    Scalar dsw_dpc(const Scalar pc) const
+    {
+        return spatialParams_.fluidMatrixInteraction(element_, scv_, elemSol_).dsw_dpc(pc);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNewAtPos<S>() && !hasNew<S>(), int> = 0>
+    Scalar dsw_dpc(const Scalar pc) const
+    {
+        return spatialParams_.fluidMatrixInteractionAtPos(scv_.center()).dsw_dpc(pc);
+    }
+
+
+
+    /*!
+     * \brief The relative permeability for the wetting phase
+     */
+    template<class S = SpatialParams, typename std::enable_if_t<!hasNew<S>() && !hasNewAtPos<S>(), int> = 0>
+    Scalar krw(const Scalar sw) const
+    {
+        const auto& params = spatialParams_.materialLawParams(element_, scv_, elemSol_);
+        return S::MaterialLaw::krw(params, sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNew<S>(), int> = 0>
+    Scalar krw(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteraction(element_, scv_, elemSol_).krw(sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNewAtPos<S>() && !hasNew<S>(), int> = 0>
+    Scalar krw(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteractionAtPos(scv_.center()).krw(sw);
+    }
+
+    /*!
+     * \brief The derivative of the relative permeability for the wetting phase w.r.t. saturation
+     */
+    template<class S = SpatialParams, typename std::enable_if_t<!hasNew<S>() && !hasNewAtPos<S>(), int> = 0>
+    Scalar dkrw_dsw(const Scalar sw) const
+    {
+        const auto& params = spatialParams_.materialLawParams(element_, scv_, elemSol_);
+        return S::MaterialLaw::dkrw_dsw(params, sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNew<S>(), int> = 0>
+    Scalar dkrw_dsw(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteraction(element_, scv_, elemSol_).dkrw_dsw(sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNewAtPos<S>() && !hasNew<S>(), int> = 0>
+    Scalar dkrw_dsw(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteractionAtPos(scv_.center()).dkrw_dsw(sw);
+    }
+
+    /*!
+     * \brief The relative permeability for the non-wetting phase
+     */
+    template<class S = SpatialParams, typename std::enable_if_t<!hasNew<S>() && !hasNewAtPos<S>(), int> = 0>
+    Scalar krn(const Scalar sw) const
+    {
+        const auto& params = spatialParams_.materialLawParams(element_, scv_, elemSol_);
+        return S::MaterialLaw::krn(params, sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNew<S>(), int> = 0>
+    Scalar krn(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteraction(element_, scv_, elemSol_).krn(sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNewAtPos<S>() && !hasNew<S>(), int> = 0>
+    Scalar krn(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteractionAtPos(scv_.center()).krn(sw);
+    }
+
+    /*!
+     * \brief The derivative of the relative permeability for the non-wetting phase w.r.t. saturation
+     */
+    template<class S = SpatialParams, typename std::enable_if_t<!hasNew<S>() && !hasNewAtPos<S>(), int> = 0>
+    Scalar dkrn_dsw(const Scalar sw) const
+    {
+        const auto& params = spatialParams_.materialLawParams(element_, scv_, elemSol_);
+        return S::MaterialLaw::dkrn_dsw(params, sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNew<S>(), int> = 0>
+    Scalar dkrn_dsw(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteraction(element_, scv_, elemSol_).dkrn_dsw(sw);
+    }
+
+    template<class S = SpatialParams, typename std::enable_if_t<hasNewAtPos<S>() && !hasNew<S>(), int> = 0>
+    Scalar dkrn_dsw(const Scalar sw) const
+    {
+        return spatialParams_.fluidMatrixInteractionAtPos(scv_.center()).dkrn_dsw(sw);
+    }
+
+private:
+    const SpatialParams& spatialParams_;
+    const Element& element_;
+    const Scv& scv_;
+    const ElemSol& elemSol_;
+};
 
 } // end namespace Deprecated
 #endif
