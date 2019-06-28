9e8c0ef0073992bf8d0d092610b226897865fe69
Katharina Heck <katharina.heck@iws.uni-stuttgart.de>
2019-06-28 11:39:37 +0200

    [tracer] add effective laws in volvars to and adapt volvar traits

--- dumux/porousmediumflow/tracer/volumevariables.hh
+++ dumux/porousmediumflow/tracer/volumevariables.hh
@@ -57,6 +57,7 @@ class TracerVolumeVariables
     using ParentType = PorousMediumFlowVolumeVariables<Traits>;
     using Scalar = typename Traits::PrimaryVariables::value_type;
     static constexpr bool useMoles = Traits::ModelTraits::useMoles();
+    using EffDiffModel = typename Traits::EffectiveDiffusivityModel;
 
 public:
     //! Export fluid system type
@@ -93,6 +94,7 @@ public:
         {
             moleOrMassFraction_[compIdx] = this->priVars()[compIdx];
             diffCoeff_[compIdx] = FluidSystem::binaryDiffusionCoefficient(compIdx, problem, element, scv);
+            effectiveDiffCoeff_[compIdx] = EffDiffModel::effectiveDiffusivity(*this, diffCoeff_[compIdx] ,0 /*phaseIdx*/);
         }
     }
 
@@ -188,6 +190,14 @@ public:
     Scalar diffusionCoefficient(int phaseIdx, int compIdx) const
     { return diffCoeff_[compIdx]; }
 
+    /*!
+     * \brief Returns the effective diffusion coefficients
+     */
+    Scalar effectiveDiffusivity(int phaseIdx, int compIdx) const
+    {
+        return effectiveDiffCoeff_[compIdx];
+    }
+
     // /*!
     //  * \brief Returns the dispersivity of the fluid's streamlines.
     //  * \todo implement me
@@ -237,6 +247,7 @@ protected:
 
     // DispersivityType dispersivity_;
     std::array<Scalar, ParentType::numFluidComponents()> diffCoeff_;
+    std::array<Scalar, ParentType::numFluidComponents()> effectiveDiffCoeff_;
     std::array<Scalar, ParentType::numFluidComponents()> moleOrMassFraction_;
 };
 
