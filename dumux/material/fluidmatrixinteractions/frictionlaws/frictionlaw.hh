d37885abbe3954bbcaa214aa593ef6f39e0f650b
Martin Utz <martin.utz@baw.de>
2019-06-28 10:15:16 +0200

    [swe][frictionlaw] Small changes.

--- dumux/material/fluidmatrixinteractions/frictionlaws/frictionlaw.hh
+++ dumux/material/fluidmatrixinteractions/frictionlaws/frictionlaw.hh
@@ -46,7 +46,7 @@ public:
      * from contiuums mechanics, but a force projected on an area. Therefore it is a
      * vector with two entries.
      *
-     * \return shear stress. First entry is the x-component, the second the y-component.
+     * \return shear stress [N/m^2]. First entry is the x-component, the second the y-component.
      */
 
     virtual Dune::FieldVector<Scalar, 2> computeShearStress(const VolumeVariables& VolVar) const = 0;
