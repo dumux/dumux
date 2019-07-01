5bce748932cc18b4c89bbb2d1af28db61f827b4e
Dennis.Glaeser <dennis.glaeser@iws.uni-stuttgart.de>
2019-07-01 15:51:02 +0200

    [mpfa][fickslaw] use effective diffusion coefficient directly in solve

--- dumux/discretization/cellcentered/mpfa/tensorlambdafactory.hh
+++ dumux/discretization/cellcentered/mpfa/tensorlambdafactory.hh
@@ -77,7 +77,7 @@ public:
                                     const auto& volVars,
                                     const auto& fvGeometry,
                                     const auto& scv)
-               { return volVars.diffusionCoefficient(phaseIdx, compIdx); };
+               { return volVars.effectiveDiffusionCoefficient(phaseIdx, compIdx, compIdx); };
     }
 
     //! return void lambda here
