cdc541998ce92301ecadd33c2f1df33930876432
Dennis.Glaeser <dennis.glaeser@iws.uni-stuttgart.de>
2019-07-09 19:29:15 +0200

    [facet][box][darcyslaw] use correct scaling for search vector

--- dumux/multidomain/facet/box/darcyslaw.hh
+++ dumux/multidomain/facet/box/darcyslaw.hh
@@ -95,7 +95,7 @@ public:
         // point inside the element by moving half its length into the interior.
         const auto& geometry = element.geometry();
         auto distVec = mv(insideVolVars.permeability(), scvf.unitOuterNormal());
-        distVec *= -1.0;
+        distVec /= -1.0*distVec.two_norm();
         distVec *= diameter(geometry)*5.0; // make sure segment will be long enough
 
         const auto p1 = scvf.ipGlobal();
