bc5c768d9e0137fa22480b97c676ad67754867f4
Dennis.Glaeser <dennis.glaeser@iws.uni-stuttgart.de>
2019-07-09 19:02:10 +0200

    [facet][box] use correct distance vector for tpfa on coupling scvfs

--- dumux/multidomain/facet/box/darcyslaw.hh
+++ dumux/multidomain/facet/box/darcyslaw.hh
@@ -131,8 +131,8 @@ public:
         }
 
         // the transmissibility on the matrix side
-        const auto dm = (scvf.ipGlobal() - supportPoint).two_norm();
-        const auto tm = 1.0/dm*vtmv(scvf.unitOuterNormal(), insideVolVars.permeability(), scvf.unitOuterNormal());
+        const auto d = scvf.ipGlobal() - supportPoint;
+        const auto tm = 1.0/d.two_norm2()*vtmv(d, insideVolVars.permeability(), scvf.unitOuterNormal());
 
         // compute flux depending on the user's choice of boundary types
         const auto bcTypes = problem.interiorBoundaryTypes(element, scvf);
