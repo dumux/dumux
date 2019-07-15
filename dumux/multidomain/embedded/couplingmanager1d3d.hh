778569671649551546a86de1c86e40c008d46847
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2019-07-15 18:51:44 +0200

    Add embedded coupling modes

--- dumux/multidomain/embedded/couplingmanager1d3d.hh
+++ dumux/multidomain/embedded/couplingmanager1d3d.hh
@@ -51,7 +51,7 @@ namespace Dumux {
  * \brief The coupling mode
  */
 enum class EmbeddedCouplingMode
-{ line, average, cylindersources, kernel, projection };
+{ line, average, cylindersources, kernel, kernelAnisotropic, wellIndex, projection };
 
 /*!
  * \ingroup EmbeddedCoupling
@@ -65,6 +65,8 @@ std::string toString(EmbeddedCouplingMode mode)
         case EmbeddedCouplingMode::average: return "average";
         case EmbeddedCouplingMode::cylindersources: return "cylinder";
         case EmbeddedCouplingMode::kernel: return "kernel";
+        case EmbeddedCouplingMode::kernelAnisotropic: return "kernelAnisotropic";
+        case EmbeddedCouplingMode::wellIndex: return "wellIndex";
         case EmbeddedCouplingMode::projection: return "projection";
         default: return "unknown";
     }
