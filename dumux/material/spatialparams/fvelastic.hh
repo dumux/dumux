fd7f6f6d70a88ffc3d279f4efebbda08498cfe80
Dennis.Glaeser <dennis.glaeser@iws.uni-stuttgart.de>
2019-07-24 17:48:13 +0200

    [elastic] implement spatial params base class for fem schemes

--- dumux/material/spatialparams/fvelastic.hh
+++ dumux/material/spatialparams/fvelastic.hh
@@ -24,153 +24,17 @@
 #ifndef DUMUX_GEOMECHANICS_ELASTIC_FV_SPATIAL_PARAMS_HH
 #define DUMUX_GEOMECHANICS_ELASTIC_FV_SPATIAL_PARAMS_HH
 
-#include <memory>
+#warning "This header is deprecated and will be removed after release 3.1. Use spatialparams/elastic.hh"
 
-#include <dune/common/exceptions.hh>
-
-#include <dumux/common/parameters.hh>
-#include <dumux/common/typetraits/isvalid.hh>
+#include "elastic.hh"
 
 namespace Dumux {
 
-#ifndef DOXYGEN
-namespace Detail {
-// helper struct detecting if the user-defined spatial params class has a lameParamsAtPos function
-// for g++ > 5.3, this can be replaced by a lambda
-template<class GlobalPosition>
-struct hasLameParamsAtPos
-{
-    template<class SpatialParams>
-    auto operator()(const SpatialParams& a)
-    -> decltype(a.lameParamsAtPos(std::declval<GlobalPosition>()))
-    {}
-};
+// Deprecated class signature
+template<class Scalar, class GridGeometry, class Implementation>
+using FVSpatialParamsElastic [[deprecated("Use SpatialParamsElastic instead. Will be removed after 3.1!")]]
+    = SpatialParamsElastic<Scalar, GridGeometry, Implementation>;
 
-} // end namespace Detail
-#endif
+} // end namespace Dumux
 
-/*!
- * \ingroup SpatialParameters
- * \brief The base class for spatial parameters of linear elastic geomechanical problems
- */
-template<class Scalar, class FVGridGeometry, class Implementation>
-class FVSpatialParamsElastic
-{
-    using FVElementGeometry = typename FVGridGeometry::LocalView;
-    using SubControlVolume = typename FVGridGeometry::SubControlVolume;
-    using GridView = typename FVGridGeometry::GridView;
-    using Element = typename GridView::template Codim<0>::Entity;
-    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
-
-    enum { dimWorld = GridView::dimensionworld };
-
-public:
-    //! The constructor
-    FVSpatialParamsElastic(std::shared_ptr<const FVGridGeometry> fvGridGeometry)
-    : fvGridGeometry_(fvGridGeometry)
-    , gravity_(0.0)
-    {
-        const bool enableGravity = getParam<bool>("Problem.EnableGravity");
-        if (enableGravity)
-            gravity_[dimWorld-1]  = -9.81;
-    }
-
-    /*!
-     * \brief Returns the acceleration due to gravity \f$\mathrm{[m/s^2]}\f$.
-     *
-     * The default behaviour is a constant gravity vector;
-     * if the <tt>Problem.EnableGravity</tt> parameter is true,
-     * \f$\boldsymbol{g} = ( 0,\dots,\ -9.81)^T \f$,
-     * else \f$\boldsymbol{g} = ( 0,\dots, 0)^T \f$.
-     *
-     * \param pos the spatial position at which to evaulate the gravity vector
-     */
-    const GlobalPosition& gravity(const GlobalPosition &pos) const
-    { return gravity_; }
-
-    /*!
-     * \brief Function for defining the solid volume fraction.
-     *        That is possibly solution dependent.
-     *
-     * \param element The current element
-     * \param scv The sub-control volume inside the element.
-     * \param elemSol The solution at the dofs connected to the element.
-     * \param compIdx The solid component index
-     * \return the volume fraction of the inert solid component with index compIdx
-     */
-    template<class SolidSystem, class ElementSolution>
-    Scalar inertVolumeFraction(const Element& element,
-                               const SubControlVolume& scv,
-                               const ElementSolution& elemSol,
-                               int compIdx) const
-    {
-        static_assert(SolidSystem::isInert(), "Elastic model can only be used with inert solid systems");
-
-        // when there is only one component, the volume fraction must be one
-        if (SolidSystem::numInertComponents == 1)
-            return 1.0;
-
-        // otherwise we require the user to define the solid composition
-        return asImp_().template inertVolumeFractionAtPos<SolidSystem>(scv.center(), compIdx);
-    }
-
-    /*!
-     * \brief Function for defining the solid volume fraction.
-     *        That is possibly solution dependent.
-     *
-     * \param globalPos The global position
-     * \param compIdx The solid component index
-     * \return the volume fraction of the inert solid component with index compIdx
-     */
-    template<class SolidSystem>
-    Scalar inertVolumeFractionAtPos(const GlobalPosition& globalPos, int compIdx) const
-    { DUNE_THROW(Dune::InvalidStateException, "The spatial parameters do not provide inertVolumeFractionAtPos() method."); }
-
-    /*!
-     * \brief Define the Lame parameters
-     * \note  These are possibly solution dependent and are evaluated
-     *        for an integration point inside the element. Therefore,
-     *        a flux variables cache object is passed to this function
-     *        containing data on shape functions at the integration point.
-     *
-     * \param element The current element
-     * \param fvGeometry The local finite volume geometry
-     * \param elemVolVars Primary/Secondary variables inside the element
-     * \param fluxVarsCache Contains data on shape functions at the integration point
-     * \return lame parameters
-     */
-    template<class ElemVolVars, class FluxVarsCache>
-    decltype(auto) lameParams(const Element& element,
-                              const FVElementGeometry& fvGeometry,
-                              const ElemVolVars& elemVolVars,
-                              const FluxVarsCache& fluxVarsCache) const
-    {
-        static_assert(decltype(isValid(Detail::hasLameParamsAtPos<GlobalPosition>())(this->asImp_()))::value," \n\n"
-        "   Your spatial params class has to either implement\n\n"
-        "         const LameParams& lameParamsAtPos(const GlobalPosition& globalPos) const\n\n"
-        "   or overload this function\n\n"
-        "         template<class ElementSolution>\n"
-        "         const LameParams& lameParams(const Element& element,\n"
-        "                                      const FVElementGeometry& fvGeometry,\n"
-        "                                      const ElemVolVars& elemVolVars,\n"
-        "                                      const FluxVarsCache& fluxVarsCache) const\n\n");
-
-        return asImp_().lameParamsAtPos(fluxVarsCache.ipGlobal());
-    }
-
-    //! The finite volume grid geometry
-    const FVGridGeometry& fvGridGeometry() const { return *fvGridGeometry_; }
-
-protected:
-    Implementation &asImp_()
-    { return *static_cast<Implementation*>(this); }
-
-    const Implementation &asImp_() const
-    { return *static_cast<const Implementation*>(this); }
-
-private:
-    std::shared_ptr<const FVGridGeometry> fvGridGeometry_;
-    GlobalPosition gravity_; //!< The gravity vector
-};
-} // end namespace Dumuxs
 #endif
