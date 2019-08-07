10041392ee86fcfb3297c6309a5bc9b15d2b0caa
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2019-08-07 16:42:10 +0200

    [doc] improve script for converting C++ to markdown

--- bin/doc/cpp_to_md.sh
+++ bin/doc/cpp_to_md.sh
@@ -1,30 +1,70 @@
 #!/bin/sh
 
 if [[ ${1: -3} == ".hh" ]]; then
-  sed '1,/define/d' $1 >tmpfile
+  sed '1,/#define/d' $1 >tmpfile
+  isheader=true
 else
-  sed '1,/\*\*\*\*\*/d' $1 >tmpfile
+  sed '1,/\*\*\*\*\*\//d' $1 >tmpfile
+  isheader=false
 fi
 
 insidecodeblock=false
+lastline=""
+strippedlastline=""
+firstline=true
 
 while IFS= read -r line
 do
   strippedline=$(echo $line | sed "s/^[ \t]*//")
-  if [[ ${strippedline:0:2} == "//" ]]; then
+  if [[ $firstline == false ]]; then
+    if [[ ${strippedline:0:2} == "//" ]]; then
+      if [[ $insidecodeblock == true ]]; then
+        if [[ $lastline != "" ]]; then
+          echo "$lastline"
+        fi
+        echo "\`\`\`"
+        insidecodeblock=false
+      else
+        linetoprint=$(echo ${strippedlastline:2} | sed "s/^[ \t]*//")
+        echo "$linetoprint"
+      fi
+    else
+      if [[ $insidecodeblock == false  ]]; then
+        linetoprint=$(echo ${strippedlastline:2} | sed "s/^[ \t]*//")
+        echo "$linetoprint"
+        echo "\`\`\`cpp"
+        insidecodeblock=true
+      else
+        echo "$lastline"
+      fi
+    fi
+  else
+    if [[ ${strippedline:0:2} != "//" && $line != "" ]]; then
+      echo "\`\`\`cpp"
+      insidecodeblock=true
+    fi
+    firstline=false
+  fi
+  lastline="$line"
+  strippedlastline="$strippedline"
+done < tmpfile
+
+if [[ $isheader == false ]]; then
+  if [[ ${strippedlastline:0:2} == "//" ]]; then
     if [[ $insidecodeblock == true ]]; then
       echo "\`\`\`"
       insidecodeblock=false
     fi
-    echo "${strippedline:2}"
+    linetoprint=$(echo ${strippedlastline:2} | sed "s/^[ \t]*//")
+    echo "$linetoprint"
   else
-    if [[ $insidecodeblock == false ]]; then
+    if [[ $insidecodeblock == false  ]]; then
       echo "\`\`\`cpp"
       insidecodeblock=true
     fi
-    echo "$line"
+    echo "$lastline"
   fi
-done < tmpfile
+fi
 
 if [[ $insidecodeblock == true ]]; then
   echo "\`\`\`"
