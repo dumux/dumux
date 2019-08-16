bcbbcde63acf1734738abc6636bbbe9f07025b5f
Dennis.Glaeser <dennis.glaeser@iws.uni-stuttgart.de>
2019-08-16 16:45:41 +0200

    FIX EVAL GRADIENTS FEM

--- dumux/discretization/evalgradients.hh
+++ dumux/discretization/evalgradients.hh
@@ -181,16 +181,12 @@ auto evalGradients(const Element& element,
         const auto& localPos = geometry.local(globalPos);
         IpDataAnsatz ipData(geometry, localPos, ansatzLocalBasis);
 
-        // the inverse transposed of the jacobian matrix
-        const auto jacInvT = geometry.jacobianInverseTransposed(localPos);
-
         // interpolate the gradients
         Dune::FieldVector<GlobalPosition, PrimaryVariables::dimension> result( GlobalPosition(0.0) );
         for (int i = 0; i < ansatzLocalView.size(); ++i)
         {
             // the global shape function gradient
-            GlobalPosition gradN;
-            jacInvT.mv(ipData.gradN(i), gradN);
+            const auto& gradN = ipData.gradN(i);
 
             // add gradient to global privar gradients
             for (unsigned int pvIdx = 0; pvIdx < PrimaryVariables::dimension; ++pvIdx)
@@ -205,7 +201,7 @@ auto evalGradients(const Element& element,
     }
     else
     {
-        DUNE_THROW(Dune::NotImplemented, "Element vertices have different phase states. Enforce calculation by setting ignoreState to true.");
+        DUNE_THROW(Dune::NotImplemented, "Element dofs have different phase states. Enforce calculation by setting ignoreState to true.");
     }
 }
 
