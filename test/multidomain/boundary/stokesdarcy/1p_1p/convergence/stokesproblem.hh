d702871424af827f48c1c2f9e7910a26f5a26b13
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2019-08-22 22:14:27 +0200

    [md][stokesdarcy][mpfa] Correct source term

--- test/multidomain/boundary/stokesdarcy/1p_1p/convergence/stokesproblem.hh
+++ test/multidomain/boundary/stokesdarcy/1p_1p/convergence/stokesproblem.hh
@@ -130,9 +130,9 @@ public:
 
         source[Indices::conti0EqIdx]  = -sin(2*M_PI*x);
         source[Indices::momentumXBalanceIdx] = -2*M_PI*y*y*sin(4*M_PI*x)
-                                               -2*y*sin(2*M_PI*x)*sin(2*M_PI*x)
+                                               -2*y*sin(2*M_PI*x)
                                                +2*M_PI*cos(2*M_PI*x) ;
-        source[Indices::momentumYBalanceIdx] =  -2*M_PI*y*y*sin(4*M_PI*x)
+        source[Indices::momentumYBalanceIdx] =  -2*M_PI*y*y*cos(2*M_PI*x)
                                                 -4*M_PI*M_PI*sin(2*M_PI*x);
 
         return source;
