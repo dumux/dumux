52795947d88515565c825968f510ecb5e6134eb5
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2019-09-13 13:30:29 +0200

    [discretization] deprecate FVGridGeometry

--- dumux/common/properties.hh
+++ dumux/common/properties.hh
@@ -113,7 +113,7 @@ struct ElementBoundaryTypes { using type = UndefinedProperty; };                
 
 // TODO: Remove deprecated property FVGridGeometry after 3.1
 template<class TypeTag, class MyTypeTag>
-struct FVGridGeometry { using type = UndefinedProperty; };                      //!< The type of the global finite volume geometry
+struct [[deprecated("Use GridGeometry instead.")]] FVGridGeometry { using type = UndefinedProperty; }; //!< The type of the global finite volume geometry
 
 // Dumux 3.1 changes the property `FVGridGeometry` to `GridGeometry`.
 // For ensuring backward compatibility, it is necessary to set the default value
