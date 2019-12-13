dd80a2bd9a1d1bd2943a5ceeba61e3fbd05fc229
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2019-12-13 22:02:07 +0100

    [test] Add test for refinement quadrature rule for simplex

---
+++ test/common/geometry/test_refinementquadraturerule.cc
@@ -0,0 +1,41 @@
+#include <config.h>
+
+#include <iostream>
+#include <array>
+
+#include <dune/common/fvector.hh>
+#include <dune/common/exceptions.hh>
+#include <dune/common/float_cmp.hh>
+#include <dumux/common/geometry/refinementquadraturerule.hh>
+#include <dune/geometry/affinegeometry.hh>
+
+int main (int argc, char *argv[]) try
+{
+    using namespace Dumux;
+
+    // make a simplex
+    const auto simplex = Dune::AffineGeometry<double, 2, 3>(
+        Dune::GeometryTypes::simplex(2),
+        std::array<Dune::FieldVector<double, 3>, 3>{{{0.0, 0.0, 1.0}, {2.0, 3.0, 4.0}, {3.0, -1.0, 1.0}}}
+    );
+
+    // make quadrature rule
+    const auto quad = RefinementQuadratureRule<double, 2>(Dune::GeometryTypes::simplex(2), /*refinementlevels=*/3);
+
+    // integrate volume
+    double volume = 0.0;
+    for (const auto& qp : quad)
+        volume += qp.weight()*simplex.integrationElement(qp.position());
+
+    if (Dune::FloatCmp::ne(volume, simplex.volume()))
+        DUNE_THROW(Dune::Exception, "Integration yields wrong simplex volume: " << volume << ", should be " << simplex.volume());
+
+    return 0;
+}
+// //////////////////////////////////
+//   Error handler
+// /////////////////////////////////
+catch (const Dune::Exception& e) {
+    std::cout << e << std::endl;
+    return 1;
+}
