d6284598cfb828b67a88e29c8e4070f5ca9fc578
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2020-01-29 14:13:19 +0100

    [solver][istl] Rename GenericIstlBackend to IstlSolverFactoryBackend. Remove TypeTag alias.

--- test/porousmediumflow/richards/implicit/lens/main.cc
+++ test/porousmediumflow/richards/implicit/lens/main.cc
@@ -27,6 +27,7 @@
 #include <ctime>
 #include <iostream>
 
+#include <dune/common/version.hh>
 #include <dune/common/parallel/mpihelper.hh>
 #include <dune/common/timer.hh>
 #include <dune/grid/io/file/dgfparser/dgfexception.hh>
@@ -37,10 +38,15 @@
 #include <dumux/common/parameters.hh>
 #include <dumux/common/valgrind.hh>
 #include <dumux/common/dumuxmessage.hh>
+#include <dumux/linear/linearsolvertraits.hh>
 
-#include <dumux/linear/genericistlbackend.hh>
+#if DUNE_VERSION_NEWER_REV(DUNE_ISTL,2,7,1)
+#include <dumux/linear/istlsolverfactorybackend.hh>
+#else
+#include <dumux/linear/amgbackend.hh>
+#endif
+
 #include <dumux/porousmediumflow/richards/newtonsolver.hh>
-
 #include <dumux/assembly/fvassembler.hh>
 
 #include <dumux/io/vtkoutputmodule.hh>
@@ -143,7 +149,14 @@ int main(int argc, char** argv) try
     auto assembler = std::make_shared<Assembler>(problem, gridGeometry, gridVariables, timeLoop, xOld);
 
     // the linear solver
-    using LinearSolver = Dumux::GenericIstlBackend<TypeTag>;
+#if DUNE_VERSION_NEWER_REV(DUNE_ISTL,2,7,1)
+    using Matrix = GetPropType<TypeTag, Properties::JacobianMatrix>;
+    using Vector = Dune::BlockVector<Dune::FieldVector<typename SolutionVector::block_type::value_type, SolutionVector::block_type::dimension>>;
+    using LinearSolver = IstlSolverFactoryBackend<Matrix, Vector, GridGeometry>;
+#else
+    using LinearSolver = AMGBiCGSTABBackend<LinearSolverTraits<GridGeometry>>;
+#endif
+
     auto linearSolver = std::make_shared<LinearSolver>(leafGridView, gridGeometry->dofMapper());
 
     // the non-linear solver
