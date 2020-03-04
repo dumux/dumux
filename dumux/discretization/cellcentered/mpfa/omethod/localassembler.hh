fa305f74457ba5024c47719751f7d49371bd0598
Dennis.Glaeser <dennis.glaeser@iws.uni-stuttgart.de>
2020-03-04 18:01:07 +0100

    [mpfa][lambdafactory] remove no-longer needed interface arguments
    Since we store the effective coefficients in the volume variables we
    don't need the cumbersome extraction of permeabilities, diffusion
    coefficients etc. anymore.

--- dumux/discretization/cellcentered/mpfa/omethod/localassembler.hh
+++ dumux/discretization/cellcentered/mpfa/omethod/localassembler.hh
@@ -195,8 +195,7 @@ private:
                 const auto& posLocalScv = iv.localScv(neighborScvIndices[0]);
                 const auto& posGlobalScv = this->fvGeometry().scv(posLocalScv.gridScvIndex());
                 const auto& posVolVars = this->elemVolVars()[posGlobalScv];
-                const auto& posElement = iv.element(neighborScvIndices[0]);
-                const auto tensor = getT(this->problem(), posElement, posVolVars, this->fvGeometry(), posGlobalScv);
+                const auto tensor = getT(posVolVars);
 
                 // the omega factors of the "positive" sub volume
                 Helper::resizeVector(wijk[faceIdx], /*no outside scvs present*/1);
@@ -232,8 +231,7 @@ private:
                 const auto& posLocalScv = iv.localScv(neighborScvIndices[0]);
                 const auto& posGlobalScv = this->fvGeometry().scv(posLocalScv.gridScvIndex());
                 const auto& posVolVars = this->elemVolVars()[posGlobalScv];
-                const auto& posElement = iv.element(neighborScvIndices[0]);
-                const auto tensor = getT(this->problem(), posElement, posVolVars, this->fvGeometry(), posGlobalScv);
+                const auto tensor = getT(posVolVars);
 
                 // the omega factors of the "positive" sub volume
                 Helper::resizeVector(wijk[faceIdx], neighborScvIndices.size());
@@ -292,8 +290,7 @@ private:
                         const auto& negLocalScv = iv.localScv( neighborScvIndices[idxOnScvf] );
                         const auto& negGlobalScv = this->fvGeometry().scv(negLocalScv.gridScvIndex());
                         const auto& negVolVars = this->elemVolVars()[negGlobalScv];
-                        const auto& negElement = iv.element( neighborScvIndices[idxOnScvf] );
-                        const auto negTensor = getT(this->problem(), negElement, negVolVars, this->fvGeometry(), negGlobalScv);
+                        const auto negTensor = getT(negVolVars);
 
                         // On surface grids, use outside face for "negative" transmissibility calculation
                         const auto& scvf = dim < dimWorld ? this->fvGeometry().flipScvf(curGlobalScvf.index(), idxInOutside)
