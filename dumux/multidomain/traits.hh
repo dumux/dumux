ddfc85634e6d468703528f511c6d394b65beda66
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2020-03-14 17:07:34 +0100

    [md][traits] Use std::conjunction

--- dumux/multidomain/traits.hh
+++ dumux/multidomain/traits.hh
@@ -49,12 +49,7 @@ namespace Detail {
 template<class Scalar, class... JacobianBlocks>
 class createMultiTypeBlockMatrixType
 {
-    //! TODO: replace by std::conjuction in C++17
-    template<bool...> struct boolPack;
-    template<bool... bools>
-    using all_true = std::is_same<boolPack<bools..., true>, boolPack<true, bools...>>;
-
-    static_assert(all_true<isBCRSMatrix<JacobianBlocks>::value...>::value, "Jacobian blocks have to be BCRSMatrices!");
+    static_assert(std::conjunction_v<isBCRSMatrix<JacobianBlocks>...>, "Jacobian blocks have to be BCRSMatrices!");
 
     template<std::size_t id>
     using JacobianDiagBlock = typename std::tuple_element_t<id, std::tuple<JacobianBlocks...>>;
