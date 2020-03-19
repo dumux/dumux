5e366ec84b7679f222931e1ae04935f9061049fa
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2020-03-19 20:48:46 +0100

    [diffusion] Suggestion for FickianDiffusionContainers

--- dumux/flux/fickiandiffusioncoefficients.hh
+++ dumux/flux/fickiandiffusioncoefficients.hh
@@ -22,10 +22,12 @@
  * \brief Container storing the diffusion coefficients required by Fick's law.
  *        Uses the minimal possible container size and provides unified access.
  */
-#ifndef DUMUX_DISCRETIZATION_FICKIAN_DIFFUSION_COEFFICIENTS_HH
-#define DUMUX_DISCRETIZATION_FICKIAN_DIFFUSION_COEFFICIENTS_HH
+#ifndef DUMUX_FLUX_FICKIAN_DIFFUSION_COEFFICIENTS_HH
+#define DUMUX_FLUX_FICKIAN_DIFFUSION_COEFFICIENTS_HH
 
+#include <array>
 #include <cassert>
+#include <algorithm>
 
 namespace Dumux {
 
@@ -53,40 +55,32 @@ class FickianDiffusionCoefficients<Scalar, numPhases, numComponents>
 {
 public:
     template<class DiffCoeffFunc>
-    void update(DiffCoeffFunc& computeDiffCoeff)
+    void update(const DiffCoeffFunc& computeDiffCoeff)
     {
-        for (unsigned int phaseIdx = 0; phaseIdx < numPhases; ++ phaseIdx)
-        {
-            unsigned int compIIdx = phaseIdx;
-            for (unsigned int compJIdx = 0; compJIdx < numComponents; ++compJIdx)
-            {
-                if (compIIdx == compJIdx)
-                    continue;
-                diffCoeff_[getIndex_(phaseIdx, compIIdx, compJIdx)] = computeDiffCoeff(phaseIdx, compIIdx, compJIdx);
-            }
-        }
+        for (int phaseIdx = 0; phaseIdx < numPhases; ++ phaseIdx)
+            for (int compIIdx = std::min(phaseIdx, numComponents), compJIdx = 0; compJIdx < numComponents; ++compJIdx)
+                if (compIIdx != compJIdx)
+                    diffCoeff_[getIndex_(phaseIdx, compIIdx, compJIdx)] = computeDiffCoeff(phaseIdx, compIIdx, compJIdx);
     }
 
-    const Scalar& operator()(int phaseIdx, int compIIdx, int compJIdx) const
+    Scalar operator() (int phaseIdx, int compIIdx, int compJIdx) const
     {
-        std::cout << phaseIdx << " " << compIIdx << " " << compJIdx << " | " << getIndex_(phaseIdx, compIIdx, compJIdx) << " | ";
-        sortComponentIndices_(compIIdx, compJIdx);
-        std::cout << phaseIdx << " " << compIIdx << " " << compJIdx << " | " << getIndex_(phaseIdx, compIIdx, compJIdx) << " | ";
-
+        sortComponentIndices_(phaseIdx, compIIdx, compJIdx);
+        assert(compIIdx != compJIdx);
         return diffCoeff_[getIndex_(phaseIdx, compIIdx, compJIdx)];
     }
 
 
 private:
     /*
-     * \brief For the mpnc case, we have a rectangular diffCoeff matrix (numPhases x numComponents).
-     *        As we do not track a diffusion coefficient for a component within it's phase,
-     *        this numComponents is reduced by 1. (numPhases x (numComponents - 1))
+     * \brief For the mpnc case, we do not track a diffusion coefficient for the component of a phase.
+     *        Hence, we have a matrix of dimensions (numPhases * (numComponents - 1))
+     *        We store this matrix in a one-dimensional array
     */
     std::array<Scalar, numPhases * (numComponents - 1)> diffCoeff_;
 
     /*
-     * \brief This calculates the linear index of the diffusion coefficient matrix.
+     * \brief This calculates the array index to a corresponding matrix entry.
      *        An example with a 3p5c model:
      *                        compJIdx
      *                   [ x  0  1  2  3  ]
@@ -99,101 +93,8 @@ private:
     constexpr int getIndex_(int phaseIdx, int compIIdx, int compJIdx) const
     { return (phaseIdx * (numComponents-1)) + compJIdx - static_cast<int>(phaseIdx < compJIdx); }
 
-    void sortComponentIndices_(int& compIIdx, int& compJIdx) const
-    { if (compIIdx > compJIdx) std::swap(compIIdx, compJIdx); }
-};
-
-//! Specialization for 1pnc & compositions containing the phases' main components
-template <class Scalar, int numComponents>
-class FickianDiffusionCoefficients<Scalar, 1, numComponents>
-{
-public:
-    template<class DiffCoeffFunc>
-    void update(DiffCoeffFunc& computeDiffCoeff)
-    {
-        for (unsigned int compJIdx = 1; compJIdx < numComponents; ++compJIdx)
-        {
-            diffCoeff_[getIndex_(0, 0, compJIdx)] = computeDiffCoeff(0, 0, compJIdx);
-        }
-    }
-
-    const Scalar& operator()(int phaseIdx, int compIIdx, int compJIdx) const
-    {
-        assert(phaseIdx == 0);
-        assert(phaseIdx != compJIdx);
-        assert(compIIdx != compJIdx);
-        assert(compIIdx < numComponents);
-        assert(compJIdx < numComponents);
-        assert(compIIdx <= compJIdx);
-        sortComponentIndices_(compIIdx, compJIdx);
-        return diffCoeff_[compJIdx-1];
-    }
-
-private:
-    std::array<Scalar, numComponents-1> diffCoeff_;
-
-    constexpr int getIndex_(int phaseIdx, int compIIdx, int compJIdx) const
-    { return  compJIdx-1; }
-
-    void sortComponentIndices_(int& compIIdx, int& compJIdx) const
-    { if (compIIdx > compJIdx) std::swap(compIIdx, compJIdx); }
-};
-
-//! Specialization for 2p2c & compositions containing the phases' main components
-template <class Scalar>
-class FickianDiffusionCoefficients<Scalar, 2, 2>
-{
-public:
-    template<class DiffCoeffFunc>
-    void update(DiffCoeffFunc& computeDiffCoeff)
-    {
-        diffCoeff_[getIndex_(0, 0, 1)] = computeDiffCoeff(0, 0, 1);
-        diffCoeff_[getIndex_(1, 1, 0)] = computeDiffCoeff(1, 1, 0);
-    }
-
-    const Scalar& operator()(int phaseIdx, int compIIdx, int compJIdx) const
-    {
-        sortComponentIndices_(compIIdx, compJIdx);
-        return diffCoeff_[phaseIdx];
-    }
-
-private:
-    std::array<Scalar, 2> diffCoeff_;
-
-    constexpr int getIndex_(int phaseIdx, int compIIdx, int compJIdx) const
-    { return  phaseIdx; }
-
-    void sortComponentIndices_(int& compIIdx, int& compJIdx) const
-    { if (compIIdx > compJIdx) std::swap(compIIdx, compJIdx); }
-};
-
-//! Specialization for 3p2c & compositions containing the phases' main components
-template <class Scalar>
-class FickianDiffusionCoefficients<Scalar, 3, 2>
-{
-public:
-    template<class DiffCoeffFunc>
-    void update(DiffCoeffFunc& computeDiffCoeff)
-    {
-        diffCoeff_[getIndex_(0, 0, 1)] = computeDiffCoeff(0, 0, 1);
-        diffCoeff_[getIndex_(1, 1, 0)] = computeDiffCoeff(1, 1, 0);
-        diffCoeff_[getIndex_(2, 1, 0)] = computeDiffCoeff(2, 1, 0);
-    }
-
-    const Scalar& operator()(int phaseIdx, int compIIdx, int compJIdx) const
-    {
-        sortComponentIndices_(compIIdx, compJIdx);
-        return diffCoeff_[phaseIdx];
-    }
-
-private:
-    std::array<Scalar, 3> diffCoeff_;
-
-    constexpr int getIndex_(int phaseIdx, int compIIdx, int compJIdx) const
-    { return phaseIdx; }
-
-    void sortComponentIndices_(int& compIIdx, int& compJIdx) const
-    { if (compIIdx > compJIdx) std::swap(compIIdx, compJIdx); }
+    void sortComponentIndices_(int phaseIdx, int& compIIdx, int& compJIdx) const
+    { if (compIIdx != phaseIdx) std::swap(compIIdx, compJIdx); }
 };
 
 //! Specialization for mpnc & compositions that only contain tracers
@@ -202,21 +103,17 @@ class FickianDiffusionCoefficients<Scalar, numPhases, numComponents, true>
 {
 public:
     template<class DiffCoeffFunc>
-    void update(DiffCoeffFunc& computeDiffCoeff)
+    void update(const DiffCoeffFunc& computeDiffCoeff)
     {
-        for (unsigned int phaseIdx = 0; phaseIdx < numPhases; ++phaseIdx)
-            for (unsigned int compIdx = 0; compIdx < numComponents; ++compIdx)
+        for (int phaseIdx = 0; phaseIdx < numPhases; ++phaseIdx)
+            for (int compIdx = 0; compIdx < numComponents; ++compIdx)
                 diffCoeff_[getIndex_(phaseIdx, compIdx)]
                     = computeDiffCoeff(phaseIdx, phaseIdx, compIdx);
     }
 
-    const Scalar& operator()(int phaseIdx, int compIIdx, int compJIdx) const
+    Scalar operator() (int phaseIdx, int compIIdx, int compJIdx) const
     {
-        sortComponentIndices_(compIIdx, compJIdx);
-        assert(phaseIdx < numPhases);
-        assert(compIIdx < numComponents);
-        assert(compJIdx < numComponents);
-        assert(compIIdx <= compJIdx);
+        sortComponentIndices_(phaseIdx, compIIdx, compJIdx);
         return diffCoeff_[getIndex_(phaseIdx, compJIdx)];
     }
 
@@ -226,8 +123,8 @@ private:
     constexpr int getIndex_(int phaseIdx, int compJIdx) const
     { return phaseIdx * numComponents + compJIdx; }
 
-    void sortComponentIndices_(int& compIIdx, int& compJIdx) const
-    { if (compIIdx > compJIdx) std::swap(compIIdx, compJIdx); }
+    void sortComponentIndices_(int phaseIdx, int& compIIdx, int& compJIdx) const
+    { if (compIIdx != phaseIdx) std::swap(compIIdx, compJIdx); }
 };
 
 } // end namespace Dumux
