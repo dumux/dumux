e2adbaae76382e7dbfd35ffceccd8272ed65b30b
Kilian <kilian.weishaupt@iws.uni-stuttgart.de>
2020-04-03 12:28:22 +0200

    [linear][solverregistry] Add header with dumux macro and Tag

---
+++ dumux/linear/istlsolverregistry.hh
@@ -0,0 +1,56 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup Linear
+ * \brief The specialized Dumux macro and tag for the ISTL registry to choose the
+ *        solver and preconditioner at runtime
+ */
+#ifndef DUMUX_LINEAR_ISTL_SOLVER_REGISTRY_HH
+#define DUMUX_LINEAR_ISTL_SOLVER_REGISTRY_HH
+
+#include <dune/common/version.hh>
+#if DUNE_VERSION_NEWER_REV(DUNE_ISTL,2,7,1)
+
+#include <dune/istl/common/registry.hh>
+#include <dune/istl/solverregistry.hh>
+
+/*!
+ * \brief Register a Dumux preconditioner
+ *
+ * Use this macro in namespace Dumux.
+ * Example:
+ * DUMUX_REGISTER_PRECONDITIONER("mypreconditioner", Dumux::MultiTypeBlockMatrixPreconditionerTag, Dune::defaultPreconditionerBlockLevelCreator<Dumux::MyPreconditioner, 1>());
+ * Expicitly specifying the namespaces is required.
+ * Set parameter Preconditioner.Type to "mypreconditioner" to use it through the factory.
+ */
+#define DUMUX_REGISTER_PRECONDITIONER(name, tag, ...)                 \
+} namespace Dune {                                               \
+DUNE_REGISTRY_PUT(tag, name, __VA_ARGS__);  \
+} namespace Dumux {                                              \
+
+namespace Dumux {
+namespace {
+struct MultiTypeBlockMatrixPreconditionerTag {};
+struct MultiTypeBlockMatrixDirectSolverTag {};
+} // end namespace
+} // end namespace Dumux
+
+#endif // DUNE version check
+#endif // DUMUX_LINEAR_ISTL_SOLVER_REGISTRY_HH
