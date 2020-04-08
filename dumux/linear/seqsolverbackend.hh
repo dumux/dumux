19fdb7c6329005a8f7c06d3b02ef08feb9f09fa8
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2020-04-08 09:58:47 +0200

    [linear] use nonoverlapping traits for box

--- dumux/linear/seqsolverbackend.hh
+++ dumux/linear/seqsolverbackend.hh
@@ -1267,7 +1267,7 @@ class ParallelBlockDiagAMGPreconditioner : public Dune::Preconditioner<X, Y>
 
     template<std::size_t i>
     using ParallelTraits = std::conditional_t<std::tuple_element_t<i, GridGeometries>::discMethod == DiscretizationMethod::box,
-                                              OverlappingTraits<i>,
+                                              NonoverlappingTraits<i>,
                                               OverlappingTraits<i>>;
 
     template<std::size_t i>
@@ -1530,7 +1530,7 @@ class BlockDiagAMGBiCGSTABSolver : public LinearSolver
 
     template<class M, class X, std::size_t i>
     using ParallelTraits = std::conditional_t<std::tuple_element_t<i, GridGeometries>::discMethod == DiscretizationMethod::box,
-                                              OverlappingTraits<M, X, i>,
+                                              NonoverlappingTraits<M, X, i>,
                                               OverlappingTraits<M, X, i>>;
 
     template<class M, class X, std::size_t i>
