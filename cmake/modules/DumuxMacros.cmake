11e68e319698a12506fe24a19a68ced44ee9ea5b
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2020-04-27 17:11:15 +0200

    [opm][cmake] adapt to changes in OPM
    It is currently required that CMake looks for OpenMP, see
    https://github.com/OPM/opm-common/issues/1751.
    On the other, setting `-fopenmp` in the test is not
    necessary anymore.

--- cmake/modules/DumuxMacros.cmake
+++ cmake/modules/DumuxMacros.cmake
@@ -14,3 +14,7 @@ find_package(PTScotch)
 find_package(PVPython)
 find_package(Valgrind)
 find_package(Quadmath)
+# The following is required for being able to depend on OPM.
+# Remove once a better solution has been found at
+# https://github.com/OPM/opm-common/issues/1751.
+find_package(OpenMP)
