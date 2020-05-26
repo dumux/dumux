563ba0c686809d8654bde8712bc11f66d1727ba8
Katharina Heck <katharina.heck@iws.uni-stuttgart.de>
2020-05-26 11:57:02 +0200

    [rans][komega] add density in all terms of k and omega calculation

--- dumux/freeflow/rans/twoeq/komega/staggered/localresidual.hh
+++ dumux/freeflow/rans/twoeq/komega/staggered/localresidual.hh
@@ -83,8 +83,8 @@ public:
     {
         CellCenterPrimaryVariables storage = ParentType::computeStorageForCellCenter(problem, scv, volVars);
 
-        storage[turbulentKineticEnergyEqIdx] = volVars.turbulentKineticEnergy();
-        storage[dissipationEqIdx] = volVars.dissipation();
+        storage[turbulentKineticEnergyEqIdx] = volVars.turbulentKineticEnergy()*volVars.density();
+        storage[dissipationEqIdx] = volVars.dissipation()*volVars.density();
 
         return storage;
     }
@@ -105,18 +105,18 @@ public:
         // production
         static const auto enableKOmegaProductionLimiter
             = getParamFromGroup<bool>(problem.paramGroup(), "KOmega.EnableProductionLimiter", false);
-        Scalar productionTerm = 2.0 * volVars.kinematicEddyViscosity() * volVars.stressTensorScalarProduct();
+        Scalar productionTerm = 2.0 * volVars.dynamicEddyViscosity() * volVars.stressTensorScalarProduct();
         if (enableKOmegaProductionLimiter)
         {
-            Scalar productionAlternative = 20.0 * volVars.betaK() * volVars.turbulentKineticEnergy() * volVars.dissipation();
+            Scalar productionAlternative = 20.0 * volVars.density() * volVars.betaK() * volVars.turbulentKineticEnergy() * volVars.dissipation();
             productionTerm = min(productionTerm, productionAlternative);
         }
         source[turbulentKineticEnergyEqIdx] += productionTerm;
         source[dissipationEqIdx] += volVars.alpha() * volVars.dissipation() / volVars.turbulentKineticEnergy() * productionTerm;
 
         // destruction
-        source[turbulentKineticEnergyEqIdx] -= volVars.betaK() * volVars.turbulentKineticEnergy() * volVars.dissipation();
-        source[dissipationEqIdx] -= volVars.betaOmega() * volVars.dissipation() * volVars.dissipation();
+        source[turbulentKineticEnergyEqIdx] -= volVars.betaK() * volVars.density() * volVars.turbulentKineticEnergy() * volVars.dissipation();
+        source[dissipationEqIdx] -= volVars.betaOmega() * volVars.density() * volVars.dissipation() * volVars.dissipation();
 
         // cross-diffusion term
         Scalar gradientProduct = 0.0;
@@ -124,7 +124,7 @@ public:
             gradientProduct += volVars.storedTurbulentKineticEnergyGradient()[i]
                                * volVars.storedDissipationGradient()[i];
         if (gradientProduct > 0.0)
-            source[dissipationEqIdx] += 0.125 / volVars.dissipation() * gradientProduct;
+            source[dissipationEqIdx] += 0.125 *volVars.density() / volVars.dissipation() * gradientProduct;
 
         return source;
     }
