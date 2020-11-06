6f2aed1fca5f24ff43bae8ddb08b5da98b406865
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2020-11-06 09:53:11 +0100

    [examples] Fix plot test on minimal installations

--- examples/liddrivencavity/run_and_plot.py
+++ examples/liddrivencavity/run_and_plot.py
@@ -26,8 +26,11 @@ for re in reynolds:
     if not args['skipsim']:
         subprocess.run(['make', 'example_ff_liddrivencavity'], check=True)
         subprocess.run(['./example_ff_liddrivencavity', 'params_re' + str(re) + '.input'], check=True)
-    y[str(re)], vx[str(re)] = np.genfromtxt('example_ff_liddrivencavity_re' + str(re) + '_vx' + '.log', skip_header= True).T
-    x[str(re)], vy[str(re)] = np.genfromtxt('example_ff_liddrivencavity_re' + str(re) + '_vy' + '.log', skip_header= True).T
+    try:
+        y[str(re)], vx[str(re)] = np.genfromtxt('example_ff_liddrivencavity_re' + str(re) + '_vx' + '.log', skip_header= True).T
+        x[str(re)], vy[str(re)] = np.genfromtxt('example_ff_liddrivencavity_re' + str(re) + '_vy' + '.log', skip_header= True).T
+    except OSError:
+        sys.exit(127)
 
 ####################################################
 #### reference #####################################
