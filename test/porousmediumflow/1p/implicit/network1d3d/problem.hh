fd55d0b6ce08587e0c247a409c4e735ddda24cb1
Yue Wang <yue.wang@iws.uni-stuttgart.de>
2020-12-02 15:46:35 +0100

    [test][1p network] separate properties

--- test/porousmediumflow/1p/implicit/network1d3d/problem.hh
+++ test/porousmediumflow/1p/implicit/network1d3d/problem.hh
@@ -26,100 +26,13 @@
 #ifndef DUMUX_ONEP_TUBES_TEST_PROBLEM_HH
 #define DUMUX_ONEP_TUBES_TEST_PROBLEM_HH
 
-#include <dune/localfunctions/lagrange/pqkfactory.hh>
-#include <dune/geometry/quadraturerules.hh>
-#include <dune/geometry/type.hh>
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
 
-#if HAVE_DUNE_FOAMGRID
-#include <dune/foamgrid/foamgrid.hh>
-#endif
-
-#include <dumux/common/reorderingdofmapper.hh>
 #include <dumux/common/boundarytypes.hh>
-
-#include <dumux/discretization/cctpfa.hh>
-#include <dumux/discretization/box.hh>
-#include <dumux/discretization/method.hh>
-#include <dumux/discretization/elementsolution.hh>
-#include <dumux/porousmediumflow/1p/model.hh>
 #include <dumux/porousmediumflow/problem.hh>
-#include <dumux/material/components/constant.hh>
-#include <dumux/material/fluidsystems/1pliquid.hh>
-
-#include "spatialparams.hh"
 
 namespace Dumux {
-
-template <class TypeTag>
-class TubesTestProblem;
-
-namespace Properties {
-
-// Create new type tags
-namespace TTag {
-struct TubesTest { using InheritsFrom = std::tuple<OneP>; };
-struct TubesTestCCTpfa { using InheritsFrom = std::tuple<TubesTest, CCTpfaModel>; };
-struct TubesTestBox { using InheritsFrom = std::tuple<TubesTest, BoxModel>; };
-} // end namespace TTag
-
-// Set the grid type
-#if HAVE_DUNE_FOAMGRID
-template<class TypeTag>
-struct Grid<TypeTag, TTag::TubesTest> { using type = Dune::FoamGrid<1, 3>; };
-#endif
-
-// if we have pt scotch use the reordering dof mapper to optimally sort the dofs (cc)
-template<class TypeTag>
-struct GridGeometry<TypeTag, TTag::TubesTestCCTpfa>
-{
-private:
-    static constexpr bool enableCache = getPropValue<TypeTag, Properties::EnableGridGeometryCache>();
-    using GridView = typename GetPropType<TypeTag, Properties::Grid>::LeafGridView;
-
-    using ElementMapper = ReorderingDofMapper<GridView>;
-    using VertexMapper = Dune::MultipleCodimMultipleGeomTypeMapper<GridView>;
-    using MapperTraits = DefaultMapperTraits<GridView, ElementMapper, VertexMapper>;
-public:
-    using type = CCTpfaFVGridGeometry<GridView, enableCache, CCTpfaDefaultGridGeometryTraits<GridView, MapperTraits>>;
-};
-
-// if we have pt scotch use the reordering dof mapper to optimally sort the dofs (box)
-template<class TypeTag>
-struct GridGeometry<TypeTag, TTag::TubesTestBox>
-{
-private:
-    static constexpr bool enableCache = getPropValue<TypeTag, Properties::EnableGridGeometryCache>();
-    using GridView = typename GetPropType<TypeTag, Properties::Grid>::LeafGridView;
-    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
-
-    using ElementMapper = Dune::MultipleCodimMultipleGeomTypeMapper<GridView>;
-    using VertexMapper = ReorderingDofMapper<GridView>;
-    using MapperTraits = DefaultMapperTraits<GridView, ElementMapper, VertexMapper>;
-public:
-    using type = BoxFVGridGeometry<Scalar, GridView, enableCache, BoxDefaultGridGeometryTraits<GridView, MapperTraits>>;
-};
-
-// Set the problem property
-template<class TypeTag>
-struct Problem<TypeTag, TTag::TubesTest> { using type = TubesTestProblem<TypeTag>; };
-
-// Set the spatial parameters
-template<class TypeTag>
-struct SpatialParams<TypeTag, TTag::TubesTest>
-{
-    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
-    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
-    using type = TubesTestSpatialParams<GridGeometry, Scalar>;
-};
-
-// the fluid system
-template<class TypeTag>
-struct FluidSystem<TypeTag, TTag::TubesTest>
-{
-    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
-    using type = FluidSystems::OnePLiquid<Scalar, Components::Constant<1, Scalar> >;
-};
-} // end namespace Properties
 
 /*!
  * \ingroup OnePTests
