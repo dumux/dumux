61ad6e1ae43acb91de1e0af2e25803c33a3d0f9e
Yue Wang <yue.wang@iws.uni-stuttgart.de>
2020-12-02 15:39:10 +0100

    [test][1p/isothermal] separate properties

--- test/porousmediumflow/1p/implicit/isothermal/problem.hh
+++ test/porousmediumflow/1p/implicit/isothermal/problem.hh
@@ -25,75 +25,12 @@
 
 #ifndef DUMUX_1PTEST_PROBLEM_HH
 #define DUMUX_1PTEST_PROBLEM_HH
-
-#include <dune/grid/yaspgrid.hh>
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
 
 #include <dumux/common/boundarytypes.hh>
-
-#include <dumux/discretization/cctpfa.hh>
-#include <dumux/discretization/ccmpfa.hh>
-#include <dumux/discretization/box.hh>
-#include <dumux/porousmediumflow/1p/model.hh>
 #include <dumux/porousmediumflow/problem.hh>
-#include <dumux/material/components/simpleh2o.hh>
-#include <dumux/material/fluidsystems/1pliquid.hh>
-
-#include "spatialparams.hh"
-
-#if FORCHHEIMER
-#include <dumux/flux/forchheimerslaw.hh>
-#endif
-
 namespace Dumux {
-
-template <class TypeTag>
-class OnePTestProblem;
-
-namespace Properties {
-
-// Create new type tags
-namespace TTag {
-struct OnePTest { using InheritsFrom = std::tuple<OneP>; };
-struct OnePTestBox { using InheritsFrom = std::tuple<BoxModel, OnePTest>; };
-struct OnePTestCCTpfa { using InheritsFrom = std::tuple<CCTpfaModel, OnePTest>; };
-struct OnePTestCCMpfa { using InheritsFrom = std::tuple<CCMpfaModel, OnePTest>; };
-} // end namespace TTag
-
-// Specialize the fluid system type for this type tag
-template<class TypeTag>
-struct FluidSystem<TypeTag, TTag::OnePTest>
-{
-    using Scalar = GetPropType<TypeTag, Scalar>;
-    using type = FluidSystems::OnePLiquid<Scalar, Components::SimpleH2O<Scalar> >;
-};
-
-// Specialize the grid type for this type tag
-template<class TypeTag>
-struct Grid<TypeTag, TTag::OnePTest>
-{ using type = Dune::YaspGrid<2>; };
-
-// Specialize the problem type for this type tag
-template<class TypeTag>
-struct Problem<TypeTag, TTag::OnePTest>
-{ using type = OnePTestProblem<TypeTag>; };
-
-// Specialize the spatial params type for this type tag
-template<class TypeTag>
-struct SpatialParams<TypeTag, TTag::OnePTest>
-{
-    using GridGeometry = GetPropType<TypeTag, GridGeometry>;
-    using Scalar = GetPropType<TypeTag, Scalar>;
-    using type = OnePTestSpatialParams<GridGeometry, Scalar>;
-};
-
-#ifdef FORCHHEIMER
-// Specialize the advection type for this type tag
-template<class TypeTag>
-struct AdvectionType<TypeTag, TTag::OnePTest>
-{ using type = ForchheimersLaw<TypeTag>; };
-#endif
-
-} // end namespace Properties
 
 /*!
  * \ingroup OnePTests
@@ -235,6 +172,6 @@ private:
     static constexpr Scalar eps_ = 1.0e-6;
 };
 
-} // end namespace Dumux
+}
 
 #endif
