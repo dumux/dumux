0c41881f36a1c53a3354cfdb353d0f7af6e5c8e1
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2020-12-02 19:17:29 +0100

    [pnm][example] Add throat area to output

--- examples/pnmpermeabilityupscaling/main.cc
+++ examples/pnmpermeabilityupscaling/main.cc
@@ -99,6 +99,7 @@ int main(int argc, char** argv) try
     VtkOutputFields::initOutputModule(vtkWriter);
     vtkWriter.addField(gridGeometry->poreVolume(), "poreVolume", PNMVtkOutputModule<TypeTag>::FieldType::vertex);
     vtkWriter.addField(gridGeometry->throatShapeFactor(), "throatShapeFactor", PNMVtkOutputModule<TypeTag>::FieldType::element);
+    vtkWriter.addField(gridGeometry->throatCrossSectionalArea(), "throatCrossSectionalArea", PNMVtkOutputModule<TypeTag>::FieldType::element);
 
     // ### Instantiate the solver
     // We use the `LinearPDESolver` class, which is instantiated on the basis
