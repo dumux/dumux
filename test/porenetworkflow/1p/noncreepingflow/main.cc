eed403214eadfc208659a9d07211a1ab4ece0ff1
Maziar Veyskarami <maziar.veyskarami@iws.uni-stuttgart.de>
2020-12-11 10:42:41 +0100

    [test][porenetworkflow] add 1p non-creeping flow test

---
+++ test/porenetworkflow/1p/noncreepingflow/main.cc
@@ -0,0 +1,184 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief test for the pore network model
+ */
+#include <config.h>
+
+#include <ctime>
+#include <iostream>
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/timer.hh>
+#include <dune/grid/io/file/dgfparser/dgfexception.hh>
+#include <dune/grid/io/file/vtk.hh>
+
+#include <dumux/assembly/fvassembler.hh>
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/dumuxmessage.hh>
+#include <dumux/linear/seqsolverbackend.hh>
+#include <dumux/nonlinear/newtonsolver.hh>
+
+#include <dumux/porenetworkflow/common/pnmvtkoutputmodule.hh>
+#include <dumux/porenetworkflow/common/boundaryflux.hh>
+#include <dumux/io/grid/porenetwork/gridmanager.hh>
+#include "properties.hh"
+
+int main(int argc, char** argv) try
+{
+    using namespace Dumux;
+
+    using TypeTag = Properties::TTag::PNMOnePNonCreepingProblem;
+
+    // initialize MPI, finalize is done automatically on exit
+    const auto& mpiHelper = Dune::MPIHelper::instance(argc, argv);
+
+    // print dumux start message
+    if (mpiHelper.rank() == 0)
+        DumuxMessage::print(/*firstCall=*/true);
+
+    ////////////////////////////////////////////////////////////
+    // parse the command line arguments and input file
+    ////////////////////////////////////////////////////////////
+
+    // parse command line arguments
+    Parameters::init(argc, argv);
+
+    //////////////////////////////////////////////////////////////////////
+    // try to create a grid (from the given grid file or the input file)
+    /////////////////////////////////////////////////////////////////////
+
+    using GridManager = Dumux::PoreNetworkGridManager<3>;
+    GridManager gridManager;
+    gridManager.init();
+
+    // we compute on the leaf grid view
+    const auto& leafGridView = gridManager.grid().leafGridView();
+    auto gridData = gridManager.getGridData();
+
+    // create the finite volume grid geometry
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    auto gridGeometry = std::make_shared<GridGeometry>(leafGridView);
+    gridGeometry->update(*gridData);
+
+    // the spatial parameters
+    using SpatialParams = GetPropType<TypeTag, Properties::SpatialParams>;
+    auto spatialParams = std::make_shared<SpatialParams>(gridGeometry);
+
+    // the problem (boundary conditions)
+    using Problem = GetPropType<TypeTag, Properties::Problem>;
+    auto problem = std::make_shared<Problem>(gridGeometry, spatialParams);
+
+    // the solution vector
+    using GridView = typename GridGeometry::GridView;
+    using SolutionVector = GetPropType<TypeTag, Properties::SolutionVector>;
+    SolutionVector x(leafGridView.size(GridView::dimension));
+    problem->applyInitialSolution(x);
+    auto xOld = x;
+
+    // the grid variables
+    using GridVariables = GetPropType<TypeTag, Properties::GridVariables>;
+    auto gridVariables = std::make_shared<GridVariables>(problem, gridGeometry);
+    gridVariables->init(x);
+
+    // make assemble and attach linear system
+    using Assembler = FVAssembler<TypeTag, DiffMethod::analytic>;
+    auto assembler = std::make_shared<Assembler>(problem, gridGeometry, gridVariables);
+    using JacobianMatrix = GetPropType<TypeTag, Properties::JacobianMatrix>;
+    auto A = std::make_shared<JacobianMatrix>();
+    auto r = std::make_shared<SolutionVector>();
+    assembler->setLinearSystem(A, r);
+
+    const auto boundaryFlux = PoreNetworkModelBoundaryFlux(*assembler, x);
+
+    Dune::Timer timer;
+    // assemble the local jacobian and the residual
+    Dune::Timer assemblyTimer;
+    if (mpiHelper.rank() == 0)
+        std::cout << "Assembling linear system ..." << std::flush;
+    assembler->assembleJacobianAndResidual(x);
+    assemblyTimer.stop();
+    if (mpiHelper.rank() == 0)
+        std::cout << " took " << assemblyTimer.elapsed() << " seconds." << std::endl;
+
+    // the linear solver
+    using LinearSolver = ILU0RestartedGMResBackend;
+    auto linearSolver = std::make_shared<LinearSolver>();
+
+    // the non-linear solver
+    using NewtonSolver = NewtonSolver<Assembler, LinearSolver>;
+    NewtonSolver nonLinearSolver(assembler, linearSolver);
+    nonLinearSolver.solve(x /*, *timeLoop*/);
+
+    // output result to vtk
+    Dune::Timer outputTimer;
+    if (mpiHelper.rank() == 0)
+        std::cout << "Writing result to file " << problem->name() << " ..." << std::flush;
+    using IOFields = GetPropType<TypeTag, Properties::IOFields>;
+    PNMVtkOutputModule<GridVariables, GetPropType<TypeTag, Properties::FluxVariables>, SolutionVector> vtkWriter(*gridVariables, x, problem->name());
+    IOFields::initOutputModule(vtkWriter);
+
+    vtkWriter.write(0.0);
+    outputTimer.stop();
+
+    std::cout << "cumulative outflux is: " << boundaryFlux.getFlux("max", 0, true) << std::endl;
+
+    if (mpiHelper.rank() == 0)
+        std::cout << " took " << outputTimer.elapsed() << " seconds." << std::endl;
+
+    timer.stop();
+
+    const auto &comm = Dune::MPIHelper::getCollectiveCommunication();
+    if (mpiHelper.rank() == 0)
+        std::cout << "Simulation took " << timer.elapsed() << " seconds on "
+                  << comm.size() << " processes.\n"
+                  << "The cumulative CPU time was " << timer.elapsed() * comm.size() << " seconds.\n";
+
+    if (mpiHelper.rank() == 0)
+        Parameters::print();
+
+    return 0;
+}
+catch (Dumux::ParameterException &e)
+{
+    std::cerr << std::endl
+              << e << " ---> Abort!" << std::endl;
+    return 1;
+}
+catch (Dune::DGFException &e)
+{
+    std::cerr << "DGF exception thrown (" << e << "). Most likely, the DGF file name is wrong "
+                                                  "or the DGF file is corrupted, "
+                                                  "e.g. missing hash at end of file or wrong number (dimensions) of entries."
+              << " ---> Abort!" << std::endl;
+    return 2;
+}
+catch (Dune::Exception &e)
+{
+    std::cerr << "Dune reported error: " << e << " ---> Abort!" << std::endl;
+    return 3;
+}
+catch (...)
+{
+    std::cerr << "Unknown exception thrown! ---> Abort!" << std::endl;
+    return 4;
+}
---
+++ test/porenetworkflow/1p/noncreepingflow/spatialparams.hh
@@ -0,0 +1,92 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \ingroup SpatialParameters
+ * \brief The base class for spatial parameters for pore network models.
+ */
+#ifndef DUMUX_PNM_NONCREEPING_SPATIAL_PARAMS_1P_HH
+#define DUMUX_PNM_NONCREEPING_SPATIAL_PARAMS_1P_HH
+
+#include <dumux/material/spatialparams/porenetwork/porenetworkbase.hh>
+#include <dumux/porenetworkflow/common/poreproperties.hh>
+#include <dumux/porenetworkflow/common/throatproperties.hh>
+
+namespace Dumux
+{
+
+template<class GridGeometry, class Scalar>
+class PNMNonCreepingSpatialParams : public PNMBaseSpatialParams<GridGeometry, Scalar, PNMNonCreepingSpatialParams<GridGeometry, Scalar>>
+{
+    using ParentType = PNMBaseSpatialParams<GridGeometry, Scalar, PNMNonCreepingSpatialParams<GridGeometry, Scalar>>;
+
+    using GridView = typename GridGeometry::GridView;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using FVElementGeometry = typename GridGeometry::LocalView;
+    using SubControlVolume = typename GridGeometry::SubControlVolume;
+
+public:
+    using PermeabilityType = Scalar;
+    using ParentType::ParentType;
+
+    template<class ElementSolutionVector>
+    auto poreShapeFactor(const Element& element,
+                         const SubControlVolume& scv,
+                         const ElementSolutionVector& elemSol) const
+    {
+        return Throat::shapeFactorCircle<Scalar>();
+    }
+
+    template<class ElementSolutionVector>
+    Scalar poreCrossSectionalArea(const Element& element,
+                                  const SubControlVolume& scv,
+                                  const ElementSolutionVector& elemSol) const
+    {
+        const auto poreRadius = this->gridGeometry().poreRadius(scv.dofIndex());
+        return poreRadius*poreRadius*M_PI;
+    }
+
+    template<class ElementSolutionVector>
+    Scalar poreLength(const Element& element,
+                      const SubControlVolume& scv,
+                      const ElementSolutionVector& elemSol) const
+    {
+        return this->gridGeometry().poreRadius(scv.dofIndex());
+    }
+
+    template<class ElementSolutionVector>
+    Scalar alpha(const Element& element,
+                 const SubControlVolume& scv,
+                 const ElementSolutionVector& elemSol) const
+    { return 1.0; }
+
+    template<class ElementSolutionVector>
+    Scalar kd(const Element& element,
+              const SubControlVolume& scv,
+              const ElementSolutionVector& elemSol) const
+    { return 1.0; }
+
+
+
+};
+
+} // namespace Dumux
+
+#endif
