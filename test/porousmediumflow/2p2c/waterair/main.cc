a10dc4ca8287ddc1de0274e30bac061d09468383
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-01-02 13:21:41 +0100

    [istlsolvers] Simplify new AMG solver impl by renaming class to AMGBiCGSTABIstlSolver

--- test/porousmediumflow/2p2c/waterair/main.cc
+++ test/porousmediumflow/2p2c/waterair/main.cc
@@ -32,7 +32,7 @@
 #include <dumux/common/parameters.hh>
 #include <dumux/common/dumuxmessage.hh>
 
-#include <dumux/linear/amgbackend.hh>
+#include <dumux/linear/istlsolvers.hh>
 #include <dumux/linear/linearsolvertraits.hh>
 #include <dumux/linear/algebratraits.hh>
 #include <dumux/nonlinear/newtonsolver.hh>
@@ -112,8 +112,8 @@ int main(int argc, char** argv)
     auto assembler = std::make_shared<Assembler>(problem, gridGeometry, gridVariables, timeLoop, xOld);
 
     // the linear solver
-    using LinearSolver = AMGBiCGSTABBackend<LinearSolverTraits<GridGeometry>,
-                                            LinearAlgebraTraitsFromAssembler<Assembler>>;
+    using LinearSolver = AMGBiCGSTABIstlSolver<LinearSolverTraits<GridGeometry>,
+                                               LinearAlgebraTraitsFromAssembler<Assembler>>;
     auto linearSolver = std::make_shared<LinearSolver>(leafGridView, gridGeometry->dofMapper());
 
     // the non-linear solver
