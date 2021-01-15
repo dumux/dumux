8e92ee0d992fa22a8d24f5ad0941a7c03c744f45
Ned Coltman <edward.coltman@iws.uni-stuttgart.de>
2021-01-15 10:29:35 +0100

    [facecentered][scvfs] Add method to shift scvf to corner for boundary eval

--- dumux/discretization/facecentered/staggered/subcontrolvolumeface.hh
+++ dumux/discretization/facecentered/staggered/subcontrolvolumeface.hh
@@ -59,6 +59,20 @@ struct FaceCenteredDefaultScvfGeometryTraits
     using Geometry = Dune::AxisAlignedCubeGeometry<Scalar, dim-1, dimWorld>;
 };
 
+/*!
+ * \ingroup FaceCenteredStaggeredDiscretization
+ * \brief Helper function to turn a given scvf into a fake boundary face with it's center at a new location
+ */
+template<class FaceCenteredStaggeredSubControlVolumeFace>
+FaceCenteredStaggeredSubControlVolumeFace makeFaceCenteredSCVF(const FaceCenteredStaggeredSubControlVolumeFace& scvf,
+                                                               const typename FaceCenteredStaggeredSubControlVolumeFace::GlobalPosition& newCenter)
+{
+    auto bf = scvf;
+    bf.setCenter(newCenter);
+    bf.setBoundary(true);
+    return bf;
+}
+
 template<class GridView, class T = FaceCenteredDefaultScvfGeometryTraits<GridView>>
 class FaceCenteredStaggeredSubControlVolumeFace
 {
@@ -180,6 +194,14 @@ public:
         inPlaneAxes.set(normalAxis_, false);
         return Geometry(corners_.front(), corners_.back(), inPlaneAxes);
     }
+
+    //! set the center to a different position
+    void setCenter(const GlobalPosition& center)
+    { center_ = center; }
+
+    //! set the boundary flag for psuedo faces on corners
+    void setBoundary(bool boundaryFlag)
+    { boundary_ = boundaryFlag; }
 
 private:
     GlobalPosition center_;
