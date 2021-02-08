db12174e9227e5c48a8c0a8bb64892121269d751
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2021-02-08 12:59:45 +0100

    [disc][fvelemvars] export local views

--- dumux/discretization/fvgridvariables.hh
+++ dumux/discretization/fvgridvariables.hh
@@ -51,12 +51,13 @@ class FVGridVariablesLocalView
     using GridView = typename GridGeometry::GridView;
     using Element = typename GridView::template Codim<0>::Entity;
 
-    using ElementVolumeVariables = typename GV::GridVolumeVariables::LocalView;
-    using ElementFluxVariablesCache = typename GV::GridFluxVariablesCache::LocalView;
-
 public:
     //! export corresponding grid-wide class
     using GridVariables = GV;
+
+    //! export underlying local views
+    using ElementVolumeVariables = typename GV::GridVolumeVariables::LocalView;
+    using ElementFluxVariablesCache = typename GV::GridFluxVariablesCache::LocalView;
 
     //! Constructor
     FVGridVariablesLocalView(const GridVariables& gridVariables)
