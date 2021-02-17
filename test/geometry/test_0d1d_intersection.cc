dumux/dumux

commit 95167ff69ad1eec38eb8ab0a7f948d1c63b4591d

[geometry][isectpointsimplex] improve robustness

dumux/geometry/intersectspointsimplex.hh
@@ -177,11 +177,26 @@ bool intersectsPointSimplex(const Dune::FieldVector<ctype, dimworld>& point,
     const ctype v1norm = v1.two_norm();
     const ctype v2norm = v2.two_norm();
 
-    // check if point and p0 are the same
+    // early exit if point and p0 are the same
     if (v2norm < v1norm*eps_)
         return true;
 
-    return (v1.dot(v2) > v1norm*v2norm*(1.0 - eps_) && v2norm < v1norm*(1.0 + eps_));
+    // early exit if the point is outside the segment (no epsilon in the
+    // first statement because we already did the above equality check)
+    if (v1.dot(v2) < 0.0 || v2norm > v1norm*(1.0 + eps_))
+        return false;
+
+    // If the area spanned by the 2 vectors is zero, the points are colinear.
+    // If that is the case, the given point is on the segment.
+    const auto n = crossProduct(v1, v2);
+    const auto eps2 = v1norm*v1norm*eps_;
+    if constexpr (dimworld == 3)
+        return n.two_norm2() < eps2*eps2;
+    else
+    {
+        using std::abs;
+        return abs(n) < eps2;
+    }
 }
 
 /*!

commit a2e950fbdcc74334f79a94d857e97aadffc178a1

[test][1d2d][intersection] Add corner case that fails without bugfix in 95167ff6

test/geometry/test_0d1d_intersection.cc
@@ -76,6 +76,13 @@ void testIntersections(std::vector<bool>& returns)
         GlobalPosition p11 = a2;
         p11[dimWorld-1] += (b2-a2).two_norm()*1.0e-6;
         returns.push_back(testIntersection(a2, b2, p11));
+
+        // test that triggers bug in 1d2d intersection query
+        // which has been fixed with !2274 (found a false positive here)
+        GlobalPosition p12(0.0); p12[0] = 0.001969*scaling; p12[1] = 0.0004995*scaling;
+        GlobalPosition c(0.0); c[1] = 0.0005*scaling;
+        GlobalPosition d = c; d[0] = 0.002*scaling;
+        returns.push_back(testIntersection(c, d, p12));
     }
 }
 