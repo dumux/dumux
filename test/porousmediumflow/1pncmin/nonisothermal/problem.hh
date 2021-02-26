37e5f05cb7fa0da1ea6b37ea904e724c693ea1fa
Yue Wang <yue.wang@iws.uni-stuttgart.de>
2021-02-27 00:17:19 +0100

    [test][1pncmin] Separate properties into their own header

--- test/porousmediumflow/1pncmin/nonisothermal/problem.hh
+++ test/porousmediumflow/1pncmin/nonisothermal/problem.hh
@@ -22,85 +22,19 @@
  * \brief Definition of a problem for thermochemical heat storage using \f$ \textnormal{CaO},
  * \textnormal{Ca} \left( \textnormal{OH} \right)_2\f$.
  */
-
 #ifndef DUMUX_THERMOCHEM_PROBLEM_HH
 #define DUMUX_THERMOCHEM_PROBLEM_HH
 
-#include <dune/grid/yaspgrid.hh>
-
-#include <dumux/porousmediumflow/1pncmin/model.hh>
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
 #include <dumux/common/boundarytypes.hh>
 
 #include <dumux/discretization/elementsolution.hh>
-#include <dumux/discretization/box.hh>
-#include <dumux/discretization/cctpfa.hh>
-#include <dumux/discretization/ccmpfa.hh>
 #include <dumux/porousmediumflow/problem.hh>
-#include <dumux/material/fluidsystems/1padapter.hh>
-#include <dumux/material/fluidsystems/h2on2.hh>
-#include <dumux/material/fluidmatrixinteractions/1p/thermalconductivityaverage.hh>
-#include <dumux/material/components/cao2h2.hh>
-#include <dumux/material/solidsystems/compositionalsolidphase.hh>
 
-#include "spatialparams.hh"
 #include "reaction.hh"
-#include "modifiedcao.hh"
 
 namespace Dumux {
-
-template <class TypeTag>
-class ThermoChemProblem;
-
-namespace Properties {
-// Create new type tags
-namespace TTag {
-struct ThermoChem { using InheritsFrom = std::tuple<OnePNCMinNI>; };
-struct ThermoChemBox { using InheritsFrom = std::tuple<ThermoChem, BoxModel>; };
-} // end namespace TTag
-
-// Set the grid type
-template<class TypeTag>
-struct Grid<TypeTag, TTag::ThermoChem> { using type = Dune::YaspGrid<2>; };
-// Set the problem property
-template<class TypeTag>
-struct Problem<TypeTag, TTag::ThermoChem> { using type = ThermoChemProblem<TypeTag>; };
-
-// The fluid system
-template<class TypeTag>
-struct FluidSystem<TypeTag, TTag::ThermoChem>
-{
-    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
-    using H2ON2 = FluidSystems::H2ON2<Scalar>;
-    static constexpr auto phaseIdx = H2ON2::gasPhaseIdx; // simulate the air phase
-    using type = FluidSystems::OnePAdapter<H2ON2, phaseIdx>;
-};
-
-template<class TypeTag>
-struct SolidSystem<TypeTag, TTag::ThermoChem>
-{
-    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
-    using ComponentOne = Components::ModifiedCaO<Scalar>;
-    using ComponentTwo = Components::CaO2H2<Scalar>;
-    using type = SolidSystems::CompositionalSolidPhase<Scalar, ComponentOne, ComponentTwo>;
-};
-
-// // Enable velocity output
-// template<class TypeTag>
-// struct VtkAddVelocity<TypeTag, TTag::ThermoChem> { static constexpr bool value = false; };
-
-// Set the spatial parameters
-template<class TypeTag>
-struct SpatialParams<TypeTag, TTag::ThermoChem>
-{
-    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
-    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
-    using type = ThermoChemSpatialParams<GridGeometry, Scalar>;
-};
-
-// Define whether mole(true) or mass (false) fractions are used
-template<class TypeTag>
-struct UseMoles<TypeTag, TTag::ThermoChem> { static constexpr bool value = true; };
-}
 
 /*!
  * \ingroup OnePNCMinTests
