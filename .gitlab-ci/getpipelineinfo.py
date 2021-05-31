90dd015618309aa189348eb13859e0e2e5ef6ef5
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2021-05-31 09:53:26 +0200

    [ci][getpipelineinfo] allow exlusion of pls with certain jobs

--- .gitlab-ci/getpipelineinfo.py
+++ .gitlab-ci/getpipelineinfo.py
@@ -11,11 +11,36 @@ except Exception:
     sys.exit('Could not import common module')
 
 
-def findPipeline(pipelineFile, predicate):
-    with open(pipelineFile) as pipeLines:
-        for pipeLine in json.load(pipeLines):
-            if predicate(pipeLine):
-                return pipeLine
+def performApiQuery(command, err='API query unsuccessful'):
+    return runCommand(command, suppressTraceBack=True, errorMessage=err)
+
+
+def getPipeLinesApiURL(apiURL):
+    return apiURL.rstrip('/') + '/pipelines/'
+
+
+def getPipelines(apiURL, token, filter=''):
+    queryURL = getPipeLinesApiURL(apiURL) + filter
+    queryCmd = 'curl --header "token={}" "{}"'.format(token, queryURL)
+    pl = performApiQuery(queryCmd, 'Could not retrieve pipelines')
+    return json.loads(pl)
+
+
+def getPipelineInfo(apiURL, token, pipeLineId, infoString):
+    queryURL = getPipeLinesApiURL(apiURL) + str(pipeLineId) + '/' + infoString
+    queryCmd = 'curl --header "token={}" "{}"'.format(token, queryURL)
+    pl = performApiQuery(queryCmd, 'Could not retrieve pipeline info')
+    return json.loads(pl)
+
+
+def getPipeLineJobs(apiURL, token, pipeLineId):
+    return getPipelineInfo(apiURL, token, pipeLineId, 'jobs')
+
+
+def findPipeline(pipeLines, predicate):
+    for pipeLine in pipeLines:
+        if predicate(pipeLine):
+            return pipeLine
     return None
 
 
@@ -23,19 +48,31 @@ parser = ArgumentParser(
     description='Find and print information on a previously run pipeline'
 )
 
-parser.add_argument('-s', '--status',
-                    required=False, default='success',
-                    help='Define the status of the pipeline to be found')
 parser.add_argument('-p', '--print-format',
                     required=True, choices=['pipeline-id', 'commit-sha'],
                     help='Switch between reporting the pipeline-id/commit-sha')
 parser.add_argument('-l', '--look-for',
                     required=True, choices=['latest', 'HEAD'],
                     help='Define how to search for pipelines')
-parser.add_argument('-f', '--status-file',
+parser.add_argument('-t', '--access-token',
                     required=True,
-                    help='The .json file with the latest pipeline statuses')
+                    help='The token to post read requests to the GitLab API')
+parser.add_argument('-u', '--project-api-url',
+                    required=False,
+                    default='https://git.iws.uni-stuttgart.de/api/v4/projects/31/',
+                    help='The token to post read requests to the GitLab API')
+parser.add_argument('-f', '--filter',
+                    required=False, default='?status=success',
+                    help='Pipeline query filter (default: "?status=success"')
+parser.add_argument('-e', '--exclude-jobs',
+                    required=False, nargs='*',
+                    default=['check-pipeline-status'],
+                    help='Exclude pipelines that contain the given jobs')
 args = vars(parser.parse_args())
+
+apiURL = args['project_api_url']
+token = args['access_token']
+pipeLines = getPipelines(apiURL, token, args['filter'])
 
 currentBranch = runCommand('git branch --show-current').strip('\n')
 currentCommitInfo = runCommand('git show HEAD').split('\n')
@@ -44,25 +81,34 @@ headIsMergeCommit = 'Merge:' in currentCommitInfo[1]
 headSHA = runCommand('git rev-list HEAD --max-count=1').strip('\n')
 if headIsMergeCommit:
     preSHA = runCommand('git rev-list HEAD --max-count=2').split('\n')[1]
-    mrSha = currentCommitInfo[1].split()[2]
+    mrSHA = currentCommitInfo[1].split()[2]
 
 
-def checkCommitSHA(pipeLine):
+def checkBranch(pipeLine):
+    return pipeLine['ref'] == currentBranch
+
+
+def checkCommit(pipeLine):
     sha = pipeLine['sha']
     if headIsMergeCommit:
-        return sha == headSHA or sha == preSHA or mrSha in sha
+        return sha == headSHA or sha == preSHA or mrSHA in sha
     return sha == headSHA
 
 
-def checkStatus(pipeLine): return pipeLine['status'] == args['status']
-def checkBranch(pipeLine): return pipeLine['ref'] == currentBranch
-def find(predicate): return findPipeline(args['status_file'], predicate)
+def skip(pipeLine):
+    jobs = getPipeLineJobs(apiURL, token, pipeLine['id'])
+    jobNames = [j['name'] for j in jobs]
+    return any(j in jobNames for j in args['exclude_jobs'])
 
 
 if args['look_for'] == 'HEAD':
-    pipeLine = find(lambda p: checkStatus(p) and checkCommitSHA(p))
+    pipeLine = findPipeline(
+        pipeLines, lambda p: checkCommit(p) and not skip(p)
+    )
 elif args['look_for'] == 'latest':
-    pipeLine = find(lambda p: checkStatus(p) and checkBranch(p))
+    pipeLine = findPipeline(
+        pipeLines, lambda p: checkBranch(p) and not skip(p)
+    )
 
 if pipeLine is not None:
     if args['print_format'] == 'pipeline-id':
