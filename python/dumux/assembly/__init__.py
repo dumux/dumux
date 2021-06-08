8cac9413206b887732ed6c21bdb0040789b0966f
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2021-06-08 17:36:05 +0200

    [python] Add FVAssembler

---
+++ python/dumux/assembly/__init__.py
@@ -0,0 +1,26 @@
+from dune.generator.generator import SimpleGenerator
+from dune.common.hashit import hashIt
+
+from dune.generator.generator import SimpleGenerator
+from dune.common.hashit import hashIt
+
+def FVAssembler(problem, gridVariables, model, diffMethod, isImplicit=True):
+
+    TypeTag = model.getTypeTag()
+
+    if diffMethod == 'numeric':
+        dm = 'Dumux::DiffMethod::numeric'
+    elif diffMethod == 'analytic':
+        dm = 'Dumux::DiffMethod::analytic'
+    else:
+        raise ValueError("Unknown diffMethod {}".format(diffMethod))
+
+    assemblerType = "Dumux::FVAssembler<{}, {}>".format(TypeTag, dm, int(isImplicit))
+    includes = problem._includes + problem.gridGeometry()._includes + ["dumux/assembly/fvassembler.hh"]
+    includes += ["dumux/python/assembly/fvassembler.hh"]
+
+    moduleName = "fvassembler_" + hashIt(assemblerType)
+    holderType = "std::shared_ptr<{}>".format(assemblerType)
+    generator = SimpleGenerator("FVAssembler", "Dumux::Python")
+    module = generator.load(includes, assemblerType, moduleName, options=[holderType], preamble=model.getProperties())
+    return module.FVAssembler(problem, problem.gridGeometry(), gridVariables)
