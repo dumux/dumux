3d956fbc296f8fd21e0cee86828cf140e8476886
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2021-06-10 15:53:32 +0200

    [python] Add gridvars

--- python/dumux/discretization/__init__.py
+++ python/dumux/discretization/__init__.py
@@ -3,7 +3,7 @@ from dune.common.hashit import hashIt
 
 # construct a GridGeometry from a gridView
 # the grid geometry is JIT compiled
-def GridGeometry(gridView, discMethod="cctpfa"):
+def GridGeometry(*, gridView, discMethod="cctpfa"):
     includes = gridView._includes + ["dumux/python/discretization/gridgeometry.hh"]
 
     if discMethod == "cctpfa":
@@ -20,3 +20,29 @@ def GridGeometry(gridView, discMethod="cctpfa"):
     generator = SimpleGenerator("GridGeometry", "Dumux::Python")
     module = generator.load(includes, typeName, moduleName, options=[holderType])
     return module.GridGeometry(gridView)
+
+
+# construct GridVariables
+# the grid variables is JIT compiled
+def GridVariables(*, problem, model):
+    includes = [
+        "dumux/discretization/fvgridvariables.hh",
+        "dumux/python/discretization/gridvariables.hh",
+    ]
+    GG = "Dumux::GetPropType<{}, Dumux::Properties::GridGeometry>".format(model.getTypeTag())
+    GVV = "Dumux::GetPropType<{}, Dumux::Properties::GridVolumeVariables>".format(
+        model.getTypeTag()
+    )
+    GFC = "Dumux::GetPropType<{}, Dumux::Properties::GridFluxVariablesCache>".format(
+        model.getTypeTag()
+    )
+    typeName = "Dumux::FVGridVariables<{}, {}, {}>".format(GG, GVV, GFC)
+
+    moduleName = "gridvariables_" + hashIt(typeName)
+    holderType = "std::shared_ptr<{}>".format(typeName)
+    generator = SimpleGenerator("GridVariables", "Dumux::Python")
+    module = generator.load(
+        includes, typeName, moduleName, options=[holderType], preamble=model.getProperties()
+    )
+    module.GridVariables._model = property(lambda self: model)
+    return module.GridVariables(problem, problem.gridGeometry())
