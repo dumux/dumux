824aabe2bf10f7fa0976d20c1e64cc66761d6184
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-06-15 20:44:40 +0200

    [md][boundary] Port Darcy-Stokes convergence test to new staggered

---
+++ test/multidomain/boundary/freeflowporousmedium/1p_1p/convergence/convergencetest.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+from math import *
+import subprocess
+import sys
+
+if len(sys.argv) < 2:
+    sys.stderr.write('Please provide a single argument <testname> to the script\n')
+    sys.exit(1)
+
+executableName = str(sys.argv[1])
+testargs = [str(i) for i in sys.argv][2:]
+testname = testargs[testargs.index('-Problem.TestCase')+1]
+
+# remove the old log files
+subprocess.call(['rm', testname + '_freeFlow.log'])
+print("Removed old log file ({})!".format(testname + '_freeFlow.log'))
+subprocess.call(['rm', testname + '_darcy.log'])
+print("Removed old log file ({})!".format(testname + '_darcy.log'))
+
+# do the runs with different refinement
+for i in [0, 1, 2]:
+    subprocess.call(['./' + executableName] + testargs + ['-Grid.Refinement', str(i)])
+
+def checkRatesFreeFlow():
+    # check the rates and append them to the log file
+    with open(testname + '_freeFlow.log', "r+") as logfile:
+
+        errorP, errorVx, errorVy = [], [], []
+        for line in logfile:
+            line = line.strip("\n").strip("\[ConvergenceTest\]").split()
+            errorP.append(float(line[2]))
+            errorVx.append(float(line[5]))
+            errorVy.append(float(line[8]))
+
+        resultsP, resultsVx, resultsVy = [], [], []
+        logfile.truncate(0)
+        logfile.write("n\terrorP\t\trateP\t\terrorVx\t\trateVx\t\terrorVy\t\trateVy\n")
+        logfile.write("-"*50 + "\n")
+        for i in range(len(errorP)-1):
+            if isnan(errorP[i]) or isinf(errorP[i]):
+                continue
+            if not ((errorP[i] < 1e-12 or errorP[i+1] < 1e-12) and (errorVx[i] < 1e-12 or errorVx[i+1] < 1e-12) and (errorVy[i] < 1e-12 or errorVy[i+1] < 1e-12)):
+                rateP = (log(errorP[i])-log(errorP[i+1]))/log(2)
+                rateVx = (log(errorVx[i])-log(errorVx[i+1]))/log(2)
+                rateVy = (log(errorVy[i])-log(errorVy[i+1]))/log(2)
+                message = "{}\t{:0.4e}\t{:0.4e}\t{:0.4e}\t{:0.4e}\t{:0.4e}\t{:0.4e}\n".format(i, errorP[i], rateP,  errorVx[i], rateVx, errorVy[i], rateVy)
+                logfile.write(message)
+                resultsP.append(rateP)
+                resultsVx.append(rateVx)
+                resultsVy.append(rateVy)
+            else:
+                logfile.write("error: exact solution!?")
+        i = len(errorP)-1
+        message = "{}\t{:0.4e}\t\t{}\t{:0.4e}\t\t{}\t{:0.4e}\t\t{}\n".format(i, errorP[i], "",  errorVx[i], "", errorVy[i], "")
+        logfile.write(message)
+
+    print("\nComputed the following convergence rates for {}:\n".format(testname))
+
+    subprocess.call(['cat', testname + '_freeFlow.log'])
+
+    return {"p" : resultsP, "v_x" : resultsVx, "v_y" : resultsVy}
+
+def checkRatesDarcy():
+    # check the rates and append them to the log file
+    with open(testname + '_darcy.log', "r+") as logfile:
+
+        errorP = []
+        for line in logfile:
+            line = line.strip("\n")
+            line = line.strip("\[ConvergenceTest\]")
+            line = line.split()
+            errorP.append(float(line[2]))
+
+        resultsP = []
+        logfile.truncate(0)
+        logfile.write("n\terrorP\t\trateP\n")
+        logfile.write("-"*50 + "\n")
+        for i in range(len(errorP)-1):
+            if isnan(errorP[i]) or isinf(errorP[i]):
+                continue
+            if not ((errorP[i] < 1e-12 or errorP[i+1] < 1e-12)):
+                rateP = (log(errorP[i])-log(errorP[i+1]))/log(2)
+                message = "{}\t{:0.4e}\t{:0.4e}\n".format(i, errorP[i], rateP)
+                logfile.write(message)
+                resultsP.append(rateP)
+            else:
+                logfile.write("error: exact solution!?")
+        i = len(errorP)-1
+        message = "{}\t{:0.4e}\n".format(i, errorP[i], "")
+        logfile.write(message)
+
+    print("\nComputed the following convergence rates for {}:\n".format(testname))
+
+    subprocess.call(['cat', testname + '_darcy.log'])
+
+    return {"p" : resultsP}
+
+def checkRatesFreeFlowAndDarcy():
+    resultsFreeFlow = checkRatesFreeFlow()
+    resultsDarcy = checkRatesDarcy()
+
+    def mean(numbers):
+        return float(sum(numbers)) / len(numbers)
+
+    # check the rates, we expect rates around 2
+    if mean(resultsFreeFlow["p"]) < 2.05 and mean(resultsFreeFlow["p"]) < 1.7:
+        sys.stderr.write("*"*70 + "\n" + "The convergence rates for pressure were not close enough to 2! Test failed.\n" + "*"*70 + "\n")
+        sys.exit(1)
+
+    if mean(resultsFreeFlow["v_x"]) < 2.05 and mean(resultsFreeFlow["v_x"]) < 1.95:
+        sys.stderr.write("*"*70 + "\n" + "The convergence rates for x-velocity were not close enough to 2! Test failed.\n" + "*"*70 + "\n")
+        sys.exit(1)
+
+    if mean(resultsFreeFlow["v_y"]) < 2.05 and mean(resultsFreeFlow["v_y"]) < 1.95:
+        sys.stderr.write("*"*70 + "\n" + "The convergence rates for y-velocity were not close enough to 2! Test failed.\n" + "*"*70 + "\n")
+        sys.exit(1)
+
+    if mean(resultsDarcy["p"]) < 2.05 and mean(resultsDarcy["p"]) < 1.95:
+        sys.stderr.write("*"*70 + "\n" + "The convergence rates for pressure were not close enough to 2! Test failed.\n" + "*"*70 + "\n")
+        sys.exit(1)
+
+
+checkRatesFreeFlowAndDarcy()
---
+++ test/multidomain/boundary/freeflowporousmedium/1p_1p/convergence/main.cc
@@ -0,0 +1,344 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup BoundaryTests
+ * \brief A convergence test for the coupled FreeFlow/Darcy problem (1p).
+ */
+
+#include <config.h>
+
+#include <iostream>
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/timer.hh>
+
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/dumuxmessage.hh>
+#include <dumux/linear/seqsolverbackend.hh>
+#include <dumux/assembly/fvassembler.hh>
+#include <dumux/discretization/method.hh>
+#include <dumux/io/format.hh>
+#include <dumux/io/vtkoutputmodule.hh>
+#include <dumux/io/grid/gridmanager_yasp.hh>
+
+#include <dumux/multidomain/staggeredtraits.hh>
+#include <dumux/multidomain/fvassembler.hh>
+#include <dumux/multidomain/newtonsolver.hh>
+#include <dumux/freeflow/navierstokes/velocityoutput.hh>
+#include <test/freeflow/navierstokes/l2error.hh>
+
+#include "testcase.hh"
+#include "properties.hh"
+
+/*!
+ * \brief Creates analytical solution vector for output
+ * Returns a tuple of the analytical solution for the velocity at the faces
+ * \param problem the problem for which to evaluate the analytical solution
+ */
+template<class Scalar, class Problem>
+auto createFaceCenterAnalyticalSolution(const Problem& problem)
+{
+    const auto& gridGeometry = problem.gridGeometry();
+    std::vector<Scalar> analyticalVelocityOnFace(gridGeometry.numDofs());
+
+    using Indices = typename Problem::Indices;
+    for (const auto& element : elements(gridGeometry.gridView()))
+    {
+        auto fvGeometry = localView(gridGeometry);
+        fvGeometry.bindElement(element);
+
+        // velocities on faces
+        for (const auto& scv : scvs(fvGeometry))
+        {
+            const auto faceDofIdx = scv.dofIndex();
+            const auto faceDofPosition = scv.center();
+            const auto dirIdx = scv.dofAxis();
+            const auto analyticalSolutionAtFace = problem.analyticalSolution(faceDofPosition);
+            analyticalVelocityOnFace[faceDofIdx] = analyticalSolutionAtFace[Indices::velocity(dirIdx)];
+        }
+    }
+
+    return analyticalVelocityOnFace;
+}
+
+/*!
+* \brief Creates analytical solution vector for output
+* Returns a tuple of the analytical solution for the pressure and velocity at cell centers
+* \param problem the problem for which to evaluate the analytical solution
+*/
+template<class Scalar, class Problem>
+auto createCellCenterAnalyticalSolution(const Problem& problem)
+{
+    const auto& gridGeometry = problem.gridGeometry();
+    using GridView = typename std::decay_t<decltype(gridGeometry)>::GridView;
+
+    static constexpr auto dim = GridView::dimension;
+    static constexpr auto dimWorld = GridView::dimensionworld;
+
+    using VelocityVector = Dune::FieldVector<Scalar, dimWorld>;
+
+    std::vector<Scalar> analyticalPressure;
+    std::vector<VelocityVector> analyticalVelocity;
+
+    analyticalPressure.resize(gridGeometry.numDofs());
+    analyticalVelocity.resize(gridGeometry.numDofs());
+
+    for (const auto& element : elements(gridGeometry.gridView()))
+    {
+        auto fvGeometry = localView(gridGeometry);
+        fvGeometry.bindElement(element);
+        for (auto&& scv : scvs(fvGeometry))
+        {
+            const auto ccDofIdx = scv.dofIndex();
+            const auto ccDofPosition = scv.dofPosition();
+            const auto analyticalSolutionAtCc = problem.fullAnalyticalSolution(ccDofPosition);
+            analyticalPressure[ccDofIdx] = analyticalSolutionAtCc[dim];
+
+            for (int dirIdx = 0; dirIdx < dim; ++dirIdx)
+                analyticalVelocity[ccDofIdx][dirIdx] = analyticalSolutionAtCc[dirIdx];
+        }
+    }
+
+    return std::make_tuple(analyticalPressure, analyticalVelocity);
+}
+
+template<class MassP, class MomP, class MassV, class MomV>
+void printFreeFlowL2Error(const MassP& massProblem, const MomP& momentumProblem,
+                          const MassV& massSol, const MomV& momSol)
+{
+    using namespace Dumux;
+
+    const auto [l2errorAbsVelocity, l2errorRelVelocity] = calculateL2Error(momentumProblem, momSol);
+    const auto [l2errorAbsPressure, l2errorRelPressure] = calculateL2Error(massProblem, massSol);
+    const int numCellCenterDofs = massProblem.gridGeometry().numDofs();
+    const int numFaceDofs = momentumProblem.gridGeometry().numDofs();
+    std::cout << Fmt::format("** L2 error (abs/rel) for {} cc dofs and {} face dofs (total: {}): ",
+                             numCellCenterDofs, numFaceDofs, numCellCenterDofs + numFaceDofs)
+              << Fmt::format("L2(p) = {:.8e} / {:.8e}", l2errorAbsPressure[0], l2errorRelPressure[0])
+              << Fmt::format(", L2(vx) = {:.8e} / {:.8e}", l2errorAbsVelocity[0], l2errorRelVelocity[0])
+              << Fmt::format(", L2(vy) = {:.8e} / {:.8e}", l2errorAbsVelocity[1], l2errorRelVelocity[1])
+              << std::endl;
+
+    // write the norm into a log file
+    std::ofstream logFile(massProblem.name() + ".log", std::ios::app);
+    logFile << "[ConvergenceTest] L2(p) = " << l2errorAbsPressure[0]
+            << " L2(vx) = " << l2errorAbsVelocity[0]
+            << " L2(vy) = " << l2errorAbsVelocity[1]
+            << std::endl;
+}
+
+template<class Problem, class SolutionVector>
+void printDarcyL2Error(const Problem& problem, const SolutionVector& x)
+{
+    using namespace Dumux;
+    using Scalar = double;
+
+    Scalar l2error = 0.0;
+    for (const auto& element : elements(problem.gridGeometry().gridView()))
+    {
+        auto fvGeometry = localView(problem.gridGeometry());
+        fvGeometry.bindElement(element);
+
+        for (auto&& scv : scvs(fvGeometry))
+        {
+            const auto dofIdx = scv.dofIndex();
+            const Scalar delta = x[dofIdx] - problem.fullAnalyticalSolution(scv.center())[2/*pressureIdx*/];
+            l2error += scv.volume()*(delta*delta);
+        }
+    }
+    using std::sqrt;
+    l2error = sqrt(l2error);
+
+    const auto numDofs = problem.gridGeometry().numDofs();
+    std::cout << Fmt::format("** L2 error (abs) for {} cc dofs", numDofs)
+              << Fmt::format("L2 error = {:.8e}", l2error)
+              << std::endl;
+
+    // write the norm into a log file
+    std::ofstream logFile(problem.name() + ".log", std::ios::app);
+    logFile << "[ConvergenceTest] L2(p) = " << l2error << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    using namespace Dumux;
+
+    // initialize MPI, finalize is done automatically on exit
+    const auto& mpiHelper = Dune::MPIHelper::instance(argc, argv);
+
+    // print dumux start message
+    if (mpiHelper.rank() == 0)
+        DumuxMessage::print(/*firstCall=*/true);
+
+    // parse command line arguments and input file
+    Parameters::init(argc, argv);
+
+    // Define the sub problem type tags
+    using FreeFlowMomentumTypeTag = Properties::TTag::FreeFlowOnePMomentum;
+    using FreeFlowMassTypeTag = Properties::TTag::FreeFlowOnePMass;
+    using DarcyTypeTag = Properties::TTag::DarcyOneP;
+
+    // try to create a grid (from the given grid file or the input file)
+    // for both sub-domains
+    using DarcyGridManager = Dumux::GridManager<GetPropType<DarcyTypeTag, Properties::Grid>>;
+    DarcyGridManager darcyGridManager;
+    darcyGridManager.init("Darcy"); // pass parameter group
+
+    using FreeFlowGridManager = Dumux::GridManager<GetPropType<FreeFlowMomentumTypeTag, Properties::Grid>>;
+    FreeFlowGridManager freeFlowGridManager;
+    freeFlowGridManager.init("FreeFlow"); // pass parameter group
+
+    // we compute on the leaf grid view
+    const auto& darcyGridView = darcyGridManager.grid().leafGridView();
+    const auto& freeFlowGridView = freeFlowGridManager.grid().leafGridView();
+
+    // create the finite volume grid geometry
+    using FreeFlowMomentumFVGridGeometry = GetPropType<FreeFlowMomentumTypeTag, Properties::GridGeometry>;
+    auto freeFlowMomentumGridGeometry = std::make_shared<FreeFlowMomentumFVGridGeometry>(freeFlowGridView);
+    freeFlowMomentumGridGeometry->update();
+    using FreeFlowMassFVGridGeometry = GetPropType<FreeFlowMassTypeTag, Properties::GridGeometry>;
+    auto freeFlowMassGridGeometry = std::make_shared<FreeFlowMassFVGridGeometry>(freeFlowGridView);
+    freeFlowMassGridGeometry->update();
+    using DarcyFVGridGeometry = GetPropType<DarcyTypeTag, Properties::GridGeometry>;
+    auto darcyGridGeometry = std::make_shared<DarcyFVGridGeometry>(darcyGridView);
+    darcyGridGeometry->update();
+
+    using Traits = MultiDomainTraits<FreeFlowMomentumTypeTag, FreeFlowMassTypeTag, DarcyTypeTag>;
+    using CouplingManager = FreeFlowPorousMediumCouplingManager<Traits>;
+    auto couplingManager = std::make_shared<CouplingManager>();
+
+    // the indices
+    constexpr auto freeFlowMomentumIndex = CouplingManager::freeFlowMomentumIndex;
+    constexpr auto freeFlowMassIndex = CouplingManager::freeFlowMassIndex;
+    constexpr auto porousMediumIndex = CouplingManager::porousMediumIndex;
+
+    // the problem (initial and boundary conditions)
+    const auto testCaseName = getParam<std::string>("Problem.TestCase");
+    const auto testCase = [&]()
+    {
+        if (testCaseName == "ShiueExampleOne")
+            return DarcyStokesTestCase::ShiueExampleOne;
+        else if (testCaseName == "ShiueExampleTwo")
+            return DarcyStokesTestCase::ShiueExampleTwo;
+        else if (testCaseName == "Rybak")
+            return DarcyStokesTestCase::Rybak;
+        else if (testCaseName == "Schneider")
+            return DarcyStokesTestCase::Schneider;
+        else
+            DUNE_THROW(Dune::InvalidStateException, testCaseName + " is not a valid test case");
+    }();
+
+    // the problem (initial and boundary conditions)
+    using FreeFlowMomentumProblem = GetPropType<FreeFlowMomentumTypeTag, Properties::Problem>;
+    auto freeFlowMomentumProblem = std::make_shared<FreeFlowMomentumProblem>(freeFlowMomentumGridGeometry, couplingManager, testCase, testCaseName);
+    using FreeFlowMassProblem = GetPropType<FreeFlowMassTypeTag, Properties::Problem>;
+    auto freeFlowMassProblem = std::make_shared<FreeFlowMassProblem>(freeFlowMassGridGeometry, couplingManager, testCase, testCaseName);
+    using DarcyProblem = GetPropType<DarcyTypeTag, Properties::Problem>;
+    auto spatialParams = std::make_shared<typename DarcyProblem::SpatialParams>(darcyGridGeometry, testCase);
+    auto darcyProblem = std::make_shared<DarcyProblem>(darcyGridGeometry, couplingManager, spatialParams, testCase, testCaseName);
+
+    // the solution vector
+    Traits::SolutionVector sol;
+    sol[freeFlowMomentumIndex].resize(freeFlowMomentumGridGeometry->numDofs());
+    sol[freeFlowMassIndex].resize(freeFlowMassGridGeometry->numDofs());
+    sol[porousMediumIndex].resize(darcyGridGeometry->numDofs());
+
+    // the grid variables
+    using FreeFlowMomentumGridVariables = GetPropType<FreeFlowMomentumTypeTag, Properties::GridVariables>;
+    auto freeFlowMomentumGridVariables = std::make_shared<FreeFlowMomentumGridVariables>(freeFlowMomentumProblem, freeFlowMomentumGridGeometry);
+    using DarcyGridVariables = GetPropType<DarcyTypeTag, Properties::GridVariables>;
+    using FreeFlowMassGridVariables = GetPropType<FreeFlowMassTypeTag, Properties::GridVariables>;
+    auto freeFlowMassGridVariables = std::make_shared<FreeFlowMassGridVariables>(freeFlowMassProblem, freeFlowMassGridGeometry);
+    using DarcyGridVariables = GetPropType<DarcyTypeTag, Properties::GridVariables>;
+    auto darcyGridVariables = std::make_shared<DarcyGridVariables>(darcyProblem, darcyGridGeometry);
+
+    couplingManager->init(freeFlowMomentumProblem, freeFlowMassProblem, darcyProblem,
+                          std::make_tuple(freeFlowMomentumGridVariables, freeFlowMassGridVariables, darcyGridVariables),
+                          sol);
+
+    freeFlowMomentumGridVariables->init(sol[freeFlowMomentumIndex]);
+    freeFlowMassGridVariables->init(sol[freeFlowMassIndex]);
+    darcyGridVariables->init(sol[porousMediumIndex]);
+
+    // initialize the vtk output module
+    VtkOutputModule freeflowVtkWriter(*freeFlowMassGridVariables, sol[freeFlowMassIndex], freeFlowMassProblem->name());
+    GetPropType<FreeFlowMassTypeTag, Properties::IOFields>::initOutputModule(freeflowVtkWriter); // Add model specific output fields
+    freeflowVtkWriter.addVelocityOutput(std::make_shared<NavierStokesVelocityOutput<FreeFlowMassGridVariables>>());
+    const auto freeFlowAnalyticalSolutionCC = createCellCenterAnalyticalSolution<double>(*freeFlowMassProblem);
+    freeflowVtkWriter.addField(std::get<0>(freeFlowAnalyticalSolutionCC), "pressureExact");
+    freeflowVtkWriter.addField(std::get<1>(freeFlowAnalyticalSolutionCC), "velocityExact");
+    freeflowVtkWriter.write(0.0);
+
+    VtkOutputModule darcyVtkWriter(*darcyGridVariables, sol[porousMediumIndex],  darcyProblem->name());
+    GetPropType<DarcyTypeTag, Properties::IOFields>::initOutputModule(darcyVtkWriter);
+    darcyVtkWriter.addVelocityOutput(std::make_shared<GetPropType<DarcyTypeTag, Properties::VelocityOutput>>(*darcyGridVariables));
+    const auto darcyAnalyticalSolution = createCellCenterAnalyticalSolution<double>(*darcyProblem);
+    darcyVtkWriter.addField(std::get<0>(darcyAnalyticalSolution), "pressureExact");
+    darcyVtkWriter.addField(std::get<1>(darcyAnalyticalSolution), "velocityExact");
+    darcyVtkWriter.write(0.0);
+
+    // the assembler for a stationary problem
+    using Assembler = MultiDomainFVAssembler<Traits, CouplingManager, DiffMethod::numeric>;
+    auto assembler = std::make_shared<Assembler>(std::make_tuple(freeFlowMomentumProblem, freeFlowMassProblem, darcyProblem),
+                                                 std::make_tuple(freeFlowMomentumGridGeometry,
+                                                                 freeFlowMassGridGeometry,
+                                                                 darcyGridGeometry),
+                                                 std::make_tuple(freeFlowMomentumGridVariables,
+                                                                 freeFlowMassGridVariables,
+                                                                 darcyGridVariables),
+                                                 couplingManager);
+
+    // the linear solver
+    using LinearSolver = UMFPackBackend;
+    auto linearSolver = std::make_shared<LinearSolver>();
+
+    // the non-linear solver
+    using NewtonSolver = MultiDomainNewtonSolver<Assembler, LinearSolver, CouplingManager>;
+    NewtonSolver nonLinearSolver(assembler, linearSolver, couplingManager);
+
+    std::cout << "NumDofs Face " << freeFlowMomentumGridGeometry->numDofs() << std::endl;
+    std::cout << "NumDofs CC " << freeFlowMassGridGeometry->numDofs() << std::endl;
+    std::cout << "NumDofs Darcy " << darcyGridGeometry->numDofs() << std::endl;
+
+    // solve the non-linear system
+    nonLinearSolver.solve(sol);
+
+    // write vtk output
+    freeflowVtkWriter.write(1.0);
+    darcyVtkWriter.write(1.0);
+
+    printFreeFlowL2Error(*freeFlowMassProblem, *freeFlowMomentumProblem,
+                         sol[freeFlowMassIndex], sol[freeFlowMomentumIndex]);
+    printDarcyL2Error(*darcyProblem, sol[porousMediumIndex]);
+
+    ////////////////////////////////////////////////////////////
+    // finalize, print dumux message to say goodbye
+    ////////////////////////////////////////////////////////////
+
+    // print dumux end message
+    if (mpiHelper.rank() == 0)
+    {
+        Parameters::print();
+        DumuxMessage::print(/*firstCall=*/false);
+    }
+
+    return 0;
+}
