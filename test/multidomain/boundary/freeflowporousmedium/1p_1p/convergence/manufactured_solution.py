4ca3a966c3e79e895134112f344c34fc8f218db7
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-06-15 20:44:40 +0200

    [md][boundary] Port Darcy-Stokes convergence test to new staggered

---
+++ test/multidomain/boundary/freeflowporousmedium/1p_1p/convergence/manufactured_solution.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+"""
+This script determines the source terms needed for analytical solutions for coupled Stokes/Darcy problems.
+Given an analytical solution, it evaluates the momentum and mass balance equations and outputs source terms
+such that the balance equations are fulfilled. It furthermore calculates the Darcy velocity from a given pressure solution.
+"""
+
+import sympy as sp
+import numpy as np
+from sympy import *
+
+# case = "Rybak"
+#case = "Shiue_1"
+case = "Schneider"
+
+# divergence of a matrix
+def div(M):
+    return np.array([sp.diff(M[0,0],x) + sp.diff(M[0,1],y) , sp.diff(M[1,0],x) + sp.diff(M[1,1],y) ])
+
+# gradient of a vector
+def grad(v):
+    return np.array([[sp.diff(v[0],x),  sp.diff(v[0],y)], [sp.diff(v[1],x),  sp.diff(v[1],y)]])
+
+# coordinates
+x, y = sp.symbols("x y")
+
+# analytical solutions for v and p in free-flow domain (see reference given in problems)
+def analyticalSolutionStokes(case):
+    if case == "Rybak":
+        vFF = np.array([-sp.cos(sp.pi*x)*sp.sin(sp.pi*y), sp.sin(sp.pi*x)*sp.cos(sp.pi*y)])
+        pFF = 0.5*y*sp.sin(sp.pi*x)
+    elif case == "Shiue_1":
+        vFF = np.array([-1.0/sp.pi * sp.exp(y) * sp.sin(sp.pi*x), (sp.exp(y) - sp.exp(1)) * sp.cos(sp.pi*x)])
+        pFF = 2.0*sp.exp(y) * sp.cos(sp.pi*x)
+    elif case == "Shiue_2":
+        vFF = np.array([(y-1.0)*(y-1.0) + x*(y-1.0) + 3.0*x - 1.0, x*(x-1.0) - 0.5*(y-1.0)*(y-1.0) - 3.0*y + 1.0])
+        pFF = 2.0*x + y - 1.0
+    else: # Schneider
+        vFF = np.array([y, -y*sp.sin(sp.pi*x)])
+        pFF = -y*y*sp.sin(sp.pi*x)*sp.sin(sp.pi*x)
+    return [vFF, pFF]
+
+vFF, pFF = analyticalSolutionStokes(case)
+
+# individual terms of the Navier-Stokes eq.
+vvT = np.outer(vFF, vFF)
+gradV = grad(vFF)
+gradVT = grad(vFF).T
+pI = np.array([[pFF,  0], [0,  pFF]])
+
+# complete momentum flux and its divergence
+if case == "Schneider":
+    momentumFlux = vvT - (gradV + gradVT) +pI
+else:
+    momentumFlux = - (gradV + gradVT) +pI # only Stokes
+divMomentumFlux = div(momentumFlux)
+
+print("\n\nStress tensor flux for case", case)
+print("-- [0][0]: ", sp.simplify(momentumFlux[0][0]))
+print("-- [1][1]: ", sp.simplify(momentumFlux[1][1]))
+print("-- [0][1] == [1][0]: ", sp.simplify(momentumFlux[0][1]))
+
+print("\n\nSource terms for case", case)
+
+# solution for source term
+print(" \nStokes:")
+print("-- Source term mass:", sp.diff(vFF[0],x) + sp.diff(vFF[1], y))
+print("-- Source term momentum x:", divMomentumFlux[0])
+print("-- Source term momentum y:", divMomentumFlux[1])
+
+# analytical solutions for p in Darcy domain (see reference given in problems)
+def analyticalSolutionDarcy(case):
+    if case == "Rybak":
+        pD = 0.5*y*y*sp.sin(sp.pi*x)
+    elif case == "Shiue_1":
+        pD = (sp.exp(y) - y*sp.exp(1)) * sp.cos(sp.pi*x)
+    elif case == "Shiue_2":
+        pD = x*(1.0-x)*(y-1.0) + pow(y-1.0, 3)/3.0 + 2.0*x + 2.0*y + 4.0
+    else: # Schneider
+        pD = (sp.exp(y+1) + 2.0 - sp.exp(2))*sp.sin(sp.pi*x)
+    return pD
+
+pD = analyticalSolutionDarcy(case)
+
+gradPdK = np.array([sp.diff(pD,x), sp.diff(pD,y)])
+vD = -gradPdK
+divVd = sp.diff(vD[0],x) + sp.diff(vD[1],y)
+
+print("\nDarcy:")
+print("-- Source term mass:", simplify(divVd))
+print("-- v x:", simplify(vD[0]))
+print("-- v_y", simplify(vD[1]))
---
+++ test/multidomain/boundary/freeflowporousmedium/1p_1p/convergence/problem_darcy.hh
@@ -0,0 +1,271 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup BoundaryTests
+ * \brief The Darcy sub-problem of coupled Stokes-Darcy convergence test
+ */
+
+#ifndef DUMUX_DARCY_SUBPROBLEM_HH
+#define DUMUX_DARCY_SUBPROBLEM_HH
+
+#include <dumux/common/boundarytypes.hh>
+#include <dumux/common/numeqvector.hh>
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+
+#include <dumux/porousmediumflow/problem.hh>
+
+#include "testcase.hh"
+#include "analyticalsolutions.hh"
+
+namespace Dumux {
+
+/*!
+ * \ingroup BoundaryTests
+ * \brief The Darcy sub-problem of coupled Stokes-Darcy convergence test
+ */
+template <class TypeTag>
+class DarcySubProblem : public PorousMediumFlowProblem<TypeTag>
+{
+    using ParentType = PorousMediumFlowProblem<TypeTag>;
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using PrimaryVariables = GetPropType<TypeTag, Properties::PrimaryVariables>;
+    using NumEqVector = Dumux::NumEqVector<PrimaryVariables>;
+    using BoundaryTypes = Dumux::BoundaryTypes<GetPropType<TypeTag, Properties::ModelTraits>::numEq()>;
+    using VolumeVariables = GetPropType<TypeTag, Properties::VolumeVariables>;
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    using GridView = typename GridGeometry::GridView;
+    using FVElementGeometry = typename GridGeometry::LocalView;
+    using SubControlVolume = typename GridGeometry::SubControlVolume;
+    using SubControlVolumeFace = typename GridGeometry::SubControlVolumeFace;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+
+    using CouplingManager = GetPropType<TypeTag, Properties::CouplingManager>;
+
+    enum class BC {
+        dirichlet, neumann, mixed
+    };
+
+public:
+    //! export the Indices
+    using Indices = typename GetPropType<TypeTag, Properties::ModelTraits>::Indices;
+
+    DarcySubProblem(std::shared_ptr<const GridGeometry> gridGeometry,
+                    std::shared_ptr<CouplingManager> couplingManager,
+                    std::shared_ptr<typename ParentType::SpatialParams> spatialParams,
+                    const DarcyStokesTestCase testCase,
+                    const std::string& name)
+    : ParentType(gridGeometry, spatialParams, "Darcy")
+    , couplingManager_(couplingManager)
+    , testCase_(testCase)
+    {
+        problemName_ = name + "_"
+            + getParamFromGroup<std::string>(this->paramGroup(), "Problem.Name");
+
+        auto bc = getParamFromGroup<std::string>(this->paramGroup(), "Problem.BoundaryConditions", "Dirichlet");
+        if (bc == "Dirichlet")
+            boundaryConditions_ = BC::dirichlet;
+        else if (bc == "Neumann")
+            boundaryConditions_ = BC::neumann;
+        else if (bc == "Mixed")
+            boundaryConditions_ = BC::mixed;
+        else
+            DUNE_THROW(Dune::Exception, "Wrong BC type choose: Dirichlet, Neumann or Mixed");
+
+        std::cout << "Porous medium domain: Using " << bc << " boundary conditions" << std::endl;
+    }
+
+    const std::string& name() const
+    { return problemName_; }
+
+    /*!
+     * \name Problem parameters
+     */
+    // \{
+
+    Scalar temperature() const
+    { return 273.15 + 10; } // 10°C
+    // \}
+
+    /*!
+     * \name Boundary conditions
+     */
+    // \{
+
+    /*!
+      * \brief Specifies which kind of boundary condition should be
+      *        used for which equation on a given boundary control volume.
+      *
+      * \param element The element
+      * \param scvf The boundary sub control volume face
+      */
+    BoundaryTypes boundaryTypes(const Element &element, const SubControlVolumeFace &scvf) const
+    {
+        BoundaryTypes values;
+
+        if (couplingManager().isCoupled(CouplingManager::porousMediumIndex, CouplingManager::freeFlowMassIndex, scvf))
+            values.setAllCouplingNeumann();
+        else
+        {
+            if (boundaryConditions_ == BC::dirichlet)
+                values.setAllDirichlet();
+            else if (boundaryConditions_ == BC::neumann)
+                values.setAllNeumann();
+            else
+            {
+                if (onLeftBoundary_(scvf.center()))
+                    values.setAllNeumann();
+                else
+                    values.setAllDirichlet();
+            }
+        }
+
+        return values;
+    }
+
+    /*!
+     * \brief Evaluates the boundary conditions for a Dirichlet control volume.
+     *
+     * \param element The element for which the Dirichlet boundary condition is set
+     * \param scvf The boundary sub-control-volume-face
+     */
+    PrimaryVariables dirichlet(const Element &element, const SubControlVolumeFace &scvf) const
+    {
+        const auto p = fullAnalyticalSolution(scvf.center())[2];
+        return PrimaryVariables(p);
+    }
+
+    /*!
+     * \brief Evaluates the boundary conditions for a Neumann control volume.
+     *
+     * \param element The element for which the Neumann boundary condition is set
+     * \param fvGeometry The fvGeometry
+     * \param elemVolVars The element volume variables
+     * \param elemFluxVarsCache Flux variables caches for all faces in stencil
+     * \param scvf The boundary sub control volume face
+     */
+    template<class ElementVolumeVariables, class ElementFluxVarsCache>
+    NumEqVector neumann(const Element& element,
+                        const FVElementGeometry& fvGeometry,
+                        const ElementVolumeVariables& elemVolVars,
+                        const ElementFluxVarsCache& elemFluxVarsCache,
+                        const SubControlVolumeFace& scvf) const
+    {
+        NumEqVector values(0.0);
+
+        if (couplingManager().isCoupled(CouplingManager::porousMediumIndex, CouplingManager::freeFlowMassIndex, scvf))
+            values[Indices::conti0EqIdx] = couplingManager().massCouplingCondition(
+                CouplingManager::porousMediumIndex, CouplingManager::freeFlowMassIndex,
+                fvGeometry, scvf, elemVolVars
+            );
+
+        else
+        {
+            const auto sol = fullAnalyticalSolution(scvf.center());
+            const auto n = scvf.unitOuterNormal();
+            auto v = n; v[0] = sol[0]; v[1] = sol[1];
+            values[Indices::conti0EqIdx] = v*n;
+        }
+
+        return values;
+    }
+
+    // \}
+
+    /*!
+     * \name Volume terms
+     */
+    // \{
+    /*!
+     * \brief Evaluates the source term for all phases within a given
+     *        sub control volume.
+     * \param globalPos The global position
+     */
+    NumEqVector sourceAtPos(const GlobalPosition& globalPos) const
+    {
+        using namespace Solution::DarcyStokes;
+        switch (testCase_)
+        {
+            case DarcyStokesTestCase::ShiueExampleOne:
+                return ShiueOne::darcyRHS(globalPos);
+            case DarcyStokesTestCase::ShiueExampleTwo:
+                return ShiueTwo::darcyRHS(globalPos);
+            case DarcyStokesTestCase::Rybak:
+                return Rybak::darcyRHS(globalPos);
+            case DarcyStokesTestCase::Schneider:
+                return Schneider::darcyRHS(globalPos);
+            default:
+                DUNE_THROW(Dune::InvalidStateException, "Invalid test case");
+        }
+    }
+
+    // \}
+
+    /*!
+     * \brief Evaluates the initial value for a control volume.
+     * \param element The element
+     */
+    PrimaryVariables initial(const Element &element) const
+    {  return PrimaryVariables(0.0); }
+
+    /*!
+     * \brief Returns the analytical solution of the problem at a given position.
+     * \param globalPos The global position
+     * Returns vector with entries: (velocity-x | velocity-y | pressure)
+     */
+    Dune::FieldVector<Scalar, 3> fullAnalyticalSolution(const GlobalPosition& globalPos) const
+    {
+        using namespace Solution::DarcyStokes;
+        switch (testCase_)
+        {
+            case DarcyStokesTestCase::ShiueExampleOne:
+                return ShiueOne::darcy(globalPos);
+            case DarcyStokesTestCase::ShiueExampleTwo:
+                return ShiueTwo::darcy(globalPos);
+            case DarcyStokesTestCase::Rybak:
+                return Rybak::darcy(globalPos);
+            case DarcyStokesTestCase::Schneider:
+                return Schneider::darcy(globalPos);
+            default:
+                DUNE_THROW(Dune::InvalidStateException, "Invalid test case");
+        }
+    }
+
+    // \}
+
+    //! Get the coupling manager
+    const CouplingManager& couplingManager() const
+    { return *couplingManager_; }
+
+private:
+    bool onLeftBoundary_(const GlobalPosition& globalPos) const
+    { return globalPos[0] < this->gridGeometry().bBoxMin()[0] + eps_; }
+
+    static constexpr Scalar eps_ = 1e-7;
+    std::shared_ptr<CouplingManager> couplingManager_;
+    std::string problemName_;
+    DarcyStokesTestCase testCase_;
+    BC boundaryConditions_;
+};
+
+} // end namespace Dumux
+
+#endif
