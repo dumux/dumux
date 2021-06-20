d22a3a8a89b883e92dc31bb398fa85f17d508498
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-06-20 18:38:42 +0000

    Merge branch 'feature/separate-vtk-field-type' into 'master'
    Feature/separate vtk field type
    
    See merge request dumux-repositories/dumux!2686
    
    (cherry picked from commit 32fc070199916f93276090c7f503c05b4244760b)
    
    f03f5df5 [vtkoutputmod] pull out field type
    20419c88 [test][poroelastic] avoid deprecation warning
    00dd1faf [io][vtk] move field type to vtk folder
    40b53a8e [io][vtk] move function header
    f8d6a924 [io][vtk] move precision header

--- dumux/io/vtk/precision.hh
+++ dumux/io/vtk/precision.hh
@@ -21,10 +21,41 @@
  * \ingroup InputOutput
  * \brief Vtk output precision options available in Dumux
  */
-#ifndef DUMUX_IO_VTKPRECISION_HH
-#define DUMUX_IO_VTKPRECISION_HH
+#ifndef DUMUX_IO_VTK_PRECISION_HH
+#define DUMUX_IO_VTK_PRECISION_HH
 
-#warning "This file is deprecated, include dumux/io/vtk/precision.hh instead!"
-#include <dumux/io/vtk/precision.hh>
+#include <array>
+#include <string_view>
+#include <dune/grid/io/file/vtk/common.hh>
+
+namespace Dumux::Vtk {
+
+using Dune::VTK::Precision;
+
+/*!
+ * \ingroup InputOutput
+ * \brief Maps a string (e.g. from input) to a Dune precision type
+ */
+inline Precision stringToPrecision(std::string_view precisionName)
+{
+    // this should really be constexpr but GCC <= 7.2 has a bug which
+    // doesn't allow string_view to be constexpr
+    static const std::array<std::pair<std::string_view, Precision>, 5> nameToPrecision
+    {{
+        { "Float32", Precision::float32 },
+        { "Float64", Precision::float64 },
+        { "UInt32", Precision::uint32 },
+        { "UInt8", Precision::uint8 },
+        { "Int32", Precision::int32 },
+    }};
+
+    for (const auto& [name, precision] : nameToPrecision)
+        if (name == precisionName)
+            return precision;
+
+    DUNE_THROW(Dune::InvalidStateException, "Unknown precision type " << precisionName);
+}
+
+} // end namespace Dumux::Vtk
 
 #endif
