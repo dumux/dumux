a13df8e51f6d027f1842cb5b0b0ec95372918ca9
Hanchuan Wu <whc.shmily@gmail.com>
2021-06-29 09:50:20 +0200

    [bin][util] Add function to write the content into the generated shell install script

--- bin/extractmodule/util.py
+++ bin/extractmodule/util.py
@@ -8,6 +8,7 @@ try:
     sys.path.append(os.path.join(path, '../util'))
 
     from common import callFromPath, runCommand
+    from getmoduleinfo import getModuleInfo
 except Exception:
     sys.exit('Could not import common module')
 
@@ -135,3 +136,183 @@ def versionTable(versions):
 
 def printVersionTable(versions):
     print(versionTable(versions=versions))
+
+
+def writeShellInstallScript(instFileName,
+                            modName, modFolder,
+                            folders, versions,
+                            patches, patchModule, patchRelPath,
+                            topFolderName, optsRelPath="dumux/cmake.opts"):
+    """
+    function to write the content into the generated shell install script
+
+    Keyword Arguments:
+
+        - instFileName -- The name of the generated shell install script
+
+        - modName -- The name of the module to be installed
+        - modFolder -- The folder containing the module to be installed
+
+        - folders -- The list containing the folders of the module to be
+            installed and all its dependencies
+        - versions -- The persistent, remotely available git versions for
+            the module to be installed and all its dependencies
+
+        - patches -- The patches for unpublished commits and uncommitted changes
+        - patchModule -- The paths for the modules which has unpublished commits and uncommited changes
+        - patchRelPath -- The realative paths of the generated patch files
+
+        - topFolderName -- The of the folder that the install script creates upon execution to install the module in.
+            If an empty string is passed, no folder will be created and installation happens in place.
+        - optsRelPath -- The relative path of custom opts file called by 'dunecontrol'
+
+    """
+
+    with open(instFileName, 'w') as installFile:
+        installFile.write("#!/bin/bash\n\n")
+        installFile.write("#"*80 + "\n")
+        installFile.write("# This script installs the module '{}'"
+                          " together with all dependencies.\n"
+                          "\n".format(modName))
+
+        # write function to install the new module and its dependencies into install script
+        installFile.write(
+            '# defines the function to install module with urls, shas and patches\n'
+            'installModule()\n'
+            '{\n'
+            '    URLS=$1\n'
+            '    DEPFOLDERS=$2\n'
+            '    DEPBRANCHES=$3\n'
+            '    DEPSHAS=$4\n'
+            '    PATCHES=$5\n'
+            '    PATCHFOLDERS=$6\n\n'
+            '    for url in ${URLS[@]}; do\n'
+            '        if ! git clone $url; then\n'
+            '            echo "--Error: failed to clone $url"\n'
+            '        fi\n'
+            '    done\n\n'
+            '    for index in ${!DEPFOLDERS[@]}; do\n'
+            '        if ! cd ${DEPFOLDERS[index]}; then\n'
+            '            echo "Error: could not enter folder ${DEPFOLDERS[index]}"\n'
+            '        fi\n'
+            '        if ! git checkout ${DEPBRANCHES[index]}; then\n'
+            '            echo "-- Error: failed to check out branch  ${DEPBRANCHES[index]} in ${DEPFOLDERS[index]}."\n'
+            '        fi\n'
+            '        if ! git reset --hard ${DEPSHAS[index]}; then\n'
+            '            echo "-- Error: failed to check out commit ${DEPSHAS[index]} in module ${DEPFOLDERS[index]}."\n'
+            '        fi\n'
+            '        cd ..\n'
+            '    done\n\n'
+            '    for i in ${!PATCHES[@]}; do\n'
+            '        cd ${PATCHFOLDERS[i]}\n'
+            f'        if ! git apply {"../" if topFolderName else ""}$'
+            '{PATCHES[i]}; then\n'
+            '            echo "--Error: failed to apply patch $patch"\n'
+            '        fi\n'
+            '        cd ..\n'
+            '    done\n'
+            '}\n\n'
+        )
+
+        # write urls, depfolders/branches/shas and patches into install script
+        installFile.write('# Information about url, folder/branch/sha and patch\n')
+        installFile.write('URLS=(\n')
+        for dep in folders:
+            installFile.write(versions[dep]['remote'] + '\n')
+        installFile.write(')\n\n')
+
+        installFile.write('DEPFOLDERS=(\n')
+        installFile.write('\n'.join(folders))
+        installFile.write('\n)\n\n')
+
+        installFile.write('DEPBRANCHES=(\n')
+        for dep in folders:
+            installFile.write(versions[dep]['branch'] + '\n')
+        installFile.write(')\n\n')
+
+        installFile.write('DEPSHAS=(\n')
+        for dep in folders:
+            installFile.write(versions[dep]['revision'] + '\n')
+        installFile.write(')\n\n')
+
+        # write patch information into install script
+        installFile.write('PATCHFOLDERS=(\n')
+        installFile.write('\n'.join(patchModule))
+        installFile.write('\n)\n\n')
+
+        installFile.write('PATCHES=(\n')
+        installFile.write('\n'.join(patchRelPath))
+        installFile.write('\n)\n\n')
+
+        for depModPath in set(patchModule):
+            depModName = getModuleInfo(depModPath, "Module")
+            if 'unpublished' in patches[depModPath]:
+                installFile.write("cat >> {}_unpublished.patch <<'EOF'\n".format(depModName)
+                                  + patches[depModPath]['unpublished']
+                                  + "EOF\n")
+            if 'uncommitted' in patches[depModPath]:
+                installFile.write("cat >> {}_uncommitted.patch <<'EOF'\n".format(depModName)
+                                  + patches[depModPath]['uncommitted']
+                                  + "EOF\n")
+
+        def writeCommandWithErrorCheck(command, errorMessage, indentationLevel=0):
+            unitIndentation = ' '*4
+            indent = unitIndentation*indentationLevel
+            nextIndent = indent + unitIndentation
+            installFile.write(
+                f"{indent}if ! {command}; then\n"
+                f"{nextIndent}echo \"{errorMessage}\"\n"
+                "fi\n"
+            )
+
+        if topFolderName:
+            installFile.write(
+                '# Everything will be installed into a new folder "{}".\n'
+                .format(topFolderName)
+            )
+            installFile.write(
+                'echo "Creating the folder {} to install the modules in"\n'
+                .format(topFolderName)
+            )
+
+            writeCommandWithErrorCheck(
+                'mkdir -p {}'.format(topFolderName),
+                '--Error: could not create top folder {}'.format(topFolderName)
+            )
+
+            writeCommandWithErrorCheck(
+                'cd {}'.format(topFolderName),
+                '--Error: could not enter top folder {}'.format(topFolderName))
+
+            installFile.write('\n')
+        else:
+            installFile.write(
+                '# Everything will be installed inside the folder from which the'
+                ' script is executed.\n\n'
+            )
+
+        installFile.write('installModule $URLS $DEPFOLDERS $DEPBRANCHES $DEPSHAS $PATCHES $PATCHFOLDERS\n\n')
+
+        # write configure command
+        installFile.write('echo "-- All modules haven been cloned successfully. '
+                        'Configuring project..."\n')
+        writeCommandWithErrorCheck(
+            './dune-common/bin/dunecontrol --opts=dumux/cmake.opts all'.format(optsRelPath),
+            '--Error: could not configure project'
+        )
+
+        # build the tests of the module
+        installFile.write(
+            '\n'
+            'echo "-- Configuring successful. Compiling applications..."\n'
+        )
+        writeCommandWithErrorCheck(
+            'cd {}/build-cmake'.format(modFolder),
+            '--Error: could not enter build directory at {}/build-cmake'
+            .format(modFolder)
+        )
+        writeCommandWithErrorCheck(
+            'make build_tests',
+            '--Error: applications could not be compiled. '
+            'Please try to compile them manually.'
+        )
