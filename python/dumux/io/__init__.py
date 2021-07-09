b17c9af1c5de5079c06c5633a9173a23a944db9c
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2021-07-09 18:17:08 +0200

    [python] Add VTKOutputmodule

---
+++ python/dumux/io/__init__.py
@@ -0,0 +1,13 @@
+from dune.generator.generator import SimpleGenerator
+from dune.common.hashit import hashIt
+
+# construct a VtkOutputModule
+# the grid geometry is JIT compiled
+def VtkOutputModule(model, gridVariables, sol, name):
+    includes = gridVariables._includes + sol._includes
+    includes += ["dumux/python/io/vtkoutputmodule.hh", "dumux/io/vtkoutputmodule.hh"]
+    typeName = "Dumux::VtkOutputModule<{}, {}>".format(gridVariables._typeName, sol._typeName)
+    moduleName = "vtkoutputmodule_" + hashIt(typeName)
+    generator = SimpleGenerator("VtkOutputModule", "Dumux::Python")
+    module = generator.load(includes, typeName, moduleName, preamble=model.getProperties())
+    return module.VtkOutputModule(gridVariables, sol, name)
