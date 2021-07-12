5346cd8df435b5930f2f8958676dc4883f489232
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2021-07-12 15:30:27 +0200

    fixup gridvars

--- python/dumux/discretization/__init__.py
+++ python/dumux/discretization/__init__.py
@@ -34,4 +34,5 @@ def GridVariables(problem, model):
     holderType = "std::shared_ptr<{}>".format(typeName)
     generator = SimpleGenerator("GridVariables", "Dumux::Python")
     module = generator.load(includes, typeName, moduleName, options=[holderType], preamble=model.getProperties())
+    module.GridVariables._properties = property(lambda self: model.getProperties())
     return module.GridVariables(problem, problem.gridGeometry())
