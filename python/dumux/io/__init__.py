08f28c0ea7fca6f8bec4bbd31989cffbcf5a9254
Kilian Weishaupt <kilian.weishaupt@iws.uni-stuttgart.de>
2021-07-12 16:15:45 +0200

    fixup io

--- python/dumux/io/__init__.py
+++ python/dumux/io/__init__.py
@@ -3,11 +3,11 @@ from dune.common.hashit import hashIt
 
 # construct a VtkOutputModule
 # the grid geometry is JIT compiled
-def VtkOutputModule(gridVariables, sol, name):
-    includes = gridVariables._includes + sol._includes
+def VtkOutputModule(*, gridVariables, solutionVector, name):
+    includes = gridVariables._includes + solutionVector._includes
     includes += ["dumux/python/io/vtkoutputmodule.hh", "dumux/io/vtkoutputmodule.hh"]
-    typeName = "Dumux::VtkOutputModule<{}, {}>".format(gridVariables._typeName, sol._typeName)
+    typeName = "Dumux::VtkOutputModule<{}, {}>".format(gridVariables._typeName, solutionVector._typeName)
     moduleName = "vtkoutputmodule_" + hashIt(typeName)
     generator = SimpleGenerator("VtkOutputModule", "Dumux::Python")
     module = generator.load(includes, typeName, moduleName, preamble=gridVariables._model.getProperties())
-    return module.VtkOutputModule(gridVariables, sol, name)
+    return module.VtkOutputModule(gridVariables, solutionVector, name)
