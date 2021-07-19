1b2ad0202b19d726f837365c842e5a96fb63f10b
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-07-19 21:00:10 +0200

    [paramlist] Format with Python black

--- bin/doc/getparameterlist.py
+++ bin/doc/getparameterlist.py
@@ -20,8 +20,12 @@ def getEnclosedContent(string, openKey, closeKey):
     result, rest = rest[0] + closeKey, rest[2]
     while result.count(openKey) != result.count(closeKey):
         rest = rest.partition(closeKey)
-        if rest[1] == '':
-            raise IOError('Could not get content between "{}" and "{}" in given string "{}"'.format(openKey, closeKey, string))
+        if rest[1] == "":
+            raise IOError(
+                'Could not get content between "{}" and "{}" in given string "{}"'.format(
+                    openKey, closeKey, string
+                )
+            )
         result, rest = result + rest[0] + closeKey, rest[2]
 
     return result.partition(openKey)[2].rpartition(closeKey)[0]
@@ -32,45 +36,49 @@ def extractParamName(line):
 
     # split occurrences of getParam<T>(CALLARGS) or getParamFromGroup<T>(CALLARGS)
     # into the template arguments T and the function arguments CALLARGS
-    if 'getParamFromGroup<' in line:
-        line = line.split('getParamFromGroup')[1]
+    if "getParamFromGroup<" in line:
+        line = line.split("getParamFromGroup")[1]
         hasGroup = True
-    elif 'getParam<' in line:
-        line = line.split('getParam')[1]
+    elif "getParam<" in line:
+        line = line.split("getParam")[1]
         hasGroup = False
     else:
         return {}
 
     # TODO: Support this also
-    if line.count('getParam') > 1:
+    if line.count("getParam") > 1:
         raise IOError('Cannot process multiple occurrences of "getParam" in one line')
 
     # remove trailing spaces and cut off everything behind semicolon
-    line = line.strip('\n').strip(' ').split(';')[0]
+    line = line.strip("\n").strip(" ").split(";")[0]
 
     # extract template arg between '<' and '>'
-    paramType = getEnclosedContent(line, '<', '>')
+    paramType = getEnclosedContent(line, "<", ">")
 
     # extract function arguments
-    functionArgs = line.partition('<' + paramType + '>')[2]
-    functionArgs = getEnclosedContent(functionArgs, '(', ')')
+    functionArgs = line.partition("<" + paramType + ">")[2]
+    functionArgs = getEnclosedContent(functionArgs, "(", ")")
 
     if hasGroup:
-        functionArgs = functionArgs.partition(',')[2]
-    functionArgs = functionArgs.partition(',')
+        functionArgs = functionArgs.partition(",")[2]
+    functionArgs = functionArgs.partition(",")
     paramName = functionArgs[0]
     defaultValue = None if not functionArgs[2] else functionArgs[2]
 
-    paramType = paramType.strip(' ')
-    paramName = paramName.strip(' ')
-    if (defaultValue):
-        defaultValue = defaultValue.strip(' ')
+    paramType = paramType.strip(" ")
+    paramName = paramName.strip(" ")
+    if defaultValue:
+        defaultValue = defaultValue.strip(" ")
 
     # if interior spaces occur in the parameter name, we can't identify it
-    if paramName[0] != '"' or paramName[-1] != '"' or ' ' in paramName:
+    if paramName[0] != '"' or paramName[-1] != '"' or " " in paramName:
         raise IOError("Could not correctly process parameter name")
 
-    return {'paramType': paramType, 'paramName': paramName.strip('"'), 'defaultValue': defaultValue}
+    return {
+        "paramType": paramType,
+        "paramName": paramName.strip('"'),
+        "defaultValue": defaultValue,
+    }
 
 
 # extract all parameters from a given file
@@ -84,14 +92,18 @@ def getParamsFromFile(file):
                 if param:
                     parameters.append(param)
             except IOError as e:
-                errors[lineIdx] = {'line': line.strip(), 'message': e}
+                errors[lineIdx] = {"line": line.strip(), "message": e}
 
     # print encountered errors
     if errors:
-        print('\n\n{} parameter{} in file {} could not be retrieved automatically. Please check them yourself:'.format(len(errors), 's' if len(errors) > 1 else '', file))
+        print(
+            "\n\n{} parameter{} in file {} could not be retrieved automatically. Please check them yourself:".format(
+                len(errors), "s" if len(errors) > 1 else "", file
+            )
+        )
         for lineIdx in errors:
-            print("\n\t-> line {}: {}".format(lineIdx, errors[lineIdx]['line']))
-            print("\t\t-> error message: {}".format(errors[lineIdx]['message']))
+            print("\n\t-> line {}: {}".format(lineIdx, errors[lineIdx]["line"]))
+            print("\t\t-> error message: {}".format(errors[lineIdx]["message"]))
 
     return parameters
 
@@ -104,9 +116,9 @@ def isEndOfTableHeader(text):
 
 class CheckExistAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string):
-        if(os.path.isfile(values)):
+        if os.path.isfile(values):
             setattr(namespace, self.dest, values)
-            setattr(namespace, 'hasInput', True)
+            setattr(namespace, "hasInput", True)
         else:
             parser.error("File {} does not exist!".format(values))
 
@@ -118,17 +130,27 @@ parser = argparse.ArgumentParser(
     If input file is given, the descriptions will be copied from the input.
     If no input file is given, the descriptions will be empty.
     """,
-    formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument("--root", help="root path of Dumux",
-                    metavar="rootpath",
-                    default=os.path.abspath(os.path.join(os.path.abspath(__file__), '../../../')))
-parser.add_argument("--param-list", help="given parameterlist file",
-                    action=CheckExistAction,
-                    metavar="input",
-                    dest="inputFile")
-parser.add_argument("--output",  help="relative path (to the root path) of the output file",
-                    metavar="output",
-                    default='doc/doxygen/extradoc/parameterlist.txt')
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+)
+parser.add_argument(
+    "--root",
+    help="root path of Dumux",
+    metavar="rootpath",
+    default=os.path.abspath(os.path.join(os.path.abspath(__file__), "../../../")),
+)
+parser.add_argument(
+    "--param-list",
+    help="given parameterlist file",
+    action=CheckExistAction,
+    metavar="input",
+    dest="inputFile",
+)
+parser.add_argument(
+    "--output",
+    help="relative path (to the root path) of the output file",
+    metavar="output",
+    default="doc/doxygen/extradoc/parameterlist.txt",
+)
 
 args = vars(parser.parse_args())
 
@@ -150,12 +172,16 @@ if args["inputFile"]:
 
             # error in input, e.g. missing the "|" at the end of line
             if line.strip()[-1] != "|":
-                print("Error in Input. Check the end of line of {}".format(valuesList[0:2]))
+                print(
+                    "Error in Input. Check the end of line of {}".format(
+                        valuesList[0:2]
+                    )
+                )
                 exit()
 
-            #remove \\b in string
+            # remove \\b in string
             if "\\b" in valuesList[0]:
-                valuesList[0]= valuesList[0][3:]
+                valuesList[0] = valuesList[0][3:]
 
             name = valuesList[0] + "." + valuesList[1]
             oldDataDict.update({name: dict(zip(keysList, valuesList))})
@@ -165,21 +191,24 @@ if args["inputFile"]:
 parameters = []
 for root, _, files in os.walk(args["root"]):
     for file in files:
-        if os.path.splitext(file)[1] == ".hh" and os.path.splitext(file)[0] != 'parameters':
+        if (
+            os.path.splitext(file)[1] == ".hh"
+            and os.path.splitext(file)[0] != "parameters"
+        ):
             parameters.extend(getParamsFromFile(os.path.join(root, file)))
 
 # make sorted dictionary of the entries
 # treat duplicates (could have differing default values or type names - e.g. via aliases)
 parameterDict = {}
 for params in parameters:
-    key = params['paramName']
+    key = params["paramName"]
     if key in parameterDict:
-        parameterDict[key]['defaultValue'].append(params['defaultValue'])
-        parameterDict[key]['paramType'].append(params['paramType'])
+        parameterDict[key]["defaultValue"].append(params["defaultValue"])
+        parameterDict[key]["paramType"].append(params["paramType"])
     else:
         parameterDict[key] = params
-        parameterDict[key]['defaultValue'] = [params['defaultValue']]
-        parameterDict[key]['paramType'] = [params['paramType']]
+        parameterDict[key]["defaultValue"] = [params["defaultValue"]]
+        parameterDict[key]["paramType"] = [params["paramType"]]
 parameterDict = {key: value for key, value in sorted(parameterDict.items())}
 
 # determine actual entries (from duplicates)
@@ -192,35 +221,65 @@ tableEntryData = []
 for key in parameterDict:
 
     entry = parameterDict[key]
-    hasGroup = True if entry['paramName'].count('.') != 0 else False
-    groupEntry = '-' if not hasGroup else entry['paramName'].split('.')[0]
-    paramName = entry['paramName'] if not hasGroup else entry['paramName'].partition('.')[2]
+    hasGroup = True if entry["paramName"].count(".") != 0 else False
+    groupEntry = "-" if not hasGroup else entry["paramName"].split(".")[0]
+    paramName = (
+        entry["paramName"] if not hasGroup else entry["paramName"].partition(".")[2]
+    )
 
     # In case of multiple occurrences, we use the first entry that is not None and print the others for possible manual editing
-    paramType = entry['paramType'][0]
-    defaultValue = next((e for e in entry['defaultValue'] if e), '-')
+    paramType = entry["paramType"][0]
+    defaultValue = next((e for e in entry["defaultValue"] if e), "-")
 
-    hasMultiplePT = True if not all(pt == paramType for pt in entry['paramType']) else False
-    hasMultipleDV = True if not all(dv == (defaultValue if defaultValue != '-' else None) for dv in entry['defaultValue']) else False
+    hasMultiplePT = (
+        True if not all(pt == paramType for pt in entry["paramType"]) else False
+    )
+    hasMultipleDV = (
+        True
+        if not all(
+            dv == (defaultValue if defaultValue != "-" else None)
+            for dv in entry["defaultValue"]
+        )
+        else False
+    )
     if hasMultiplePT or hasMultipleDV:
-        print('\nFound multiple occurrences of parameter ' + paramName + ' with differing specifications: ')
+        print(
+            "\nFound multiple occurrences of parameter "
+            + paramName
+            + " with differing specifications: "
+        )
     if hasMultiplePT:
-        print(' -> Specified type names:')
-        for typeName in entry['paramType']:
-            print(' '*8 + typeName)
-        print(' ---> For the parameters list, ' + paramType + ' has been chosen. Please adapt manually if desired.')
+        print(" -> Specified type names:")
+        for typeName in entry["paramType"]:
+            print(" " * 8 + typeName)
+        print(
+            " ---> For the parameters list, "
+            + paramType
+            + " has been chosen. Please adapt manually if desired."
+        )
     if hasMultipleDV:
-        print(' -> Specified default values:')
-        for default in entry['defaultValue']:
-            print(' '*8 + (default if default else '- (none given)'))
-        print(' ---> For the parameters list, ' + defaultValue + ' has been chosen. Please adapt manually if desired.')
+        print(" -> Specified default values:")
+        for default in entry["defaultValue"]:
+            print(" " * 8 + (default if default else "- (none given)"))
+        print(
+            " ---> For the parameters list, "
+            + defaultValue
+            + " has been chosen. Please adapt manually if desired."
+        )
 
     # +3 because \b will be added later
-    maxGroupWidth = max(maxGroupWidth, len(groupEntry)+3)
+    maxGroupWidth = max(maxGroupWidth, len(groupEntry) + 3)
     maxParamWidth = max(maxParamWidth, len(paramName))
     maxTypeWidth = max(maxTypeWidth, len(paramType))
     maxDefaultWidth = max(maxDefaultWidth, len(defaultValue))
-    tableEntryData.append({'group': groupEntry, 'name': paramName, 'type': paramType, 'default': defaultValue})
+    tableEntryData.append(
+        {
+            "group": groupEntry,
+            "name": paramName,
+            "type": paramType,
+            "default": defaultValue,
+        }
+    )
 
 # generate actual table entries
 tableEntriesWithGroup = []
@@ -229,27 +288,29 @@ previousGroupEntry = None
 
 for data in tableEntryData:
 
-    groupEntry = data['group']
-    paramName = data['name']
-    paramType = data['type']
-    defaultValue = data['default']
+    groupEntry = data["group"]
+    paramName = data["name"]
+    paramType = data["type"]
+    defaultValue = data["default"]
     paramKey = groupEntry + "." + paramName
-    explanation = "" if paramKey not in oldDataDict else oldDataDict[paramKey]["Explanation"]
+    explanation = (
+        "" if paramKey not in oldDataDict else oldDataDict[paramKey]["Explanation"]
+    )
 
     if groupEntry != previousGroupEntry:
         previousGroupEntry = groupEntry
-        if groupEntry != '-':
-            groupEntry = '\\b ' + groupEntry
+        if groupEntry != "-":
+            groupEntry = "\\b " + groupEntry
 
-    tableEntry = ' * | {} | {} | {} | {} | {} |'.format(
+    tableEntry = " * | {} | {} | {} | {} | {} |".format(
         groupEntry.ljust(maxGroupWidth),
         paramName.ljust(maxParamWidth),
         paramType.ljust(maxTypeWidth),
         defaultValue.ljust(maxDefaultWidth),
-        explanation.ljust(maxExplanationWidth)
+        explanation.ljust(maxExplanationWidth),
     )
 
-    if groupEntry != '-':
+    if groupEntry != "-":
         tableEntriesWithGroup.append(tableEntry)
     else:
         tableEntriesWithoutGroup.append(tableEntry)
@@ -289,6 +350,10 @@ header += " | :-         |\n"
 with open(os.path.join(args["root"], args["output"]), "w") as outputfile:
     outputfile.write(header)
     for e in tableEntries:
-        outputfile.write(e + '\n')
-    outputfile.write(' */\n')
-    print("The parameter list is written to {}".format(os.path.join(args["root"], args["output"])))
+        outputfile.write(e + "\n")
+    outputfile.write(" */\n")
+    print(
+        "The parameter list is written to {}".format(
+            os.path.join(args["root"], args["output"])
+        )
+    )
