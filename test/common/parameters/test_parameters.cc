b8afd2b4de2324216a75b4545174a5422f80959d
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-08-15 12:05:39 +0200

    [test] Add unit test for parameters

---
+++ test/common/parameters/test_parameters.cc
@@ -0,0 +1,27 @@
+#include <config.h>
+#include <iostream>
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/exceptions.hh>
+
+#include <dumux/common/parameters.hh>
+
+int main (int argc, char *argv[])
+{
+    using namespace Dumux;
+
+    // maybe initialize mpi
+    Dune::MPIHelper::instance(argc, argv);
+
+    auto defaultParams = [] (Dune::ParameterTree& p) {
+        p["Grid.UpperRight"] = "2 2";
+        p["Grid.Cells"] = "20 20";
+    };
+
+    // initialize parameter tree
+    Parameters::init(argc, argv, defaultParams);
+
+    Parameters::print();
+
+    return 0;
+}
