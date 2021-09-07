10e46e1493a32341f6606e9f11dfad7675834cee
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-09-07 09:37:32 +0200

    [test][1p_richards] Fix spatial paramter input name

--- test/multidomain/embedded/1d3d/1p_richards/spatialparams_root.hh
+++ test/multidomain/embedded/1d3d/1p_richards/spatialparams_root.hh
@@ -66,8 +66,8 @@ public:
     : ParentType(gridGeometry), gridData_(gridData)
     {
         porosity_ = getParam<Scalar>("Root.SpatialParams.Porosity", 0.4);
-        constantKx_ = getParam<Scalar>("SpatialParams.Kx", 5.0968e-17);
-        constantKr_ = getParam<Scalar>("SpatialParams.Kr", 2.04e-13);
+        constantKx_ = getParam<Scalar>("Root.SpatialParams.Kx", 5.0968e-17);
+        constantKr_ = getParam<Scalar>("Root.SpatialParams.Kr", 2.04e-13);
 
         const auto& gv = gridGeometry->gridView();
         radii_.resize(gv.size(0));
