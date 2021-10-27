95fc6132092ca47eb65ac86d4a4bffa4eb014466
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2021-10-27 11:13:12 +0200

    [test] avoid depr warnings from 1pconstant spatial params

--- test/porousmediumflow/1p/pointsources/timeindependent/properties.hh
+++ test/porousmediumflow/1p/pointsources/timeindependent/properties.hh
@@ -33,10 +33,10 @@
 #include <dumux/discretization/cctpfa.hh>
 #include <dumux/discretization/box.hh>
 #include <dumux/porousmediumflow/1p/model.hh>
+#include <dumux/porousmediumflow/fvspatialparams1pconstant.hh>
 
 #include <dumux/material/components/simpleh2o.hh>
 #include <dumux/material/fluidsystems/1pliquid.hh>
-#include <dumux/material/spatialparams/fv1pconstant.hh>
 
 #ifndef GRIDTYPE // default to yasp grid if not provided by CMake
 #define GRIDTYPE Dune::YaspGrid<2, Dune::EquidistantOffsetCoordinates<GetPropType<TypeTag, Properties::Scalar>, 2> >
@@ -73,7 +73,7 @@ struct SpatialParams<TypeTag, TTag::OnePSingularity>
 {
     using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
     using Scalar = GetPropType<TypeTag, Properties::Scalar>;
-    using type = FVSpatialParamsOnePConstant<GridGeometry, Scalar>;
+    using type = FVPorousMediumSpatialParamsOnePConstant<GridGeometry, Scalar>;
 };
 
 } // end namespace Dumux
