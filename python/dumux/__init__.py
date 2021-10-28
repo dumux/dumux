648a88f08dfe7989a19c730d514722123036708a
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-10-28 18:33:06 +0200

    Revert "[python] Use implicit namespace package PEP 420"
    This reverts commit 0ffc1fca8215a5c8b6eb7620fb464ac7d9d92480.

---
+++ python/dumux/__init__.py
@@ -0,0 +1,13 @@
+"""The DuMux Python-bindings for using DuMux entirely from Python
+
+DuMux is
+* short for Dune for Multi-{Phase, Component, Scale, Physics, …} flow and transport in porous media
+* a free and open-source simulator for flow and transport processes in porous media
+* a research code written in C++
+* based on Dune (Distributed and Unified Numerics Environment)
+* a Dune user module in the Dune environment
+
+https://dumux.org/
+"""
+
+__import__("pkg_resources").declare_namespace(__name__)
