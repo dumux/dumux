b01add08bda59367d25ecb99d406ea65543a658c
Timo Koch <timokoch@math.uio.no>
2021-10-30 19:03:38 +0000

    fiup

--- bin/testing/ci-setup-python-env.sh
+++ bin/testing/ci-setup-python-env.sh
@@ -2,6 +2,7 @@
 
 if [ -d "/dune/modules/dune-common/build-cmake/dune-env" ]; then
     # Use internal venv of DUNE
+    echo "Activating the Python virtual environment of dune-common"
     source /dune/modules/dune-common/build-cmake/dune-env/bin/activate
 else
     if [ -L /dune/bin/setup-python ] && [ -e /dune/bin/setup-python ] ; then
