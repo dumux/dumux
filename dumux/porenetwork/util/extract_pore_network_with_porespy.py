bc04b71e6261df3f18ef3bb610d693ab7fc7957a
Timo Koch <timokoch@math.uio.no>
2021-11-10 13:48:44 +0000

    [pnm][ci] Lint scripts in CI

--- dumux/porenetwork/util/extract_pore_network_with_porespy.py
+++ dumux/porenetwork/util/extract_pore_network_with_porespy.py
@@ -9,85 +9,102 @@ Requires the PoreSpy (https://porespy.org/) and OpenPNM package (https://openpnm
 
 from argparse import ArgumentParser
 import numpy as np
-import openpnm as op
-import porespy as ps
-from porespy.tools import randomize_colors
-from loguru import logger
+import openpnm as op  # pylint: disable=import-error
+import porespy as ps  # pylint: disable=import-error
+from porespy.tools import randomize_colors  # pylint: disable=import-error
+from loguru import logger  # pylint: disable=import-error
 
 
 def printReadMessage(fileName, fileType, numVoxels):
-    dim = len(numVoxels)
+    """DOC ME!"""
+
+    dimension = len(numVoxels)
     print(
         "Reading",
-        str(dim) + "D",
+        str(dimension) + "D",
         fileType,
         "image file",
         fileName,
         "with",
         numVoxels,
-        "voxels" if dim == 3 else "pixels",
+        "voxels" if dimension == 3 else "pixels",
     )
 
 
 def openRawFile(fileName, numVoxels):
+    """DOC ME!"""
+
     assert len(numVoxels) == 2 or len(numVoxels) == 3
-    im = np.fromfile(fileName, dtype="uint8", sep="").reshape(numVoxels)
-    im = np.array(im, dtype=bool)
-    dim = len(im.shape)
-    printReadMessage(fileName, "raw", im.shape)
-    if dim == 3:
-        im = np.swapaxes(im, 0, 2)
-    return im
+    image = np.fromfile(fileName, dtype="uint8", sep="").reshape(numVoxels)
+    image = np.array(image, dtype=bool)
+    dimension = len(image.shape)
+    printReadMessage(fileName, "raw", image.shape)
+    if dimension == 3:
+        image = np.swapaxes(image, 0, 2)
+    return image
 
 
 def openNrrdFile(fileName):
-    import nrrd
+    """DOC ME!"""
 
-    im, header = nrrd.read(fileName)
-    im = np.array(im, dtype=bool)
-    printReadMessage(fileName, "nrrd", im.shape)
-    return im
+    import nrrd  # pylint: disable=import-outside-toplevel,import-error
+
+    image, _ = nrrd.read(fileName)
+    image = np.array(image, dtype=bool)
+    printReadMessage(fileName, "nrrd", image.shape)
+    return image
 
 
 def openPngFile(fileName):
-    import matplotlib.pyplot as plt
+    """DOC ME!"""
 
-    im = plt.imread(fileName)
-    im = np.array(im, dtype=bool)
-    printReadMessage(fileName, "png", im.shape)
-    return im
+    import matplotlib.pyplot as plt  # pylint: disable=import-outside-toplevel,import-error
+
+    image = plt.imread(fileName)
+    image = np.array(image, dtype=bool)
+    printReadMessage(fileName, "png", image.shape)
+    return image
 
 
 def openFile(fileName, numVoxels):
+    """Opens the image with fileName detecting the file type from extension"""
+
     if fileName.endswith(".raw"):
         return openRawFile(fileName, numVoxels)
-    elif fileName.endswith(".nrrd"):
+    if fileName.endswith(".nrrd"):
         if len(numVoxels) > 0:
             logger.warning("Argument numVoxels ignored for nrrd files")
         return openNrrdFile(fileName)
-    elif fileName.endswith(".png"):
+    if fileName.endswith(".png"):
         if len(numVoxels) > 0:
             logger.warning("Argument numVoxels ignored for png files")
         return openPngFile(fileName)
-    else:
-        raise NotImplementedError(fileName + "not supported")
+
+    raise NotImplementedError(fileName + "not supported")
 
 
-def extrude2D(im, layers, topAndBottomValues):
-    if not len(im.shape) == 2:
+def extrude2D(image, layers, topAndBottomValues):
+    """DOC ME!"""
+
+    if not len(image.shape) == 2:
         raise IOError("Extrusion only works for 2D images")
     if not len(topAndBottomValues) == 2:
         raise IOError("topAndBottomValues can only have two entries")
-    im = np.tile(np.atleast_3d(im), reps=[1, 1, layers])
-    im[..., 0:1] = topAndBottomValues[0]
-    im[..., -1:] = topAndBottomValues[1]
+    image = np.tile(np.atleast_3d(image), reps=[1, 1, layers])
+    image[..., 0:1] = topAndBottomValues[0]
+    image[..., -1:] = topAndBottomValues[1]
     print("Extruding image to 3D.")
-    return im
+    return image
 
 
-def runSnow(im, resolution, sigma, rMax, boundaryWidth, useMarchingCubes):
+def runSnow(
+    image, resolution, sigma, rMax, boundaryWidth, useMarchingCubes
+):  # pylint: disable=too-many-arguments
+    """DOC ME!"""
+
+    # pylint: disable=no-member
     return ps.networks.snow2(
-        im,
+        image,
         voxel_size=resolution,
         sigma=sigma,
         r_max=rMax,
@@ -96,9 +113,14 @@ def runSnow(im, resolution, sigma, rMax, boundaryWidth, useMarchingCubes):
     )
 
 
-def runDualSnow(im, resolution, sigma, rMax, boundaryWidth, useMarchingCubes):
+def runDualSnow(
+    image, resolution, sigma, rMax, boundaryWidth, useMarchingCubes
+):  # pylint: disable=too-many-arguments
+    """DOC ME!"""
+
+    # pylint: disable=no-member
     return ps.networks.snow2(
-        im + 1,
+        image + 1,
         voxel_size=resolution,
         sigma=sigma,
         r_max=rMax,
@@ -109,8 +131,10 @@ def runDualSnow(im, resolution, sigma, rMax, boundaryWidth, useMarchingCubes):
 
 
 def writeSegmentationInfoFile(snowOutput, fileName, resolution):
-    dim = len(snowOutput.phases.shape)
-    if dim == 2:
+    """DOC ME!"""
+
+    dimension = len(snowOutput.phases.shape)
+    if dimension == 2:
         ps.io.to_vtk(
             np.array(randomize_colors(snowOutput.regions), dtype=int)[:, :, np.newaxis],
             fileName,
@@ -133,7 +157,7 @@ def extractNetwork(
     boundaryWidth=3,
     dualSnow=False,
     useMarchingCubes=False,
-):
+):  # pylint: disable=too-many-arguments
     """
     Extracts a porenetwork from an image file and exports
     it as VTK Polydata file (*.vtp) and OpenPNM network file (*.pnm).
@@ -142,11 +166,11 @@ def extractNetwork(
     to the Dune grid file format (*.dgf) that can be read by DuMux.
     """
 
-    print("Porosity is", ps.metrics.porosity(im))
+    print("Porosity is", ps.metrics.porosity(image))
 
     if not dualSnow:
         snowOutput = runSnow(
-            im=im,
+            image=image,
             resolution=resolution,
             sigma=sigma,
             rMax=rMax,
@@ -155,7 +179,7 @@ def extractNetwork(
         )
     else:
         snowOutput = runDualSnow(
-            im=im,
+            image=image,
             resolution=resolution,
             sigma=sigma,
             rMax=rMax,
@@ -167,22 +191,22 @@ def extractNetwork(
     writeSegmentationInfoFile(snowOutput, outputName + "_segmentation", resolution)
 
     # use PoreSpy to sanitize some parameters (might become obsolete as some point)
-    pn, geo = op.io.PoreSpy.import_data(snowOutput.network)
+    porenetwork, _ = op.io.PoreSpy.import_data(snowOutput.network)
 
     # trimming pore network to avoid singularity
-    print("Number of pores before trimming: ", pn.Np)
-    h = pn.check_network_health()
-    op.topotools.trim(network=pn, pores=h["trim_pores"])
-    print("Number of pores after trimming: ", pn.Np)
+    print("Number of pores before trimming: ", porenetwork.Np)
+    health = porenetwork.check_network_health()
+    op.topotools.trim(network=porenetwork, pores=health["trim_pores"])
+    print("Number of pores after trimming: ", porenetwork.Np)
 
     filename = outputName + ("_dual" if dualSnow else "")
 
     # export to vtk
-    op.io.VTK.save(network=pn, filename=filename)
+    op.io.VTK.save(network=porenetwork, filename=filename)
     print("Writing", filename + ".vtp")
 
     # export network for OpenPNM
-    pn.project.save_project(filename)
+    porenetwork.project.save_project(filename)
     print("Writing", filename + ".pnm")
 
 
@@ -230,43 +254,36 @@ if __name__ == "__main__":
         help="The number of pixels/voxels in each direction",
         default=[],
     )
-    parser.add_argument(
-        "-n", "--outputName", type=str, help="The output file name", default=""
-    )
+    parser.add_argument("-n", "--outputName", type=str, help="The output file name", default="")
     parser.add_argument(
         "-m",
         "--marchingCubesArea",
         action="store_true",
         help="Use marching cubes alg. to calculate throat area",
     )
-    parser.add_argument(
-        "-d", "--dualSnow", action="store_true", help="Extract dual network"
-    )
-    parser.add_argument(
-        "-i", "--invert", action="store_true", help="Invert the image data"
-    )
+    parser.add_argument("-d", "--dualSnow", action="store_true", help="Extract dual network")
+    parser.add_argument("-i", "--invert", action="store_true", help="Invert the image data")
     args = vars(parser.parse_args())
 
-    im = openFile(args["file"], args["numVoxels"])
-    dim = len(im.shape)
+    imageData = openFile(args["file"], args["numVoxels"])
 
     if not args["outputName"]:
-        # poreSpy has issues with dots in file names. Replace with underscores here and strip file suffix.
+        # poreSpy has issues with dots in file names
+        # Replace with underscores here and strip file suffix.
         args["outputName"] = ("_").join((args["file"]).split(".")[0:-1])
 
     if len(args["boundaryWidth"]) == 0:
         args["boundaryWidth"] = 3
 
     if args["extrusionLayers"] > 0:
-        im = extrude2D(im, args["extrusionLayers"], args["topAndBottomValues"])
-        dim = 3
+        imageData = extrude2D(imageData, args["extrusionLayers"], args["topAndBottomValues"])
 
     if args["invert"]:
         print("Inverting data")
-        im = ~im
+        imageData = ~imageData
 
     extractNetwork(
-        im,
+        imageData,
         args["resolution"],
         outputName=args["outputName"],
         sigma=args["sigma"],
--- dumux/porenetwork/util/openpnm2dgf.py
+++ dumux/porenetwork/util/openpnm2dgf.py
@@ -1,27 +1,35 @@
-import os
-import openpnm as op
-import numpy as np
+"""
+Creates a DUNE Grid Format file (*.dgf)
+from an OpenPNM porenetwork file (*.pnm)
+"""
+
 import argparse
-from openpnm.topotools import trim
+import os
 import copy
+import openpnm as op  # pylint: disable=import-error
+import numpy as np
+from openpnm.topotools import trim  # pylint: disable=import-error
 
 
 def load(filename):
-    # create workspace instance
-    ws = op.Workspace()
-    ws.clear()
-    project = ws.load_project(filename=args["file"])
+    """Create workspace instance"""
+
+    workspace = op.Workspace()
+    workspace.clear()
+    project = workspace.load_project(filename=filename)
     print(project)
     return project["net_01"], project["geo_01"]
 
 
 def removeBoundaryThroats(net):
+    """DOC ME!"""
+
     pores = net.pores("boundary")
     net["throat.boundary"] = False
 
-    for idx, throat in enumerate (net["throat.conns"]):
-            if net["pore.boundary"][throat[0]] or net["pore.boundary"][throat[1]]:
-                net["throat.boundary"][idx] = True
+    for idx, throat in enumerate(net["throat.conns"]):
+        if net["pore.boundary"][throat[0]] or net["pore.boundary"][throat[1]]:
+            net["throat.boundary"][idx] = True
 
     throats = net.throats("boundary")
     cnBoundary = net["throat.conns"][throats]
@@ -44,66 +52,35 @@ def removeBoundaryThroats(net):
                 # make sure both pores get same label
                 net[label][throat[0]] = val
                 net[label][throat[1]] = val
-        except:
+        except KeyError:
             print(f"{label} not found. Skipping.")
 
     trim(network=net, throats=throats)
     trim(network=net, pores=net["pore.delete"])
 
 
-def poreLabel(net):
-    label = np.full(len(net["pore.coords"]), -1, dtype=int)
-    label[net["pore.xmin"]] = 1
-    label[net["pore.xmax"]] = 2
-    label[net["pore.ymin"]] = 3
-    label[net["pore.ymax"]] = 4
-    try:
-        label[net["pore.zmin"]] = 5
-        label[net["pore.zmax"]] = 6
-    except (IndexError, KeyError):
-        pass
-
-    if "pore.fake_void" in net.keys() and "pore.fake_solid" in net.keys():
-        label[net["pore.fake_void"]] = 22
-        label[net["pore.fake_solid"]] = 33
-
-    return label
-
-
-def throatLabel(net, policy=None):
-    # TODO add policy
-    pLabels = poreLabel(net)
-    label = np.full(len(net["throat.conns"]), -1, dtype=int)
-
-    for eIdx, element in enumerate(net["throat.conns"]):
-        label[eIdx] = np.max(
-            pLabels[element]
-        )  # take the label with higher value, TODO add policy here
-
-    if "throat.fake_void" in net.keys() and "throat.fake_solid" in net.keys():
-        label[net["throat.fake_void"]] = 22
-        label[net["throat.fake_solid"]] = 33
-
-    return label
-
-
 def getDualGridCoordinationNumber(net):
+    """DOC ME!"""
+
     numPoresTotal = len(net["pore.coords"])
     coordNumVoid = np.zeros(numPoresTotal, dtype=np.int64)
     coordNumSolid = np.zeros(numPoresTotal, dtype=np.int64)
 
-    for vT in net["throat.conns"][net["throat.void"]]:
-        coordNumVoid[vT[0]] += 1
-        coordNumVoid[vT[1]] += 1
+    for voidThroat in net["throat.conns"][net["throat.void"]]:
+        coordNumVoid[voidThroat[0]] += 1
+        coordNumVoid[voidThroat[1]] += 1
 
-    for sT in net["throat.conns"][net["throat.solid"]]:
-        coordNumSolid[sT[0]] += 1
-        coordNumSolid[sT[1]] += 1
+    for solidThroat in net["throat.conns"][net["throat.solid"]]:
+        coordNumSolid[solidThroat[0]] += 1
+        coordNumSolid[solidThroat[1]] += 1
 
     return coordNumVoid, coordNumSolid
 
 
 def sanitizeDualNetwork(net, splitSinglePores):
+    """DOC ME!"""
+    # pylint: disable=too-many-locals,too-many-branches,too-many-statements
+
     geo = net.project["geo_01"]
 
     print("Num. void throats before dual grid sanitation", np.sum(net["throat.void"]))
@@ -115,8 +92,10 @@ def sanitizeDualNetwork(net, splitSinglePores):
     poreKeys = [key for key in geo if key.startswith("pore.")]
     throatKeys = [key for key in geo if key.startswith("throat.")]
 
-    # HACK We want to add geometry information for the new fake pores and throats but it is not clear how to change the entries in geo.
-    # We therefore replace the geo object by a dictionary which behaves similarly.
+    # HACK We want to add geometry information for the new fake pores
+    # and throats but it is not clear how to change the entries in geo.
+    # We therefore replace the geo object
+    # by a dictionary which behaves similarly.
     # This should be improved!
     if splitSinglePores:
         newGeo = {}
@@ -136,7 +115,8 @@ def sanitizeDualNetwork(net, splitSinglePores):
             # label = 33
             # domainType = 1
 
-        # identify pores only connect to pores of other phase and add an artificial throat or delete those pores
+        # identify pores only connect to pores of other phase and
+        # add an artificial throat or delete those pores
         for vIdx, coordNum in enumerate(coordinationNumber):
             if net["pore." + domain][vIdx] == 1 and coordNum < 1:
                 if domain == "void":
@@ -173,7 +153,8 @@ def sanitizeDualNetwork(net, splitSinglePores):
                     if "diameter" in key:
                         geo[key] = np.append(
                             geo[key], geo[key][vIdx]
-                        )  # use same diamter for both pores # TODO is this the right approach?
+                        )  # use same diamter for both pores
+                        # TODO is this the right approach? # pylint: disable=fixme
                     if "volume" in key:
                         geo[key] = np.append(
                             geo[key], 0.5 * geo[key][vIdx]
@@ -181,10 +162,8 @@ def sanitizeDualNetwork(net, splitSinglePores):
                         geo[key][vIdx] *= 0.5
 
                 poreVolume = geo["pore.region_volume"][vIdx]
-                eqRadius = (poreVolume * 3.0 / 4.0 * 1.0 / np.pi) ** (
-                    1.0 / 3.0
-                )  # assume sphere
-                eqArea = np.pi * eqRadius ** 2
+                eqRadius = (poreVolume * 3.0 / 4.0 * 1.0 / np.pi) ** (1.0 / 3.0)  # assume sphere
+                eqArea = np.pi * eqRadius**2
 
                 poreCenter0 = geo["pore.centroid"][vIdx]
                 poreCenter1 = poreCenter0 + np.array([1e-10, 1e-10, 1e-10])
@@ -192,23 +171,15 @@ def sanitizeDualNetwork(net, splitSinglePores):
 
                 for key in throatKeys:
                     if "diameter" in key:
-                        geo[key] = np.append(
-                            geo[key], 2 * eqRadius
-                        )  # use equivalent diameter
+                        geo[key] = np.append(geo[key], 2 * eqRadius)  # use equivalent diameter
                     if "area" in key:
                         geo[key] = np.append(geo[key], eqArea)  # use equivalent area
                     if "length" in key:
-                        geo[key] = np.append(
-                            geo[key], eqRadius
-                        )  # use equivalent radius
+                        geo[key] = np.append(geo[key], eqRadius)  # use equivalent radius
                     if "perimeter" in key:
-                        geo[key] = np.append(
-                            geo[key], eqRadius
-                        )  # use equivalent radius
+                        geo[key] = np.append(geo[key], eqRadius)  # use equivalent radius
                     if "centroid" in key:
-                        geo[key] = np.append(
-                            geo[key], throatCenter.reshape(-1, 3), axis=0
-                        )
+                        geo[key] = np.append(geo[key], throatCenter.reshape(-1, 3), axis=0)
 
             print(
                 "Num. void throats after dual grid sanitation",
@@ -234,8 +205,9 @@ def sanitizeDualNetwork(net, splitSinglePores):
     return net, geo
 
 
-def writeDGF(filename, net, geo, policy):
-    isDualNetwork = "pore.solid" in net.keys()
+def _computeVertexElementData(net, geo, policy):
+    """Compute element and vertex data for DGF output"""
+    # pylint: disable=too-many-locals
 
     vertices = net["pore.coords"]
     elements = net["throat.conns"]
@@ -243,7 +215,6 @@ def writeDGF(filename, net, geo, policy):
     poreRadius = policy["pore.radius"](net, geo)
     poreVolume = policy["pore.volume"](net, geo)
     poreLabel = policy["pore.label"](net, geo)
-
     throatRadius = policy["throat.radius"](net, geo)
     throatLength = policy["throat.length"](net, geo)
     throatArea = policy["throat.area"](net, geo)
@@ -252,16 +223,19 @@ def writeDGF(filename, net, geo, policy):
     throatCenter = net["throat.global_peak"]
 
     # create dgf data
+    isDualNetwork = "pore.solid" in net.keys()
     if isDualNetwork:
-        # 1D array (one entry for each throat) containg 0 for void, 1 for solid and 2 for interconnect
+        # 1D array (one entry for each throat)
+        # containing 0 for void, 1 for solid and 2 for interconnect
         throatDomainType = np.zeros(len(elements), dtype=np.int64)
         throatDomainType[net["throat.solid"]] = 1
         throatDomainType[net["throat.interconnect"]] = 2
 
-        # 1D array (one entry for each pore) containg 0 for void, 1 for solid and 2
+        # 1D array (one entry for each pore)
+        # containing 0 for void, 1 for solid and 2
         poreDomainType = np.zeros(len(vertices), dtype=np.int64)
         poreDomainType[net["pore.solid"]] = 1
-        vdata = np.stack(
+        vertexData = np.stack(
             [
                 vertices[:, 0],
                 vertices[:, 1],
@@ -273,7 +247,7 @@ def writeDGF(filename, net, geo, policy):
             ],
             axis=1,
         ).reshape(len(poreLabel), 7)
-        edata = np.stack(
+        elementData = np.stack(
             [
                 elements[:, 0],
                 elements[:, 1],
@@ -290,7 +264,7 @@ def writeDGF(filename, net, geo, policy):
             axis=1,
         ).reshape(len(throatLabel), 11)
     else:
-        vdata = np.stack(
+        vertexData = np.stack(
             [
                 vertices[:, 0],
                 vertices[:, 1],
@@ -301,7 +275,7 @@ def writeDGF(filename, net, geo, policy):
             ],
             axis=1,
         ).reshape(len(poreLabel), 6)
-        edata = np.stack(
+        elementData = np.stack(
             [
                 elements[:, 0],
                 elements[:, 1],
@@ -317,43 +291,46 @@ def writeDGF(filename, net, geo, policy):
             axis=1,
         ).reshape(len(throatLabel), 10)
 
+    return vertexData, elementData
+
+
+def writeDGF(filename, net, geo, policy):
+    """DOC ME!"""
+
+    vertexData, elementData = _computeVertexElementData(net, geo, policy)
+    isDualNetwork = "pore.solid" in net.keys()
+
     # write DGF file
     with open(filename, "w") as outputfile:
         outputfile.write("DGF\n")
         outputfile.write(
             "% Vertex parameters: PoreInscribedRadius PoreVolume PoreLabel"
-            + (" PoreDomainType" if isDualNetwork else "")
-            + "\n"
+            f"{' PoreDomainType' if isDualNetwork else ''}\n"
         )
         outputfile.write(
-            "% Element parameters: ThroatInscribedRadius ThroatLength ThroatCrossSectionalArea ThroatShapeFactor ThroatCenterX ThroatCenterY ThroatCenterZ ThroatLabel"
-            + (" ThroatDomainType" if isDualNetwork else "")
-            + "\n"
+            "% Element parameters: ThroatInscribedRadius ThroatLength ThroatCrossSectionalArea "
+            "ThroatShapeFactor ThroatCenterX ThroatCenterY ThroatCenterZ ThroatLabel"
+            f"{' ThroatDomainType' if isDualNetwork else ''}\n"
         )
         outputfile.write("Vertex\n")
-        outputfile.write("parameters " + str(len(vdata[0]) - 3) + "\n")  # vertex data
-        for i in range(len(poreLabel)):
-            outputfile.write(" ".join([str(v) for v in vdata[i]]) + "\n")
+        outputfile.write("parameters " + str(len(vertexData[0]) - 3) + "\n")  # vertex data
+        for i in range(len(vertexData)):
+            outputfile.write(" ".join([str(v) for v in vertexData[i]]) + "\n")
         outputfile.write("\n#\n")
         outputfile.write("SIMPLEX\n")
-        outputfile.write("parameters " + str(len(edata[0]) - 2) + "\n")  # cell data
-        for i in range(len(throatLabel)):
+        outputfile.write("parameters " + str(len(elementData[0]) - 2) + "\n")  # cell data
+        for i in range(len(elementData)):
             outputfile.write(
-                " ".join(
-                    [str(v if idx > 1 else int(v)) for idx, v in enumerate(edata[i])]
-                )
-                + "\n"
+                " ".join([str(v if idx > 1 else int(v)) for idx, v in enumerate(elementData[i])])
             )
-        outputfile.write("\n#\n")
+        outputfile.write("\n\n#\n")
         outputfile.write("BOUNDARYDOMAIN\ndefault 1\n#")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert .pnm to .dgf")
     parser.add_argument("file", type=str, help="The .pnm file")
-    parser.add_argument(
-        "-n", "--outputname", type=str, help="The output file name", default=""
-    )
+    parser.add_argument("-n", "--outputname", type=str, help="The output file name", default="")
     parser.add_argument(
         "-d",
         "--deleteBoundaryThroats",
@@ -368,24 +345,25 @@ if __name__ == "__main__":
     )
     args = vars(parser.parse_args())
 
-    # net contains topology and some labels, geo contains pore sizes etc
-    net, geo = load(args["file"])
-    isDualNetwork = "pore.solid" in net.keys()
+    # net contains topology and some labels, geometry contains pore sizes etc
+    network, geometry = load(args["file"])
 
     # maybe remove boundary throats
     if args["deleteBoundaryThroats"]:
         print("Removing boundary throats")
-        removeBoundaryThroats(net)
+        removeBoundaryThroats(network)
 
-    if isDualNetwork:
-        net, geo = sanitizeDualNetwork(net, args["splitSinglePores"])
+    # check if we have a dual network
+    if "pore.solid" in network.keys():
+        network, geometry = sanitizeDualNetwork(network, args["splitSinglePores"])
 
     # begin policy //////////////////////////////////////////
-    # TODO: how can we conveniently load / exchange this?
-    def throatLength(n, g):
-        elements = n["throat.conns"]
-        length = copy.deepcopy(g["throat.direct_length"])
-        poreRadius = g["pore.extended_diameter"] / 2.0
+    # TODO: How can we conveniently load / exchange this? # pylint: disable=fixme
+    def throatLengthFunc(net, geo):
+        """DOC ME!"""
+        elements = net["throat.conns"]
+        length = copy.deepcopy(geo["throat.direct_length"])
+        poreRadius = geo["pore.extended_diameter"] / 2.0
 
         for eIdx, element in enumerate(elements):
             vIdx0, vIdx1 = element[0], element[1]
@@ -393,30 +371,68 @@ if __name__ == "__main__":
 
         return np.clip(length, 1e-9, np.max(length))
 
-    def throatShapeFactor(n, g):
-        G = g["throat.cross_sectional_area"] / g["throat.perimeter"] ** 2
+    def throatShapeFactorFunc(net, geo):  # pylint: disable=unused-argument
+        """DOC ME!"""
+        transmissibilityG = geo["throat.cross_sectional_area"] / geo["throat.perimeter"] ** 2
         maxG = 1.0 / 16.0  # for circle
-        return np.clip(G, 0.0, maxG)
+        return np.clip(transmissibilityG, 0.0, maxG)
 
-    policy = {
+    def poreLabelFunc(net, geo):  # pylint: disable=unused-argument
+        """DOC ME!"""
+        label = np.full(len(net["pore.coords"]), -1, dtype=int)
+        label[net["pore.xmin"]] = 1
+        label[net["pore.xmax"]] = 2
+        label[net["pore.ymin"]] = 3
+        label[net["pore.ymax"]] = 4
+        try:
+            label[net["pore.zmin"]] = 5
+            label[net["pore.zmax"]] = 6
+        except (IndexError, KeyError):
+            pass
+
+        if "pore.fake_void" in net.keys() and "pore.fake_solid" in net.keys():
+            label[net["pore.fake_void"]] = 22
+            label[net["pore.fake_solid"]] = 33
+
+        return label
+
+    def throatLabelFunc(net, geo, policy=None):
+        """DOC ME!"""
+        # TODO add policy # pylint: disable=fixme
+        _ = policy
+        pLabels = poreLabelFunc(net, geo)
+        label = np.full(len(net["throat.conns"]), -1, dtype=int)
+
+        for eIdx, element in enumerate(net["throat.conns"]):
+            label[eIdx] = np.max(
+                pLabels[element]
+            )  # take the label with higher value, -> add policy here
+
+        if "throat.fake_void" in net.keys() and "throat.fake_solid" in net.keys():
+            label[net["throat.fake_void"]] = 22
+            label[net["throat.fake_solid"]] = 33
+
+        return label
+
+    POLICY = {
         "pore.radius": lambda n, g: g["pore.extended_diameter"] / 2.0,
         "pore.volume": lambda n, g: g["pore.region_volume"],
-        "pore.label": lambda n, g: poreLabel(n),
+        "pore.label": poreLabelFunc,
         "throat.radius": lambda n, g: g["throat.inscribed_diameter"] / 2.0,
-        "throat.length": throatLength,
+        "throat.length": throatLengthFunc,
         "throat.area": lambda n, g: g["throat.cross_sectional_area"],
-        "throat.throatShapeFactor": throatShapeFactor,
-        "throat.label": throatLabel,
+        "throat.throatShapeFactor": throatShapeFactorFunc,
+        "throat.label": throatLabelFunc,
     }
     # end policy /////////////////////////////////////////////
 
     if not args["outputname"]:
-        filename = os.path.splitext(args["file"])[0] + ".dgf"
+        outputFilename = os.path.splitext(args["file"])[0] + ".dgf"
     else:
-        filename = os.path.splitext(args["outputname"])[0] + ".dgf"
+        outputFilename = os.path.splitext(args["outputname"])[0] + ".dgf"
 
-    print("Writing", filename)
-    writeDGF(filename, net, geo, policy)
+    print("Writing", outputFilename)
+    writeDGF(outputFilename, network, geometry, POLICY)
     # export to vtk
-    op.io.VTK.save(network=net, filename=filename)
-    print("Writing", filename + ".vtp")
+    op.io.VTK.save(network=network, filename=outputFilename)
+    print("Writing", outputFilename + ".vtp")
