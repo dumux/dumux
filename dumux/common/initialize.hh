523bbb227394f5e93c325da3720d82c308554f3d
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-11-10 16:38:49 +0100

    [multithreading] Add TBB, OpenMP, Kokkos support to Dumux::initialize

--- dumux/common/initialize.hh
+++ dumux/common/initialize.hh
@@ -26,6 +26,64 @@
 
 #include <dune/common/parallel/mpihelper.hh>
 
+#if HAVE_TBB
+#include <cstdlib>
+#include <string>
+#include <oneapi/tbb/info.h>
+#include <oneapi/tbb/global_control.h>
+
+#ifndef DOXYGEN
+namespace Dumux::Detail {
+
+class TBBGlobalControl
+{
+public:
+    static oneapi::tbb::global_control& instance(int& argc, char* argv[])
+    {
+        int maxNumThreads = oneapi::tbb::info::default_concurrency();
+        if (const char* dumuxNumThreads = std::getenv("DUMUX_NUM_THREADS"))
+            maxNumThreads = std::max(1, std::stoi(std::string{ dumuxNumThreads }));
+
+        static oneapi::tbb::global_control global_limit(
+            oneapi::tbb::global_control::max_allowed_parallelism, maxNumThreads
+        );
+
+        return global_limit;
+    }
+};
+
+} // namespace Dumux::Detail
+#endif // DOXYGEN
+
+#endif // HAVE_TBB
+
+
+#if HAVE_OPENMP
+#include <omp.h>
+#endif // HAVE_OPENMP
+
+
+#if HAVE_KOKKOS
+#include <Kokkos_Core.hpp>
+
+#ifndef DOXYGEN
+namespace Dumux::Detail {
+
+class KokkosScopeGuard
+{
+public:
+    static Kokkos::ScopeGuard& instance(int& argc, char* argv[])
+    {
+        static Kokkos::ScopeGuard guard(argc, argv);
+        return guard;
+    }
+};
+
+} // namespace Dumux::Detail
+#endif // DOXYGEN
+
+#endif // HAVE_KOKKOS
+
 namespace Dumux {
 
 void initialize(int& argc, char* argv[])
@@ -33,6 +91,23 @@ void initialize(int& argc, char* argv[])
     // initialize MPI if available
     // otherwise this will create a sequential (fake) helper
     Dune::MPIHelper::instance(argc, argv);
+
+#if HAVE_TBB
+    // initialize TBB and keep global control alive
+    Detail::TBBGlobalControl::instance(argc, argv);
+#endif
+
+#if HAVE_OPENMP
+    if (const char* dumuxNumThreads = std::getenv("DUMUX_NUM_THREADS"))
+        omp_set_num_threads(
+            std::max(1, std::stoi(std::string{ dumuxNumThreads }))
+        );
+#endif
+
+#if HAVE_KOKKOS
+    // initialize Kokkos (command line / environmental variable interface)
+    Detail::KokkosScopeGuard::instance(argc, argv);
+#endif
 }
 
 } // end namespace Dumux
