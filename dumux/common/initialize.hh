1aec66b43683df7c74dd7950f48ba097f34e632b
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-11-10 16:37:35 +0100

    [common] Add Dumux::initialize to help with initialization of parallelism packages

---
+++ dumux/common/initialize.hh
@@ -0,0 +1,40 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup Common
+ * \brief The initialize function to be called before using Dumux
+ */
+#ifndef DUMUX_COMMON_INITIALIZE_HH
+#define DUMUX_COMMON_INITIALIZE_HH
+
+#include <dune/common/parallel/mpihelper.hh>
+
+namespace Dumux {
+
+void initialize(int& argc, char* argv[])
+{
+    // initialize MPI if available
+    // otherwise this will create a sequential (fake) helper
+    Dune::MPIHelper::instance(argc, argv);
+}
+
+} // end namespace Dumux
+
+#endif
