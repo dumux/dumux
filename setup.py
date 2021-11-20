b03b5ddd0574679456c80fdee043e4c400e51391
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-11-20 03:14:31 +0100

    [python][cleanup] Run black on setup.py

--- setup.py
+++ setup.py
@@ -9,28 +9,31 @@ from skbuild import setup
 # > python setup.py sdist
 # > python -m twine upload dist/*
 
-dumuxVersion = '3.4.0.dev20210308'
-duneVersion  = '2.8.0.dev20210308'
+dumuxVersion = "3.4.0.dev20210308"
+duneVersion = "2.8.0.dev20210308"
 
 metadata = metaData(duneVersion)[1]
-metadata['version'] = dumuxVersion
-metadata['long_description'] = metadata['long_description'].replace(
-    'doc/logo/dumux_logo_hires_whitebg.png',
-    'https://dumux.org/images/logo.svg'
+metadata["version"] = dumuxVersion
+metadata["long_description"] = metadata["long_description"].replace(
+    "doc/logo/dumux_logo_hires_whitebg.png", "https://dumux.org/images/logo.svg"
 )
-metadata['packages'] = find_namespace_packages(where='python', include=['dumux.*'])
+metadata["packages"] = find_namespace_packages(where="python", include=["dumux.*"])
 
 # auto-generate pyproject.toml with duneVersion when building sdist
 from skbuild.command.sdist import sdist
+
+
 class mysdist(sdist):
     def run(self):
-        requires = ['setuptools', 'wheel', 'scikit-build', 'cmake', 'ninja', 'requests']
-        requires += metadata['install_requires']
-        with open('pyproject.toml', 'w') as f:
+        requires = ["setuptools", "wheel", "scikit-build", "cmake", "ninja", "requests"]
+        requires += metadata["install_requires"]
+        with open("pyproject.toml", "w") as f:
             f.write("[build-system]\n")
-            f.write("requires = ['"+"', '".join(requires)+"']\n")
+            f.write("requires = ['" + "', '".join(requires) + "']\n")
             f.write("build-backend = 'setuptools.build_meta'\n")
         sdist.run(self)
-metadata['cmdclass'] = {'sdist': mysdist}
+
+
+metadata["cmdclass"] = {"sdist": mysdist}
 
 setup(**metadata)
