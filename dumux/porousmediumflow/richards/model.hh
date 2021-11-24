5b6365cda747645a4615884bdfa748e1a9b59bbe
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2021-11-24 13:27:53 +0100

    [richards] remove unnecessary inclusion of spatial params header

--- dumux/porousmediumflow/richards/model.hh
+++ dumux/porousmediumflow/richards/model.hh
@@ -98,7 +98,6 @@
 #include <dumux/porousmediumflow/compositional/switchableprimaryvariables.hh>
 #include <dumux/material/fluidmatrixinteractions/diffusivitymillingtonquirk.hh>
 #include <dumux/material/fluidmatrixinteractions/2p/thermalconductivity/somerton.hh>
-#include <dumux/material/spatialparams/fv.hh>
 #include <dumux/material/components/simpleh2o.hh>
 #include <dumux/material/fluidsystems/h2oair.hh>
 #include <dumux/material/fluidstates/immiscible.hh>
