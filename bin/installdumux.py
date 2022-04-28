82b763ba80b5652782d98a2a75ba374675666bbb
Yue Wang <yue.wang@iws.uni-stuttgart.de>
2022-04-28 21:40:17 +0200

    [script/handbook] update release branch in script

--- bin/installdumux.py
+++ bin/installdumux.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(
 )
 # Optional arguments
 parser.add_argument("--dune-version", default="2.8", help="Dune version to be checked out.")
-parser.add_argument("--dumux-version", default="3.4", help="Dumux version to be checked out.")
+parser.add_argument("--dumux-version", default="3.5", help="Dumux version to be checked out.")
 args = vars(parser.parse_args())
 
 duneBranch = (
