0b5853b69976c45c1495c59829c4b2dddfaf2a29
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2022-05-04 12:07:05 +0200

    [tests][ff][convergence] Move base script to upper folder and reuse functions

--- test/freeflow/navierstokes/donea/run_simulations.py
+++ test/freeflow/navierstokes/donea/run_simulations.py
@@ -2,114 +2,21 @@
 
 import os
 import sys
-import subprocess
-from math import pow
+
 from argparse import ArgumentParser
-import numpy as np
-import matplotlib.pyplot as plt
 
-def initializePlot():
-    plt.figure()
-    plt.clf()
-
-
-def plotResultsFromSubFolder(fileName, idx, label):
-    errors = np.genfromtxt(fileName, delimiter=',',skip_header=0, dtype=float)
-    h = errors[:,2]
-    values = errors[:,idx]
-    plt.loglog(h, values, label=label, marker='o')
-    plt.legend()
-    plt.grid(True)
+try:
+    commonPath = os.path.dirname(os.path.abspath(__file__))
+    # It is assumed that functions are provided by file in parent folder
+    commonPath = commonPath.rsplit("/build-cmake")[0] + commonPath.rsplit("/build-cmake")[1] + "/.."
+    sys.path.append(commonPath)
+    from convergencetests import runTestsAndPlotResults
+except Exception:
+    sys.exit("Could not import runTestsAndPlotResults")
 
 
-def plotReferenceCurve(fileName, idx):
-    errors = np.genfromtxt(fileName, delimiter=',',skip_header=0, dtype=float)
-    h = errors[:,2]
-    values = errors[:,idx]
-    referenceValue = values[0]/1.2
-    plt.loglog(h,
-              [referenceValue*h[i]*h[i] / (h[0]*h[0]) for i in range(len(values))],
-              marker="*",
-              linestyle="--",
-              label="$\mathcal{O}(h^2)$")
-    plt.legend()
+#sys.path.append('/home/martins/DUMUX/dumux/test/freeflow/navierstokes')
 
-
-def setAxesLabels(xlabel, ylabel):
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-
-
-def savePlot(name):
-    plt.savefig(name, bbox_inches="tight")
-
-def exitWithError(msg):
-    sys.stderr.write(msg)
-    sys.exit(1)
-
-
-def fileHasExtension(file, ext):
-    fileExt = os.path.splitext(file)[1]
-    return fileExt == ext or fileExt.strip(".") == ext
-
-
-def findFilesWithExtension(ext):
-    return list(filter(
-        lambda f: os.path.isfile(f) and fileHasExtension(f, ext),
-        os.listdir()
-    ))
-
-
-def applyToFilesWithExtension(ext, functor):
-    for file in findFilesWithExtension(ext):
-        functor(file)
-
-
-def removeFilesWithExtension(ext):
-    applyToFilesWithExtension(ext, os.remove)
-
-
-def moveFilesWithExtension(ext, targetFolder):
-    def makeTargetPath(f):
-        return os.path.join(targetFolder, f)
-    applyToFilesWithExtension(ext, lambda f: os.replace(f, makeTargetPath(f)))
-
-
-def removePreviousResults():
-    removeFilesWithExtension("vtu")
-    removeFilesWithExtension("vtp")
-    removeFilesWithExtension("pvd")
-    removeFilesWithExtension("log")
-    removeFilesWithExtension("csv")
-
-
-def moveResultsToSubFolder(folderName):
-    moveFilesWithExtension("vtu", folderName)
-    moveFilesWithExtension("vtp", folderName)
-    moveFilesWithExtension("pvd", folderName)
-    moveFilesWithExtension("log", folderName)
-    moveFilesWithExtension("csv", folderName)
-
-def compile(exeName):
-    subprocess.run(["make", exeName], check=True)
-
-
-def runConvergenceTest(exeName, outputName, numRefinements=5, extraArgs=[]):
-    for i in range(numRefinements):
-        baseCall = [
-            "./" + exeName, "params.input",
-            "-Grid.Refinement", str(i),
-            "-Vtk.OutputName", str(outputName)
-        ] + extraArgs
-        subprocess.run(" ".join(str(x) for x in baseCall), shell=True, executable='/bin/bash')
-
-
-def runConvergenceTestAndMoveToFolder(
-    exeName, folderName, outputName, numRefinements=5, extraArgs=[]
-):
-    os.makedirs(folderName, exist_ok=True)
-    runConvergenceTest(exeName, outputName, numRefinements, extraArgs)
-    moveResultsToSubFolder(folderName)
 
 if __name__ == "__main__":
     parser = ArgumentParser(
@@ -121,45 +28,17 @@ if __name__ == "__main__":
     args = vars(parser.parse_args())
 
     numRefinements = int(args["num_refinements"])
-    #exe = str(args["executable"])
-    #outputName = str(args["outputname"])
-
-    removePreviousResults()
 
     testPrefix = "donea-momentum"
     executables = ["test_ff_stokes_donea_momentum_diamond_simplex", "test_ff_stokes_donea_momentum_diamond_quad"]
-    gridArgsTests = [ [ [], ["-Grid.File ./grids/unstructured_simplex.msh"]],
-                      [ [], ["-Grid.File ./grids/unstructured_quad.msh"   ]] ]
-    gridNamesTests = [ ["simplex-structured", "simplex-unstructured" ],
-                       ["quad-structured", "quad-unstructured"       ] ]
+    grids =    [
+                [ ["simplex-structured", []], ["simplex-unstructured", ["-Grid.File ./grids/unstructured_simplex.msh"]] ],
+                [ ["quad-structured", []],    ["quad-unstructured",    ["-Grid.File ./grids/unstructured_quad.msh"   ]] ]
+               ]
 
-    ##################################################
-    # structured simplex grid
-    ##################################################
+    methods =    [
+                  ["weak-sym", []],
+                  ["unsym", ["-FreeFlow.EnableUnsymmetrizedVelocityGradient", "true"]]
+                 ]
 
-    for k in range(len(executables)):
-        exe = executables[k]
-        compile(exe)
-        gridArgs = gridArgsTests[k]
-        gridNames = gridNamesTests[k]
-        for i in range(len(gridArgs)):
-            initializePlot()
-            # weak symmetry
-            folderName = testPrefix + "-" + gridNames[i]
-
-            methods = ["weak-sym", "unsym"]
-            methodArgs = [ [], ["-FreeFlow.EnableUnsymmetrizedVelocityGradient", "true"]]
-
-            for j in range(len(methodArgs)):
-                method = methods[j]
-                outputName = folderName + "-" + method
-                extraArgs = gridArgs[i] + methodArgs[j]
-                runConvergenceTestAndMoveToFolder(
-                    exe, folderName, outputName, numRefinements, extraArgs
-                )
-                fileName = folderName + "/" + "errors.csv"
-                plotResultsFromSubFolder(fileName, 3, method)
-
-            plotReferenceCurve(fileName, 3)
-            setAxesLabels("$h_\mathrm{max}$", "$e_{v}$")
-            savePlot(folderName + "/" + "vel-error-" + folderName + ".pdf")
+    runTestsAndPlotResults(testPrefix, executables, grids, methods, numRefinements)
