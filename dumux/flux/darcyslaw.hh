8b234649e38b934cbf4a40b79bd58c204e10beda
Theresa Schollenberger <theresa.schollenberger@iws.uni-stuttgart.de>
2022-06-29 15:11:06 +0200

    [flux][documentation] add description for Darcys law

--- dumux/flux/darcyslaw.hh
+++ dumux/flux/darcyslaw.hh
@@ -19,10 +19,29 @@
 /*!
  * \file
  * \ingroup Flux
- * \brief Darcy's law specialized for different discretization schemes
- *        This file contains the data which is required to calculate
- *        volume and mass fluxes of fluid phases over a face of a finite volume by means
- *        of the Darcy approximation. Specializations are provided for the different discretization methods.
+ * \brief
+ *
+ * Darcy's law describes the advective flux in porous media on the macro-scale and is valid for Reynolds numbers below 1.
+ * The advective flux characterizes the bulk flow for each fluid phase including all components in case of compositional flow.
+ * It is driven by the potential gradient \f$\textbf{grad}\, p - \varrho {\textbf g}\f$,
+ * accounting for both pressure difference and gravitation.
+ * The velocity is proportional to the potential gradient with the proportional factor \f$\frac{\textbf K}{\mu}\f$,
+ * including the intrinsic permeability of the porous medium, and the viscosity µ of the fluid phase. For one-phase flow it is:
+ * \f[
+ * v = - \frac{\mathbf K}{\mu}
+ * \left(\textbf{grad}\, p - \varrho {\mathbf g} \right)
+ * \f]
+ * This equation can be extended to calculate the velocity \f$v_\alpha\f$ of phase \f$\alpha\f$ in the case of multi-phase
+ * flow by considering the relative permeability \f$k_{r\alpha}\f$:
+ * \f[
+ * v_\alpha = - \frac{k_{r\alpha}}{\mu_\alpha} \mathbf{K}
+ * \left(\text{grad}\, p_\alpha - \varrho_{\alpha} \mathbf{g} \right)
+ * \f]
+ *
+ * Darcy's law is specialized for different discretization schemes.
+ * This file contains the data which is required to calculate
+ * volume and mass fluxes of fluid phases over a face of a finite volume by means
+ * of the Darcy approximation. See the corresponding header files for the specific different discretization methods.
  */
 #ifndef DUMUX_FLUX_DARCYS_LAW_HH
 #define DUMUX_FLUX_DARCYS_LAW_HH
