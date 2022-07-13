00ac1c500f0eebf6d4b5dbee31b855aeb43ba36b
Maziar Veyskarami <maziar.veyskarami@iws.uni-stuttgart.de>
2022-07-13 10:18:32 +0200

    [porenetwork][2pnc] add model, volumevariable and iofields files, necessary for 2pnc pore network model

---
+++ dumux/porenetwork/2pnc/iofields.hh
@@ -0,0 +1,79 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup PoreNetworkModel
+ * \copydoc Dumux::PoreNetwork::TwoPNCIOFields
+ */
+#ifndef DUMUX_PNM_2P_NC_IO_FIELDS_HH
+#define DUMUX_PNM_2P_NC_IO_FIELDS_HH
+
+#include <dumux/porousmediumflow/2pnc/iofields.hh>
+#include <dumux/porenetwork/common/iofields.hh>
+
+namespace Dumux::PoreNetwork {
+
+/*!
+ * \ingroup PoreNetworkTwoPNCModel
+ * \brief Adds output fields specific to the PNM 2pnc model
+ */
+class TwoPNCIOFields
+{
+public:
+    template <class OutputModule>
+    static void initOutputModule(OutputModule& out)
+    {
+         // use default fields from the 2pnc model
+        Dumux::TwoPNCIOFields::initOutputModule(out);
+
+        using VolumeVariables = typename OutputModule::VolumeVariables;
+        using FS = typename VolumeVariables::FluidSystem;
+
+        CommonIOFields::initOutputModule(out);
+
+        out.addFluxVariable([](const auto& fluxVars, const auto& fluxVarsCache)
+                              { return fluxVarsCache.pcEntry(); }, "pcEntry");
+
+        out.addFluxVariable([](const auto& fluxVars, const auto& fluxVarsCache)
+                              { return fluxVarsCache.transmissibility(FS::phase0Idx); }, "transmissibilityW");
+
+        out.addFluxVariable([](const auto& fluxVars, const auto& fluxVarsCache)
+                              { return fluxVarsCache.transmissibility(FS::phase1Idx); }, "transmissibilityN");
+
+        auto volumeFluxW = [](const auto& fluxVars, const auto& fluxVarsCache)
+        {
+            auto upwindTerm = [](const auto& volVars) { return volVars.mobility(FS::phase0Idx); };
+            using std::abs;
+            return abs(fluxVars.advectiveFlux(FS::phase0Idx, upwindTerm));
+        };
+        out.addFluxVariable(volumeFluxW, "volumeFluxW");
+
+        auto volumeFluxN = [](const auto& fluxVars, const auto& fluxVarsCache)
+        {
+            auto upwindTerm = [](const auto& volVars) { return volVars.mobility(FS::phase1Idx); };
+            using std::abs;
+            return abs(fluxVars.advectiveFlux(FS::phase1Idx, upwindTerm));
+        };
+        out.addFluxVariable(volumeFluxN, "volumeFluxN");
+    }
+};
+
+} // end namespace Dumux::PoreNetwork
+
+#endif
---
+++ dumux/porenetwork/2pnc/model.hh
@@ -0,0 +1,248 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \brief Base class for all models which use the one-phase,
+ *        fully implicit model.
+ *        Adaption of the fully implicit scheme to the one-phase flow model.
+ */
+
+#ifndef DUMUX_PNM_2P_NC_MODEL_HH
+#define DUMUX_PNM_2P_NC_MODEL_HH
+
+#include <dumux/common/properties.hh>
+#include <dumux/porenetwork/properties.hh>
+#include <dumux/flux/porenetwork/fickslaw.hh>
+
+#include <dumux/porousmediumflow/nonisothermal/model.hh>
+#include <dumux/porousmediumflow/nonisothermal/indices.hh>
+#include <dumux/porousmediumflow/nonisothermal/iofields.hh>
+
+#include <dumux/flux/porenetwork/advection.hh>
+#include <dumux/porousmediumflow/2pnc/model.hh>
+#include <dumux/porousmediumflow/compositional/switchableprimaryvariables.hh>
+#include <dumux/material/fluidmatrixinteractions/porenetwork/throat/transmissibility1p.hh>
+#include <dumux/material/fluidmatrixinteractions/porenetwork/throat/transmissibility2p.hh>
+#include <dumux/material/fluidmatrixinteractions/porenetwork/pore/2p/multishapelocalrules.hh>
+
+#include <dumux/porenetwork/2p/fluxvariablescache.hh>
+#include <dumux/porenetwork/2p/gridfluxvariablescache.hh>
+#include <dumux/porenetwork/2p/spatialparams.hh>
+#include <dumux/porousmediumflow/compositional/localresidual.hh>
+
+
+#include "iofields.hh"
+#include "volumevariables.hh"
+
+namespace Dumux::Properties{
+// \{
+//////////////////////////////////////////////////////////////////
+// Type tags
+//////////////////////////////////////////////////////////////////
+
+//! The type tags for the implicit two-phase problems
+// Create new type tags
+namespace TTag {
+struct PNMTwoPNC { using InheritsFrom = std::tuple<PoreNetworkModel, TwoPNC>; };
+
+//! The type tags for the corresponding non-isothermal problems
+struct PNMTwoPNCNI { using InheritsFrom = std::tuple<PNMTwoPNC>; };
+} // end namespace TTag
+
+//////////////////////////////////////////////////////////////////
+// Property tags
+//////////////////////////////////////////////////////////////////
+
+//! Set the volume variables property
+template<class TypeTag>
+struct VolumeVariables<TypeTag, TTag::PNMTwoPNC>
+{
+private:
+    using PV = GetPropType<TypeTag, Properties::PrimaryVariables>;
+    using FSY = GetPropType<TypeTag, Properties::FluidSystem>;
+    using FST = GetPropType<TypeTag, Properties::FluidState>;
+    using SSY = GetPropType<TypeTag, Properties::SolidSystem>;
+    using SST = GetPropType<TypeTag, Properties::SolidState>;
+    using PT = typename GetPropType<TypeTag, Properties::SpatialParams>::PermeabilityType;
+    using MT = GetPropType<TypeTag, Properties::ModelTraits>;
+    using DM = typename GetPropType<TypeTag, Properties::GridGeometry>::DiscretizationMethod;
+    static constexpr bool enableIS = getPropValue<TypeTag, Properties::EnableBoxInterfaceSolver>();
+    // class used for scv-wise reconstruction of nonwetting phase saturations
+    using SR = TwoPScvSaturationReconstruction<DM, enableIS>;
+    using BaseTraits = TwoPVolumeVariablesTraits<PV, FSY, FST, SSY, SST, PT, MT, SR>;
+
+    using DT = GetPropType<TypeTag, Properties::MolecularDiffusionType>;
+    using EDM = GetPropType<TypeTag, Properties::EffectiveDiffusivityModel>;
+
+    template<class BaseTraits, class DT, class EDM>
+    struct NCTraits : public BaseTraits
+    {
+        using DiffusionType = DT;
+        using EffectiveDiffusivityModel = EDM;
+    };
+public:
+    using type = PoreNetwork::TwoPNCVolumeVariables<NCTraits<BaseTraits, DT, EDM>>;
+};
+
+//! The primary variables vector for the 2pnc model
+template<class TypeTag>
+struct PrimaryVariables<TypeTag, TTag::PNMTwoPNC>
+{
+private:
+    using PrimaryVariablesVector = Dune::FieldVector<GetPropType<TypeTag, Properties::Scalar>,
+                                                     GetPropType<TypeTag, Properties::ModelTraits>::numEq()>;
+public:
+    using type = SwitchablePrimaryVariables<PrimaryVariablesVector, int>;
+};
+
+template<class TypeTag>
+struct Formulation<TypeTag, TTag::PNMTwoPNC>
+{ static constexpr auto value = TwoPFormulation::p0s1; };
+
+
+//! The spatial parameters to be employed.
+//! Use PNMTwoPSpatialParams by default.
+template<class TypeTag>
+struct SpatialParams<TypeTag, TTag::PNMTwoPNC>
+{
+private:
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using LocalRules = PoreNetwork::FluidMatrix::MultiShapeTwoPLocalRules<Scalar>;
+public:
+    using type = PoreNetwork::TwoPDefaultSpatialParams<GridGeometry, Scalar, LocalRules>;
+};
+
+//! The advection type
+template<class TypeTag>
+struct AdvectionType<TypeTag, TTag::PNMTwoPNC>
+{
+private:
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using S = PoreNetwork::TransmissibilityPatzekSilin<Scalar>;
+    using W = PoreNetwork::WettingLayerTransmissibility::RansohoffRadke<Scalar>;
+    using N = PoreNetwork::NonWettingPhaseTransmissibility::BakkeOren<Scalar>;
+
+public:
+    using type = PoreNetwork::CreepingFlow<Scalar, S, W, N>;
+};
+
+template<class TypeTag>
+struct IOFields<TypeTag, TTag::PNMTwoPNC> { using type = PoreNetwork::TwoPNCIOFields; };
+
+template<class TypeTag>
+struct EnergyLocalResidual<TypeTag, TTag::PNMTwoPNC> { using type = Dumux::EnergyLocalResidual<TypeTag> ; };
+
+//! The grid flux variables cache vector class
+template<class TypeTag>
+struct GridFluxVariablesCache<TypeTag, TTag::PNMTwoPNC>
+{
+private:
+    static constexpr bool enableCache = getPropValue<TypeTag, Properties::EnableGridFluxVariablesCache>();
+    using Problem = GetPropType<TypeTag, Properties::Problem>;
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using FluxVariablesCache = GetPropTypeOr<TypeTag,
+        Properties::FluxVariablesCache, FluxVariablesCaching::EmptyCache<Scalar>
+    >;
+    using Traits = PoreNetwork::PNMTwoPDefaultGridFVCTraits<Problem, FluxVariablesCache>;
+public:
+    using type = PoreNetwork::PNMTwoPGridFluxVariablesCache<Problem, FluxVariablesCache, enableCache, Traits>;
+};
+
+//! The flux variables cache
+template<class TypeTag>
+struct FluxVariablesCache<TypeTag, TTag::PNMTwoPNC>
+{ using type = PoreNetwork::TwoPFluxVariablesCache<GetPropType<TypeTag, Properties::AdvectionType>>; };
+
+//! We use fick's law as the default for the diffusive fluxes
+template<class TypeTag>
+struct MolecularDiffusionType<TypeTag, TTag::PNMTwoPNC>
+{
+private:
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using ModelTraits = GetPropType<TypeTag, Properties::ModelTraits>;
+public:
+    using type = PoreNetwork::PNMFicksLaw<Scalar, ModelTraits::numFluidPhases(), ModelTraits::numFluidComponents()>;
+};
+
+//////////////////////////////////////////////////////////////////
+// Property values for isothermal model required for the general non-isothermal model
+//////////////////////////////////////////////////////////////////
+template<class TypeTag>
+struct ModelTraits<TypeTag, TTag::PNMTwoPNCNI>
+{
+private:
+    //! we use the number of components specified by the fluid system here
+    using FluidSystem = GetPropType<TypeTag, Properties::FluidSystem>;
+    static_assert(FluidSystem::numPhases == 2, "Only fluid systems with 2 fluid phases are supported by the 2p-nc model!");
+    using IsothermalTraits = TwoPNCModelTraits<FluidSystem::numComponents,
+                                               getPropValue<TypeTag, Properties::UseMoles>(),
+                                               getPropValue<TypeTag, Properties::SetMoleFractionsForFirstPhase>(),
+                                               getPropValue<TypeTag, Properties::Formulation>(), getPropValue<TypeTag, Properties::ReplaceCompEqIdx>()>;
+public:
+    using type = PorousMediumFlowNIModelTraits<IsothermalTraits>;
+}; //!< The model traits of the non-isothermal model
+
+//! Set the volume variables property
+template<class TypeTag>
+struct VolumeVariables<TypeTag, TTag::PNMTwoPNCNI>
+{
+private:
+    using PV = GetPropType<TypeTag, Properties::PrimaryVariables>;
+    using FSY = GetPropType<TypeTag, Properties::FluidSystem>;
+    using FST = GetPropType<TypeTag, Properties::FluidState>;
+    using SSY = GetPropType<TypeTag, Properties::SolidSystem>;
+    using SST = GetPropType<TypeTag, Properties::SolidState>;
+    using PT = typename GetPropType<TypeTag, Properties::SpatialParams>::PermeabilityType;
+    using MT = GetPropType<TypeTag, Properties::ModelTraits>;
+    using DM = typename GetPropType<TypeTag, Properties::GridGeometry>::DiscretizationMethod;
+    static constexpr bool enableIS = getPropValue<TypeTag, Properties::EnableBoxInterfaceSolver>();
+    // class used for scv-wise reconstruction of nonwetting phase saturations
+    using SR = TwoPScvSaturationReconstruction<DM, enableIS>;
+    using BaseTraits = TwoPVolumeVariablesTraits<PV, FSY, FST, SSY, SST, PT, MT, SR>;
+
+    using DT = GetPropType<TypeTag, Properties::MolecularDiffusionType>;
+    using EDM = GetPropType<TypeTag, Properties::EffectiveDiffusivityModel>;
+    using ETCM = GetPropType< TypeTag, Properties:: ThermalConductivityModel>;
+
+    template<class BaseTraits, class DT, class EDM, class ETCM>
+    struct NCNITraits : public BaseTraits
+    {
+        using DiffusionType = DT;
+        using EffectiveDiffusivityModel = EDM;
+        using EffectiveThermalConductivityModel = ETCM;
+    };
+public:
+    using type = PoreNetwork::TwoPNCVolumeVariables<NCNITraits<BaseTraits, DT, EDM, ETCM>>;
+};
+
+//! Set non-isothermal output fields
+template<class TypeTag>
+struct IOFields<TypeTag, TTag::PNMTwoPNCNI> { using type = EnergyIOFields<PoreNetwork::TwoPNCIOFields>; };
+
+template<class TypeTag>
+struct ThermalConductivityModel<TypeTag, TTag::PNMTwoPNCNI>
+{
+    using type = ThermalConductivitySomerton<GetPropType<TypeTag, Properties::Scalar>>;
+}; //!< Use the average for effective conductivities
+
+} // end namespace Dumux::Properties
+
+#endif
