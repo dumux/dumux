77cc63406eb0e76c759e420983baee1714bbac49
hanchuan <whc.shmily@gmail.com>
2022-07-25 10:35:21 +0200

    [inexact-newton] Simplify newton solver

--- dumux/porenetwork/2p/newtonsolver.hh
+++ dumux/porenetwork/2p/newtonsolver.hh
@@ -49,20 +49,6 @@ class TwoPNewtonSolver : public Dumux::NewtonSolver<Assembler, LinearSolver>
 public:
     using ParentType::ParentType;
 
-    void newtonBegin(Variables &vars)
-    {
-        auto upwindNewtonInitial = getParam<bool>("Newton.UpwindInitial", false);
-        if (upwindNewtonInitial)
-        {
-            auto& gridVariables = this->assembler().gridVariables();
-            auto& invasionState = gridVariables.gridFluxVarsCache().invasionState();
-            auto& uCurrentIter = Backend::dofs(vars);
-            invasionState.updateForFirstStep(uCurrentIter, gridVariables.curGridVolVars(),
-                                         gridVariables.gridFluxVarsCache());
-        }
-        ParentType::newtonBegin(vars);
-    }
-
     /*!
      * \brief Called after each Newton update
      *
@@ -79,28 +65,6 @@ public:
         auto& invasionState = gridVariables.gridFluxVarsCache().invasionState();
 
         invasionState.update(uCurrentIter, gridVariables.curGridVolVars(), gridVariables.gridFluxVarsCache());
-
-        // If the solution is about to be accepted, check for accuracy and trigger a retry
-        // with a decreased time step size if necessary.
-        if (newtonConverged())
-        {
-            NewtonConsistencyChecks<typename Assembler::GridVariables, SolutionVector> checks;
-            checks.performChecks(gridVariables, uCurrentIter, this->assembler().prevSol());
-        }
-    }
-
-    /*!
-     * \brief Returns true if the current solution can be considered to
-     *        be accurate enough. We enforce an additional Newton iteration if the invasion
-     *        switch was triggered in the last iteration.
-     */
-    bool newtonConverged() const final
-    {
-        //!TODO: last iteration not chopped
-        if (lastIterationWasChopped_)
-            return false;
-
-        return ParentType::newtonConverged();
     }
 
     /*!
@@ -112,7 +76,6 @@ public:
         ParentType::newtonFail(u);
         auto& gridVariables = this->assembler().gridVariables();
         gridVariables.gridFluxVarsCache().invasionState().reset();
-        lastIterationWasChopped_ = false;
     }
 
     // /*!
@@ -129,74 +92,6 @@ public:
     { preIndex_ = poreindex; }
 
 private:
-
-    /*!
-     * \brief Update the current solution of the Newton method
-     *
-     * \param varsCurrentIter The variables after the current Newton iteration \f$ u^{k+1} \f$
-     * \param uLastIter The solution after the last Newton iteration \f$ u^k \f$
-     * \param deltaU The vector of differences between the last
-     *               iterative solution and the next one \f$ \Delta u^k \f$
-     */
-    void choppedUpdate_(Variables &varsCurrentIter,
-                        const SolutionVector &uLastIter,
-                        const SolutionVector &deltaU) final
-    {
-        auto uCurrentIter = uLastIter;
-        uCurrentIter -= deltaU;
-
-        // do not clamp anything after 5 iterations
-        static const int maxNumChoppedUpdates = getParam<int>("Newton.NumChoppedUpdates", 4);
-        if (maxNumChoppedUpdates && this->numSteps_ <= maxNumChoppedUpdates)
-        {
-            lastIterationWasChopped_ = true;
-            // clamp saturation change to at most 20% per iteration
-            const auto& gridGeometry = this->assembler().gridGeometry();
-            auto fvGeometry = localView(gridGeometry);
-            for (const auto& element : elements(gridGeometry.gridView()))
-            {
-                fvGeometry.bindElement(element);
-
-                for (auto&& scv : scvs(fvGeometry))
-                {
-                    auto dofIdxGlobal = scv.dofIndex();
-
-                    // calculate the old wetting phase saturation
-                    const auto& spatialParams = this->assembler().problem().spatialParams();
-                    const auto elemSol = elementSolution(element, uCurrentIter, gridGeometry);
-
-                    const auto fluidMatrixInteraction = spatialParams.fluidMatrixInteraction(element, scv, elemSol);
-                    const double pw = uLastIter[dofIdxGlobal][0];
-                    const double SnOld = uLastIter[dofIdxGlobal][1];
-
-                    // const double SwOld = 1.0 - SnOld;
-                    // const double pcOld = fluidMatrixInteraction.pc(SwOld);
-                    // const double pn = pcOld + pw;
-                    // const double pwMin = pn - fluidMatrixInteraction.pc(SwOld - 0.1);
-                    // const double pwMax = pn - fluidMatrixInteraction.pc(SwOld + 0.1);
-
-                    const double pwMin = pw - 1000;
-                    const double pwMax = pw + 1000;
-
-                    // clamp the result
-                    using std::clamp;
-                    uCurrentIter[dofIdxGlobal][0] = clamp(uCurrentIter[dofIdxGlobal][0], pwMin, pwMax);
-                    uCurrentIter[dofIdxGlobal][1] = clamp(uCurrentIter[dofIdxGlobal][1], (SnOld - 0.1), (SnOld + 0.1));
-                }
-            }
-        }
-        else
-            lastIterationWasChopped_ = false;
-
-        // update the variables
-        this->solutionChanged_(varsCurrentIter, uCurrentIter);
-
-        if (this->enableResidualCriterion())
-            this->computeResidualReduction_(varsCurrentIter);
-    }
-
-    bool lastIterationWasChopped_ = false;
-    bool switchedInLastIteration_{false};
     std::vector<bool> preIndex_; // return the pore indicies which are predicted to be invaded
 };
 
