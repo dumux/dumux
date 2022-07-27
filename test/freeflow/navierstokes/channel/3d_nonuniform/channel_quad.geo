39fcfed51a6ac78f59d7ec4e6b002be696939d7c
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2022-07-27 13:00:31 +0200

    [test][ff][diamond] Add 3d nonuniform channel test

---
+++ test/freeflow/navierstokes/channel/3d_nonuniform/channel_quad.geo
@@ -0,0 +1,21 @@
+Include "channel.geo";
+
+factor = 2;
+
+Transfinite Curve {37, 35, 60, 36} = 5*factor;
+Transfinite Curve {38, 39, 40, 41} = 5*factor;
+Transfinite Curve {42, 43, 44, 45} = 5*factor;
+Transfinite Curve {49, 46, 47, 48} = 5*factor;
+Transfinite Curve {53, 50, 51, 52} = 5*factor;
+Transfinite Curve {57, 54, 55, 56} = 5*factor;
+Transfinite Curve {8, 59, 25, 58} = 5*factor;
+Transfinite Curve {34, -16, 33, -17} = 5*factor;
+
+Transfinite Curve {15, 18, 1, 32, 7, 9, 24, 26} = 4*factor;
+Transfinite Curve {3, 30, 13, 20, 11, 22, 5, 28} = 3*factor;
+Transfinite Curve {2, 31, 14, 19, 6, 10, 23, 27} = 10*factor;
+Transfinite Curve {12, 21, 4, 29} = 7*factor;
+
+Transfinite Surface "*";
+Recombine Surface "*";
+Transfinite Volume "*";
---
+++ test/freeflow/navierstokes/channel/3d_nonuniform/main.cc
@@ -0,0 +1,167 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup NavierStokesTests
+ * \brief 3D Channel flow test for the staggered grid (Navier-)Stokes model
+ */
+
+#include <config.h>
+
+#include <iostream>
+#include <random>
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/timer.hh>
+
+#include <dumux/common/initialize.hh>
+#include <dumux/common/dumuxmessage.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/properties.hh>
+
+#include <dumux/io/vtkoutputmodule.hh>
+#include <dumux/io/grid/gridmanager_ug.hh>
+
+#include <dumux/linear/seqsolverbackend.hh>
+#include <dumux/linear/linearsolvertraits.hh>
+
+#include <dumux/multidomain/fvassembler.hh>
+#include <dumux/multidomain/traits.hh>
+#include <dumux/multidomain/newtonsolver.hh>
+
+#include <dumux/freeflow/navierstokes/velocityoutput.hh>
+
+#include "properties.hh"
+
+int main(int argc, char** argv)
+{
+    using namespace Dumux;
+
+    // define the type tag for this problem
+    using MomentumTypeTag = Properties::TTag::ThreeDChannelTestMomentum;
+    using MassTypeTag = Properties::TTag::ThreeDChannelTestMass;
+
+    // initialize MPI+X
+    initialize(argc, argv);
+    const auto& mpiHelper = Dune::MPIHelper::instance();
+
+    // print dumux start message
+    if (mpiHelper.rank() == 0)
+        DumuxMessage::print(/*firstCall=*/true);
+
+    // parse command line arguments and input file
+    Parameters::init(argc, argv);
+
+    // time this test
+    Dune::Timer timer;
+
+    // create a grid
+    using Grid = GetPropType<MassTypeTag, Properties::Grid>;
+    Dumux::GridManager<Grid> gridManager;
+    gridManager.init();
+    auto gridData = gridManager.getGridData();
+
+    // we compute on the leaf grid view
+    const auto& leafGridView = gridManager.grid().leafGridView();
+
+    // create the finite volume grid geometry
+    using MomentumGridGeometry = GetPropType<MomentumTypeTag, Properties::GridGeometry>;
+    auto momentumGridGeometry = std::make_shared<MomentumGridGeometry>(leafGridView);
+    using MassGridGeometry = GetPropType<MassTypeTag, Properties::GridGeometry>;
+    auto massGridGeometry = std::make_shared<MassGridGeometry>(leafGridView);
+
+    // the coupling manager
+    using Traits = MultiDomainTraits<MomentumTypeTag, MassTypeTag>;
+    using CouplingManager = FreeFlowCouplingManager<Traits>;
+    auto couplingManager = std::make_shared<CouplingManager>();
+
+    // the problems (boundary conditions)
+    using MomentumProblem = GetPropType<MomentumTypeTag, Properties::Problem>;
+    auto momentumProblem = std::make_shared<MomentumProblem>(momentumGridGeometry, couplingManager, gridData);
+    using MassProblem = GetPropType<MassTypeTag, Properties::Problem>;
+    auto massProblem = std::make_shared<MassProblem>(massGridGeometry, couplingManager, gridData);
+
+    // the solution vector
+    constexpr auto momentumIdx = CouplingManager::freeFlowMomentumIndex;
+    constexpr auto massIdx = CouplingManager::freeFlowMassIndex;
+    using SolutionVector = typename Traits::SolutionVector;
+    SolutionVector x;
+    x[momentumIdx].resize(momentumGridGeometry->numDofs());
+    x[massIdx].resize(massGridGeometry->numDofs());
+    std::cout << "Total number of dofs: "
+        << massGridGeometry->numDofs() + momentumGridGeometry->numDofs()*MomentumGridGeometry::GridView::dimension
+        << std::endl;
+
+    // the grid variables
+    using MomentumGridVariables = GetPropType<MomentumTypeTag, Properties::GridVariables>;
+    auto momentumGridVariables = std::make_shared<MomentumGridVariables>(momentumProblem, momentumGridGeometry);
+    using MassGridVariables = GetPropType<MassTypeTag, Properties::GridVariables>;
+    auto massGridVariables = std::make_shared<MassGridVariables>(massProblem, massGridGeometry);
+
+    // compute coupling stencil and afterwards initialize grid variables (need coupling information)
+    couplingManager->init(momentumProblem, massProblem, std::make_tuple(momentumGridVariables, massGridVariables), x);
+    massGridVariables->init(x[massIdx]);
+    momentumGridVariables->init(x[momentumIdx]);
+
+    using Assembler = MultiDomainFVAssembler<Traits, CouplingManager, DiffMethod::numeric>;
+    auto assembler = std::make_shared<Assembler>(std::make_tuple(momentumProblem, massProblem),
+                                                 std::make_tuple(momentumGridGeometry, massGridGeometry),
+                                                 std::make_tuple(momentumGridVariables, massGridVariables),
+                                                 couplingManager);
+
+    // initialize the vtk output module
+    using IOFields = GetPropType<MassTypeTag, Properties::IOFields>;
+    VtkOutputModule vtkWriter(*massGridVariables, x[massIdx], massProblem->name());
+    IOFields::initOutputModule(vtkWriter); // Add model specific output fields
+    vtkWriter.addVelocityOutput(std::make_shared<NavierStokesVelocityOutput<MassGridVariables>>());
+    vtkWriter.write(0.0);
+
+    // the linearize and solve
+    using LinearSolver = UMFPackBackend;
+    auto linearSolver = std::make_shared<LinearSolver>();
+    using NewtonSolver = MultiDomainNewtonSolver<Assembler, LinearSolver, CouplingManager>;
+    NewtonSolver nonLinearSolver(assembler, linearSolver, couplingManager);
+    nonLinearSolver.solve(x);
+
+    // write vtk output
+    vtkWriter.write(1.0);
+
+    // compute influx and outflux
+    couplingManager->updateSolution(x);
+    momentumProblem->computeFluxes(x[momentumIdx]);
+
+#if HAVE_DUNE_FOAMGRID
+    momentumProblem->computeWallShearStress(x[momentumIdx], *momentumGridVariables, *couplingManager, *assembler);
+#endif
+
+    timer.stop();
+    const auto& comm = leafGridView.comm();
+    std::cout << "Simulation took " << timer.elapsed() << " seconds on "
+              << comm.size() << " processes.\n"
+              << "The cumulative CPU time was " << timer.elapsed()*comm.size() << " seconds.\n";
+
+    // print dumux end message
+    if (mpiHelper.rank() == 0)
+    {
+        Parameters::print();
+        DumuxMessage::print(/*firstCall=*/false);
+    }
+
+    return 0;
+}
