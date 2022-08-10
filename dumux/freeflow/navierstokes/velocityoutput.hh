10ed83f72fd7772e30d0868c51a14651fe1cd043
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2022-08-10 14:31:35 +0200

    [ff][cvfe] Implement velocity output for cvfe scheme

--- dumux/freeflow/navierstokes/velocityoutput.hh
+++ dumux/freeflow/navierstokes/velocityoutput.hh
@@ -83,8 +83,12 @@ public:
         using MomGG = std::decay_t<decltype(std::declval<CouplingManager>().problem(CouplingManager::freeFlowMomentumIndex).gridGeometry())>;
         if constexpr (MomGG::discMethod == DiscretizationMethods::fcstaggered)
             calculateVelocityForStaggeredGrid_(velocity, element, fvGeometry, elemVolVars);
-        else if constexpr (MomGG::discMethod == DiscretizationMethods::fcdiamond)
-            calculateVelocityForDiamondSchemes_(velocity, element, fvGeometry, elemVolVars);
+        else if constexpr (
+            MomGG::discMethod == DiscretizationMethods::box
+            || MomGG::discMethod == DiscretizationMethods::fcdiamond
+            || MomGG::discMethod == DiscretizationMethods::pq1bubble
+        )
+            calculateVelocityForCVFESchemes_(velocity, element, fvGeometry, elemVolVars);
     }
 
 private:
@@ -102,10 +106,10 @@ private:
         velocity[eIdx] = StaggeredVelocityReconstruction::cellCenterVelocity(getFaceVelocity, fvGeometry);
     }
 
-    void calculateVelocityForDiamondSchemes_(VelocityVector& velocity,
-                                             const Element& element,
-                                             const FVElementGeometry& fvGeometry,
-                                             const ElementVolumeVariables& elemVolVars) const
+    void calculateVelocityForCVFESchemes_(VelocityVector& velocity,
+                                          const Element& element,
+                                          const FVElementGeometry& fvGeometry,
+                                          const ElementVolumeVariables& elemVolVars) const
     {
         const auto eIdx = fvGeometry.gridGeometry().elementMapper().index(element);
         const auto getFaceVelocity = [&](const FVElementGeometry& fvG, const auto& scvf)
