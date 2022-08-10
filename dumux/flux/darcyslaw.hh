786377609add3294caa33527e9daac3016b989bd
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2022-08-10 21:09:45 +0200

    [flux] Reduce some code duplication for control-volume fe schemes

--- dumux/flux/darcyslaw.hh
+++ dumux/flux/darcyslaw.hh
@@ -48,9 +48,8 @@
 
 #include <dumux/flux/darcyslaw_fwd.hh>
 
-#include <dumux/flux/box/darcyslaw.hh>
 #include <dumux/flux/cctpfa/darcyslaw.hh>
 #include <dumux/flux/ccmpfa/darcyslaw.hh>
-#include <dumux/flux/fcdiamond/darcyslaw.hh>
+#include <dumux/flux/cvfe/darcyslaw.hh>
 
 #endif
