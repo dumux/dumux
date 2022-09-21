f046d8064a8f0c51aea8347c111bdbee1882d99f
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2022-09-21 22:58:59 +0200

    [test][1pnc][cleanup] Remove unused temperature function

--- test/porousmediumflow/1pnc/1p2c/nonisothermal/transientbc/problem.hh
+++ test/porousmediumflow/1pnc/1p2c/nonisothermal/transientbc/problem.hh
@@ -115,21 +115,6 @@ public:
     }
 
     /*!
-     * \name Problem parameters
-     */
-    // \{
-
-    /*!
-     * \brief Returns the temperature within the domain [K].
-     *
-     * This problem assumes a temperature of 20 degrees Celsius.
-     */
-    Scalar temperature() const
-    { return 273.15 + 20; } // in [K]
-
-    // \}
-
-    /*!
      * \name Boundary conditions
      */
     // \{
