03a6d523eeb4d1103be630d2b865c2daaba4002e
Mathis Kelm <mathis.kelm@iws.uni-stuttgart.de>
2022-10-04 14:04:29 +0200

    [doc] replace outdated copyright notice

--- dumux/common/doubleexpintegrationconstants.hh
+++ dumux/common/doubleexpintegrationconstants.hh
@@ -15,38 +15,6 @@
  *                                                                           *
  *   You should have received a copy of the GNU General Public License       *
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
- *                                                                           *
- *   This version is modified after the original version by John D. Cook     *
- *   see https://www.codeproject.com/                                        *
- *       Articles/31550/Fast-Numerical-Integration                           *
- *   which is licensed under BSD-2-clause, which reads as follows:           *
- *   Copyright John D. Cook                                                  *
- *                                                                           *
- *   Redistribution and use in source and binary forms, with or without      *
- *   modification, are permitted provided that the following                 *
- *   conditions are met:                                                     *
- *   1. Redistributions of source code must retain the above                 *
- *      copyright notice, this list of conditions                            *
-        and the following disclaimer.                                        *
- *   2. Redistributions in binary form must reproduce the above              *
- *      copyright notice, this list of conditions                            *
- *      and the following disclaimer                                         *
- *   in the documentation and/or other materials                             *
- *   provided with the distribution.                                         *
- *                                                                           *
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     *
- *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       *
- *   LIMITED TO, THE IMPLIED  WARRANTIES OF MERCHANTABILITY AND FITNESS      *
- *   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE          *
- *   COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,              *
- *   INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL              *
- *   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE       *
- *   GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS           *
- *   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,            *
- *   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    *
- *   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,       *
- *   EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                      *
- *                                                                           *
  *****************************************************************************/
 /*!
  * \file
