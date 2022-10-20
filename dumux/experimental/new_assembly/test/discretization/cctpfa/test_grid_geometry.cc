26276a725fe09b58c1cc6872df6367fd7b627af1
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-10-20 12:55:10 +0200

    [test][disc][tpfa] test grid geometry impl

---
+++ dumux/experimental/new_assembly/test/discretization/cctpfa/test_grid_geometry.cc
@@ -0,0 +1,237 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup CCTpfaDiscretization
+ * \brief Test for the `CCTpfaGridGeometry` class.
+ */
+#include <iostream>
+#include <cmath>
+
+#include <dune/grid/yaspgrid.hh>
+#include <dune/common/float_cmp.hh>
+#include <dune/common/timer.hh>
+
+#include <dumux/experimental/new_assembly/dumux/discretization/cctpfa/gridgeometry.hh>
+
+template<typename GridGeometry>
+auto tolerance(const GridGeometry& gg)
+{
+    typename GridGeometry::GridView::ctype tol = 0.0;
+    using std::min;
+    for (int dim = 0; dim < GridGeometry::GridView::dimension; ++dim)
+        tol = min(tol, gg.bBoxMax()[dim] - gg.bBoxMin()[dim]);
+    return tol*1e-10;
+}
+
+template<std::floating_point ctype>
+bool equal(const ctype a, const ctype b, const ctype tolerance)
+{ return Dune::FloatCmp::eq(a, b, tolerance); }
+
+template<typename ctype, int dim>
+bool equal(const Dune::FieldVector<ctype, dim>& p0,
+           const Dune::FieldVector<ctype, dim>& p1,
+           const ctype tolerance)
+{
+    return std::ranges::equal(p0, p1, [&] (const ctype a, const ctype b) {
+        return equal(a, b, tolerance);
+    });
+}
+
+template<typename GridView,
+         bool caching = false,
+         typename Traits = Dumux::DefaultCCTpfaGridGeometryTraits<GridView>>
+void testCCTpfaGridGeometry(const GridView& gridView, const std::size_t expectedNumScvf)
+{
+    Dune::Timer timer;
+    Dumux::CCTpfaGridGeometry<GridView, caching, Traits> gridGeometry{gridView};
+    timer.stop();
+    std::cout << "Construction took " << timer.elapsed() << " seconds\n";
+    return;
+    std::size_t numScv = 0;
+    std::size_t numScvf = 0;
+    for (const auto& element : elements(gridView))
+    {
+        const auto fvGeometry = localView(gridGeometry).bindElement(element);
+        for ([[maybe_unused]] const auto& scv : scvs(fvGeometry))
+            numScv++;
+        for ([[maybe_unused]] const auto& scvf : scvfs(fvGeometry))
+            numScvf++;
+    }
+
+    if (gridGeometry.numDofs() != gridView.size(0))
+        DUNE_THROW(Dune::InvalidStateException, "Unexpected numDofs()");
+    if (numScv != gridView.size(0))
+        DUNE_THROW(Dune::InvalidStateException, "Unexpected numScv()");
+    if (numScvf != expectedNumScvf)
+        DUNE_THROW(Dune::InvalidStateException, "Unexpected numScvf()");
+
+    const auto tol = tolerance(gridGeometry);
+    const auto testLocalView = [&] <typename LocalView> (const auto& element,
+                                                         const LocalView& localView,
+                                                         bool withStencil) {
+        static_assert(Dumux::Concepts::CCGridGeometryLocalView<LocalView>);
+
+        int count = 0;
+        int scvDofIdx = -1;
+        for (const auto& scv : scvs(localView))
+        {
+            count++;
+            scvDofIdx = static_cast<std::size_t>(scv.dofIndex());
+            if (!equal(scv.center(), element.geometry().center(), tol))
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected scv center");
+            if (!equal(scv.center(), localView.element().geometry().center(), tol))
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected localView-element center");
+            if (!equal(element.geometry().center(), localView.geometry(scv).center(), tol))
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected scv geometry center");
+            if (!Dune::FloatCmp::eq(scv.volume(), element.geometry().volume(), tol))
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected scv volume");
+            if (!Dune::FloatCmp::eq(localView.geometry(scv).volume(), element.geometry().volume(), tol))
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected scv geometry volume");
+        }
+
+        if (count != 1)
+            DUNE_THROW(Dune::InvalidStateException, "Unexpected number of scvs");
+
+        if (withStencil)
+        {
+            std::vector<std::size_t> neighborScvIndices;
+            for (const auto& scv : neighborScvs(localView))
+                if (neighborScvIndices.push_back(scv.dofIndex()); scv.dofIndex() == scvDofIdx)
+                    DUNE_THROW(Dune::InvalidStateException, "Unexpected neighbor scv dof");
+
+            std::vector<std::size_t> neighborInsideScvIndices;
+            for (const auto& scvf : neighborScvfs(localView))
+                neighborInsideScvIndices.push_back(localView.insideScv(scvf).dofIndex());
+
+            if (neighborScvIndices.size() < GridView::dimension)
+                DUNE_THROW(Dune::InvalidStateException,
+                           "Expected at least " << int(GridView::dimension)
+                                                << " neighbors, found: " << neighborScvIndices.size());
+
+            if (neighborInsideScvIndices.size() != neighborScvIndices.size())
+                DUNE_THROW(Dune::InvalidStateException, "Outside scv/scvf size mismatch");
+
+            std::ranges::sort(neighborScvIndices);
+            std::ranges::sort(neighborInsideScvIndices);
+            if (!std::ranges::equal(neighborInsideScvIndices, neighborScvIndices))
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected neighbor scv indices");
+
+            const auto numNeighbors = neighborScvIndices.size();
+            std::ranges::unique(neighborScvIndices);
+            if (neighborScvIndices.size() != numNeighbors)
+                DUNE_THROW(Dune::InvalidStateException, "Duplicate neighbor scvs");
+        }
+
+        count = 0;
+        for (const auto& scvf : scvfs(localView))
+        {
+            count++;
+            const auto c = scvf.center();
+            auto d = c - element.geometry().center();
+            d /= d.two_norm();
+
+            if (!equal(scvf.center(), localView.geometry(scvf).center(), tol))
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected face center");
+            if (!Dune::FloatCmp::eq(scvf.area(), localView.geometry(scvf).volume(), tol))
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected face area " << localView.geometry(scvf).volume() << " vs " << scvf.area());
+            if (!Dune::FloatCmp::eq(d*scvf.unitOuterNormal(), 1.0, tol))
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected scvf normal");
+            if (localView.insideScv(scvf).dofIndex() != scvDofIdx)
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected inside scv");
+            if (!localView.onBoundary(scvf))
+            {
+
+                d = scvf.center()
+                    - localView.insideScv(scvf).center();
+                d /= d.two_norm();
+                if (!Dune::FloatCmp::eq(d*scvf.unitOuterNormal(), 1.0, tol))
+                    DUNE_THROW(Dune::InvalidStateException, "Unexpected scvf normal");
+                if (withStencil)
+                {
+                    d = localView.outsideScv(scvf).center()
+                        - localView.insideScv(scvf).center();
+                    d /= d.two_norm();
+                    if (!Dune::FloatCmp::eq(d*scvf.unitOuterNormal(), 1.0, tol))
+                        DUNE_THROW(Dune::InvalidStateException, "Unexpected outside scv center");
+                    if (!equal(scvf.center(), localView.flipScvf(scvf).center(), tol))
+                        DUNE_THROW(Dune::InvalidStateException, "Unexpected Flip Scvf center");
+                    if (localView.insideScv(localView.flipScvf(scvf)).dofIndex()
+                        != localView.outsideScv(scvf).dofIndex())
+                        DUNE_THROW(Dune::InvalidStateException, "insiceScv(flipScvf) != outsideScv(scvf)");
+                }
+            }
+            else if (!equal(c[0], gridGeometry.bBoxMin()[0], tol) &&
+                     !equal(c[0], gridGeometry.bBoxMax()[0], tol) &&
+                     !equal(c[1], gridGeometry.bBoxMin()[1], tol) &&
+                     !equal(c[1], gridGeometry.bBoxMax()[1], tol))
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected boundary face position");
+        }
+
+        if (count != element.subEntities(1))
+            DUNE_THROW(Dune::InvalidStateException, "Unexpected number of scvfs: " << count);
+    };
+
+    for (const auto& element : elements(gridView))
+    {
+        auto fvGeometry = localView(gridGeometry);
+
+        fvGeometry.bindElement(element);
+        testLocalView(element, fvGeometry, false);
+        testLocalView(element, localView(gridGeometry).bindElement(element), false);
+
+        fvGeometry.bind(element);
+        testLocalView(element, fvGeometry, true);
+        testLocalView(element, localView(gridGeometry).bind(element), true);
+    }
+}
+
+template<typename GV>
+struct DynamicallySizedTraits : public Dumux::DefaultMapperTraits<GV>
+{
+    static constexpr auto maxNumScvfsPerElement = Dumux::Size::dynamic;
+    static constexpr auto maxNumBranchesPerScvf = Dumux::Size::dynamic;
+};
+
+void test(const Dune::YaspGrid<2>& grid)
+{
+    using GV = std::decay_t<decltype(grid.leafGridView())>;
+    using DynamicTraits = DynamicallySizedTraits<GV>;
+
+    const std::size_t numScvf = grid.leafGridView().size(0)*4;
+    testCCTpfaGridGeometry<GV, true>(grid.leafGridView(), numScvf);
+    testCCTpfaGridGeometry<GV, false>(grid.leafGridView(), numScvf);
+    testCCTpfaGridGeometry<GV, true, DynamicTraits>(grid.leafGridView(), numScvf);
+    testCCTpfaGridGeometry<GV, false, DynamicTraits>(grid.leafGridView(), numScvf);
+}
+
+void test(const Dune::YaspGrid<3>& grid)
+{
+    const std::size_t numScvf = grid.leafGridView().size(0)*6;
+    testCCTpfaGridGeometry(grid.leafGridView(), numScvf);
+}
+
+int main (int argc, char *argv[])
+{
+    test(Dune::YaspGrid<2>{{1.0, 1.0}, {10, 10}});
+    test(Dune::YaspGrid<3>{{1.0, 1.0, 1.0}, {10, 10, 10}});
+
+    std::cout << "\nAll tests passed" << std::endl;
+    return 0;
+}
---
+++ dumux/experimental/new_assembly/test/discretization/cctpfa/test_grid_geometry_non_conforming.cc
@@ -0,0 +1,182 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup CCTpfaDiscretization
+ * \brief Test for the `CCTpfaGridGeometry` class on a locally refined grid.
+ */
+#include <config.h>
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+#include <dune/alugrid/grid.hh>
+#include <dumux/common/initialize.hh>
+
+#include <dumux/experimental/new_assembly/dumux/discretization/cctpfa/gridgeometry.hh>
+
+static constexpr double domainSize = 1.0;
+static constexpr int numCellsPerSide = 3;
+
+template<typename Element>
+bool isCenterElement(const Element& element)
+{
+    using std::abs;
+    const auto center = element.geometry().center();
+    return abs(center[0] - domainSize/2.0) < 1e-6
+        && abs(center[1] - domainSize/2.0) < 1e-6;
+}
+
+template<typename Grid>
+void refineCenterElement(Grid& grid)
+{
+    for (const auto& element : elements(grid.leafGridView()))
+        if (isCenterElement(element))
+            grid.mark(1, element);
+
+    grid.preAdapt();
+    if (!grid.adapt())
+        DUNE_THROW(Dune::InvalidStateException, "Adaptation did not occur");
+    grid.postAdapt();
+}
+
+template<typename Grid>
+auto makeGrid()
+{
+    std::unique_ptr<Grid> grid = Dune::StructuredGridFactory<Grid>::createCubeGrid(
+        {0.0, 0.0}, {domainSize,domainSize}, {numCellsPerSide, numCellsPerSide}
+    );
+
+    refineCenterElement(*grid);
+    if (grid->leafGridView().size(0) != 12)
+        DUNE_THROW(Dune::InvalidStateException, "Unexpected number of cells after adaptation: " << grid->leafGridView().size(0));
+
+    return grid;
+}
+
+template<typename GridView, typename Element>
+std::vector<double> expectedScvfAreas(const GridView& gridView, const Element& element)
+{
+    std::vector<double> areas;
+    for (const auto& is : intersections(gridView, element))
+        areas.push_back(is.geometry().volume());
+    return areas;
+}
+
+template<typename GridView, typename Element>
+auto expectedScvfCenters(const GridView& gridView, const Element& element)
+{
+    std::vector<typename Element::Geometry::GlobalCoordinate> centers;
+    for (const auto& is : intersections(gridView, element))
+        centers.push_back(is.geometry().center());
+    return centers;
+}
+
+template<typename GridGeometry, typename Element>
+void checkScvfAreas(const GridGeometry& gridGeometry, const Element& element)
+{
+    const auto fvGeometry = localView(gridGeometry).bindElement(element);
+    auto expectedAreas = expectedScvfAreas(gridGeometry.gridView(), element);
+    std::ranges::sort(expectedAreas);
+
+    const auto testScvfAreas = [&] (auto& scvfAreas) {
+        if (expectedAreas.size() != scvfAreas.size())
+            DUNE_THROW(Dune::InvalidStateException, "Unexpected number of scvfs");
+
+        using std::abs;
+        std::ranges::sort(scvfAreas);
+        for (std::size_t i = 0; i < scvfAreas.size(); ++i)
+            if (abs(scvfAreas[i] - expectedAreas[i]) > 1e-6)
+                DUNE_THROW(Dune::InvalidStateException, "Unexpected scvf area");
+    };
+
+    std::vector<double> scvfAreas;
+    for (const auto& scvf : scvfs(fvGeometry))
+        scvfAreas.push_back(scvf.area());
+    testScvfAreas(scvfAreas);
+
+    scvfAreas.clear();
+    for (const auto& scvf : scvfs(fvGeometry))
+        scvfAreas.push_back(fvGeometry.geometry(scvf).volume());
+    testScvfAreas(scvfAreas);
+}
+
+template<typename GridGeometry, typename Element>
+void checkScvfCenters(const GridGeometry& gridGeometry, const Element& element)
+{
+    auto expectedCenters = expectedScvfCenters(gridGeometry.gridView(), element);
+
+    std::vector<typename Element::Geometry::GlobalCoordinate> scvfCenters;
+    for (const auto& scvf : scvfs(localView(gridGeometry).bind(element)))
+        scvfCenters.push_back(scvf.center());
+
+    std::ranges::all_of(scvfCenters, [&] (const auto& center) {
+        return std::ranges::any_of(expectedCenters, [&] (const auto& expectedCenter) {
+            return (expectedCenter - center).two_norm() < 1e-6;
+        });
+    });
+}
+
+
+template<typename GridGeometry, typename Element>
+void checkElementScvfs(const GridGeometry& gridGeometry, const Element& element)
+{
+    checkScvfAreas(gridGeometry, element);
+    checkScvfCenters(gridGeometry, element);
+}
+
+template<typename GridGeometry>
+void testGridGeometry(const GridGeometry& gridGeometry)
+{
+    std::size_t numScv = 0;
+    std::size_t numScvf = 0;
+    for (const auto& element : elements(gridGeometry.gridView()))
+    {
+        const auto fvGeometry = localView(gridGeometry).bindElement(element);
+        for ([[maybe_unused]] const auto& scv : scvs(fvGeometry))
+            numScv++;
+        for ([[maybe_unused]] const auto& scvf : scvfs(fvGeometry))
+            numScvf++;
+    }
+    if (numScv != 12)
+        DUNE_THROW(Dune::InvalidStateException, "Unexpected number of scvs: " << numScv);
+    if (numScvf != 52)
+        DUNE_THROW(Dune::InvalidStateException, "Unexpected number of scvfs: " << numScvf);
+
+    std::vector<std::size_t> scvfCounts;
+    for (const auto& element : elements(gridGeometry.gridView()))
+        checkElementScvfs(gridGeometry, element);
+}
+
+
+int main (int argc, char *argv[])
+{
+    Dumux::initialize(argc, argv);
+
+    using Grid = Dune::ALUGrid<2, 2, Dune::cube, Dune::nonconforming>;
+    using GridView = typename Grid::LeafGridView;
+
+    auto grid = makeGrid<Grid>();
+    testGridGeometry(Dumux::CCTpfaGridGeometry<GridView, true>{grid->leafGridView()});
+    testGridGeometry(Dumux::CCTpfaGridGeometry<GridView, false>{grid->leafGridView()});
+
+    std::cout << "\nAll tests passed" << std::endl;
+    return 0;
+}
