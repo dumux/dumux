f0e9c53ccc6d0053f6786d22c3d53d7507343760
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-03-13 18:12:09 +0100

    [mat][fs][2pimm] fix doxygen warnings

--- dumux/material/fluidsystems/2pimmiscible.hh
+++ dumux/material/fluidsystems/2pimmiscible.hh
@@ -19,7 +19,7 @@
 /*!
  * \file
  * \ingroup FluidSystems
- * \brief @copybrief Dumux::FluidSystems::TwoPImmiscible
+ * \copybrief Dumux::FluidSystems::TwoPImmiscible
  */
 #ifndef DUMUX_2P_IMMISCIBLE_FLUID_SYSTEM_HH
 #define DUMUX_2P_IMMISCIBLE_FLUID_SYSTEM_HH
@@ -65,7 +65,6 @@ class TwoPImmiscible
     static_assert(!Fluid0::isGas() || !Fluid1::isGas(), "One phase has to be a liquid!");
 
     using ThisType = TwoPImmiscible<Scalar, Fluid0, Fluid1>;
-    using Base = Dumux::FluidSystems::Base<Scalar, ThisType>;
 
 public:
     static constexpr int numPhases = 2; //!< Number of phases in the fluid system
@@ -316,11 +315,8 @@ public:
             Fluid1::Component::init(tempMin, tempMax, nTemp, pressMin, pressMax, nPress);
     }
 
-    using Base::density;
-    /*!
-     * \brief Calculate the density \f$\mathrm{[kg/m^3]}\f$ of a fluid phase
-     *
-     */
+    using Base<Scalar, ThisType>::density;
+    //! \copybrief Base<Scalar,ThisType>::density(const FluidState&,int)
     template <class FluidState>
     static Scalar density(const FluidState &fluidState,
                           int phaseIdx)
@@ -334,16 +330,8 @@ public:
         return Fluid1::density(temperature, pressure);
     }
 
-    using Base::molarDensity;
-    /*!
-     * \brief The molar density \f$\rho_{mol,\alpha}\f$
-     *   of a fluid phase \f$\alpha\f$ in \f$\mathrm{[mol/m^3]}\f$
-     *
-     * The molar density is defined by the
-     * mass density \f$\rho_\alpha\f$ and the component molar mass \f$M_\alpha\f$:
-     *
-     * \f[\rho_{mol,\alpha} = \frac{\rho_\alpha}{M_\alpha} \;.\f]
-     */
+    using Base<Scalar, ThisType>::molarDensity;
+    //! \copybrief Base<Scalar,ThisType>::molarDensity(const FluidState&,int)
     template <class FluidState>
     static Scalar molarDensity(const FluidState &fluidState, int phaseIdx)
     {
@@ -354,12 +342,8 @@ public:
        return Fluid1::molarDensity(temperature, pressure);
     }
 
-    using Base::viscosity;
-    /*!
-     * \brief Return the viscosity of a phase \f$\mathrm{[Pa*s]}\f$.
-     * \param fluidState The fluid state of the two-phase model
-     * \param phaseIdx Index of the fluid phase
-     */
+    using Base<Scalar, ThisType>::viscosity;
+    //! \copybrief Base<Scalar,ThisType>::viscosity(const FluidState&,int)
     template <class FluidState>
     static Scalar viscosity(const FluidState &fluidState,
                             int phaseIdx)
@@ -373,23 +357,8 @@ public:
         return Fluid1::viscosity(temperature, pressure);
     }
 
-    using Base::fugacityCoefficient;
-    /*!
-     * \brief Calculate the fugacity coefficient \f$\mathrm{[-]}\f$ of an individual
-     *        component in a fluid phase
-     *
-     * The fugacity coefficient \f$\mathrm{\phi^\kappa_\alpha}\f$ is connected to the
-     * fugacity \f$\mathrm{f^\kappa_\alpha}\f$ and the component's mole
-     * fraction \f$\mathrm{x^\kappa_\alpha}\f$ by means of the relation
-     *
-     * \f[
-     f^\kappa_\alpha = \phi^\kappa_\alpha\;x^\kappa_\alpha\;p_\alpha
-     * \f]
-     *
-     * \param fluidState The fluid state of the two-phase model
-     * \param phaseIdx Index of the fluid phase
-     * \param compIdx index of the component
-     */
+    using Base<Scalar, ThisType>::fugacityCoefficient;
+    //! \copybrief Base<Scalar,ThisType>::fugacityCoefficient(const FluidState&,int,int)
     template <class FluidState>
     static Scalar fugacityCoefficient(const FluidState &fluidState,
                                       int phaseIdx,
@@ -407,29 +376,8 @@ public:
         return std::numeric_limits<Scalar>::infinity();
     }
 
-    using Base::diffusionCoefficient;
-    /*!
-     * \brief Calculate the binary molecular diffusion coefficient for
-     *        a component in a fluid phase \f$\mathrm{[mol^2 * s / (kg*m^3)]}\f$
-     * \param fluidState The fluid state of the two-phase model
-     * \param phaseIdx Index of the fluid phase
-     * \param compIdx index of the component
-     *
-     * Molecular diffusion of a component \f$\mathrm{\kappa}\f$ is caused by a
-     * gradient of the chemical potential and follows the law
-     *
-     * \f[ J = - D \mathbf{grad} \mu_\kappa \f]
-     *
-     * where \f$\mathrm{\mu_\kappa]}\f$ is the component's chemical potential,
-     * \f$\mathrm{D}\f$ is the diffusion coefficient and \f$\mathrm{J}\f$ is the
-     * diffusive flux. \f$\mathrm{\mu_\kappa}\f$ is connected to the component's
-     * fugacity \f$\mathrm{f_\kappa}\f$ by the relation
-     *
-     * \f[ \mu_\kappa = R T_\alpha \mathrm{ln} \frac{f_\kappa}{p_\alpha} \f]
-     *
-     * where \f$\mathrm{p_\alpha}\f$ and \f$\mathrm{T_\alpha}\f$ are the fluid phase'
-     * pressure and temperature.
-     */
+    using Base<Scalar, ThisType>::diffusionCoefficient;
+    //! \copybrief Base<Scalar,ThisType>::diffusionCoefficient(const FluidState&,int,int)
     template <class FluidState>
     static Scalar diffusionCoefficient(const FluidState &fluidState,
                                        int phaseIdx,
@@ -440,16 +388,8 @@ public:
                    " immiscibility is assumed");
     }
 
-    using Base::binaryDiffusionCoefficient;
-    /*!
-     * \brief Given a phase's composition, temperature and pressure,
-     *        return the binary diffusion coefficient \f$\mathrm{[m^2/s]}\f$ for components
-     *        \f$\mathrm{i}\f$ and \f$\mathrm{j}\f$ in this phase.
-     * \param fluidState The fluid state of the two-phase model
-     * \param phaseIdx Index of the fluid phase
-     * \param compIIdx index of the component i
-     * \param compJIdx index of the component j
-     */
+    using Base<Scalar, ThisType>::binaryDiffusionCoefficient;
+    //! \copybrief Base<Scalar,ThisType>::binaryDiffusionCoefficient(const FluidState&,int,int,int)
     template <class FluidState>
     static Scalar binaryDiffusionCoefficient(const FluidState &fluidState,
                                              int phaseIdx,
@@ -462,12 +402,8 @@ public:
                    " immiscibility is assumed");
     }
 
-    using Base::enthalpy;
-    /*!
-     * \brief Return the specific enthalpy of a fluid phase \f$\mathrm{[J/kg]}\f$.
-     * \param fluidState The fluid state of the two-phase model
-     * \param phaseIdx Index of the fluid phase
-     */
+    using Base<Scalar, ThisType>::enthalpy;
+    //! \copybrief Base<Scalar,ThisType>::enthalpy(const FluidState&,int)
     template <class FluidState>
     static Scalar enthalpy(const FluidState &fluidState,
                                  int phaseIdx)
@@ -481,12 +417,8 @@ public:
         return Fluid1::enthalpy(temperature, pressure);
     }
 
-    using Base::thermalConductivity;
-    /*!
-     * \brief Thermal conductivity of a fluid phase \f$\mathrm{[W/(m K)]}\f$.
-     * \param fluidState The fluid state of the two-phase model
-     * \param phaseIdx Index of the fluid phase
-     */
+    using Base<Scalar, ThisType>::thermalConductivity;
+    //! \copybrief Base<Scalar,ThisType>::thermalConductivity(const FluidState&,int)
     template <class FluidState>
     static Scalar thermalConductivity(const FluidState &fluidState,
                                       int phaseIdx)
@@ -500,18 +432,8 @@ public:
         return Fluid1::thermalConductivity(temperature, pressure);
     }
 
-    using Base::heatCapacity;
-    /*!
-     * @copybrief Base::thermalConductivity
-     *
-     * Additional comments:
-     *
-     * Specific isobaric heat capacity of a fluid phase.
-     *        \f$\mathrm{[J/(kg*K)]}\f$.
-     *
-     * \param fluidState The fluid state of the two-phase model
-     * \param phaseIdx for which phase to give back the heat capacity
-     */
+    using Base<Scalar, ThisType>::heatCapacity;
+    //! \copybrief Base<Scalar,ThisType>::heatCapacity(const FluidState&,int)
     template <class FluidState>
     static Scalar heatCapacity(const FluidState &fluidState,
                                int phaseIdx)
