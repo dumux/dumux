87e8669d451b2ab5bfbb1c48fa08e79d23817c11
Hamza Oukili <hamza.oukili@iws.uni-stuttgart.de>
2023-03-24 05:19:23 +0100

    Update versions in installation files for 3.7

--- bin/installdumux.py
+++ bin/installdumux.py
@@ -30,8 +30,8 @@ parser = argparse.ArgumentParser(
     description="This script downloads and compiles the latest release of Dumux.",
 )
 # Optional arguments
-parser.add_argument("--dune-version", default="2.8", help="Dune version to be checked out.")
-parser.add_argument("--dumux-version", default="3.6", help="Dumux version to be checked out.")
+parser.add_argument("--dune-version", default="2.9", help="Dune version to be checked out.")
+parser.add_argument("--dumux-version", default="3.7", help="Dumux version to be checked out.")
 args = vars(parser.parse_args())
 
 duneBranch = (
