c6da1883fa44f66def513779d7be1fd9b98477cd
hanchuan <whc.shmily@gmail.com>
2023-07-26 12:31:10 +0200

    [test][pnm] Add a 2p test for pnm

---
+++ test/porenetwork/2pupscaling/main.cc
@@ -0,0 +1,163 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ *
+ * \brief Test for the two-phase pore-network model
+ */
+#include <config.h>
+
+#include <ctime>
+#include <iostream>
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/timer.hh>
+#include <dune/grid/io/file/dgfparser/dgfexception.hh>
+#include <dune/grid/io/file/vtk.hh>
+
+#include <dumux/assembly/fvassembler.hh>
+#include <dumux/common/initialize.hh>
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/dumuxmessage.hh>
+#include <dumux/io/grid/porenetwork/gridmanager.hh>
+#include <dumux/linear/istlsolvers.hh>
+#include <dumux/linear/linearsolvertraits.hh>
+#include <dumux/linear/linearalgebratraits.hh>
+#include <dumux/porenetwork/common/pnmvtkoutputmodule.hh>
+#include <dumux/porenetwork/2p/newtonsolver.hh>
+
+#include "properties.hh"
+
+int main(int argc, char** argv)
+{
+    using namespace Dumux;
+
+    using TypeTag = Properties::TTag::DrainageProblem;
+
+    // maybe initialize MPI and/or multithreading backend
+    Dumux::initialize(argc, argv);
+    const auto& mpiHelper = Dune::MPIHelper::instance();
+
+    // print dumux start message
+    if (mpiHelper.rank() == 0)
+        DumuxMessage::print(/*firstCall=*/true);
+
+    ////////////////////////////////////////////////////////////
+    // parse the command line arguments and input file
+    ////////////////////////////////////////////////////////////
+
+    // parse command line arguments
+    Parameters::init(argc, argv);
+
+    //////////////////////////////////////////////////////////////////////
+    // try to create a grid (from the given grid file or the input file)
+    /////////////////////////////////////////////////////////////////////
+
+    using GridManager = Dumux::PoreNetwork::GridManager<3>;
+    GridManager gridManager;
+    gridManager.init();
+
+    // we compute on the leaf grid view
+    const auto& leafGridView = gridManager.grid().leafGridView();
+    auto gridData = gridManager.getGridData();
+
+    // create the finite volume grid geometry
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    auto gridGeometry = std::make_shared<GridGeometry>(leafGridView, *gridData);
+
+    // the spatial parameters
+    using SpatialParams = GetPropType<TypeTag, Properties::SpatialParams>;
+    auto spatialParams = std::make_shared<SpatialParams>(gridGeometry);
+
+    // the problem (boundary conditions)
+    using Problem = GetPropType<TypeTag, Properties::Problem>;
+    auto problem = std::make_shared<Problem>(gridGeometry, spatialParams);
+
+    // the solution vector
+    using GridView = typename GridGeometry::GridView;
+    using SolutionVector = GetPropType<TypeTag, Properties::SolutionVector>;
+    SolutionVector x(leafGridView.size(GridView::dimension));
+    problem->applyInitialSolution(x);
+    auto xOld = x;
+
+    // the grid variables
+    using GridVariables = GetPropType<TypeTag, Properties::GridVariables>;
+    auto gridVariables = std::make_shared<GridVariables>(problem, gridGeometry);
+    gridVariables->init(x);
+
+    // get some time loop parameters
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    const auto tEnd = getParam<Scalar>("TimeLoop.TEnd");
+    const auto maxDt = getParam<Scalar>("TimeLoop.MaxTimeStepSize");
+    auto dt = getParam<Scalar>("TimeLoop.DtInitial");
+
+    // check if we are about to restart a previously interrupted simulation
+    Scalar restartTime = 0;
+    if (Parameters::getTree().hasKey("Restart") || Parameters::getTree().hasKey("TimeLoop.Restart"))
+        restartTime = getParam<Scalar>("TimeLoop.Restart");
+
+    // initialize the vtk output module
+    using IOFields = GetPropType<TypeTag, Properties::IOFields>;
+    PoreNetwork::VtkOutputModule<GridVariables, GetPropType<TypeTag, Properties::FluxVariables>, SolutionVector> vtkWriter(*gridVariables, x, problem->name());
+    IOFields::initOutputModule(vtkWriter); //! Add model specific output fields
+
+    vtkWriter.write(0.0);
+
+    // instantiate time loop
+    auto timeLoop = std::make_shared<TimeLoop<Scalar>>(restartTime, dt, tEnd);
+    timeLoop->setMaxTimeStepSize(maxDt);
+
+    // the assembler with time loop for instationary problem
+    using Assembler = FVAssembler<TypeTag, DiffMethod::numeric>;
+    auto assembler = std::make_shared<Assembler>(problem, gridGeometry, gridVariables, timeLoop, xOld);
+
+    // the linear solver
+    using LinearSolver = UMFPackIstlSolver<SeqLinearSolverTraits, LinearAlgebraTraitsFromAssembler<Assembler>>;
+    auto linearSolver = std::make_shared<LinearSolver>();
+
+    // the non-linear solver
+    using NewtonSolver = PoreNetwork::TwoPNewtonSolver<Assembler, LinearSolver>;
+    NewtonSolver nonLinearSolver(assembler, linearSolver);
+
+    // time loop
+    timeLoop->start(); do
+    {
+        // try solving the non-linear system
+        nonLinearSolver.solve(x, *timeLoop);
+
+        // make the new solution the old solution
+        xOld = x;
+        gridVariables->advanceTimeStep();
+
+        // advance to the time loop to the next step
+        timeLoop->advanceTimeStep();
+
+        // write vtk output
+        if(problem->shouldWriteOutput(timeLoop->timeStepIndex(), *gridVariables))
+            vtkWriter.write(timeLoop->time());
+
+        // report statistics of this time step
+        timeLoop->reportTimeStep();
+
+        // set new dt as suggested by newton solver
+        timeLoop->setTimeStepSize(nonLinearSolver.suggestTimeStepSize(timeLoop->timeStepSize()));
+
+    } while (!timeLoop->finished());
+
+    ////////////////////////////////////////////////////////////
+    // finalize, print dumux message to say goodbye
+    ////////////////////////////////////////////////////////////
+    timeLoop->finalize(leafGridView.comm());
+
+    // print dumux end message
+    if (mpiHelper.rank() == 0)
+    {
+        Parameters::print();
+        DumuxMessage::print(/*firstCall=*/false);
+    }
+}
---
+++ test/porenetwork/2pupscaling/problem.hh
@@ -0,0 +1,207 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ *
+ * \brief A test problem for the two-phase pore network model.
+ */
+#ifndef DUMUX_PNM2P_PROBLEM_HH
+#define DUMUX_PNM2P_PROBLEM_HH
+
+#include <dumux/common/boundarytypes.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/porenetwork/2p/model.hh>
+#include <dumux/porousmediumflow/problem.hh>
+
+namespace Dumux {
+
+template <class TypeTag>
+class DrainageProblem;
+
+template <class TypeTag>
+class DrainageProblem : public PorousMediumFlowProblem<TypeTag>
+{
+    using ParentType = PorousMediumFlowProblem<TypeTag>;
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using PrimaryVariables = GetPropType<TypeTag, Properties::PrimaryVariables>;
+    using BoundaryTypes = Dumux::BoundaryTypes<GetPropType<TypeTag, Properties::ModelTraits>::numEq()>;
+    using ElementVolumeVariables = typename GetPropType<TypeTag, Properties::GridVolumeVariables>::LocalView;
+    using FVElementGeometry = typename GetPropType<TypeTag, Properties::GridGeometry>::LocalView;
+    using SubControlVolume = typename FVElementGeometry::SubControlVolume;
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    using GridView = typename GridGeometry::GridView;
+    using FluidSystem = GetPropType<TypeTag, Properties::FluidSystem>;
+    using GridVariables = GetPropType<TypeTag, Properties::GridVariables>;
+
+    // copy some indices for convenience
+    using Indices = typename GetPropType<TypeTag, Properties::ModelTraits>::Indices;
+    using Labels = GetPropType<TypeTag, Properties::Labels>;
+    enum {
+        pwIdx = Indices::pressureIdx,
+        snIdx = Indices::saturationIdx,
+        nPhaseIdx = FluidSystem::phase1Idx,
+
+#if !ISOTHERMAL
+        temperatureIdx = Indices::temperatureIdx,
+        energyEqIdx = Indices::energyEqIdx,
+#endif
+    };
+
+    using Element = typename GridView::template Codim<0>::Entity;
+    using Vertex = typename GridView::template Codim<GridView::dimension>::Entity;
+
+public:
+    template<class SpatialParams>
+    DrainageProblem(std::shared_ptr<const GridGeometry> gridGeometry, std::shared_ptr<SpatialParams> spatialParams)
+    : ParentType(gridGeometry, spatialParams)
+    {
+        vtpOutputFrequency_ = getParam<int>("Problem.VtpOutputFrequency");
+        useFixedPressureAndSaturationBoundary_ = getParam<bool>("Problem.UseFixedPressureAndSaturationBoundary", false);
+        pc_ = getParam<Scalar>("Problem.CapillaryPressure");
+    }
+
+    /*!
+     * \name Simulation steering
+     */
+    // \{
+
+    /*!
+     * \name Problem parameters
+     */
+    // \{
+
+    bool shouldWriteOutput(const int timeStepIndex, const GridVariables& gridVariables) const
+    {
+        if (vtpOutputFrequency_ < 0)
+            return true;
+
+        if (vtpOutputFrequency_ == 0)
+            return (timeStepIndex == 0 || gridVariables.gridFluxVarsCache().invasionState().hasChanged());
+        else
+            return (timeStepIndex % vtpOutputFrequency_ == 0 || gridVariables.gridFluxVarsCache().invasionState().hasChanged());
+    }
+
+     /*!
+     * \name Boundary conditions
+     */
+    // \{
+    //! Specifies which kind of boundary condition should be used for
+    //! which equation for a finite volume on the boundary.
+    BoundaryTypes boundaryTypes(const Element& element, const SubControlVolume& scv) const
+    {
+        BoundaryTypes bcTypes;
+
+        // If a global phase pressure difference (pn,inlet - pw,outlet) with fixed saturations is specified, use a Dirichlet BC here
+        if (useFixedPressureAndSaturationBoundary_ && isInletPore_(scv))
+            bcTypes.setAllDirichlet();
+        else if (isOutletPore_(scv))
+            bcTypes.setAllDirichlet();
+
+#if !ISOTHERMAL
+        bcTypes.setDirichlet(temperatureIdx);
+#endif
+        return bcTypes;
+    }
+
+
+    //! Evaluate the boundary conditions for a Dirichlet control volume.
+    PrimaryVariables dirichlet(const Element& element,
+                               const SubControlVolume& scv) const
+    {
+        PrimaryVariables values(0.0);
+        values[pwIdx] = 1e5;
+        values[snIdx] = 0.0;
+
+        // If a global phase pressure difference (pn,inlet - pw,outlet) is specified and the saturation shall also be fixed, apply:
+        // pw,inlet = pw,outlet = 1e5; pn,outlet = pw,outlet + pc(S=0) = pw,outlet; pn,inlet = pw,inlet + pc_
+        if (useFixedPressureAndSaturationBoundary_ && isInletPore_(scv))
+            values[snIdx] = 1.0 - this->spatialParams().fluidMatrixInteraction(element, scv, int()/*dummyElemsol*/).sw(pc_);
+
+#if !ISOTHERMAL
+        if (isInletPore_(scv))
+            values[temperatureIdx] = 273.15 + 15;
+        else
+            values[temperatureIdx] = 273.15 + 10;
+#endif
+        return values;
+    }
+
+
+    // \}
+
+    /*!
+     * \name Volume terms
+     */
+    // \{
+
+    //! Evaluate the source term for all phases within a given sub-control-volume.
+    PrimaryVariables source(const Element& element,
+                            const FVElementGeometry& fvGeometry,
+                            const ElementVolumeVariables& elemVolVars,
+                            const SubControlVolume& scv) const
+    {
+        PrimaryVariables values(0.0);
+
+        // If we do not want to use global phase pressure difference with fixed saturations and pressures,
+        // we can instead only fix the non-wetting phase pressure and allow the wetting phase saturation to changle freely
+        // by applying a Nitsche-type boundary condition which tries to minimize the difference between the present pn and the given value
+        if (!useFixedPressureAndSaturationBoundary_ && isInletPore_(scv))
+            values[snIdx] = (elemVolVars[scv].pressure(nPhaseIdx) - (1e5 + pc_)) * 1e8;
+
+        return values;
+    }
+    // \}
+
+    //! Evaluate the initial value for a control volume.
+    PrimaryVariables initial(const Vertex& vertex) const
+    {
+        PrimaryVariables values(0.0);
+        values[pwIdx] = 1e5;
+
+        // get global index of pore
+        const auto dofIdxGlobal = this->gridGeometry().vertexMapper().index(vertex);
+        if (isInletPore_(dofIdxGlobal))
+            values[snIdx] = 0.5;
+        else
+            values[snIdx] = 0.0;
+
+#if !ISOTHERMAL
+        values[temperatureIdx] = 273.15 + 10;
+#endif
+        return values;
+    }
+
+    //!  Evaluate the initial invasion state of a pore throat
+    bool initialInvasionState(const Element& element) const
+    { return false; }
+
+    // \}
+
+private:
+
+    bool isInletPore_(const SubControlVolume& scv) const
+    {
+        return isInletPore_(scv.dofIndex());
+    }
+
+    bool isInletPore_(const std::size_t dofIdxGlobal) const
+    {
+        return this->gridGeometry().poreLabel(dofIdxGlobal) == Labels::inlet;
+    }
+
+    bool isOutletPore_(const SubControlVolume& scv) const
+    {
+        return this->gridGeometry().poreLabel(scv.dofIndex()) == Labels::outlet;
+    }
+
+    int vtpOutputFrequency_;
+    bool useFixedPressureAndSaturationBoundary_;
+    Scalar pc_;
+};
+} //end namespace Dumux
+
+#endif
