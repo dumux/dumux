ba6652fe1cc1f871d1e9aeb0f52437f0c6018819
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-09-29 08:03:55 +0200

    [mpfa][computetij] remove unneccessary  overload

--- dumux/discretization/cellcentered/mpfa/computetransmissibility.hh
+++ dumux/discretization/cellcentered/mpfa/computetransmissibility.hh
@@ -22,6 +22,18 @@
 
 namespace Dumux {
 
+#ifndef DOXYGEN
+namespace Detail {
+
+template<class T, class Enable = void>
+struct TensorFieldType { using type = typename T::field_type; };
+
+template<class T>
+struct TensorFieldType<T, std::enable_if_t<Dune::IsNumber<T>::value>> { using type = T; };
+
+} // namespace Detail
+#endif // DOXYGEN
+
 /*!
  * \ingroup CCMpfaDiscretization
  * \brief Free function to evaluate the Mpfa transmissibility associated
@@ -32,50 +44,17 @@ namespace Dumux {
  * \param fvGeometry The element-centered control volume geometry
  * \param scv The iv-local sub-control volume
  * \param scvf The grid sub-control volume face
- * \param t The tensor living in the scv
+ * \param t The tensor/Scalar living in the scv
  * \param extrusionFactor The extrusion factor of the scv
  */
 template<class EG, class IVSubControlVolume, class Tensor>
-Dune::FieldVector<typename Tensor::field_type, IVSubControlVolume::myDimension>
-computeMpfaTransmissibility(const EG& fvGeometry,
-                            const IVSubControlVolume& scv,
-                            const typename EG::SubControlVolumeFace& scvf,
-                            const Tensor& t,
-                            typename IVSubControlVolume::ctype extrusionFactor)
+auto computeMpfaTransmissibility(const EG& fvGeometry,
+                                 const IVSubControlVolume& scv,
+                                 const typename EG::SubControlVolumeFace& scvf,
+                                 const Tensor& t,
+                                 typename IVSubControlVolume::ctype extrusionFactor)
 {
-    Dune::FieldVector<typename Tensor::field_type, IVSubControlVolume::myDimension> wijk;
-    for (unsigned int dir = 0; dir < IVSubControlVolume::myDimension; ++dir)
-        wijk[dir] = vtmv(scvf.unitOuterNormal(), t, scv.nu(dir));
-
-    using Extrusion = Extrusion_t<typename EG::GridGeometry>;
-    wijk *= Extrusion::area(fvGeometry, scvf)*extrusionFactor;
-    wijk /= scv.detX();
-
-    return wijk;
-}
-
-/*!
- * \ingroup CCMpfaDiscretization
- * \brief Free function to evaluate the Mpfa transmissibility associated
- *        with the flux (in the form of flux = t*gradU) across a
- *        sub-control volume face stemming from a given sub-control
- *        volume with corresponding tensor t, where t is a scalar.
- *
- * \param fvGeometry The element-centered control volume geometry
- * \param scv The iv-local sub-control volume
- * \param scvf The grid sub-control volume face
- * \param t the scalar quantity living in the scv
- * \param extrusionFactor The extrusion factor of the scv
- */
-template< class EG, class IVSubControlVolume, class Tensor, std::enable_if_t< Dune::IsNumber<Tensor>::value, int > = 1 >
-Dune::FieldVector<Tensor, IVSubControlVolume::myDimension>
-computeMpfaTransmissibility(const EG& fvGeometry,
-                            const IVSubControlVolume& scv,
-                            const typename EG::SubControlVolumeFace& scvf,
-                            const Tensor& t,
-                            typename IVSubControlVolume::ctype extrusionFactor)
-{
-    Dune::FieldVector<Tensor, IVSubControlVolume::myDimension> wijk;
+    Dune::FieldVector<typename Detail::TensorFieldType<Tensor>::type, IVSubControlVolume::myDimension> wijk;
     for (unsigned int dir = 0; dir < IVSubControlVolume::myDimension; ++dir)
         wijk[dir] = vtmv(scvf.unitOuterNormal(), t, scv.nu(dir));
 
