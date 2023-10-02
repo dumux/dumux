b36ca2d2e61f669632b16d381fa2777e5ddd2fdf
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-10-02 16:37:01 +0200

    [mpfa][ivindexsets] use factory pattern for iv construction

--- dumux/discretization/cellcentered/mpfa/gridinteractionvolumeindexsets.hh
+++ dumux/discretization/cellcentered/mpfa/gridinteractionvolumeindexsets.hh
@@ -13,10 +13,46 @@
 #define DUMUX_DISCRETIZATION_MPFA_O_GRIDINTERACTIONVOLUME_INDEXSETS_HH
 
 #include <memory>
+#include <functional>
 
 #include "dualgridindexset.hh"
 
 namespace Dumux {
+
+template<class GridView>
+class CCMpfaInteractionVolume
+{
+public:
+    virtual ~CCMpfaInteractionVolume() = default;
+
+    using GridIndex = typename GridView::IndexSet::IndexType;
+    using GridIndexVisitor = std::function<void(const GridIndex&)>;
+
+    void visitGridScvfIndices(const GridIndexVisitor& v) const
+    { return visitGridScvfIndices_(v); }
+
+private:
+    virtual void visitGridScvfIndices_(const GridIndexVisitor&) const = 0;
+};
+
+template<typename GridView>
+class CCMpfaInteractionVolumeFactory
+{
+public:
+    virtual ~CCMpfaInteractionVolumeFactory() = default;
+
+    using InteractionVolumePointer = std::unique_ptr<CCMpfaInteractionVolume<GridView>>;
+    using DualGridNodalIndexSet = CCMpfaDualGridNodalIndexSet<GridView>;
+    using InteractionVolumesVisitor = std::function<void(InteractionVolumePointer&&)>;
+
+    void visitInteractionVolumesAt(const DualGridNodalIndexSet& ni,
+                                   const InteractionVolumesVisitor& v) const
+    { visitInteractionVolumesAt_(ni, v); }
+
+private:
+    virtual void visitInteractionVolumesAt_(const DualGridNodalIndexSet& ni,
+                                            const InteractionVolumesVisitor&) const = 0;
+};
 
 /*!
  * \ingroup CCMpfaDiscretization
@@ -32,6 +68,35 @@ class CCMpfaGridInteractionVolumeIndexSets
     using SubControlVolumeFace = typename FVG::SubControlVolumeFace;
     using GV = typename FVG::GridView;
 
+    class DummyInteractionVolumeFactory : public CCMpfaInteractionVolumeFactory<GV>
+    {
+        class DummyInteractionVolume : public CCMpfaInteractionVolume<GV>
+        {
+        public:
+            using typename CCMpfaInteractionVolume<GV>::GridIndexVisitor;
+            DummyInteractionVolume(const CCMpfaDualGridNodalIndexSet<GV>& ni)
+            : ni_{ni}
+            {}
+
+        private:
+            void visitGridScvfIndices_(const GridIndexVisitor& v) const
+            {
+                for (const auto& scvfIndex : ni_.gridScvfIndices())
+                    v(scvfIndex);
+            }
+
+            const CCMpfaDualGridNodalIndexSet<GV>& ni_;
+        };
+
+        using typename CCMpfaInteractionVolumeFactory<GV>::DualGridNodalIndexSet;
+        using typename CCMpfaInteractionVolumeFactory<GV>::InteractionVolumesVisitor;
+        void visitInteractionVolumesAt_(const DualGridNodalIndexSet& ni,
+                                        const InteractionVolumesVisitor& v) const
+        {
+            v(std::make_unique<DummyInteractionVolume>(ni));
+        }
+    };
+
 public:
     using GridGeometry = FVG;
     using PrimaryInteractionVolume = PI;
@@ -41,23 +106,35 @@ public:
     using NodalIndexSet = typename DualGridIndexSet::NodalIndexSet;
     using GridIndexType = typename DualGridIndexSet::GridIndexType;
 
+    void update(GridGeometry& gridGeometry,
+                DualGridIndexSet&& dualGridIdSet)
+    {
+        DummyInteractionVolumeFactory f{};
+        update(gridGeometry, std::move(dualGridIdSet), f);
+    }
+
     /*!
      * \brief Construct all interaction volume index sets on the grid view
      *
      * \param gridGeometry The finite volume geometry on the grid view
      * \param dualGridIdSet The index sets of the dual grid on the grid view
+     * \param ivFactory Factory class for constructing interaction volumes around vertices.
      */
-    void update(GridGeometry& gridGeometry, DualGridIndexSet&& dualGridIdSet)
+    void update(GridGeometry& gridGeometry,
+                DualGridIndexSet&& dualGridIdSet,
+                const CCMpfaInteractionVolumeFactory<GV>& factory)
     {
         dualGridIndexSet_ = std::make_unique<DualGridIndexSet>(std::move(dualGridIdSet));
         scvfIndexMap_.clear();
         scvfIndexMap_.resize(gridGeometry.numScvf());
         for (const auto& vertex : vertices(gridGeometry.gridView()))
-        {
-            const auto vIdxGlobal = gridGeometry.vertexMapper().index(vertex);
-            for (const auto scvfIdx : (*dualGridIndexSet_)[vIdxGlobal].gridScvfIndices())
-                scvfIndexMap_[scvfIdx] = vIdxGlobal;
-        }
+            factory.visitInteractionVolumesAt((*dualGridIndexSet_)[gridGeometry.vertexMapper().index(vertex)], [&] (auto&& iv) {
+                const auto ivIndex = interactionVolumes_.size();
+                interactionVolumes_.emplace_back(std::move(iv));
+                interactionVolumes_.back()->visitGridScvfIndices([&] (auto scvfIdx) {
+                    scvfIndexMap_[scvfIdx] = ivIndex;
+                });
+            });
     }
 
     //! Return the iv index set in which a given scvf is embedded in
@@ -74,6 +151,7 @@ public:
 private:
     std::vector<GridIndexType> scvfIndexMap_;
     std::unique_ptr<DualGridIndexSet> dualGridIndexSet_;
+    std::vector<std::unique_ptr<CCMpfaInteractionVolume<GV>>> interactionVolumes_;
 };
 
 } // end namespace Dumux
