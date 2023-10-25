5288a828743dec7088b83269e00c795853987c50
Timo Koch <timokoch@uio.no>
2023-10-25 11:33:08 +0200

    [test][timeloop] Fix rhs-scaling of floating point comparison check

--- test/common/timeloop/test_timeloop.cc
+++ test/common/timeloop/test_timeloop.cc
@@ -150,7 +150,7 @@ void testTimeLoops(double tStart,
 
         timeLoop.finalize();
 
-        if (std::abs(timeLoop.time()-1e9*timeSpan) > 1e-15*timeSpan)
+        if (std::abs(timeLoop.time()-1e9*timeSpan) > 1e9*1e-15*timeSpan)
             DUNE_THROW(Dune::InvalidStateException, "Ended with wrong end time!");
 
     }
