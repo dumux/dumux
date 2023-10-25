efa33253769bf23b7d7ac0dd013c8f7d8e939f10
Anna Mareike Kostelecky <anmako96@web.de>
2023-10-25 15:03:31 +0200

    [pnm][fourierslaw] add comment for "UseVolumeEqualPyramid" parameter

--- dumux/flux/porenetwork/grainfourierslaw.hh
+++ dumux/flux/porenetwork/grainfourierslaw.hh
@@ -50,6 +50,9 @@ struct TruncatedPyramidGrainFouriersLaw
 
         auto getPyramidBaseLengthFromVolume = [&](const Scalar v, const Scalar h)
         {
+            // Using the formula for the volume of a pyramid frustum to calculate its base length:
+            // v = 1/3 h * (a^2 + a*b + b^2), where a is the base side length, b the top side length,
+            // h the height and v the volume of the frustum.
             const Scalar b = topSideLength;
             using std::sqrt;
             return 0.5*sqrt(3.0) * sqrt(-(b*b*h-4.0*v)/h) -0.5*b;
