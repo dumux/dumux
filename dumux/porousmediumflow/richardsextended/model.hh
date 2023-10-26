146428f01c6b03f0b1cf00d4f234427e0d435edb
Yue Wang <yue.wang@iws.uni-stuttgart.de>
2023-10-26 23:34:25 +0200

    [pm/richardsextended] change descriptions to collapsible section

--- dumux/porousmediumflow/richardsextended/model.hh
+++ dumux/porousmediumflow/richardsextended/model.hh
@@ -27,19 +27,21 @@
  =
  q_w,
  \f]
- * Where:
- * * \f$ \phi \f$ is the porosity of the porous-medium,
- * * \f$ S_w \f$ represents the saturation of the wetting-phase,
- * * \f$ \varrho_w \f$ is the mass density of the wetting phase,
- * * \f$ \varrho_n \f$ is the mass density of the non-wetting phase,
- * * \f$ k_{rw} \f$ is the relative permeability of the wetting phase,
- * * \f$ \mu_w \f$ is the dynamic viscosity of the wetting phase,
- * * \f$ \mathbf{K} \f$ is the intrinsic permeability tensor,
- * * \f$ p_w \f$ is the pressure of the wetting phase,
- * * \f$ \mathbf{g} \f$ is the gravitational acceleration vector,
- * * \f$ \bf D_{n,pm}^{w} \f$ is the diffusivity of water in the non-wetting phase,
- * * \f$ X_n^w \f$ is the mass fraction of water in the non-wetting phase,
- * * \f$ q_w \f$ is a source or sink term in the wetting phase.
+ * <details>
+ * <summary>Click for variables explanations:</summary>
+ * \f$ \cdot \f$ \f$ \phi \f$ is the porosity of the porous-medium, <br>
+ * \f$ \cdot \f$ \f$ S_w \f$ represents the saturation of the wetting-phase, <br>
+ * \f$ \cdot \f$ \f$ \varrho_w \f$ is the mass density of the wetting phase, <br>
+ * \f$ \cdot \f$ \f$ \varrho_n \f$ is the mass density of the non-wetting phase, <br>
+ * \f$ \cdot \f$ \f$ k_{rw} \f$ is the relative permeability of the wetting phase, <br>
+ * \f$ \cdot \f$ \f$ \mu_w \f$ is the dynamic viscosity of the wetting phase, <br>
+ * \f$ \cdot \f$ \f$ \mathbf{K} \f$ is the intrinsic permeability tensor, <br>
+ * \f$ \cdot \f$ \f$ p_w \f$ is the pressure of the wetting phase, <br>
+ * \f$ \cdot \f$ \f$ \mathbf{g} \f$ is the gravitational acceleration vector, <br>
+ * \f$ \cdot \f$ \f$ \bf D_{n,pm}^{w} \f$ is the diffusivity of water in the non-wetting phase, <br>
+ * \f$ \cdot \f$ \f$ X_n^w \f$ is the mass fraction of water in the non-wetting phase, <br>
+ * \f$ \cdot \f$ \f$ q_w \f$ is a source or sink term in the wetting phase.
+ * </details>
  *
  * is frequently used to
  * approximate the water distribution above the groundwater level.
@@ -58,16 +60,18 @@
  q_\alpha,
  \f]
  *
- * Where:
- * * \f$ \phi \f$ is the porosity of the porous-medium,
- * * \f$ S_\alpha \f$ represents the saturation of phase \f$ \alpha \f$,
- * * \f$ \varrho_\alpha \f$ is the mass density of phase \f$ \alpha \f$,
- * * \f$ k_{r\alpha} \f$ is the relative permeability of phase \f$ \alpha \f$,
- * * \f$ \mu_\alpha \f$ is the dynamic viscosity of phase \f$ \alpha \f$,
- * * \f$ \mathbf{K} \f$ is the intrinsic permeability tensor,
- * * \f$ p_\alpha \f$ is the pressure of phase \f$ \alpha \f$,
- * * \f$ \mathbf{g} \f$ is the gravitational acceleration vector,
- * * \f$ q_\alpha \f$ is a source or sink term.
+ * <details>
+ * <summary>Click for variables explanations:</summary>
+ * \f$ \cdot \f$ \f$ \phi \f$ is the porosity of the porous-medium, <br>
+ * \f$ \cdot \f$ \f$ S_\alpha \f$ represents the saturation of phase \f$ \alpha \f$, <br>
+ * \f$ \cdot \f$ \f$ \varrho_\alpha \f$ is the mass density of phase \f$ \alpha \f$, <br>
+ * \f$ \cdot \f$ \f$ k_{r\alpha} \f$ is the relative permeability of phase \f$ \alpha \f$, <br>
+ * \f$ \cdot \f$ \f$ \mu_\alpha \f$ is the dynamic viscosity of phase \f$ \alpha \f$, <br>
+ * \f$ \cdot \f$ \f$ \mathbf{K} \f$ is the intrinsic permeability tensor, <br>
+ * \f$ \cdot \f$ \f$ p_\alpha \f$ is the pressure of phase \f$ \alpha \f$, <br>
+ * \f$ \cdot \f$ \f$ \mathbf{g} \f$ is the gravitational acceleration vector, <br>
+ * \f$ \cdot \f$ \f$ q_\alpha \f$ is a source or sink term.
+ * </details>
  *
  * In contrast to the full two-phase model, the Richards model assumes
  * gas as the nonwetting fluid and that it exhibits a much lower
