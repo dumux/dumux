d24920a2d4b33ba716e600e998a49eca9ce08d14
Mathis Kelm <mathis.kelm@iws.uni-stuttgart.de>
2023-12-01 16:32:05 +0100

    [cppcheck] run project check with GNU parallel

---
+++ bin/cppcheck_parallel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+options="--error-exitcode=1 --inline-suppr -iformat/fmt/ --enable=missingInclude --suppress=*:*/dune/* --suppress=*:*/fmt/* --suppress=*:*/xml/* --suppress=*:*/json/*"
+tmp_dir=$(mktemp -d)
+jq -c '.[]' build-cmake/compile_commands.json | while read -r compile_command; do
+    tmp_file=$(mktemp "${tmp_dir}/XXX.json")
+    echo "[$compile_command]" > "$tmp_file"
+    echo "$tmp_file"
+done | parallel --keep-order cppcheck "$options" --project={}
+rm -r "$tmp_dir"
