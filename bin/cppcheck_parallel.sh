ee66f0046e459f4b9f86a63259f8128cbfc69983
Mathis Kelm <mathis.kelm@iws.uni-stuttgart.de>
2023-12-04 14:03:58 +0100

    [cpppcheck] suppress missingIncludeSystem information messages

--- bin/cppcheck_parallel.sh
+++ bin/cppcheck_parallel.sh
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-options="--error-exitcode=1 --inline-suppr -iformat/fmt/ --enable=missingInclude --suppress=*:*/dune/* --suppress=*:*/fmt/* --suppress=*:*/xml/* --suppress=*:*/json/*"
+options="--error-exitcode=1 --inline-suppr -iformat/fmt/ --enable=missingInclude --suppress=*:*/dune/* --suppress=*:*/fmt/* --suppress=*:*/xml/* --suppress=*:*/json/* --suppress=missingIncludeSystem:*/dumux/*"
 tmp_dir=$(mktemp -d)
 jq -c '.[]' build-cmake/compile_commands.json | while read -r compile_command; do
     tmp_file=$(mktemp "${tmp_dir}/XXX.json")
