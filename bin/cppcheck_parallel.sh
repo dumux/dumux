4be3a3450662da3f318825e057ff05a1027516a3
Mathis Kelm <mathis.kelm@iws.uni-stuttgart.de>
2023-12-08 11:41:03 +0100

    [ci][cppcheck] use affectedtests.json to filter compile commands

--- bin/cppcheck_parallel.sh
+++ bin/cppcheck_parallel.sh
@@ -4,7 +4,14 @@
 
 options="--error-exitcode=1 --inline-suppr -iformat/fmt/ --enable=missingInclude --suppress=*:*/dune/* --suppress=*:*/fmt/* --suppress=*:*/xml/* --suppress=*:*/json/* --suppress=missingIncludeSystem:*/dumux/* --suppress=preprocessorErrorDirective:*/pyconfig.h"
 tmp_dir=$(mktemp -d)
+if [[ -s "affectedtests.json" ]]; then
+    affected_targets="$(jq -r 'to_entries[] | "\(.value | .target)"' affectedtests.json)"
+fi
 jq -c '.[]' build-cmake/compile_commands.json | while read -r compile_command; do
+    target=$(echo "$compile_command" | sed "s/.*CMakeFiles\/\(.*\)\.dir.*/\1/")
+    if ([[ -n $affected_targets ]] && [[ ! $affected_targets =~ $target ]]); then
+        continue
+    fi
     tmp_file=$(mktemp "${tmp_dir}/XXX.json")
     echo "[$compile_command]" > "$tmp_file"
     echo "$tmp_file"
