68cb2878c1815370e884d29686f058a6da83cd29
Timo Koch <timokoch@uio.no>
2023-12-09 18:11:39 +0100

    [ff][brinkman] Add viscosity scaling

--- dumux/freeflow/navierstokes/momentum/brinkman.hh
+++ dumux/freeflow/navierstokes/momentum/brinkman.hh
@@ -33,7 +33,7 @@ namespace Dumux {
  * The Navier-Stokes model can be extended to a Darcy-Brinkman model by adding
  * the term:
  * \f[
-      + \epsilon_B \mathbf{K}^{-1} \mathbf{v}
+      + \epsilon_B \mu \mathbf{K}^{-1} \mathbf{v}
  * \f]
  * to the momentum balance. This can be achieved with the helper function \ref addBrinkmanTerm.
  * The function relies on the spatial parameters class being based on <code>BrinkmanSpatialParams</code>
@@ -55,26 +55,27 @@ void addBrinkmanTerm(
     {
         const auto brinkmanEpsilon = problem.spatialParams().brinkmanEpsilon(element, fvGeometry, scv);
         const auto invK = problem.spatialParams().inversePermeability(element, fvGeometry, scv);
+        const auto mu = problem.effectiveViscosity(element, fvGeometry, scv);
 
         using DM = typename FVElementGeometry::GridGeometry::DiscretizationMethod;
         if constexpr (DiscretizationMethods::isCVFE<DM>)
         {
             const auto velocity = elemVolVars[scv].velocity();
-            source -= brinkmanEpsilon * mv(invK, velocity); // eps K^-1 velocity
+            source -= mu * brinkmanEpsilon * mv(invK, velocity);
         }
         else if constexpr (DM{} == DiscretizationMethods::fcstaggered)
         {
             if constexpr (Dune::IsNumber<std::decay_t<decltype(invK)>>::value)
             {
                 const auto velocity = elemVolVars[scv].velocity();
-                source -= brinkmanEpsilon * invK * velocity;
+                source -= mu * brinkmanEpsilon * invK * velocity;
             }
             else
             {
                 // permeability is tensor-valued, use velocity vector reconstruction
                 const auto getVelocitySCV = [&](const auto& scv){ return elemVolVars[scv].velocity(); };
                 const auto velocity = StaggeredVelocityReconstruction::faceVelocity(scv, fvGeometry, getVelocitySCV);
-                source -= brinkmanEpsilon * mv(invK, velocity)[scv.dofAxis()]; // eps K^-1 velocity
+                source -= mu * brinkmanEpsilon * mv(invK, velocity)[scv.dofAxis()];
             }
         }
         else
