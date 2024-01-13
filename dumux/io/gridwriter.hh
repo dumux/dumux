12b12efca072f6798c854c6b661b2207f6525ebf
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2024-01-13 15:22:23 +0100

    [io] first draft of gfmt wrapper

---
+++ dumux/io/gridwriter.hh
@@ -0,0 +1,195 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup InputOutput
+ * \brief Generic writer for a variety of grid file formats.
+ */
+#ifndef DUMUX_IO_GRID_WRITER_HH
+#define DUMUX_IO_GRID_WRITER_HH
+
+#include <config.h>
+
+#include <type_traits>
+
+#include <gridformat/gridformat.hpp>
+#include <gridformat/traits/dune.hpp>
+
+namespace Dumux::IO {
+
+
+#ifndef DOXYGEN
+namespace Detail {
+
+template<typename Grid, typename Format, typename Comm, typename... Args>
+auto makeParallelWriter(const Grid& grid, const Format& fmt, const Dune::Communication<Comm>& comm, Args&&... args)
+{
+    return comm.size() > 1
+        ? GridFormat::Writer<Grid>{fmt, grid, static_cast<Comm>(comm), std::forward<Args>(args)...}
+        : GridFormat::Writer<Grid>{fmt, grid, std::forward<Args>(args)...};
+}
+
+template<typename Grid, typename Format, typename... Args>
+auto makeParallelWriter(const Grid& grid, const Format& fmt, const Dune::Communication<Dune::No_Comm>&, Args&&... args)
+{ return GridFormat::Writer<Grid>{fmt, grid, std::forward<Args>(args)...}; }
+
+template<typename Grid, typename Format, typename... Args>
+auto makeWriter(const Grid& grid, const Format& fmt, Args&&... args)
+{
+    const auto& comm = GridFormat::Dune::Traits::GridView<Grid>::get(grid).comm();
+    return makeParallelWriter(grid, fmt, comm, std::forward<Args>(args)...);
+}
+
+} // namespace Detail
+#endif // DOXYGEN
+
+
+namespace VTK { using namespace GridFormat::VTK; }
+namespace Format { using namespace GridFormat::Formats; }
+namespace Encoding { using namespace GridFormat::Encoding; }
+namespace Compression { using namespace GridFormat::Compression; using GridFormat::none; }
+namespace Precision {
+    using GridFormat::float32;
+    using GridFormat::float64;
+
+    using GridFormat::uint64;
+    using GridFormat::uint32;
+    using GridFormat::uint16;
+    using GridFormat::uint8;
+
+    using GridFormat::int64;
+    using GridFormat::int32;
+    using GridFormat::int16;
+    using GridFormat::int8;
+} // namespace Precision
+
+
+template<int order>
+struct Order { static_assert(order > 0, "order must be > 0"); };
+
+inline constexpr auto firstOrder = Order<1>{};
+inline constexpr auto secondOrder = Order<2>{};
+inline constexpr auto thirdOrder = Order<3>{};
+
+template<GridFormat::Concepts::Grid GridView, int order = 1>
+class GridWriter
+{
+#if HAVE_DUNE_LOCALFUNCTIONS
+    static constexpr bool have_dune_local_functions_ = true;
+#else
+    static constexpr bool have_dune_local_functions_ = false;
+#endif
+    using Grid = std::conditional_t<
+        (order > 1),
+        GridFormat::Dune::LagrangePolynomialGrid<GridView>,
+        GridView
+    >;
+    using Cell = GridFormat::Cell<Grid>;
+    using Vertex = typename GridView::template Codim<GridView::dimension>::Entity;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using Coordinate = typename Element::Geometry::GlobalCoordinate;
+    using Writer = GridFormat::Writer<Grid>;
+
+    static_assert(
+        order == 1 || have_dune_local_functions_,
+        "dune-localfunctions required for higher-order output"
+    );
+
+ public:
+    //! Constructor for static grid file formats
+    template<typename Format>
+    explicit GridWriter(const Format& fmt,
+                        const GridView& gridView,
+                        const Order<order>& = {})
+    : gridView_{gridView}
+    , grid_{makeGrid_(gridView)}
+    , writer_{Detail::makeWriter(grid_, fmt)}
+    { writer_.set_meta_data("rank", gridView_.comm().rank()); }
+
+    //! Constructor for time series
+    template<typename Format>
+    explicit GridWriter(const Format& fmt,
+                        const GridView& gridView,
+                        const std::string& filename,
+                        const Order<order>& = {})
+    : gridView_{gridView}
+    , grid_{makeGrid_(gridView)}
+    , writer_{Detail::makeWriter(grid_, fmt, filename)}
+    { writer_.set_meta_data("rank", gridView_.comm().rank()); }
+
+    //! Write a single grid file
+    std::string write(const std::string& name) const
+    { return writer_.write(name); }
+
+    //! Write a step in a time series
+    template<std::floating_point T>
+    std::string write(T time) const
+    { return writer_.write(time); }
+
+    //! Set a cell field via a lambda invoked with grid elements
+    template<GridFormat::Concepts::CellFunction<GridView> F,
+             GridFormat::Concepts::Scalar T = GridFormat::FieldScalar<std::invoke_result_t<F, Element>>>
+    void setCellField(const std::string& name, F&& f, const GridFormat::Precision<T>& prec = {})
+    { writer_.set_cell_field(name, std::move(f), prec); }
+
+    //! Set a Dune::Function as cell field
+    template<GridFormat::Dune::Concepts::Function<GridView> F>
+    void setCellField(const std::string& name, F&& f)
+    { GridFormat::Dune::set_cell_function(std::forward<F>(f), writer_, name); }
+
+    //! Set a Dune::Function as cell field with custom precision
+    template<typename F, GridFormat::Concepts::Scalar T>
+    void setCellField(const std::string& name, F&& f, const GridFormat::Precision<T>& prec)
+    { GridFormat::Dune::set_cell_function(std::forward<F>(f), writer_, name, prec); }
+
+    //! Set a point field via a lambda invoked with grid vertices
+    template<GridFormat::Concepts::PointFunction<GridView> F,
+             GridFormat::Concepts::Scalar T = GridFormat::FieldScalar<std::invoke_result_t<F, Vertex>>>
+    void setPointField(const std::string& name, F&& f, const GridFormat::Precision<T>& prec = {})
+    {
+        static_assert(order == 1, "Point lambdas can only be used for order == 1. Use Dune::Functions instead.");
+        writer_.set_point_field(name, std::move(f), prec);
+    }
+
+    //! Set a Dune::Function as point field
+    template<GridFormat::Dune::Concepts::Function<GridView> F>
+    void setPointField(const std::string& name, F&& f)
+    { GridFormat::Dune::set_point_function(std::forward<F>(f), writer_, name); }
+
+    //! Set a Dune::Function as point data with custom precision
+    template<GridFormat::Dune::Concepts::Function<GridView> F, GridFormat::Concepts::Scalar T>
+    void setPointField(const std::string& name, F&& f, const GridFormat::Precision<T>& prec)
+    { GridFormat::Dune::set_point_function(std::forward<F>(f), writer_, name, prec); }
+
+    //! Clear all data
+    void clear()
+    { writer_.clear(); }
+
+    //! Update (must be called when the grid has changed)
+    void update()
+    {
+        if constexpr (order > 1)
+            grid_.update(gridView_);
+    }
+
+ private:
+    Grid makeGrid_(const GridView& gv) const
+    {
+        if constexpr (order > 1)
+            return Grid{gv, order};
+        else
+            return gv;
+    }
+
+    GridView gridView_;
+    Grid grid_;
+    Writer writer_;
+};
+
+} // namespace Dumux::IO
+
+#endif // DUMUX_IO_GRID_HH
---
+++ test/io/test_grid_writer.cc
@@ -0,0 +1,80 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \brief Test for reading/writing from/to different formats with the generic grid writers & readers.
+ */
+#include <config.h>
+
+#include <dune/grid/yaspgrid.hh>
+#if HAVE_DUNE_FUNCTIONS
+#include <dune/functions/gridfunctions/analyticgridviewfunction.hh>
+#endif
+
+#include <dumux/common/initialize.hh>
+#include <dumux/io/gridwriter.hh>
+
+using namespace Dumux::IO;
+
+template<class Writer>
+void setFields(Writer& writer)
+{
+    writer.setPointField("pcoords", [] (const auto& p) {
+        return p.geometry().center();
+    });
+    writer.setCellField("ccoords", [] (const auto& e) {
+        return e.geometry().center();
+    });
+}
+
+template<typename GridView>
+auto writeFirstOrder(const GridView& gridView)
+{
+    GridWriter writer{Format::vtu, gridView, firstOrder};
+    setFields(writer);
+    std::cout << "Wrote " << writer.write("test_grid_writer_first_order") << std::endl;
+}
+
+template<typename GridView>
+auto writeSecondOrder(const GridView& gridView)
+{
+#if HAVE_DUNE_FUNCTIONS
+    GridWriter writer{Format::vtu, gridView, secondOrder};
+    auto f = Dune::Functions::makeAnalyticGridViewFunction([&] (const auto& x) {
+        return x[0]*x[1];
+    }, gridView);
+    writer.setPointField("point_function", f);
+    writer.setCellField("cell_function", f);
+    std::cout << "Wrote " << writer.write("test_grid_writer_second_order") << std::endl;
+#endif
+}
+
+template<typename GridView>
+auto writeSpecifiedFormat(const GridView& gridView)
+{
+    GridWriter writer{Format::vtu.with({.encoder = Encoding::raw}), gridView};
+    setFields(writer);
+    std::cout << "Wrote " << writer.write("test_grid_writer_format_selection") << std::endl;;
+}
+
+int main(int argc, char** argv)
+{
+    Dumux::initialize(argc, argv);
+
+    Dune::YaspGrid<2> grid{
+        {1.0, 1.0},
+        {20, 20},
+        std::bitset<2>(), // periodicity
+        0                // overlap
+    };
+
+    writeFirstOrder(grid.leafGridView());
+    writeSecondOrder(grid.leafGridView());
+    writeSpecifiedFormat(grid.leafGridView());
+
+    return 0;
+}
