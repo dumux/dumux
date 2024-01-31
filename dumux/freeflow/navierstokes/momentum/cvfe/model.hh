664040b7e65aac9d5e540e7e495f4fd29baecd5b
Kai Wendel <kai.wendel@iws.uni-stuttgart.de>
2024-01-31 14:34:55 +0100

    [doc] one-phase one-component does not have molecular diffusion

--- dumux/freeflow/navierstokes/momentum/cvfe/model.hh
+++ dumux/freeflow/navierstokes/momentum/cvfe/model.hh
@@ -75,7 +75,7 @@ struct NavierStokesMomentumCVFEModelTraits
     //! Enable advection
     static constexpr bool enableAdvection() { return true; }
 
-    //! The one-phase model has no molecular diffusion
+    //! The one-phase one-component model has no molecular diffusion
     static constexpr bool enableMolecularDiffusion() { return false; }
 
     //! The model is isothermal
