05b5753af0f1aa4bfd289278d33af45d1f72ee96
Kai Wendel <kai.wendel@iws.uni-stuttgart.de>
2024-01-31 10:44:11 +0100

    [test][channel][doc] changing the explanation for the inlet pressure

--- test/freeflow/navierstokesnc/channel/problem.hh
+++ test/freeflow/navierstokesnc/channel/problem.hh
@@ -135,8 +135,10 @@ public:
             // give the system some time so that the pressure can equilibrate, then start the injection of the tracer
             if (isInlet_(globalPos))
             {
-                // we have formally a dirichlet condition at the inlet for pressure,
-                // the pressure is obtained from the momentum problem via the coupling manager.
+                // With respect to implementation, pressure is treated as if it were a dirichlet condition.
+                // As we want to prescribe the velocity profile in the inlet, we cannot fix the pressure at the same time.
+                // Therefore, the coupling manager is used to get the pressure from the momentum problem and use it as
+                // entry for the dirichlet condition in the mass problem.
                 values[Indices::pressureIdx] = this->couplingManager().cellPressure(element, scvf);
 
                 if (time() >= 10.0 || inletVelocity_  < eps_)
