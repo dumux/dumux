668203eb25e19e0b53229dc4af85b24f8c6cc951
Yue Wang <yue.wang@iws.uni-stuttgart.de>
2024-02-28 16:21:18 +0100

    [material] add new class Shomatemethod

---
+++ dumux/material/components/shomate.hh
@@ -0,0 +1,130 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup Components
+ * \brief Apply Shomate equations for enthalpy and heat capacity.
+ */
+#ifndef DUMUX_SHOMATE_HH
+#define DUMUX_SHOMATE_HH
+#include <vector>
+#include <stdexcept>
+#include <iostream>
+
+namespace Dumux {
+template<class Scalar>
+class ShomateMethod
+{
+public:
+    ShomateMethod(std::vector<Scalar> tempRange, std::vector<std::vector<Scalar>> coeffs)
+    : tempRange_(tempRange)
+    , coeffs_(coeffs)
+    {
+        if (tempRange_.size()-1 != coeffs_.size())
+        {
+            DUNE_THROW(Dune::InvalidStateException,"Temperature range and coefficients must have the same size.");
+        }
+        for(size_t i=0; i<coeffs_.size(); i++)
+        {
+            if (coeffs_[i].size() != 8)
+            {
+                DUNE_THROW(Dune::InvalidStateException,"Shomate coeffs must have 8 elements.");
+            }
+        }
+    }
+
+    std::vector<Scalar> paramsAtTemperature(const Scalar& T) const
+    {
+        // check if T is smaller or higher than allowed min/max T
+        if (T < tempRange_[0])
+        {
+            if (!warningPrinted_)
+            {
+                std::cerr << "Temperature "<< T << " [K] is out of range. Enthalpy values might be extrapolated." << std::endl;
+                warningPrinted_ = true;
+            }
+            return coeffs_[0];
+        }
+
+        for (size_t i = 0; i < tempRange_.size()-1; i++)
+        {
+            if (T >= tempRange_[i] && T < tempRange_[i+1])
+            {
+                return coeffs_[i];
+            }
+        }
+
+        if (!warningPrinted_)
+        {
+            std::cerr << "Temperature "<< T << " [K] is out of range. Enthalpy values might be extrapolated." << std::endl;
+            warningPrinted_ = true;
+        }
+        return coeffs_[tempRange_.size()-1];
+    }
+
+    /**
+     * @brief return enthalpy in kJ/mol
+     *
+     * @param temperature in K
+     * @param pressure in Pa
+     * @return Scalar
+     */
+    Scalar gasEnthalpy(const Scalar& temperature,
+                       const Scalar& pressure) const
+    {
+        auto correctCoeffs = paramsAtTemperature(temperature);
+
+        Scalar A = correctCoeffs[0];
+        Scalar B = correctCoeffs[1];
+        Scalar C = correctCoeffs[2];
+        Scalar D = correctCoeffs[3];
+        Scalar E = correctCoeffs[4];
+        Scalar F = correctCoeffs[5];
+        [[maybe_unused]]Scalar G = correctCoeffs[6];
+        Scalar H = correctCoeffs[7];
+        //convert temperature to correct format
+        Scalar t = temperature/1000.0;
+        //calculate standard enthalpy difference according to Shomate
+        Scalar standardEnthalpyDiff = A*t + B*t*t/2.0 + C*t*t*t/3.0 + D*t*t*t*t/4.0 - E/t + F - H;
+        //unit is kJ/mol
+        return standardEnthalpyDiff;
+    }
+
+    /**
+     * @brief return heat capacity in J/(mol*K)
+     *
+     * @param temperature in K
+     * @param pressure in Pa
+     * @return Scalar
+     */
+    Scalar heatCapacity(const Scalar& temperature,
+                        const Scalar& pressure) const
+    {
+        auto correctCoeffs = paramsAtTemperature(temperature);
+        Scalar A = correctCoeffs[0];
+        Scalar B = correctCoeffs[1];
+        Scalar C = correctCoeffs[2];
+        Scalar D = correctCoeffs[3];
+        Scalar E = correctCoeffs[4];
+        //convert temperature to correct format
+        Scalar t = temperature/1000.0;
+        //calculate standard enthalpy difference according to Shomate
+        Scalar heatCapacity = A + B*t + C*t*t + D*t*t*t + E/(t*t);
+        //unit is J/(mol*K)
+        return heatCapacity;
+    }
+
+private:
+        std::vector<Scalar> tempRange_;
+        std::vector<std::vector<Scalar>> coeffs_;
+        static bool warningPrinted_;
+};
+
+template<class Scalar>
+bool ShomateMethod<Scalar>::warningPrinted_ = false;
+} // namespace Dumux
+#endif // DUMUX_SHOMATE_HH
