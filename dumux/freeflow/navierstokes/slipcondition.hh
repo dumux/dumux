9fa5ee4793d26c221790844c0f0179d150015689
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2024-02-28 20:19:38 +0100

    [tmp][slip] Change interface of velocity helper

--- dumux/freeflow/navierstokes/slipcondition.hh
+++ dumux/freeflow/navierstokes/slipcondition.hh
@@ -31,122 +31,69 @@ inline constexpr BJS bjs{};
 
 } // end namespace SlipModel
 
-template<class GridGeometry, class SlipCondition>
+template<class DiscretizationMethod, class SlipCondition>
 class SlipVelocityHelper;
 
 /*!
  * \ingroup NavierStokesModel
- * \brief Navier Stokes slip velocity helper for Beavers-Joseph condition
+ * \brief Navier Stokes slip velocity helper for fcstaggered discretization
  */
-template<class GridGeometry>
-class SlipVelocityHelper<GridGeometry, SlipConditions::BJ>
+template< class SlipCondition>
+struct SlipVelocityHelper<DiscretizationMethods::FCStaggered, SlipCondition>
 {
-    using GridView = typename GridGeometry::GridView;
-    using Element = typename GridView::template Codim<0>::Entity;
+    static constexpr SlipCondition slipCondition{};
 
-    using FVElementGeometry = typename GridGeometry::LocalView;
-    using SubControlVolume = typename FVElementGeometry::SubControlVolume;
-    using SubControlVolumeFace = typename FVElementGeometry::SubControlVolumeFace;
-
-    static constexpr int dimWorld = GridView::dimensionworld;
-    using Scalar = typename GridView::ctype;
-    using Vector = Dune::FieldVector<Scalar, dimWorld>;
-
-public:
-    template<class Problem, class ElementVolumeVariables>
-    static Vector velocity(const Problem& problem,
+    /*!
+     * \brief Returns the slip velocity at a porous boundary based on the Beavers-Joseph(-Saffman) condition.
+     * \note This only returns a vector filled with one component of the slip velocity (corresponding to the dof axis of the scv the svf belongs to)
+     * \param problem The problem
+     * \param fvGeometry The finite-volume geometry
+     * \param scvf The sub control volume face
+     * \param elemVolVars The volume variables for the element
+     * \param tangentialVelocityDeriv Pre-calculated velocity derivative
+     */
+    template<class Problem, class FVElementGeometry, class ElementVolumeVariables, class Scalar>
+    static auto velocity(const Problem& problem,
                            const FVElementGeometry& fvGeometry,
-                           const SubControlVolumeFace& scvf,
+                           const typename FVElementGeometry::SubControlVolumeFace& scvf,
                            const ElementVolumeVariables& elemVolVars,
-                           Scalar tangentialVelocityGradient)
+                           Scalar tangentialVelocityDeriv)
    {
-        if constexpr (GridGeometry::discMethod == DiscretizationMethods::fcstaggered)
-        {
-            assert(scvf.isLateral());
-            assert(scvf.boundary());
+        assert(scvf.isLateral());
+        assert(scvf.boundary());
 
-            const auto& scv = fvGeometry.scv(scvf.insideScvIdx());
+        static constexpr int dimWorld = FVElementGeometry::GridGeometry::GridView::dimensionworld;
+        using Vector = Dune::FieldVector<Scalar, dimWorld>;
 
-            // create a unit normal vector oriented in positive coordinate direction
-            Vector tangent(0.0);
-            tangent[scv.dofAxis()] = 1.0;
+        Vector porousMediumVelocity(0.0);
 
-            // du/dy + dv/dx = beta * (u_boundary-uPM)
-            // beta = alpha/sqrt(K)
-            const Scalar betaBJ = problem.betaBJ(fvGeometry, scvf, tangent);
-            const Scalar distanceNormalToBoundary = (scvf.ipGlobal() - scv.dofPosition()).two_norm();
+        if constexpr (slipCondition == SlipConditions::bj)
+            porousMediumVelocity = problem.porousMediumVelocity(fvGeometry, scvf);
+        else if (!(slipCondition == SlipConditions::bjs))
+            DUNE_THROW(Dune::NotImplemented, "Fcstaggered currently only implements BJ or BJS slip conditions");
 
-            static const bool onlyNormalGradient = getParamFromGroup<bool>(problem.paramGroup(), "FreeFlow.EnableUnsymmetrizedVelocityGradientForBeaversJoseph", false);
-            if (onlyNormalGradient)
-                tangentialVelocityGradient = 0.0;
+        const auto& scv = fvGeometry.scv(scvf.insideScvIdx());
 
-            const auto& porousMediumVelocity = problem.porousMediumVelocity(fvGeometry, scvf);
-            const Scalar scalarSlipVelocity = (tangentialVelocityGradient*distanceNormalToBoundary
-                + porousMediumVelocity * tangent * betaBJ * distanceNormalToBoundary
-                + elemVolVars[scv].velocity()) / (betaBJ*distanceNormalToBoundary + 1.0);
+        // create a unit normal vector oriented in positive coordinate direction
+        Vector tangent(0.0);
+        tangent[scv.dofAxis()] = 1.0;
 
-            return scalarSlipVelocity*tangent;
-        }
-        else
-            DUNE_THROW(Dune::NotImplemented, "Slip velocity currently only implemented for fcstaggered");
+        // du/dy + dv/dx = beta * (u_boundary-uPM)
+        // beta = alpha/sqrt(K)
+        const Scalar betaBJ = problem.betaBJ(fvGeometry, scvf, tangent);
+        const Scalar distanceNormalToBoundary = (scvf.ipGlobal() - scv.dofPosition()).two_norm();
+
+        static const bool onlyNormalGradient = getParamFromGroup<bool>(problem.paramGroup(), "FreeFlow.EnableUnsymmetrizedVelocityGradientForBeaversJoseph", false);
+        if (onlyNormalGradient)
+            tangentialVelocityDeriv = 0.0;
+
+        const Scalar scalarSlipVelocity = (tangentialVelocityDeriv*distanceNormalToBoundary
+            + porousMediumVelocity * tangent * betaBJ * distanceNormalToBoundary
+            + elemVolVars[scv].velocity()) / (betaBJ*distanceNormalToBoundary + 1.0);
+
+        return scalarSlipVelocity*tangent;
     }
 };
-
-/*!
- * \ingroup NavierStokesModel
- * \brief Navier Stokes slip velocity helper for Beavers-Joseph-Saffman condition
- */
-template<class GridGeometry>
-class SlipVelocityHelper<GridGeometry, SlipConditions::BJS>
-{
-    using GridView = typename GridGeometry::GridView;
-    using Element = typename GridView::template Codim<0>::Entity;
-
-    using FVElementGeometry = typename GridGeometry::LocalView;
-    using SubControlVolume = typename FVElementGeometry::SubControlVolume;
-    using SubControlVolumeFace = typename FVElementGeometry::SubControlVolumeFace;
-
-    static constexpr int dimWorld = GridView::dimensionworld;
-    using Scalar = typename GridView::ctype;
-    using Vector = Dune::FieldVector<Scalar, dimWorld>;
-
-public:
-    template<class Problem, class ElementVolumeVariables>
-    static Vector velocity(const Problem& problem,
-                           const FVElementGeometry& fvGeometry,
-                           const SubControlVolumeFace& scvf,
-                           const ElementVolumeVariables& elemVolVars,
-                           Scalar tangentialVelocityGradient)
-   {
-        if constexpr (GridGeometry::discMethod == DiscretizationMethods::fcstaggered)
-        {
-            assert(scvf.isLateral());
-            assert(scvf.boundary());
-
-            const auto& scv = fvGeometry.scv(scvf.insideScvIdx());
-
-            // create a unit normal vector oriented in positive coordinate direction
-            Vector tangent(0.0);
-            tangent[scv.dofAxis()] = 1.0;
-
-            // du/dy + dv/dx = beta * (u_boundary-uPM)
-            // beta = alpha/sqrt(K)
-            const Scalar betaBJ = problem.betaBJ(fvGeometry, scvf, tangent);
-            const Scalar distanceNormalToBoundary = (scvf.ipGlobal() - scv.dofPosition()).two_norm();
-
-            static const bool onlyNormalGradient = getParamFromGroup<bool>(problem.paramGroup(), "FreeFlow.EnableUnsymmetrizedVelocityGradientForBeaversJoseph", false);
-            if (onlyNormalGradient)
-                tangentialVelocityGradient = 0.0;
-            const Scalar scalarSlipVelocity = (tangentialVelocityGradient*distanceNormalToBoundary
-                + elemVolVars[scv].velocity()) / (betaBJ*distanceNormalToBoundary + 1.0);
-
-            return scalarSlipVelocity*tangent;
-        }
-        else
-            DUNE_THROW(Dune::NotImplemented, "Slip velocity currently only implemented for fcstaggered");
-    }
-};
-
 
 } // end namespace Dumux
 
