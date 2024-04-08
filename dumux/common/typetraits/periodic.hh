b76a47736af1a877f1ab7459b3361ef196a4f131
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2024-04-08 17:24:49 +0200

    [assembly][periodic] Use dof  map whenever existent

---
+++ dumux/common/typetraits/periodic.hh
@@ -0,0 +1,29 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup Typetraits
+ * \brief Type traits to detect periodicity support
+ */
+#ifndef DUMUX_TYPETRAITS_PERIODIC_HH
+#define DUMUX_TYPETRAITS_PERIODIC_HH
+
+#include <dune/common/std/type_traits.hh>
+
+namespace Dumux::Detail {
+
+//! helper struct detecting if a gridGeometry object has a periodicDofMap() function
+template<class GG>
+using GGPeriodicMapDetector = decltype(std::declval<GG>().periodicDofMap());
+
+template<class GG>
+constexpr inline bool hasPeriodicDofMap()
+{ return Dune::Std::is_detected<GGPeriodicMapDetector, GG>::value; }
+
+} // end namespace Dumux::Detail
+
+#endif
