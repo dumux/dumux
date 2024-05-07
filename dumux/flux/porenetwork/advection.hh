f244219528dc3ea0b18c917b787e974625458c94
hanchuan <whc.shmily@gmail.com>
2024-05-07 13:19:26 +0200

    [flux][porenetwork] Add advection type which blocks wetting phase back flow at outlet

--- dumux/flux/porenetwork/advection.hh
+++ dumux/flux/porenetwork/advection.hh
@@ -72,10 +72,141 @@ public:
         assert(isfinite(transmissibility));
         Scalar volumeFlow = transmissibility*deltaP;
 
-#if BLOCKWETTINGOUTLETBACKFLOW
+        // add gravity term
+        static const bool enableGravity = getParamFromGroup<bool>(problem.paramGroup(), "Problem.EnableGravity");
+        if (enableGravity)
+        {
+            const Scalar rho = 0.5*insideVolVars.density(phaseIdx) + 0.5*outsideVolVars.density(phaseIdx);
+            const Scalar g = problem.spatialParams().gravity(scvf.center()) * scvf.unitOuterNormal();
+
+            // The transmissibility is with respect to the effective throat length (potentially dropping the pore body radii).
+            // For gravity, we need to consider the total throat length (i.e., the cell-center to cell-center distance).
+            // This might cause some inconsistencies TODO: is there a better way?
+            volumeFlow += transmissibility * fluxVarsCache.poreToPoreDistance() * rho * g;
+        }
+
+        return volumeFlow;
+    }
+
+    /*!
+     * \brief Returns the throat conductivity
+     */
+    template<class Problem, class Element, class FVElementGeometry, class ElementVolumeVariables, class FluxVariablesCache>
+    static Scalar calculateTransmissibility(const Problem& problem,
+                                            const Element& element,
+                                            const FVElementGeometry& fvGeometry,
+                                            const typename FVElementGeometry::SubControlVolumeFace& scvf,
+                                            const ElementVolumeVariables& elemVolVars,
+                                            const FluxVariablesCache& fluxVarsCache,
+                                            const int phaseIdx)
+    {
+        if constexpr (ElementVolumeVariables::VolumeVariables::numFluidPhases() == 1)
+            return Transmissibility::singlePhaseTransmissibility(problem, element, fvGeometry, scvf, elemVolVars, fluxVarsCache, phaseIdx);
+        else
+        {
+            static_assert(ElementVolumeVariables::VolumeVariables::numFluidPhases() == 2);
+
+            const auto& spatialParams = problem.spatialParams();
+            using FluidSystem = typename ElementVolumeVariables::VolumeVariables::FluidSystem;
+            const int wPhaseIdx = spatialParams.template wettingPhase<FluidSystem>(element, elemVolVars);
+            using SubControlVolumeFace = typename FVElementGeometry::SubControlVolumeFace;
+            const bool invaded = fluxVarsCache.invaded();
+            if constexpr (Dumux::Detail::hasProblemThetaFunction<Problem, Element, FVElementGeometry, ElementVolumeVariables, FluxVariablesCache, SubControlVolumeFace>())
+            {
+                auto theta = problem.theta(element, fvGeometry, elemVolVars, fluxVarsCache, scvf);
+                theta = std::clamp(theta, 0.0, 1.0);
+                if (phaseIdx == wPhaseIdx)
+                {
+                    const Scalar k1p = Transmissibility::singlePhaseTransmissibility(problem, element, fvGeometry, scvf, elemVolVars, fluxVarsCache, phaseIdx);
+                    const Scalar kw = invaded ? Transmissibility::wettingLayerTransmissibility(element, fvGeometry, scvf, fluxVarsCache)
+                                              : Transmissibility::entryWettingLayerTransmissibility(element, fvGeometry, scvf, fluxVarsCache);
+
+                    return theta*kw + (1-theta)*k1p;
+                }
+                else // non-wetting phase
+                {
+                    // auto entryKn = Transmissibility::entryNonWettingPhaseTransmissibility(element, fvGeometry, scvf, fluxVarsCache);
+                    return  theta*Transmissibility::nonWettingPhaseTransmissibility(element, fvGeometry, scvf, fluxVarsCache);
+                }
+            }
+            else
+            {
+                if (phaseIdx == wPhaseIdx)
+                {
+                    return invaded ? Transmissibility::wettingLayerTransmissibility(element, fvGeometry, scvf, fluxVarsCache)
+                                   : Transmissibility::singlePhaseTransmissibility(problem, element, fvGeometry, scvf, elemVolVars, fluxVarsCache, phaseIdx);
+                }
+                else // non-wetting phase
+                {
+                    return invaded ? Transmissibility::nonWettingPhaseTransmissibility(element, fvGeometry, scvf, fluxVarsCache)
+                                   : 0.0;
+                }
+            }
+        }
+    }
+
+    template<class Problem, class Element, class FVElementGeometry, class ElementVolumeVariables, class FluxVariablesCache>
+    static std::array<Scalar, 2> calculateTransmissibilities(const Problem& problem,
+                                                             const Element& element,
+                                                             const FVElementGeometry& fvGeometry,
+                                                             const ElementVolumeVariables& elemVolVars,
+                                                             const typename FVElementGeometry::SubControlVolumeFace& scvf,
+                                                             const FluxVariablesCache& fluxVarsCache)
+    {
+        static_assert(ElementVolumeVariables::VolumeVariables::numFluidPhases() == 1);
+        const Scalar t = calculateTransmissibility(problem, element, fvGeometry, scvf, elemVolVars, fluxVarsCache, 0);
+        return {t, -t};
+    }
+};
+
+/*!
+ * \ingroup PoreNetworkFlux
+ * \brief Hagen–Poiseuille-type flux law to describe the advective flux, blocking wetting phase backflow at outlet
+ */
+template<class ScalarT, class... TransmissibilityLawTypes>
+class CreepingFlowBlockingWPhaseOutletBack
+{
+
+public:
+    //! Export the Scalar type
+    using Scalar = ScalarT;
+
+    //! Export the transmissibility law
+    using Transmissibility = Detail::Transmissibility<TransmissibilityLawTypes...>;
+
+    /*!
+     * \brief Returns the advective flux of a fluid phase
+     *        across the given sub-control volume face (corresponding to a pore throat).
+     * \note The flux is given in N*m, and can be converted
+     *       into a volume flux (m^3/s) or mass flux (kg/s) by applying an upwind scheme
+     *       for the mobility (1/viscosity) or the product of density and mobility, respectively.
+     */
+    template<class Problem, class Element, class FVElementGeometry,
+             class ElementVolumeVariables, class SubControlVolumeFace, class ElemFluxVarsCache>
+    static Scalar flux(const Problem& problem,
+                       const Element& element,
+                       const FVElementGeometry& fvGeometry,
+                       const ElementVolumeVariables& elemVolVars,
+                       const SubControlVolumeFace& scvf,
+                       const int phaseIdx,
+                       const ElemFluxVarsCache& elemFluxVarsCache)
+    {
+        const auto& fluxVarsCache = elemFluxVarsCache[scvf];
+
+        // Get the inside and outside volume variables
+        const auto& insideScv = fvGeometry.scv(scvf.insideScvIdx());
+        const auto& outsideScv = fvGeometry.scv(scvf.outsideScvIdx());
+        const auto& insideVolVars = elemVolVars[insideScv];
+        const auto& outsideVolVars = elemVolVars[outsideScv];
+        // calculate the pressure difference
+        const Scalar deltaP = insideVolVars.pressure(phaseIdx) - outsideVolVars.pressure(phaseIdx);
+        const Scalar transmissibility = fluxVarsCache.transmissibility(phaseIdx);
+        using std::isfinite;
+        assert(isfinite(transmissibility));
+        Scalar volumeFlow = transmissibility*deltaP;
+
         if (fvGeometry.gridGeometry().throatLabel(fvGeometry.gridGeometry().elementMapper().index(element)) == 3 && deltaP < 0 && phaseIdx == 0)
             return 0;
-#endif
 
         // add gravity term
         static const bool enableGravity = getParamFromGroup<bool>(problem.paramGroup(), "Problem.EnableGravity");
