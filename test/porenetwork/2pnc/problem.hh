45941e070286c8e2fa94b6145c01462bf2feb9e1
Leon Keim <leon.keim@googlemail.com>
2024-06-24 12:26:54 +0000

    reorder includes

--- test/porenetwork/2pnc/problem.hh
+++ test/porenetwork/2pnc/problem.hh
@@ -12,11 +12,11 @@
 #ifndef DUMUX_PNM_2P_NC_PROBLEM_HH
 #define DUMUX_PNM_2P_NC_PROBLEM_HH
 
-#include <dumux/porenetwork/2pnc/model.hh>
 #include <dumux/common/boundarytypes.hh>
 #include <dumux/common/parameters.hh>
-#include <dumux/porousmediumflow/problem.hh>
 #include <dumux/material/components/air.hh>
+#include <dumux/porousmediumflow/problem.hh>
+#include <dumux/porenetwork/2pnc/model.hh>
 
 namespace Dumux {
 
