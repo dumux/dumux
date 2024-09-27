01713e30d156eeba32ec7fa794520bedbec488f7
Timo Koch <timokoch@uio.no>
2024-09-27 14:35:18 +0200

    [test][linear] Add integration test with MultiTypeBlockMatrix

--- test/linear/test_linearsolver.cc
+++ test/linear/test_linearsolver.cc
@@ -11,12 +11,16 @@
 #include <dune/common/exceptions.hh>
 #include <dune/common/fvector.hh>
 #include <dune/common/fmatrix.hh>
+#include <dune/common/classname.hh>
+#include <dune/common/hybridutilities.hh>
 
 #include <dune/grid/common/mcmgmapper.hh>
 #include <dune/grid/yaspgrid.hh>
 
 #include <dune/istl/bvector.hh>
 #include <dune/istl/bcrsmatrix.hh>
+#include <dune/istl/multitypeblockvector.hh>
+#include <dune/istl/multitypeblockmatrix.hh>
 
 #include <dune/istl/test/laplacian.hh>
 #include <dune/istl/paamg/test/anisotropic.hh>
@@ -50,7 +54,7 @@ void solveWithFactory(M& A, X& x, V& b, const std::string& paramGroup)
                                                   LinearAlgebraTraits<M,V>>;
     LinearSolver solver(paramGroup);
 
-    std::cout << "Solving Laplace problem with " << solver.name() << "\n";
+    std::cout << "Solving Laplace problem with " << solver.name() << " (matrix type: " << Dune::className<M>() << ")\n";
     solver.solve(A, x, b);
     if (!solver.result().converged)
         DUNE_THROW(Dune::Exception, solver.name() << " did not converge!");
@@ -71,14 +75,15 @@ int main(int argc, char* argv[])
     using Matrix = Dune::BCRSMatrix<Dune::FieldMatrix<double, 2, 2>>;
 
     // create matrix & vectors (we want to solve Ax=b)
-    Matrix A; const int numDofs = getParam<int>("ProblemSize");
+    Matrix A;
+    const int numDofs = getParam<int>("ProblemSize");
     setupLaplacian(A, numDofs);
 
     Vector x(A.N()); Vector b(A.M());
-    x = 0; b = 1;
 
     // AMGBiCGSTABBackend
     {
+        x = 0; b = 1;
         std::cout << std::endl;
 
         const auto testSolverName = "AMGBiCGSTAB";
@@ -96,8 +101,41 @@ int main(int argc, char* argv[])
     }
 
     // IstlSolverFactoryBackend
-    Test::solveWithFactory(A, x, b, "AMGCG");
-    Test::solveWithFactory(A, x, b, "SSORCG");
+    {
+        x = 0; b = 1;
+        Test::solveWithFactory(A, x, b, "AMGCG");
+
+        x = 0; b = 1;
+        Test::solveWithFactory(A, x, b, "SSORCG");
+    }
+
+    // MultiTypeBlockMatrix and IstlSolverFactoryBackend
+    {
+        using MTVector = Dune::MultiTypeBlockVector<Vector, Vector, Vector>;
+        using MTMatrixRow = Dune::MultiTypeBlockVector<Matrix, Matrix, Matrix>;
+        using MTMatrix = Dune::MultiTypeBlockMatrix<MTMatrixRow, MTMatrixRow, MTMatrixRow>;
+        MTMatrix MTA; MTVector MTx, MTb;
+
+        // reset matrix & vectors
+        setupLaplacian(A, numDofs);
+        x = 0; b = 1;
+
+        Dune::Hybrid::forEach(std::make_index_sequence<MTA.N()>{}, [&](auto i){
+            MTA[i][i] = A;
+            MTx[i] = x;
+            MTb[i] = b;
+
+            Dune::Hybrid::forEach(std::make_index_sequence<MTA.M()>{}, [&](auto j){
+                if constexpr (i != j)
+                {
+                    MTA[i][j] = A; // set some pattern
+                    MTA[i][j] = 0; // set all entries to zero to solve uncoupled problems
+                }
+            });
+        });
+
+        Test::solveWithFactory(MTA, MTx, MTb, "SSORBiCGSTAB");
+    }
 
     return 0;
 }
