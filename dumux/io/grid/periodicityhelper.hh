10946a92e85193ff78305f4f9dde31042eee9693
Mathis Kelm <mathis.kelm@iws.uni-stuttgart.de>
2024-10-10 10:47:31 +0200

    [grid] Add periodicity check for YASP-Grid

--- dumux/io/grid/periodicityhelper.hh
+++ dumux/io/grid/periodicityhelper.hh
@@ -2,7 +2,10 @@
 #ifndef DUMUX_DISCRETIZATION_PERIODICITY_HELPER_HH
 #define DUMUX_DISCRETIZATION_PERIODICITY_HELPER_HH
 
+#include <dune/common/float_cmp.hh>
+
 #include <dune/grid/spgrid.hh>
+#include <dune/grid/yaspgrid.hh>
 
 namespace Dumux {
 
@@ -23,6 +26,23 @@ struct PeriodicityHelper<Dune::SPGrid<ct, dim, Ref, Comm>>
     static bool isPeriodic (const Intersection& intersection)
     {
         return intersection.neighbor() && intersection.boundary();
+    }
+};
+
+template<class ct, int dim>
+struct PeriodicityHelper<Dune::YaspGrid<dim, ct>>
+{
+    template<typename Intersection>
+    static bool isPeriodic (const Intersection& intersection)
+    {
+        if (!intersection.neighbor())
+            return false;
+        const auto distance = intersection.outside().geometry().center() - intersection.inside().geometry().center();
+        const auto& intersectionUnitOuterNormal = intersection.centerUnitOuterNormal();
+        const auto reverseNormal = distance/distance.two_norm();
+        if (Dune::FloatCmp::eq(intersectionUnitOuterNormal*reverseNormal, -1.0, 1e-7))
+            return true;
+        return false;
     }
 };
 
