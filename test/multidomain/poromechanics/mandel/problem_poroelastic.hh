8f8be7fb1889d73aa4dd8fe49bb03c3c86dca789
Yue Wang <yue.wang@iws.uni-stuttgart.de>
2024-10-17 16:48:59 +0200

    changed 1p file and elspatial

---
+++ test/multidomain/poromechanics/mandel/problem_poroelastic.hh
@@ -0,0 +1,111 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup PoromechanicsTests
+ * \brief The poro-elastic sub-problem in the el1p coupled problem.
+ */
+
+#ifndef DUMUX_POROELASTIC_SUBPROBLEM_HH
+#define DUMUX_POROELASTIC_SUBPROBLEM_HH
+
+#include <dune/common/fmatrix.hh>
+
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/boundarytypes.hh>
+
+#include <dumux/geomechanics/fvproblem.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup PoromechanicsTests
+ * \brief The poro-elastic sub-problem in the el1p coupled problem.
+ */
+template<class TypeTag>
+class PoroElasticSubProblem : public GeomechanicsFVProblem<TypeTag>
+{
+    using ParentType = GeomechanicsFVProblem<TypeTag>;
+
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using Indices = typename GetPropType<TypeTag, Properties::ModelTraits>::Indices;
+    using BoundaryTypes = Dumux::BoundaryTypes<GetPropType<TypeTag, Properties::ModelTraits>::numEq()>;
+    using PrimaryVariables = GetPropType<TypeTag, Properties::PrimaryVariables>;
+    using ElementVolumeVariables = typename GetPropType<TypeTag, Properties::GridVolumeVariables>::LocalView;
+
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    using FVElementGeometry = typename GridGeometry::LocalView;
+    using SubControlVolume = typename GridGeometry::SubControlVolume;
+
+    using GridView = typename GetPropType<TypeTag, Properties::GridGeometry>::GridView;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+
+    static constexpr int dim = GridView::dimension;
+    static constexpr int dimWorld = GridView::dimensionworld;
+    using GradU = Dune::FieldMatrix<Scalar, dim, dimWorld>;
+
+public:
+    PoroElasticSubProblem(std::shared_ptr<const GridGeometry> gridGeometry,
+                          std::shared_ptr<GetPropType<TypeTag, Properties::SpatialParams>> spatialParams,
+                          const std::string& paramGroup = "PoroElastic")
+    : ParentType(gridGeometry, spatialParams, paramGroup)
+    {
+        problemName_  =  getParam<std::string>("Vtk.OutputName") + "_" + getParamFromGroup<std::string>(this->paramGroup(), "Problem.Name");
+    }
+
+    /*!
+     * \brief The problem name.
+     */
+    const std::string& name() const
+    {
+        return problemName_;
+    }
+    //! Evaluates the initial value for a control volume.
+    PrimaryVariables initialAtPos(const GlobalPosition& globalPos) const
+    { return PrimaryVariables(0.0); }
+
+    //! Evaluates the boundary conditions for a Dirichlet boundary segment.
+    PrimaryVariables dirichletAtPos(const GlobalPosition& globalPos) const
+    { return PrimaryVariables(0.0); }
+
+    //! Evaluates the boundary conditions for a Neumann boundary segment.
+    PrimaryVariables neumannAtPos(const GlobalPosition& globalPos) const
+    { return PrimaryVariables(0.0); }
+
+    /*!
+     * \brief Specifies which kind of boundary condition should be
+     *        used for which equation on a given boundary segment.
+     *
+     * \param globalPos The global position
+     */
+    BoundaryTypes boundaryTypesAtPos(const GlobalPosition& globalPos) const
+    {
+        BoundaryTypes values;
+        values.setAllDirichlet();
+        return values;
+    }
+
+    /*!
+     * \brief Evaluates the source term for all phases within a given
+     *        sub-control volume.
+     */
+    PrimaryVariables source(const Element& element,
+                            const FVElementGeometry& fvGeometry,
+                            const ElementVolumeVariables& elemVolVars,
+                            const SubControlVolume& scv) const
+    { return PrimaryVariables(0.0); }
+
+private:
+    static constexpr Scalar eps_ = 3e-6;
+    std::string problemName_;
+};
+
+} // end namespace Dumux
+
+#endif
---
+++ test/multidomain/poromechanics/mandel/spatialparams_poroelastic.hh
@@ -0,0 +1,122 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup PoromechanicsTests
+ * \brief Definition of the spatial parameters for the poro-elastic problem.
+ */
+#ifndef DUMUX_POROELASTIC_SPATIAL_PARAMS_HH
+#define DUMUX_POROELASTIC_SPATIAL_PARAMS_HH
+
+#include <dumux/geomechanics/lameparams.hh>
+#include <dumux/geomechanics/poroelastic/fvspatialparams.hh>
+#include <dumux/material/fluidmatrixinteractions/porositydeformation.hh>
+
+#include "analyticalsolution/analyticalsolution.hh"
+namespace Dumux {
+
+/*!
+ * \ingroup PoromechanicsTests
+ * \brief Definition of the spatial parameters for the poro-elastic
+ *        sub-problem in the coupled poro-mechanical el1p problem.
+ */
+template<class Scalar, class GridGeometry, class CouplingManager>
+class PoroElasticSpatialParams : public FVPoroElasticSpatialParams< GridGeometry,
+                                                                    Scalar,
+                                                                    PoroElasticSpatialParams<Scalar, GridGeometry, CouplingManager> >
+{
+    using ThisType = PoroElasticSpatialParams<Scalar, GridGeometry, CouplingManager>;
+    using ParentType = FVPoroElasticSpatialParams<GridGeometry, Scalar, ThisType>;
+
+    using SubControlVolume = typename GridGeometry::SubControlVolume;
+    using GridView = typename GridGeometry::GridView;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using FVElementGeometry = typename GridGeometry::LocalView;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+    using AnalyticalSolution = MandelAnalyticalSolution<Scalar>;
+public:
+    //! Export the type of the lame parameters
+    using LameParams = Dumux::LameParams<Scalar>;
+
+    PoroElasticSpatialParams(std::shared_ptr<const GridGeometry> gridGeometry,
+                             std::shared_ptr<CouplingManager> couplingManagerPtr)
+    : ParentType(gridGeometry)
+    , couplingManager_(couplingManagerPtr)
+    , initPorosity_(getParam<Scalar>("SpatialParams.InitialPorosity"))
+    {
+        // Young's modulus [Pa]
+        Scalar E = getParam<Scalar>("MaterialParameters.EModulus");
+        // Poisson's ratio [-]
+        Scalar nu = getParam<Scalar>("MaterialParameters.Nu");
+        // Lame parameters [Pa]
+        lameParams_.setLambda((E * nu) / ((1 + nu) * (1 - 2 * nu)));
+        lameParams_.setMu(E / (2 * (1 + nu)));
+    }
+
+    //! Defines the Lame parameters.
+    const LameParams& lameParamsAtPos(const GlobalPosition& globalPos) const
+    { return lameParams_; }
+
+    //! Returns the porosity of the porous medium.
+    template<class ElementSolution>
+    Scalar porosity(const Element& element,
+                    const SubControlVolume& scv,
+                    const ElementSolution& elemSol) const
+
+    {
+        return PorosityDeformation<Scalar>::evaluatePorosity(this->gridGeometry(), element, scv, elemSol, initPorosity_);
+    }
+
+    /*!
+     * \brief Returns the effective fluid density.
+     */
+    Scalar effectiveFluidDensity(const Element& element,
+                                 const SubControlVolume& scv) const
+    {
+        // get porous medium flow volume variables from coupling manager
+        const auto& pmFlowVolVars = couplingManager().getPMFlowVolVars(element);
+        return pmFlowVolVars.density();
+    }
+
+    /*!
+     * \brief Returns the effective pore pressure.
+     */
+    template<class ElementVolumeVariables, class FluxVarsCache>
+    Scalar effectivePorePressure(const Element& element,
+                                 const FVElementGeometry& fvGeometry,
+                                 const ElementVolumeVariables& elemVolVars,
+                                 const FluxVarsCache& fluxVarsCache) const
+    {
+        // get porous medium flow volume variables from coupling manager
+        const auto& pmFlowVolVars = couplingManager().getPMFlowVolVars(element);
+        return pmFlowVolVars.pressure();
+    }
+
+    //! Returns the Biot coefficient of the porous medium.
+    Scalar biotCoefficientAtPos(const GlobalPosition& globalPos) const
+    { return 1.0; }
+
+    //! Returns reference to the coupling manager.
+    const CouplingManager& couplingManager() const
+    { return *couplingManager_; }
+
+    const auto& analyticalSolution() const
+    { return *analyticalSolutionPtr_;}
+
+    void setAnalyticalSolution(const AnalyticalSolution& analyticalSolution)
+    { analyticalSolutionPtr_ = &analyticalSolution;}
+
+private:
+    std::shared_ptr<const CouplingManager> couplingManager_;
+    const AnalyticalSolution *analyticalSolutionPtr_;
+    Scalar initPorosity_;
+    LameParams lameParams_;
+};
+
+} // end namespace Dumux
+
+#endif
