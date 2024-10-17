4fa480d9b8fe6d981ce7b4a0e4bcd4eb9fe385b1
Yue Wang <yue.wang@iws.uni-stuttgart.de>
2024-10-17 12:33:04 +0200

    [test/mandel] add test

---
+++ test/multidomain/poromechanics/mandel/problem_1p.hh
@@ -0,0 +1,101 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup PoromechanicsTests
+ * \brief Definition of the spatial parameters for the single-phase flow
+ *        sub-problem in the coupled poro-mechanical el1p problem.
+ */
+#ifndef DUMUX_1P_SUB_PROBLEM_HH
+#define DUMUX_1P_SUB_PROBLEM_HH
+
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/boundarytypes.hh>
+#include <dumux/common/numeqvector.hh>
+
+#include <dumux/porousmediumflow/problem.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup PoromechanicsTests
+ * \brief The single-phase sub problem in the el1p coupled problem.
+ */
+template <class TypeTag>
+class OnePSubProblem : public PorousMediumFlowProblem<TypeTag>
+{
+    using ParentType = PorousMediumFlowProblem<TypeTag>;
+
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using GridView = typename GetPropType<TypeTag, Properties::GridGeometry>::GridView;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+
+    // copy pressure index for convenience
+    enum { pressureIdx = GetPropType<TypeTag, Properties::ModelTraits>::Indices::pressureIdx };
+
+    using PrimaryVariables = GetPropType<TypeTag, Properties::PrimaryVariables>;
+    using NumEqVector = Dumux::NumEqVector<PrimaryVariables>;
+    using BoundaryTypes = Dumux::BoundaryTypes<GetPropType<TypeTag, Properties::ModelTraits>::numEq()>;
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+
+public:
+    OnePSubProblem(std::shared_ptr<const GridGeometry> gridGeometry,
+                   std::shared_ptr<GetPropType<TypeTag, Properties::SpatialParams>> spatialParams,
+                   const std::string& paramGroup = "OneP")
+    : ParentType(gridGeometry, spatialParams, paramGroup)
+    {
+        problemName_  =  getParam<std::string>("Vtk.OutputName") + "_" + getParamFromGroup<std::string>(this->paramGroup(), "Problem.Name");
+    }
+
+    /*!
+     * \brief The problem name.
+     */
+    const std::string& name() const
+    {
+        return problemName_;
+    }
+
+    //! Evaluates the boundary conditions for a Dirichlet boundary segment.
+    PrimaryVariables dirichletAtPos(const GlobalPosition &globalPos) const
+    { return PrimaryVariables(0.0); }
+
+    //! Evaluates the initial value for a control volume.
+    PrimaryVariables initialAtPos(const GlobalPosition& globalPos) const
+    { return this->spatialParams().analyticalSolution().initialPressure();}
+
+    //! Evaluates source terms.
+    NumEqVector sourceAtPos(const GlobalPosition& globalPos) const
+    { return NumEqVector(0.0);}
+
+    /*!
+     * \brief Specifies which kind of boundary condition should be
+     *        used for which equation on a given boundary segment.
+     *
+     * \param globalPos The global position
+     */
+    BoundaryTypes boundaryTypesAtPos(const GlobalPosition &globalPos) const
+    {
+       BoundaryTypes values;
+        values.setAllNeumann();
+
+        // right boundary
+        if (globalPos[0] > this->gridGeometry().bBoxMax()[0]- eps_)
+            values.setAllDirichlet();
+
+        return values;
+    }
+
+private:
+    static constexpr Scalar eps_ = 1.0e-6;
+    std::string problemName_;
+};
+
+} // end namespace Dumux
+
+#endif
---
+++ test/multidomain/poromechanics/mandel/spatialparams_1p.hh
@@ -0,0 +1,103 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup PoromechanicsTests
+ * \brief The spatial parameters class for the test problem using the
+ *        1p box model.
+ */
+
+#ifndef DUMUX_1P_TEST_SPATIALPARAMS_HH
+#define DUMUX_1P_TEST_SPATIALPARAMS_HH
+
+#include <dumux/discretization/elementsolution.hh>
+
+#include <dumux/porousmediumflow/fvspatialparams1p.hh>
+#include <dumux/material/gstatrandomfield.hh>
+#include <dumux/material/fluidmatrixinteractions/porositydeformation.hh>
+
+#include "analyticalsolution/analyticalsolution.hh"
+namespace Dumux {
+
+/*!
+ * \ingroup PoromechanicsTests
+ * \brief The spatial parameters class for the test problem using the
+ *        1p box model.
+ */
+template<class GridGeometry, class Scalar, class CouplingManager>
+class OnePSpatialParams : public FVPorousMediumFlowSpatialParamsOneP<GridGeometry, Scalar,
+                                                                 OnePSpatialParams<GridGeometry, Scalar, CouplingManager>>
+{
+    using SubControlVolume = typename GridGeometry::SubControlVolume;
+    using GridView = typename GridGeometry::GridView;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+
+    using ThisType = OnePSpatialParams<GridGeometry, Scalar, CouplingManager>;
+    using ParentType = FVPorousMediumFlowSpatialParamsOneP<GridGeometry, Scalar, ThisType>;
+
+    using AnalyticalSolution = MandelAnalyticalSolution<Scalar>;
+public:
+    // export permeability type
+    using PermeabilityType = Scalar;
+
+    OnePSpatialParams(std::shared_ptr<const GridGeometry> gridGeometry,
+                      std::shared_ptr<CouplingManager> couplingManagerPtr)
+    : ParentType(gridGeometry)
+    , couplingManagerPtr_(couplingManagerPtr)
+    , permeability_(getParam<Scalar>("SpatialParams.Permeability"))
+    , initPorosity_(getParam<Scalar>("SpatialParams.InitialPorosity"))
+    {}
+
+    //! Returns the permeability at a given position.
+    PermeabilityType permeabilityAtPos(const GlobalPosition& globalPoss) const
+    { return permeability_; }
+
+    //! Returns the porosity for a sub-control volume.
+    template<class ElementSolution>
+    Scalar porosity(const Element& element,
+                    const SubControlVolume& scv,
+                    const ElementSolution& elemSol) const
+    {
+        static constexpr auto poroMechId = CouplingManager::poroMechId;
+
+        const auto& poroMechGridGeom = couplingManagerPtr_->problem(poroMechId).gridGeometry();
+        const auto poroMechElemSol = elementSolution(element, couplingManagerPtr_->curSol(poroMechId), poroMechGridGeom);
+
+        // evaluate the deformation-dependent porosity at the scv center
+        return PorosityDeformation<Scalar>::evaluatePorosity(poroMechGridGeom, element, scv.center(), poroMechElemSol, initPorosity_);
+    }
+
+    template<class SubControlVolume>
+    Scalar initPorePressure(const Element& element, const SubControlVolume& scv) const
+    {
+        return analyticalSolution().initialPressure();
+    }
+
+    //! Returns reference to the coupling manager.
+    const CouplingManager& couplingManager() const
+    { return *couplingManagerPtr_; }
+
+    const auto& analyticalSolution() const
+    {
+        return *analyticalSolutionPtr_;
+    }
+
+    void setAnalyticalSolution(const AnalyticalSolution& analyticalSolution)
+    {
+        analyticalSolutionPtr_ = &analyticalSolution;
+    }
+private:
+    std::shared_ptr<const CouplingManager> couplingManagerPtr_;
+    const AnalyticalSolution *analyticalSolutionPtr_;
+    Scalar permeability_;
+    Scalar initPorosity_;
+};
+
+} // end namespace Dumux
+
+#endif
