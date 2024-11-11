5c49d05d433fccb5eb4c9b33f330999c36123f05
Timo Koch <timok@simula.no>
2024-11-11 12:10:35 +0100

    [test][mandel] Cleanup and test analytical solution

--- test/multidomain/poromechanics/mandel/analyticalsolution/test_analyticalsolution.cc
+++ test/multidomain/poromechanics/mandel/analyticalsolution/test_analyticalsolution.cc
@@ -5,37 +5,43 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 //
 
+#include <dumux/common/initialize.hh>
 #include <dumux/common/parameters.hh>
-#include <dune/common/fvector.hh>
+
 #include <dumux/io/container.hh>
+#include <dumux/io/format.hh>
+
+#include <dune/common/fvector.hh>
+
 #include "analyticalsolution.hh"
 
-int main(int argc, char * argv[])
+int main(int argc, char** argv)
 {
     using namespace Dumux;
 
-    // initialize parameter tree
+    Dumux::initialize(argc, argv);
     Parameters::init(argc, argv);
-    const auto posNormx = readFileToContainer<std::vector<double>>("pos.txt");
+
     MandelAnalyticalSolution<double> solver;
 
-    std::vector<int> timeSeries{10,50, 100,1000,5000,8000,10000,20000,30000,50000};
-    for(const auto & t : timeSeries)
+    const auto posx = readFileToContainer<std::vector<double>>("pos.txt");
+    std::vector<int> times{ 10, 50, 100, 1000, 5000, 8000, 10000, 20000, 30000, 50000 };
+    for(const auto& t : times)
     {
         std::vector<double> pSol;
         std::vector<double> uxSol;
 
-        for(auto uNormx : posNormx)
+        for (const auto x : posx)
         {
-            auto v = Dune::FieldVector<double,2>{uNormx*100,0.0};
-            auto p = solver.pressureAtPos(v,t) * 100 / 6e8;
-            auto ux = solver.displacementAtPos(v,t)[0];
+            auto pos = Dune::FieldVector<double, 2>{ x*100.0, 0.0 };
+            auto p = solver.pressure(pos, t) * 100.0 / 6e8;
+            auto ux = solver.displacement(pos, t)[0];
             pSol.push_back(p);
-            uxSol.push_back(ux/100);
-           // std::cout <<"uNormx"<< uNormx << "p= "<< p<< std::endl;
+            uxSol.push_back(ux / 100.0);
         }
-        writeContainerToFile(pSol,"pSol"+std::to_string(t)+".txt");
-        writeContainerToFile(uxSol,"uxSol"+std::to_string(t)+".txt");
+
+        writeContainerToFile(pSol, Fmt::format("p-{}.txt", t));
+        writeContainerToFile(uxSol, Fmt::format("ux-{}.txt", t));
     }
 
     return 0;
