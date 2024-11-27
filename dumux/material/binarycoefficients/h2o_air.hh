1a1958b1ca23881ebf926e27908294a65429d5d6
mkosiol <mn_github@kosiol.net>
2024-11-27 13:29:10 +0100

    Made henry variables static constexpr

--- dumux/material/binarycoefficients/h2o_air.hh
+++ dumux/material/binarycoefficients/h2o_air.hh
@@ -58,10 +58,10 @@ public:
     template <class Scalar>
     static Scalar henryMixture(Scalar temperature)
     {
-        const Scalar yO2_air=Dumux::Components::Air<Scalar>::airMoleFraction::O2;
-        const Scalar yN2_air=Dumux::Components::Air<Scalar>::airMoleFraction::N2;
-        const Scalar yCO2_air=Dumux::Components::Air<Scalar>::airMoleFraction::CO2;
-        const Scalar yAr_air=Dumux::Components::Air<Scalar>::airMoleFraction::Ar;
+        static constexpr Scalar yO2_air=Dumux::Components::Air<Scalar>::airMoleFraction::O2;
+        static constexpr Scalar yCO2_air=Dumux::Components::Air<Scalar>::airMoleFraction::CO2;
+        static constexpr Scalar yAr_air=Dumux::Components::Air<Scalar>::airMoleFraction::Ar;
+        static constexpr Scalar yN2_air=Dumux::Components::Air<Scalar>::airMoleFraction::N2;
 
         Scalar kh_O2=H2O_O2::henry(temperature);
         Scalar kh_N2=H2O_N2::henry(temperature);
