918f228c9433574ef03dbe517e0d39d086a1b240
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2025-01-13 12:12:16 +0100

    [typetraits] Add detectors and default ranges for local dofs

---
+++ dumux/common/typetraits/localdofs.hh
@@ -0,0 +1,38 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup Typetraits
+ * \brief Type traits to be used for detecting new interfaces related to local dofs
+ */
+#ifndef DUMUX_TYPETRAITS_LOCALDOFS_HH
+#define DUMUX_TYPETRAITS_LOCALDOFS_HH
+
+#include <dune/common/std/type_traits.hh>
+#include <dune/common/rangeutilities.hh>
+
+#include <dumux/common/indextraits.hh>
+#include <dumux/discretization/cvfe/localdof.hh>
+
+namespace Dumux::Detail {
+
+//! helper struct detecting if a fvElementGeometry object has maxNumElementDofs
+template<class Imp>
+using MaxNumElementDofs = decltype( Imp::maxNumElementDofs );
+
+template<typename FVElementGeometry>
+constexpr int maxNumLocalDofs()
+{
+    if constexpr (Dune::Std::is_detected<MaxNumElementDofs, FVElementGeometry>::value)
+        return FVElementGeometry::maxNumElementDofs;
+    else
+        return FVElementGeometry::maxNumElementScvs;
+}
+
+} // end namespace Dumux::Detail
+
+#endif
