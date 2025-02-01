58484ff2f5028d789c19b0795cf6fe036e9df88b
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2025-02-01 11:42:07 +0100

    [tests][ffmomentum][donea] Implement new tests according new problem interfaces
    * New boundary functions
    * Global constraint map
    * Add hybrid test case

--- test/freeflow/navierstokes/donea/properties_momentum.hh
+++ test/freeflow/navierstokes/donea/properties_momentum.hh
@@ -28,6 +28,14 @@
 #define DISCRETIZATION_MODEL FaceCenteredStaggeredModel
 #endif
 
+#ifndef NEW_PROBLEM_INTERFACE
+#define NEW_PROBLEM_INTERFACE 0
+#endif
+
+#ifndef NEW_VARIABLES_INTERFACE
+#define NEW_VARIABLES_INTERFACE 0
+#endif
+
 #include <dune/grid/yaspgrid.hh>
 
 #include <dumux/flux/fluxvariablescaching.hh>
@@ -39,11 +47,13 @@
 #include <dumux/freeflow/navierstokes/momentum/problem.hh>
 #include <dumux/discretization/fcstaggered.hh>
 #include <dumux/freeflow/navierstokes/momentum/cvfe/model.hh>
+#include <dumux/freeflow/navierstokes/momentum/cvfe/variables.hh>
 
 #include <dumux/discretization/fcdiamond.hh>
 #include <dumux/discretization/pq1bubble.hh>
 
 #include "problem.hh"
+#include "problem_newinterface.hh"
 
 namespace Dumux::Properties {
 
@@ -56,7 +66,11 @@ struct DoneaTestMomentum { using InheritsFrom = std::tuple<NAVIER_STOKES_MODEL, 
 template<class TypeTag>
 struct Problem<TypeTag, TTag::DoneaTestMomentum>
 {
+#if NEW_PROBLEM_INTERFACE
+    using type = Dumux::DoneaTestProblemNewInterface<TypeTag, Dumux::CVFENavierStokesMomentumProblem<TypeTag>>;
+#else
     using type = Dumux::DoneaTestProblem<TypeTag, Dumux::NavierStokesMomentumProblem<TypeTag>>;
+#endif
 };
 
 // the fluid system
@@ -66,6 +80,27 @@ struct FluidSystem<TypeTag, TTag::DoneaTestMomentum>
     using Scalar = GetPropType<TypeTag, Properties::Scalar>;
     using type = FluidSystems::OnePLiquid<Scalar, Components::Constant<1, Scalar> >;
 };
+
+#if NEW_VARIABLES_INTERFACE
+// the variables
+template<class TypeTag>
+struct VolumeVariables<TypeTag, TTag::DoneaTestMomentum>
+{
+private:
+    using PV = GetPropType<TypeTag, Properties::PrimaryVariables>;
+    using FSY = GetPropType<TypeTag, Properties::FluidSystem>;
+    using FST = GetPropType<TypeTag, Properties::FluidState>;
+    using MT = GetPropType<TypeTag, Properties::ModelTraits>;
+
+    static_assert(FSY::numPhases == MT::numFluidPhases(), "Number of phases mismatch between model and fluid system");
+    static_assert(FST::numPhases == MT::numFluidPhases(), "Number of phases mismatch between model and fluid state");
+    static_assert(!FSY::isMiscible(), "The Navier-Stokes model only works with immiscible fluid systems.");
+
+    using Traits = NavierStokesMomentumCVFEVolumeVariablesTraits<PV, FSY, FST, MT>;
+public:
+    using type = NavierStokesMomentumCVFEVariables<Traits>;
+};
+#endif
 
 // Set the grid type
 template<class TypeTag>
