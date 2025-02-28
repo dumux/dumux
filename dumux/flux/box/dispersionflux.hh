1766687087dde7cc89d50e106db5083a4b14c3d0
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2025-02-28 10:29:18 +0100

    [fix][disp][fluxes] Correction if mainComponentIsBalanced

--- dumux/flux/box/dispersionflux.hh
+++ dumux/flux/box/dispersionflux.hh
@@ -109,6 +109,10 @@ public:
 
         for (int compIdx = 0; compIdx < numComponents; compIdx++)
         {
+            if constexpr (!FluidSystem::isTracerFluidSystem())
+                if (compIdx == FluidSystem::getMainComponent(phaseIdx))
+                    continue;
+
             // collect the dispersion tensor, the fluxVarsCache and the shape values
             const auto& dispersionTensor = [&]()
             {
@@ -140,7 +144,7 @@ public:
             // compute the dispersion flux
             componentFlux[compIdx] = -1.0 * rhoMassOrMole * vtmv(scvf.unitOuterNormal(), dispersionTensor, gradX)*Extrusion::area(fvGeometry, scvf);
             if (BalanceEqOpts::mainComponentIsBalanced(phaseIdx) && !FluidSystem::isTracerFluidSystem())
-                componentFlux[phaseIdx] -= componentFlux[compIdx];
+                componentFlux[FluidSystem::getMainComponent(phaseIdx)] -= componentFlux[compIdx];
         }
         return componentFlux;
     }
