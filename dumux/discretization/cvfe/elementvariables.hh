aa36aed0ad904abf5dec6456ad383a3259f7ff49
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2025-03-01 20:56:41 +0100

    [disc][cvfe] New variables concept

---
+++ dumux/discretization/cvfe/elementvariables.hh
@@ -0,0 +1,261 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup CVFEDiscretization
+ * \brief The element variables class
+ */
+#ifndef DUMUX_DISCRETIZATION_CVFE_ELEMENT_VARIABLES_HH
+#define DUMUX_DISCRETIZATION_CVFE_ELEMENT_VARIABLES_HH
+
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#include <dumux/common/typetraits/localdofs.hh>
+#include <dumux/discretization/elementsolution.hh>
+
+#include <dumux/discretization/variablesdeflectionhelper.hh>
+#include <dumux/discretization/variablesupdatehelper.hh>
+
+namespace Dumux::Detail::CVFE {
+
+/*!
+ * \ingroup CVFEDiscretization
+ * \brief The (stencil) element variables class for control-volume finite element
+ * \note The class is specialized for versions with and without caching
+ * \tparam GVC the grid variables cache type
+ * \tparam cachingEnabled if the cache is enabled
+ */
+template<class GVC, bool cachingEnabled>
+class CVFEElementVariables;
+
+/*!
+ * \ingroup CVFEDiscretization
+ * \brief The (stencil) element variables class for control-volume finite element with caching
+ * \note the variables are stored for the whole grid view in the corresponding GridVariables class
+ */
+template<class GVC>
+class CVFEElementVariables<GVC, /*cachingEnabled*/true>
+{
+public:
+    //! export type of the grid variables
+    using GridVariablesCache = GVC;
+    //! TODO: Delete after new variables concept has been implemented
+    //! this is currently needed to support old interface
+    using GridVolumeVariables = GridVariablesCache;
+
+    //! export type of the variables
+    using Variables = typename GridVariablesCache::VolumeVariables;
+    //! TODO: Delete after new variables concept has been implemented
+    //! this is currently needed to support old interface
+    using VolumeVariables = Variables;
+
+    //! export type to update local variables
+    using UpdateHelper = VariablesUpdateHelper<GridVariablesCache>;
+
+    //! export type to deflect local variables
+    template<class FVElementGeometry>
+    using DeflectionHelper = VariablesDeflectionHelper<GridVariablesCache, FVElementGeometry>;
+
+    //! Constructor
+    CVFEElementVariables(const GridVariablesCache& gridVariablesCache)
+    : gridVariablesCachePtr_(&gridVariablesCache) {}
+
+    const Variables& operator [](std::size_t localDofIdx) const
+    { return gridVolVars().volVars(eIdx_, localDofIdx); }
+
+    template<class ScvOrLocalDof, typename std::enable_if_t<!std::is_integral<ScvOrLocalDof>::value, int> = 0>
+    const Variables& operator [](const ScvOrLocalDof& scvOrLocalDof) const
+    {
+        if constexpr (Detail::isLocalDofType<ScvOrLocalDof>())
+            return gridVolVars().volVars(eIdx_, scvOrLocalDof.index());
+        else
+            return gridVolVars().volVars(eIdx_, scvOrLocalDof.indexInElement());
+    }
+
+    /*!
+    * \brief bind the local view (r-value overload)
+    * This overload is called when an instance of this class is a temporary in the usage context
+    * This allows a usage like this: `const auto view = localView(...).bind(element);`
+    */
+    template<class FVElementGeometry, class SolutionVector>
+    CVFEElementVariables bind(const typename FVElementGeometry::GridGeometry::GridView::template Codim<0>::Entity& element,
+                              const FVElementGeometry& fvGeometry,
+                              const SolutionVector& sol)  &&
+    {
+        this->bindElement(element, fvGeometry, sol);
+        return std::move(*this);
+    }
+
+    // For compatibility reasons with the case of not storing the variables.
+    // function to be called before assembling an element, preparing the variables within the stencil
+    template<class FVElementGeometry, class SolutionVector>
+    void bind(const typename FVElementGeometry::GridGeometry::GridView::template Codim<0>::Entity& element,
+              const FVElementGeometry& fvGeometry,
+              const SolutionVector& sol) &
+    {
+        bindElement(element, fvGeometry, sol);
+    }
+
+    /*!
+    * \brief bind the local view (r-value overload)
+    * This overload is called when an instance of this class is a temporary in the usage context
+    * This allows a usage like this: `const auto view = localView(...).bind(element);`
+    */
+    template<class FVElementGeometry, class SolutionVector>
+    CVFEElementVariables bindElement(const typename FVElementGeometry::GridGeometry::GridView::template Codim<0>::Entity& element,
+                                          const FVElementGeometry& fvGeometry,
+                                          const SolutionVector& sol)  &&
+    {
+        this->bindElement(element, fvGeometry, sol);
+        return std::move(*this);
+    }
+
+    // function to prepare the variables within the element
+    template<class FVElementGeometry, class SolutionVector>
+    void bindElement(const typename FVElementGeometry::GridGeometry::GridView::template Codim<0>::Entity& element,
+                     const FVElementGeometry& fvGeometry,
+                     const SolutionVector& sol) &
+    {
+        eIdx_ = fvGeometry.gridGeometry().elementMapper().index(element);
+    }
+
+    //! The grid variables cache object we are a restriction of
+    //! TODO: Rename after new variables concept has been implemented
+    //! this is currently needed to support old interface
+    const GridVariablesCache& gridVolVars() const
+    { return *gridVariablesCachePtr_; }
+
+private:
+    const GridVariablesCache* gridVariablesCachePtr_;
+    std::size_t eIdx_;
+};
+
+/*!
+ * \ingroup CVFEDiscretization
+ * \brief The local (stencil) element variables class for control-volume finite element without caching
+ */
+template<class GVC>
+class CVFEElementVariables<GVC, /*cachingEnabled*/false>
+{
+public:
+    //! export type of the grid variables
+    using GridVariablesCache = GVC;
+    //! TODO: Delete after new variables concept has been implemented
+    //! this is currently needed to support old interface
+    using GridVolumeVariables = GridVariablesCache;
+
+    //! export type of the variables
+    using Variables = typename GridVariablesCache::VolumeVariables;
+    //! TODO: Delete after new variables concept has been implemented
+    //! this is currently needed to support old interface
+    using VolumeVariables = Variables;
+
+    //! export type to update local variables
+    using UpdateHelper = VariablesUpdateHelper<GridVariablesCache>;
+
+    //! export type to deflect local variables
+    template<class FVElementGeometry>
+    using DeflectionHelper = VariablesDeflectionHelper<GridVariablesCache, FVElementGeometry>;
+
+    //! Constructor
+    CVFEElementVariables(const GridVariablesCache& gridVarsCache)
+    : gridVariablesCachePtr_(&gridVarsCache) {}
+
+    /*!
+    * \brief bind the local view (r-value overload)
+    * This overload is called when an instance of this class is a temporary in the usage context
+    * This allows a usage like this: `const auto view = localView(...).bind(element);`
+    */
+    template<class FVElementGeometry, class SolutionVector>
+    CVFEElementVariables bind(const typename FVElementGeometry::GridGeometry::GridView::template Codim<0>::Entity& element,
+                              const FVElementGeometry& fvGeometry,
+                              const SolutionVector& sol)  &&
+    {
+        this->bindElement(element, fvGeometry, sol);
+        return std::move(*this);
+    }
+
+    // specialization for control-volume finite element, simply forwards to the bindElement method
+    template<class FVElementGeometry, class SolutionVector>
+    void bind(const typename FVElementGeometry::GridGeometry::GridView::template Codim<0>::Entity& element,
+              const FVElementGeometry& fvGeometry,
+              const SolutionVector& sol) &
+    {
+        bindElement(element, fvGeometry, sol);
+    }
+
+    /*!
+    * \brief bind the local view (r-value overload)
+    * This overload is called when an instance of this class is a temporary in the usage context
+    * This allows a usage like this: `const auto view = localView(...).bind(element);`
+    */
+    template<class FVElementGeometry, class SolutionVector>
+    CVFEElementVariables bindElement(const typename FVElementGeometry::GridGeometry::GridView::template Codim<0>::Entity& element,
+                                          const FVElementGeometry& fvGeometry,
+                                          const SolutionVector& sol)  &&
+    {
+        this->bindElement(element, fvGeometry, sol);
+        return std::move(*this);
+    }
+
+    // specialization for control-volume finite element
+    template<class FVElementGeometry, class SolutionVector>
+    void bindElement(const typename FVElementGeometry::GridGeometry::GridView::template Codim<0>::Entity& element,
+                     const FVElementGeometry& fvGeometry,
+                     const SolutionVector& sol) &
+    {
+        // get the solution at the dofs of the element
+        auto elemSol = elementSolution(element, sol, fvGeometry.gridGeometry());
+
+        // resize variables to the required size
+        variables_.resize(Detail::numLocalDofs(fvGeometry));
+
+        // update cv related dofs where there exists a localDof
+        for (const auto& localDof : localDofs(fvGeometry))
+            variables_[localDof.index()].update(elemSol, gridVolVars().problem(), fvGeometry, localDof);
+    }
+
+    const Variables& operator [](std::size_t localIdx) const
+    { return variables_[localIdx]; }
+
+    Variables& operator [](std::size_t localIdx)
+    { return variables_[localIdx]; }
+
+    template<class ScvOrLocalDof, typename std::enable_if_t<!std::is_integral<ScvOrLocalDof>::value, int> = 0>
+    const Variables& operator [](const ScvOrLocalDof& scvOrLocalDof) const
+    {
+        if constexpr (Detail::isLocalDofType<ScvOrLocalDof>())
+            return variables_[scvOrLocalDof.index()];
+        else
+            return variables_[scvOrLocalDof.indexInElement()];
+    }
+
+    template<class ScvOrLocalDof, typename std::enable_if_t<!std::is_integral<ScvOrLocalDof>::value, int> = 0>
+    Variables& operator [](const ScvOrLocalDof& scvOrLocalDof)
+    {
+        if constexpr (Detail::isLocalDofType<ScvOrLocalDof>())
+            return variables_[scvOrLocalDof.index()];
+        else
+            return variables_[scvOrLocalDof.indexInElement()];
+    }
+
+    //! The grid variables cache object we are a restriction of
+    //! TODO: Rename after new variables concept has been implemented
+    //! this is currently needed to support old interface
+    const GridVariablesCache& gridVolVars() const
+    { return *gridVariablesCachePtr_; }
+
+private:
+    const GridVariablesCache* gridVariablesCachePtr_;
+    std::vector<Variables> variables_;
+};
+
+} // end namespace Dumux
+
+#endif
