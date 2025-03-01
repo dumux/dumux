658abe51de6da4d2fb15bc7962a2e9ea8ea6021e
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2025-03-01 21:06:18 +0100

    [dic][variables] LocalVariables interface for VolumeVariables

---
+++ dumux/discretization/cvfe/localvariables.hh
@@ -0,0 +1,51 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightInfo: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup CVFEDiscretization
+ */
+#ifndef DUMUX_CVFE_LOCAL_VARIABLES_INTERFACE_HH
+#define DUMUX_CVFE_LOCAL_VARIABLES_INTERFACE_HH
+
+namespace Dumux {
+
+/*!
+ * \ingroup CVFEDiscretization
+ * \brief A class for providing the local dof interface for volume variables
+ */
+template <class VolumeVariables>
+class LocalVariablesInterface : public VolumeVariables
+{
+public:
+    //! export the type used for the primary variables
+    using PrimaryVariables = typename VolumeVariables::PrimaryVariables;
+
+    //! export the indices type
+    using Indices = typename VolumeVariables::Indices;
+
+    /*!
+     * \brief Update all quantities for a given control volume
+     *
+     * \param elemSol A vector containing all primary variables connected to the element
+     * \param problem The object specifying the problem which ought to
+     *                be simulated
+     * \param fvGeometry The local finite volume geometry
+     * \param localDof The local dof
+     */
+    template<class ElementSolution, class Problem, class FVElementGeometry, class LocalDof>
+    void update(const ElementSolution& elemSol,
+                const Problem& problem,
+                const FVElementGeometry& fvGeometry,
+                const LocalDof& localDof)
+    {
+        VolumeVariables::update(elemSol, problem, fvGeometry.element(), fvGeometry.scv(localDof.index()));
+    };
+};
+
+} // end namespace Dumux
+
+#endif
