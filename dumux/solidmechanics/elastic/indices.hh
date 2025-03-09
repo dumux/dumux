0e6e7af957edd30689f907a227eeb6f281e8c900
Maziar Veyskarami <maziar.veyskarami@iws.uni-stuttgart.de>
2025-03-09 09:20:26 +0100

    [geomechanics] move hyperelastic and elastic to solidmechanics

---
+++ dumux/solidmechanics/elastic/indices.hh
@@ -0,0 +1,42 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightText: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup Elastic
+ * \brief Defines the indices for the elastic model
+ */
+#ifndef DUMUX_SOLIDMECHANICS_ELASTIC_INDICES_HH
+#define DUMUX_SOLIDMECHANICS_ELASTIC_INDICES_HH
+
+namespace Dumux {
+
+/*!
+ * \ingroup Elastic
+ * \brief The indices for the linear elasticity model.
+ */
+struct ElasticIndices
+{
+    // returns the equation index for a given space direction
+    static constexpr int momentum(int dirIdx) { return dirIdx; };
+
+    // returns the primary variable index for a given space direction
+    static constexpr int u(int dirIdx) { return dirIdx; };
+
+    // Equation indices
+    static constexpr int momentumXEqIdx = 0;
+    static constexpr int momentumYEqIdx = 1;
+    static constexpr int momentumZEqIdx = 2;
+
+    // primary variable indices
+    static constexpr int uxIdx = 0;
+    static constexpr int uyIdx = 1;
+    static constexpr int uzIdx = 2;
+};
+
+} // end namespace Dumux
+
+#endif
---
+++ dumux/solidmechanics/lameparams.hh
@@ -0,0 +1,53 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightText: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup GeomechanicsModels
+ * \brief \copydoc Dumux::LameParams
+ */
+#ifndef DUMUX_SOLIDMECHANICS_LAME_PARAMS_HH
+#define DUMUX_SOLIDMECHANICS_LAME_PARAMS_HH
+
+namespace Dumux {
+
+/*!
+ * \ingroup GeomechanicsModels
+ * \brief Structure encapsulating the lame parameters
+ */
+template<class Scalar>
+struct LameParams
+{
+    //! Default constructor
+    LameParams() = default;
+
+    //! Constructor taking lambda and mu directly
+    LameParams(Scalar lambda, Scalar mu)
+    : lambda_(lambda) , mu_(mu)
+    {}
+
+    //! Return the first lame parameter
+    Scalar lambda() const
+    { return lambda_; }
+
+    //! Return the second lame parameter
+    Scalar mu() const
+    { return mu_; }
+
+    //! set the first lame parameter
+    void setLambda(Scalar lambda)
+    { lambda_ = lambda; }
+
+    //! set the second lame parameter
+    void setMu(Scalar mu)
+    { mu_ = mu; }
+
+private:
+    Scalar lambda_;
+    Scalar mu_;
+};
+} // end namespace Dumux
+#endif
