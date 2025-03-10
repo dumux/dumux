c087770d61f9e51e288bf3e9ab762ab73e841f8a
Timo Koch <timok@simula.no>
2025-03-10 19:19:23 +0000

    fixup

--- python/dumux/assembly/__init__.py
+++ python/dumux/assembly/__init__.py
@@ -50,6 +50,11 @@ def _createFVAssembler(*, problem, gridVariables, model, diffMethod="numeric", i
         moduleName,
         holder="std::shared_ptr",
         preamble=model.cppHeader,
+        # make sure the assembler is compiled with the Serial backend
+        # as currently the assembly in combination with Python is not thread-safe
+        extraCMake=[
+             "target_compile_definitions(TARGET PUBLIC DUMUX_MULTITHREADING_BACKEND=Serial)"
+        ],
     )
     return module.FVAssembler(problem, problem.gridGeometry(), gridVariables)
 
