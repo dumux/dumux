0467d6c89d170eabb82c6187d1dac4968220fad6
leonidas <leon.keim@googlemail.com>
2025-03-10 20:47:26 +0100

    split binarycoeff test in diffcoeff and henrycoeff. henrycoeffs can also take a implementation tag.

--- test/material/binarycoefficients/test_binarycoefficients.cc
+++ test/material/binarycoefficients/test_binarycoefficients.cc
@@ -32,7 +32,7 @@
 #include <dumux/material/components/defaultco2table.hh>
 
 template<class Scalar, class BinaryCoefficients>
-int checkBinaryCoefficients()
+int checkDiffusionCoefficients()
 {
     // Define a reference temperature and pressure for evaluation:
     const Scalar t = 293.15;
@@ -74,6 +74,45 @@ int checkBinaryCoefficients()
         return 1;
 }
 
+struct voidTag{};
+
+template<class Scalar, class BinaryCoefficients, class Implementation=voidTag>
+int checkHenryCoefficients(Implementation impl = Implementation{})
+{
+    // Define a reference temperature and pressure for evaluation:
+    const Scalar t = 293.15;
+    const Scalar p = 1.013e5;
+
+    const std::string className = Dune::className<BinaryCoefficients>();
+    std::string collectedErrors;
+
+    try
+    {
+        if constexpr (std::is_same_v<Implementation, voidTag>) {
+            [[maybe_unused]] Scalar val = BinaryCoefficients::henry(t);
+            std::cout << className << ": t=" << t << ", p=" << p << ", henry: " << val << std::endl;
+        } else {
+            [[maybe_unused]] Scalar val = BinaryCoefficients::henry(t, impl);
+            std::cout << className << ": t=" << t << ", p=" << p << ", henry with specific implementation: " << val << std::endl;
+        }
+    } catch (Dune::NotImplemented&)
+    {
+        std::cout << "warning: " << className << "::henry() is not implemented." << std::endl;
+    } catch (const std::exception& e)
+    {
+        collectedErrors += "error: " + className + "::henry() throws exception: "
+                           + std::string(e.what()) + "\n";
+    }
+
+    std::cout << collectedErrors;
+    if (collectedErrors.empty()) // success
+        return 0;
+    else
+        return 1;
+}
+
+
+
 int main()
 {
     using namespace Dumux;
@@ -82,20 +121,34 @@ int main()
 
     int success = 0;
 
-   success += checkBinaryCoefficients<Scalar, Air_Mesitylene>();
-   success += checkBinaryCoefficients<Scalar, Air_Xylene>();
-   success += checkBinaryCoefficients<Scalar, Brine_CO2<Scalar, Components::CO2<Scalar, GeneratedCO2Tables::CO2Tables>>>();
-   success += checkBinaryCoefficients<Scalar, H2O_Air>();
-   success += checkBinaryCoefficients<Scalar, H2O_CH4>();
-   success += checkBinaryCoefficients<Scalar, H2O_Component<Scalar, Components::Constant<0, Scalar>>>();
-   success += checkBinaryCoefficients<Scalar, H2O_HeavyOil>();
-   success += checkBinaryCoefficients<Scalar, H2O_Mesitylene>();
-   success += checkBinaryCoefficients<Scalar, H2O_N2>();
-   success += checkBinaryCoefficients<Scalar, H2O_O2>();
-   success += checkBinaryCoefficients<Scalar, H2O_CO2>();
-   success += checkBinaryCoefficients<Scalar, H2O_AR>();
-   success += checkBinaryCoefficients<Scalar, H2O_Xylene>();
-   success += checkBinaryCoefficients<Scalar, N2_O2>();
+   success += checkDiffusionCoefficients<Scalar, Air_Mesitylene>();
+   success += checkDiffusionCoefficients<Scalar, Air_Xylene>();
+   success += checkDiffusionCoefficients<Scalar, Brine_CO2<Scalar, Components::CO2<Scalar, GeneratedCO2Tables::CO2Tables>>>();
+   success += checkDiffusionCoefficients<Scalar, H2O_Air>();
+   success += checkDiffusionCoefficients<Scalar, H2O_CH4>();
+   success += checkDiffusionCoefficients<Scalar, H2O_Component<Scalar, Components::Constant<0, Scalar>>>();
+   success += checkDiffusionCoefficients<Scalar, H2O_HeavyOil>();
+   success += checkDiffusionCoefficients<Scalar, H2O_Mesitylene>();
+   success += checkDiffusionCoefficients<Scalar, H2O_N2>();
+   success += checkDiffusionCoefficients<Scalar, H2O_O2>();
+   success += checkDiffusionCoefficients<Scalar, H2O_CO2>();
+   success += checkDiffusionCoefficients<Scalar, H2O_AR>();
+   success += checkDiffusionCoefficients<Scalar, H2O_Xylene>();
+   success += checkDiffusionCoefficients<Scalar, N2_O2>();
+
+    success += checkHenryCoefficients<Scalar, Air_Mesitylene>();
+    success += checkHenryCoefficients<Scalar, Air_Xylene>();
+    success += checkHenryCoefficients<Scalar, H2O_Air>();
+    success += checkHenryCoefficients<Scalar, H2O_Air>(H2O_Air::HenryLaws::FinsterleLaw{});
+    success += checkHenryCoefficients<Scalar, H2O_Air>(H2O_Air::HenryLaws::IdealMixtureLaw{});
+    success += checkHenryCoefficients<Scalar, H2O_CH4>();
+    success += checkHenryCoefficients<Scalar, H2O_Mesitylene>();
+    success += checkHenryCoefficients<Scalar, H2O_N2>();
+    success += checkHenryCoefficients<Scalar, H2O_O2>();
+    success += checkHenryCoefficients<Scalar, H2O_CO2>();
+    success += checkHenryCoefficients<Scalar, H2O_AR>();
+    success += checkHenryCoefficients<Scalar, H2O_Xylene>();
+    success += checkHenryCoefficients<Scalar, N2_O2>();
 
     return success;
 }
