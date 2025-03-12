efe8c5bcf2611763d866c5157f3e8534acf4647d
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2025-03-12 18:57:03 +0100

    [disc][pq1bubble][hybrid] Scvs no longer necessary for hybrid scheme

--- dumux/discretization/pq1bubble/fvgridgeometry.hh
+++ dumux/discretization/pq1bubble/fvgridgeometry.hh
@@ -130,8 +130,6 @@ class PQ1BubbleFVGridGeometry
 
     static_assert(dim > 1, "Only implemented for dim > 1");
 
-    static constexpr bool enableHybridCVFE = Detail::enablesHybridCVFE<Traits>;
-
 public:
     //! export the discretization method this geometry belongs to
     using DiscretizationMethod = DiscretizationMethods::PQ1Bubble;
@@ -308,10 +306,9 @@ private:
             // instantiate the geometry helper
             GeometryHelper geometryHelper(elementGeometry);
 
-            // ToDo: Currently we still create scvs also for Fe dofs
-            numScv_ += GeometryHelper::numElementDofs(elementGeometry.type());
+            numScv_ += geometryHelper.numScv();
             // construct the sub control volumes
-            cache_.scvs_[eIdx].resize(GeometryHelper::numElementDofs(elementGeometry.type()));
+            cache_.scvs_[eIdx].resize(geometryHelper.numScv());
 
             // Scvs related to control volumes
             for (LocalIndexType scvLocalIdx = 0; scvLocalIdx < geometryHelper.numScv(); ++scvLocalIdx)
@@ -328,28 +325,12 @@ private:
                 );
             }
 
-            // Hybrid scvs, i.e. Fem dofs
-            if constexpr (enableHybridCVFE)
-            {
-                // We add the additional Fe dof
-                // ToDo: get rid of building scvs for Fe dofs
-                LocalIndexType scvLocalIdx = geometryHelper.numScv();
-                cache_.scvs_[eIdx][scvLocalIdx] = SubControlVolume(
-                    elementGeometry.volume(),
-                    elementGeometry.center(),
-                    elementGeometry.center(),
-                    scvLocalIdx,
-                    eIdx,
-                    GeometryHelper::dofIndex(this->dofMapper(), element, scvLocalIdx),
-                    true
-                );
-            }
-
             // construct the sub control volume faces
-            numScvf_ += GeometryHelper::numInteriorScvf(elementGeometry.type());
-            cache_.scvfs_[eIdx].resize(GeometryHelper::numInteriorScvf(elementGeometry.type()));
+            const auto numInteriorScvfs = GeometryHelper::numInteriorScvf(elementGeometry.type());
+            numScvf_ += numInteriorScvfs;
+            cache_.scvfs_[eIdx].resize(numInteriorScvfs);
             LocalIndexType scvfLocalIdx = 0;
-            for (; scvfLocalIdx < GeometryHelper::numInteriorScvf(elementGeometry.type()); ++scvfLocalIdx)
+            for (; scvfLocalIdx < numInteriorScvfs; ++scvfLocalIdx)
             {
                 const auto scvPair = geometryHelper.getScvPairForScvf(scvfLocalIdx);
                 const auto corners = geometryHelper.getScvfCorners(scvfLocalIdx);
