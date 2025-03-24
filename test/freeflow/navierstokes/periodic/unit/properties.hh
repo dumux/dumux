5466085dbe4176803c6fc885700514afa03c4081
Timo Koch <timok@simula.no>
2025-03-24 20:45:02 +0000

    Merge branch 'feature/test_periodic_subgrid' into 'master'
    [test] Test periodic subgrids
    
    See merge request dumux-repositories/dumux!3166
    
    (cherry picked from commit 9d833072bc74e7ff0abc8902b9f3a83f836bbc69)
    
    d2395b3f [test][subgrid][periodic] Add to the subgrid unit test for periodic hostgrids
    86e956e9 [test][freeflow] Split periodic test to multiple folders
    409bbea2 [test][freeflow][subgrid] Add a flow test for subgrid periodicity
    1fdc1385 [test][ff][periodic] Rename subfolders
    2309bef9 [test][gridmanager] Adapt SubGrid<SPGrid> periodicity to dumux wrapper
    a062a27c [test][ff][periodic] Fix one pressure dof
    
    Co-authored-by: Timo Koch <timok@simula.no>

---
+++ test/freeflow/navierstokes/periodic/unit/properties.hh
@@ -0,0 +1,96 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightText: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup NavierStokesTests
+ * \brief Stationary test for the staggered grid Navier-Stokes model with periodic BC
+ */
+
+#ifndef DUMUX_TEST_FREEFLOW_NAVIERSTOKES_PERIODIC_PROPERTIES_HH
+#define DUMUX_TEST_FREEFLOW_NAVIERSTOKES_PERIODIC_PROPERTIES_HH
+
+
+#include <dune/grid/spgrid.hh>
+#if HAVE_DUNE_SUBGRID
+#include <dune/subgrid/subgrid.hh>
+#endif
+
+#include <dumux/material/fluidsystems/1pliquid.hh>
+#include <dumux/material/components/constant.hh>
+
+#include <dumux/freeflow/navierstokes/momentum/model.hh>
+#include <dumux/freeflow/navierstokes/mass/1p/model.hh>
+#include <dumux/freeflow/navierstokes/momentum/problem.hh>
+#include <dumux/freeflow/navierstokes/mass/problem.hh>
+
+#include <dumux/multidomain/freeflow/couplingmanager.hh>
+#include <dumux/multidomain/traits.hh>
+
+#include <dumux/discretization/fcstaggered.hh>
+#include <dumux/discretization/cctpfa.hh>
+
+#include "problem.hh"
+
+#ifndef USESUBGRID
+#define USESUBGRID 0
+#endif
+
+namespace Dumux::Properties {
+
+// Create new type tags
+namespace TTag {
+struct PeriodicTest {};
+struct PeriodicTestMomentum { using InheritsFrom = std::tuple<PeriodicTest, NavierStokesMomentum, FaceCenteredStaggeredModel>; };
+struct PeriodicTestMass { using InheritsFrom = std::tuple<PeriodicTest, NavierStokesMassOneP, CCTpfaModel>; };
+} // end namespace TTag
+
+
+// the fluid system
+template<class TypeTag>
+struct FluidSystem<TypeTag, TTag::PeriodicTest>
+{
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using type = FluidSystems::OnePLiquid<Scalar, Components::Constant<1, Scalar> >;
+};
+
+// Set the grid type
+template<class TypeTag>
+struct Grid<TypeTag, TTag::PeriodicTest> {
+    using HostGrid = Dune::SPGrid<double, 2>;
+#if HAVE_DUNE_SUBGRID && USESUBGRID
+    using type = Dune::SubGrid<2, HostGrid>;
+#else
+    using type = HostGrid;
+#endif
+};
+
+// Set the problem property
+template<class TypeTag>
+struct Problem<TypeTag, TTag::PeriodicTestMomentum>
+{ using type = PeriodicTestProblem<TypeTag, Dumux::NavierStokesMomentumProblem<TypeTag>>; };
+
+template<class TypeTag>
+struct Problem<TypeTag, TTag::PeriodicTestMass>
+{ using type = PeriodicTestProblem<TypeTag, Dumux::NavierStokesMassProblem<TypeTag>>; };
+
+template<class TypeTag>
+struct EnableGridGeometryCache<TypeTag, TTag::PeriodicTest> { static constexpr bool value = true; };
+template<class TypeTag>
+struct EnableGridFluxVariablesCache<TypeTag, TTag::PeriodicTest> { static constexpr bool value = true; };
+template<class TypeTag>
+struct EnableGridVolumeVariablesCache<TypeTag, TTag::PeriodicTest> { static constexpr bool value = true; };
+
+template<class TypeTag>
+struct CouplingManager<TypeTag, TTag::PeriodicTest>
+{
+    using Traits = MultiDomainTraits<TTag::PeriodicTestMomentum, TTag::PeriodicTestMass>;
+    using type = FreeFlowCouplingManager<Traits>;
+};
+
+} // end namespace Dumux::Properties
+
+#endif
