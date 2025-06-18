b4882c8a3f75fe08ee2b5f0e365ad4e72491b5ac
Mathis Kelm <mathis.kelm@iws.uni-stuttgart.de>
2025-06-18 19:22:39 +0200

    [deprecated] Add deprecation helpers for periodic dof ranges

--- dumux/common/deprecated.hh
+++ dumux/common/deprecated.hh
@@ -14,6 +14,7 @@
 #define DUMUX_COMMON_DEPRECATED_HH
 
 #include <utility>
+#include <ranges>
 
 #include <dune/common/ftraits.hh>
 #include <dune/common/exceptions.hh>
@@ -40,6 +41,45 @@ namespace Deprecated {
 #pragma clang diagnostic pop
 #endif  // __clang__
 
+// Helper concept detecting new interface.
+// Remove after release 3.11
+template<class GG, class GI>
+concept hasRangeOfPeriodicallyMappedDofs = requires (GG gg, GI gi) {
+    { gg.periodicallyMappedDofs(gi) } -> std::ranges::range;
+};
+
+// helper function triggering deprecation warning if grid geometry does not implement new interface or program uses old interface.
+// Remove after release 3.11
+template<class Dof>
+[[deprecated("The periodicDofMap with values of single periodic dofs will not be supported after release 3.11. Use/define in custom grid geometry a periodicDofMap with a range of dofs as value type, as well as accessor periodicallyMappedDofs returing a std::views::all/single of the storage if possible")]]
+inline const std::ranges::range auto wrapSinglePeriodicDof(const Dof& dof)
+{
+    return std::ranges::views::single(dof);
+}
+
+// Helper function to access values from map of periodic dofs.
+// Remove after release 3.11
+template<typename T>
+inline const std::ranges::range auto ensureRangeOfPeriodicDofs(const T& t);
+
+template<std::ranges::range T>
+inline const std::ranges::range auto ensureRangeOfPeriodicDofs(const T& range)
+{ return range; }
+
+template<typename T> requires (!std::ranges::range<T>)
+inline const std::ranges::range auto ensureRangeOfPeriodicDofs(const T& entry)
+{ return wrapSinglePeriodicDof(entry); }
+
+// Helper functions to access ranges of periodically mapped dofs through common interface.
+// Remove after release 3.11
+template<class GG, class GI>
+inline const std::ranges::range auto rangeOfPeriodicallyMappedDofs(const GG& gg, const GI& gi)
+{
+    if constexpr (hasRangeOfPeriodicallyMappedDofs<GG, GI>)
+        return ensureRangeOfPeriodicDofs(gg.periodicallyMappedDofs(gi));
+    else
+        return wrapSinglePeriodicDof(gg.periodicallyMappedDof(gi));
+}
 
 } // end namespace Deprecated
 #endif
