77fef2131e56a0a3a6fe9729c9d72f996f580fa4
Anna Mareike Kostelecky <anmako96@web.de>
2025-08-23 20:23:59 +0200

    [md][boundary][ffpnm] 3D test case
    ... no-flow at the moment as flow in ff domain currently leads to non-convergence

---
+++ test/multidomain/boundary/freeflowporenetwork/1p_1p/3D/problem_freeflow.hh
@@ -0,0 +1,297 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightText: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup BoundaryTests
+ * \brief Free-flow sub-problem for the coupled 1p_1p free-flow/pore-network-model test
+ */
+
+#ifndef DUMUX_TEST_MULTIDOMAIN_BOUNDARY_FREEFLOW_PORE_NETWORK_PROBLEM_FREEFLOW_HH
+#define DUMUX_TEST_MULTIDOMAIN_BOUNDARY_FREEFLOW_PORE_NETWORK_PROBLEM_FREEFLOW_HH
+
+#include <dumux/common/properties.hh>
+
+#include <dumux/freeflow/navierstokes/boundarytypes.hh>
+#include <dumux/freeflow/navierstokes/momentum/fluxhelper.hh>
+#include <dumux/freeflow/navierstokes/scalarfluxhelper.hh>
+#include <dumux/freeflow/navierstokes/mass/1p/advectiveflux.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup BoundaryTests
+ * \brief  Free-flow sub-problem for the coupled 1p_1p free-flow/pore-network-model test
+ *         A two-dimensional Stokes flow region coupled to a pore-network model.
+ */
+template <class TypeTag, class BaseProblem>
+class FreeFlowOnePTestProblem :  public BaseProblem
+{
+    using ParentType = BaseProblem;
+
+    using BoundaryTypes = typename ParentType::BoundaryTypes;
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    using FVElementGeometry = typename GridGeometry::LocalView;
+    using SubControlVolume = typename FVElementGeometry::SubControlVolume;
+    using SubControlVolumeFace = typename FVElementGeometry::SubControlVolumeFace;
+    using Indices = typename GetPropType<TypeTag, Properties::ModelTraits>::Indices;
+    using ModelTraits = GetPropType<TypeTag, Properties::ModelTraits>;
+    using InitialValues = typename ParentType::InitialValues;
+    using Sources = typename ParentType::Sources;
+    using DirichletValues = typename ParentType::DirichletValues;
+    using BoundaryFluxes = typename ParentType::BoundaryFluxes;
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using SolutionVector = GetPropType<TypeTag, Properties::SolutionVector>;
+
+    static constexpr auto dimWorld = GridGeometry::GridView::dimensionworld;
+    using Element = typename GridGeometry::GridView::template Codim<0>::Entity;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+    using VelocityVector = Dune::FieldVector<Scalar, dimWorld>;
+
+    using CouplingManager = GetPropType<TypeTag, Properties::CouplingManager>;
+
+public:
+    FreeFlowOnePTestProblem(std::shared_ptr<const GridGeometry> gridGeometry, std::shared_ptr<CouplingManager> couplingManager)
+    : ParentType(gridGeometry, couplingManager, "FreeFlow")
+    , couplingManager_(couplingManager)
+    {
+        problemName_ = getParam<std::string>("Vtk.OutputName") + "_" + getParamFromGroup<std::string>(this->paramGroup(), "Problem.Name");
+        initialPressure_ = getParamFromGroup<Scalar>(this->paramGroup(), "Problem.InitialPressure", 1e5);
+        inletPressure_ = getParamFromGroup<Scalar>(this->paramGroup(), "Problem.InletPressure", 1.01e5);
+        outletPressure_ = getParamFromGroup<Scalar>(this->paramGroup(), "Problem.OutletPressure", 1e5);
+    }
+
+    /*!
+     * \name Problem parameters
+     */
+    // \{
+
+    const std::string& name() const
+    { return problemName_; }
+
+    // \}
+
+    /*!
+     * \name Boundary conditions
+     */
+    // \{
+
+    /*!
+     * \brief Specifies which kind of boundary condition should be
+     *        used for which equation on a given boundary segment.
+     *
+     * \param element The finite element
+     * \param scvf The sub control volume face
+     */
+    BoundaryTypes boundaryTypes(const Element& element,
+                                const SubControlVolumeFace& scvf) const
+    {
+        BoundaryTypes values;
+        // const auto& globalPos = scvf.center(); //avoid ambiguities at corners
+
+        if constexpr (ParentType::isMomentumProblem())
+        {
+            if (couplingManager_->isCoupled(CouplingManager::freeFlowMomentumIndex, CouplingManager::poreNetworkIndex, scvf))
+            {
+                values.setCouplingNeumann(Indices::momentumXBalanceIdx);
+                values.setCouplingNeumann(Indices::momentumYBalanceIdx);
+            }
+            // else if (onInlet_(globalPos) || onOutlet_(globalPos))
+            //     values.setAllNeumann();
+            else
+                values.setAllDirichlet(); //e.g. fixed velocities at walls
+        }
+        else
+        {
+            if (couplingManager_->isCoupled(CouplingManager::freeFlowMassIndex, CouplingManager::poreNetworkIndex, scvf))
+                values.setAllCouplingNeumann(); //mass and energy coupling
+            // else if (onInlet_(globalPos))
+            //     values.setAllDirichlet();
+            // else if (onOutlet_(globalPos))
+            //     values.setAllNeumann();
+            else
+                values.setAllNeumann(); //outflow or zero flux BCs for p,T
+        }
+        return values;
+    }
+
+    /*!
+     * \brief Returns Dirichlet boundary values at a given position.
+     *
+     * \param globalPos The global position
+     */
+    DirichletValues dirichletAtPos(const GlobalPosition& globalPos) const
+    {
+        DirichletValues values(0.0); //velocity is 0.0
+
+        // if constexpr (!ParentType::isMomentumProblem())
+        // {
+        //     if (onInlet_(globalPos))
+        //     {
+        //         values[Indices::pressureIdx] = inletPressure_;
+        //     }
+        //     else if (onOutlet_(globalPos))
+        //     {
+        //         values[Indices::pressureIdx] = outletPressure_;
+        //     }
+        // }
+        return values;
+    }
+
+    /*!
+     * \brief Evaluates the boundary conditions for a Neumann
+     *        boundary segment/ control volume.
+     *
+     * This is the method for the case where the Neumann condition is
+     * potentially solution dependent
+     *
+     * \param element The finite element
+     * \param fvGeometry The finite-volume geometry
+     * \param elemVolVars All volume variables for the element
+     * \param elemFluxVarsCache Flux variables caches for all faces in stencil
+     * \param scvf The sub control volume face
+     *
+     * Negative values mean influx.
+     * E.g. for the mass balance that would be the mass flux in \f$ [ kg / (m^2 \cdot s)] \f$.
+     */
+    template<class ElementVolumeVariables, class ElementFluxVariablesCache>
+    BoundaryFluxes neumann(const Element& element,
+                           const FVElementGeometry& fvGeometry,
+                           const ElementVolumeVariables& elemVolVars,
+                           const ElementFluxVariablesCache& elemFluxVarsCache,
+                           const SubControlVolumeFace& scvf) const
+    {
+        BoundaryFluxes values(0.0);
+        // const auto& globalPos = scvf.ipGlobal();
+        using SlipVelocityPolicy = NavierStokesSlipVelocity<typename GridGeometry::DiscretizationMethod,  NavierStokes::SlipConditions::BJ>;
+        using FluxHelper = NavierStokesMomentumBoundaryFlux<typename GridGeometry::DiscretizationMethod, SlipVelocityPolicy>;
+
+        if constexpr (ParentType::isMomentumProblem())
+        {
+
+            if (couplingManager_->isCoupled(CouplingManager::freeFlowMomentumIndex, CouplingManager::poreNetworkIndex, scvf))
+            {
+                values[scvf.normalAxis()] += couplingManager_->momentumCouplingCondition(
+                    CouplingManager::freeFlowMomentumIndex, CouplingManager::poreNetworkIndex,
+                    fvGeometry, scvf, elemVolVars
+                );
+
+                values += FluxHelper::slipVelocityMomentumFlux(
+                    *this, fvGeometry, scvf, elemVolVars, elemFluxVarsCache
+                );
+            }
+            // else if (onInlet_(globalPos))
+            // {
+            //     values = FluxHelper::fixedPressureMomentumFlux(
+            //         *this, fvGeometry, scvf, elemVolVars,
+            //         elemFluxVarsCache, inletPressure_, true /*zeroNormalVelocityGradient*/
+            //     );
+            // }
+            // else if (onOutlet_(globalPos))
+            // {
+            //     values = FluxHelper::fixedPressureMomentumFlux(
+            //         *this, fvGeometry, scvf, elemVolVars,
+            //         elemFluxVarsCache, outletPressure_, true /*zeroNormalVelocityGradient*/
+            //     );
+            // }
+        }
+        else
+        {
+            if (couplingManager_->isCoupled(CouplingManager::freeFlowMassIndex, CouplingManager::poreNetworkIndex, scvf))
+            {
+                values[Indices::conti0EqIdx] = couplingManager_->massCouplingCondition(
+                    CouplingManager::freeFlowMassIndex, CouplingManager::poreNetworkIndex,
+                    fvGeometry, scvf, elemVolVars);
+            }
+            // else if (onOutlet_(globalPos))
+            // {
+            //     using FluxHelper = NavierStokesScalarBoundaryFluxHelper<AdvectiveFlux<ModelTraits>>;
+            //     // values = FluxHelper::scalarOutflowFlux(
+            //     //     *this, element, fvGeometry, scvf, elemVolVars, std::move(outletPressure_)
+            //     // );
+            //     values = FluxHelper::scalarOutflowFlux(
+            //         *this, element, fvGeometry, scvf, elemVolVars
+            //     );
+            // }
+        }
+
+        return values;
+    }
+
+    /*!
+     * \brief Evaluates the source term for all phases within a given
+     *        sub-control volume
+     */
+    template<class ElementVolumeVariables>
+    Sources source(const Element& element,
+                       const FVElementGeometry& fvGeometry,
+                       const ElementVolumeVariables& elemVolVars,
+                       const SubControlVolume& scv) const
+    {
+        auto source = Sources(0.0);
+        return source;
+    }
+
+    /*!
+     * \brief Returns true if the scvf lies on a porous slip boundary
+     */
+    bool onSlipBoundary(const FVElementGeometry& fvGeometry, const SubControlVolumeFace& scvf) const
+    {
+        assert(scvf.isFrontal());
+        return scvf.boundary() && couplingManager_->isCoupled(CouplingManager::freeFlowMomentumIndex, CouplingManager::poreNetworkIndex, scvf);
+    }
+
+    /*!
+     * \brief Returns the beta value
+     */
+    Scalar betaBJ(const FVElementGeometry& fvGeometry, const SubControlVolumeFace& scvf, const GlobalPosition& tangentialVector) const
+    {
+        const Scalar radius = couplingManager_->coupledPoreInscribedRadius(fvGeometry, scvf);
+        return 5.73 / radius; // this value is only an approximation of wall friction is considered
+    }
+
+    /*!
+     * \brief Returns the velocity in the porous medium (which is 0 by default according to Saffmann).
+     */
+    VelocityVector porousMediumVelocity(const FVElementGeometry& fvGeometry, const SubControlVolumeFace& scvf) const
+    {
+        return couplingManager_->interfaceThroatVelocity(fvGeometry, scvf);
+    }
+
+    // \}
+
+private:
+
+    bool onInlet_(const GlobalPosition &globalPos) const
+    {
+        return onLeftBoundary_(globalPos);
+    }
+
+    bool onOutlet_(const GlobalPosition &globalPos) const
+    {
+        return onRightBoundary_(globalPos);
+    }
+
+    bool onLeftBoundary_(const GlobalPosition &globalPos) const
+    { return globalPos[0] < this->gridGeometry().bBoxMin()[0] + eps_; }
+
+    bool onRightBoundary_(const GlobalPosition &globalPos) const
+    { return globalPos[0] > this->gridGeometry().bBoxMax()[0] - eps_; }
+
+    bool onUpperBoundary_(const GlobalPosition &globalPos) const
+    { return globalPos[1] > this->gridGeometry().bBoxMax()[1] - eps_; }
+
+    std::string problemName_;
+    static constexpr Scalar eps_ = 1e-6;
+    Scalar initialPressure_;
+    Scalar inletPressure_;
+    Scalar outletPressure_;
+
+    std::shared_ptr<CouplingManager> couplingManager_;
+};
+} // end namespace Dumux
+
+#endif
---
+++ test/multidomain/boundary/freeflowporenetwork/1p_1p/3D/problem_pnm.hh
@@ -0,0 +1,208 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightText: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup BoundaryTests
+ * \brief Pore-network sub-problem for the coupled 1p_1p free-flow/pore-network-model test
+ */
+
+#ifndef DUMUX_TEST_MULTIDOMAIN_BOUNDARY_FREEFLOW_PORE_NETWORK_PROBLEM_PNM_HH
+#define DUMUX_TEST_MULTIDOMAIN_BOUNDARY_FREEFLOW_PORE_NETWORK_PROBLEM_PNM_HH
+
+#include <dumux/common/boundarytypes.hh>
+#include <dumux/common/numeqvector.hh>
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/porousmediumflow/problem.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup BoundaryTests
+ * \brief  Pore-network sub-problem for the coupled 1p_1p free-flow/pore-network-model test
+ *         A two-dimensional pore-network region coupled to a free-flow model.
+ */
+template <class TypeTag>
+class PNMOnePProblem : public PorousMediumFlowProblem<TypeTag>
+{
+    using ParentType = PorousMediumFlowProblem<TypeTag>;
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using PrimaryVariables = GetPropType<TypeTag, Properties::PrimaryVariables>;
+    using BoundaryTypes = Dumux::BoundaryTypes<GetPropType<TypeTag, Properties::ModelTraits>::numEq()>;
+    using FVElementGeometry = typename GetPropType<TypeTag, Properties::GridGeometry>::LocalView;
+    using SubControlVolume = typename FVElementGeometry::SubControlVolume;
+    using GridGeometry = GetPropType<TypeTag, Properties::GridGeometry>;
+    using GridView = typename GridGeometry::GridView;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using Indices = typename GetPropType<TypeTag, Properties::ModelTraits>::Indices;
+
+    using CouplingManager = GetPropType<TypeTag, Properties::CouplingManager>;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+
+public:
+    template<class SpatialParams>
+    PNMOnePProblem(std::shared_ptr<const GridGeometry> gridGeometry,
+                   std::shared_ptr<SpatialParams> spatialParams,
+                   std::shared_ptr<CouplingManager> couplingManager)
+    : ParentType(gridGeometry, spatialParams, "PNM"), couplingManager_(couplingManager)
+    {
+        initialPressure_ = getParamFromGroup<Scalar>(this->paramGroup(), "Problem.InitialPressure", 1e5);
+        inletPressure_ = getParamFromGroup<Scalar>(this->paramGroup(), "Problem.InletPressure", 1.01e5);
+        outletPressure_ = getParamFromGroup<Scalar>(this->paramGroup(), "Problem.OutletPressure", 1e5);
+    }
+
+    /*!
+     * \name Simulation steering
+     */
+    // \{
+
+    /*!
+     * \name Problem parameters
+     */
+    // \{
+
+    const std::string& name() const
+    {
+        static const std::string problemName = getParam<std::string>("Vtk.OutputName") + "_" + getParamFromGroup<std::string>(this->paramGroup(), "Problem.Name");
+        return problemName;
+    }
+
+    // \}
+
+    /*!
+     * \name Boundary conditions
+     */
+    // \{
+
+    /*!
+     * \brief Specifies which kind of boundary condition should be used for
+     *        which equation for a finite volume on the boundary.
+     */
+    BoundaryTypes boundaryTypes(const Element& element, const SubControlVolume& scv) const
+    {
+        BoundaryTypes bcTypes;
+        if (couplingManager().isCoupled(CouplingManager::poreNetworkIndex, CouplingManager::freeFlowMassIndex, scv))
+            bcTypes.setAllCouplingNeumann();
+        else
+        {
+            // if(onInlet_(scv) && onOutlet_(scv))
+            //     bcTypes.setAllDirichlet(); //pressure (and Temperature) fixed for inflow from bottom
+            // else
+            //     bcTypes.setAllNeumann();
+            bcTypes.setAllNeumann();
+        }
+        return bcTypes;
+    }
+
+    /*!
+     * \brief Evaluate the boundary conditions for a dirichlet
+     *        control volume.
+     */
+    PrimaryVariables dirichlet(const Element& element,
+                               const SubControlVolume& scv) const
+    {
+        PrimaryVariables priVars (0.0);
+        // if(onInlet_(scv))
+        // {
+        //     priVars[Indices::pressureIdx] = inletPressure_;
+        // }
+        // else if(onOutlet_(scv))
+        // {
+        //     priVars[Indices::pressureIdx] = outletPressure_;
+        // }
+        return priVars;
+    }
+
+    // \}
+
+    /*!
+     * \name Volume terms
+     */
+    // \{
+
+    /*!
+     * \brief Evaluate the source term for all phases within a given
+     *        sub-control-volume.
+     *
+     * This is the method for the case where the source term is
+     * potentially solution dependent and requires some quantities that
+     * are specific to the fully-implicit method.
+     *
+     * \param element The finite element
+     * \param fvGeometry The finite-volume geometry
+     * \param elemVolVars All volume variables for the element
+     * \param scv The sub control volume
+     *
+     * For this method, the return parameter stores the conserved quantity rate
+     * generated or annihilate per volume unit. Positive values mean
+     * that the conserved quantity is created, negative ones mean that it vanishes.
+     * E.g. for the mass balance that would be a mass rate in \f$ [ kg / (m^3 \cdot s)] \f$.
+     */
+    template<class ElementVolumeVariables>
+    PrimaryVariables source(const Element &element,
+                            const FVElementGeometry& fvGeometry,
+                            const ElementVolumeVariables& elemVolVars,
+                            const SubControlVolume &scv) const
+    {
+        PrimaryVariables values(0.0);
+
+        if (couplingManager().isCoupled(CouplingManager::poreNetworkIndex, CouplingManager::freeFlowMassIndex, scv))
+        {
+            values[Indices::conti0EqIdx] = couplingManager().massCouplingCondition(
+                CouplingManager::poreNetworkIndex,
+                CouplingManager::freeFlowMassIndex, fvGeometry,
+                scv,
+                elemVolVars);
+        }
+        values /= this->gridGeometry().poreVolume(scv.dofIndex());
+
+        return values;
+    }
+
+    // \}
+
+    //! Set the coupling manager
+    void setCouplingManager(std::shared_ptr<CouplingManager> cm)
+    { couplingManager_ = cm; }
+
+    //! Get the coupling manager
+    const CouplingManager& couplingManager() const
+    { return *couplingManager_; }
+
+private:
+    bool onInlet_(const SubControlVolume& scv) const
+    {
+        return onLeftBoundary_(scv);
+    }
+
+    bool onOutlet_(const SubControlVolume& scv) const
+    {
+        return onRightBoundary_(scv);
+    }
+
+    bool onLeftBoundary_(const SubControlVolume& scv) const
+    { return this->gridGeometry().poreLabel(scv.dofIndex()) == 0; }
+
+    bool onRightBoundary_(const SubControlVolume& scv) const
+    { return this->gridGeometry().poreLabel(scv.dofIndex()) == 1; }
+
+    bool onLowerBoundary_(const SubControlVolume& scv) const
+    { return this->gridGeometry().poreLabel(scv.dofIndex()) == 2; }
+
+    bool onUpperBoundary_(const SubControlVolume& scv) const
+    { return this->gridGeometry().poreLabel(scv.dofIndex()) == 3; }
+
+
+    std::shared_ptr<CouplingManager> couplingManager_;
+    Scalar initialPressure_;
+    Scalar inletPressure_;
+    Scalar outletPressure_;
+};
+
+} // end namespace Dumux
+
+#endif
