3357825cbe5704b156fe0cc361a076bf3ed112fc
Anna Mareike Kostelecky <anmako96@web.de>
2025-08-23 20:23:59 +0200

    [md][boundary][ffpnm] 3D test case
    ... no-flow at the moment as flow in ff domain currently leads to non-convergence

---
+++ test/multidomain/boundary/freeflowporenetwork/1p_1p/3D/properties.hh
@@ -0,0 +1,131 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightText: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup BoundaryTests
+ * \brief The properties for a simple Darcy test (cell-centered finite volume method)
+ */
+#ifndef DUMUX_PNMSTOKES_PROPERTIES_HH
+#define DUMUX_PNMSTOKES_PROPERTIES_HH
+
+#include <dune/foamgrid/foamgrid.hh>
+#include <dune/grid/yaspgrid.hh>
+
+#include <dumux/material/fluidsystems/1pliquid.hh>
+#include <dumux/material/components/constant.hh>
+
+#include <dumux/porenetwork/1p/model.hh>
+#include <dumux/freeflow/navierstokes/mass/1p/model.hh>
+#include <dumux/freeflow/navierstokes/momentum/fcstaggered/model.hh>
+#include <dumux/freeflow/navierstokes/mass/problem.hh>
+#include <dumux/freeflow/navierstokes/momentum/problem.hh>
+#include <dumux/discretization/cctpfa.hh>
+#include <dumux/discretization/fcstaggered.hh>
+#include <dumux/multidomain/boundary/freeflowporenetwork/couplingmanager.hh>
+
+#include "problem_pnm.hh"
+#include "problem_freeflow.hh"
+
+namespace Dumux::Properties {
+
+// Create new type tags
+namespace TTag {
+#if ISOTHERMAL
+struct PNMOnePModel { using InheritsFrom = std::tuple<PNMOneP>; };
+#else
+struct PNMOnePModel { using InheritsFrom = std::tuple<PNMOnePNI>; };
+#endif
+} // end namespace TTag
+
+// Set the problem property
+template<class TypeTag>
+struct Problem<TypeTag, TTag::PNMOnePModel> { using type = Dumux::PNMOnePProblem<TypeTag>; };
+
+// the fluid system
+template<class TypeTag>
+struct FluidSystem<TypeTag, TTag::PNMOnePModel>
+{
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using type = FluidSystems::OnePLiquid<Scalar, Dumux::Components::Constant<1, Scalar> > ;
+};
+
+//! The advection type
+template<class TypeTag>
+struct AdvectionType<TypeTag, TTag::PNMOnePModel>
+{
+private:
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using TransmissibilityLaw = Dumux::PoreNetwork::TransmissibilityBruus<GetPropType<TypeTag, Properties::Scalar>>;
+public:
+    using type =  Dumux::PoreNetwork::CreepingFlow<Scalar, TransmissibilityLaw>;
+};
+
+// Set the grid type
+template<class TypeTag>
+struct Grid<TypeTag, TTag::PNMOnePModel> { using type = Dune::FoamGrid<1, 3>; };
+
+// Create new type tags
+namespace TTag {
+struct FreeFlowOneP {};
+struct FreeFlowOnePMomentum { using InheritsFrom = std::tuple<FreeFlowOneP, NavierStokesMomentum, FaceCenteredStaggeredModel>; };
+#if ISOTHERMAL
+struct FreeFlowOnePMass { using InheritsFrom = std::tuple<FreeFlowOneP, NavierStokesMassOneP, CCTpfaModel>; };
+#else
+struct FreeFlowOnePMass { using InheritsFrom = std::tuple<FreeFlowOneP, NavierStokesMassOnePNI, CCTpfaModel>; };
+#endif
+} // end namespace TTag
+
+
+// the fluid system
+template<class TypeTag>
+struct FluidSystem<TypeTag, TTag::FreeFlowOneP>
+{
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using type = FluidSystems::OnePLiquid<Scalar, Dumux::Components::Constant<1, Scalar> > ;
+};
+
+// Set the grid type
+template<class TypeTag>
+struct Grid<TypeTag, TTag::FreeFlowOneP>
+{
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using type = Dune::YaspGrid<3, Dune::TensorProductCoordinates<Scalar, 3>>;
+};
+
+// Set the problem property
+template<class TypeTag>
+struct Problem<TypeTag, TTag::FreeFlowOnePMomentum>
+{ using type = Dumux::FreeFlowOnePTestProblem<TypeTag, Dumux::NavierStokesMomentumProblem<TypeTag>>; };
+
+template<class TypeTag>
+struct Problem<TypeTag, TTag::FreeFlowOnePMass>
+{ using type = Dumux::FreeFlowOnePTestProblem<TypeTag, Dumux::NavierStokesMassProblem<TypeTag>>; };
+
+template<class TypeTag>
+struct EnableGridGeometryCache<TypeTag, TTag::FreeFlowOneP> { static constexpr bool value = true; };
+template<class TypeTag>
+struct EnableGridFluxVariablesCache<TypeTag, TTag::FreeFlowOneP> { static constexpr bool value = true; };
+template<class TypeTag>
+struct EnableGridVolumeVariablesCache<TypeTag, TTag::FreeFlowOneP> { static constexpr bool value = true; };
+
+template<class TypeTag>
+struct CouplingManager<TypeTag, TTag::PNMOnePModel>
+{
+    using Traits = MultiDomainTraits<Properties::TTag::FreeFlowOnePMomentum, Properties::TTag::FreeFlowOnePMass, TTag::PNMOnePModel>;
+    using type = Dumux::FreeFlowPoreNetworkCouplingManager<Traits>;
+};
+
+template<class TypeTag>
+struct CouplingManager<TypeTag, TTag::FreeFlowOneP>
+{
+    using Traits = MultiDomainTraits<Properties::TTag::FreeFlowOnePMomentum, Properties::TTag::FreeFlowOnePMass, TTag::PNMOnePModel>;
+    using type = Dumux::FreeFlowPoreNetworkCouplingManager<Traits>;
+};
+
+} // end namespace Dumux::Properties
+
+#endif
