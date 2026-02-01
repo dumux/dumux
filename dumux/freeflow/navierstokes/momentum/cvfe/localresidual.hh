8a7191feabd31ac42d79f9a2b4f74a18e2b2775a
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2026-02-01 10:12:07 +0100

    [ffns][cvfe][momentum] New integral functions in local residual

--- dumux/freeflow/navierstokes/momentum/cvfe/localresidual.hh
+++ dumux/freeflow/navierstokes/momentum/cvfe/localresidual.hh
@@ -24,6 +24,7 @@
 #include <dumux/discretization/method.hh>
 #include <dumux/discretization/fem/interpolationpointdata.hh>
 #include <dumux/discretization/cvfe/interpolationpointdata.hh>
+#include <dumux/discretization/cvfe/quadraturerules.hh>
 #include <dumux/assembly/cvfelocalresidual.hh>
 
 #include <dumux/freeflow/navierstokes/momentum/cvfe/flux.hh>
@@ -57,9 +58,9 @@ class NavierStokesMomentumCVFELocalResidual
 
     using GridVariables = GetPropType<TypeTag, Properties::GridVariables>;
 
-    using GridVolumeVariables = typename GridVariables::GridVolumeVariables;
-    using ElementVolumeVariables = typename GridVolumeVariables::LocalView;
-    using VolumeVariables = typename GridVolumeVariables::VolumeVariables;
+    using GridVariablesCache = typename GridVariables::GridVolumeVariables;
+    using ElementVariables = typename GridVariablesCache::LocalView;
+    using Variables = typename GridVariablesCache::VolumeVariables;
 
     using GridFluxVariablesCache = typename GridVariables::GridFluxVariablesCache;
     using ElementFluxVariablesCache = typename GridFluxVariablesCache::LocalView;
@@ -86,9 +87,14 @@ class NavierStokesMomentumCVFELocalResidual
     using LocalBasis = typename GridGeometry::FeCache::FiniteElementType::Traits::LocalBasisType;
     using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
     using BaseIpData = Dumux::CVFE::InterpolationPointData<typename GridView::template Codim<0>::Entity::Geometry::LocalCoordinate, GlobalPosition>;
-    using FluxContext = NavierStokesMomentumFluxContext<Problem, FVElementGeometry, ElementVolumeVariables, ElementFluxVariablesCache>;
+    using FluxContext = NavierStokesMomentumFluxContext<Problem, FVElementGeometry, ElementVariables, ElementFluxVariablesCache>;
     using FluxHelper = NavierStokesMomentumFluxCVFE<GridGeometry, NumEqVector>;
     using FeResidual = NavierStokesMomentumFELocalResidual<Scalar, NumEqVector, LocalBasis, Extrusion>;
+
+    using FeIpData = FEInterpolationPointData<GlobalPosition, LocalBasis>;
+    using FeFaceIpData = Dumux::CVFE::FaceInterpolationPointData<FeIpData, typename SubControlVolumeFace::Traits::LocalIndexType>;
+    using FluxFunctionContext = NavierStokesMomentumFluxFunctionContext<Problem, FVElementGeometry, ElementVariables, FeFaceIpData>;
+    using FluxFunctionHelper = NavierStokesMomentumFluxFunctionCVFE<GridGeometry, NumEqVector>;
 
 public:
     //! Use the parent type's constructor
@@ -108,10 +114,34 @@ public:
     NumEqVector computeStorage(const Problem& problem,
                                const FVElementGeometry& fvGeometry,
                                const SubControlVolume& scv,
-                               const VolumeVariables& volVars,
+                               const Variables& volVars,
                                const bool isPreviousStorage) const
     {
         return problem.density(fvGeometry.element(), fvGeometry, ipData(fvGeometry, scv), isPreviousStorage) * volVars.velocity();
+    }
+
+    /*!
+     * \brief Calculate the storage integral
+     *
+     * \param fvGeometry The finite-volume geometry of the element
+     * \param elemVars The variables for all local dofs of the element
+     * \param scv The sub control volume
+     * \param isPreviousTimeLevel If set to true, the storage term is evaluated on the previous time level.
+     *
+     */
+    NumEqVector storageIntegral(const FVElementGeometry& fvGeometry,
+                                const ElementVariables& elemVars,
+                                const SubControlVolume& scv,
+                                bool isPreviousTimeLevel) const
+    {
+        const auto& volVars = elemVars[scv];
+        // We apply mass lumping
+        NumEqVector storage = this->asImp().problem().density(fvGeometry.element(), fvGeometry, ipData(fvGeometry, scv), isPreviousTimeLevel)
+                            * volVars.velocity();
+
+        storage *= Extrusion::volume(fvGeometry, scv) * volVars.extrusionFactor();
+
+        return storage;
     }
 
     /*!
@@ -121,29 +151,29 @@ public:
      * \param element The DUNE Codim<0> entity for which the residual
      *                ought to be calculated
      * \param fvGeometry The finite-volume geometry of the element
-     * \param elemVolVars The volume variables associated with the element stencil
+     * \param elemVars The variables for all local dofs of the element
      * \param scv The sub-control volume over which we integrate the source term
      *
      */
     NumEqVector computeSource(const Problem& problem,
                               const Element& element,
                               const FVElementGeometry& fvGeometry,
-                              const ElementVolumeVariables& elemVolVars,
+                              const ElementVariables& elemVars,
                               const SubControlVolume& scv) const
     {
         NumEqVector source;
 
-        if constexpr (Detail::hasProblemSourceWithIpDataInterface<Problem, FVElementGeometry, ElementVolumeVariables, BaseIpData>())
+        if constexpr (Detail::hasProblemSourceWithIpDataInterface<Problem, FVElementGeometry, ElementVariables, BaseIpData>())
         {
-            source = problem.source(fvGeometry, elemVolVars, ipData(fvGeometry, scv.center()));
+            source = problem.source(fvGeometry, elemVars, ipData(fvGeometry, scv.center()));
 
             // ToDo: point source data with ipData
             // add contribution from possible point sources
             if (!problem.pointSourceMap().empty())
-                source += problem.scvPointSources(element, fvGeometry, elemVolVars, scv);
+                source += problem.scvPointSources(element, fvGeometry, elemVars, scv);
         }
         else
-            source = ParentType::computeSource(problem, element, fvGeometry, elemVolVars, scv);
+            source = ParentType::computeSource(problem, element, fvGeometry, elemVars, scv);
 
 
         // add rho*g (note that gravity might be zero in case it's disabled in the problem)
@@ -162,7 +192,7 @@ public:
             // The velocity term is new with respect to Cartesian coordinates and handled below as a source term
             // It only enters the balance of the momentum balance in radial direction
             source[Extrusion::radialAxis] += -2.0*problem.effectiveViscosity(element, fvGeometry, data)
-                * elemVolVars[scv].velocity(Extrusion::radialAxis) / (r*r);
+                * elemVars[scv].velocity(Extrusion::radialAxis) / (r*r);
 
             // Pressure term (needed because we incorporate pressure in terms of a surface integral).
             // grad(p) becomes div(pI) + (p/r)*n_r in cylindrical coordinates. The second term
@@ -174,23 +204,56 @@ public:
     }
 
     /*!
+     * \brief Calculate the source integral
+     *
+     * \param fvGeometry The finite-volume geometry of the element
+     * \param elemVars The variables for all local dofs of the element
+     * \param scv The sub control volume
+     *
+     */
+    NumEqVector sourceIntegral(const FVElementGeometry& fvGeometry,
+                               const ElementVariables& elemVars,
+                               const SubControlVolume& scv) const
+    {
+        static_assert(!(dim == 2 && isRotationalExtrusion<Extrusion>), "Rotational extrusion source terms are not implemented for integral interface.");
+
+        const auto& problem = this->asImp().problem();
+
+        NumEqVector source(0.0);
+        for (const auto& qpData : CVFE::quadratureRule(fvGeometry, scv))
+        {
+            source += qpData.weight() * (problem.source(fvGeometry, elemVars, qpData.ipData())
+                                        + problem.density(fvGeometry.element(), fvGeometry, qpData.ipData()) * problem.gravity());
+        }
+
+        // ToDo: point source data with ipData
+        // add contribution from possible point sources
+        if (!problem.pointSourceMap().empty())
+            source += Extrusion::volume(fvGeometry, scv) * problem.scvPointSources(fvGeometry.element(), fvGeometry, elemVars, scv);
+
+        source *= elemVars[scv].extrusionFactor();
+
+        return source;
+    }
+
+    /*!
      * \brief Evaluates the mass flux over a face of a sub control volume.
      *
      * \param problem The problem
      * \param element The element
      * \param fvGeometry The finite volume geometry context
-     * \param elemVolVars The volume variables for all flux stencil elements
+     * \param elemVars The variables for all local dofs of the element
      * \param scvf The sub control volume face to compute the flux on
      * \param elemFluxVarsCache The cache related to flux computation
      */
     NumEqVector computeFlux(const Problem& problem,
                             const Element& element,
                             const FVElementGeometry& fvGeometry,
-                            const ElementVolumeVariables& elemVolVars,
+                            const ElementVariables& elemVars,
                             const SubControlVolumeFace& scvf,
                             const ElementFluxVariablesCache& elemFluxVarsCache) const
     {
-        FluxContext context(problem, fvGeometry, elemVolVars, elemFluxVarsCache, scvf);
+        FluxContext context(problem, fvGeometry, elemVars, elemFluxVarsCache, scvf);
         FluxHelper fluxHelper;
 
         NumEqVector flux(0.0);
@@ -200,12 +263,51 @@ public:
         return flux;
     }
 
+    /*!
+     * \brief Calculates the flux integral over a sub control volume face.
+     *
+     * \param fvGeometry The finite-volume geometry of the element
+     * \param elemVars The variables for all local dofs of the element
+     * \param elemFluxVarsCache the flux variable caches for the element's flux stencils
+     * \param scvf The sub control volume face
+     *
+     */
+    NumEqVector fluxIntegral(const FVElementGeometry& fvGeometry,
+                             const ElementVariables& elemVars,
+                             const ElementFluxVariablesCache& elemFluxVarsCache,
+                             const SubControlVolumeFace& scvf) const
+    {
+        const auto& problem = this->asImp().problem();
+
+        NumEqVector flux(0.0);
+        GlobalPosition velIntegral(0.0);
+        FluxFunctionHelper fluxFunctionHelper;
+        const auto& localBasis = fvGeometry.feLocalBasis();
+        const auto& elementGeo = fvGeometry.elementGeometry();
+
+        for (const auto& qpData : CVFE::quadratureRule(fvGeometry, scvf))
+        {
+            const auto& faceIpData = qpData.ipData();
+            FeFaceIpData feIpData(faceIpData.unitOuterNormal(), faceIpData.scvfIndex(), elementGeo, faceIpData.local(), faceIpData.global(), localBasis);
+            FluxFunctionContext context(problem, fvGeometry, elemVars, feIpData);
+
+            velIntegral += context.velocity() * qpData.weight();
+            flux += qpData.weight() * ( fluxFunctionHelper.diffusiveMomentumFluxIntegrand(context)
+                                      + fluxFunctionHelper.pressureFluxIntegrand(context) );
+        }
+        flux += fluxFunctionHelper.advectiveMomentumFluxIntegral(problem, fvGeometry, elemVars, elemFluxVarsCache, scvf, velIntegral);
+
+        flux *= elemVars[fvGeometry.scv(scvf.insideScvIdx())].extrusionFactor();
+
+        return flux;
+    }
+
     void addToElementStorageResidual(ElementResidualVector& residual,
                                      const Problem& problem,
                                      const Element& element,
                                      const FVElementGeometry& fvGeometry,
-                                     const ElementVolumeVariables& prevElemVolVars,
-                                     const ElementVolumeVariables& curElemVolVars) const
+                                     const ElementVariables& prevElemVolVars,
+                                     const ElementVariables& curElemVolVars) const
     {
         FeResidual::addStorageTerms(
             residual, problem, fvGeometry, prevElemVolVars, curElemVolVars, this->timeLoop().timeStepSize()
@@ -216,7 +318,7 @@ public:
                                            const Problem& problem,
                                            const Element& element,
                                            const FVElementGeometry& fvGeometry,
-                                           const ElementVolumeVariables& elemVolVars,
+                                           const ElementVariables& elemVars,
                                            const ElementFluxVariablesCache& elemFluxVarsCache,
                                            const ElementBoundaryTypes &elemBcTypes) const
     {
@@ -224,8 +326,6 @@ public:
             residual, problem, fvGeometry, elemVars, elemFluxVarsCache, elemBcTypes
         );
     }
-
-
 };
 
 } // end namespace Dumux
