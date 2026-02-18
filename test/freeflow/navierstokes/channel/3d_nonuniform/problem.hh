3a019926fc4be23171d432a377086a4a38a5f652
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2026-02-18 14:01:40 +0100

    [tests][ffns][3d_nonuniform] Move computeWallShearStress into main

--- test/freeflow/navierstokes/channel/3d_nonuniform/problem.hh
+++ test/freeflow/navierstokes/channel/3d_nonuniform/problem.hh
@@ -7,23 +7,11 @@
 #ifndef DUMUX_TEST_FREEFLOW_NAVIERSTOKES_3D_CHANNEL_PROBLEM_HH
 #define DUMUX_TEST_FREEFLOW_NAVIERSTOKES_3D_CHANNEL_PROBLEM_HH
 
-#if HAVE_DUNE_FOAMGRID
-#include <dune/foamgrid/foamgrid.hh>
-#endif
+#include <iostream>
 
-#include <dune/common/float_cmp.hh>
-
-#include <dumux/common/math.hh>
 #include <dumux/common/properties.hh>
 #include <dumux/common/parameters.hh>
-#include <dumux/io/vtkoutputmodule.hh>
-#include <dumux/io/vtk/function.hh>
 #include <dumux/io/grid/griddata.hh>
-
-#include <dumux/geometry/diameter.hh>
-#include <dumux/geometry/geometricentityset.hh>
-#include <dumux/geometry/boundingboxtree.hh>
-#include <dumux/multidomain/embedded/circlepoints.hh>
 
 namespace Dumux {
 
@@ -46,16 +34,13 @@ class ThreeDChannelTestProblem : public BaseProblem
     using Indices = typename GetPropType<TypeTag, Properties::ModelTraits>::Indices;
     using BoundaryFluxes = typename ParentType::BoundaryFluxes;
     using DirichletValues = typename ParentType::DirichletValues;
-    using ModelTraits = GetPropType<TypeTag, Properties::ModelTraits>;
     using Scalar = GetPropType<TypeTag, Properties::Scalar>;
-    using SolutionVector = GetPropType<TypeTag, Properties::SolutionVector>;
 
     static constexpr int dim = GridGeometry::GridView::dimension;
     static constexpr auto dimWorld = GridGeometry::GridView::dimensionworld;
 
     using Element = typename GridGeometry::GridView::template Codim<0>::Entity;
     using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
-    using VelocityVector = Dune::FieldVector<Scalar, dimWorld>;
 
     using CouplingManager = GetPropType<TypeTag, Properties::CouplingManager>;
 
@@ -224,162 +209,6 @@ public:
 
         std::cout << "Transmissibility: " << outflux / deltaP_ << " m^3/(s*Pa)" << std::endl;
     }
-
-#if HAVE_DUNE_FOAMGRID
-    // Compute the wall shear stress
-    template<class VelSolutionVector, class GridVariables, class CouplingManager, class Assembler>
-    void computeWallShearStress(
-        const VelSolutionVector& curSol,
-        const GridVariables& gridVariables,
-        CouplingManager& couplingManager,
-        const Assembler& assembler
-    ){
-        const auto& gg = this->gridGeometry();
-        const auto& gv = gg.gridView();
-
-        using Grid = Dune::FoamGrid<dim-1, dimWorld>;
-        Dune::GridFactory<Grid> factory;
-        {
-            std::vector<std::vector<std::vector<unsigned int>>> elems;
-            elems.reserve(gg.numBoundaryScvf());
-            std::vector<bool> insertedVertex(gv.size(dim), false);
-            std::vector<int> vertexIndexMap(gv.size(dim), -1);
-            std::vector<Dune::FieldVector<double, 3>> points;
-            points.reserve(gg.numBoundaryScvf()*4);
-            std::size_t boundaryElementIndex = 0;
-            for (const auto& element : elements(gv))
-            {
-                for (const auto& intersection : intersections(gv, element))
-                {
-                    if (intersection.boundary())
-                    {
-                        const auto insideIdx = intersection.indexInInside();
-                        const auto refElement = referenceElement(element);
-                        const auto numVertices = refElement.size(insideIdx, 1, dim);
-                        if (numVertices == 3)
-                            elems.push_back({std::vector<unsigned int>{}});
-                        else if (numVertices == 4)
-                            elems.push_back({std::vector<unsigned int>{},
-                                             std::vector<unsigned int>{}}); // add two triangles
-                        else
-                                DUNE_THROW(Dune::NotImplemented,
-                                    "Wall shear stress for boundary type with " << numVertices << " corners"
-                                );
-
-
-                        for (int i = 0; i < numVertices; ++i)
-                        {
-                            const auto localVIdx = refElement.subEntity(insideIdx, 1, i, dim);
-                            const auto& vertex = element.template subEntity<dim>(localVIdx);
-                            const auto vIdx = gg.vertexMapper().index(vertex);
-                            if (!insertedVertex[vIdx])
-                            {
-                                vertexIndexMap[vIdx] = points.size();
-                                points.push_back(vertex.geometry().corner(0));
-                                insertedVertex[vIdx] = true;
-                            }
-
-
-                            if (numVertices == 3)
-                                elems[boundaryElementIndex][0].push_back(vertexIndexMap[vIdx]);
-
-                            else if (numVertices == 4)
-                            {
-                                // add two triangles
-                                if (i == 0)
-                                    elems[boundaryElementIndex][0].push_back(vertexIndexMap[vIdx]);
-                                else if (i == 1 || i == 2)
-                                {
-                                    elems[boundaryElementIndex][0].push_back(vertexIndexMap[vIdx]);
-                                    elems[boundaryElementIndex][1].push_back(vertexIndexMap[vIdx]);
-                                }
-                                else
-                                    elems[boundaryElementIndex][1].push_back(vertexIndexMap[vIdx]);
-                            }
-                            else
-                                DUNE_THROW(Dune::NotImplemented,
-                                    "Wall shear stress for boundary type with " << numVertices << " corners"
-                                );
-                        }
-
-                        ++boundaryElementIndex;
-                    }
-                }
-            }
-
-            for (const auto& p : points)
-                factory.insertVertex(p);
-            for (const auto& e : elems)
-                for (const auto& ee : e)
-                    factory.insertElement(Dune::GeometryTypes::simplex(dim-1), ee);
-        }
-
-        auto bGrid = factory.createGrid();
-
-        using ElementSet = GridViewGeometricEntitySet<typename Grid::LeafGridView, 0>;
-        using Tree = BoundingBoxTree<ElementSet>;
-        Tree tree(std::make_shared<ElementSet>(bGrid->leafGridView()));
-
-        std::vector<GlobalPosition> wallShearStress(bGrid->leafGridView().size(0));
-
-        auto fvGeometry = localView(gg);
-        auto elemVolVars = localView(gridVariables.curGridVolVars());
-        auto elemFluxVarsCache = localView(gridVariables.gridFluxVarsCache());
-        for (const auto& element : elements(gv))
-        {
-            const auto h = diameter(element.geometry());
-            couplingManager.bindCouplingContext(
-                CouplingManager::freeFlowMomentumIndex, element, assembler
-            );
-            fvGeometry.bind(element);
-            elemVolVars.bind(element, fvGeometry, curSol);
-            elemFluxVarsCache.bind(element, fvGeometry, elemVolVars);
-
-            for (const auto& scvf : scvfs(fvGeometry))
-            {
-                if (scvf.boundary())
-                {
-                    const auto ip = scvf.ipGlobal();
-
-                    // assemble wss = sigma*n - (sigma*n*n)*n
-                    const auto& fluxVarCache = elemFluxVarsCache[scvf];
-                    using Tensor = Dune::FieldMatrix<Scalar, dim>;
-                    Tensor sigma(0.0);
-                    for (const auto& scv : scvs(fvGeometry))
-                    {
-                       const auto& volVars = elemVolVars[scv];
-                       for (int dir = 0; dir < dim; ++dir)
-                           sigma[dir].axpy(volVars.velocity(dir), fluxVarCache.gradN(scv.indexInElement()));
-                    }
-                    sigma += getTransposed(sigma);
-                    sigma *= -this->effectiveViscosity(element, fvGeometry, scvf);
-                    for (int dir = 0; dir < dim; ++dir)
-                        sigma[dir][dir] += this->pressure(element, fvGeometry, scvf);
-
-                    const auto sigman = mv(sigma, scvf.unitOuterNormal());
-                    const auto normalStress = (sigman * scvf.unitOuterNormal())*scvf.unitOuterNormal();
-                    const auto wss = sigman - normalStress;
-
-                    // make sure we hit all triangles of this face
-                    const auto points = EmbeddedCoupling::circlePoints(ip, scvf.unitOuterNormal(), 0.05*h, 4);
-                    for (const auto& p : points)
-                        for (auto b : intersectingEntities(p, tree))
-                            wallShearStress[b] = wss;
-                }
-            }
-        }
-
-        Dune::MultipleCodimMultipleGeomTypeMapper<typename Grid::LeafGridView> bMapper(
-            bGrid->leafGridView(), Dune::mcmgLayout(Dune::Codim<0>())
-        );
-        using Field = Dumux::Vtk::Field<typename Grid::LeafGridView>;
-        Dune::VTKWriter<typename Grid::LeafGridView> writer(bGrid->leafGridView());
-        writer.addCellData(Field(
-            bGrid->leafGridView(), bMapper, wallShearStress, "wallShearStress", dimWorld
-        ).get());
-        writer.write(this->name() + "_wall_shear_stress");
-    }
-#endif
 
 private:
     bool isInlet_(const SubControlVolumeFace& scvf) const
