1969565d47853dc697fd8050195749a19b42461a
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2026-02-20 17:38:36 +0100

    [disc] General gridvariables class

---
+++ dumux/discretization/gridvariables.hh
@@ -0,0 +1,159 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+//
+// SPDX-FileCopyrightText: Copyright © DuMux Project contributors, see AUTHORS.md in root folder
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+/*!
+ * \file
+ * \ingroup Discretization
+ * \brief The grid variables class for for general schemes,
+ *        storing variables and data on the grid
+ */
+#ifndef DUMUX_GRID_VARIABLES_HH
+#define DUMUX_GRID_VARIABLES_HH
+
+#include <type_traits>
+#include <memory>
+
+namespace Dumux {
+
+/*!
+ * \ingroup Discretization
+ * \brief The grid variables class for general schemes, storing variables and data on the grid
+ * \tparam the type of the grid geometry
+ * \tparam the type of the grid variables cache
+ * \tparam the type of the grid data (interpolation data) cache
+ */
+template<class GG, class GVC, class GDC>
+class GridVariables
+{
+public:
+    //! export type of the finite volume grid geometry
+    using GridGeometry = GG;
+
+    //! export type of the grid variables cache
+    using GridVariablesCache = GVC;
+
+    //! export type of the volume variables
+    using Variables = typename GridVariablesCache::Variables;
+
+    //! export primary variable type
+    using PrimaryVariables = typename Variables::PrimaryVariables;
+
+    //! export scalar
+    using Scalar = typename PrimaryVariables::value_type;
+
+    //! export type of the finite volume grid geometry
+    using GridDataCache = GDC;
+
+    template<class Problem>
+    GridVariables(std::shared_ptr<Problem> problem,
+                  std::shared_ptr<const GridGeometry> gridGeometry)
+    : gridGeometry_(gridGeometry)
+    , curGridVars_(*problem)
+    , prevGridVars_(*problem)
+    , gridDataCache_(*problem)
+    {}
+
+    //! initialize all variables (stationary case)
+    template<class SolutionVector>
+    void init(const SolutionVector& curSol)
+    {
+        // resize and update the volVars with the initial solution
+        curGridVars_.update(*gridGeometry_, curSol);
+
+        // update the data cache (always force data cache update on initialization)
+        gridDataCache_.update(*gridGeometry_, curGridVars_, curSol, true);
+
+        // set the variables of the previous time step in case we have an instationary problem
+        // note that this means some memory overhead in the case of enabled caching, however
+        // this it outweighted by the advantage of having a single grid variables object for
+        // stationary and instationary problems
+        prevGridVars_ = curGridVars_;
+    }
+
+    //! update all variables
+    template<class SolutionVector>
+    void update(const SolutionVector& curSol, bool forceDataCacheUpdate = false)
+    {
+        // resize and update the volVars with the initial solution
+        curGridVars_.update(*gridGeometry_, curSol);
+
+        // update the data cache
+        gridDataCache_.update(*gridGeometry_, curGridVars_, curSol, forceDataCacheUpdate);
+    }
+
+    //! update all variables after grid adaption
+    template<class SolutionVector>
+    void updateAfterGridAdaption(const SolutionVector& curSol)
+    {
+        // update (always force data cache update as the grid changed)
+        update(curSol, true);
+
+        // for instationary problems also update the variables
+        // for the previous time step to the new grid
+        prevGridVars_ = curGridVars_;
+    }
+
+    /*!
+     * \brief Sets the current state as the previous for next time step
+     * \note this has to be called at the end of each time step
+     */
+    void advanceTimeStep()
+    {
+        prevGridVars_ = curGridVars_;
+    }
+
+    //! resets state to the one before time integration
+    template<class SolutionVector>
+    void resetTimeStep(const SolutionVector& solution)
+    {
+        // set the new time step variables to old variables
+        curGridVars_ = prevGridVars_;
+
+        // update the data cache
+        gridDataCache_.update(*gridGeometry_, curGridVars_, solution);
+    }
+
+    //! return the data cache
+    const GridDataCache& gridDataCache() const
+    { return gridDataCache_; }
+
+    //! return the data cache
+    GridDataCache& gridDataCache()
+    { return gridDataCache_; }
+
+    //! return the current variables
+    const GridVariablesCache& curGridVars() const
+    { return curGridVars_; }
+
+    //! return the current variables
+    GridVariablesCache& curGridVars()
+    { return curGridVars_; }
+
+    //! return the variables of the previous time step (for instationary problems)
+    const GridVariablesCache& prevGridVars() const
+    { return prevGridVars_; }
+
+    //! return the variables of the previous time step (for instationary problems)
+    GridVariablesCache& prevGridVars()
+    { return prevGridVars_; }
+
+    //! return the finite volume grid geometry
+    const GridGeometry& gridGeometry() const
+    { return *gridGeometry_; }
+
+protected:
+    std::shared_ptr<const GridGeometry> gridGeometry_; //!< pointer to the constant grid geometry
+
+private:
+    GridVariablesCache curGridVars_; //!< the current variables (primary and secondary variables)
+    GridVariablesCache prevGridVars_; //!< the previous time step's variables (primary and secondary variables)
+
+    GridDataCache gridDataCache_; //!< the data cache
+};
+
+} // end namespace Dumux
+
+#endif
