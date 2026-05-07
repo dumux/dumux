b106efe8c84628718f187c2ab6c8f7dcef80acf1
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2026-05-07 15:21:15 +0200

    [assembly] Default assembleInitialSolution for cvfe schemes

--- dumux/assembly/initialsolution.hh
+++ dumux/assembly/initialsolution.hh
@@ -13,7 +13,11 @@
 #define DUMUX_ASSEMBLY_INITIAL_SOLUTION_HH
 
 #include <vector>
+#include <cassert>
+#include <utility>
 #include <type_traits>
+
+#include <dune/common/hybridutilities.hh>
 
 #include <dumux/discretization/method.hh>
 
@@ -29,39 +33,99 @@ void assembleInitialSolution(SolutionVector& sol, const Problem& problem)
     const auto& gg = problem.gridGeometry();
     using GridGeometry = std::decay_t<decltype(gg)>;
 
-    // box method
-    if constexpr (GridGeometry::discMethod == DiscretizationMethods::box)
+    static constexpr bool isCVFE = DiscretizationMethods::isCVFE<typename GridGeometry::DiscretizationMethod>;
+    // CVFE schemes
+    if constexpr (isCVFE)
     {
         constexpr int dim = GridGeometry::GridView::dimension;
-        const auto numDofs = gg.vertexMapper().size();
-        const auto numVert = gg.gridView().size(dim);
+        const auto numDofs = gg.numDofs();
         sol.resize(numDofs);
 
-        // if there are more dofs than vertices (enriched nodal dofs), we have to
-        // call initial for all dofs at the nodes, coming from all neighboring elements.
-        if (numDofs != numVert)
+        if constexpr (GridGeometry::discMethod == DiscretizationMethods::box)
+        {
+            const auto numVert = gg.gridView().size(dim);
+            // if there are more dofs than vertices (enriched nodal dofs), we have to
+            // call initial for all dofs at the nodes, coming from all neighboring elements.
+            if (numDofs != numVert)
+            {
+                std::vector<bool> dofVisited(numDofs, false);
+                for (const auto& element : elements(gg.gridView()))
+                {
+                    for (int i = 0; i < element.subEntities(dim); ++i)
+                    {
+                        const auto dofIdxGlobal = gg.vertexMapper().subIndex(element, i, dim);
+                        // forward to implementation if value at dof is not set yet
+                        if (!dofVisited[dofIdxGlobal])
+                        {
+                            sol[dofIdxGlobal] = problem.initial(element.template subEntity<dim>(i));
+                            dofVisited[dofIdxGlobal] = true;
+                        }
+                    }
+                }
+            }
+
+            // otherwise we directly loop over the vertices
+            else
+            {
+                for (const auto& vertex : vertices(gg.gridView()))
+                    sol[gg.vertexMapper().index(vertex)] = problem.initial(vertex);
+            }
+        }
+        else if constexpr (GridGeometry::discMethod == DiscretizationMethods::pq1bubble)
+        {
+            for (const auto& vertex : vertices(gg.gridView()))
+                sol[gg.dofMapper().index(vertex)] = problem.initial(vertex);
+
+            // This assumed that we have only one bubble dof per element
+            for (const auto& element : elements(gg.gridView()))
+                sol[gg.dofMapper().index(element)] = problem.initial(element);
+        }
+        else if constexpr (GridGeometry::discMethod == DiscretizationMethods::fcdiamond)
         {
             std::vector<bool> dofVisited(numDofs, false);
             for (const auto& element : elements(gg.gridView()))
             {
-                for (int i = 0; i < element.subEntities(dim); ++i)
+                for (const auto& intersection : intersections(gg.gridView(), element))
                 {
-                    const auto dofIdxGlobal = gg.vertexMapper().subIndex(element, i, dim);
+                    const auto dofIdxGlobal = gg.dofMapper().subIndex(element, intersection.indexInInside(), 1);
                     // forward to implementation if value at dof is not set yet
                     if (!dofVisited[dofIdxGlobal])
                     {
-                        sol[dofIdxGlobal] = problem.initial(element.template subEntity<dim>(i));
+                        sol[dofIdxGlobal] = problem.initial(element.template subEntity<dim-1>(intersection.indexInInside()));
                         dofVisited[dofIdxGlobal] = true;
                     }
                 }
+
             }
         }
-
-        // otherwise we directly loop over the vertices
         else
         {
-            for (const auto& vertex : vertices(gg.gridView()))
-                sol[gg.vertexMapper().index(vertex)] = problem.initial(vertex);
+            // General CVFE schemes
+            std::vector<bool> dofVisited(numDofs, false);
+            for (const auto& element : elements(gg.gridView()))
+            {
+                const auto& localCoefficients = gg.feCache().get(element.type()).localCoefficients();
+                for (int localDofIdx = 0; localDofIdx < localCoefficients.size(); ++localDofIdx)
+                {
+                    // This in general only works if there is only one dof per sub-entity
+                    const auto& localKey = localCoefficients.localKey(localDofIdx);
+                    assert(localKey.index() == 0);
+                    const auto dofIdxGlobal = gg.dofMapper().subIndex(element, localKey.subEntity(), localKey.codim());
+
+                    // forward to implementation if value at dof is not set yet
+                    if (!dofVisited[dofIdxGlobal])
+                    {
+                        Dune::Hybrid::forEach(std::make_integer_sequence<int, dim+1>{}, [&](auto d)
+                        {
+                            constexpr int codim = dim - d;
+                            if (localKey.codim() == codim)
+                                sol[dofIdxGlobal] = problem.initial(element.template subEntity<codim>(localKey.subEntity()));
+                        });
+                        dofVisited[dofIdxGlobal] = true;
+                    }
+                }
+
+            }
         }
     }
 
