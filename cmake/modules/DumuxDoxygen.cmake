c82377e33fba3c9b6f367560044317ee7245b79c
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2026-05-28 19:22:48 +0200

    [fix] doxygen

--- cmake/modules/DumuxDoxygen.cmake
+++ cmake/modules/DumuxDoxygen.cmake
@@ -11,8 +11,15 @@ include(DuneDoxygen)
 macro (add_dumux_doxygen_target)
   if(DOXYGEN_FOUND)
     add_doxygen_target()
-    add_custom_target(doxygen_${ProjectName}_prebuild
-                      COMMAND rm -rf ${CMAKE_BINARY_DIR}/doc/doxygen/html)
-    add_dependencies(doxygen_${ProjectName} doxygen_${ProjectName}_prebuild)
+    # New dune-common creates doxygen_${ProjectName}_build; old creates doxygen_${ProjectName}
+    if(TARGET doxygen_${ProjectName}_build)
+      add_custom_target(doxygen_${ProjectName}_prebuild
+                        COMMAND rm -rf ${CMAKE_BINARY_DIR}/doc/doxygen/html)
+      add_dependencies(doxygen_${ProjectName}_build doxygen_${ProjectName}_prebuild)
+    elseif(TARGET doxygen_${ProjectName})
+      add_custom_target(doxygen_${ProjectName}_prebuild
+                        COMMAND rm -rf ${CMAKE_BINARY_DIR}/doc/doxygen/html)
+      add_dependencies(doxygen_${ProjectName} doxygen_${ProjectName}_prebuild)
+    endif()
   endif()
 endmacro ()
