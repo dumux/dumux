a6fe7eb92015549b4d7216229999c69ec723564c
Martin Schneider <martin.schneider@iws.uni-stuttgart.de>
2026-09-16 17:45:03 +0200

    [immiscible] Add the finite element terms for local dofs without a control volume

--- dumux/porousmediumflow/immiscible/localresidual_.hh
+++ dumux/porousmediumflow/immiscible/localresidual_.hh
@@ -13,10 +13,12 @@
 #ifndef DUMUX_IMMISCIBLE_LOCAL_RESIDUAL__HH
 #define DUMUX_IMMISCIBLE_LOCAL_RESIDUAL__HH
 
+#include <vector>
 
 #include <dumux/common/properties.hh>
 #include <dumux/common/parameters.hh>
 #include <dumux/common/numeqvector.hh>
+#include <dumux/common/typetraits/localdofs_.hh>
 #include <dumux/discretization/extrusion.hh>
 #include <dumux/flux/upwindscheme.hh>
 #include <dumux/discretization/cvfe/quadraturerules.hh>
@@ -36,6 +38,8 @@ class ImmiscibleLocalResidual : public DiscretizationDefaultLocalOperator<TypeTa
     using ParentType = DiscretizationDefaultLocalOperator<TypeTag>;
 
     using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    using Problem = GetPropType<TypeTag, Properties::Problem>;
+    using Element = typename GetPropType<TypeTag, Properties::GridGeometry>::GridView::template Codim<0>::Entity;
     using NumEqVector = Dumux::NumEqVector<GetPropType<TypeTag, Properties::PrimaryVariables>>;
     using AdvectionType = GetPropType<TypeTag, Properties::AdvectionType>;
 
@@ -56,6 +60,7 @@ class ImmiscibleLocalResidual : public DiscretizationDefaultLocalOperator<TypeTa
     static_assert(!ModelTraits::enableEnergyBalance(), "The energy balance is not implemented yet");
 
 public:
+    using ElementResidualVector = typename ParentType::ElementResidualVector;
     using ParentType::ParentType;
 
     /*!
@@ -134,6 +139,107 @@ public:
 
         return flux;
     }
+
+    /*!
+     * \brief Add the storage contribution of the local dofs owning no control volume
+     *
+     * \note Those degrees of freedom carry a finite element residual instead of a balance
+     *       over a control volume, so their storage term is the mass-lumped integral of the
+     *       shape function against the time derivative.
+     */
+    void addToElementStorageResidual(ElementResidualVector& residual,
+                                     const Problem& problem,
+                                     const Element& element,
+                                     const ElementDiscretization& elemDisc,
+                                     const ElementVariables& prevElemVars,
+                                     const ElementVariables& curElemVars) const
+    {
+        if constexpr (Dumux::Detail::LocalDofs::hasNonCVLocalDofsInterface<ElementDiscretization>())
+        {
+            if (nonCVLocalDofs(elemDisc).empty())
+                return;
+
+            // mass lumping leaves the integral of the shape function as the only weight
+            std::vector<Scalar> integralShapeFunctions(Dumux::Detail::LocalDofs::numLocalDofs(elemDisc), 0.0);
+            for (const auto& qpData : Dumux::CVFE::quadratureRule(elemDisc, element))
+            {
+                const auto& shapeValues = cache(curElemVars, qpData.ipData()).shapeValues();
+                for (const auto& localDof : nonCVLocalDofs(elemDisc))
+                    integralShapeFunctions[localDof.index()] += qpData.weight()*shapeValues[localDof.index()][0];
+            }
+
+            const auto timeStepSize = this->timeLoop().timeStepSize();
+            for (const auto& localDof : nonCVLocalDofs(elemDisc))
+            {
+                const auto localDofIdx = localDof.index();
+                const auto& curVars = curElemVars[localDof];
+                const auto& prevVars = prevElemVars[localDof];
+
+                for (int phaseIdx = 0; phaseIdx < numPhases; ++phaseIdx)
+                {
+                    const auto curStorage = curVars.porosity()*curVars.density(phaseIdx)*curVars.saturation(phaseIdx);
+                    const auto prevStorage = prevVars.porosity()*prevVars.density(phaseIdx)*prevVars.saturation(phaseIdx);
+
+                    residual[localDofIdx][conti0EqIdx + phaseIdx]
+                        += integralShapeFunctions[localDofIdx]*(curStorage - prevStorage)/timeStepSize;
+                }
+            }
+        }
+    }
+
+    /*!
+     * \brief Add the flux and source contribution of the local dofs owning no control volume
+     *
+     * \note This is the weak form of the mass balance tested with the shape function of the
+     *       degree of freedom, \f$\int \rho \lambda \mathbf{v} \cdot \nabla N_i - \int N_i q\f$.
+     */
+    void addToElementFluxAndSourceResidual(ElementResidualVector& residual,
+                                           const Problem& problem,
+                                           const Element& element,
+                                           const ElementDiscretization& elemDisc,
+                                           const ElementVariables& elemVars) const
+    {
+        if constexpr (Dumux::Detail::LocalDofs::hasNonCVLocalDofsInterface<ElementDiscretization>())
+        {
+            if (nonCVLocalDofs(elemDisc).empty())
+                return;
+
+            for (const auto& qpData : Dumux::CVFE::quadratureRule(elemDisc, element))
+            {
+                const auto& ipData = qpData.ipData();
+                const auto& ipCache = cache(elemVars, ipData);
+                const auto& shapeValues = ipCache.shapeValues();
+                const auto source = problem.source(elemDisc, elemVars, ipData);
+
+                for (int phaseIdx = 0; phaseIdx < numPhases; ++phaseIdx)
+                {
+                    // There is no face across which an upwind direction could be defined, so the
+                    // mobility is part of the tensor and interpolated like every other coefficient.
+                    const auto tensor = [phaseIdx] (const auto& vars)
+                    {
+                        auto permeability = vars.permeability();
+                        permeability *= vars.extrusionFactor()
+                                        * vars.density(phaseIdx) * vars.mobility(phaseIdx);
+                        return permeability;
+                    };
+
+                    const auto fluxTerm = AdvectionType::fluxTerm(
+                        problem, element, elemDisc, elemVars, phaseIdx, ipData, tensor
+                    );
+
+                    const auto eqIdx = conti0EqIdx + phaseIdx;
+                    for (const auto& localDof : nonCVLocalDofs(elemDisc))
+                    {
+                        const auto localDofIdx = localDof.index();
+                        const auto flux = -(fluxTerm*ipCache.gradN(localDofIdx));
+                        residual[localDofIdx][eqIdx]
+                            += qpData.weight()*(flux - shapeValues[localDofIdx][0]*source[eqIdx]);
+                    }
+                }
+            }
+        }
+    }
+
 };
 
 } // end namespace Dumux::Experimental
